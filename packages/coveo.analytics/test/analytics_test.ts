de97e57b8119593fae1e7b7a97b0e63556968c6a
Pierre-Alexandre St-Jean <pastjean@coveo.com>
2016-05-05 11:30:57 -0400

    Tests for more things that can happen
    Original-commit: 503f4b01f5742ecc0fcffa7271625df56d3565ec

--- packages/coveo.analytics/test/analytics_test.ts
+++ packages/coveo.analytics/test/analytics_test.ts
@@ -2,20 +2,39 @@ import test from 'ava';
 import * as analytics from '../src/analytics';
 import * as express from 'express';
 import * as http from 'http';
+import * as bodyParser from 'body-parser';
 
 var app: express.Application = express();
 const server: http.Server = (<any>http).createServer(app).listen();
 app.set('port', server.address().port);
+app.use(bodyParser.json());
 
 test('Analytics: can post a view event', t => {
 
+    const viewEvent: analytics.ViewEventRequest = {location: 'here'};
     const response: analytics.ViewEventResponse = {
         raw: undefined,
         visitId : '123',
         visitorId: '213'
     };
 
-    app.post('/analytics/view', (req, res) => {
+    app.post('/analytics/view', (req: express.Request, res: express.Response) => {
+        if (req.header('authorization').indexOf('Bearer ') != 0) {
+            res.status(500).send(JSON.stringify({error: 'no auth token were provided'}));
+            return;
+        }
+        if (req.header('content-type').indexOf('application/json') != 0) {
+            res.status(500).send(JSON.stringify({error: 'you must provide content-type'}));
+            return;
+        }
+
+        if (req.body === undefined || req.body.length <= 0) {
+            res.status(500).send(JSON.stringify({error: 'no event were sent'}));
+            return;
+        }
+
+        t.is(req.body.location, viewEvent.location);
+
         res.status(200).send(JSON.stringify(response));
     });
 
@@ -24,9 +43,9 @@ test('Analytics: can post a view event', t => {
         endpoint: `http://localhost:${server.address().port}`
     });
 
-    return client.sendViewEvent({location: 'here'}).then((res: analytics.ViewEventResponse) => {
+    return client.sendViewEvent(viewEvent).then((res: analytics.ViewEventResponse) => {
         t.not(res.raw, undefined);
         t.is(res.visitId, response.visitId);
         t.is(res.visitorId, response.visitorId);
-    });
+    }, (error) => t.fail());
 });
