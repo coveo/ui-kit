95293665815357077c6c3be74c06b4170953912a
Mathieu Carpentier <mcarpentier@coveo.com>
2016-05-10 14:43:14 -0400

    Fix build errors https://coveord.atlassian.net/browse/JSUI-769
    Original-commit: 52bec9b259ea59377feecb03bc764ef5d479bdff

--- packages/coveo.analytics/test/history_test.ts
+++ packages/coveo.analytics/test/history_test.ts
@@ -11,36 +11,36 @@ test.beforeEach(t => {
   storage = new NullStorage();
   storageMock = sinon.mock(storage);
   historyStore = new history.HistoryStore(storage);
-})
+});
 
 test.afterEach( t => {
   storage = null;
   storageMock = null;
   historyStore = null;
-})
+});
 
 test('HistoryStore should be able to add an element in the history', t => {
-  let data = "test";
-  storageMock.expects("setItem").once().withArgs(history.STORE_KEY, sinon.match(data));
+  let data = 'test';
+  storageMock.expects('setItem').once().withArgs(history.STORE_KEY, sinon.match(data));
   historyStore.addElement(data);
   storageMock.verify();
-})
+});
 
 test('HistoryStore should be able to get the history', t => {
-  storageMock.expects("getItem").once().withArgs(history.STORE_KEY);
+  storageMock.expects('getItem').once().withArgs(history.STORE_KEY);
   historyStore.getHistory();
   storageMock.verify();
-})
+});
 
 test('HistoryStore should remove item when cleared', t => {
-  storageMock.expects("removeItem").once().withArgs(history.STORE_KEY);
+  storageMock.expects('removeItem').once().withArgs(history.STORE_KEY);
   historyStore.clear();
   storageMock.verify();
-})
+});
 
 test('HistoryStore should be able to set the history', t => {
-  var historyElements = ["test", "another_test"]
-  storageMock.expects("setItem").once().withArgs(history.STORE_KEY, JSON.stringify(historyElements));
+  var historyElements = ['test', 'another_test'];
+  storageMock.expects('setItem').once().withArgs(history.STORE_KEY, JSON.stringify(historyElements));
   historyStore.setHistory(historyElements);
   storageMock.verify();
-})
+});
