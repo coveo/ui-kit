06a2ea887f5da749eb1f6331641bbfc14d2c7be1
Pierre-Alexandre St-Jean <pastjean@coveo.com>
2016-05-18 15:20:30 -0400

    deploy plz
    Original-commit: 683720341710cc7aece2ac1d94ce989134d83dc5

--- packages/coveo.analytics/deploy.sh
+++ packages/coveo.analytics/deploy.sh
@@ -3,10 +3,11 @@
 git config user.name "via Jenkins"
 git config user.email "jenkins@coveo.com"
 
-echo "git checkout $RELEASE_TAG"
+echo "> git checkout $RELEASE_TAG"
 git checkout $RELEASE_TAG
 
-echo "git tag -a -f latest -m \"Deploy $RELEASE_TAG\""
+echo "> git tag -a -f latest -m \"Deploy $RELEASE_TAG\""
 git tag -a -f latest -m "Deploy $RELEASE_TAG"
 
+echo "> git push --force --tags \"https://${GH_TOKEN}@github.com/coveo/coveo.analytics.js.git\" master:gh-pages > /dev/null 2>&1"
 git push --force --tags "https://${GH_TOKEN}@github.com/coveo/coveo.analytics.js.git" master:gh-pages > /dev/null 2>&1
