05252ba2a8aeb7d71e20ed6ce95333bade1c3b9e
Pierre-Alexandre St-Jean <pastjean@coveo.com>
2016-05-18 14:03:49 -0400

    Deploy plz
    Original-commit: e6b1292d31f9850d771c3424647e0939132307fd

--- packages/coveo.analytics/deploy.sh
+++ packages/coveo.analytics/deploy.sh
@@ -1,2 +1,7 @@
+#!/bin/env bash
+
+echo "git checkout $RELEASE_TAG"
 git checkout $RELEASE_TAG
-git tag -a -f -m "Deploy $RELEASE_TAG" 
+
+echo "git tag -a -f latest -m \"Deploy $RELEASE_TAG\""
+git tag -a -f latest -m "Deploy $RELEASE_TAG"
