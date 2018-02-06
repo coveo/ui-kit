07749a65f3484b340ca7c7f9b452c2868a434410
Pixel <louis.vincent.bompart@gmail.com>
2018-02-06 18:24:05 -0500

    Add polyfill guard condition
    Original-commit: 967d3823dbf65776a694227f51d0b36655a61bf2

--- packages/coveo.analytics/src/index.ts
+++ packages/coveo.analytics/src/index.ts
@@ -3,6 +3,13 @@ import * as SimpleAnalytics from './simpleanalytics';
 import * as history from './history';
 import * as donottrack from './donottrack';
 import * as storage from './storage';
+
+declare var require: any;
+const promise = (window as any)['Promise'];
+if (!(promise instanceof Function)) {
+  require('es6-promise').polyfill();
+}
+
 export {
     analytics,
     donottrack,
