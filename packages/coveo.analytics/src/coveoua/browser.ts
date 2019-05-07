437d41caea14bcbf1b2e6a6d8b8991cc73de6336
Francois Lachance-Guillemette <francois.lguillemette@gmail.com>
2019-05-07 16:47:21 -0400

    Also add a message for window.fetch polyfill
    Original-commit: 4f87d92351cca407389a00af27e8516438234141

--- packages/coveo.analytics/src/coveoua/browser.ts
+++ packages/coveo.analytics/src/coveoua/browser.ts
@@ -7,13 +7,17 @@ const promise = (window as any)['Promise'];
 if (!(promise instanceof Function)) {
     console.error(`This script uses window.Promise which is not supported in your browser. Consider adding a polyfill like "es6-promise".`);
 }
+const fetch = (window as any)['fetch'];
+if (!(fetch instanceof Function)) {
+    console.error(`This script uses window.fetch which is not supported in your browser. Consider adding a polyfill like "fetch".`);
+}
 
 // CoveoUAGlobal is the interface for the global function which also has a
 // queue `q` of unexecuted parameters
 export interface CoveoUAGlobal {
     (action: string, ...params: string[]): void;
     // CoveoAnalytics.q is the queue of last called actions before lib was included
-    q?: string[][];
+    q?: [string, any[]][];
 }
 
 // On load of this script we get the global object `coveoua` (which would be)
@@ -28,7 +32,7 @@ global.coveoanalytics = analytics;
 // On normal execution this library should be loaded after the snippet execution
 // so we will execute the actions in the `q` array
 if (coveoua.q) {
-    coveoua.q.forEach(([eventName, ...args]: any[]) => handleOneAnalyticsEvent.call(void 0, eventName, args));
+    coveoua.q.forEach((args: [string, any[]]) => handleOneAnalyticsEvent.apply(void 0, args));
 }
 
 export default coveoua;
