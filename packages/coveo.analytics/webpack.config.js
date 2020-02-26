1c63dca0fea559cc0b28a6fa73bf89bf116b946e
Francois Lachance-Guillemette <francois.lguillemette@gmail.com>
2020-02-26 15:26:01 -0500

    Add prettier instead of tslint
    Original-commit: 500f08396714b47046ecbd1631d6a120bfccd218

--- packages/coveo.analytics/webpack.config.js
+++ packages/coveo.analytics/webpack.config.js
@@ -15,22 +15,12 @@ module.exports = {
     },
     devtool: "source-map",
     module: {
-        rules: [{
-            enforce: 'pre',
-            test: /\.ts$/,
-            use: {
-                loader: 'tslint-loader',
-                options: {
-                    emitErrors: true,
-                    failOnHint: true,
-                    formattersDirectory: "node_modules/tslint-loader/formatters/",
-                    configuration: require(__dirname + '/tslint.json')
-                }
-            }
-        }, {
-            test: /\.ts$/,
-            loader: 'ts-loader',
-        }]
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+            },
+        ],
     },
     devServer: {
         contentBase: [path.join(__dirname, "public"), path.join(__dirname, "dist")]
