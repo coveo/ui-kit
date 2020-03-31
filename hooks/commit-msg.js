b6f94ecc12501ab5cb58d636323307fd87141dc6
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2020-03-31 17:24:31 -0400

    Add commit msg hooks
    https://coveord.atlassian.net/browse/KIT-1

---
+++ hooks/commit-msg.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+/* eslint-disable no-process-exit */
+/* eslint-disable node/shebang */
+
+'use strict';
+
+const childProcess = require('child_process');
+const fs = require('fs');
+const os = require('os');
+
+const urlBase = 'https://coveord.atlassian.net/browse/';
+const projectAcronym = 'KIT';
+
+let issueNumber;
+const branchName = childProcess
+  .execSync('git symbolic-ref --short HEAD', {encoding: 'utf8'})
+  .trim();
+const issueRegex = new RegExp(projectAcronym + '-[\\d]+', 'i');
+
+const match = branchName.match(issueRegex);
+if (match) {
+  issueNumber = match[0].toUpperCase();
+} else {
+  console.log(
+    `Could not find JIRA issue in branch name. Is your branch named ${projectAcronym}-<jira number>?`
+  );
+  process.exit();
+}
+
+const commitMessageFilename = '.git/COMMIT_EDITMSG';
+const commitMessage = fs.readFileSync(commitMessageFilename, {
+  encoding: 'utf8',
+});
+
+function commitHasIssue(commitMessage) {
+  const urlIssueRegex = new RegExp(urlBase + issueRegex.source);
+  return commitMessage.search(urlIssueRegex) !== -1;
+}
+
+function commitHasIssueNumber(commitMessage, issueNumber) {
+  return commitMessage.indexOf(urlBase + issueNumber) !== -1;
+}
+
+if (commitHasIssueNumber(commitMessage, issueNumber)) {
+  process.exit(0);
+}
+
+if (commitHasIssue(commitMessage)) {
+  console.log("Oops... Branch name and issue in commit message don't match");
+  process.exit(1);
+}
+
+fs.appendFileSync(commitMessageFilename, os.EOL + urlBase + issueNumber);
+console.log(`Appended ${urlBase}${issueNumber} to commit message`);
