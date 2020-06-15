aedb6fec5a57a1176be085b99cf960960d0c29a7
Olivier Lamothe <olamothe@coveo.com>
2020-06-15 19:41:40 +0000

    Merged in KIT-43 (pull request #43)
    KIT-43 Update coveo.analytics
    
    https://coveord.atlassian.net/browse/KIT-43
    
    Approved-by: Sami Sayegh

--- benchmark/bundle-size/bundle-size.js
+++ benchmark/bundle-size/bundle-size.js
@@ -6,6 +6,11 @@ const exec = promisify(require('child_process').exec);
 const sourceBranch = process.env.BITBUCKET_SOURCE_BRANCH || 'unknown';
 const targetBranch = process.env.BITBUCKET_TARGET_BRANCH || 'master';
 
+async function deleteNodeModules() {
+  console.log('deleting node_modules');
+  await exec('rm -rf packages/headless/node_modules');
+}
+
 async function discardChanges() {
   console.log('discarding changes');
   await exec('git checkout -- .');
@@ -20,6 +25,7 @@ async function main() {
   console.log(`on branch: ${sourceBranch}`);
   const newSizes = await computeFileSizes();
 
+  await deleteNodeModules();
   await discardChanges();
   await checkoutTargetBranch();
 
