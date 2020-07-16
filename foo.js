49c8251dac6183594fd34943e1e65ed7a4fad1c3
olamothe <olamothe@coveo.com>
2020-07-16 17:30:40 -0400

    https://coveord.atlassian.net/browse/KIT-74

--- foo.js
+++ foo.js
@@ -1,1 +1,29 @@
-console.log('asdfss');
+const lint = require('@commitlint/lint');
+const conventialConfig = require('@commitlint/config-conventional');
+const lernaConfig = require('@commitlint/config-lerna-scopes');
+
+console.log(conventialConfig.rules);
+
+const getLernaPackages = async () => {
+  return await lernaConfig.rules['scope-enum']();
+};
+
+const doLint = async () => {
+  const packages = await getLernaPackages();
+  lint
+    .default(
+      `
+    chore: qwww
+    www
+    `,
+      {
+        ...conventialConfig.rules,
+        'scope-enum': packages,
+      }
+    )
+    .then((report) => console.log(report));
+};
+
+doLint();
+
+//console.log()
