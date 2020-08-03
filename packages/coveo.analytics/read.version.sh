5fb6a4ea5631cd06fba02fdb57337c95ce0c2a1c
François Lachance-Guillemette <flguillemette@coveo.com>
2020-08-03 14:59:59 -0400

    Invalidate cloudfront cache when deploying
    [COM-420]
    
    Original-commit: f804012ef1040474f6254cb6e3cbd20e993bc5b5

--- packages/coveo.analytics/read.version.sh
+++ packages/coveo.analytics/read.version.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+export PACKAGE_JSON_MAJOR_MINOR_PATCH_VERSION=`cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | xargs`
 export PACKAGE_JSON_MAJOR_MINOR_VERSION=`cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | sed -E 's/\.[0-9]+$//g' | xargs`
+export PACKAGE_JSON_MAJOR_VERSION=`cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | sed -E 's/\.[0-9]+\.[0-9]+$//g' | xargs`
