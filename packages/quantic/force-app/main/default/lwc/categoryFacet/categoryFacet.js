809710695257a25517a82d6175cf38d139b39b96
Nafees Rawji <nrawji@coveo.com>
2020-10-09 19:14:05 +0000

    Merged in KIT-146 (pull request #175)
    KIT-146 feat(quantic): created CategoryFacet component
    
    https://coveord.atlassian.net/browse/KIT-146

---
+++ packages/quantic/force-app/main/default/lwc/categoryFacet/categoryFacet.js
@@ -0,0 +1,94 @@
+import {api, LightningElement, track} from 'lwc';
+import { initializeComponent } from 'c/initialization';
+
+export default class CategoryFacet extends LightningElement {
+  /** @type {import("coveo").CategoryFacetState} */
+  @track state = {
+    values: [],
+    parents: []
+  };
+  /** @type {string} */
+  @api field;
+  /** @type {string} */
+  @api label;
+
+  /** @type {import("coveo").CategoryFacet}} */
+  facet;
+  /** @type {import("coveo").Unsubscribe} */
+  unsubscribe;
+
+  connectedCallback() {
+    initializeComponent(this);
+  }
+
+  /**
+   * @param {import("coveo").Engine} engine
+   */
+  @api
+  initialize(engine) {
+    this.facet = CoveoHeadless.buildCategoryFacet(engine, {
+      options: {
+        field: this.field,
+        delimitingCharacter: ';'
+      },
+    });
+    this.unsubscribe = this.facet.subscribe(() => this.updateState());
+  }
+
+  updateState() {
+    this.state = this.facet.state;
+  }
+
+  get values() {
+    return this.state.values;
+  }
+
+  get parents() {
+    return this.state.parents;
+  }
+
+  get canShowMore() {
+    if (!this.facet) {
+      return false;
+    }
+    return this.state.canShowMoreValues;
+  }
+
+  get canShowLess() {
+    if (!this.facet) {
+      return false;
+    }
+    return this.state.canShowLessValues;
+  }
+
+  get hasParents() {
+    return this.state.parents.length !== 0;
+  }
+
+  get hasValues() {
+    return this.state.values.length !== 0
+  }
+
+  get hasParentsOrValues() {
+    return this.hasParents || this.hasValues;
+  }
+
+  /**
+   * @param {CustomEvent<import("coveo").CategoryFacetValue>} evt
+   */
+  onSelect(evt) {
+    this.facet.toggleSelect(evt.detail);
+  }
+
+  showMore() {
+    this.facet.showMoreValues();
+  }
+
+  showLess() {
+    this.facet.showLessValues();
+  }
+
+  reset() {
+    this.facet.deselectAll();
+  }
+}
