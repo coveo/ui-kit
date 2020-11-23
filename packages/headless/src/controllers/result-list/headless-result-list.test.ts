7619d69d82754b839370740962edb5de0904f6c7
Nafees Rawji <nrawji@coveo.com>
2020-11-22 22:16:04 -0500

    feat(headless): add fieldsToInclude on headless result list controller
    KIT-257
    
    https://coveord.atlassian.net/browse/KIT-257

--- packages/headless/src/controllers/result-list/headless-result-list.test.ts
+++ packages/headless/src/controllers/result-list/headless-result-list.test.ts
@@ -1,8 +1,26 @@
 import {buildResultList} from './headless-result-list';
-import {buildMockSearchAppEngine} from '../../test/mock-engine';
+import {buildMockSearchAppEngine, MockEngine} from '../../test/mock-engine';
+import {SearchAppState} from '../..';
+import {registerFieldsToInclude} from '../../features/fields/fields-actions';
 
 describe('ResultList', () => {
-  it('initializes correctly', () => {
-    expect(buildResultList(buildMockSearchAppEngine())).toBeTruthy();
+  let engine: MockEngine<SearchAppState>;
+
+  beforeEach(() => {
+    engine = buildMockSearchAppEngine();
+  });
+
+  it('initializes correctly with no fields to include', () => {
+    expect(buildResultList(engine)).toBeTruthy();
+    expect(engine.actions).not.toContainEqual(registerFieldsToInclude([]));
+  });
+
+  it('initializes correctly with fields to include', () => {
+    expect(
+      buildResultList(engine, {
+        options: {fieldsToInclude: ['test']},
+      })
+    ).toBeTruthy();
+    expect(engine.actions).toContainEqual(registerFieldsToInclude(['test']));
   });
 });
--- packages/headless/src/controllers/result-list/headless-result-list.ts
+++ packages/headless/src/controllers/result-list/headless-result-list.ts
@@ -1,13 +1,29 @@
 import {Engine} from '../../app/headless-engine';
 import {SearchSection} from '../../state/state-sections';
 import {buildController} from '../controller/headless-controller';
+import {registerFieldsToInclude} from '../../features/fields/fields-actions';
+
+type ResultListOptions = {
+  fieldsToInclude?: string[];
+};
+
+type ResultListProps = {
+  options?: ResultListOptions;
+};
 
 /** The state relevant to the `ResultList` controller.*/
 export type ResultListState = ResultList['state'];
 export type ResultList = ReturnType<typeof buildResultList>;
 
-export const buildResultList = (engine: Engine<SearchSection>) => {
+export const buildResultList = (
+  engine: Engine<SearchSection>,
+  props?: ResultListProps
+) => {
   const controller = buildController(engine);
+  const {dispatch} = engine;
+  if (props?.options?.fieldsToInclude) {
+    dispatch(registerFieldsToInclude(props.options.fieldsToInclude));
+  }
 
   return {
     ...controller,
