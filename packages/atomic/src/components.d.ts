7a38eab3296c39f5a0704fb9aa2d0c6338a7b01d
nrawji <nrawji@coveo.com>
2020-11-30 16:08:55 -0500

    Merged with master
    https://coveord.atlassian.net/browse/KIT-280

--- packages/atomic/src/components.d.ts
+++ packages/atomic/src/components.d.ts
@@ -59,6 +59,7 @@ export namespace Components {
     interface AtomicResultLink {
     }
     interface AtomicResultList {
+        "fieldsToInclude": string;
         /**
           * Css class for the list wrapper
          */
@@ -335,6 +336,7 @@ declare namespace LocalJSX {
     interface AtomicResultLink {
     }
     interface AtomicResultList {
+        "fieldsToInclude"?: string;
         /**
           * Css class for the list wrapper
          */
