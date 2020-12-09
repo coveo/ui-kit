ce59ff0cb6b743f69289cd5103246e7605407aef
Sami Sayegh <ssayegh@coveo.com>
2020-12-09 15:34:01 -0500

    document range facets
    https://coveord.atlassian.net/browse/KIT-306

--- packages/headless/src/features/facets/range-facets/generic/interfaces/request.ts
+++ packages/headless/src/features/facets/range-facets/generic/interfaces/request.ts
@@ -8,6 +8,12 @@ export const rangeFacetSortCriteria = ['ascending', 'descending'] as const;
 export type RangeFacetSortCriterion = typeof rangeFacetSortCriteria[number];
 
 export interface AutomaticRanges<T extends boolean> {
+  /** Whether the index should automatically create range values.
+   *
+   * Tip: If you set this parameter to true, you should ensure that the Use cache for numeric queries option is enabled for the Facet field in your index in order to speed up automatic range evaluation (see [Add or Edit Fields](https://docs.coveo.com/en/1982/index-content/add-or-edit-a-field)).
+   *
+   * @default false
+   */
   generateAutomaticRanges: T;
 }
 
@@ -21,4 +27,7 @@ export interface RangeRequest<T extends string | number>
 export interface BaseRangeFacetRequest
   extends BaseFacetRequest,
     AutomaticRanges<boolean>,
-    SortCriteria<RangeFacetSortCriterion> {}
+    SortCriteria<RangeFacetSortCriterion> {
+  /** @default "ascending" */
+  sortCriteria: RangeFacetSortCriterion;
+}
