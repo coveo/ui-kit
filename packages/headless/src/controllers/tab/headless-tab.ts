4e871418b5ab41fec5620a29b5f01bd89e47e56c
olamothe <olamothe@coveo.com>
2020-12-15 11:19:17 -0500

    docs(headless): headless tab annotation/documentation
    https://coveord.atlassian.net/browse/KIT-321

--- packages/headless/src/controllers/tab/headless-tab.ts
+++ packages/headless/src/controllers/tab/headless-tab.ts
@@ -7,14 +7,29 @@ import {
   AdvancedSearchQueriesSection,
   ConfigurationSection,
 } from '../../state/state-sections';
-import {BooleanValue, Schema, SchemaValues, StringValue} from '@coveo/bueno';
+import {BooleanValue, Schema, StringValue} from '@coveo/bueno';
 import {
   validateInitialState,
   validateOptions,
 } from '../../utils/validate-payload';
 
 type TabOptions = {
+  /**
+   * A constant query expression or filter that the Tab should add to any outgoing query.
+   *
+   * **Example:**
+   *
+   * `@objecttype==Message`
+   */
   expression: string;
+};
+
+type TabInitialState = {
+  /**
+   * Specifies if the tab is currently active, or selected.
+   * Note that there can be only one active tab for a given headless engine.
+   */
+  isActive: boolean;
 };
 
 const optionsSchema = new Schema({
@@ -26,15 +41,26 @@ const initialStateSchema = new Schema({
 });
 
 export interface TabProps {
+  /**
+   * The options for the `Tab` controller.
+   */
   options: TabOptions;
+  /**
+   * The initial state that should be applied to this `Tab` controller.
+   */
   initialState?: TabInitialState;
 }
 
-export type TabInitialState = SchemaValues<typeof initialStateSchema>;
-
 export type Tab = ReturnType<typeof buildTab>;
+/**
+ * A scoped and simplified part of the headless state that is relevant to the `Tab` controller.
+ */
 export type TabState = Tab['state'];
 
+/**
+ * The `Tab` headless controller offers a high-level interface that allows the end user to select a specific search interface.
+ * It is in charge of adding a constant expression to the outgoing query in order to refine the results.
+ */
 export function buildTab(
   engine: Engine<ConfigurationSection & AdvancedSearchQueriesSection>,
   props: TabProps
@@ -62,13 +88,16 @@ export function buildTab(
   return {
     ...controller,
     /**
-     * Makes this tab the active one
+     * Select and make this tab the currently active one.
      */
     select() {
       dispatch(updateAdvancedSearchQueries({cq: options.expression}));
       dispatch(executeSearch(logInterfaceChange()));
     },
 
+    /**
+     * @returns (TabState) The state of the `Tab` controller.
+     */
     get state() {
       const isActive =
         engine.state.advancedSearchQueries.cq === options.expression;
