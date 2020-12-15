b74d439012825c0f556c291d083f1ec5999ccbb2
Jean-Felix Thibodeau <thibodeaujf@gmail.com>
2020-12-15 17:20:55 -0500

    refactor and add UTs
    https://coveord.atlassian.net/browse/KIT-316

--- packages/headless/src/controllers/breadcrumb-manager/headless-breadcrumb-manager.test.ts
+++ packages/headless/src/controllers/breadcrumb-manager/headless-breadcrumb-manager.test.ts
@@ -9,6 +9,10 @@ import {buildMockFacetResponse} from '../../test/mock-facet-response';
 import {
   BreadcrumbManager,
   buildBreadcrumbManager,
+  CategoryFacetBreadcrumb,
+  DateFacetBreadcrumb,
+  FacetBreadcrumb,
+  NumericFacetBreadcrumb,
 } from './headless-breadcrumb-manager';
 import {SearchAppState} from '../../state/search-app-state';
 import {buildMockDateFacetRequest} from '../../test/mock-date-facet-request';
@@ -22,6 +26,15 @@ import {buildMockCategoryFacetValue} from '../../test/mock-category-facet-value'
 import {buildMockCategoryFacetResponse} from '../../test/mock-category-facet-response';
 import {deselectAllFacets} from '../../features/facets/generic/facet-actions';
 import {executeSearch} from '../../features/search/search-actions';
+import {FacetValue} from '../../features/facets/facet-set/interfaces/response';
+import {getSearchInitialState} from '../../features/search/search-state';
+import {executeFacetBreadcrumb} from '../../features/facets/facet-set/facet-set-controller-actions';
+import {DateFacetValue} from '../../features/facets/range-facets/date-facet-set/interfaces/response';
+import {executeDateFacetBreadcrumb} from '../../features/facets/range-facets/date-facet-set/date-facet-controller-actions';
+import {NumericFacetValue} from '../../features/facets/range-facets/numeric-facet-set/interfaces/response';
+import {executeNumericFacetBreadcrumb} from '../../features/facets/range-facets/numeric-facet-set/numeric-facet-controller-actions';
+import {CategoryFacetValue} from '../../features/facets/category-facet-set/interfaces/response';
+import {executeDeselectAllCategoryFacetValues} from '../../features/facets/category-facet-set/category-facet-set-controller-actions';
 
 describe('headless breadcrumb manager', () => {
   const facetId = 'abc123';
@@ -29,59 +42,177 @@ describe('headless breadcrumb manager', () => {
   let state: SearchAppState;
   let breadcrumbManager: BreadcrumbManager;
 
-  beforeEach(() => {
-    state = createMockState();
+  function initController(mockState: SearchAppState = createMockState()) {
+    state = mockState;
     engine = buildMockSearchAppEngine({state});
     breadcrumbManager = buildBreadcrumbManager(engine);
+  }
+
+  beforeEach(() => {
+    initController();
   });
 
-  it('#state gets facet breadcrumbs correctly', () => {
-    state.facetSet[facetId] = buildMockFacetRequest({facetId});
-    const mockValue = buildMockFacetValue({state: 'selected'});
-    state.search.response.facets = [
-      buildMockFacetResponse({facetId, values: [mockValue]}),
-    ];
-    const facetBreadcrumbs = breadcrumbManager.state.facetBreadcrumbs;
-    expect(facetBreadcrumbs?.[0]?.values[0].value).toBe(mockValue);
-  });
+  describe('facet breadcrumbs', () => {
+    let mockValue: FacetValue;
+    let facetBreadcrumbs: FacetBreadcrumb[];
 
-  it('#state gets date facet breadcrumbs correctly', () => {
-    state.dateFacetSet[facetId] = buildMockDateFacetRequest({facetId});
-    const mockValue = buildMockDateFacetValue({state: 'selected'});
-    state.search.response.facets = [
-      buildMockDateFacetResponse({facetId, values: [mockValue]}),
-    ];
-    const facetBreadcrumbs = breadcrumbManager.state.dateFacetBreadcrumbs;
-    expect(facetBreadcrumbs?.[0]?.values[0].value).toBe(mockValue);
-  });
+    beforeEach(() => {
+      mockValue = buildMockFacetValue({state: 'selected'});
 
-  it('#state gets numeric facet breadcrumbs correctly', () => {
-    state.numericFacetSet[facetId] = buildMockNumericFacetRequest({facetId});
-    const mockValue = buildMockNumericFacetValue({state: 'selected'});
-    state.search.response.facets = [
-      buildMockNumericFacetResponse({facetId, values: [mockValue]}),
-    ];
-    const facetBreadcrumbs = breadcrumbManager.state.numericFacetBreadcrumbs;
-    expect(facetBreadcrumbs?.[0]?.values[0].value).toBe(mockValue);
-  });
+      initController(
+        createMockState({
+          search: {
+            ...getSearchInitialState(),
+            response: {
+              ...getSearchInitialState().response,
+              facets: [buildMockFacetResponse({facetId, values: [mockValue]})],
+            },
+          },
+          facetSet: {
+            [facetId]: buildMockFacetRequest({facetId}),
+          },
+        })
+      );
 
-  it('#state gets category facet breadcrumbs correctly', () => {
-    const otherFacetId = 'def456';
-    state.categoryFacetSet[facetId] = buildMockCategoryFacetRequest({facetId});
-    state.categoryFacetSet[otherFacetId] = buildMockCategoryFacetRequest({
-      facetId: otherFacetId,
+      facetBreadcrumbs = breadcrumbManager.state.facetBreadcrumbs;
     });
-    const mockValue = buildMockCategoryFacetValue({state: 'selected'});
-    state.search.response.facets = [
-      buildMockCategoryFacetResponse({facetId, values: [mockValue]}),
-      buildMockCategoryFacetResponse({
-        facetId: otherFacetId,
-        values: [mockValue],
-      }),
-    ];
-    const facetBreadcrumbs = breadcrumbManager.state.categoryFacetBreadcrumbs;
-    expect(facetBreadcrumbs?.[0].path).toEqual([mockValue]);
-    expect(facetBreadcrumbs?.[1].path).toEqual([mockValue]);
+
+    it('#state gets facet breadcrumbs correctly', () => {
+      expect(facetBreadcrumbs[0].values[0].value).toBe(mockValue);
+    });
+
+    it('dispatches an executeFacetBreadcrumb action on deselection', () => {
+      facetBreadcrumbs[0].values[0].deselect();
+      expect(
+        engine.findAsyncAction(executeFacetBreadcrumb.pending)
+      ).toBeTruthy();
+    });
+  });
+
+  describe('date facet breadcrumbs', () => {
+    let mockValue: DateFacetValue;
+    let facetBreadcrumbs: DateFacetBreadcrumb[];
+
+    beforeEach(() => {
+      mockValue = buildMockDateFacetValue({state: 'selected'});
+
+      initController(
+        createMockState({
+          search: {
+            ...getSearchInitialState(),
+            response: {
+              ...getSearchInitialState().response,
+              facets: [
+                buildMockDateFacetResponse({facetId, values: [mockValue]}),
+              ],
+            },
+          },
+          dateFacetSet: {
+            [facetId]: buildMockDateFacetRequest({facetId}),
+          },
+        })
+      );
+
+      facetBreadcrumbs = breadcrumbManager.state.dateFacetBreadcrumbs;
+    });
+
+    it('#state gets date facet breadcrumbs correctly', () => {
+      expect(facetBreadcrumbs[0].values[0].value).toBe(mockValue);
+    });
+
+    it('dispatches an executeDateFacetBreadcrumb action on deselection', () => {
+      facetBreadcrumbs[0].values[0].deselect();
+      expect(
+        engine.findAsyncAction(executeDateFacetBreadcrumb.pending)
+      ).toBeTruthy();
+    });
+  });
+
+  describe('numeric facet breadcrumbs', () => {
+    let mockValue: NumericFacetValue;
+    let facetBreadcrumbs: NumericFacetBreadcrumb[];
+
+    beforeEach(() => {
+      mockValue = buildMockNumericFacetValue({state: 'selected'});
+
+      initController(
+        createMockState({
+          search: {
+            ...getSearchInitialState(),
+            response: {
+              ...getSearchInitialState().response,
+              facets: [
+                buildMockNumericFacetResponse({facetId, values: [mockValue]}),
+              ],
+            },
+          },
+          numericFacetSet: {
+            [facetId]: buildMockNumericFacetRequest({facetId}),
+          },
+        })
+      );
+
+      facetBreadcrumbs = breadcrumbManager.state.numericFacetBreadcrumbs;
+    });
+
+    it('#state gets numeric facet breadcrumbs correctly', () => {
+      expect(facetBreadcrumbs[0].values[0].value).toBe(mockValue);
+    });
+
+    it('dispatches an executeNumericFacetBreadcrumb action on deselection', () => {
+      facetBreadcrumbs[0].values[0].deselect();
+      expect(
+        engine.findAsyncAction(executeNumericFacetBreadcrumb.pending)
+      ).toBeTruthy();
+    });
+  });
+
+  describe('category facet breadcrumbs', () => {
+    let mockValue: CategoryFacetValue;
+    let facetBreadcrumbs: CategoryFacetBreadcrumb[];
+    const otherFacetId = 'def456';
+
+    beforeEach(() => {
+      mockValue = buildMockCategoryFacetValue({state: 'selected'});
+
+      initController(
+        createMockState({
+          search: {
+            ...getSearchInitialState(),
+            response: {
+              ...getSearchInitialState().response,
+              facets: [
+                buildMockCategoryFacetResponse({facetId, values: [mockValue]}),
+                buildMockCategoryFacetResponse({
+                  facetId: otherFacetId,
+                  values: [mockValue],
+                }),
+              ],
+            },
+          },
+          categoryFacetSet: {
+            [facetId]: buildMockCategoryFacetRequest({facetId}),
+            [otherFacetId]: buildMockCategoryFacetRequest({
+              facetId: otherFacetId,
+            }),
+          },
+        })
+      );
+
+      facetBreadcrumbs = breadcrumbManager.state.categoryFacetBreadcrumbs;
+    });
+
+    it('#state gets category facet breadcrumbs correctly', () => {
+      expect(facetBreadcrumbs[0].path).toEqual([mockValue]);
+      expect(facetBreadcrumbs[1].path).toEqual([mockValue]);
+    });
+
+    it('dispatches an executeDeselectAllCategoryFacetValues action on deselection', () => {
+      facetBreadcrumbs[0].deselect();
+      expect(
+        engine.findAsyncAction(executeDeselectAllCategoryFacetValues.pending)
+      ).toBeTruthy();
+    });
   });
 
   it('hasBreadcrumbs returns true when a facet value is selected', () => {
