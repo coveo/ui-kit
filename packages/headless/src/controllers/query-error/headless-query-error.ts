789e43d5fbae5f736f4cb98c0d6652294f3955d5
olamothe <olamothe@coveo.com>
2020-12-16 10:21:39 -0500

    docs(headless): documentation improvement for query error
    https://coveord.atlassian.net/browse/KIT-328

--- packages/headless/src/controllers/query-error/headless-query-error.ts
+++ packages/headless/src/controllers/query-error/headless-query-error.ts
@@ -6,6 +6,9 @@ import {buildController} from '../controller/headless-controller';
  * `QueryError` controller allows to retrieve information about the current error returned by the search API, if any.
  */
 export type QueryError = ReturnType<typeof buildQueryError>;
+/**
+ * A scoped and simplified part of the headless state that is relevant to the `QueryError` controller.
+ */
 export type QueryErrorState = QueryError['state'];
 
 export const buildQueryError = (engine: Engine<SearchSection>) => {
@@ -13,10 +16,18 @@ export const buildQueryError = (engine: Engine<SearchSection>) => {
 
   return {
     ...controller,
-
+    /**
+     * A scoped and simplified part of the headless state that is relevant to the `Pager` controller.
+     */
     get state() {
       return {
+        /**
+         * Determine if there is an error for the last executed query.
+         */
         hasError: engine.state.search.error !== null,
+        /**
+         * The current error for the last executed query, or `null` if none is present.
+         */
         error: engine.state.search.error,
       };
     },
