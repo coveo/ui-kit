97f9e3a537af5f6a5322913d3910a0f15211612f
jgeddes-coveo <66330619+jgeddes-coveo@users.noreply.github.com>
2020-12-17 14:22:46 -0500

    Update packages/headless/src/controllers/results-per-page/headless-results-per-page.ts
    Co-authored-by: Kaitlyn Katz <64869151+kaitlynkatz@users.noreply.github.com>

--- packages/headless/src/controllers/results-per-page/headless-results-per-page.ts
+++ packages/headless/src/controllers/results-per-page/headless-results-per-page.ts
@@ -81,7 +81,7 @@ export function buildResultsPerPage(
 
     /** Checks whether the number of results per page is equal to the specified number.
      * @param num The number of results.
-     * @returns Is `true` if the number of results is equal to the passed value, and `false` otherwise.
+     * @returns `true` if the number of results is equal to the passed value, and `false` otherwise.
      */
     isSetTo(num: number) {
       return num === this.state.numberOfResults;
