41879599fd91b426faff3ea5cbcb29c50eda349b
Olivier Lamothe <olamothe@coveo.com>
2020-12-17 11:38:33 -0500

    docs(headless): document state section + cleanup result interface (#378)
    https://coveord.atlassian.net/browse/KIT-332

--- packages/headless/src/api/search/facet-search/category-facet-search/category-facet-search-response.ts
+++ packages/headless/src/api/search/facet-search/category-facet-search/category-facet-search-response.ts
@@ -4,6 +4,9 @@ import {
 } from '../base/base-facet-search-response';
 
 export interface CategoryFacetSearchResult extends BaseFacetSearchResult {
+  /**
+   * The hierarchical path to the value.
+   */
   path: string[];
 }
 
