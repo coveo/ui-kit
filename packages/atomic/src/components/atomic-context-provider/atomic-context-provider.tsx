0682c1dcdcf6720cd07f899c71d2aa8fc4488a04
Nafees Rawji <nrawji@coveo.com>
2020-12-18 09:35:19 -0500

    feat(headless): send context info inside analytics event payload
    KIT-333
    
    https://coveord.atlassian.net/browse/KIT-333

---
+++ packages/atomic/src/components/atomic-context-provider/atomic-context-provider.tsx
@@ -0,0 +1,19 @@
+import {Component, Prop} from '@stencil/core';
+import {buildContext, Engine} from '@coveo/headless';
+import {Initialization} from '../../utils/initialization-utils';
+
+@Component({
+  tag: 'atomic-context-provider',
+  shadow: false,
+})
+export class AtomicContextProvider {
+  @Prop() context = '{}';
+  private engine!: Engine;
+
+  @Initialization()
+  public initialize() {
+    const context = buildContext(this.engine);
+    const contextObject = JSON.parse(this.context);
+    context.set(contextObject);
+  }
+}
