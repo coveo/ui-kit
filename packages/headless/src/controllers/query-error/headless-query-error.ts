4917f3ad7268a01454147ddc32d7f852e93b867f
olamothe <olamothe@coveo.com>
2021-01-12 10:50:42 -0500

    PR

--- packages/headless/src/controllers/query-error/headless-query-error.ts
+++ packages/headless/src/controllers/query-error/headless-query-error.ts
@@ -2,27 +2,27 @@ import {Engine} from '../../app/headless-engine';
 import {SearchSection} from '../../state/state-sections';
 import {buildController} from '../controller/headless-controller';
 
+/**
+ * `QueryError` controller allows to retrieve information about the current error returned by the search API, if any.
+ */
 export type QueryError = ReturnType<typeof buildQueryError>;
 /**
  * A scoped and simplified part of the headless state that is relevant to the `QueryError` controller.
  */
 export type QueryErrorState = QueryError['state'];
 
-/**
- * `QueryError` controller allows to retrieve information about the current error returned by the search API, if any.
- */
 export const buildQueryError = (engine: Engine<SearchSection>) => {
   const controller = buildController(engine);
 
   return {
     ...controller,
     /**
-     * A scoped and simplified part of the headless state that is relevant to the `QueryError` controller.
+     * The state of the `QueryError` controller.
      */
     get state() {
       return {
         /**
-         * Determine if there is an error for the last executed query.
+         * `true` if there is an error for the last executed query and `false` otherwise.
          */
         hasError: engine.state.search.error !== null,
         /**
