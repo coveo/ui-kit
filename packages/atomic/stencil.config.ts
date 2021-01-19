ff8abfc38fd0a0a0fd3ea8bd3279d53e846c3389
Nafees Rawji <nrawji@coveo.com>
2021-01-19 14:54:37 -0500

    Fixed build

--- packages/atomic/stencil.config.ts
+++ packages/atomic/stencil.config.ts
@@ -1,5 +1,5 @@
 import {Config} from '@stencil/core';
-import {postcss } from '@stencil/postcss';
+import { postcss } from '@stencil/postcss';
 import alias from '@rollup/plugin-alias';
 import path from 'path';
 import html from 'rollup-plugin-html';
@@ -48,7 +48,6 @@ export const config: Config = {
       plugins: [
         atImport(),
         tailwind(),
-        nano(),https://cssnano.co/
         purgecss({
           content: ['./src/**/*.tsx', './src/index.html', './src/**/*.pcss'],
           defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
