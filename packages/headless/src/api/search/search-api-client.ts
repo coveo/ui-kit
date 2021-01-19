ac1cc8bd6ea689d9324c604ce05db7f263bbc0da
Olivier Lamothe <olamothe@coveo.com>
2021-01-19 11:25:14 -0500

    fix(headless): make AbortController usable in locker service (#420)
    https://coveord.atlassian.net/browse/KIT-357

--- packages/headless/src/api/search/search-api-client.ts
+++ packages/headless/src/api/search/search-api-client.ts
@@ -1,4 +1,3 @@
-import AbortController from 'node-abort-controller';
 import {
   PlatformClient,
   PlatformResponse,
@@ -109,13 +108,13 @@ export class SearchAPIClient {
       this.options.logger.warn('Cancelling current pending search query');
       this.searchAbortController.abort();
     }
-    this.searchAbortController = new AbortController();
+    this.searchAbortController = this.getAbortControllerInstanceIfAvailable();
 
     const platformResponse = await PlatformClient.call<SearchRequest, Search>({
       ...baseSearchRequest(req, 'POST', 'application/json', ''),
       requestParams: pickNonBaseParams(req),
       ...this.options,
-      signal: this.searchAbortController.signal,
+      signal: this.searchAbortController?.signal,
     });
 
     this.searchAbortController = null;
@@ -190,6 +189,22 @@ export class SearchAPIClient {
     return {
       error: unwrapError(platformResponse),
     };
+  }
+
+  private getAbortControllerInstanceIfAvailable(): AbortController | null {
+    // For nodejs environments only, we want to load the implementation of AbortController from node-abort-controller package.
+    // For browser environments, we need to make sure that we don't use AbortController as it might not be available (Locker Service in Salesforce)
+    // This is not something that can be polyfilled in a meaningful manner.
+    // This is a low level browser API after all, and only JS code inside a polyfill cannot actually cancel network requests done by the browser.
+
+    if (typeof window === 'undefined') {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const nodeAbort = require('node-abort-controller');
+      return new nodeAbort() as AbortController;
+    }
+    return typeof AbortController === 'undefined'
+      ? null
+      : new AbortController();
   }
 }
 
