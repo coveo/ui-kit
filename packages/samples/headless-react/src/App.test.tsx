57b5931efa43224188ce73d0d7c16067e87864f9
Sami Sayegh <ssayegh@coveo.com>
2021-01-27 13:41:37 -0500

    delete test file
    https://coveord.atlassian.net/browse/KIT-365

--- packages/samples/headless-react/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import {render} from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const page = render(<App />);
-  const logo = page.getByAltText('logo');
-  expect(logo).toBeTruthy();
-});
