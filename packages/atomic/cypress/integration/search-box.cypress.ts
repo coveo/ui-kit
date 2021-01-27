93462cf444199bd3d8ecfb0648a6b65766563e3c
Nafees Rawji <nrawji@coveo.com>
2021-01-27 13:33:19 -0500

    Fixed test naming

--- packages/atomic/cypress/integration/search-box.cypress.ts
+++ packages/atomic/cypress/integration/search-box.cypress.ts
@@ -8,18 +8,18 @@ import {
 const queryText = 'test';
 const htmlCode = '<atomic-search-box></atomic-search-box>';
 
-describe('SearchBox Test Suites', () => {
+describe('Search Box Test Suites', () => {
   beforeEach(() => {
     setUpPage(htmlCode);
     cy.wait(500);
     generateAliasForSearchBox();
   });
 
-  it('Searchbox should load', () => {
+  it('should load', () => {
     cy.get(SearchBoxSelectors.component).should('be.visible');
   });
 
-  it('Searchbox should show query suggestions', async () => {
+  it('should show query suggestions', async () => {
     cy.get('@searchInput').type(queryText, {force: true});
     cy.wait(500);
     for (let i = 1; i < queryText.length - 1; i++) {
@@ -33,7 +33,7 @@ describe('SearchBox Test Suites', () => {
       .should('have.length', search.completions.length);
   });
 
-  it('Searchbox should execute a query on button click', async () => {
+  it('should execute a query on button click', async () => {
     cy.get('@searchInput').type(queryText);
     cy.get('@searchBtn').click();
     // Search section make sure number of items displays should be same as what returns from api call
@@ -42,13 +42,13 @@ describe('SearchBox Test Suites', () => {
     expect(jsonResponse.results?.length).to.be.eq(10);
   });
 
-  it('Searchbox should clear query on clear button click', async () => {
+  it('should clear query on clear button click', async () => {
     cy.get('@searchInput').type(queryText);
     cy.get('button').find('.clear').click();
     cy.get('@searchInput').should('be.empty');
   });
 
-  it('Searchbox should log UA when excute a query', async () => {
+  it('should log UA when excute a query', async () => {
     cy.get('@searchInput').type(queryText, {force: true});
     cy.get('@searchBtn').click();
 
@@ -67,11 +67,11 @@ describe('SearchBox Test Suites', () => {
     );
   });
 
-  it('Searchbox passes automated accessibility tests with no query', () => {
+  it('passes automated accessibility tests with no query', () => {
     cy.checkA11y(SearchBoxSelectors.component);
   });
 
-  it('Searchbox passes auutomated accessibility tests with a query', () => {
+  it('passes automated accessibility tests with a query', () => {
     cy.get('@searchInput').type(queryText, {force: true});
     cy.checkA11y(SearchBoxSelectors.component);
   });
