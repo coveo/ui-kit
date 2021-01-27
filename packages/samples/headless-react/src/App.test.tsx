d789871e1c7ea846a06f71643b4f221b6a20eabc
Sami Sayegh <ssayegh@coveo.com>
2021-01-27 14:07:09 -0500

    readd App.test file
    https://coveord.atlassian.net/browse/KIT-365

--- packages/samples/headless-react/src/App.test.tsx
+++ packages/samples/headless-react/src/App.test.tsx
@@ -1,7 +1,7 @@
 import {render} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react logo', () => {
   const page = render(<App />);
   const logo = page.getByAltText('logo');
   expect(logo).toBeTruthy();
