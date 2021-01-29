34bfaea7c4b4486673a89a0694f4068b62a5fe01
Benjamin <btaillon@coveo.com>
2021-01-29 14:39:19 -0500

    docs(headless): add React samples for headless facet
    https://coveord.atlassian.net/browse/KIT-392

--- packages/samples/headless-react/src/App.tsx
+++ packages/samples/headless-react/src/App.tsx
@@ -9,15 +9,21 @@ import {
   SearchBoxOptions,
   buildQuerySummary,
   buildResultList,
+  buildFacet,
 } from '@coveo/headless';
 import {engine} from './engine';
 import {Section} from './layout/section';
 import {QuerySummary} from './components/query-summary/query-summary.class';
 import {QuerySummary as QuerySummaryFn} from './components/query-summary/query-summary.fn';
+import {Facet} from './components/facet/facet.class';
+import {Facet as FacetFn} from './components/facet/facet.fn';
 
 const options: SearchBoxOptions = {numberOfSuggestions: 8};
 const searchBox = buildSearchBox(engine, {options});
+
 const querySummary = buildQuerySummary(engine);
+
+const facet = buildFacet(engine, {options: {field: 'filetype'}});
 
 const resultList = buildResultList(engine);
 
@@ -33,6 +39,10 @@ function App() {
         <Section title="query-summary">
           <QuerySummary />
           <QuerySummaryFn controller={querySummary} />
+        </Section>
+        <Section title="facet">
+          <Facet field="author" />
+          <FacetFn controller={facet} />
         </Section>
         <Section title="result-list">
           <ResultList />
