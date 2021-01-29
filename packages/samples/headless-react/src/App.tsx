32627857835c83b9529d86ac6ef1a0db7a653e16
Benjamin <btaillon@coveo.com>
2021-01-29 16:17:42 -0500

    docs(headless): add React samples for headless did-you-mean
    https://coveord.atlassian.net/browse/KIT-382

--- packages/samples/headless-react/src/App.tsx
+++ packages/samples/headless-react/src/App.tsx
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import {SearchBox} from './components/search-box/search-box.class';
 import {SearchBox as SearchBoxFn} from './components/search-box/search-box.fn';
+import {DidYouMean} from './components/did-you-mean/did-you-mean.class';
+import {DidYouMean as DidYouMeanFn} from './components/did-you-mean/did-you-mean.fn';
 import {ResultList} from './components/result-list/result-list.class';
 import {ResultList as ResultListFn} from './components/result-list/result-list.fn';
 import {
   buildSearchBox,
   SearchBoxOptions,
+  buildDidYouMean,
   buildQuerySummary,
   buildResultList,
 } from '@coveo/headless';
@@ -16,7 +19,11 @@ import {QuerySummary} from './components/query-summary/query-summary.class';
 import {QuerySummary as QuerySummaryFn} from './components/query-summary/query-summary.fn';
 
 const options: SearchBoxOptions = {numberOfSuggestions: 8};
+
 const searchBox = buildSearchBox(engine, {options});
+
+const didYouMean = buildDidYouMean(engine);
+
 const querySummary = buildQuerySummary(engine);
 
 const resultList = buildResultList(engine);
@@ -29,6 +36,10 @@ function App() {
         <Section title="search-box">
           <SearchBox />
           <SearchBoxFn controller={searchBox} />
+        </Section>
+        <Section title="did-you-mean">
+          <DidYouMean />
+          <DidYouMeanFn controller={didYouMean} />
         </Section>
         <Section title="query-summary">
           <QuerySummary />
