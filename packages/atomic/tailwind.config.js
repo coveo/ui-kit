af75a9bc918d98ef8fe65d8b2899190e432de227
Jean-Félix Thibodeau <ThibodeauJF@users.noreply.github.com>
2021-02-01 08:44:01 -0500

    feat(atomic): atomic-pager & atomic-result-per-page components completion (#456)
    https://coveord.atlassian.net/browse/KIT-372
    https://coveord.atlassian.net/browse/KIT-373

--- packages/atomic/tailwind.config.js
+++ packages/atomic/tailwind.config.js
@@ -11,18 +11,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "primary": "var(--background)",
-        "on-primary": "var(--on-primary)",
-        "secondary": "var(--on-secondary)",
-        "background": "var(--background)",
-        "on-background": "var(--on-background)",
-        "error": "var(--error)",
-        "on-error": "var(--on-error)",
+        "primary": "var(--atomic-primary, #1372EC)",
+        "primary-variant": "var(--atomic-primary-variant, #215CD3)",
+        "on-primary": "var(--atomic-on-primary, #FFFFFF)",
+        "secondary": "var(--atomic-secondary, #333357)",
+        "secondary-variant": "var(--atomic-secondary-variant, #333357)",
+        "on-secondary": "var(--atomic-on-secondary, #FFFFFF)",
+        "background": "var(--atomic-background, #FFFFFF)",
+        "background-variant": "var(--atomic-background-variant, #F6F7F9)",
+        "on-background": "var(--atomic-on-background, #282829)",
+        "divider": "var(--atomic-divider, #E5E8E8)",
+        "error": "var(--atomic-error, #CE3F00)",
+        "on-error": "var(--atomic-on-error, #FFFFFF)",
       }
     },
     backgroundColor: (theme) => ({
       ...theme('colors'),
     }),
+    fontFamily: {
+      "sans": "var(--atomic-font-family, Lato, Arial, Helvetica, sans-serif)"
+    }
   },
   variants: {
     extend: {},
