e44701b3d35960ddfa23bf1b495fae983390db14
Sami Sayegh <ssayegh@coveo.com>
2021-02-02 17:55:17 -0500

    adjust Pager types for api-extractor
    https://coveord.atlassian.net/browse/KIT-402

--- packages/headless/src/controllers/pager/headless-pager.ts
+++ packages/headless/src/controllers/pager/headless-pager.ts
@@ -28,15 +28,15 @@ import {
   validateOptions,
 } from '../../utils/validate-payload';
 
-export type PagerInitialState = {
+export interface PagerInitialState {
   /** The initial page number */
   page?: number;
-};
+}
 
-export type PagerOptions = {
+export interface PagerOptions {
   /** The number of pages to display in the pager. */
   numberOfPages?: number;
-};
+}
 
 export interface PagerProps {
   /**
@@ -57,12 +57,49 @@ const initialStateSchema = new Schema({
   page: new NumberValue({min: 1}),
 });
 
-export type Pager = ReturnType<typeof buildPager>;
+export interface Pager {
+  /**
+   * Updates the results to those on the passed page.
+   * @param page The page number.
+   */
+  selectPage: (page: number) => void;
+
+  /** Updates the results to those on the next page.*/
+  nextPage: () => void;
+
+  /** Updates the results to those on the previous page.*/
+  previousPage: () => void;
+
+  /**
+   * @returns `true` when the current page is equal to the current page, and `false` otherwise.
+   * @param page The page number to check.
+   * @returns boolean.
+   */
+  isCurrentPage: (page: number) => void;
+
+  /** The state of the Pager controller */
+  state: PagerState;
+}
 
 /**
  * A scoped and simplified part of the headless state that is relevant to the `Pager` controller.
  */
-export type PagerState = Pager['state'];
+export interface PagerState {
+  /** @returns the current selected page */
+  currentPage: number;
+
+  /** @returns the current pages range */
+  currentPages: number[];
+
+  /**  @returns the max available page for this query */
+  maxPage: number;
+
+  /** @returns `true` when a previous page is available, and `false` otherwise.*/
+  hasPreviousPage: boolean;
+
+  /** @returns `true` when a next page is available, and `false` otherwise. */
+  hasNextPage: boolean;
+}
 
 /**
  * The `Pager` controller allows to navigate through the different result pages.
@@ -70,7 +107,7 @@ export type PagerState = Pager['state'];
 export function buildPager(
   engine: Engine<PaginationSection & ConfigurationSection>,
   props: PagerProps = {}
-) {
+): Pager {
   const controller = buildController(engine);
   const {dispatch} = engine;
 
@@ -107,9 +144,7 @@ export function buildPager(
 
   return {
     ...controller,
-    /**
-     * @returns {PagerState} The state of the `Pager` controller.
-     */
+
     get state() {
       const currentPage = getCurrentPage();
       const maxPage = getMaxPage();
@@ -117,53 +152,29 @@ export function buildPager(
       const hasNextPage = currentPage < maxPage;
 
       return {
-        /** @returns the current selected page */
         currentPage,
-
-        /** @returns the current pages range */
         currentPages: getCurrentPages(),
-
-        /**  @returns the max available page for this query */
         maxPage,
-
-        /** @returns `true` when a previous page is available, and `false` otherwise.*/
         hasPreviousPage,
-
-        /** @returns `true` when a next page is available, and `false` otherwise. */
         hasNextPage,
       };
     },
 
-    /**
-     * Updates the results to those on the passed page.
-     * @param page The page number.
-     */
     selectPage(page: number) {
       dispatch(updatePage(page));
       dispatch(executeSearch(logPageNumber()));
     },
 
-    /**
-     * Updates the results to those on the next page.
-     */
     nextPage() {
       dispatch(nextPage());
       dispatch(executeSearch(logPageNext()));
     },
 
-    /**
-     * Updates the results to those on the previous page.
-     */
     previousPage() {
       dispatch(previousPage());
       dispatch(executeSearch(logPagePrevious()));
     },
 
-    /**
-     * @returns `true` when the current page is equal to the current page, and `false` otherwise.
-     * @param page The page number to check.
-     * @returns boolean.
-     */
     isCurrentPage(page: number) {
       return page === this.state.currentPage;
     },
