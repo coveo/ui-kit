780b764cdf1a35a30d07a55a3bbe5e151e66f5a3
btaillon <54454747+btaillon@users.noreply.github.com>
2021-02-02 16:52:59 -0500

    docs(headless): add React samples for headless sort (#458)
    * docs(headless): add React samples for headless sort
    
    https://coveord.atlassian.net/browse/KIT-389

--- packages/samples/headless-react/src/App.tsx
+++ packages/samples/headless-react/src/App.tsx
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import {SearchBox} from './components/search-box/search-box.class';
 import {SearchBox as SearchBoxFn} from './components/search-box/search-box.fn';
+import {Sort} from './components/sort/sort.class';
+import {Sort as SortFn} from './components/sort/sort.fn';
 import {ResultList} from './components/result-list/result-list.class';
 import {ResultList as ResultListFn} from './components/result-list/result-list.fn';
 import {Pager} from './components/pager/pager.class';
@@ -10,6 +12,12 @@ import {
   buildSearchBox,
   buildQuerySummary,
   buildResultList,
+  buildDateSortCriterion,
+  buildFieldSortCriterion,
+  buildRelevanceSortCriterion,
+  buildSort,
+  SortOrder,
+  SortCriterion,
   buildResultsPerPage,
   buildPager,
 } from '@coveo/headless';
@@ -23,6 +31,18 @@ import {QuerySummary as QuerySummaryFn} from './components/query-summary/query-s
 const searchBox = buildSearchBox(engine, {options: {numberOfSuggestions: 8}});
 
 const querySummary = buildQuerySummary(engine);
+
+const criteria: [string, SortCriterion][] = [
+  ['Relevance', buildRelevanceSortCriterion()],
+  ['Date (Ascending)', buildDateSortCriterion(SortOrder.Ascending)],
+  ['Date (Descending)', buildDateSortCriterion(SortOrder.Descending)],
+  ['Size (Ascending)', buildFieldSortCriterion('size', SortOrder.Ascending)],
+  ['Size (Descending)', buildFieldSortCriterion('size', SortOrder.Descending)],
+];
+const initialCriterion = criteria[0][1];
+const sort = buildSort(engine, {
+  initialState: {criterion: initialCriterion},
+});
 
 const resultList = buildResultList(engine);
 
@@ -45,6 +65,10 @@ function App() {
         <Section title="query-summary">
           <QuerySummary />
           <QuerySummaryFn controller={querySummary} />
+        </Section>
+        <Section title="sort">
+          <Sort criteria={criteria} initialCriterion={initialCriterion} />
+          <SortFn controller={sort} criteria={criteria} />
         </Section>
         <Section title="result-list">
           <ResultList />
