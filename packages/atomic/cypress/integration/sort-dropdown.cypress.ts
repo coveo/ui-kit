4b975151a7445a40ae1780690fe5c1074be3f807
Jean-Félix Thibodeau <ThibodeauJF@users.noreply.github.com>
2021-02-04 15:28:26 -0500

    test(atomic): tests for sort dropdown (#480)
    https://coveord.atlassian.net/browse/KIT-394

---
+++ packages/atomic/cypress/integration/sort-dropdown.cypress.ts
@@ -0,0 +1,90 @@
+import {getApiRequestBody, getAnalytics} from '../utils/network';
+import {setUpPage} from '../utils/setupComponent';
+
+const sortDropdown = 'atomic-sort-dropdown';
+const criteria = ['relevancy', 'date descending'];
+
+describe('Sort Dropdown Component', () => {
+  function setup(attributes = '') {
+    setUpPage(`
+      <atomic-sort-dropdown ${attributes}>
+        <atomic-sort-criteria caption="relevance" criteria="${criteria[0]}"></atomic-sort-criteria>
+        <atomic-sort-criteria caption="mostRecent" criteria="${criteria[1]}"></atomic-sort-criteria>
+      </atomic-sort-dropdown>
+    `);
+    cy.wait(500);
+  }
+
+  function selectOption(value: string) {
+    return cy.get(sortDropdown).shadow().find('select').select(value);
+  }
+
+  function shouldRenderErrorComponent() {
+    cy.get(sortDropdown)
+      .shadow()
+      .find('atomic-component-error')
+      .should('exist');
+  }
+
+  it('should load', () => {
+    setup();
+    cy.get(sortDropdown).should('be.visible');
+  });
+
+  it('passes automated accessibility', () => {
+    setup();
+    cy.checkA11y(sortDropdown);
+  });
+
+  it('should display a label by default', () => {
+    setup();
+    cy.get(sortDropdown).shadow().find('label').should('exist');
+  });
+
+  it('should not display a label when display-label="false"', () => {
+    setup('display-label="false"');
+    cy.get(sortDropdown).shadow().find('label').should('not.exist');
+  });
+
+  it('should display the localized string for the option innerHTML', () => {
+    setup();
+    cy.get(sortDropdown)
+      .shadow()
+      .find('option[value="relevancy"]')
+      .contains('Relevance');
+  });
+
+  it('should execute a query with the correct sort order on selection', async () => {
+    setup();
+    selectOption(criteria[1]);
+
+    const request = await getApiRequestBody('@coveoSearch');
+    expect(request.sortCriteria).to.be.eq(criteria[1]);
+  });
+
+  it('should log the right analytics on selection', async () => {
+    setup();
+    selectOption(criteria[1]);
+
+    const analytics = await getAnalytics('@coveoAnalytics');
+    expect(analytics.request.body).to.have.property(
+      'actionCause',
+      'resultsSort'
+    );
+    expect(analytics.request.body.customData).to.have.property(
+      'resultsSortBy',
+      criteria[1]
+    );
+  });
+
+  it(`when a criteria is not valid
+    should render an error`, () => {
+    setUpPage(`
+      <atomic-sort-dropdown>
+        <atomic-sort-criteria</atomic-sort-criteria>
+      </atomic-sort-dropdown>
+    `);
+    cy.wait(500);
+    shouldRenderErrorComponent();
+  });
+});
--- packages/atomic/cypress/utils/network.ts
+++ packages/atomic/cypress/utils/network.ts
@@ -1,13 +1,27 @@
+import {Interception} from 'cypress/types/net-stubbing';
+import {SearchRequest} from '../../../headless/dist/api/search/search/search-request';
 import {SearchResponseSuccess} from '../../../headless/dist/api/search/search/search-response';
 
-export function getApiResponseBody(
-  selector: string
-): Promise<SearchResponseSuccess> {
+function getApiCall(selector: string): Promise<Interception> {
   return new Promise((resolve) => {
     cy.wait(selector).then((interception) => {
-      resolve(interception.response!.body);
+      resolve(interception);
     });
   });
+}
+
+export async function getApiResponseBody(
+  selector: string
+): Promise<SearchResponseSuccess> {
+  const interception = await getApiCall(selector);
+  return interception.response!.body;
+}
+
+export async function getApiRequestBody(
+  selector: string
+): Promise<SearchRequest> {
+  const interception = await getApiCall(selector);
+  return interception.request.body;
 }
 
 export function getAnalytics(selector: string): Promise<any> {
