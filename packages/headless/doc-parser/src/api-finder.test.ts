e4d556bce4ff6d908e2b296e9a06b59bd69e7641
Sami Sayegh <ssayegh@coveo.com>
2021-02-08 18:07:29 -0500

    extract canonical name before performing search
    https://coveord.atlassian.net/browse/KIT-402

--- packages/headless/doc-parser/src/api-finder.test.ts
+++ packages/headless/doc-parser/src/api-finder.test.ts
@@ -1,4 +1,5 @@
 import {buildMockApiFunction} from '../mocks/mock-api-function';
+import {buildMockApiInterface} from '../mocks/mock-api-interface';
 import {buildMockEntryPoint} from '../mocks/mock-entry-point';
 import {findApi} from './api-finder';
 
@@ -19,5 +20,14 @@ describe('#findApi', () => {
     expect(() => findApi(entryPoint, 'buildPager')).toThrow(
       'No api found for buildPager'
     );
+  });
+
+  it('when an api name has a $1 suffix, it is able to retrieve the api', () => {
+    const entryPoint = buildMockEntryPoint();
+    const apiInterface = buildMockApiInterface({name: 'FacetOptions'});
+    entryPoint.addMember(apiInterface);
+
+    const result = findApi(entryPoint, 'FacetOptions$1');
+    expect(result.displayName).toBe('FacetOptions');
   });
 });
