eb5edf0c00c7997e9152372f0c1b8a1e71014e70
Benjamin <btaillon@coveo.com>
2021-02-09 14:00:58 -0500

    Dispatched modified analytics action
    https://coveord.atlassian.net/browse/KIT-395

--- packages/headless/src/features/search/search-actions.ts
+++ packages/headless/src/features/search/search-actions.ts
@@ -30,7 +30,11 @@ import {
   SearchSection,
   SortSection,
 } from '../../state/state-sections';
-import {getVisitorID, historyStore} from '../../api/analytics/analytics';
+import {
+  getVisitorID,
+  historyStore,
+  StateNeededByAnalyticsProvider,
+} from '../../api/analytics/analytics';
 import {AnyFacetRequest} from '../facets/generic/interfaces/generic-facet-request';
 import {SearchRequest} from '../../api/search/search/search-request';
 import {getContextInitialState} from '../context/context-state';
@@ -55,6 +59,7 @@ import {HistoryState} from '../history/history-state';
 import {sortFacets} from '../../utils/facet-utils';
 import {getDebugInitialState} from '../debug/debug-state';
 import {getFacetOrderInitialState} from '../facets/facet-order/facet-order-state';
+import {getSearchInitialState} from './search-state';
 
 export type StateNeededByExecuteSearch = ConfigurationSection &
   Partial<
@@ -115,12 +120,12 @@ export const executeSearch = createAsyncThunk<
   'search/executeSearch',
   async (
     analyticsAction: SearchAction,
-    {getState, dispatch, rejectWithValue, extra: {searchAPIClient}}
+    {getState, dispatch, rejectWithValue, extra}
   ) => {
     const state = getState();
     addEntryInActionsHistory(state);
     const fetched = await fetchFromAPI(
-      searchAPIClient,
+      extra.searchAPIClient,
       state,
       buildSearchRequest(state)
     );
@@ -142,11 +147,10 @@ export const executeSearch = createAsyncThunk<
       };
     }
 
-    dispatch(analyticsAction);
-
+    const {correctedQuery} = fetched.response.success.queryCorrections[0];
     const retried = await automaticallyRetryQueryWithCorrection(
-      searchAPIClient,
-      fetched.response.success.queryCorrections[0].correctedQuery,
+      extra.searchAPIClient,
+      correctedQuery,
       getState,
       dispatch
     );
@@ -156,6 +160,12 @@ export const executeSearch = createAsyncThunk<
       return rejectWithValue(retried.response.error);
     }
 
+    const retriedResponse = retried.response.success;
+    analyticsAction(
+      dispatch,
+      () => getStateAfterResponse(correctedQuery, state, retriedResponse),
+      extra
+    );
     dispatch(snapshot(extractHistory(getState())));
 
     return {
@@ -202,6 +212,25 @@ export const fetchMoreResults = createAsyncThunk<
     };
   }
 );
+
+const getStateAfterResponse: (
+  query: string,
+  previousState: StateNeededByExecuteSearch,
+  response: SearchResponseSuccess
+) => StateNeededByAnalyticsProvider = (query, previousState, response) => ({
+  ...previousState,
+  query: {
+    q: query,
+    enableQuerySyntax:
+      previousState.query?.enableQuerySyntax ??
+      getQueryInitialState().enableQuerySyntax,
+  },
+  search: {
+    ...getSearchInitialState(),
+    response,
+    results: response.results,
+  },
+});
 
 const automaticallyRetryQueryWithCorrection = async (
   client: SearchAPIClient,
