0d2d53e2810e3590b07284c9a608c88dcff8ecce
Benjamin <btaillon@coveo.com>
2021-02-12 11:06:47 -0500

    Flattened ResultLink for documentation
    https://coveord.atlassian.net/browse/KIT-408

--- packages/headless/src/controllers/result-list/headless-result-link.ts
+++ packages/headless/src/controllers/result-list/headless-result-link.ts
@@ -1,11 +1,12 @@
 import {Result} from '../../api/search/search/result';
 import {Engine} from '../../app/headless-engine';
 import {logDocumentOpen} from '../../features/result/result-analytics-actions';
+import {ConfigurationSection} from '../../state/state-sections';
 
 // 1 second is a reasonable amount of time to catch most longpress actions
 const defaultDelay = 1000;
 
-type ResultLinkOptions = {
+export interface ResultLinkOptions {
   /**
    * The query result.
    */
@@ -14,19 +15,45 @@ type ResultLinkOptions = {
    * The amount of time to wait before selecting the result after calling `beginDelayedSelect`.
    */
   selectionDelay?: number;
-};
+}
 
-type ResultLinkProps = {
+export interface ResultLinkProps {
   /** The options for the `ResultLink` controller. */
   options: ResultLinkOptions;
-};
+}
 
 /**
  * The `ResultLink` controller provides an interface for triggering desirable side effects, such as logging UA events to the Coveo Platform, when a user selects a query result.
  */
-export type ResultLink = ReturnType<typeof buildResultLink>;
+export interface ResultLink {
+  /**
+   * Selects the result, logging a UA event to the Coveo Platform if the result wasn't selected before.
+   *
+   * In a DOM context, it's recommended to call this method on all of the following events:
+   * * `contextmenu`
+   * * `click`
+   * * `mouseup`
+   * * `mousedown`
+   */
+  select: () => void;
+  /**
+   * Prepares to select the result after a certain delay, sending analytics if it was never selected before.
+   *
+   * In a DOM context, it's recommended to call this method on the `touchstart` event.
+   */
+  beginDelayedSelect: () => void;
+  /**
+   * Cancels the pending selection caused by `beginDelayedSelect`.
+   *
+   * In a DOM context, it's recommended to call this method on the `touchend` event.
+   */
+  cancelPendingSelect: () => void;
+}
 
-export function buildResultLink(engine: Engine, props: ResultLinkProps) {
+export function buildResultLink(
+  engine: Engine<ConfigurationSection>,
+  props: ResultLinkProps
+): ResultLink {
   const options: Required<ResultLinkOptions> = {
     selectionDelay: defaultDelay,
     ...props.options,
@@ -44,33 +71,16 @@ export function buildResultLink(engine: Engine, props: ResultLinkProps) {
   let longPressTimer: NodeJS.Timeout;
 
   return {
-    /**
-     * Selects the result, logging a UA event to the Coveo Platform if the result wasn't selected before.
-     *
-     * In a DOM context, it's recommended to call this method on all of the following events:
-     * * `contextmenu`
-     * * `click`
-     * * `mouseup`
-     * * `mousedown`
-     */
     select: logAnalyticsIfNeverOpened,
-    /**
-     * Prepares to select the result after a certain delay, sending analytics if it was never selected before.
-     *
-     * In a DOM context, it's recommended to call this method on the `touchstart` event.
-     */
-    beginDelayedSelect: () => {
+
+    beginDelayedSelect() {
       longPressTimer = setTimeout(
         logAnalyticsIfNeverOpened,
         options.selectionDelay
       );
     },
-    /**
-     * Cancels the pending selection caused by `beginDelayedSelect`.
-     *
-     * In a DOM context, it's recommended to call this method on the `touchend` event.
-     */
-    cancelPendingSelect: () => {
+
+    cancelPendingSelect() {
       longPressTimer && clearTimeout(longPressTimer);
     },
   };
