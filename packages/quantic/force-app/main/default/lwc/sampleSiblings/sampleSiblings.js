14ba78d2f1780ad521723506f02ac264c5f6e794
nlafrance-coveo <nlafrance@coveo.com>
2021-02-12 11:28:22 -0500

    spike(siblings): added test component

---
+++ packages/quantic/force-app/main/default/lwc/sampleSiblings/sampleSiblings.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class SampleApp extends LightningElement {
+  renderedCallback() {
+    // Example on how to init the search interface
+    // const searchInterface = this.template.querySelector('c-search-interface');
+    // searchInterface.initialize({
+    //   organizationId: 'my_org',
+    //   accessToken: 'my_access_token'
+    // });
+  }
+}
