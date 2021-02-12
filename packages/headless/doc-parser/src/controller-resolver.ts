cff366f2d01fe371309672bf05315c479df3dffe
Sami Sayegh <ssayegh@coveo.com>
2021-02-12 16:42:10 -0500

    extract function resolving logic into dedicated file
    https://coveord.atlassian.net/browse/KIT-439

--- packages/headless/doc-parser/src/controller-resolver.ts
+++ packages/headless/doc-parser/src/controller-resolver.ts
@@ -1,15 +1,7 @@
-import {
-  ApiEntryPoint,
-  ApiFunction,
-  ApiInterface,
-  Parameter,
-} from '@microsoft/api-extractor-model';
-import {DocComment} from '@microsoft/tsdoc';
+import {ApiEntryPoint, ApiFunction} from '@microsoft/api-extractor-model';
 import {findApi} from './api-finder';
-import {FuncEntity, ObjEntity} from './entity';
-import {buildEntity, buildFuncEntity, buildParamEntity} from './entity-builder';
-import {buildParamEntityBasedOnKind} from './function-param-resolver';
-import {resolveInterfaceMembers} from './interface-resolver';
+import {FuncEntity} from './entity';
+import {resolveFunction} from './function-resolver';
 
 interface Controller {
   initializer: FuncEntity;
@@ -26,45 +18,5 @@ export function resolveController(
 }
 
 function resolveControllerFunction(entry: ApiEntryPoint, fn: ApiFunction) {
-  const params = fn.parameters.map((p, index) =>
-    resolveControllerParam(entry, p, index)
-  );
-  const returnTypeText = fn.returnTypeExcerpt.text;
-  const returnTypeInterface = findApi(entry, returnTypeText) as ApiInterface;
-
-  const returnType = buildObjEntityFromInterface(entry, returnTypeInterface);
-
-  return buildFuncEntity({
-    name: fn.name,
-    comment: (fn.tsdocComment as unknown) as DocComment,
-    params,
-    returnType,
-  });
-}
-
-function resolveControllerParam(
-  entryPoint: ApiEntryPoint,
-  p: Parameter,
-  index: number
-) {
-  const isEngine = index === 0;
-  return isEngine
-    ? buildParamEntity(p)
-    : buildParamEntityBasedOnKind(entryPoint, p);
-}
-
-function buildObjEntityFromInterface(
-  entryPoint: ApiEntryPoint,
-  apiInterface: ApiInterface
-): ObjEntity {
-  const name = apiInterface.name;
-  const members = resolveInterfaceMembers(entryPoint, apiInterface);
-  const entity = buildEntity({
-    name,
-    type: name,
-    isOptional: false,
-    comment: (apiInterface.tsdocComment as unknown) as DocComment,
-  });
-
-  return {...entity, members};
+  return resolveFunction(entry, fn, [0]);
 }
--- packages/headless/doc-parser/src/function-param-resolver.ts
+++ packages/headless/doc-parser/src/function-param-resolver.ts
@@ -1,5 +1,6 @@
 import {
   ApiEntryPoint,
+  ApiFunction,
   ApiInterface,
   ExcerptTokenKind,
   Parameter,
@@ -9,10 +10,20 @@ import {ObjEntity} from './entity';
 import {buildParamEntity} from './entity-builder';
 import {resolveInterfaceMembers} from './interface-resolver';
 
-export function buildParamEntityBasedOnKind(
-  entryPoint: ApiEntryPoint,
-  p: Parameter
+export function resolveParams(
+  entry: ApiEntryPoint,
+  fn: ApiFunction,
+  shallowParamIndices: number[]
 ) {
+  return fn.parameters.map((p, index) => {
+    const shouldResolveShallow = shallowParamIndices.includes(index);
+    return shouldResolveShallow
+      ? buildParamEntity(p)
+      : buildParamEntityBasedOnKind(entry, p);
+  });
+}
+
+function buildParamEntityBasedOnKind(entryPoint: ApiEntryPoint, p: Parameter) {
   const {kind} = p.parameterTypeExcerpt.spannedTokens[0];
   const isReference = kind === ExcerptTokenKind.Reference;
 
