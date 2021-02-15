00d4a99a7ac82618fd4c2fe78db20eb2b4f88bed
Sami Sayegh <ssayegh@coveo.com>
2021-02-15 13:38:04 -0500

    support utils in controller configuration
    https://coveord.atlassian.net/browse/KIT-439

--- packages/headless/doc-parser/doc-parser.ts
+++ packages/headless/doc-parser/doc-parser.ts
@@ -1,15 +1,16 @@
 import {ApiModel} from '@microsoft/api-extractor-model';
 import {writeFileSync} from 'fs';
-import {resolveController} from './src/controller-resolver';
+import {
+  buildControllerConfiguration,
+  resolveController,
+} from './src/controller-resolver';
 
 const apiModel = new ApiModel();
 const apiPackage = apiModel.loadPackage('temp/headless.api.json');
 const entryPoint = apiPackage.entryPoints[0];
 
-const config = {
-  initializer: 'buildPager',
-};
+const config = buildControllerConfiguration({initializer: 'buildPager'});
 
-const result = resolveController(entryPoint, config.initializer);
+const result = resolveController(entryPoint, config);
 
 writeFileSync('dist/parsed_doc.json', JSON.stringify(result, null, 2));
--- packages/headless/doc-parser/src/controller-resolver.ts
+++ packages/headless/doc-parser/src/controller-resolver.ts
@@ -3,20 +3,37 @@ import {findApi} from './api-finder';
 import {FuncEntity} from './entity';
 import {resolveFunction} from './function-resolver';
 
+interface ControllerConfiguration {
+  initializer: string;
+  utils?: string[];
+}
+
 interface Controller {
   initializer: FuncEntity;
+  utils: FuncEntity[];
+}
+
+export function buildControllerConfiguration(
+  config: ControllerConfiguration
+): Required<ControllerConfiguration> {
+  return {utils: [], ...config};
 }
 
 export function resolveController(
-  entryPoint: ApiEntryPoint,
-  controllerName: string
+  entry: ApiEntryPoint,
+  config: Required<ControllerConfiguration>
 ): Controller {
-  const controller = findApi(entryPoint, controllerName) as ApiFunction;
-  const initializer = resolveControllerFunction(entryPoint, controller);
-
-  return {initializer};
+  const initializer = resolveControllerFunction(entry, config.initializer);
+  const utils = config.utils.map((util) => resolveUtility(entry, util));
+  return {initializer, utils};
 }
 
-function resolveControllerFunction(entry: ApiEntryPoint, fn: ApiFunction) {
+function resolveControllerFunction(entry: ApiEntryPoint, name: string) {
+  const fn = findApi(entry, name) as ApiFunction;
   return resolveFunction(entry, fn, [0]);
+}
+
+function resolveUtility(entry: ApiEntryPoint, name: string) {
+  const fn = findApi(entry, name) as ApiFunction;
+  return resolveFunction(entry, fn, []);
 }
