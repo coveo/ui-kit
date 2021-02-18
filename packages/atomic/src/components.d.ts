1387a4b246edb4ab3b0c0bc476b22ec821d51bc5
Nafees Rawji <69480401+nrawji@users.noreply.github.com>
2021-02-18 10:47:21 -0500

    refactor(atomic): use functional components in base facet and facet value (#512)
    KIT-443
    
    https://coveord.atlassian.net/browse/KIT-443

--- packages/atomic/src/components.d.ts
+++ packages/atomic/src/components.d.ts
@@ -176,18 +176,9 @@ export namespace Components {
          */
         "value": string;
     }
-    interface BaseFacet {
-        "hasActiveValues": boolean;
-        "label": string;
-    }
     interface FacetSearch {
         "facetSearchResults": BaseFacetSearchResult[];
         "moreValuesAvailable": boolean;
-    }
-    interface FacetValue {
-        "isSelected": boolean;
-        "label": string;
-        "numberOfResults": number;
     }
 }
 declare global {
@@ -359,23 +350,11 @@ declare global {
         prototype: HTMLAtomicTextElement;
         new (): HTMLAtomicTextElement;
     };
-    interface HTMLBaseFacetElement extends Components.BaseFacet, HTMLStencilElement {
-    }
-    var HTMLBaseFacetElement: {
-        prototype: HTMLBaseFacetElement;
-        new (): HTMLBaseFacetElement;
-    };
     interface HTMLFacetSearchElement extends Components.FacetSearch, HTMLStencilElement {
     }
     var HTMLFacetSearchElement: {
         prototype: HTMLFacetSearchElement;
         new (): HTMLFacetSearchElement;
-    };
-    interface HTMLFacetValueElement extends Components.FacetValue, HTMLStencilElement {
-    }
-    var HTMLFacetValueElement: {
-        prototype: HTMLFacetValueElement;
-        new (): HTMLFacetValueElement;
     };
     interface HTMLElementTagNameMap {
         "atomic-breadcrumb-manager": HTMLAtomicBreadcrumbManagerElement;
@@ -406,9 +385,7 @@ declare global {
         "atomic-sort-dropdown": HTMLAtomicSortDropdownElement;
         "atomic-tab": HTMLAtomicTabElement;
         "atomic-text": HTMLAtomicTextElement;
-        "base-facet": HTMLBaseFacetElement;
         "facet-search": HTMLFacetSearchElement;
-        "facet-value": HTMLFacetValueElement;
     }
 }
 declare namespace LocalJSX {
@@ -572,23 +549,12 @@ declare namespace LocalJSX {
          */
         "value": string;
     }
-    interface BaseFacet {
-        "hasActiveValues": boolean;
-        "label": string;
-        "onDeselectAll"?: (event: CustomEvent<void>) => void;
-    }
     interface FacetSearch {
         "facetSearchResults": BaseFacetSearchResult[];
         "moreValuesAvailable": boolean;
         "onFacetSearch"?: (event: CustomEvent<string>) => void;
         "onResultSelected"?: (event: CustomEvent<BaseFacetSearchResult>) => void;
         "onShowMoreResults"?: (event: CustomEvent<void>) => void;
-    }
-    interface FacetValue {
-        "isSelected": boolean;
-        "label": string;
-        "numberOfResults": number;
-        "onFacetValueSelected"?: (event: CustomEvent<void>) => void;
     }
     interface IntrinsicElements {
         "atomic-breadcrumb-manager": AtomicBreadcrumbManager;
@@ -619,9 +585,7 @@ declare namespace LocalJSX {
         "atomic-sort-dropdown": AtomicSortDropdown;
         "atomic-tab": AtomicTab;
         "atomic-text": AtomicText;
-        "base-facet": BaseFacet;
         "facet-search": FacetSearch;
-        "facet-value": FacetValue;
     }
 }
 export { LocalJSX as JSX };
@@ -656,9 +620,7 @@ declare module "@stencil/core" {
             "atomic-sort-dropdown": LocalJSX.AtomicSortDropdown & JSXBase.HTMLAttributes<HTMLAtomicSortDropdownElement>;
             "atomic-tab": LocalJSX.AtomicTab & JSXBase.HTMLAttributes<HTMLAtomicTabElement>;
             "atomic-text": LocalJSX.AtomicText & JSXBase.HTMLAttributes<HTMLAtomicTextElement>;
-            "base-facet": LocalJSX.BaseFacet & JSXBase.HTMLAttributes<HTMLBaseFacetElement>;
             "facet-search": LocalJSX.FacetSearch & JSXBase.HTMLAttributes<HTMLFacetSearchElement>;
-            "facet-value": LocalJSX.FacetValue & JSXBase.HTMLAttributes<HTMLFacetValueElement>;
         }
     }
 }
