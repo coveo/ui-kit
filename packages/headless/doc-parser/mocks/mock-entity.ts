0bb06a44f8c600e5ad02db041f95643761001a84
Sami Sayegh <ssayegh@coveo.com>
2021-02-19 15:58:00 -0500

    add #kind attribute
    https://coveord.atlassian.net/browse/KIT-456

--- packages/headless/doc-parser/mocks/mock-entity.ts
+++ packages/headless/doc-parser/mocks/mock-entity.ts
@@ -2,6 +2,7 @@ import {Entity} from '../src/entity';
 
 export function buildMockEntity(config: Partial<Entity> = {}): Entity {
   return {
+    kind: 'primitive',
     desc: '',
     isOptional: false,
     name: '',
--- packages/headless/doc-parser/src/entity.ts
+++ packages/headless/doc-parser/src/entity.ts
@@ -1,19 +1,44 @@
-export interface Entity {
+type EntityKind = 'primitive' | 'object' | 'function';
+
+interface Kind<T extends EntityKind> {
+  kind: T;
+}
+
+interface Name {
   name: string;
+}
+
+interface Description {
   desc: string;
+}
+
+interface Type {
   type: string;
+}
+
+interface Optional {
   isOptional: boolean;
 }
 
-export interface ObjEntity extends Entity {
+export interface Entity
+  extends Kind<'primitive'>,
+    Name,
+    Description,
+    Type,
+    Optional {}
+
+export interface ObjEntity
+  extends Kind<'object'>,
+    Name,
+    Description,
+    Type,
+    Optional {
   members: AnyEntity[];
   isTypeExtracted: boolean;
   typeName: string;
 }
 
-export interface FuncEntity {
-  name: string;
-  desc: string;
+export interface FuncEntity extends Kind<'function'>, Name, Description {
   params: AnyEntity[];
   returnType: string | ObjEntity | FuncEntity;
 }
