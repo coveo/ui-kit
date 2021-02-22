c1a1a7fb6c3992d166d514e139d8a0e28734ff19
Benjamin <btaillon@coveo.com>
2021-02-22 08:42:36 -0500

    docs(headless): add React sample for headless relevance inspector
    https://coveord.atlassian.net/browse/KIT-423

--- packages/samples/headless-react/src/App.tsx
+++ packages/samples/headless-react/src/App.tsx
@@ -28,6 +28,8 @@ import {Facet} from './components/facet/facet.class';
 import {Facet as FacetFn} from './components/facet/facet.fn';
 import {History} from './components/history/history.class';
 import {History as HistoryFn} from './components/history/history.fn';
+import {RelevanceInspector} from './components/relevance-inspector/relevance-inspector.class';
+import {RelevanceInspector as RelevanceInspectorFn} from './components/relevance-inspector/relevance-inspector.fn';
 import {
   buildRecommendationList,
   buildTab,
@@ -46,6 +48,7 @@ import {
   buildResultsPerPage,
   buildPager,
   buildHistory,
+  buildRelevanceInspector,
 } from '@coveo/headless';
 import {bindSearchParametersToURI} from './components/search-parameter-manager/search-parameter-manager';
 
@@ -99,6 +102,8 @@ const resultsPerPage = buildResultsPerPage(engine, {
 const pager = buildPager(engine, {options: {numberOfPages: 6}});
 
 const history = buildHistory(engine);
+
+const relevanceInspector = buildRelevanceInspector(engine);
 
 const {autoUpdateURI: startUpdatingURI} = bindSearchParametersToURI(engine);
 
@@ -169,6 +174,10 @@ function App() {
         <Section title="history">
           <History />
           <HistoryFn controller={history} />
+        </Section>
+        <Section title="relevance-inspector">
+          <RelevanceInspector />
+          <RelevanceInspectorFn controller={relevanceInspector} />
         </Section>
       </header>
     </div>
---
+++ packages/samples/headless-react/src/components/relevance-inspector/relevance-inspector.fn.tsx
@@ -0,0 +1,47 @@
+import {useEffect, useState, FunctionComponent} from 'react';
+import {
+  buildRelevanceInspector,
+  RelevanceInspector as HeadlessRelevanceInspector,
+} from '@coveo/headless';
+import {engine} from '../../engine';
+
+interface RelevanceInspectorProps {
+  controller: HeadlessRelevanceInspector;
+}
+
+export const RelevanceInspector: FunctionComponent<RelevanceInspectorProps> = (
+  props
+) => {
+  const {controller} = props;
+  const [state, setState] = useState(controller.state);
+
+  useEffect(
+    () =>
+      controller.subscribe(() => {
+        console.info('Debug information [fn]', controller.state);
+        setState(controller.state);
+      }),
+    []
+  );
+
+  return (
+    <div>
+      <label>
+        Debugging:{' '}
+        <input
+          type="checkbox"
+          checked={state.isEnabled}
+          onChange={() =>
+            state.isEnabled ? controller.disable() : controller.enable()
+          }
+        />
+      </label>
+    </div>
+  );
+};
+
+// usage
+
+const controller = buildRelevanceInspector(engine);
+
+<RelevanceInspector controller={controller} />;
