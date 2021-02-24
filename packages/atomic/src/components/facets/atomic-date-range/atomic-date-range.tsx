f6a8cddf5d4820b3ccdea393f93f4317df1a0e7c
Nafees Rawji <69480401+nrawji@users.noreply.github.com>
2021-02-24 09:47:38 -0500

    feat(atomic): date facet component  (#534)
    KIT-459
    
    https://coveord.atlassian.net/browse/KIT-459

---
+++ packages/atomic/src/components/facets/atomic-date-range/atomic-date-range.tsx
@@ -0,0 +1,28 @@
+import {Component, Element, Prop} from '@stencil/core';
+
+/**
+ * Component that defines a date facet range.
+ * Has to be used inside an `atomic-date-facet` component.
+ */
+@Component({
+  tag: 'atomic-date-range',
+  shadow: false,
+})
+export class AtomicDateRange {
+  @Element() public host!: HTMLElement;
+
+  /**
+   * The starting date for the range.
+   * It can be expressed as a Javascript date, as a number using epoch time or as a string using the ISO 8601 format
+   */
+  @Prop() public start!: Date | string | number;
+  /**
+   * The ending date for the range.
+   * It can be expressed as a Javascript date, as a number using epoch time or as a string using the ISO 8601 format
+   */
+  @Prop() public end!: Date | string | number;
+  /**
+   * Specifies whether or not the end date should be included in the range
+   */
+  @Prop() public endInclusive = false;
+}
