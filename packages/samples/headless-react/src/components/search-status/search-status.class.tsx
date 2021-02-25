031c97a9211b273b9acf41d95ee34df3e92879f6
Benjamin <btaillon@coveo.com>
2021-02-25 15:15:28 -0500

    made possible states more obvious
    https://coveord.atlassian.net/browse/KIT-445

--- packages/samples/headless-react/src/components/search-status/search-status.class.tsx
+++ packages/samples/headless-react/src/components/search-status/search-status.class.tsx
@@ -34,25 +34,31 @@ export class SearchStatus extends Component {
   render() {
     const {hasResults, firstSearchExecuted, isLoading} = this.state;
 
-    if (!hasResults) {
-      if (!firstSearchExecuted) {
-        if (isLoading) {
-          return <p>The first search ever is currently loading.</p>;
-        }
-        return <p>No search was ever executed.</p>;
-      }
+    if (!hasResults && !firstSearchExecuted && isLoading) {
+      return <p>The first search ever is currently loading.</p>;
+    }
 
-      if (isLoading) {
-        return (
-          <p>The previous search gave no results, but new ones are pending.</p>
-        );
-      }
+    if (!hasResults && !firstSearchExecuted && !isLoading) {
+      return <p>No search was ever executed.</p>;
+    }
+
+    if (!hasResults && firstSearchExecuted && isLoading) {
+      <p>The previous search gave no results, but new ones are pending.</p>;
+    }
+
+    if (!hasResults && firstSearchExecuted && !isLoading) {
       return <p>A search was executed but gave no results.</p>;
     }
 
-    if (isLoading) {
+    if (hasResults && isLoading) {
       return <p>The previous search gave results, but new ones are loading.</p>;
     }
-    return <p>There are results and no pending search.</p>;
+
+    if (hasResults && !isLoading) {
+      return <p>There are results and no pending search.</p>;
+    }
+
+    // Unreachable. The code is structured this way to demonstrate possible states.
+    return null;
   }
 }
--- packages/samples/headless-react/src/components/search-status/search-status.fn.tsx
+++ packages/samples/headless-react/src/components/search-status/search-status.fn.tsx
@@ -17,26 +17,32 @@ export const SearchStatus: FunctionComponent<SearchStatusProps> = (props) => {
 
   const {hasResults, firstSearchExecuted, isLoading} = state;
 
-  if (!hasResults) {
-    if (!firstSearchExecuted) {
-      if (isLoading) {
-        return <p>The first search ever is currently loading.</p>;
-      }
-      return <p>No search was ever executed.</p>;
-    }
+  if (!hasResults && !firstSearchExecuted && isLoading) {
+    return <p>The first search ever is currently loading.</p>;
+  }
 
-    if (isLoading) {
-      return (
-        <p>The previous search gave no results, but new ones are pending.</p>
-      );
-    }
+  if (!hasResults && !firstSearchExecuted && !isLoading) {
+    return <p>No search was ever executed.</p>;
+  }
+
+  if (!hasResults && firstSearchExecuted && isLoading) {
+    <p>The previous search gave no results, but new ones are pending.</p>;
+  }
+
+  if (!hasResults && firstSearchExecuted && !isLoading) {
     return <p>A search was executed but gave no results.</p>;
   }
 
-  if (isLoading) {
+  if (hasResults && isLoading) {
     return <p>The previous search gave results, but new ones are loading.</p>;
   }
-  return <p>There are results and no pending search.</p>;
+
+  if (hasResults && !isLoading) {
+    return <p>There are results and no pending search.</p>;
+  }
+
+  // Unreachable. The code is structured this way to demonstrate possible states.
+  return null;
 };
 
 // usage
