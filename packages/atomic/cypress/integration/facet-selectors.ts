78f97eb61d9a7c0c063beed5c1d678bafd7a41f7
Lucille Vu <lvu@coveo.com>
2021-03-05 14:34:04 -0500

    test(atomic): cypress test for standard facet (#576)
    https://coveord.atlassian.net/browse/KIT-460
    
    * KIT-460: Add more cypress test for Facet
    * Removed unneeded option role
    
    Co-authored-by: nrawji <nrawji@coveo.com>

---
+++ packages/atomic/cypress/integration/facet-selectors.ts
@@ -0,0 +1,31 @@
+export const FacetSelectors = {
+  facetStandard: 'atomic-facet',
+  facetSearchbox: 'input[part="search-input"]',
+  checkbox: 'input[type="checkbox"]',
+  showMoreButton: 'button[part="show-more"]',
+  showLessButton: 'button[part="show-less"]',
+  clearAllButton: 'button[part="reset-button"]',
+};
+
+export const BreadcrumbSelectors = {
+  breadcrumb: 'atomic-breadcrumb-manager',
+};
+export function createBreadcrumbShadowAlias() {
+  cy.get(BreadcrumbSelectors.breadcrumb)
+    .shadow()
+    .find('div span:nth-child(1)')
+    .as('breadcrumbFacet');
+  cy.get(BreadcrumbSelectors.breadcrumb)
+    .shadow()
+    .find('div span:nth-child(2)')
+    .as('breadcrumbClearAllFilter');
+}
+
+export function createAliasShadow(field: string) {
+  const facetSelector = `${FacetSelectors.facetStandard}[field="${field}"]`;
+  cy.get(facetSelector).shadow().find('div.facet div').as('facetShadow');
+}
+export function createAliasFacetUL(field: string) {
+  createAliasShadow(field);
+  cy.get('@facetShadow').find('div:nth-child(2) ul').as('facet_ul');
+}
