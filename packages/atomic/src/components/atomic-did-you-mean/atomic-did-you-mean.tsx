9873b3259c96f2625efbf007d09d05b40cdfa92e
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-03-15 09:46:39 -0400

    feat(atomic): complete atomic-did-you-mean component
    https://coveord.atlassian.net/browse/KIT-518

--- packages/atomic/src/components/atomic-did-you-mean/atomic-did-you-mean.tsx
+++ packages/atomic/src/components/atomic-did-you-mean/atomic-did-you-mean.tsx
@@ -3,10 +3,22 @@ import {DidYouMean, DidYouMeanState, buildDidYouMean} from '@coveo/headless';
 import {
   Bindings,
   BindStateToController,
+  BindStateToI18n,
   InitializableComponent,
   InitializeBindings,
 } from '../../utils/initialization-utils';
+import {sanitize} from '../../utils/xss-utils';
 
+/**
+ * The DidYouMean component is responsible for handling query corrections.
+ * When a query returns no result but finds a possible query correction, the component either suggests the correction or
+ * automatically triggers a new query with the suggested term.
+ *
+ * @part no-results - The text displayed for no results
+ * @part auto-corrected - The text displayed for the automatically corrected query
+ * @part correction-btn - The button that allows to manually correct a query
+ * @part highlight - The query highlights
+ */
 @Component({
   tag: 'atomic-did-you-mean',
   styleUrl: 'atomic-did-you-mean.pcss',
@@ -20,6 +32,18 @@ export class AtomicDidYouMean implements InitializableComponent {
   @State()
   private didYouMeanState!: DidYouMeanState;
   @State() public error!: Error;
+  @BindStateToI18n()
+  @State()
+  private strings = {
+    withQuery: (
+      key: 'noResultsFor' | 'queryAutoCorrectedTo' | 'didYouMean',
+      query: string
+    ) =>
+      this.bindings.i18n.t(key, {
+        interpolation: {escapeValue: false},
+        query: `<b part="highlight">${sanitize(query)}</b>`,
+      }),
+  };
 
   public initialize() {
     this.didYouMean = buildDidYouMean(this.bindings.engine);
@@ -29,33 +53,52 @@ export class AtomicDidYouMean implements InitializableComponent {
     this.didYouMean.applyCorrection();
   }
 
+  private renderAutomaticallyCorrected() {
+    const noResults = this.strings.withQuery(
+      'noResultsFor',
+      this.didYouMeanState.queryCorrection.wordCorrections[0].originalWord
+    );
+    const queryAutoCorrectedTo = this.strings.withQuery(
+      'queryAutoCorrectedTo',
+      this.didYouMeanState.wasCorrectedTo
+    );
+    return [
+      <p
+        class="text-on-background"
+        part="no-results"
+        innerHTML={noResults}
+      ></p>,
+      <p
+        class="text-on-background"
+        part="auto-corrected"
+        innerHTML={queryAutoCorrectedTo}
+      ></p>,
+    ];
+  }
+
+  private renderCorrection() {
+    const didYouMean = this.strings.withQuery(
+      'didYouMean',
+      this.didYouMeanState.queryCorrection.correctedQuery
+    );
+    return (
+      <button
+        class="text-primary"
+        onClick={() => this.applyCorrection()}
+        innerHTML={didYouMean}
+      ></button>
+    );
+  }
+
   public render() {
     if (!this.didYouMeanState.hasQueryCorrection) {
-      return '';
+      return;
     }
 
-    if (this.didYouMeanState.wasAutomaticallyCorrected) {
-      return [
-        <p>
-          No results for{' '}
-          <b>
-            {
-              this.didYouMeanState.queryCorrection.wordCorrections[0]
-                .originalWord
-            }
-          </b>
-        </p>,
-        <p>
-          Query was automatically corrected to{' '}
-          <b>{this.didYouMeanState.wasCorrectedTo}</b>
-        </p>,
-      ];
+    if (!this.didYouMeanState.wasAutomaticallyCorrected) {
+      return this.renderAutomaticallyCorrected();
     }
 
-    return (
-      <button onClick={() => this.applyCorrection()}>
-        Did you mean: {this.didYouMeanState.queryCorrection.correctedQuery} ?
-      </button>
-    );
+    return this.renderCorrection();
   }
 }
