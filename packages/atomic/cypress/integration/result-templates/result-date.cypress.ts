80d6765bcdc92c2e0eba49f95624869291d1e668
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-03-26 08:46:04 -0400

    test(atomic): write tests for result-list & all result template components
    https://coveord.atlassian.net/browse/KIT-512

---
+++ packages/atomic/cypress/integration/result-templates/result-date.cypress.ts
@@ -0,0 +1,21 @@
+describe('Result Date Component', () => {
+  describe('when not a child of an "template" element', () => {
+    it.skip('should render an "atomic-component-error" component');
+  });
+
+  describe('when the format is invalid', () => {
+    it.skip('should remove the component from the DOM');
+  });
+
+  describe('when the field does not exist for the result', () => {
+    it.skip('should remove the component from the DOM');
+  });
+
+  describe('when the field value is not a date', () => {
+    it.skip('should remove the component from the DOM');
+  });
+
+  describe('when the field & format are valid', () => {
+    it.skip('should render the field value formatted correctly');
+  });
+});
---
+++ packages/atomic/cypress/integration/result-templates/result-number.cypress.ts
@@ -0,0 +1,20 @@
+describe('Result Number Component', () => {
+  describe('when not a child of an "template" element', () => {
+    it.skip('should render an "atomic-component-error" component');
+  });
+
+  describe('when the field does not exist for the result', () => {
+    it.skip('should remove the component from the DOM');
+  });
+
+  describe('when the field value cannot be parsed to a number', () => {
+    // TODO: development required
+    it.skip('should remove the component from the DOM');
+  });
+
+  describe('when the field value is valid', () => {
+    it.skip('should render the component');
+
+    it.skip("the rendered value should respect the interface's language");
+  });
+});
