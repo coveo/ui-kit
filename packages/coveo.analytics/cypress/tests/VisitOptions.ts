80625326f1e4960fac1559c65e9c0d861b2dd068
Nicholas Labarre <Spuffynism@users.noreply.github.com>
2021-03-29 14:13:22 -0400

    Add FTs as certifier for dev (coveo/coveo.analytics.js#268)
    * [COM-814] support s3 location + add certifier
    
    * [COM-814] determine url according to env
    
    * [COM-814] improve scriptUrl building
    
    Original-commit: 40ccaef8c59317fab6cf101fdd6e94a06204f5b4

--- packages/coveo.analytics/cypress/tests/VisitOptions.ts
+++ packages/coveo.analytics/cypress/tests/VisitOptions.ts
@@ -3,11 +3,13 @@ export const getQueryParameters = () => {
         version: getVersion(),
         endpoint: getEndpoint(),
         token: getToken(),
+        targetEnv: getTargetEnv(),
     };
 };
 
 const getToken = () => (Cypress.env('token') as string) || 'invalid';
 const getVersion = () => (Cypress.env('version') as string) || '2';
 const getEndpoint = () => (Cypress.env('endpoint') as string) || 'https://platform.cloud.coveo.com/rest/ua';
+const getTargetEnv = () => (Cypress.env('targetEnv') as string) || 'prd';
 
 export const getCollectEndpoint = () => `${getEndpoint()}/v15/analytics/collect`;
