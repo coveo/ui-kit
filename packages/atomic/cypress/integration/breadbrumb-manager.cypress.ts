9c4c3cd08ec1972c433a5e40e89f67d599d2c0d0
William Mannard <wmannard@coveo.com>
2021-04-01 10:08:02 -0400

    test(atomic): steps for breadcrumb test
    https://coveord.atlassian.net/browse/KIT-492

--- packages/atomic/cypress/integration/breadbrumb-manager.cypress.ts
+++ packages/atomic/cypress/integration/breadbrumb-manager.cypress.ts
@@ -1,28 +1,30 @@
-import {injectComponent, setUpPage} from '../utils/setupComponent';
-
 describe('Breadcrumb Manager Test Suites', () => {
-  const tag = 'atomic-breadcrumb-manager';
-  const component = (attributes = '') => `<${tag} ${attributes}></${tag}>`;
-  const wait = 1000;
+  describe('Tests with default properties', () => {
+    it.skip('should not initially display any breadcrumbs');
 
-  describe('Default properties test', () => {
-    it.skip('should initially show not breadcrumbs');
     it.skip('should display one breadcrumb when a regular facet is selected.');
-    it.skip('should hide the breadcrumb when delelected in the facet');
-    it.skip('should hide the breadcrumb when it is clicked');
+
+    it.skip(
+      'should remove the breadcrumb when field is de-selected in the facet'
+    );
+
+    it.skip('should remove the breadcrumb when it is clicked');
+
     it.skip('should display multiple fields from a regular and category facet');
-    it.skip('should hide all breadcrumbs on clicking "Clear All Filters"');
+
+    it.skip('should remove all breadcrumbs on clicking "Clear All Filters"');
+
     it.skip('should hide 2 fields when 7 are slected');
+
     it.skip('should display all fields on clicking "Show More"');
+
     it.skip('should use "/" as separator for category-facet subcategories');
   });
 
-  const customized_component = (
-    attributes = 'collapse-threshold=4 category-divider=";"'
-  ) => `<${tag} ${attributes}></${tag}>`;
-
-  describe('Custom properties test ()', () => {
+  describe('Tests with customized properties', () => {
+    //'collapse-threshold=4 category-divider=";"'
     it.skip('should hide 3 fields when 7 are slected');
+
     it.skip('should use ";" as separator for category-facet subcategories');
   });
 });
