9d1b39981733946be4c20e1eb02c2276ba8373e1
olamothe <olamothe@coveo.com>
2021-04-06 15:16:18 -0400

    fix(atomic): fix breadcrumb label overflow
    https://coveord.atlassian.net/browse/KIT-577

--- packages/atomic/src/components/atomic-breadcrumb-manager/atomic-breadcrumb-manager.tsx
+++ packages/atomic/src/components/atomic-breadcrumb-manager/atomic-breadcrumb-manager.tsx
@@ -82,8 +82,9 @@ export class AtomicBreadcrumbManager implements InitializableComponent {
     return (
       <button
         part="breadcrumb-button"
-        class="text-on-background-variant breadcrumb-button flex items-center hover:text-primary-variant"
+        class="text-on-background-variant breadcrumb-button flex items-center hover:text-primary-variant w-full"
         aria-label={this.strings.breadcrumb({value})}
+        title={value}
         onClick={() =>
           this.breadcrumbManager.deselectBreadcrumb(breadcrumbValue)
         }
