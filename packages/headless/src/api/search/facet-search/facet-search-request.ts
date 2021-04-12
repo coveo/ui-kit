eae46a7c6165588beabf804d8eda174ea6165144
Luc Bergeron <10946843+lbergeron@users.noreply.github.com>
2021-04-12 09:17:13 -0400

    chore(headless): share request parameters

--- packages/headless/src/api/search/facet-search/facet-search-request.ts
+++ packages/headless/src/api/search/facet-search/facet-search-request.ts
@@ -1,6 +1,6 @@
 import {SpecificFacetSearchRequest} from './specific-facet-search/specific-facet-search-request';
 import {CategoryFacetSearchRequest} from './category-facet-search/category-facet-search-request';
-import {BaseParam} from '../search-api-params';
+import {BaseParam} from '../../platform-service-params';
 
 export type FacetSearchRequest = BaseParam &
   (SpecificFacetSearchRequest | CategoryFacetSearchRequest);
