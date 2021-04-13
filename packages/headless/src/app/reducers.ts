3557dd2201e10795966407e12a703e3461eded67
Sami Sayegh <ssayegh@coveo.com>
2021-04-13 18:22:44 -0400

    add facet reducers from controller
    https://coveord.atlassian.net/browse/KIT-264

--- packages/headless/src/app/reducers.ts
+++ packages/headless/src/app/reducers.ts
@@ -1,6 +1,11 @@
 import {configurationReducer} from '../features/configuration/configuration-slice';
+import {specificFacetSearchSetReducer} from '../features/facets/facet-search-set/specific/specific-facet-search-set-slice';
+import {facetSetReducer} from '../features/facets/facet-set/facet-set-slice';
 import {paginationReducer} from '../features/pagination/pagination-slice';
+import {searchReducer} from '../features/search/search-slice';
 
 export const configuration = configurationReducer;
-
 export const pagination = paginationReducer;
+export const facetSet = facetSetReducer;
+export const facetSearchSet = specificFacetSearchSetReducer;
+export const search = searchReducer;
