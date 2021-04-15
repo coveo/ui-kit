710b5b75750100992bafcb7c596525ae0a1a6001
Sami Sayegh <ssayegh@coveo.com>
2021-04-15 17:52:45 -0400

    fix definitions
    https://coveord.atlassian.net/browse/KIT-605

--- packages/quantic/coveo.d.ts
+++ packages/quantic/coveo.d.ts
@@ -1,8 +1,8 @@
 /* eslint-disable node/no-unpublished-import */
-import * as HeadlessTypes from './force-app/main/default/staticresources/coveoheadless/index';
-export * from './force-app/main/default/staticresources/coveoheadless/index';
+import * as HeadlessTypes from './force-app/main/default/staticresources/coveoheadless/definitions/index';
+export * from './force-app/main/default/staticresources/coveoheadless/definitions/index';
 import {LightningElement} from 'lwc';
-import {HeadlessEngine} from '../headless/dist/definitions/index';
+
 declare global {
   // eslint-disable-next-line no-undef
   const CoveoHeadless: typeof HeadlessTypes;
@@ -14,8 +14,8 @@ declare global {
           element: LightningElement;
           initialized: boolean;
         }[];
-        config: HeadlessConfigurationOptions;
-        engine: HeadlessEngine;
+        config: HeadlessTypes.HeadlessConfigurationOptions;
+        engine: HeadlessTypes.HeadlessEngine;
       };
     };
   }
