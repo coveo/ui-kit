328f9116b63710def9eed27d3ade0ff7d0e81f07
Sami Sayegh <ssayegh@coveo.com>
2021-04-22 19:30:24 -0400

    simplify HeadlessEngine tests
    https://coveord.atlassian.net/browse/KIT-632

--- packages/headless/src/app/headless-engine.test.ts
+++ packages/headless/src/app/headless-engine.test.ts
@@ -1,33 +1,16 @@
 import {HeadlessEngine, HeadlessOptions} from './headless-engine';
 import {updateSearchConfiguration} from '../features/configuration/configuration-actions';
-import * as storeConfig from './store';
+import * as Store from './store';
 import {searchAppReducers} from './search-app-reducers';
-import {AnalyticsClientSendEventHook} from 'coveo.analytics/dist/definitions/client/analytics';
-import pino from 'pino';
-import {validatePayloadAndThrow} from '../utils/validate-payload';
-import {buildMockSearchAPIClient} from '../test/mock-search-api-client';
-import {combineReducers} from '@reduxjs/toolkit';
 import {Engine} from './engine';
+import {buildMockStore} from '../test/mock-store';
 
 describe('headless engine', () => {
   let options: HeadlessOptions<typeof searchAppReducers>;
-  let configureStoreSpy: jest.SpyInstance;
   let engine: Engine;
 
   beforeEach(() => {
-    const store = storeConfig.configureStore({
-      reducer: combineReducers(searchAppReducers),
-      thunkExtraArguments: {
-        searchAPIClient: buildMockSearchAPIClient(),
-        analyticsClientMiddleware: {} as AnalyticsClientSendEventHook,
-        logger: pino({level: 'silent'}),
-        validatePayload: validatePayloadAndThrow,
-      },
-    });
-    jest.spyOn(store, 'dispatch');
-    configureStoreSpy = jest
-      .spyOn(storeConfig, 'configureStore')
-      .mockReturnValue(store);
+    jest.spyOn(Store, 'configureStore').mockReturnValue(buildMockStore());
 
     options = {
       configuration: HeadlessEngine.getSampleConfiguration(),
@@ -38,18 +21,8 @@ describe('headless engine', () => {
   });
 
   it('should thrown an error if the engine is constructed with invalid options', () => {
-    const invalidOptions: HeadlessOptions<typeof searchAppReducers> = {
-      ...options,
-      configuration: {
-        ...options.configuration,
-        organizationId: (123 as unknown) as string,
-      },
-    };
-    expect(() => new HeadlessEngine(invalidOptions)).toThrow();
-  });
-
-  it('should call configureStore', () => {
-    expect(configureStoreSpy).toHaveBeenCalled();
+    options.configuration.organizationId = (123 as unknown) as string;
+    expect(() => new HeadlessEngine(options)).toThrow();
   });
 
   it(`when there is a search param in the configuration
