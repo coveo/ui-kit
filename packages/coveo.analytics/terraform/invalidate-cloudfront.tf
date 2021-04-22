20b811d1a97c8cd22fa0ef8ab8e0d5bfa3a2c15d
François Lachance-Guillemette <flguillemette@coveo.com>
2021-04-22 10:50:56 -0400

    feat(deploy): invalidate the cloudfront cache when deploying to prod
    [COM-956]
    
    Original-commit: bac06eda0c696628ca400eb59538fc18202f9cd9

---
+++ packages/coveo.analytics/terraform/invalidate-cloudfront.tf
@@ -0,0 +1,19 @@
+variable "package_json_major_version" {
+  description = "Defines the major version that has just been deployed"
+}
+
+data "aws_ssm_parameter" "svc_coveoanalyticsjs_secret" {
+  name = "/${var.env}/coveoanalyticsjs/svcAccountSecret"
+}
+
+resource "null_resource" "invalidate-cloudfront" {
+  provisioner "local-exec" {
+    command = "aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths \"/coveo.analytics.js/${var.package_json_major_version}/*\""
+
+    environment = {
+      CLOUDFRONT_DISTRIBUTION_ID="E2VWLFSCSD1GLA"
+      AWS_ACCESS_KEY_ID="AKIAYKDJLZITZZKEN7WY"
+      AWS_SECRET_ACCESS_KEY=data.aws_ssm_parameter.svc_coveoanalyticsjs_secret
+    }
+  }
+}
