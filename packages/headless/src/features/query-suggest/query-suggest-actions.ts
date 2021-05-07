b97642df8b7831ca570bcd32b1c0ac45a591cf78
Sami Sayegh <ssayegh@coveo.com>
2021-05-07 14:53:12 -0400

    add loadQuerySuggestActions function
    https://coveord.atlassian.net/browse/KIT-661

--- packages/headless/src/features/query-suggest/query-suggest-actions.ts
+++ packages/headless/src/features/query-suggest/query-suggest-actions.ts
@@ -5,8 +5,6 @@ import {
   requiredEmptyAllowedString,
 } from '../../utils/validate-payload';
 import {NumberValue, StringValue} from '@coveo/bueno';
-import {QuerySuggestSuccessResponse} from '../../api/search/query-suggest/query-suggest-response';
-import {SearchAPIErrorWithStatusCode} from '../../api/search/search-api-error-response';
 import {
   isErrorResponse,
   AsyncThunkSearchOptions,
@@ -21,6 +19,7 @@ import {
 } from '../../state/state-sections';
 import {QuerySuggestRequest} from '../../api/search/query-suggest/query-suggest-request';
 import {historyStore} from '../../api/analytics/analytics';
+import {QuerySuggestSuccessResponse} from '../../api/search/query-suggest/query-suggest-response';
 
 export type StateNeededByQuerySuggest = ConfigurationSection &
   QuerySuggestionSection &
@@ -34,6 +33,25 @@ export interface QuerySuggestionID {
   id: string;
 }
 
+export interface RegisterQuerySuggestActionCreatorPayload {
+  /**
+   * A unique identifier for the new query suggest entity (e.g., `b953ab2e-022b-4de4-903f-68b2c0682942`).
+   */
+  id: string;
+
+  /**
+   * The partial basic query expression for which to request query suggestions (e.g., `cov`).
+   */
+  q?: string;
+
+  /**
+   * The number of query suggestions to request from Coveo ML (e.g., `3`).
+   *
+   * @defaultValue `5`.
+   */
+  count?: number;
+}
+
 /**
  * Registers a new query suggest entity to the headless state to enable the Coveo ML query suggestions feature.
  * @param id (string) A unique identifier for the new query suggest entity (e.g., `b953ab2e-022b-4de4-903f-68b2c0682942`).
@@ -42,7 +60,7 @@ export interface QuerySuggestionID {
  */
 export const registerQuerySuggest = createAction(
   'querySuggest/register',
-  (payload: {id: string; q?: string; count?: number}) =>
+  (payload: RegisterQuerySuggestActionCreatorPayload) =>
     validatePayload(payload, {
       ...idDefinition,
       q: new StringValue(),
@@ -59,6 +77,18 @@ export const unregisterQuerySuggest = createAction(
   (payload: {id: string}) => validatePayload(payload, idDefinition)
 );
 
+export interface SelectQuerySuggestionActionCreatorPayload {
+  /**
+   * The unique identifier of the target query suggest entity (e.g., `b953ab2e-022b-4de4-903f-68b2c0682942`).
+   */
+  id: string;
+
+  /**
+   * The selected query suggestion (e.g., `coveo`).
+   */
+  expression: string;
+}
+
 /**
  * Selects a suggestion provided through a specific query suggest entity.
  * @param id (string) The unique identifier of the target query suggest entity (e.g., `b953ab2e-022b-4de4-903f-68b2c0682942`).
@@ -66,12 +96,19 @@ export const unregisterQuerySuggest = createAction(
  */
 export const selectQuerySuggestion = createAction(
   'querySuggest/selectSuggestion',
-  (payload: {id: string; expression: string}) =>
+  (payload: SelectQuerySuggestionActionCreatorPayload) =>
     validatePayload(payload, {
       ...idDefinition,
       expression: requiredEmptyAllowedString,
     })
 );
+
+export interface ClearQuerySuggestActionCreatorPayload {
+  /**
+   * The unique identifier of the target query suggest entity (e.g., `b953ab2e-022b-4de4-903f-68b2c0682942`).
+   */
+  id: string;
+}
 
 /**
  * Clears the current partial basic query expression and the list of query suggestions in a specific query suggest entity.
@@ -79,7 +116,8 @@ export const selectQuerySuggestion = createAction(
  */
 export const clearQuerySuggest = createAction(
   'querySuggest/clear',
-  (payload: {id: string}) => validatePayload(payload, idDefinition)
+  (payload: ClearQuerySuggestActionCreatorPayload) =>
+    validatePayload(payload, idDefinition)
 );
 
 /**
@@ -91,15 +129,26 @@ export const clearQuerySuggestCompletions = createAction(
   (payload: {id: string}) => validatePayload(payload, idDefinition)
 );
 
+export interface FetchQuerySuggestionsActionCreatorPayload {
+  /**
+   * The unique identifier of the target query suggest entity (e.g., `b953ab2e-022b-4de4-903f-68b2c0682942`).
+   */
+  id: string;
+}
+
+export interface FetchQuerySuggestionsThunkReturn
+  extends FetchQuerySuggestionsActionCreatorPayload,
+    QuerySuggestSuccessResponse {}
+
 /**
  * Fetches a list of query suggestions for a specific query suggest entity according to the current headless state.
  * @param id (string) The unique identifier of the target query suggest entity (e.g., `b953ab2e-022b-4de4-903f-68b2c0682942`).
  */
 export const fetchQuerySuggestions = createAsyncThunk<
-  QuerySuggestionID & QuerySuggestSuccessResponse,
-  QuerySuggestionID,
+  FetchQuerySuggestionsThunkReturn,
+  FetchQuerySuggestionsActionCreatorPayload,
   AsyncThunkSearchOptions<StateNeededByQuerySuggest> & {
-    rejectValue: SearchAPIErrorWithStatusCode & QuerySuggestionID;
+    rejectValue: FetchQuerySuggestionsActionCreatorPayload;
   }
 >(
   'querySuggest/fetch',
