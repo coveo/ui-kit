e9b0c906b61275db6de8cf3dd81d9f7a2b833e53
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-05-07 14:39:10 -0400

    fix(atomic): category facet x overflow & breadcrumb x position
    https://coveord.atlassian.net/browse/KIT-672

--- packages/atomic/src/components/atomic-breadcrumb-manager/atomic-breadcrumb-manager.tsx
+++ packages/atomic/src/components/atomic-breadcrumb-manager/atomic-breadcrumb-manager.tsx
@@ -90,7 +90,7 @@ export class AtomicBreadcrumbManager implements InitializableComponent {
         <span class="ellipsed">{value}</span>
         <div
           role="button"
-          class="pt-1 w-2.5 ml-1.5 flex-shrink-0"
+          class="w-2.5 ml-1.5 flex-shrink-0 self-center"
           innerHTML={mainclear}
         />
       </button>
