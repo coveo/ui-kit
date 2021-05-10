875831fe4145242a51da5d4c8e9bc85f918cc535
Benjamin Taillon <btaillon@coveo.com>
2021-05-10 15:03:36 -0400

    documented folding action payload
    https://coveord.atlassian.net/browse/KIT-623

--- packages/headless/src/features/folding/folding-actions.ts
+++ packages/headless/src/features/folding/folding-actions.ts
@@ -2,15 +2,35 @@ import {NumberValue, SchemaDefinition, StringValue} from '@coveo/bueno';
 import {createAction} from '@reduxjs/toolkit';
 import {validatePayload} from '../../utils/validate-payload';
 
-export interface RegisterFoldingPayload {
+export interface RegisterFoldingActionCreatorPayload {
+  /**
+   * The name of the field on which to do the folding. The folded result list component will use the values of this field to resolve the collections of result items.
+   *
+   * @defaultValue `foldingcollection`
+   */
   collectionField?: string;
+  /**
+   * The name of the field that determines whether a certain result is a top result containing other child results within a collection.
+   *
+   * @defaultValue `foldingparent`
+   */
   parentField?: string;
+  /**
+   * The name of the field that uniquely identifies a result within a collection.
+   *
+   * @defaultValue `foldingchild`
+   */
   childField?: string;
+  /**
+   * The number of child results to fold under the root collection element, before expansion.
+   *
+   * @defaultValue `2`
+   */
   numberOfFoldedResults?: number;
 }
 
 export const foldingOptionsSchemaDefinition: SchemaDefinition<Required<
-  RegisterFoldingPayload
+  RegisterFoldingActionCreatorPayload
 >> = {
   collectionField: new StringValue(),
   parentField: new StringValue(),
@@ -20,6 +40,6 @@ export const foldingOptionsSchemaDefinition: SchemaDefinition<Required<
 
 export const registerFolding = createAction(
   'folding/register',
-  (payload: RegisterFoldingPayload) =>
+  (payload: RegisterFoldingActionCreatorPayload) =>
     validatePayload(payload, foldingOptionsSchemaDefinition)
 );
