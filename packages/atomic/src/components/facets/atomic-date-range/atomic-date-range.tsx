a0dff4a29826bd57b4474c33b7e3c8d6c1758b06
Nicolas Buisson <nbuisson@coveo.com>
2021-05-17 16:13:26 -0400

    added parameter validation to start and end of AtomicDateRange
    https://coveord.atlassian.net/browse/KIT-659

--- packages/atomic/src/components/facets/atomic-date-range/atomic-date-range.tsx
+++ packages/atomic/src/components/facets/atomic-date-range/atomic-date-range.tsx
@@ -1,4 +1,4 @@
-import {Component, Element, Prop} from '@stencil/core';
+import {Component, Element, Prop, Watch} from '@stencil/core';
 
 /**
  * The `atomic-date-range` component defines the range of an `atomic-date-facet`, and therefore must be defined within an `atomic-date-facet` component.
@@ -15,9 +15,24 @@ export class AtomicDateRange {
    * It can be expressed as a Javascript date, as a number using epoch time or as a string using the ISO 8601 format.
    */
   @Prop() public start!: Date | string | number;
+
+  @Watch('start')
+  validateStart() {
+    if (!this.start) {
+      throw new Error('The start date has not been set.');
+    }
+  }
+
   /**
    * The ending date for the range.
    * It can be expressed as a Javascript date, as a number using epoch time or as a string using the ISO 8601 format.
    */
   @Prop() public end!: Date | string | number;
+
+  @Watch('end')
+  validateEnd() {
+    if (!this.end) {
+      throw new Error('The end date has not been set.');
+    }
+  }
 }
