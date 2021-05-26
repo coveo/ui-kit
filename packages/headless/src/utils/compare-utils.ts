5afb18815d7705fe287418cefec0721b8a781cd7
Jean-Félix Thibodeau <ThibodeauJF@users.noreply.github.com>
2021-05-26 10:42:46 -0400

    fix(atomic,headless): url manager only updates when the paramaters values change and not their order (#834)
    https://coveord.atlassian.net/browse/KIT-660

---
+++ packages/headless/src/utils/compare-utils.ts
@@ -0,0 +1,27 @@
+import {createCustomEqual} from 'fast-equals';
+
+export function arrayEqual<T>(firstArray: T[], secondArray: T[]) {
+  return (
+    firstArray.length === secondArray.length &&
+    firstArray.findIndex((val, i) => secondArray[i] !== val) === -1
+  );
+}
+
+export const deepEqualAnyOrder = createCustomEqual(
+  (deepEqual) => (firstObject, secondObject) => {
+    if (Array.isArray(firstObject) && Array.isArray(secondObject)) {
+      if (firstObject.length !== secondObject.length) {
+        return false;
+      }
+
+      return firstObject.every(
+        (firstVal) =>
+          secondObject.findIndex((secondVal) =>
+            deepEqual(firstVal, secondVal)
+          ) !== -1
+      );
+    }
+
+    return deepEqual(firstObject, secondObject);
+  }
+);
