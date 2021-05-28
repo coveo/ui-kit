98045c34d63808ce106d99a08ebae7bfa15dd0dd
Sami Sayegh <ssayegh@coveo.com>
2021-05-28 16:45:19 -0400

    add deprecation messages
    https://coveord.atlassian.net/browse/KIT-696

--- packages/headless/src/api/platform-client.ts
+++ packages/headless/src/api/platform-client.ts
@@ -21,6 +21,9 @@ export interface PlatformClientCallOptions {
   headers?: Record<string, string>;
   requestParams: unknown;
   accessToken: string;
+  /**
+   * @deprecated - Please configure this option when configuring the engine. It will be remove in the next major version.
+   */
   renewAccessToken: () => Promise<string>;
   preprocessRequest: PreprocessRequest;
   deprecatedPreprocessRequest: PreprocessRequestMiddleware;
