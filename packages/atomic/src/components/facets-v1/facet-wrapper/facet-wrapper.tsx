6ecdd189c2cde059f6938a7b88784a8e3a698ffa
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-06-01 15:31:05 -0400

    delete wrapper
    https://coveord.atlassian.net/browse/KIT-698

--- packages/atomic/src/components/facets-v1/facet-wrapper/facet-wrapper.tsx
+++
@@ -1,7 +0,0 @@
-import {FunctionalComponent, h} from '@stencil/core';
-
-export const FacetWrapper: FunctionalComponent = (_, children) => (
-  <div class="border border-neutral rounded-lg p-6" part="facet">
-    {children}
-  </div>
-);
