37e0dc6cbfbfca4b6d9c60c8bd2b2958ea00092f
btaillon <54454747+btaillon@users.noreply.github.com>
2021-06-08 11:50:17 -0400

    feat(headless,atomic): added error when losing connection (#865)
    https://coveord.atlassian.net/browse/KIT-637

--- packages/headless/src/api/search/search-api-error-response.ts
+++ packages/headless/src/api/search/search-api-error-response.ts
@@ -1,3 +1,4 @@
+import {DisconnectedError} from '../../utils/errors';
 import {QueryException} from './search/query-exception';
 
 export interface SearchAPIErrorWithStatusCode {
@@ -8,4 +9,21 @@ export interface SearchAPIErrorWithStatusCode {
 
 export interface SearchAPIErrorWithExceptionInBody {
   exception: QueryException;
+}
+
+function buildDisconnectedError(): SearchAPIErrorWithStatusCode {
+  return {
+    statusCode: 0,
+    type: 'Disconnected',
+    message: 'Could not connect',
+  };
+}
+
+export function buildAPIResponseFromErrorOrThrow(
+  error: Error
+): {error: SearchAPIErrorWithStatusCode} {
+  if (error instanceof DisconnectedError) {
+    return {error: buildDisconnectedError()};
+  }
+  throw error;
 }
