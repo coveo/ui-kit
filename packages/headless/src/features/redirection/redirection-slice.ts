6c6615dbb2d28e53c354f8ce4eaa003dc1d724c0
Nicolas Buisson <nbuisson@coveo.com>
2021-06-08 16:06:24 -0400

    added executeSeearch action to redirection slice
    https://coveord.atlassian.net/browse/KIT-548

--- packages/headless/src/features/redirection/redirection-slice.ts
+++ packages/headless/src/features/redirection/redirection-slice.ts
@@ -1,11 +1,24 @@
 import {createReducer} from '@reduxjs/toolkit';
+import {executeSearch} from '../search/search-actions';
 import {checkForRedirection} from './redirection-actions';
 import {getRedirectionInitialState} from './redirection-state';
+import {
+  Trigger,
+  TriggerRedirect,
+  isTriggerRedirect,
+} from './../../api/search/trigger';
 
 export const redirectionReducer = createReducer(
   getRedirectionInitialState(),
   (builder) =>
-    builder.addCase(checkForRedirection.fulfilled, (state, action) => {
-      state.redirectTo = action.payload;
-    })
+    builder
+      .addCase(checkForRedirection.fulfilled, (state, action) => {
+        state.redirectTo = action.payload;
+      })
+      .addCase(executeSearch.fulfilled, (state, action) => {
+        const redirectTriggers: Trigger[] = action.payload.response.triggers.filter(
+          (trigger) => isTriggerRedirect(trigger)
+        );
+        state.redirectTo = (redirectTriggers[0] as TriggerRedirect).content;
+      })
 );
