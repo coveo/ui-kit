b61e0990e577c3dec4a972bbcf44fa7659ab1951
Sami <ssayegh@coveo.com>
2021-06-09 14:01:48 -0400

    feat(headless): expose generic analytics actions via loader function (#873)
    https://coveord.atlassian.net/browse/KIT-723

--- packages/headless/src/features/analytics/index.ts
+++ packages/headless/src/features/analytics/index.ts
@@ -187,3 +187,4 @@ export namespace SearchAnalyticsActions {
 
 export * from './search-analytics-actions-loader';
 export * from './click-analytics-actions-loader';
+export * from './generic-analytics-actions-loader';
