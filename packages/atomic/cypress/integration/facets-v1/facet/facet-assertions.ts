355e636a366e834d2b83b5c0b946ccb94d398fa6
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-06-10 14:23:49 -0400

    test(atomic): first cypress tests for facet v1
    https://coveord.atlassian.net/browse/KIT-725

---
+++ packages/atomic/cypress/integration/facets-v1/facet/facet-assertions.ts
@@ -0,0 +1,155 @@
+import {TestFixture} from '../../../fixtures/test-fixture';
+import {doSortAlphanumeric} from '../../../utils/componentUtils';
+import {ComponentErrorSelectors} from '../../component-error-selectors';
+import {facetComponent, FacetSelectors} from './facet-selectors';
+
+function should(should: boolean) {
+  return should ? 'should' : 'should not';
+}
+
+export function assertAccessibility() {
+  it('should pass accessibility tests', () => {
+    cy.checkA11y(facetComponent);
+  });
+}
+
+export function assertContainsComponentError(display: boolean) {
+  it(`${should(display)} display an error component`, () => {
+    FacetSelectors.shadow()
+      .find(ComponentErrorSelectors.component)
+      .should(display ? 'be.visible' : 'not.exist');
+  });
+}
+
+export function assertDisplayFacet(display: boolean) {
+  it(`${should(display)} display the facet`, () => {
+    FacetSelectors.wrapper().should(display ? 'be.visible' : 'not.exist');
+  });
+}
+
+export function assertDisplayPlaceholder(display: boolean) {
+  it(`${should(display)} display the placeholder`, () => {
+    FacetSelectors.placeholder().should(display ? 'be.visible' : 'not.exist');
+  });
+}
+
+export function assertNumberOfSelectedCheckboxValues(value: number) {
+  it(`should display ${value} number of selected checkbox values`, () => {
+    if (value > 0) {
+      FacetSelectors.selectedCheckboxValue().its('length').should('eq', value);
+      return;
+    }
+
+    FacetSelectors.selectedCheckboxValue().should('not.exist');
+  });
+}
+
+export function assertNumberOfIdleCheckboxValues(value: number) {
+  it(`should display ${value} number of idle checkbox values`, () => {
+    if (value > 0) {
+      FacetSelectors.idleCheckboxValue().its('length').should('eq', value);
+      return;
+    }
+
+    FacetSelectors.idleCheckboxValue().should('not.exist');
+  });
+}
+
+export function assertDisplayShowMoreButton(display: boolean) {
+  it(`${should(display)} display a "Show more" button`, () => {
+    FacetSelectors.showMoreButton().should(
+      display ? 'be.visible' : 'not.exist'
+    );
+  });
+}
+
+export function assertDisplayShowLessButton(display: boolean) {
+  it(`${should(display)} display a "Show less" button`, () => {
+    FacetSelectors.showLessButton().should(
+      display ? 'be.visible' : 'not.exist'
+    );
+  });
+}
+
+export function assertDisplayClearButton(display: boolean) {
+  it(`${should(display)} display a "Clear filter" button`, () => {
+    FacetSelectors.clearButton().should(display ? 'be.visible' : 'not.exist');
+  });
+}
+
+export function assertLabelContains(label: string) {
+  it(`should have the label ${label}`, () => {
+    FacetSelectors.labelButton().contains(label);
+  });
+}
+
+export function assertDisplaySearchInput(display: boolean) {
+  it(`${should(display)} display a the facet search input`, () => {
+    FacetSelectors.searchInput().should(display ? 'be.visible' : 'not.exist');
+  });
+}
+
+export function assertLogFacetSelect(field: string, index: number) {
+  it('should log the facet select results to UA ', () => {
+    cy.wait(TestFixture.interceptAliases.UA).then((intercept) => {
+      const analyticsBody = intercept.request.body;
+      expect(analyticsBody).to.have.property('actionCause', 'facetSelect');
+      expect(analyticsBody.customData).to.have.property('facetField', field);
+      expect(analyticsBody.facetState[0]).to.have.property('state', 'selected');
+      expect(analyticsBody.facetState[0]).to.have.property('field', field);
+
+      FacetSelectors.facetValueLabelAtIndex(index)
+        .invoke('text')
+        .then((txt: string) => {
+          expect(analyticsBody.customData).to.have.property('facetValue', txt);
+        });
+    });
+  });
+}
+
+export function assertLogClearFacetValues(field: string) {
+  it('should log the facet clear all to UA', () => {
+    cy.wait(TestFixture.interceptAliases.UA).then((intercept) => {
+      const analyticsBody = intercept.request.body;
+      expect(analyticsBody).to.have.property('actionCause', 'facetClearAll');
+      expect(analyticsBody.customData).to.have.property('facetField', field);
+    });
+  });
+}
+
+export function assertValuesSortedAlphanumerically() {
+  it('values should be ordered alphanumerically', () => {
+    FacetSelectors.valueLabel().as('facetAllValuesLabel');
+    cy.getTextOfAllElements('@facetAllValuesLabel').then((originalValues) => {
+      expect(originalValues).to.eql(doSortAlphanumeric(originalValues));
+    });
+  });
+}
+
+export function assertLogFacetShowMore(field: string) {
+  it('should log the facet show more results to UA', () => {
+    cy.wait(TestFixture.interceptAliases.UA).then((intercept) => {
+      const analyticsBody = intercept.request.body;
+      expect(analyticsBody).to.have.property('eventType', 'facet');
+      expect(analyticsBody).to.have.property(
+        'eventValue',
+        'showMoreFacetResults'
+      );
+      expect(analyticsBody.customData).to.have.property('facetField', field);
+    });
+  });
+}
+
+export function assertLogFacetShowLess(field: string) {
+  it('should log the facet show less results to UA', () => {
+    cy.wait(TestFixture.interceptAliases.UA).then((intercept) => {
+      const analyticsBody = intercept.request.body;
+      expect(analyticsBody).to.have.property('eventType', 'facet');
+      expect(analyticsBody).to.have.property(
+        'eventValue',
+        'showLessFacetResults'
+      );
+      expect(analyticsBody.customData).to.have.property('facetField', field);
+    });
+  });
+}
---
+++ packages/atomic/cypress/integration/facets-v1/facet/facet.cypress.ts
@@ -0,0 +1,182 @@
+import {TestFixture} from '../../../fixtures/test-fixture';
+import {FacetSelectors} from './facet-selectors';
+import {
+  addFacet,
+  field,
+  label,
+  defaultNumberOfValues,
+  selectIdleCheckboxValueAt,
+} from './facet-actions';
+import * as FacetAssertions from './facet-assertions';
+
+describe('Facet v1 Test Suites', () => {
+  describe('with checkbox values', () => {
+    function setupWithCheckboxValues() {
+      new TestFixture().with(addFacet({field, label})).init();
+      cy.wait(TestFixture.interceptAliases.UA);
+    }
+
+    describe('verify rendering', () => {
+      before(setupWithCheckboxValues);
+
+      FacetAssertions.assertAccessibility();
+      FacetAssertions.assertContainsComponentError(false);
+      FacetAssertions.assertDisplayFacet(true);
+      FacetAssertions.assertDisplayPlaceholder(false);
+      FacetAssertions.assertNumberOfSelectedCheckboxValues(0);
+      FacetAssertions.assertNumberOfIdleCheckboxValues(defaultNumberOfValues);
+      FacetAssertions.assertDisplayShowMoreButton(true);
+      FacetAssertions.assertDisplayShowLessButton(false);
+      FacetAssertions.assertLabelContains(label);
+      FacetAssertions.assertDisplayClearButton(false);
+      FacetAssertions.assertDisplaySearchInput(true);
+    });
+
+    describe('when selecting a value', () => {
+      const selectionIndex = 2;
+      function setupSelectCheckboxValue() {
+        setupWithCheckboxValues();
+        selectIdleCheckboxValueAt(selectionIndex);
+        cy.wait(TestFixture.interceptAliases.Search);
+      }
+
+      describe('verify rendering', () => {
+        before(setupSelectCheckboxValue);
+
+        FacetAssertions.assertAccessibility();
+        FacetAssertions.assertDisplayClearButton(true);
+        FacetAssertions.assertNumberOfSelectedCheckboxValues(1);
+        FacetAssertions.assertNumberOfIdleCheckboxValues(
+          defaultNumberOfValues - 1
+        );
+      });
+
+      describe('verify analytics', () => {
+        before(setupSelectCheckboxValue);
+        FacetAssertions.assertLogFacetSelect(field, selectionIndex);
+      });
+
+      describe('when selecting a second value', () => {
+        const secondSelectionIndex = 0;
+        function setupSelectSecondCheckboxValue() {
+          setupSelectCheckboxValue();
+          cy.wait(TestFixture.interceptAliases.UA);
+          selectIdleCheckboxValueAt(secondSelectionIndex);
+          cy.wait(TestFixture.interceptAliases.Search);
+        }
+
+        describe('verify rendering', () => {
+          before(setupSelectSecondCheckboxValue);
+
+          FacetAssertions.assertDisplayClearButton(true);
+          FacetAssertions.assertNumberOfSelectedCheckboxValues(2);
+          FacetAssertions.assertNumberOfIdleCheckboxValues(
+            defaultNumberOfValues - 2
+          );
+        });
+
+        describe('verify analytics', () => {
+          before(setupSelectSecondCheckboxValue);
+
+          FacetAssertions.assertLogFacetSelect(field, secondSelectionIndex);
+        });
+
+        describe('when selecting the "Clear" button', () => {
+          function setupClearCheckboxValues() {
+            setupSelectSecondCheckboxValue();
+            cy.wait(TestFixture.interceptAliases.UA);
+            FacetSelectors.clearButton().click();
+            cy.wait(TestFixture.interceptAliases.Search);
+          }
+
+          describe('verify rendering', () => {
+            before(setupClearCheckboxValues);
+
+            FacetAssertions.assertDisplayClearButton(true);
+            FacetAssertions.assertNumberOfSelectedCheckboxValues(0);
+            FacetAssertions.assertNumberOfIdleCheckboxValues(
+              defaultNumberOfValues
+            );
+          });
+          describe('verify analytics', () => {
+            before(setupClearCheckboxValues);
+
+            FacetAssertions.assertLogClearFacetValues(field);
+          });
+        });
+      });
+    });
+
+    describe('when selecting the "Show more" button', () => {
+      function setupSelectShowMore() {
+        setupWithCheckboxValues();
+        FacetSelectors.showMoreButton().click();
+        cy.wait(TestFixture.interceptAliases.Search);
+      }
+
+      describe('verify rendering', () => {
+        before(setupSelectShowMore);
+
+        FacetAssertions.assertDisplayShowMoreButton(true);
+        FacetAssertions.assertDisplayShowLessButton(true);
+        FacetAssertions.assertValuesSortedAlphanumerically();
+        FacetAssertions.assertNumberOfIdleCheckboxValues(
+          defaultNumberOfValues * 2
+        );
+      });
+
+      describe('verify analytics', () => {
+        before(setupSelectShowMore);
+
+        FacetAssertions.assertLogFacetShowMore(field);
+      });
+
+      describe('when selecting the "Show less" button', () => {
+        function setupSelectShowLess() {
+          setupSelectShowMore();
+          cy.wait(TestFixture.interceptAliases.UA);
+          FacetSelectors.showLessButton().click();
+          cy.wait(TestFixture.interceptAliases.Search);
+        }
+
+        describe('verify rendering', () => {
+          before(setupSelectShowLess);
+
+          FacetAssertions.assertDisplayShowMoreButton(true);
+          FacetAssertions.assertDisplayShowLessButton(false);
+          FacetAssertions.assertNumberOfIdleCheckboxValues(
+            defaultNumberOfValues
+          );
+        });
+
+        describe('verify analytics', () => {
+          before(setupSelectShowLess);
+
+          FacetAssertions.assertLogFacetShowLess(field);
+        });
+      });
+    });
+  });
+
+  describe.skip('with link values', () => {});
+
+  describe.skip('with box values', () => {});
+
+  describe.skip('when being collapsed', () => {});
+
+  describe.skip('with custom #numberOfValues', () => {});
+
+  describe.skip('with custom #sortCriteria, alphanumeric', () => {});
+
+  describe.skip('with custom #sortCriteria, occurrences', () => {});
+
+  describe.skip('with #withSearch to false', () => {});
+
+  describe.skip('when no search has yet been executed', () => {});
+
+  describe.skip('with an invalid option', () => {});
+
+  describe.skip('when field returns no results', () => {});
+
+  describe.skip('with a selected path in the URL', () => {});
+});
