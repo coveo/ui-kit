dd1fdfb53e99c027344bae4cf660732cd21f920a
Sami Sayegh <ssayegh@coveo.com>
2021-06-16 15:51:35 -0400

    export recommendation click actions
    https://coveord.atlassian.net/browse/KIT-757

--- packages/headless/src/recommendation.index.ts
+++ packages/headless/src/recommendation.index.ts
@@ -23,6 +23,7 @@ export * from './features/pipeline/pipeline-actions-loader';
 export * from './features/search-hub/search-hub-actions-loader';
 export * from './features/debug/debug-actions-loader';
 export * from './features/recommendation/recommendation-actions-loader';
+export * from './features/recommendation/recommendation-click-analytics-actions-loader';
 
 // Controllers
 export {
