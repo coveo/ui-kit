dd8e52c0f607278f5977e38318ebdaa5da3497c4
Nicolas Buisson <nbuisson@coveo.com>
2021-06-22 13:38:02 -0400

    initial version controller
    https://coveord.atlassian.net/browse/KIT-764

---
+++ packages/headless/src/controllers/triggers/headless-query-trigger.test.ts
@@ -0,0 +1,67 @@
+import {QueryTrigger, buildQueryTrigger} from './headless-query-trigger';
+import {buildMockSearchAppEngine, MockEngine} from '../../test/mock-engine';
+import {SearchAppState} from '../../state/search-app-state';
+import {triggers, configuration} from '../../app/reducers';
+import {updateQuery} from '../../features/query/query-actions';
+import {executeSearch} from '../../features/search/search-actions';
+
+describe('QueryTrigger', () => {
+  let engine: MockEngine<SearchAppState>;
+  let queryTrigger: QueryTrigger;
+
+  function initQueryTrigger() {
+    queryTrigger = buildQueryTrigger(engine);
+  }
+
+  function getUpdateQueryAction() {
+    return engine.actions.find((a) => a.type === updateQuery.type);
+  }
+
+  function registeredListeners() {
+    return (engine.subscribe as jest.Mock).mock.calls.map((args) => args[0]);
+  }
+
+  beforeEach(() => {
+    engine = buildMockSearchAppEngine();
+    initQueryTrigger();
+  });
+
+  it('initializes', () => {
+    expect(queryTrigger).toBeTruthy();
+  });
+
+  it('it adds the correct reducers to the engine', () => {
+    expect(engine.addReducers).toHaveBeenCalledWith({
+      triggers,
+      configuration,
+    });
+  });
+
+  it('exposes a #subscribe method', () => {
+    expect(queryTrigger.subscribe).toBeTruthy();
+  });
+
+  it('when the #engine.state.triggers.query is not updated, it does not dispatch #updateQuery, #executeSearch, and #logTriggerQuery', () => {
+    const listener = jest.fn();
+    queryTrigger.subscribe(listener);
+
+    const [firstListener] = registeredListeners();
+    firstListener();
+
+    expect(listener).toHaveBeenCalledTimes(0);
+    expect(getUpdateQueryAction()).toBeFalsy();
+    expect(engine.findAsyncAction(executeSearch.pending)).toBeFalsy();
+  });
+  it('when the #engine.state.triggers.query is updated, it dispatches #updateQuery, #executeSearch, and #logTriggerQuery', () => {
+    const listener = jest.fn();
+    queryTrigger.subscribe(listener);
+
+    engine.state.triggers.query = 'bananas';
+    const [firstListener] = registeredListeners();
+    firstListener();
+
+    expect(listener).toHaveBeenCalledTimes(1);
+    expect(getUpdateQueryAction()).toBeTruthy();
+    expect(engine.findAsyncAction(executeSearch.pending)).toBeTruthy();
+  });
+});
---
+++ packages/headless/src/controllers/triggers/headless-query-trigger.ts
@@ -0,0 +1,82 @@
+import {Engine} from '../../app/headless-engine';
+import {ConfigurationSection, TriggerSection} from '../../state/state-sections';
+import {configuration, triggers} from '../../app/reducers';
+import {buildController, Controller} from '../controller/headless-controller';
+import {loadReducerError} from '../../utils/errors';
+import {executeSearch} from '../../features/search/search-actions';
+import {
+  updateQuery,
+  UpdateQueryActionCreatorPayload,
+} from '../../features/query/query-actions';
+import {logTriggerQuery} from '../../features/triggers/trigger-analytics-actions';
+
+/**
+ * The `QueryTrigger` controller handles query triggers.
+ */
+export interface QueryTrigger extends Controller {
+  /**
+   * the state of the `QueryTrigger` controller.
+   */
+  state: QueryTriggerState;
+}
+
+/**
+ * A scoped and simplified part of the headless state that is relevant to the `QueryTrigger` controller.
+ */
+export interface QueryTriggerState {
+  /**
+   * The new query to perform a search with after receiving a query trigger.
+   */
+  query: string | null;
+}
+
+/**
+ * Creates a `QueryTrigger` controller instance.
+ *
+ * @param engine - The headless engine.
+ * @returns A `QueryTrigger` controller instance.
+ * */
+export function buildQueryTrigger(engine: Engine<object>): QueryTrigger {
+  if (!loadQueryTriggerReducers(engine)) {
+    throw loadReducerError;
+  }
+
+  const controller = buildController(engine);
+  const {dispatch} = engine;
+
+  const getState = () => engine.state;
+
+  return {
+    ...controller,
+
+    subscribe(listener: () => void) {
+      const strictListener = () => {
+        if (this.state.query) {
+          listener();
+          const updateQueryPayload: UpdateQueryActionCreatorPayload = {
+            q: this.state.query,
+          };
+          dispatch(updateQuery(updateQueryPayload));
+          //maybe logSearchBoxSubmit instead
+          //and dispatch logTriggerQuery  separately?
+          dispatch(executeSearch(logTriggerQuery()));
+        }
+      };
+      strictListener();
+      return engine.subscribe(strictListener);
+    },
+
+    get state() {
+      return {
+        query: getState().triggers.query,
+      };
+    },
+  };
+}
+
+function loadQueryTriggerReducers(
+  engine: Engine<object>
+): engine is Engine<TriggerSection & ConfigurationSection> {
+  engine.addReducers({configuration, triggers});
+  return true;
+}
