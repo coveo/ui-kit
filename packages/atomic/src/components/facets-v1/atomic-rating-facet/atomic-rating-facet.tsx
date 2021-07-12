b7e2694a991cce6c889ad49452434b320b6e8154
Nicolas Buisson <nbuisson@coveo.com>
2021-07-12 16:35:31 -0400

    icon display with classes
    https://coveord.atlassian.net/browse/KIT-812

--- packages/atomic/src/components/facets-v1/atomic-rating-facet/atomic-rating-facet.tsx
+++ packages/atomic/src/components/facets-v1/atomic-rating-facet/atomic-rating-facet.tsx
@@ -70,18 +70,30 @@ export class AtomicRatingFacet
   @State() public isCollapsed = false;
 
   //make prop in the future so devs can pass in their own svg
-  private icon = (
+  private iconActive = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="12"
       height="13"
       viewBox="0 0 12 13"
       fill="none"
+      class="star-active"
     >
       <path d="M2.12261 7.32528L0.386144 5.64869C-0.196726 5.08592 0.0965754 4.09991 0.892272 3.94721L3.52262 3.44244C3.84159 3.38123 4.11096 3.16892 4.24501 2.87309L5.08915 1.01016C5.44396 0.227133 6.55604 0.227132 6.91085 1.01016L7.75499 2.87309C7.88904 3.16892 8.15841 3.38123 8.47738 3.44244L11.1077 3.94721C11.9034 4.09991 12.1967 5.08592 11.6139 5.64869L9.87739 7.32528C9.64107 7.55345 9.53188 7.88314 9.58529 8.20726L10.0399 10.966C10.1756 11.79 9.3013 12.4076 8.57008 12.0042L6.48308 10.8527C6.18239 10.6868 5.81761 10.6868 5.51692 10.8527L3.42992 12.0042C2.6987 12.4076 1.82437 11.79 1.96014 10.966L2.41471 8.20726C2.46812 7.88314 2.35893 7.55345 2.12261 7.32528Z" />
     </svg>
   );
-  private iconDisplay: VNode[] = [];
+  private iconInactive = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="13"
+      viewBox="0 0 12 13"
+      fill="none"
+      class="star-inactive"
+    >
+      <path d="M2.12261 7.32528L0.386144 5.64869C-0.196726 5.08592 0.0965754 4.09991 0.892272 3.94721L3.52262 3.44244C3.84159 3.38123 4.11096 3.16892 4.24501 2.87309L5.08915 1.01016C5.44396 0.227133 6.55604 0.227132 6.91085 1.01016L7.75499 2.87309C7.88904 3.16892 8.15841 3.38123 8.47738 3.44244L11.1077 3.94721C11.9034 4.09991 12.1967 5.08592 11.6139 5.64869L9.87739 7.32528C9.64107 7.55345 9.53188 7.88314 9.58529 8.20726L10.0399 10.966C10.1756 11.79 9.3013 12.4076 8.57008 12.0042L6.48308 10.8527C6.18239 10.6868 5.81761 10.6868 5.51692 10.8527L3.42992 12.0042C2.6987 12.4076 1.82437 11.79 1.96014 10.966L2.41471 8.20726C2.46812 7.88314 2.35893 7.55345 2.12261 7.32528Z" />
+    </svg>
+  );
 
   /**
    * Specifies a unique identifier for the facet.
@@ -108,7 +120,6 @@ export class AtomicRatingFacet
   public initialize() {
     this.searchStatus = buildSearchStatus(this.bindings.engine);
     this.initializeFacet();
-    this.iconDisplay = this.generateIconDisplay();
   }
 
   private initializeFacet() {
@@ -147,14 +158,19 @@ export class AtomicRatingFacet
     return currentValues;
   }
 
-  private generateIconDisplay() {
+  private generateIconDisplay(facetValue: NumericFacetValue) {
     const iconDisplay: VNode[] = [];
     for (let i = 0; i < this.numberOfStars; i++) {
-      iconDisplay.push(this.icon);
+      if (i < facetValue.end) {
+        iconDisplay.push(this.iconActive);
+      } else {
+        iconDisplay.push(this.iconInactive);
+      }
     }
     return iconDisplay;
   }
 
+  //remove this function later
   private formatFacetValue(facetValue: NumericFacetValue) {
     //TODO: add stars here but display value is a string
     //maybe better to pass svg as children/ Vnode
@@ -190,7 +206,7 @@ export class AtomicRatingFacet
             i18n={this.bindings.i18n}
             onClick={onClick}
           >
-            {this.iconDisplay}
+            {this.generateIconDisplay(facetValue)}
           </FacetValueCheckbox>
         );
       case 'link':
@@ -202,7 +218,7 @@ export class AtomicRatingFacet
             i18n={this.bindings.i18n}
             onClick={onClick}
           >
-            {this.iconDisplay}
+            {this.generateIconDisplay(facetValue)}
           </FacetValueLink>
         );
     }
