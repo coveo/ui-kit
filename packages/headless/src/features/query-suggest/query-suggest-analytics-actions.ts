e7c6d22d779cdde02d122f05e497de4d6c4ccfea
Sami <ssayegh@coveo.com>
2021-07-14 16:18:42 -0400

    feat(headless): improve standalone search box experience (#962)
    https://coveord.atlassian.net/browse/KIT-815

--- packages/headless/src/features/query-suggest/query-suggest-analytics-actions.ts
+++ packages/headless/src/features/query-suggest/query-suggest-analytics-actions.ts
@@ -1,4 +1,5 @@
 import {OmniboxSuggestionsMetadata} from 'coveo.analytics/src/searchPage/searchPageEvents';
+import {SearchAppState} from '../../state/search-app-state';
 import {AnalyticsType, makeAnalyticsAction} from '../analytics/analytics-utils';
 
 export interface LogQuerySuggestionClickActionCreatorPayload {
@@ -20,23 +21,35 @@ export const logQuerySuggestionClick = (
     'analytics/querySuggest',
     AnalyticsType.Search,
     (client, state) => {
-      const {id, suggestion} = payload;
-      const querySuggest = state.querySuggest && state.querySuggest[id];
-      if (querySuggest !== null && querySuggest !== undefined) {
-        const suggestions = querySuggest.completions.map(
-          (completion) => completion.expression
-        );
-
-        const payload: OmniboxSuggestionsMetadata = {
-          suggestionRanking: suggestions.indexOf(suggestion),
-          partialQuery:
-            querySuggest.partialQueries[querySuggest.partialQueries.length - 1],
-          partialQueries: querySuggest.partialQueries,
-          suggestions,
-        };
-        return client.logOmniboxAnalytics(payload);
-      }
-
-      return;
+      const metadata = buildOmniboxSuggestionMetadata(state, payload);
+      return client.logOmniboxAnalytics(metadata);
     }
   )();
+
+export type OmniboxSuggestionMetadata = OmniboxSuggestionsMetadata;
+
+export function buildOmniboxSuggestionMetadata(
+  state: Partial<SearchAppState>,
+  payload: LogQuerySuggestionClickActionCreatorPayload
+): OmniboxSuggestionMetadata {
+  const {id, suggestion} = payload;
+  const querySuggest = state.querySuggest && state.querySuggest[id];
+
+  if (!querySuggest) {
+    throw new Error(
+      `Unable to determine the query suggest analytics metadata to send because no query suggest with id "${id}" was found. Please check the sent #id.`
+    );
+  }
+
+  const suggestions = querySuggest.completions.map(
+    (completion) => completion.expression
+  );
+
+  return {
+    suggestionRanking: suggestions.indexOf(suggestion),
+    partialQuery:
+      querySuggest.partialQueries[querySuggest.partialQueries.length - 1],
+    partialQueries: querySuggest.partialQueries,
+    suggestions,
+  };
+}
