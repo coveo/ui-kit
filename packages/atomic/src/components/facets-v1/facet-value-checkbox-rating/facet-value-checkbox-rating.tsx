6b7aff881107db5592c5ee74c556f6a82884e892
Nicolas Buisson <nbuisson@coveo.com>
2021-07-15 09:19:24 -0400

    stars on facet-value-checkbox-rating
    https://coveord.atlassian.net/browse/KIT-812

--- packages/atomic/src/components/facets-v1/facet-value-checkbox-rating/facet-value-checkbox-rating.tsx
+++ packages/atomic/src/components/facets-v1/facet-value-checkbox-rating/facet-value-checkbox-rating.tsx
@@ -31,7 +31,7 @@ export const FacetValueCheckboxRating: FunctionalComponent<FacetValueProps> = (
       <label
         htmlFor={id}
         part="value-checkbox-label"
-        class="w-full flex items-baseline pl-2 py-2.5 text-on-background cursor-pointer ellipsed"
+        class="w-full flex items-center pl-2 py-2.5 text-on-background cursor-pointer ellipsed"
       >
         <div class="flex items-center gap-0.5 pt-0.5 pb-0.5" part="value-label">
           {children}
