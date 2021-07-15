22a3fe5234ce6ddfba72cf728487a7c846c8458d
Sami Sayegh <ssayegh@coveo.com>
2021-07-15 17:46:46 -0400

    use react-router
    https://coveord.atlassian.net/browse/KIT-822

--- packages/samples/headless-react/src/App.tsx
+++ packages/samples/headless-react/src/App.tsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import {SearchPage, SearchPageProps} from './pages/SearchPage';
 import {AboutPage} from './pages/AboutPage';
+import {BrowserRouter, Link, Switch, Route} from 'react-router-dom';
 
 export type Page = 'search' | 'about';
 
@@ -12,27 +13,32 @@ function App(props: SearchPageProps) {
   const [state, setState] = useState<AppState>({currentPage: 'search'});
 
   return (
-    <main className="App">
-      <nav>
-        <button
-          disabled={state.currentPage === 'search'}
-          onClick={() => setState({currentPage: 'search'})}
-        >
-          Search
-        </button>
-        <button
-          disabled={state.currentPage === 'about'}
-          onClick={() => setState({currentPage: 'about'})}
-        >
-          About
-        </button>
-      </nav>
-      {state.currentPage === 'search' ? (
-        <SearchPage {...props} />
-      ) : (
-        <AboutPage />
-      )}
-    </main>
+    <BrowserRouter>
+      <main className="App">
+        <nav>
+          <button
+            disabled={state.currentPage === 'search'}
+            onClick={() => setState({currentPage: 'search'})}
+          >
+            <Link to="/search">Search</Link>
+          </button>
+          <button
+            disabled={state.currentPage === 'about'}
+            onClick={() => setState({currentPage: 'about'})}
+          >
+            <Link to="/about">About</Link>
+          </button>
+        </nav>
+        <Switch>
+          <Route path="/search">
+            <SearchPage {...props} />
+          </Route>
+          <Route path="/about">
+            <AboutPage />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
