04a6bbab8921d02de5b16a9a7c58a183004475c7
Nicolas Buisson <nbuisson@coveo.com>
2021-07-15 16:26:07 -0400

    use scaleFactor for star display
    https://coveord.atlassian.net/browse/KIT-812

--- packages/atomic/src/components/facets-v1/atomic-rating-facet/atomic-rating-facet.tsx
+++ packages/atomic/src/components/facets-v1/atomic-rating-facet/atomic-rating-facet.tsx
@@ -93,6 +93,7 @@ export class AtomicRatingFacet
       <path d="M2.12261 7.32528L0.386144 5.64869C-0.196726 5.08592 0.0965754 4.09991 0.892272 3.94721L3.52262 3.44244C3.84159 3.38123 4.11096 3.16892 4.24501 2.87309L5.08915 1.01016C5.44396 0.227133 6.55604 0.227132 6.91085 1.01016L7.75499 2.87309C7.88904 3.16892 8.15841 3.38123 8.47738 3.44244L11.1077 3.94721C11.9034 4.09991 12.1967 5.08592 11.6139 5.64869L9.87739 7.32528C9.64107 7.55345 9.53188 7.88314 9.58529 8.20726L10.0399 10.966C10.1756 11.79 9.3013 12.4076 8.57008 12.0042L6.48308 10.8527C6.18239 10.6868 5.81761 10.6868 5.51692 10.8527L3.42992 12.0042C2.6987 12.4076 1.82437 11.79 1.96014 10.966L2.41471 8.20726C2.46812 7.88314 2.35893 7.55345 2.12261 7.32528Z" />
     </svg>
   );
+  private scaleFactor = 1;
 
   /**
    * Specifies a unique identifier for the facet.
@@ -126,6 +127,7 @@ export class AtomicRatingFacet
   }
 
   private initializeFacet() {
+    this.scaleFactor = this.maxValueInIndex / this.numberOfStars;
     const options: NumericFacetOptions = {
       facetId: this.facetId,
       field: this.field,
@@ -148,13 +150,12 @@ export class AtomicRatingFacet
   }
 
   private generateCurrentValues() {
-    const scaleFactor = this.maxValueInIndex / this.numberOfStars;
     const currentValues: NumericRangeRequest[] = [];
     for (let i = 0; i < this.numberOfStars; i++) {
       currentValues.push(
         buildNumericRange({
-          start: Math.round(i * scaleFactor * 100) / 100,
-          end: Math.round((i + 1) * scaleFactor * 100) / 100,
+          start: Math.round(i * this.scaleFactor * 100) / 100,
+          end: Math.round((i + 1) * this.scaleFactor * 100) / 100,
           endInclusive: true,
         })
       );
@@ -165,7 +166,7 @@ export class AtomicRatingFacet
   private generateIconDisplay(facetValue: NumericFacetValue) {
     const iconDisplay: VNode[] = [];
     for (let i = 0; i < this.numberOfStars; i++) {
-      if (i < facetValue.end) {
+      if (i < facetValue.end * (1 / this.scaleFactor)) {
         iconDisplay.push(this.iconActive);
       } else {
         iconDisplay.push(this.iconInactive);
