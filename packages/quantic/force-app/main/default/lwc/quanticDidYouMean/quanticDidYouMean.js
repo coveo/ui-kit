677bc4dd007545917f250e45d60160835a796358
nlafrance-coveo <nlafrance@coveo.com>
2021-07-19 18:29:44 -0400

    feat(quantic): localized didyoumean

--- packages/quantic/force-app/main/default/lwc/quanticDidYouMean/quanticDidYouMean.js
+++ packages/quantic/force-app/main/default/lwc/quanticDidYouMean/quanticDidYouMean.js
@@ -1,54 +1,73 @@
 import { api, LightningElement, track } from 'lwc';
 import { initializeWithHeadless, registerComponentForInit } from 'c/quanticHeadlessLoader';
+import {I18nService} from 'c/quanticUtils';
+
+import didYouMean from '@salesforce/label/c.quantic_DidYouMean';
+import noResultsFor from '@salesforce/label/c.quantic_NoResultsFor';
+import queryCorrectedTo from '@salesforce/label/c.quantic_QueryCorrectedTo';
 
 export default class QuanticDidYouMean extends LightningElement {
-    /** @type {()=> void} */
-    unsubscribe;
-    /** @type {Boolean}*/
-    @track hasQueryCorrection;
-    /** @type {Boolean}*/
-    @track wasAutomaticallyCorrected;
-    /** @type {string}*/
-    @track originalQuery;
-    /** @type {string}*/
-    @track correctedQuery;
-    /** @type {string}*/
-    @api engineId;
+  /** @type {()=> void} */
+  unsubscribe;
+  /** @type {Boolean}*/
+  @track hasQueryCorrection;
+  /** @type {Boolean}*/
+  @track wasAutomaticallyCorrected;
+  /** @type {string}*/
+  @track originalQuery;
+  /** @type {string}*/
+  @track correctedQuery;
+  /** @type {string}*/
+  @api engineId;
 
-    /** @type {import("coveo").DidYouMean} */
-    didYouMean;
+  /** @type {import("coveo").DidYouMean} */
+  didYouMean;
 
-    connectedCallback() {
-        registerComponentForInit(this, this.engineId);
-    }
-    
-    renderedCallback() {
-        initializeWithHeadless(this, this.engineId, this.initialize.bind(this));
-    }
+  label = {
+    didYouMean,
+    noResultsFor,
+    queryCorrectedTo,
+  }
 
-    /**
-     * @param {import("coveo").SearchEngine} engine
-     */
-    @api
-    initialize(engine) {
-        this.didYouMean = CoveoHeadless.buildDidYouMean(engine);
-        this.unsubscribe = this.didYouMean.subscribe(() => this.updateState());
-    }
+  connectedCallback() {
+    registerComponentForInit(this, this.engineId);
+  }
+  
+  renderedCallback() {
+    initializeWithHeadless(this, this.engineId, this.initialize.bind(this));
+  }
 
-    disconnectedCallback() {
-        if (this.unsubscribe) {
-          this.unsubscribe();
-        }
-    }
+  /**
+   * @param {import("coveo").SearchEngine} engine
+   */
+  @api
+  initialize(engine) {
+    this.didYouMean = CoveoHeadless.buildDidYouMean(engine);
+    this.unsubscribe = this.didYouMean.subscribe(() => this.updateState());
+  }
 
-    updateState() {
-        this.hasQueryCorrection = this.didYouMean.state.hasQueryCorrection;
-        this.wasAutomaticallyCorrected = this.didYouMean.state.wasAutomaticallyCorrected;
-        this.originalQuery = this.didYouMean.state.originalQuery;
-        this.correctedQuery = this.didYouMean.state.queryCorrection.correctedQuery;
+  disconnectedCallback() {
+    if (this.unsubscribe) {
+      this.unsubscribe();
     }
+  }
 
-    applyCorrection() {
-        this.didYouMean.applyCorrection();
-    }
+  updateState() {
+    this.hasQueryCorrection = this.didYouMean.state.hasQueryCorrection;
+    this.wasAutomaticallyCorrected = this.didYouMean.state.wasAutomaticallyCorrected;
+    this.originalQuery = this.didYouMean.state.originalQuery;
+    this.correctedQuery = this.didYouMean.state.queryCorrection.correctedQuery;
+  }
+
+  applyCorrection() {
+    this.didYouMean.applyCorrection();
+  }
+
+  get noResultsLabel() {
+    return I18nService.format(this.label.noResultsFor, I18nService.getTextBold(this.originalQuery));
+  }
+
+  get correctedQueryLabel() {
+    return I18nService.format(this.label.queryCorrectedTo, I18nService.getTextBold(this.correctedQuery));
+  }
 }
