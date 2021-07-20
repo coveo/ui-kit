97f5efc0e3d7e7b8f2dde45d69dace47bf0c4f27
fbeaupreCoveo <fbeaupre@coveo.com>
2021-07-20 12:56:35 -0400

    feat(quantic): used pre-built component with hidden label

--- packages/quantic/force-app/main/default/lwc/quanticSort/quanticSort.js
+++ packages/quantic/force-app/main/default/lwc/quanticSort/quanticSort.js
@@ -11,22 +11,6 @@ export default class QuanticSort extends LightningElement {
   sort;
   /** @type {import("coveo").Unsubscribe} */
   unsubscribe;
-  /** @type {HTMLElement} */
-  combobox;
-  /** @type {HTMLElement} */
-  relevancyOption;
-  /** @type {HTMLElement} */
-  newestOption;
-  /** @type {HTMLElement} */
-  oldestOption;
-  /** @type {HTMLElement} */
-  checkIcon;
-  /** @type {HTMLElement} */
-  sortIcon;
-  /** @type {HTMLElement} */
-  selectedOption;
-  /** @type {string} */
-  sortMethod;
 
   connectedCallback() {
     registerComponentForInit(this, this.engineId);
@@ -34,34 +18,6 @@ export default class QuanticSort extends LightningElement {
 
   renderedCallback() {
     initializeWithHeadless(this, this.engineId, this.initialize.bind(this));
-    if(!this.combobox){
-      this.combobox = this.template.querySelector('.slds-dropdown-trigger_click');
-    }
-    if(!this.relevancyOption){
-      this.relevancyOption = this.template.querySelector('.sort__option_relevancy');
-    }
-    if(!this.newestOption){
-      this.newestOption = this.template.querySelector('.sort__option_newest');
-    }
-    if(!this.oldestOption){
-      this.oldestOption = this.template.querySelector('.sort__option_oldest');
-    }
-    if(!this.sortIcon){
-      this.sortIcon = this.template.querySelector('.combobox__sort-icon-container');
-    }
-    if(!this.checkIcon){
-      this.checkIcon = this.template.querySelector('.combobox__check-icon-container');
-    }
-    if(!this.selectedOption){
-      this.selectedOption = this.relevancyOption;
-      this.selectedOption.classList.add('slds-is-selected');
-      this.relevancyOption.children[0].children[0].classList.remove('slds-hidden');
-      this.relevancyOption.setAttribute("aria-checked", "true");
-      this.relevancyOption.setAttribute("aria-selected", "true");
-    }
-    if(!this.sortMethod){
-      this.sortMethod = 'Sort By';
-    }
   }
 
   /**
@@ -78,61 +34,27 @@ export default class QuanticSort extends LightningElement {
   }
 
   /**
-   * @param {MouseEvent} event
+   * @param {CustomEvent<{value: string}>} e
    */
-  handleSelection(event){
-    event.preventDefault();
-    const selected = event.target.innerText;
-    
-    this.selectedOption.classList.remove('slds-is-selected');
-    this.selectedOption.setAttribute("aria-checked", "false");
-    this.selectedOption.setAttribute("aria-selected", "false");
-    this.newestOption.children[0].children[0].classList.add('slds-hidden');
-    this.relevancyOption.children[0].children[0].classList.add('slds-hidden');
-    this.oldestOption.children[0].children[0].classList.add('slds-hidden');
-    
-    switch(selected){
-      case 'Relevancy':
-        this.sortMethod = 'Relevancy';
-        this.selectedOption = this.relevancyOption;
+  handleChange(e) {
+    const selected = e.detail.value;
+
+    switch (selected) {
+      case 'relevancy':
         this.sort.sortBy(this.relevance);
         break;
-      case 'Newest':
-        this.sortMethod = 'Newest';
-        this.selectedOption = this.newestOption;
+
+      case 'newest':
         this.sort.sortBy(this.dateDescending);
         break;
-      case 'Oldest':
-        this.sortMethod = 'Oldest';
-        this.selectedOption = this.oldestOption;
+
+      case 'oldest':
         this.sort.sortBy(this.dateAscending);
         break;
+
       default:
         break;
     }
-    this.selectedOption.classList.add('slds-is-selected');
-    this.selectedOption.setAttribute("aria-checked", "true");
-    this.selectedOption.setAttribute("aria-selected", "true");
-    this.selectedOption.children[0].children[0].classList.remove('slds-hidden');
-    this.closeListbox();
-  }
-
-  onMouseOver(){
-    this.checkIcon.classList.add('combobox__blue-icon', 'slds-current-color');
-    this.sortIcon.classList.add('combobox__blue-icon', 'slds-current-color');
-  }
-
-  onMouseOut(){
-    this.checkIcon.classList.remove('combobox__blue-icon', 'slds-current-color');
-    this.sortIcon.classList.remove('combobox__blue-icon', 'slds-current-color');
-  }
-
-  openListbox(){
-    this.combobox.classList.add('slds-is-open');
-  }
-
-  closeListbox(){
-    this.combobox.classList.remove('slds-is-open');
   }
 
   get relevance() {
