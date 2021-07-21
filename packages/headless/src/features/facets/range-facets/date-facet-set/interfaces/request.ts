5a008a3a444f39a9021815d5eea2159bf85a32d7
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-07-21 17:13:11 -0400

    add API date facet values mappings
    https://coveord.atlassian.net/browse/KIT-819

--- packages/headless/src/features/facets/range-facets/date-facet-set/interfaces/request.ts
+++ packages/headless/src/features/facets/range-facets/date-facet-set/interfaces/request.ts
@@ -1,6 +1,7 @@
 import {BaseRangeFacetRequest} from '../../generic/interfaces/request';
 import {CurrentValues, Type} from '../../../facet-api/request';
 import {FacetValueState} from '../../../facet-api/value';
+import {AnyFacetRequest} from '../../../generic/interfaces/generic-facet-request';
 
 /**
  * The options defining a value to display in a `DateFacet`.
@@ -25,6 +26,12 @@ export interface DateRangeRequest {
    * The current facet value state.
    */
   state: FacetValueState;
+}
+
+export function isDateFacetRequest(
+  request: AnyFacetRequest
+): request is DateFacetRequest {
+  return request.type === 'dateRange';
 }
 
 export interface DateFacetRequest
---
+++ packages/headless/src/features/search/search-mappings.ts
@@ -0,0 +1,137 @@
+import {
+  formatRelativeDateForSearchApi,
+  isRelativeDateFormat,
+} from '../../api/search/date/relative-date';
+import {SearchAPIClientResponse} from '../../api/search/search-api-client';
+import {SearchRequest} from '../../api/search/search/search-request';
+import {SearchResponseSuccess} from '../../api/search/search/search-response';
+import {AnyFacetRequest} from '../facets/generic/interfaces/generic-facet-request';
+import {AnyFacetResponse} from '../facets/generic/interfaces/generic-facet-response';
+import {
+  DateRangeRequest,
+  isDateFacetRequest,
+} from '../facets/range-facets/date-facet-set/interfaces/request';
+import {
+  DateFacetResponse,
+  DateFacetValue,
+} from '../facets/range-facets/date-facet-set/interfaces/response';
+
+function formatStartFacetValue(value: string) {
+  return `start${value}`;
+}
+
+function formatEndFacetValue(value: string) {
+  return `start${value}`;
+}
+
+interface SearchMappings {
+  dateFacetValueMap: Record<string, Record<string, string>>;
+}
+
+const initialSearchMappings: () => SearchMappings = () => ({
+  dateFacetValueMap: {},
+});
+
+function mapDateRangeRequest(
+  value: DateRangeRequest,
+  facetId: string,
+  mappings: SearchMappings
+) {
+  let start = value.start;
+  let end = value.end;
+  if (isRelativeDateFormat(start)) {
+    start = formatRelativeDateForSearchApi(start);
+    mappings.dateFacetValueMap[facetId][formatStartFacetValue(start)] =
+      value.start;
+  }
+  if (isRelativeDateFormat(end)) {
+    end = formatRelativeDateForSearchApi(end);
+    mappings.dateFacetValueMap[facetId][formatEndFacetValue(end)] = value.end;
+  }
+
+  return {...value, start, end};
+}
+
+function mapFacetRequest(
+  facetRequest: AnyFacetRequest,
+  mappings: SearchMappings
+) {
+  if (isDateFacetRequest(facetRequest)) {
+    const {facetId, currentValues} = facetRequest;
+    mappings.dateFacetValueMap[facetId] = {};
+    return {
+      ...facetRequest,
+      currentValues: currentValues.map((value) =>
+        mapDateRangeRequest(value, facetId, mappings)
+      ),
+    };
+  }
+
+  return facetRequest;
+}
+
+export function mapSearchRequest(searchRequest: SearchRequest) {
+  const mappings = initialSearchMappings();
+  const request: SearchRequest = {
+    ...searchRequest,
+    facets: searchRequest.facets?.map((facetRequest) =>
+      mapFacetRequest(facetRequest, mappings)
+    ),
+  };
+  return {request, mappings};
+}
+
+function mapDateRangeResponse(
+  value: DateFacetValue,
+  facetId: string,
+  mappings: SearchMappings
+) {
+  return {
+    ...value,
+    start:
+      mappings.dateFacetValueMap[facetId][formatStartFacetValue(value.start)] ||
+      value.start,
+    end:
+      mappings.dateFacetValueMap[facetId][formatEndFacetValue(value.end)] ||
+      value.end,
+  };
+}
+
+function isDateFacetResponse(
+  facetResponse: AnyFacetResponse,
+  mappings: SearchMappings
+): facetResponse is DateFacetResponse {
+  return facetResponse.facetId in mappings.dateFacetValueMap;
+}
+
+function mapFacetResponse(
+  facetResponse: AnyFacetResponse,
+  mappings: SearchMappings
+) {
+  if (isDateFacetResponse(facetResponse, mappings)) {
+    return {
+      ...facetResponse,
+      values: facetResponse.values.map((value) =>
+        mapDateRangeResponse(value, facetResponse.facetId, mappings)
+      ),
+    };
+  }
+  return facetResponse;
+}
+
+export function mapSearchResponse(
+  response: SearchAPIClientResponse<SearchResponseSuccess>,
+  mappings: SearchMappings
+) {
+  if ('success' in response) {
+    const success: SearchResponseSuccess = {
+      ...response.success,
+      facets: response.success.facets?.map((facetResponse) =>
+        mapFacetResponse(facetResponse, mappings)
+      ),
+    };
+    return {success};
+  }
+
+  return response;
+}
