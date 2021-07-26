4d0417b4b917b3f333f1b8beee6d7b2c8379d266
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-07-26 08:15:12 -0400

    update importe name
    https://coveord.atlassian.net/browse/KIT-804

--- packages/atomic/src/components/facets-v1/atomic-timeframe-facet/atomic-timeframe-facet.tsx
+++ packages/atomic/src/components/facets-v1/atomic-timeframe-facet/atomic-timeframe-facet.tsx
@@ -10,7 +10,7 @@ import {
   DateFacetValue,
   DateRangeRequest,
   buildDateRange,
-  parseRelativeDate,
+  deserializeRelativeDate,
 } from '@coveo/headless';
 import {
   Bindings,
@@ -142,7 +142,7 @@ export class AtomicTimeframeFacet
 
   private formatFacetValue(facetValue: DateFacetValue) {
     try {
-      const relativeDate = parseRelativeDate(facetValue.start);
+      const relativeDate = deserializeRelativeDate(facetValue.start);
       const timeframe = this.getManualTimeframes().find(
         (timeframe) =>
           timeframe.period === relativeDate.period &&
