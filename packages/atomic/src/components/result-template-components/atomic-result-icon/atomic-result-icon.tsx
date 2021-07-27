57454d921ae6281ac49c3e6109c988f545a31777
Benjamin Taillon <btaillon@coveo.com>
2021-07-27 15:36:48 -0500

    updated direct link feature
    https://coveord.atlassian.net/browse/KIT-841

--- packages/atomic/src/components/result-template-components/atomic-result-icon/atomic-result-icon.tsx
+++ packages/atomic/src/components/result-template-components/atomic-result-icon/atomic-result-icon.tsx
@@ -20,7 +20,7 @@ export class AtomicResultIcon {
   @Element() host!: HTMLElement;
 
   /**
-   * Specifies the icon to display from the list of available icons.
+   * Specifies the icon to display, either from the list of available icons or a direct link.
    *
    * By default, this will parse the `objecttype` and `filetype` fields to find a matching icon. If none are available, it will use the `custom` icon.
    */
@@ -43,15 +43,13 @@ export class AtomicResultIcon {
     return objectType || fileType || 'custom';
   }
 
-  private get isCDNIcon() {
-    return !!this.icon?.match(/^https?:\/\//);
+  private get isPath() {
+    return !!this.icon?.match(/^(https?:\/\/|\.\/|\.\.\/)/);
   }
 
   public async componentWillLoad() {
     const icon = this.icon || this.defaultIcon;
-    const iconPath = this.isCDNIcon
-      ? icon
-      : getAssetPath(`./assets/${icon}.svg`);
+    const iconPath = this.isPath ? icon : getAssetPath(`./assets/${icon}.svg`);
     const response = await fetch(iconPath);
     this.svg =
       response.status === 200 || response.status === 304
@@ -62,7 +60,7 @@ export class AtomicResultIcon {
   public render() {
     return (
       <Host
-        class={!this.isCDNIcon ? this.icon || this.defaultIcon : ''}
+        class={!this.isPath ? this.icon || this.defaultIcon : ''}
         innerHTML={this.svg}
       ></Host>
     );
