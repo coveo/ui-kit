19ea4ce00fae4d2d112af02ae8885f28cd15ada9
Asma Elfaleh <aelfaleh@coveo.com>
2021-07-28 13:50:22 -0400

    SFINT-3983: Fix build

--- packages/quantic/force-app/main/default/lwc/quanticSvg/quanticSvg.js
+++ packages/quantic/force-app/main/default/lwc/quanticSvg/quanticSvg.js
@@ -1,6 +1,9 @@
 import { LightningElement, api } from 'lwc';
+// @ts-ignore
 import pageNotFound from './pageNotFound.html';
+// @ts-ignore
 import desert from './desert.html';
+// @ts-ignore
 import quanticSvg from './quanticSvg.html';
 
 export default class QuanticSvgLoader extends LightningElement {
