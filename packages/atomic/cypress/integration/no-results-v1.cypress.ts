281741ea350b5c6420d766a3cc863e400a392603
Nicolas Buisson <nbuisson@coveo.com>
2021-08-06 13:53:14 -0400

    no-results-v1 cypress test
    https://coveord.atlassian.net/browse/KIT-887

---
+++ packages/atomic/cypress/integration/no-results-v1.cypress.ts
@@ -0,0 +1,54 @@
+import {
+  buildTestUrl,
+  injectComponent,
+  setUpPage,
+} from '../utils/setupComponent';
+import {generateAliasForSearchBox} from './search-box-selectors';
+
+describe('No Results Test Suites', () => {
+  const tag = 'atomic-no-results-v1';
+  const component = (attributes = '') => `<${tag} ${attributes}></${tag}>`;
+  const searchBox = '<atomic-search-box></atomic-search-box>';
+  const wait = 1000;
+
+  it('should not be visible when there are results', () => {
+    setUpPage(component());
+    cy.get('atomic-no-results-v1').should('not.be.visible');
+  });
+
+  it('should be visible when there are no results', () => {
+    cy.visit(buildTestUrl('q=gahaiusdhgaiuewjfsf'));
+    injectComponent(component() + searchBox);
+    cy.wait(wait);
+    cy.get('atomic-no-results-v1').should('be.visible');
+  });
+
+  it('cancel button should not be visible when there is no history', () => {
+    cy.visit(buildTestUrl('q=gahaiusdhgaiuewjfsf'));
+    injectComponent(component());
+    cy.wait(wait);
+    cy.get('atomic-no-results-v1').shadow().get('button').should('not.exist');
+  });
+
+  function submitNoResultsSearch() {
+    setUpPage(component() + searchBox);
+    generateAliasForSearchBox();
+    cy.get('@searchBoxFirstDiv')
+      .find('.search-input')
+      .type('asiufasfgasiufhsaiufgsa');
+    cy.get('@searchBoxFirstDiv').find('.submit-button').click();
+    cy.wait(wait);
+  }
+
+  it('cancel button should be visible when there is history', () => {
+    submitNoResultsSearch();
+    cy.get('atomic-no-results-v1').shadow().find('button').should('be.visible');
+  });
+
+  it('clicking on cancel should go back in history and hide the atomic-no-results component', () => {
+    submitNoResultsSearch();
+    cy.get('atomic-no-results-v1').shadow().find('button').click();
+    cy.wait(wait);
+    cy.get('atomic-no-results-v1').should('not.be.visible');
+  });
+});
