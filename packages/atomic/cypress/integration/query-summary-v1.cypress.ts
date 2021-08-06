c342757408a6865deb63134a559e884acc0c3386
Nicolas Buisson <nbuisson@coveo.com>
2021-08-06 11:10:20 -0400

    cypress test
    https://coveord.atlassian.net/browse/KIT-848

---
+++ packages/atomic/cypress/integration/query-summary-v1.cypress.ts
@@ -0,0 +1,66 @@
+import {
+  buildTestUrl,
+  injectComponent,
+  setUpPage,
+} from '../utils/setupComponent';
+
+describe('Query Summary Test Suites', () => {
+  const tag = 'atomic-query-summary-v1';
+  const searchBox = '<atomic-search-box></atomic-search-box>';
+  const component = (attributes = '') => `<${tag} ${attributes}></${tag}>`;
+  const wait = 1000;
+
+  function contentShouldMatch(content: RegExp | string) {
+    cy.get(tag).shadow().find('div[part="container"]').contains(content);
+  }
+
+  it('should be visible', () => {
+    setUpPage(component());
+    cy.get(tag).should('be.visible');
+  });
+
+  describe('should match text content', () => {
+    it('with a query yielding multiple results', () => {
+      cy.visit(buildTestUrl('q=test'));
+      injectComponent(component() + searchBox);
+      cy.wait(wait);
+      contentShouldMatch(/^Results 1-10 of [\d,]+ for test$/);
+    });
+
+    it('with a query yielding a single result', () => {
+      cy.visit(
+        buildTestUrl(
+          "q=Queen's%20Gambit%20sparks%20world%20of%20online%20chess%20celebrities"
+        )
+      );
+      injectComponent(component() + searchBox);
+      cy.wait(wait);
+      contentShouldMatch(
+        /^Result 1 of [\d,]+ for Queen's Gambit sparks world of online chess celebrities$/
+      );
+    });
+
+    it('with a query yielding no results', () => {
+      cy.visit(buildTestUrl('q=gahaiusdhgaiuewjfsf'));
+      injectComponent(component() + searchBox);
+      cy.wait(wait);
+      contentShouldMatch(/^We couldn't find anything for gahaiusdhgaiuewjfsf$/);
+    });
+
+    it('with no query yielding no results', () => {
+      cy.visit(buildTestUrl('numberOfResults=0'));
+      injectComponent(component());
+      cy.wait(wait);
+      contentShouldMatch(/^No results$/);
+    });
+
+    it('with a query containing an XSS injection', () => {
+      cy.visit(buildTestUrl('q=<script>alert("hello");</script>'));
+      injectComponent(component() + searchBox);
+      cy.wait(wait);
+      contentShouldMatch(
+        'We couldn\'t find anything for <script>alert("hello");</script>'
+      );
+    });
+  });
+});
