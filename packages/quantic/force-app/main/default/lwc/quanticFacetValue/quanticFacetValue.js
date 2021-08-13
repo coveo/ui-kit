595f92013cd65497e19b2f880b2b34fb415cdfc5
fbeaupreCoveo <fbeaupre@coveo.com>
2021-08-13 11:17:52 -0400

    feat(quantic): merge facet value components into one

--- packages/quantic/force-app/main/default/lwc/quanticFacetValue/quanticFacetValue.js
+++ packages/quantic/force-app/main/default/lwc/quanticFacetValue/quanticFacetValue.js
@@ -1,10 +1,46 @@
 import {LightningElement, api} from 'lwc';
+import LOCALE from '@salesforce/i18n/locale';
 
 export default class QuanticFacetValue extends LightningElement {
-  /** @type {import("coveo").FacetValue} */
   @api item;
   /** @type {boolean} */
   @api isChecked;
+  /** @type {string} */
+  @api variant;
+  /** @type {string} */
+  @api start;
+  /** @type {string} */
+  @api end;
+
+  Variants = {
+    NumericFacet: 'numeric',
+    DateFacet: 'date',
+  };
+
+  connectedCallback() {
+    if (this.isDateFacet) {
+      // eslint-disable-next-line  @lwc/lwc/no-api-reassignments
+      this.start = new Intl.DateTimeFormat(LOCALE).format(
+        new Date(this.item.start)
+      );
+      // eslint-disable-next-line  @lwc/lwc/no-api-reassignments
+      this.end = new Intl.DateTimeFormat(LOCALE).format(
+        new Date(this.item.end)
+      );
+    }
+  }
+
+  get isDefaultFacet() {
+    return !this.variant;
+  }
+
+  get isNumericFacet() {
+    return this.variant === this.Variants.NumericFacet;
+  }
+
+  get isDateFacet() {
+    return this.variant === this.Variants.DateFacet;
+  }
 
   /**
    * @param {InputEvent} evt
