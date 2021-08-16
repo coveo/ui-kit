ca26065f3c57e1b7d0b88d70d465920e4ced1516
nlafrance-coveo <nlafrance@coveo.com>
2021-08-16 17:11:06 -0400

    feat(quantic): recent queries slice tests

---
+++ packages/headless/src/features/recent-queries/recent-queries-slice.test.ts
@@ -0,0 +1,66 @@
+import {buildMockSearch} from '../../test/mock-search';
+import {buildMockSearchResponse} from '../../test/mock-search-response';
+import {logSearchEvent} from '../analytics/analytics-actions';
+import {executeSearch} from '../search/search-actions';
+import {
+  clearRecentQueries,
+  registerRecentQueries,
+} from './recent-queries-actions';
+import {recentQueriesReducer} from './recent-queries-slice';
+import {
+  getRecentQueriesInitialState,
+  RecentQueriesState,
+} from './recent-queries-state';
+
+fdescribe('recent-queries slice', () => {
+  let state: RecentQueriesState;
+
+  const testQuery = 'what is a query';
+  const testQueries = [testQuery];
+  const testMaxQueries = 5;
+
+  beforeEach(() => {
+    state = getRecentQueriesInitialState();
+  });
+
+  it('should have initial state', () => {
+    expect(recentQueriesReducer(undefined, {type: 'foo'})).toEqual(
+      'RecentQueries'
+    );
+  });
+
+  it('#registerRecentQueries should set queries and maxQueries params in state', () => {
+    state = recentQueriesReducer(
+      state,
+      registerRecentQueries({queries: testQueries, maxQueries: testMaxQueries})
+    );
+
+    expect(state.queries).toEqual(testQueries);
+    expect(state.maxQueries).toEqual(testMaxQueries);
+  });
+
+  it('#clearRecentQueries should remove all queries from the queue in state', () => {
+    state.queries = testQueries;
+
+    state = recentQueriesReducer(state, clearRecentQueries());
+
+    expect(state.queries).toEqual([]);
+  });
+
+  it('should set new recent query on search fulfilled', () => {
+    const searchAction = executeSearch.fulfilled(
+      buildMockSearch({
+        queryExecuted: testQuery,
+        response: buildMockSearchResponse({
+          queryCorrections: [{correctedQuery: 'foo', wordCorrections: []}],
+        }),
+      }),
+      '',
+      logSearchEvent({evt: 'foo'})
+    );
+
+    expect(recentQueriesReducer(state, searchAction).queries).toEqual(
+      testQueries
+    );
+  });
+});
