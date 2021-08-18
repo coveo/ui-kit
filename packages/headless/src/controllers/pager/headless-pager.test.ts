672e8af4da204a3d4b38447a1e559c4189d53ec1
Sami <ssayegh@coveo.com>
2021-08-18 11:10:39 -0400

    chore: extract core pager logic (#1101)
    https://coveord.atlassian.net/browse/KIT-909

--- packages/headless/src/controllers/pager/headless-pager.test.ts
+++ packages/headless/src/controllers/pager/headless-pager.test.ts
@@ -8,25 +8,13 @@ import {
   buildMockSearchAppEngine,
   MockSearchEngine,
 } from '../../test/mock-engine';
-import {
-  updatePage,
-  registerPage,
-  nextPage,
-  previousPage,
-} from '../../features/pagination/pagination-actions';
 import {executeSearch} from '../../features/search/search-actions';
-import {pagination, configuration} from '../../app/reducers';
 
 describe('Pager', () => {
   let engine: MockSearchEngine;
   let options: PagerOptions;
   let initialState: PagerInitialState;
   let pager: Pager;
-
-  function setCurrentPage(page: number) {
-    const {numberOfResults} = engine.state.pagination;
-    engine.state.pagination.firstResult = (page - 1) * numberOfResults;
-  }
 
   function setMaxPage(page: number) {
     const {numberOfResults} = engine.state.pagination;
@@ -48,138 +36,26 @@ describe('Pager', () => {
     expect(pager).toBeTruthy();
   });
 
-  it('it adds the correct reducers to engine', () => {
-    expect(engine.addReducers).toHaveBeenCalledWith({
-      pagination,
-      configuration,
-    });
-  });
-
-  it('exposes a #subscribe method', () => {
-    expect(pager.subscribe).toBeTruthy();
-  });
-
-  it('when initialState #isActive is an invalid value, it throws an error', () => {
-    initialState.page = ('1' as unknown) as number;
-    expect(() => initPager()).toThrow('Check the initialState of buildPager');
-  });
-
-  it('when options #expression is an invalid value, it throws an error', () => {
-    options.numberOfPages = ('1' as unknown) as number;
-    expect(() => initPager()).toThrow('Check the options of buildPager');
-  });
-
   it('#state.currentPages returns 5 pages by default', () => {
     setMaxPage(10);
     expect(pager.state.currentPages.length).toBe(5);
   });
 
-  it('when numberOfPages is 2, #state.currentPages returns two page numbers', () => {
-    options.numberOfPages = 2;
-    setMaxPage(10);
-    initPager();
-
-    expect(pager.state.currentPages.length).toBe(2);
-  });
-
-  it('when numberOfPages is -1, the pager fails to initialize', () => {
-    options.numberOfPages = -1;
-    expect(() => initPager()).toThrow();
-  });
-
-  it('when #initialState.page is defined, it registers the page', () => {
-    initialState.page = 2;
-    initPager();
-
-    expect(engine.actions).toContainEqual(registerPage(2));
-  });
-
-  it('#selectPage dispatches #updatePage with the passed page', () => {
-    pager.selectPage(2);
-    expect(engine.actions).toContainEqual(updatePage(2));
-  });
-
   it('#selectPage dispatches #executeSearch', () => {
     pager.selectPage(2);
-    const action = engine.actions.find(
-      (a) => a.type === executeSearch.pending.type
-    );
+    const action = engine.findAsyncAction(executeSearch.pending);
     expect(action).toBeTruthy();
-  });
-
-  it('#nextPage dispatches a #nextPage action', () => {
-    pager.nextPage();
-    expect(engine.actions).toContainEqual(nextPage());
   });
 
   it('#nextPage dispatches #executeSearch', () => {
     pager.nextPage();
-    const action = engine.actions.find(
-      (a) => a.type === executeSearch.pending.type
-    );
+    const action = engine.findAsyncAction(executeSearch.pending);
     expect(action).toBeTruthy();
-  });
-
-  it('#previousPage dispatches a #previousPage action', () => {
-    pager.previousPage();
-    expect(engine.actions).toContainEqual(previousPage());
   });
 
   it('#previousPage dispatches #executeSearch', () => {
     pager.previousPage();
-    const action = engine.actions.find(
-      (a) => a.type === executeSearch.pending.type
-    );
+    const action = engine.findAsyncAction(executeSearch.pending);
     expect(engine.actions).toContainEqual(action);
-  });
-
-  it('calling #isCurrentPage with a page number not equal to the one in state returns false', () => {
-    setCurrentPage(2);
-    expect(pager.isCurrentPage(1)).toBe(false);
-  });
-
-  it('calling #isCurrentPage with a page number equal to the one in state returns true', () => {
-    setCurrentPage(2);
-    expect(pager.isCurrentPage(2)).toBe(true);
-  });
-
-  it('state exposes a maxPage property', () => {
-    setMaxPage(10);
-    expect(pager.state.maxPage).toBe(10);
-  });
-
-  it('when on page 1 and maxPage is 2, #state.hasNextPage is true', () => {
-    setCurrentPage(1);
-    setMaxPage(2);
-
-    expect(pager.state.hasNextPage).toBe(true);
-  });
-
-  it('when on page 2 and maxPage is 2, #state.hasNextPage returns false', () => {
-    setCurrentPage(2);
-    setMaxPage(2);
-
-    expect(pager.state.hasNextPage).toBe(false);
-  });
-
-  it('when on page 2 and maxPage is 2, #state.hasPreviousPage returns true', () => {
-    setCurrentPage(2);
-    setMaxPage(2);
-
-    expect(pager.state.hasPreviousPage).toBe(true);
-  });
-
-  it('when on page 1 and maxPage is 2, #state.hasPreviousPage returns false', () => {
-    setCurrentPage(1);
-    setMaxPage(2);
-
-    expect(pager.state.hasPreviousPage).toBe(false);
-  });
-
-  it('when on page 2 and maxPage is 0, #state.hasPreviousPage returns false', () => {
-    setCurrentPage(2);
-    setMaxPage(0);
-
-    expect(pager.state.hasPreviousPage).toBe(false);
   });
 });
--- packages/headless/src/controllers/pager/headless-pager.ts
+++ packages/headless/src/controllers/pager/headless-pager.ts
@@ -1,134 +1,20 @@
-import {Schema, NumberValue} from '@coveo/bueno';
-import {buildController, Controller} from '../controller/headless-controller';
-import {
-  updatePage,
-  registerPage,
-  nextPage,
-  previousPage,
-} from '../../features/pagination/pagination-actions';
-import {
-  currentPageSelector,
-  currentPagesSelector,
-  maxPageSelector,
-} from '../../features/pagination/pagination-selectors';
 import {executeSearch} from '../../features/search/search-actions';
-import {minimumPage} from '../../features/pagination/pagination-slice';
 import {
   logPageNumber,
   logPageNext,
   logPagePrevious,
 } from '../../features/pagination/pagination-analytics-actions';
-import {
-  ConfigurationSection,
-  PaginationSection,
-} from '../../state/state-sections';
-import {
-  validateInitialState,
-  validateOptions,
-} from '../../utils/validate-payload';
-import {configuration, pagination} from '../../app/reducers';
-import {loadReducerError} from '../../utils/errors';
 import {SearchEngine} from '../../app/search-engine/search-engine';
+import {
+  buildCorePager,
+  PagerInitialState,
+  PagerOptions,
+  PagerProps,
+  Pager,
+  PagerState,
+} from '../core/pager/headless-core-pager';
 
-export interface PagerInitialState {
-  /**
-   * The initial page number.
-   * */
-  page?: number;
-}
-
-export interface PagerOptions {
-  /**
-   * The number of pages to display in the pager.
-   *
-   * @defaultValue `5`
-   * */
-  numberOfPages?: number;
-}
-
-export interface PagerProps {
-  /**
-   * The options for the `Pager` controller.
-   */
-  options?: PagerOptions;
-  /**
-   * The initial state that should be applied to the `Pager` controller.
-   */
-  initialState?: PagerInitialState;
-}
-
-const optionsSchema = new Schema({
-  numberOfPages: new NumberValue({default: 5, min: 0}),
-});
-
-const initialStateSchema = new Schema({
-  page: new NumberValue({min: 1}),
-});
-
-/**
- * The `Pager` controller allows to navigate through the different result pages.
- */
-export interface Pager extends Controller {
-  /**
-   * Updates the results to those on the passed page.
-   *
-   * @param page - The page number.
-   */
-  selectPage(page: number): void;
-
-  /**
-   * Updates the results to those on the next page.
-   * */
-  nextPage(): void;
-
-  /**
-   * Updates the results to those on the previous page.
-   * */
-  previousPage(): void;
-
-  /**
-   * Returns `true` when the current page is equal to the passed page, and `false` otherwise.
-   *
-   * @param page - The page number to check.
-   * @returns Whether the passed page is selected.
-   */
-  isCurrentPage(page: number): boolean;
-
-  /**
-   * The state of the Pager controller.
-   * */
-  state: PagerState;
-}
-
-/**
- * A scoped and simplified part of the headless state that is relevant to the `Pager` controller.
- */
-export interface PagerState {
-  /**
-   * The active page number.
-   * */
-  currentPage: number;
-
-  /**
-   * The page range to display.
-   * */
-  currentPages: number[];
-
-  /**
-   * The maximum available page.
-   * */
-  maxPage: number;
-
-  /**
-   * Returns `true` when a previous page is available, and `false` otherwise.
-   * */
-  hasPreviousPage: boolean;
-
-  /**
-   * Returns `true` when a next page is available, and `false` otherwise.
-   * */
-  hasNextPage: boolean;
-}
+export {PagerInitialState, PagerOptions, PagerProps, Pager, PagerState};
 
 /**
  * Creates a `Pager` controller instance.
@@ -141,86 +27,29 @@ export function buildPager(
   engine: SearchEngine,
   props: PagerProps = {}
 ): Pager {
-  if (!loadPagerReducers(engine)) {
-    throw loadReducerError;
-  }
-
-  const controller = buildController(engine);
   const {dispatch} = engine;
-
-  const options = validateOptions(
-    engine,
-    optionsSchema,
-    props.options,
-    'buildPager'
-  ) as Required<PagerOptions>;
-  const initialState = validateInitialState(
-    engine,
-    initialStateSchema,
-    props.initialState,
-    'buildPager'
-  );
-  const page = initialState.page;
-
-  if (page) {
-    dispatch(registerPage(page));
-  }
-
-  const getCurrentPage = () => {
-    return currentPageSelector(engine.state);
-  };
-
-  const getCurrentPages = () => {
-    const {numberOfPages} = options;
-    return currentPagesSelector(engine.state, numberOfPages);
-  };
-
-  const getMaxPage = () => {
-    return maxPageSelector(engine.state);
-  };
+  const pager = buildCorePager(engine, props);
 
   return {
-    ...controller,
+    ...pager,
 
     get state() {
-      const currentPage = getCurrentPage();
-      const maxPage = getMaxPage();
-      const hasPreviousPage = currentPage > minimumPage && maxPage > 0;
-      const hasNextPage = currentPage < maxPage;
-
-      return {
-        currentPage,
-        currentPages: getCurrentPages(),
-        maxPage,
-        hasPreviousPage,
-        hasNextPage,
-      };
+      return pager.state;
     },
 
     selectPage(page: number) {
-      dispatch(updatePage(page));
+      pager.selectPage(page);
       dispatch(executeSearch(logPageNumber()));
     },
 
     nextPage() {
-      dispatch(nextPage());
+      pager.nextPage();
       dispatch(executeSearch(logPageNext()));
     },
 
     previousPage() {
-      dispatch(previousPage());
+      pager.previousPage();
       dispatch(executeSearch(logPagePrevious()));
     },
-
-    isCurrentPage(page: number) {
-      return page === this.state.currentPage;
-    },
   };
-}
-
-function loadPagerReducers(
-  engine: SearchEngine
-): engine is SearchEngine<PaginationSection & ConfigurationSection> {
-  engine.addReducers({configuration, pagination});
-  return true;
 }
