1f6b9afe0fae248474adc5adf666527931e657cd
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-08-20 12:10:23 -0400

    extract breadcrumb values
    https://coveord.atlassian.net/browse/KIT-850

--- packages/atomic/src/components/atomic-breadbox/atomic-breadbox.tsx
+++ packages/atomic/src/components/atomic-breadbox/atomic-breadbox.tsx
@@ -15,6 +15,18 @@ import {
 } from '@coveo/headless';
 import {Button} from '../common/button';
 import CloseIcon from 'coveo-styleguide/resources/icons/svg/close.svg';
+import {getFieldValueCaption} from '../../utils/field-utils';
+
+interface Breadcrumb {
+  facetId: string;
+  label: string;
+  formattedValue: string | string[];
+  deselect: () => void;
+}
+
+const CHARACTER_LIMIT = 30;
+const SEPARATOR = '/';
+const ELLIPSIS = '...';
 
 /**
  * The `atomic-breadbox` component creates breadcrumbs that display a summary of the currently active facet values.
@@ -48,19 +60,29 @@ export class AtomicBreadbox implements InitializableComponent {
   }
 
   private limitLength(value: string) {
-    const length = 30;
-    return value.length > length ? `${value.substring(0, length)}...` : value;
+    return value.length > CHARACTER_LIMIT
+      ? `${value.substring(0, CHARACTER_LIMIT)}${ELLIPSIS}`
+      : value;
   }
 
-  private renderBreadcrumb() {
-    const limitedLabel = this.limitLength('Brand');
-    const limitedValue = this.limitLength('Adidas');
+  // TODO: limit path
+  private limitPath(path: string[]) {}
+
+  private renderBreadcrumb(breadcrumb: Breadcrumb) {
+    const limitedLabel = this.limitLength(breadcrumb.label);
+    const fullValue = Array.isArray(breadcrumb.formattedValue)
+      ? breadcrumb.formattedValue.join(SEPARATOR)
+      : breadcrumb.formattedValue;
+    const limitedValue = Array.isArray(breadcrumb.formattedValue)
+      ? this.limitPath(breadcrumb.formattedValue)
+      : this.limitLength(breadcrumb.formattedValue);
+
     return (
       <li>
         <Button
           style="outline-neutral"
           class="py-2 px-3 flex items-center btn-pill"
-          title={`${'Brand'}: ${'Adidas'}`}
+          title={`${breadcrumb.label}: ${fullValue}`}
         >
           <span class="with-colon text-neutral-dark mr-px">{limitedLabel}</span>
           <span>{limitedValue}</span>
@@ -109,10 +131,67 @@ export class AtomicBreadbox implements InitializableComponent {
     );
   }
 
-  private renderBreadcrumbs() {
-    // TODO: loop, sort and cut
+  private get allBreadcrumbs(): Breadcrumb[] {
     return [
-      this.renderBreadcrumb(),
+      ...this.breadcrumbManagerState.facetBreadcrumbs
+        .map(({facetId, field, values}) =>
+          values.map((value) => ({value, facetId, field}))
+        )
+        .flat()
+        .map(({value, facetId, field}) => ({
+          facetId,
+          label: this.bindings.store.state.facets[facetId].label,
+          deselect: value.deselect,
+          formattedValue: getFieldValueCaption(
+            field,
+            value.value.value,
+            this.bindings.i18n
+          ),
+        })),
+      ...this.breadcrumbManagerState.categoryFacetBreadcrumbs.map(
+        ({facetId, field, path, deselect}) => ({
+          facetId,
+          label: this.bindings.store.state.categoryFacets[facetId].label,
+          deselect: deselect,
+          formattedValue: path.map((pathValue) =>
+            getFieldValueCaption(field, pathValue.value, this.bindings.i18n)
+          ),
+        })
+      ),
+      ...this.breadcrumbManagerState.numericFacetBreadcrumbs
+        .map(({facetId, field, values}) =>
+          values.map((value) => ({value, facetId, field}))
+        )
+        .flat()
+        .map(({value, facetId}) => ({
+          facetId,
+          label: this.bindings.store.state.numericFacets[facetId].label,
+          deselect: value.deselect,
+          formattedValue: this.bindings.store.state.numericFacets[
+            facetId
+          ].format(value.value),
+        })),
+      ...this.breadcrumbManagerState.dateFacetBreadcrumbs
+        .map(({facetId, field, values}) =>
+          values.map((value) => ({value, facetId, field}))
+        )
+        .flat()
+        .map(({value, facetId}) => ({
+          facetId,
+          label: this.bindings.store.state.dateFacets[facetId].label,
+          deselect: value.deselect,
+          formattedValue: this.bindings.store.state.dateFacets[facetId].format(
+            value.value
+          ),
+        })),
+    ];
+  }
+
+  private renderBreadcrumbs() {
+    // TODO: sort and cut
+    const sortedBreadcrumbs = this.allBreadcrumbs;
+    return [
+      sortedBreadcrumbs.map((breadcrumb) => this.renderBreadcrumb(breadcrumb)),
       this.renderShowMore(15),
       this.renderShowLess(),
       this.renderClearAll(),
