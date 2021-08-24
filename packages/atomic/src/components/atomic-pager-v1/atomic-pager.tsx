39b5c010deccfa7c8b699f04f4f97dcfc177bd87
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-08-24 09:01:14 -0400

    create files
    https://coveord.atlassian.net/browse/KIT-933

---
+++ packages/atomic/src/components/atomic-pager-v1/atomic-pager.tsx
@@ -0,0 +1,170 @@
+import {Component, h, Prop, State, Event, EventEmitter} from '@stencil/core';
+import {
+  Pager,
+  PagerState,
+  buildPager,
+  buildSearchStatus,
+  SearchStatus,
+  SearchStatusState,
+} from '@coveo/headless';
+import {
+  Bindings,
+  BindStateToController,
+  InitializableComponent,
+  InitializeBindings,
+} from '../../utils/initialization-utils';
+import ArrowRightIcon from 'coveo-styleguide/resources/icons/svg/arrow-right-rounded.svg';
+import ArrowLeftIcon from 'coveo-styleguide/resources/icons/svg/arrow-left-rounded.svg';
+
+/**
+ * The `atomic-pager` provides buttons that allow the end user to navigate through the different result pages.
+ *
+ * @part buttons - The list of buttons.
+ * @part previous-button - The previous button.
+ * @part next-button - The next button.
+ * @part page-button - The page button.
+ * @part active-page-button - The active page button.
+ */
+@Component({
+  tag: 'atomic-pager-v1', // TODO: remove v1
+  styleUrl: 'atomic-pager.pcss',
+  shadow: true,
+})
+export class AtomicPager implements InitializableComponent {
+  @InitializeBindings() public bindings!: Bindings;
+  private pager!: Pager;
+  public searchStatus!: SearchStatus;
+
+  @BindStateToController('pager')
+  @State()
+  private pagerState!: PagerState;
+  @BindStateToController('searchStatus')
+  @State()
+  private searchStatusState!: SearchStatusState;
+  private strings = {
+    pagination: () => this.bindings.i18n.t('pagination'),
+    previous: () => this.bindings.i18n.t('previous'),
+    next: () => this.bindings.i18n.t('next'),
+    pageNumber: (page: number) => this.bindings.i18n.t('page-number', {page}),
+  };
+  @State() error!: Error;
+
+  @Event({
+    eventName: 'atomic/scrollToTop',
+  })
+  private scrollToTopEvent!: EventEmitter;
+
+  /**
+   * Specifies how many page buttons to display in the pager.
+   */
+  @Prop() numberOfPages = 5;
+
+  public initialize() {
+    this.searchStatus = buildSearchStatus(this.bindings.engine);
+    this.pager = buildPager(this.bindings.engine, {
+      options: {numberOfPages: this.numberOfPages},
+    });
+  }
+
+  private scrollToTop() {
+    this.scrollToTopEvent.emit();
+  }
+
+  private buildButton(options: {
+    part: string;
+    disabled: boolean;
+    ariaLabel: string;
+    callback: () => void;
+    icon: string;
+  }) {
+    return (
+      <li>
+        <button
+          part={options.part}
+          class={`text-primary ${
+            options.disabled
+              ? 'opacity-50 cursor-not-allowed'
+              : 'hover:text-primary-light'
+          }`}
+          disabled={options.disabled}
+          aria-label={options.ariaLabel}
+          onClick={() => {
+            options.callback();
+            this.scrollToTop();
+          }}
+        >
+          <atomic-icon icon={options.icon} class="h-full"></atomic-icon>
+        </button>
+      </li>
+    );
+  }
+
+  private get previousButton() {
+    return this.buildButton({
+      ariaLabel: this.strings.previous(),
+      callback: () => {
+        this.pager.previousPage();
+      },
+      disabled: !this.pagerState.hasPreviousPage,
+      icon: ArrowLeftIcon,
+      part: 'previous-button',
+    });
+  }
+
+  private get nextButton() {
+    return this.buildButton({
+      ariaLabel: this.strings.next(),
+      callback: () => {
+        this.pager.nextPage();
+      },
+      disabled: !this.pagerState.hasNextPage,
+      icon: ArrowRightIcon,
+      part: 'next-button',
+    });
+  }
+
+  private get pages() {
+    const pages = this.pager.state.currentPages;
+    return pages.map((page) => this.buildPage(page));
+  }
+
+  private buildPage(page: number) {
+    const isSelected = this.pager.isCurrentPage(page);
+    const classes = isSelected
+      ? 'text-on-primary bg-primary hover:bg-primary-light'
+      : 'text-on-background';
+
+    return (
+      <li>
+        <button
+          class={`hover:underline ${classes}`}
+          aria-current={isSelected ? 'page' : null}
+          part={`page-button ${isSelected && 'active-page-button'}`}
+          aria-label={this.strings.pageNumber(page)}
+          onClick={() => {
+            this.pager.selectPage(page);
+            this.scrollToTop();
+          }}
+        >
+          {page.toLocaleString(this.bindings.i18n.language)}
+        </button>
+      </li>
+    );
+  }
+
+  public render() {
+    if (!this.searchStatusState.hasResults) {
+      return;
+    }
+
+    return (
+      <nav aria-label={this.strings.pagination()} class="items-center">
+        <ul part="buttons" class="flex space-x-2 flex-wrap">
+          {this.previousButton}
+          {this.pages}
+          {this.nextButton}
+        </ul>
+      </nav>
+    );
+  }
+}
---
+++ packages/atomic/src/components/atomic-results-per-page-v1/atomic-results-per-page.tsx
@@ -0,0 +1,134 @@
+import {Component, h, Prop, State} from '@stencil/core';
+import {
+  ResultsPerPage,
+  buildResultsPerPage,
+  ResultsPerPageState,
+  buildSearchStatus,
+  SearchStatus,
+  SearchStatusState,
+} from '@coveo/headless';
+import {
+  Bindings,
+  BindStateToController,
+  InitializableComponent,
+  InitializeBindings,
+} from '../../utils/initialization-utils';
+
+/**
+ * The `atomic-results-per-page` component determines how many results to display per page.
+ *
+ * @part label - The "Results per page" label.
+ * @part buttons - The list of buttons.
+ * @part page-button - The page button.
+ * @part active-page-button - The active page button.
+ */
+@Component({
+  tag: 'atomic-results-per-page-v1', // TODO: remove v1
+  styleUrl: 'atomic-results-per-page.pcss',
+  shadow: true,
+})
+export class AtomicResultsPerPage implements InitializableComponent {
+  @InitializeBindings() public bindings!: Bindings;
+  private resultPerPage!: ResultsPerPage;
+  public searchStatus!: SearchStatus;
+  private choices!: number[];
+
+  @State()
+  @BindStateToController('resultPerPage')
+  public resultPerPageState!: ResultsPerPageState;
+  @BindStateToController('searchStatus')
+  @State()
+  private searchStatusState!: SearchStatusState;
+  private strings = {
+    resultsPerPage: () => this.bindings.i18n.t('results-per-page'),
+    displayResultsPerPage: (results: number) =>
+      this.bindings.i18n.t('display-results-per-page', {results}),
+  };
+  @State() public error!: Error;
+
+  /**
+   * A list of choices for the number of results to display per page, separated by commas.
+   */
+  @Prop() choicesDisplayed = '10,25,50,100';
+  /**
+   * The initial selection for the number of result per page. This should be part of the `choicesDisplayed` option. By default, this is set to the first value in `choicesDisplayed`.
+   */
+  @Prop({mutable: true}) initialChoice?: number;
+
+  public initialize() {
+    this.choices = this.validateChoicesDisplayed();
+    this.validateInitialChoice();
+
+    this.searchStatus = buildSearchStatus(this.bindings.engine);
+    this.resultPerPage = buildResultsPerPage(this.bindings.engine, {
+      initialState: {numberOfResults: this.initialChoice},
+    });
+  }
+
+  private validateChoicesDisplayed() {
+    return this.choicesDisplayed.split(',').map((choice) => {
+      const parsedChoice = parseInt(choice);
+      if (isNaN(parsedChoice)) {
+        const errorMsg = `The choice value "${choice}" from the "choicesDisplayed" option is not a number.`;
+        this.bindings.engine.logger.error(errorMsg, this);
+        throw new Error(errorMsg);
+      }
+
+      return parsedChoice;
+    });
+  }
+
+  private validateInitialChoice() {
+    if (!this.initialChoice) {
+      this.initialChoice = this.choices[0];
+      return;
+    }
+    if (!this.choices.includes(this.initialChoice)) {
+      const errorMsg = `The "initialChoice" option value "${this.initialChoice}" is not included in the "choicesDisplayed" option "${this.choicesDisplayed}".`;
+      this.bindings.engine.logger.error(errorMsg, this);
+      throw new Error(errorMsg);
+    }
+  }
+
+  private buildChoice(choice: number) {
+    const isSelected = this.resultPerPage.isSetTo(choice);
+    const classes = isSelected
+      ? 'text-on-primary bg-primary hover:bg-primary-light'
+      : 'text-on-background';
+
+    return (
+      <button
+        role="radio"
+        aria-label={this.strings.displayResultsPerPage(choice)}
+        aria-checked={`${isSelected}`}
+        class={`hover:underline ${classes}`}
+        part={`page-button ${isSelected && 'active-page-button'}`}
+        onClick={() => this.resultPerPage.set(choice)}
+      >
+        {choice.toLocaleString(this.bindings.i18n.language)}
+      </button>
+    );
+  }
+
+  public render() {
+    if (!this.searchStatusState.hasResults) {
+      return;
+    }
+
+    return (
+      <div class="flex items-center">
+        <span part="label" class="text-on-background pr-4">
+          {this.strings.resultsPerPage()}
+        </span>
+        <div
+          part="buttons"
+          role="radiogroup"
+          aria-label={this.strings.resultsPerPage()}
+          class="flex flex-wrap flex-grow	mr-2"
+        >
+          {this.choices.map((choice) => this.buildChoice(choice))}
+        </div>
+      </div>
+    );
+  }
+}
