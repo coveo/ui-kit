bc09d4a7d6ad2b034ba5938c31a5c557041ce498
nlafrance-coveo <nlafrance@coveo.com>
2021-08-25 09:23:34 -0400

    feat(headless): fixed mismatched action names

--- packages/headless/src/features/recent-queries/recent-queries-analytics-actions.ts
+++ packages/headless/src/features/recent-queries/recent-queries-analytics-actions.ts
@@ -1,17 +1,6 @@
 import {makeAnalyticsAction, AnalyticsType} from '../analytics/analytics-utils';
 
 /**
- * Logs a search event with an `actionCause` value of `recentQueriesClick`.
- */
-export const logRecentQueryClick = makeAnalyticsAction(
-  'analytics/recentQueries/click',
-  AnalyticsType.Search,
-  (client) => {
-    return client.logRecentQueryClick();
-  }
-);
-
-/**
  * Logs a custom event with an `actionCause` value of `clearRecentQueries`.
  */
 export const logClearRecentQueries = makeAnalyticsAction(
@@ -19,5 +8,16 @@ export const logClearRecentQueries = makeAnalyticsAction(
   AnalyticsType.Custom,
   (client) => {
     return client.logClearRecentQueries();
+  }
+);
+
+/**
+ * Logs a search event with an `actionCause` value of `recentQueriesClick`.
+ */
+export const logRecentQueryClick = makeAnalyticsAction(
+  'analytics/recentQueries/click',
+  AnalyticsType.Search,
+  (client) => {
+    return client.logRecentQueryClick();
   }
 );
