b208e9fbec2c29ae75d8dbbe70ef6dd91c33b644
btaillon <54454747+btaillon@users.noreply.github.com>
2021-08-31 09:01:07 -0500

    docs(atomic): added documentation for result sections (#1145)
    https://coveord.atlassian.net/browse/KIT-790

--- packages/atomic/src/components/result-template-components-v1/atomic-result-sections/atomic-result-section-title-metadata.tsx
+++ packages/atomic/src/components/result-template-components-v1/atomic-result-sections/atomic-result-section-title-metadata.tsx
@@ -2,9 +2,14 @@ import {Element, Component} from '@stencil/core';
 import {containsVisualElement} from '../../../utils/utils';
 
 /**
- * The `atomic-result-section-title-metadata` element, when added to a result template,
- * changes the style and position of its content to match specifications
- * from the result list element.
+ * The title metadata section helps the information seeker understand some
+ * fields that are in direct relationship with the item name. E.g.: in
+ * commerce the rating of an item is closely tied to what the product is,
+ * rather than a descriptive of the product.
+ *
+ * Behaviour:
+ * * Has a very small font size
+ * * Is the closest element underneath the title section
  */
 @Component({
   tag: 'atomic-result-section-title-metadata',
