4c60ea0fa48abfa3ef18592be73b2b2d98ba45b0
pkuttappan <pkuttappan@coveo.com>
2021-09-08 13:32:40 -0400

    Changed the name of the component, improved readability and added the changes suggested

--- packages/quantic/force-app/main/default/lwc/quanticRecentQueriesList/quanticRecentQueriesList.js
+++ packages/quantic/force-app/main/default/lwc/quanticRecentQueriesList/quanticRecentQueriesList.js
@@ -1,31 +1,36 @@
 import { LightningElement, api, track } from 'lwc';
 import { registerComponentForInit, initializeWithHeadless } from 'c/quanticHeadlessLoader';
-import { getItemFromLocalStorage, setItemInLocalStorage } from 'c/quanticUtils';
-import recentQueriesLabel from '@salesforce/label/c.quantic_RecentQueries';
-import emptyListLabel from '@salesforce/label/c.quantic_EmptyRecentQueriesLabel';
+import { I18nUtils, getItemFromLocalStorage, setItemInLocalStorage } from 'c/quanticUtils';
 
-export default class QuanticRecentQueries extends LightningElement {
+import recentQueriesLabel from '@salesforce/label/c.quantic_RecentQueries';
+import emptyListLabel from '@salesforce/label/c.quantic_EmptyRecentQueriesListLabel';
+import collapse from '@salesforce/label/c.quantic_Collapse';
+import expand from '@salesforce/label/c.quantic_Expand';
+
+export default class QuanticRecentQueriesList extends LightningElement {
   labels = {
     recentQueriesLabel,
     emptyListLabel,
+    collapse,
+    expand
   }
-  
+
   /** @type {import("coveo").RecentQueriesState} */
   @track state;
+
   /** @type {string} */
   @api label = this.labels.recentQueriesLabel;
+  /** @type {number} */
+  @api maxLength = 10;
   /** @type {string} */
   @api engineId;
+  
   /** @type {import("coveo").RecentQueriesList} */
   recentQueriesList;
   /** @type {()=> void} */
   unsubscribe;
   /** @type {boolean} */
-  isCollapsed = false;
-  /** @type {string} */
-  collapseIcon = 'utility:dash';
-  /** @type {number} */
-  @api maxLength = 10;
+  isExpanded = true;
 
   connectedCallback() {
     registerComponentForInit(this, this.engineId);
@@ -45,7 +50,7 @@ export default class QuanticRecentQueries extends LightningElement {
         queries: getItemFromLocalStorage(this.localStorageKey) ?? [],
       },
       options: {
-        maxLength: this.maxLength,
+        maxLength: Number(this.maxLength),
       },
     });
     this.unsubscribe = this.recentQueriesList.subscribe(() => this.updateState());
@@ -65,8 +70,7 @@ export default class QuanticRecentQueries extends LightningElement {
   }
 
   toggleVisibility() {
-    this.collapseIcon = this.isCollapsed ? 'utility:dash' : 'utility:add';
-    this.isCollapsed = !this.isCollapsed;
+    this.isExpanded = !this.isExpanded;
   }
 
   get queries() {
@@ -79,5 +83,14 @@ export default class QuanticRecentQueries extends LightningElement {
 
   get localStorageKey() {
     return `${this.engineId}_quantic-recent-queries`;
+  }
+
+  get actionButtonIcon() {
+    return this.isExpanded ? 'utility:dash' : 'utility:add';
+  }
+
+  get actionButtonLabel() {
+    const label = this.isExpanded ? this.labels.collapse : this.labels.expand;
+    return I18nUtils.format(label, this.label);
   }
 }
