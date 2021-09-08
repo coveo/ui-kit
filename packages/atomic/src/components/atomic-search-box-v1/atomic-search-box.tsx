6f539364ef3537559dab36dc7616d7d1f5e0b9c5
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-09-08 15:14:59 -0400

    split render methods
    https://coveord.atlassian.net/browse/KIT-851

--- packages/atomic/src/components/atomic-search-box-v1/atomic-search-box.tsx
+++ packages/atomic/src/components/atomic-search-box-v1/atomic-search-box.tsx
@@ -1,7 +1,12 @@
 import SearchIcon from 'coveo-styleguide/resources/icons/svg/search.svg';
 import ClearIcon from 'coveo-styleguide/resources/icons/svg/clear.svg';
 import {Component, h, State} from '@stencil/core';
-import {SearchBox, SearchBoxState, buildSearchBox} from '@coveo/headless';
+import {
+  SearchBox,
+  SearchBoxState,
+  buildSearchBox,
+  Suggestion,
+} from '@coveo/headless';
 import {
   Bindings,
   BindStateToController,
@@ -46,41 +51,64 @@ export class AtomicSearchBox {
     });
   }
 
+  private renderInput() {
+    return (
+      <input
+        placeholder={this.bindings.i18n.t('search')}
+        aria-label={this.bindings.i18n.t('search-box')}
+        type="text"
+        class="h-full outline-none bg-transparent flex-grow px-4 text-neutral-dark placeholder-neutral-dark text-lg"
+        value={this.searchBoxState.value}
+        onFocus={() => (this.hasFocus = true)}
+        onBlur={() => (this.hasFocus = false)}
+        onInput={(e) =>
+          this.searchBox.updateText((e.target as HTMLInputElement).value)
+        }
+      />
+    );
+  }
+
+  private renderClearButton() {
+    return (
+      <Button
+        style="text-transparent"
+        part="clear-button"
+        class="w-8 h-8 mr-1.5 text-neutral-dark"
+        onClick={() => this.searchBox.clear()}
+        ariaLabel={this.bindings.i18n.t('clear')}
+      >
+        <atomic-icon icon={ClearIcon} class="w-3 h-3"></atomic-icon>
+      </Button>
+    );
+  }
+
   private renderInputContainer() {
+    const isLoading = this.searchBoxState.isLoading;
+    const hasValue = this.searchBoxState.value.trim() !== '';
     return (
       <div class="flex-grow flex items-center">
-        <input
-          placeholder={this.bindings.i18n.t('search')}
-          aria-label={this.bindings.i18n.t('search-box')}
-          type="text"
-          class="h-full outline-none bg-transparent flex-grow px-4 text-neutral-dark placeholder-neutral-dark text-lg"
-          value={this.searchBoxState.value}
-          onFocus={() => (this.hasFocus = true)}
-          onBlur={() => (this.hasFocus = false)}
-          onInput={(e) =>
-            this.searchBox.updateText((e.target as HTMLInputElement).value)
-          }
-        />
-        {this.searchBoxState.isLoading && (
+        {this.renderInput()}
+        {isLoading && (
           <span class="loading w-5 h-5 rounded-full bg-gradient-to-r animate-spin mr-3 grid place-items-center"></span>
         )}
-        {!this.searchBoxState.isLoading &&
-          this.searchBoxState.value.trim() !== '' && (
-            <Button
-              style="text-transparent"
-              part="clear-button"
-              class="w-8 h-8 mr-1.5 text-neutral-dark"
-              onClick={() => this.searchBox.clear()}
-              ariaLabel={this.bindings.i18n.t('clear')}
-            >
-              <atomic-icon icon={ClearIcon} class="w-3 h-3"></atomic-icon>
-            </Button>
-          )}
+        {!isLoading && hasValue && this.renderClearButton()}
       </div>
     );
   }
 
-  // TODO: adapt for atomic-query-suggestions & atomic-recent-queries
+  // TODO: move inside the atomic-query-suggestions/atomic-recent-queries components
+  private renderSuggestion(suggestion: Suggestion) {
+    return (
+      <li
+        key={suggestion.rawValue}
+        class="flex px-4 h-10 items-center text-neutral-dark hover:bg-neutral-light cursor-pointer first:rounded-t-md last:rounded-b-md"
+      >
+        {/* TODO: add icon when mixed suggestions */}
+        <span innerHTML={suggestion.highlightedValue}></span>
+      </li>
+    );
+  }
+
   private renderSuggestions() {
     if (!this.searchBoxState.suggestions.length) {
       return;
@@ -92,17 +120,9 @@ export class AtomicSearchBox {
 
     return (
       <ul class="w-full absolute left-0 top-full rounded-md bg-background border border-neutral">
-        {this.searchBoxState.suggestions.map((suggestion) => {
-          return (
-            <li
-              key={suggestion.rawValue}
-              class="flex px-4 h-10 items-center text-neutral-dark hover:bg-neutral-light cursor-pointer first:rounded-t-md last:rounded-b-md"
-            >
-              {/* TODO: add icon when mixed suggestions */}
-              <span innerHTML={suggestion.highlightedValue}></span>
-            </li>
-          );
-        })}
+        {this.searchBoxState.suggestions.map((suggestion) =>
+          this.renderSuggestion(suggestion)
+        )}
       </ul>
     );
   }
