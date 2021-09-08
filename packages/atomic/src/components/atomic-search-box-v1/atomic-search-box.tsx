4fe0355670b5a5dd62d505a3f3af7adf95dc45cd
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-09-08 16:01:55 -0400

    improve accessibility
    https://coveord.atlassian.net/browse/KIT-851

--- packages/atomic/src/components/atomic-search-box-v1/atomic-search-box.tsx
+++ packages/atomic/src/components/atomic-search-box-v1/atomic-search-box.tsx
@@ -13,6 +13,7 @@ import {
   InitializeBindings,
 } from '../../utils/initialization-utils';
 import {Button} from '../common/button';
+import {randomID} from '../../utils/utils';
 
 /**
  * The `atomic-search-box` component creates a search box with built-in support for suggestions.
@@ -26,16 +27,20 @@ export class AtomicSearchBox {
   @InitializeBindings() public bindings!: Bindings;
 
   private searchBox!: SearchBox;
+  private id!: string;
 
   @BindStateToController('searchBox')
   @State()
   private searchBoxState!: SearchBoxState;
   @State() public error!: Error;
-  @State() hasFocus = false;
+  @State() private isExpanded = false;
+  @State() private activeDescendant = '';
 
   public initialize() {
+    this.id = randomID('atomic-search-box-');
     this.searchBox = buildSearchBox(this.bindings.engine, {
       options: {
+        id: this.id,
         numberOfSuggestions: 8, // TODO: handle when adding query suggestion component
         highlightOptions: {
           notMatchDelimiters: {
@@ -51,16 +56,29 @@ export class AtomicSearchBox {
     });
   }
 
+  private get popupId() {
+    return `${this.id}-popup`;
+  }
+
   private renderInput() {
     return (
       <input
+        role="combobox"
+        aria-autocomplete="both"
+        aria-haspopup="true"
+        aria-owns={this.popupId}
+        aria-expanded={`${this.isExpanded}`}
+        aria-activedescendant={this.activeDescendant}
+        autocomplete="off"
+        autocapitalize="off"
+        autocorrect="off"
         placeholder={this.bindings.i18n.t('search')}
         aria-label={this.bindings.i18n.t('search-box')}
         type="text"
         class="h-full outline-none bg-transparent flex-grow px-4 text-neutral-dark placeholder-neutral-dark text-lg"
         value={this.searchBoxState.value}
-        onFocus={() => (this.hasFocus = true)}
-        onBlur={() => (this.hasFocus = false)}
+        onFocus={() => (this.isExpanded = true)}
+        onBlur={() => (this.isExpanded = false)}
         onInput={(e) =>
           this.searchBox.updateText((e.target as HTMLInputElement).value)
         }
@@ -98,8 +116,13 @@ export class AtomicSearchBox {
 
   // TODO: move inside the atomic-query-suggestions/atomic-recent-queries components
   private renderSuggestion(suggestion: Suggestion) {
+    const id = `${this.id}-${suggestion.rawValue}`;
+    const isSelected = id === this.activeDescendant;
     return (
       <li
+        id={id}
+        role="option"
+        aria-selected={`${isSelected}`}
         key={suggestion.rawValue}
         class="flex px-4 h-10 items-center text-neutral-dark hover:bg-neutral-light cursor-pointer first:rounded-t-md last:rounded-b-md"
       >
@@ -114,12 +137,17 @@ export class AtomicSearchBox {
       return;
     }
 
-    if (!this.hasFocus) {
+    if (!this.isExpanded) {
       return;
     }
 
     return (
-      <ul class="w-full absolute left-0 top-full rounded-md bg-background border border-neutral">
+      <ul
+        id={this.popupId}
+        role="listbox"
+        aria-label={this.bindings.i18n.t('query-suggestion-list')}
+        class="w-full absolute left-0 top-full rounded-md bg-background border border-neutral"
+      >
         {this.searchBoxState.suggestions.map((suggestion) =>
           this.renderSuggestion(suggestion)
         )}
@@ -133,6 +161,7 @@ export class AtomicSearchBox {
         style="primary"
         class="w-12 h-full rounded-r-md rounded-l-none"
         part="submit-button"
+        ariaLabel={this.bindings.i18n.t('search')}
         onClick={() => this.searchBox.submit()}
       >
         <atomic-icon icon={SearchIcon} class="w-4 h-4"></atomic-icon>
@@ -144,7 +173,7 @@ export class AtomicSearchBox {
     return (
       <div
         class={`relative flex bg-background h-full w-full box-border border-neutral rounded-md ${
-          this.hasFocus ? 'border-primary ring ring-ring-primary' : ''
+          this.isExpanded ? 'border-primary ring ring-ring-primary' : ''
         }`}
       >
         {this.renderInputContainer()}
