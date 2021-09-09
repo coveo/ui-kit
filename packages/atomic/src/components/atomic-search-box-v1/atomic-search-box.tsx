517b1cca3564681b80a406b01e0e10091f656504
Jean-Félix Thibodeau <ThibodeauJF@users.noreply.github.com>
2021-09-09 16:58:05 -0400

    feat(atomic): add search-box v1 interactivity + accessibility (#1186)
    https://coveord.atlassian.net/browse/KIT-851

--- packages/atomic/src/components/atomic-search-box-v1/atomic-search-box.tsx
+++ packages/atomic/src/components/atomic-search-box-v1/atomic-search-box.tsx
@@ -1,16 +1,33 @@
 import SearchIcon from 'coveo-styleguide/resources/icons/svg/search.svg';
 import ClearIcon from 'coveo-styleguide/resources/icons/svg/clear.svg';
 import {Component, h, State} from '@stencil/core';
-import {SearchBox, SearchBoxState, buildSearchBox} from '@coveo/headless';
+import {
+  SearchBox,
+  SearchBoxState,
+  buildSearchBox,
+  Suggestion,
+  loadQuerySetActions,
+  QuerySetActionCreators,
+} from '@coveo/headless';
 import {
   Bindings,
   BindStateToController,
   InitializeBindings,
 } from '../../utils/initialization-utils';
 import {Button} from '../common/button';
+import {randomID} from '../../utils/utils';
+import {isNullOrUndefined} from '@coveo/bueno';
 
 /**
  * The `atomic-search-box` component creates a search box with built-in support for suggestions.
+ *
+ * @part input - The search box input.
+ * @part loading - The search box loading animation.
+ * @part clear-button - The button to clear the search box of input.
+ * @part submit-button - The search box submit button.
+ * @part suggestions - A list of suggested query corrections.
+ * @part suggestion - A suggested query correction.
+ * @part active-suggestion - The currently active suggestion.
  */
 @Component({
   tag: 'atomic-search-box-v1', // TODO: remove v1
@@ -19,18 +36,25 @@ import {Button} from '../common/button';
 })
 export class AtomicSearchBox {
   @InitializeBindings() public bindings!: Bindings;
-
   private searchBox!: SearchBox;
+  private id!: string;
+  private inputRef!: HTMLInputElement;
+  private listRef!: HTMLElement;
+  private querySetActions!: QuerySetActionCreators;
 
   @BindStateToController('searchBox')
   @State()
   private searchBoxState!: SearchBoxState;
   @State() public error!: Error;
-  @State() hasFocus = false;
+  @State() private isExpanded = false;
+  @State() private activeDescendant = '';
 
   public initialize() {
+    this.id = randomID('atomic-search-box-');
+    this.querySetActions = loadQuerySetActions(this.bindings.engine);
     this.searchBox = buildSearchBox(this.bindings.engine, {
       options: {
+        id: this.id,
         numberOfSuggestions: 8, // TODO: handle when adding query suggestion component
         highlightOptions: {
           notMatchDelimiters: {
@@ -46,60 +70,236 @@ export class AtomicSearchBox {
     });
   }
 
+  private get popupId() {
+    return `${this.id}-popup`;
+  }
+
+  private get hasSuggestions() {
+    return !!this.searchBoxState.suggestions.length;
+  }
+
+  private get hasActiveDescendant() {
+    return this.activeDescendant !== '';
+  }
+
+  private updateActiveDescendant(activeDescendant = '') {
+    this.activeDescendant = activeDescendant;
+  }
+
+  private get activeDescendantElement(): HTMLLIElement | null {
+    if (!this.hasActiveDescendant) {
+      return null;
+    }
+
+    return this.listRef.querySelector(`#${this.activeDescendant}`);
+  }
+
+  private get firstValue() {
+    return this.listRef.firstElementChild!;
+  }
+
+  private get lastValue() {
+    return this.listRef.lastElementChild!;
+  }
+
+  private get nextOrFirstValue() {
+    if (!this.hasActiveDescendant) {
+      return this.firstValue;
+    }
+
+    return this.activeDescendantElement?.nextElementSibling || this.firstValue;
+  }
+
+  private get previousOrLastValue() {
+    if (!this.hasActiveDescendant) {
+      return this.lastValue;
+    }
+
+    return (
+      this.activeDescendantElement?.previousElementSibling || this.lastValue
+    );
+  }
+
+  private focusNextValue() {
+    if (!this.hasSuggestions) {
+      return;
+    }
+
+    const query = this.nextOrFirstValue.getAttribute('data-value');
+    !isNullOrUndefined(query) && this.updateQuery(query);
+    this.updateActiveDescendant(this.nextOrFirstValue.id);
+  }
+
+  private focusPreviousValue() {
+    if (!this.hasSuggestions) {
+      return;
+    }
+
+    const query = this.previousOrLastValue.getAttribute('data-value');
+    !isNullOrUndefined(query) && this.updateQuery(query);
+    this.updateActiveDescendant(this.previousOrLastValue.id);
+  }
+
+  private onInput(value: string) {
+    this.searchBox.updateText(value);
+    this.updateActiveDescendant();
+  }
+
+  private onFocus() {
+    this.isExpanded = true;
+    if (!this.searchBoxState.suggestions.length) {
+      this.searchBox.showSuggestions();
+    }
+  }
+
+  private onBlur() {
+    this.isExpanded = false;
+    this.updateActiveDescendant();
+  }
+
+  private onSubmit() {
+    if (this.activeDescendantElement) {
+      this.activeDescendantElement.click();
+      this.updateActiveDescendant();
+      return;
+    }
+
+    this.searchBox.submit();
+    this.updateActiveDescendant();
+  }
+
+  private updateQuery(query: string) {
+    this.bindings.engine.dispatch(
+      this.querySetActions.updateQuerySetQuery({
+        id: this.id,
+        query,
+      })
+    );
+  }
+
+  private onKeyDown(e: KeyboardEvent) {
+    switch (e.key) {
+      case 'Enter':
+        this.onSubmit();
+        break;
+      case 'Escape':
+        this.onBlur();
+        break;
+      case 'ArrowDown':
+        e.preventDefault();
+        this.focusNextValue();
+        break;
+      case 'ArrowUp':
+        e.preventDefault();
+        this.focusPreviousValue();
+        break;
+    }
+  }
+
+  private renderInput() {
+    return (
+      <input
+        part="input"
+        ref={(el) => (this.inputRef = el as HTMLInputElement)}
+        role="combobox"
+        aria-autocomplete="both"
+        aria-haspopup="true"
+        aria-owns={this.popupId}
+        aria-expanded={`${this.isExpanded}`}
+        aria-activedescendant={this.activeDescendant}
+        autocomplete="off"
+        autocapitalize="off"
+        autocorrect="off"
+        placeholder={this.bindings.i18n.t('search')}
+        aria-label={this.bindings.i18n.t('search-box')}
+        type="text"
+        class="h-full outline-none bg-transparent flex-grow px-4 text-neutral-dark placeholder-neutral-dark text-lg"
+        value={this.searchBoxState.value}
+        onFocus={() => this.onFocus()}
+        onBlur={() => this.onBlur()}
+        onInput={(e) => this.onInput((e.target as HTMLInputElement).value)}
+        onKeyDown={(e) => this.onKeyDown(e)}
+      />
+    );
+  }
+
+  private renderClearButton() {
+    return (
+      <Button
+        style="text-transparent"
+        part="clear-button"
+        class="w-8 h-8 mr-1.5 text-neutral-dark"
+        onClick={() => {
+          this.searchBox.clear();
+          this.inputRef.focus();
+        }}
+        ariaLabel={this.bindings.i18n.t('clear')}
+      >
+        <atomic-icon icon={ClearIcon} class="w-3 h-3"></atomic-icon>
+      </Button>
+    );
+  }
+
   private renderInputContainer() {
+    const isLoading = this.searchBoxState.isLoading;
+    const hasValue = this.searchBoxState.value !== '';
     return (
       <div class="flex-grow flex items-center">
-        <input
-          placeholder={this.bindings.i18n.t('search')}
-          aria-label={this.bindings.i18n.t('search-box')}
-          type="text"
-          class="h-full outline-none bg-transparent flex-grow px-4 text-neutral-dark placeholder-neutral-dark text-lg"
-          value={this.searchBoxState.value}
-          onFocus={() => (this.hasFocus = true)}
-          onBlur={() => (this.hasFocus = false)}
-          onInput={(e) =>
-            this.searchBox.updateText((e.target as HTMLInputElement).value)
-          }
-        />
-        {this.searchBoxState.isLoading && (
-          <span class="loading w-5 h-5 rounded-full bg-gradient-to-r animate-spin mr-3 grid place-items-center"></span>
+        {this.renderInput()}
+        {isLoading && (
+          <span
+            part="loading"
+            class="loading w-5 h-5 rounded-full bg-gradient-to-r animate-spin mr-3 grid place-items-center"
+          ></span>
         )}
-        {!this.searchBoxState.isLoading &&
-          this.searchBoxState.value.trim() !== '' && (
-            <Button
-              style="text-transparent"
-              part="clear-button"
-              class="w-8 h-8 mr-1.5 text-neutral-dark"
-              onClick={() => this.searchBox.clear()}
-              ariaLabel={this.bindings.i18n.t('clear')}
-            >
-              <atomic-icon icon={ClearIcon} class="w-3 h-3"></atomic-icon>
-            </Button>
-          )}
+        {!isLoading && hasValue && this.renderClearButton()}
       </div>
     );
   }
 
-  // TODO: adapt for atomic-query-suggestions & atomic-recent-queries
-  private renderSuggestions() {
-    if (!this.searchBoxState.suggestions.length) {
-      return;
-    }
+  // TODO: move inside the atomic-query-suggestions/atomic-recent-queries components
+  private renderSuggestion(suggestion: Suggestion, index: number) {
+    const id = `${this.id}-suggestion-${index}`;
+    const isSelected = id === this.activeDescendant;
+    return (
+      <li
+        id={id}
+        role="option"
+        aria-selected={`${isSelected}`}
+        key={suggestion.rawValue}
+        data-value={suggestion.rawValue}
+        part={isSelected ? 'active-suggestion suggestion' : 'suggestion'}
+        class={`flex px-4 h-10 items-center text-neutral-dark hover:bg-neutral-light cursor-pointer first:rounded-t-md last:rounded-b-md ${
+          isSelected ? 'bg-neutral-light' : ''
+        }`}
+        onMouseDown={(e) => e.preventDefault()}
+        onClick={() => {
+          this.searchBox.selectSuggestion(suggestion.rawValue);
+        }}
+      >
+        {/* TODO: add icon when mixed suggestions */}
+        <span innerHTML={suggestion.highlightedValue}></span>
+      </li>
+    );
+  }
 
-    if (!this.hasFocus) {
-      return;
-    }
+  private renderSuggestions() {
+    const showSuggestions = this.hasSuggestions && this.isExpanded;
 
     return (
-      <ul class="w-full absolute left-0 top-full rounded-md bg-background border border-neutral">
-        {this.searchBoxState.suggestions.map((suggestion) => {
-          return (
-            <li class="flex px-4 h-10 items-center text-neutral-dark hover:bg-neutral-light cursor-pointer first:rounded-t-md last:rounded-b-md">
-              {/* TODO: add icon when mixed suggestions */}
-              <span innerHTML={suggestion.highlightedValue}></span>
-            </li>
-          );
-        })}
+      <ul
+        id={this.popupId}
+        role="listbox"
+        part="suggestions"
+        aria-label={this.bindings.i18n.t('query-suggestion-list')}
+        ref={(el) => (this.listRef = el!)}
+        class={`w-full z-10 absolute left-0 top-full rounded-md bg-background border border-neutral ${
+          showSuggestions ? '' : 'hidden'
+        }`}
+      >
+        {this.searchBoxState.suggestions.map((suggestion, index) =>
+          this.renderSuggestion(suggestion, index)
+        )}
       </ul>
     );
   }
@@ -110,6 +310,7 @@ export class AtomicSearchBox {
         style="primary"
         class="w-12 h-full rounded-r-md rounded-l-none"
         part="submit-button"
+        ariaLabel={this.bindings.i18n.t('search')}
         onClick={() => this.searchBox.submit()}
       >
         <atomic-icon icon={SearchIcon} class="w-4 h-4"></atomic-icon>
@@ -121,7 +322,7 @@ export class AtomicSearchBox {
     return (
       <div
         class={`relative flex bg-background h-full w-full box-border border-neutral rounded-md ${
-          this.hasFocus ? 'border-primary ring ring-ring-primary' : ''
+          this.isExpanded ? 'border-primary ring ring-ring-primary' : ''
         }`}
       >
         {this.renderInputContainer()}
