916cbd57883b4f1b6062cd7cf938ecde266d2677
Danny Lutz <dlutz@coveo.com>
2021-09-15 14:43:33 -0500

    update annotations from old PR

--- packages/atomic/src/components/result-template-components/atomic-result-sections/atomic-result-section-visual.tsx
+++ packages/atomic/src/components/result-template-components/atomic-result-sections/atomic-result-section-visual.tsx
@@ -2,19 +2,18 @@ import {Element, Component} from '@stencil/core';
 import {containsVisualElement} from '../../../utils/utils';
 
 /**
- * The visual section helps the information seeker understand information about
- * the item visually, for example in Commerce an image is a great descriptive
- * for some category of products, an icon can help understand what type an item
- * is or an avatar understand whom it is related to.
+ * This section provides visual information about the item.
+ * For example, in Commerce, an image is a great shorthand for a product category.
+ * An icon can quickly show the item type, or an avatar can help identify to whom it is related.
  *
- * Behaviour:
- * * Has a fixed size that depends on the specified image size, the layout, the density, and screen size.
- *   * When the image size is set to icon, this section stays very small.
- *   * You should ensure that elements inside of it take the available space.
- *   * You may use `atomic-size-condition` to display elements conditionally to the available space.
+ * Behavior:
+ * * Has a fixed size that depends on the specified image size, the layout, the density, and the screen size.
+ * ** When the image size is set to `icon`, this section stays very small.
+ * ** You should ensure that elements inside of it take the available space.
+ * ** You may use `atomic-size-condition` to display child elements if this section meets the defined size constraints.
  * * Always has a 1:1 aspect ratio.
- * * Has rounded corners, hiding anything that overflows out of them.
- * * Has a background color when the image size is `small` or `large`.
+ * * Has rounded corners, hiding anything that overflows beyond them.
+ * * Has a gray background when the image size is `small` or `large`.
  */
 @Component({
   tag: 'atomic-result-section-visual',
