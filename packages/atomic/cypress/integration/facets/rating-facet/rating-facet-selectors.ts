17511a43b24a0fa3b600ff99abcda10448d7fdaf
Lucille Vu <lvu@coveo.com>
2021-09-16 10:49:04 -0400

    test(atomic) Cypress test for Rating & Rating Range Facet (#1211)
    * KIT-999: Add Cypress test for Rating & Rating Range Facet
    
    https://coveord.atlassian.net/browse/KIT-999
    
    * KIT-999 Adjust the selectors
    
    https://coveord.atlassian.net/browse/KIT-999

---
+++ packages/atomic/cypress/integration/facets/rating-facet/rating-facet-selectors.ts
@@ -0,0 +1,34 @@
+export const ratingFacetComponent = 'atomic-rating-facet';
+export const RatingFacetSelectors = {
+  shadow: () => cy.get(ratingFacetComponent).shadow(),
+  wrapper: () => RatingFacetSelectors.shadow().find('[part="facet"]'),
+  placeholder: () => RatingFacetSelectors.shadow().find('[part="placeholder"]'),
+  values: () => RatingFacetSelectors.shadow().find('[part="values"]'),
+  clearButton: () =>
+    RatingFacetSelectors.shadow().find('[part="clear-button"]'),
+  labelButton: () =>
+    RatingFacetSelectors.shadow().find('[part="label-button"]'),
+  valueRating: () =>
+    RatingFacetSelectors.shadow().find('[part="value-rating"]'),
+  valueLabel: () => RatingFacetSelectors.shadow().find('[part="value-label"]'),
+  selectedCheckboxValue: () =>
+    RatingFacetSelectors.shadow().find(
+      '[part="value-checkbox"][aria-checked="true"]'
+    ),
+  idleCheckboxValue: () =>
+    RatingFacetSelectors.shadow().find(
+      '[part="value-checkbox"][aria-checked="false"]'
+    ),
+  selectedLinkValue: () =>
+    RatingFacetSelectors.shadow().find(
+      '[part="value-link"][aria-pressed="true"]'
+    ),
+  idleLinkValue: () =>
+    RatingFacetSelectors.shadow().find(
+      '[part="value-link"][aria-pressed="false"]'
+    ),
+  facetValueAtIndex: (index: number) =>
+    RatingFacetSelectors.valueRating().eq(index),
+  starsIconAtIndex: (index: number) =>
+    RatingFacetSelectors.facetValueAtIndex(index).find('atomic-icon'),
+};
