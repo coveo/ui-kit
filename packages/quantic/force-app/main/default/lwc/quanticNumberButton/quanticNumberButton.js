53420cb8045912e8fd5aeee781f8936c1cbfb4c6
nlafrance-coveo <nlafrance@coveo.com>
2021-09-20 08:57:47 -0400

    feat(quantic): numberbutton comments

--- packages/quantic/force-app/main/default/lwc/quanticNumberButton/quanticNumberButton.js
+++ packages/quantic/force-app/main/default/lwc/quanticNumberButton/quanticNumberButton.js
@@ -1,24 +1,35 @@
 import {LightningElement, api} from 'lwc';
 
 /**
- * The `QuanticNumberButton` component is used internally to display a button with a numeric label.
+ * The `QuanticNumberButton` component is used internally to display a button in a set of buttons with numeric labels.
  * @category LWC
+ * @fires CustomEvent#goto
  * @example
  * <c-quantic-number-button number="1" current="1" ongoto={goto}></c-quantic-number-button>
  */
 export default class QuanticNumberButton extends LightningElement {
+  /**
+   * The number to display as button label.
+   * @api
+   * @type {number}
+   */
   @api number;
+  /**
+   * The selected number in the set of buttons.
+   * @api
+   * @type {number}
+   */
   @api current;
 
   get isCurrent() {
     return this.number === this.current;
   }
 
-  goto() {
-    this.dispatchEvent(new CustomEvent('goto', {detail: this.number}));
-  }
-
   get variant() {
     return this.number === this.current ? 'brand-outline' : 'brand';
+  }
+
+  goto() {
+    this.dispatchEvent(new CustomEvent('goto', {detail: this.number}));
   }
 }
