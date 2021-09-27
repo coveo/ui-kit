aa410f88861750cbbc3f1a48fe550dbb5ceda806
Sami Sayegh <ssayegh@coveo.com>
2021-09-27 18:14:23 -0400

    add actions file
    https://coveord.atlassian.net/browse/KIT-354

---
+++ packages/headless/src/features/dictionary-field-context/dictionary-field-context-actions.ts
@@ -0,0 +1,62 @@
+import {RecordValue} from '@coveo/bueno';
+import {createAction} from '@reduxjs/toolkit';
+import {
+  requiredEmptyAllowedString,
+  validatePayload,
+} from '../../utils/validate-payload';
+import {DictionaryFieldContextPayload} from './dictionary-field-context-state';
+
+export const setDictionaryFieldContext = createAction(
+  'dictionaryFieldContext/set',
+  (payload: DictionaryFieldContextPayload) => {
+    const objSchema = new RecordValue({options: {required: true}});
+    const objError = validatePayload(payload, objSchema).error;
+
+    if (objError) {
+      return {payload, error: objError};
+    }
+
+    for (const [_, value] of Object.entries(payload)) {
+      const valueError = validatePayload(
+        value,
+        requiredEmptyAllowedString
+      ).error;
+
+      if (valueError) {
+        return {payload, error: valueError};
+      }
+    }
+
+    return {payload};
+  }
+);
+
+export interface AddDictionaryFieldContextActionCreatorPayload {
+  /**
+   * The name of the dictionary field.
+   */
+  field: string;
+
+  /**
+   * The dictionary field key to return the value of.
+   */
+  key: string;
+}
+
+export const addDictionaryFieldContext = createAction(
+  'dictionaryFieldContext/add',
+  (payload: AddDictionaryFieldContextActionCreatorPayload) => {
+    const schema = new RecordValue({
+      options: {required: true},
+      values: {
+        field: requiredEmptyAllowedString,
+        key: requiredEmptyAllowedString,
+      },
+    });
+
+    return validatePayload<AddDictionaryFieldContextActionCreatorPayload>(
+      payload,
+      schema
+    );
+  }
+);
