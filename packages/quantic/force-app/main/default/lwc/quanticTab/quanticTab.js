9abb6c391cbed5fd8092b95160e3affeafdbb0b6
nlafrance-coveo <nlafrance@coveo.com>
2021-09-28 18:11:13 -0400

    feat(quantic): improved isActive

--- packages/quantic/force-app/main/default/lwc/quanticTab/quanticTab.js
+++ packages/quantic/force-app/main/default/lwc/quanticTab/quanticTab.js
@@ -7,8 +7,6 @@ export default class QuanticTab extends LightningElement {
   tab;
   /** @type {()=> void} */
   unsubscribe;
-  /** @type {boolean} */
-  isActive;
   /** @type {string} */
   @api label;
   /** @type {string} */
@@ -16,7 +14,14 @@ export default class QuanticTab extends LightningElement {
   /** @type {string} */
   @api engineId;
   /** @type {boolean} */
-  @api initAsActive = false;
+  @api get isActive() {
+    return this._isActive;
+  }
+  set isActive(isActive) {
+    this._isActive = isActive;
+  }
+
+  _isActive = false;
 
   connectedCallback() {
     registerComponentForInit(this, this.engineId);
@@ -36,7 +41,7 @@ export default class QuanticTab extends LightningElement {
         id: this.label,
       },
       initialState: {
-        isActive: this.initAsActive,
+        isActive: this.isActive,
       }
     });
     this.unsubscribe = this.tab.subscribe(() => this.updateState());
@@ -47,7 +52,7 @@ export default class QuanticTab extends LightningElement {
   }
 
   updateState() {
-    this.isActive = this.tab.state.isActive;
+    this._isActive = this.tab.state.isActive;
   }
 
   select() {
