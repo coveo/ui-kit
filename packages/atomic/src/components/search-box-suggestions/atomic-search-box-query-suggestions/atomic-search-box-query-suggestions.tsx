421db2ca321e47339ba9b02e03db2cfa2deda8b1
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-09-28 16:09:31 -0400

    split into methods
    https://coveord.atlassian.net/browse/KIT-853

--- packages/atomic/src/components/search-box-suggestions/atomic-search-box-query-suggestions/atomic-search-box-query-suggestions.tsx
+++ packages/atomic/src/components/search-box-suggestions/atomic-search-box-query-suggestions/atomic-search-box-query-suggestions.tsx
@@ -1,13 +1,17 @@
 import {loadQuerySuggestActions, SearchEngine} from '@coveo/headless';
 import {QuerySuggestionSection} from '@coveo/headless/dist/definitions/state/state-sections';
 import {Component, Element, Prop, State, h} from '@stencil/core';
-import {dispatchSearchBoxSuggestionsEvent} from '../suggestions-common';
+import {
+  dispatchSearchBoxSuggestionsEvent,
+  SearchBoxSuggestionsBindings,
+} from '../suggestions-common';
 
 @Component({
   tag: 'atomic-search-box-query-suggestions',
   shadow: true,
 })
 export class AtomicSearchBoxQuerySuggestions {
+  private bindings!: SearchBoxSuggestionsBindings;
   @Element() private host!: HTMLElement;
 
   @State() public error!: Error;
@@ -17,51 +21,56 @@ export class AtomicSearchBoxQuerySuggestions {
 
   componentWillLoad() {
     try {
-      dispatchSearchBoxSuggestionsEvent(
-        ({engine, id, searchBoxController, numberOfQueries}) => {
-          const {registerQuerySuggest, fetchQuerySuggestions} =
-            loadQuerySuggestActions(engine);
-
-          (engine as SearchEngine<QuerySuggestionSection>).dispatch(
-            registerQuerySuggest({
-              id,
-              count: numberOfQueries,
-            })
-          );
-
-          return {
-            position: Array.from(this.host.parentNode!.children).indexOf(
-              this.host
-            ),
-            onInput: () =>
-              (engine as SearchEngine<QuerySuggestionSection>).dispatch(
-                fetchQuerySuggestions({
-                  id,
-                })
-              ),
-            renderItems: () => {
-              // TODO: filter out identical recent queries, might require "context"
-              const hasQuery = searchBoxController.state.value !== '';
-              const max = hasQuery ? this.maxWithQuery : this.maxWithoutQuery;
-              return searchBoxController.state.suggestions
-                .slice(0, max)
-                .map((suggestion) => ({
-                  content: (
-                    // TODO: add icon when other types of suggestions can appear
-                    <span innerHTML={suggestion.highlightedValue}></span>
-                  ),
-                  value: suggestion.rawValue,
-                  onClick: () =>
-                    searchBoxController.selectSuggestion(suggestion.rawValue),
-                }));
-            },
-          };
-        },
-        this.host
-      );
+      this.initialize();
     } catch (error) {
       this.error = error as Error;
     }
+  }
+
+  private initialize() {
+    dispatchSearchBoxSuggestionsEvent((bindings) => {
+      this.bindings = bindings;
+      const engine = bindings.engine as SearchEngine<QuerySuggestionSection>;
+      const {registerQuerySuggest, fetchQuerySuggestions} =
+        loadQuerySuggestActions(engine);
+
+      engine.dispatch(
+        registerQuerySuggest({
+          id: bindings.id,
+          count: bindings.numberOfQueries,
+        })
+      );
+
+      return {
+        position: Array.from(this.host.parentNode!.children).indexOf(this.host),
+        onInput: () =>
+          engine.dispatch(
+            fetchQuerySuggestions({
+              id: bindings.id,
+            })
+          ),
+        renderItems: () => this.renderItems(),
+      };
+    }, this.host);
+  }
+
+  private renderItems() {
+    // TODO: filter out identical recent queries, might require "context"
+    const hasQuery = this.bindings.searchBoxController.state.value !== '';
+    const max = hasQuery ? this.maxWithQuery : this.maxWithoutQuery;
+    return this.bindings.searchBoxController.state.suggestions
+      .slice(0, max)
+      .map((suggestion) => ({
+        content: (
+          // TODO: add icon when other types of suggestions can appear
+          <span innerHTML={suggestion.highlightedValue}></span>
+        ),
+        value: suggestion.rawValue,
+        onClick: () =>
+          this.bindings.searchBoxController.selectSuggestion(
+            suggestion.rawValue
+          ),
+      }));
   }
 
   public render() {
--- packages/atomic/src/components/search-box-suggestions/atomic-search-box-recent-queries/atomic-search-box-recent-queries.tsx
+++ packages/atomic/src/components/search-box-suggestions/atomic-search-box-recent-queries/atomic-search-box-recent-queries.tsx
@@ -1,12 +1,22 @@
-import {buildRecentQueriesList, HighlightUtils} from '@coveo/headless';
+import {
+  buildRecentQueriesList,
+  HighlightUtils,
+  RecentQueriesList,
+} from '@coveo/headless';
 import {Component, Element, Prop, State, h} from '@stencil/core';
-import {dispatchSearchBoxSuggestionsEvent} from '../suggestions-common';
+import {
+  dispatchSearchBoxSuggestionsEvent,
+  SearchBoxSuggestionsBindings,
+} from '../suggestions-common';
 
 @Component({
   tag: 'atomic-search-box-recent-queries',
   shadow: true,
 })
 export class AtomicSearchBoxRecentQueries {
+  private bindings!: SearchBoxSuggestionsBindings;
+  private recentQueriesList!: RecentQueriesList;
+
   @Element() private host!: HTMLElement;
 
   @State() public error!: Error;
@@ -16,60 +26,65 @@ export class AtomicSearchBoxRecentQueries {
 
   componentWillLoad() {
     try {
-      dispatchSearchBoxSuggestionsEvent(({engine, searchBoxController}) => {
-        const recentQueriesList = buildRecentQueriesList(engine, {
-          // TODO: fetch initial state from cookies or local storage
-          initialState: {queries: ['hello', 'hola', 'bonjour', 'buongiorno']},
-          options: {maxLength: 1000},
-        });
-
-        return {
-          position: Array.from(this.host.parentNode!.children).indexOf(
-            this.host
-          ),
-          onInput: () => {},
-          renderItems: () => {
-            const query = searchBoxController.state.value;
-            const hasQuery = query !== '';
-            const max = hasQuery ? this.maxWithQuery : this.maxWithoutQuery;
-            const filteredQueries = recentQueriesList.state.queries
-              .filter((recentQuery) =>
-                recentQuery.toLowerCase().startsWith(query.toLowerCase())
-              )
-              .slice(0, max);
-
-            // TODO: add "clear recent queries element"
-            return filteredQueries.map((value) => ({
-              value,
-              content: (
-                // TODO: add icon when other types of suggestions can appear
-                <span
-                  innerHTML={HighlightUtils.highlightString({
-                    content: value,
-                    openingDelimiter: '<span class="font-bold">',
-                    closingDelimiter: '</span>',
-                    highlights: [
-                      {
-                        offset: query.length,
-                        length: value.length - query.length,
-                      },
-                    ],
-                  })}
-                ></span>
-              ),
-              onClick: () => {
-                // TODO: save state to local storage
-                recentQueriesList.executeRecentQuery(
-                  recentQueriesList.state.queries.indexOf(value)
-                );
-              },
-            }));
-          },
-        };
-      }, this.host);
+      this.initialize();
     } catch (error) {
       this.error = error as Error;
     }
+  }
+
+  private initialize() {
+    dispatchSearchBoxSuggestionsEvent((bindings) => {
+      this.bindings = bindings;
+      this.recentQueriesList = buildRecentQueriesList(bindings.engine, {
+        // TODO: fetch initial state from cookies or local storage
+        initialState: {queries: ['hello', 'hola', 'bonjour', 'buongiorno']},
+        options: {maxLength: 1000},
+      });
+
+      return {
+        position: Array.from(this.host.parentNode!.children).indexOf(this.host),
+        onInput: () => {},
+        renderItems: () => this.renderItems(),
+      };
+    }, this.host);
+  }
+
+  private renderItems() {
+    const query = this.bindings.searchBoxController.state.value;
+    const hasQuery = query !== '';
+    const max = hasQuery ? this.maxWithQuery : this.maxWithoutQuery;
+    const filteredQueries = this.recentQueriesList.state.queries
+      .filter((recentQuery) =>
+        recentQuery.toLowerCase().startsWith(query.toLowerCase())
+      )
+      .slice(0, max);
+
+    // TODO: add "clear recent queries element"
+    return filteredQueries.map((value) => ({
+      value,
+      content: (
+        // TODO: add icon when other types of suggestions can appear
+        <span
+          innerHTML={HighlightUtils.highlightString({
+            content: value,
+            openingDelimiter: '<span class="font-bold">',
+            closingDelimiter: '</span>',
+            highlights: [
+              {
+                offset: query.length,
+                length: value.length - query.length,
+              },
+            ],
+          })}
+        ></span>
+      ),
+      onClick: () => {
+        // TODO: save state to local storage
+        this.recentQueriesList.executeRecentQuery(
+          this.recentQueriesList.state.queries.indexOf(value)
+        );
+      },
+    }));
   }
 
   public render() {
