61f7f137f2986612e6caf95f2cd386cb0e7072ef
Sami Sayegh <ssayegh@coveo.com>
2021-09-29 13:37:10 -0400

    add actions loader
    https://coveord.atlassian.net/browse/KIT-354

---
+++ packages/headless/src/features/dictionary-field-context/dictionary-field-context-actions-loader.ts
@@ -0,0 +1,63 @@
+import {PayloadAction} from '@reduxjs/toolkit';
+import {CoreEngine} from '../..';
+import {dictionaryFieldContext} from '../../app/reducers';
+import {
+  addContext,
+  AddDictionaryFieldContextActionCreatorPayload,
+  removeContext,
+  setContext,
+} from './dictionary-field-context-actions';
+import {DictionaryFieldContextPayload} from './dictionary-field-context-state';
+
+export {AddDictionaryFieldContextActionCreatorPayload};
+
+/**
+ * The dictionary field context action creators.
+ */
+export interface DictionaryFieldContextActionCreators {
+  /**
+   * Adds a new dictionary field context value.
+   *
+   * @param payload - The action creator payload.
+   * @returns A dispatchable action.
+   */
+  addContext(
+    payload: AddDictionaryFieldContextActionCreatorPayload
+  ): PayloadAction<AddDictionaryFieldContextActionCreatorPayload>;
+
+  /**
+   * Removes a dictionary field context field-key pair.
+   *
+   * @param field - The field to remove from the context (e.g., "price").
+   * @returns A dispatchable action.
+   */
+  removeContext(field: string): PayloadAction<string>;
+
+  /**
+   * Sets the dictionary field context.
+   *
+   * @param payload - The action creator payload.
+   * @returns A dispatchable action.
+   */
+  setContext(
+    payload: DictionaryFieldContextPayload
+  ): PayloadAction<DictionaryFieldContextPayload>;
+}
+
+/**
+ * Loads the `context` reducer and returns possible action creators.
+ *
+ * @param engine - The headless engine.
+ * @returns An object holding the action creators.
+ */
+export function loadDictionaryFieldContextActions(
+  engine: CoreEngine
+): DictionaryFieldContextActionCreators {
+  engine.addReducers({dictionaryFieldContext});
+
+  return {
+    addContext,
+    removeContext,
+    setContext,
+  };
+}
