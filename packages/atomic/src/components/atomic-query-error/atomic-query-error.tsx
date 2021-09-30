4ffb9b67e167fdd655e565518d6805933ba5cc15
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-09-30 16:06:58 -0400

    feat(atomic): improve Query Error Component with new illustations
    https://coveord.atlassian.net/browse/KIT-1035

--- packages/atomic/src/components/atomic-query-error/atomic-query-error.tsx
+++ packages/atomic/src/components/atomic-query-error/atomic-query-error.tsx
@@ -6,8 +6,13 @@ import {
   InitializableComponent,
   InitializeBindings,
 } from '../../utils/initialization-utils';
+import {Button} from '../common/button';
+import CannotAccess from '../../images/cannot-access.svg';
+import SomethingWrong from '../../images/something-wrong.svg';
+import SearchInactive from '../../images/search-inactive.svg';
+import NoConnection from '../../images/no-connection.svg';
+import Indexing from '../../images/indexing.svg';
 
-// TODO: Get type from Headless once the API provides more streamlined errors.
 const disconnectedException = 'Disconnected';
 const noEndpointsException = 'NoEndpointsException';
 const invalidTokenException = 'InvalidTokenException';
@@ -16,6 +21,7 @@ const organizationIsPausedException = 'OrganizationIsPausedException';
 /**
  * The `atomic-query-error` component handles fatal errors when performing a query on the index or Search API. When the error is known, it displays a link to relevant documentation link for debugging purposes. When the error is unknown, it displays a small text area with the JSON content of the error.
  *
+ * @part icon - The svg related to the error.
  * @part title - The title of the error.
  * @part description - A description of the error.
  * @part doc-link - A link to the relevant documentation.
@@ -31,21 +37,6 @@ export class AtomicQueryError implements InitializableComponent {
   @InitializeBindings() public bindings!: Bindings;
   public queryError!: QueryError;
 
-  private strings = {
-    disconnectedTitle: () => this.bindings.i18n.t('disconnected'),
-    disconnectedDesc: () => this.bindings.i18n.t('check-your-connection'),
-    noEndpointsTitle: () => this.bindings.i18n.t('no-endpoints'),
-    noEndpointsDesc: () => this.bindings.i18n.t('add-sources'),
-    invalidTokenTitle: () => this.bindings.i18n.t('cannot-access'),
-    invalidTokenDesc: () => this.bindings.i18n.t('invalid-token'),
-    genericErrorTitle: () => this.bindings.i18n.t('something-went-wrong'),
-    genericErrorDesc: () => this.bindings.i18n.t('if-problem-persists'),
-    helpLink: () => this.bindings.i18n.t('coveo-online-help'),
-    moreInfo: () => this.bindings.i18n.t('more-info'),
-    organizationIsPaused: () => this.bindings.i18n.t('organization-is-paused'),
-    organizationWillResume: () =>
-      this.bindings.i18n.t('organization-will-resume'),
-  };
   @BindStateToController('queryError')
   @State()
   private queryErrorState!: QueryErrorState;
@@ -59,62 +50,87 @@ export class AtomicQueryError implements InitializableComponent {
   private renderShowMoreInfo() {
     if (!this.showMoreInfo) {
       return (
-        <button
+        <Button
           part="more-info-btn"
-          class="text-primary"
+          style="primary"
+          class="p-3 mt-8"
           onClick={() => (this.showMoreInfo = true)}
-        >
-          {this.strings.moreInfo()}
-        </button>
+          text={this.bindings.i18n.t('more-info')}
+        ></Button>
       );
     }
 
     return (
       <pre
         part="error-info"
-        class="text-left border border-neutral bg-neutral-light p-3 rounded my-4 whitespace-pre-wrap"
+        class="text-left border border-neutral bg-neutral-light p-3 rounded mt-8 whitespace-pre-wrap"
       >
         <code>{JSON.stringify(this.queryErrorState.error, null, 2)}</code>
       </pre>
     );
   }
 
-  private get title() {
-    switch (this.queryErrorState.error!.type) {
+  private get org() {
+    return this.bindings.engine.state.configuration.organizationId;
+  }
+
+  private get errorType() {
+    return this.queryErrorState.error!.type;
+  }
+
+  private get icon() {
+    switch (this.errorType) {
       case disconnectedException:
-        return this.strings.disconnectedTitle();
+        return NoConnection;
       case noEndpointsException:
-        return this.strings.noEndpointsTitle();
+        return Indexing;
       case invalidTokenException:
-        return this.strings.invalidTokenTitle();
+        return CannotAccess;
       case organizationIsPausedException:
-        return this.strings.organizationWillResume();
+        return SearchInactive;
       default:
-        return this.strings.genericErrorTitle();
+        return SomethingWrong;
+    }
+  }
+
+  private get title() {
+    switch (this.errorType) {
+      case disconnectedException:
+        return this.bindings.i18n.t('disconnected');
+      case noEndpointsException:
+        return this.bindings.i18n.t('no-endpoints', {org: this.org});
+      case invalidTokenException:
+        return this.bindings.i18n.t('cannot-access', {org: this.org});
+      case organizationIsPausedException:
+        return this.bindings.i18n.t('organization-is-paused', {org: this.org});
+      default:
+        return this.bindings.i18n.t('something-went-wrong');
     }
   }
 
   private get description() {
-    switch (this.queryErrorState.error!.type) {
+    switch (this.errorType) {
       case disconnectedException:
-        return this.strings.disconnectedDesc();
+        return this.bindings.i18n.t('check-your-connection');
       case noEndpointsException:
-        return this.strings.noEndpointsDesc();
+        return this.bindings.i18n.t('add-sources');
       case invalidTokenException:
-        return this.strings.invalidTokenDesc();
+        return this.bindings.i18n.t('invalid-token');
       case organizationIsPausedException:
-        return this.strings.organizationIsPaused();
+        return this.bindings.i18n.t('organization-will-resume', {
+          org: this.org,
+        });
       default:
-        return this.strings.genericErrorDesc();
+        return this.bindings.i18n.t('if-problem-persists');
     }
   }
 
   private get link() {
-    switch (this.queryErrorState.error!.type) {
+    switch (this.errorType) {
       case noEndpointsException:
-        return 'https://docs.coveo.com/'; // TODO: update link
+        return 'https://docs.coveo.com/'; // TODO: KIT-1061 update link
       case invalidTokenException:
-        return 'https://docs.coveo.com/'; // TODO: update link
+        return 'https://docs.coveo.com/'; // TODO: KIT-1061 update link
       case organizationIsPausedException:
         return 'https://docs.coveo.com/l6af0467';
       default:
@@ -129,19 +145,24 @@ export class AtomicQueryError implements InitializableComponent {
 
     return (
       <div class="text-center">
-        <h3 part="title" class="text-2xl text-primary my-4">
+        <atomic-icon
+          part="icon"
+          icon={this.icon}
+          class="w-1/2 mt-8"
+        ></atomic-icon>
+        <h3 part="title" class="text-2xl text-on-background mt-8">
           {this.title}
         </h3>
-        <p part="description" class="text-xl text-primary my-4">
+        <p part="description" class="text-lg text-neutral-dark mt-2.5">
           {this.description}
         </p>
         {this.link ? (
           <a
             href={this.link}
             part="doc-link"
-            class="text-primary text-xl hover:underline"
+            class="btn-primary p-3 mt-10 inline-block"
           >
-            {this.strings.helpLink()}
+            {this.bindings.i18n.t('coveo-online-help')}
           </a>
         ) : (
           this.renderShowMoreInfo()
