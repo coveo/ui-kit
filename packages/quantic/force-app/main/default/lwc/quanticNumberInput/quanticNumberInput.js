c69cd33dfbf24e5b3cba48201dbdeadc8a5f47c2
nlafrance-coveo <nlafrance@coveo.com>
2021-10-01 11:36:51 -0400

    feat(quantic): potential filter fix

--- packages/quantic/force-app/main/default/lwc/quanticNumberInput/quanticNumberInput.js
+++ packages/quantic/force-app/main/default/lwc/quanticNumberInput/quanticNumberInput.js
@@ -1,4 +1,4 @@
-import { LightningElement, api } from 'lwc';
+import {LightningElement, api} from 'lwc';
 
 import min from "@salesforce/label/c.quantic_Min";
 import max from "@salesforce/label/c.quantic_Max";
@@ -11,57 +11,96 @@ import {I18nUtils} from 'c/quanticUtils';
 
 
 export default class QuanticNumberInput extends LightningElement {
+  /**@type {number} */
+  @api start;
+  /**@type {number} */
+  @api end;
+  /** @type {string} */
+  @api label;
 
-    /**@type {number} */
-    @api start;
-    /**@type {number} */
-    @api end;
-    /** @type {string} */
-    @api label;
+  minSafeInteger = Number.MIN_SAFE_INTEGER;
+  maxSafeInteger = Number.MAX_SAFE_INTEGER;
 
-    minSafeInteger = Number.MIN_SAFE_INTEGER;
-    maxSafeInteger = Number.MAX_SAFE_INTEGER;
+  labels = {
+    min,
+    max,
+    numberInputMinimum,
+    numberInputMaximum,
+    apply,
+    numberInputApply
+  }
 
-    labels = {
-        min,
-        max,
-        numberInputMinimum,
-        numberInputMaximum,
-        apply,
-        numberInputApply
+  setValidityParameters() {
+    this.inputMin.max = this.maxValue || this.maxSafeInteger;
+    this.inputMax.min = this.minValue || this.minSafeInteger;
+    this.inputMin.required = true;
+    this.inputMax.required = true;
+  }
+
+  resetValidityParameters() {
+    this.inputMin.max = this.maxSafeInteger;
+    this.inputMax.min = this.minSafeInteger;
+    this.inputMin.required = false;
+    this.inputMax.required = false;
+  }
+
+  /**
+  * @param {InputEvent} evt
+  */
+  onApply(evt) {
+    evt.preventDefault();
+
+    this.setValidityParameters();
+
+    const allValid = [...this.template.querySelectorAll('lightning-input')]
+      .reduce((validSoFar, inputCmp) => {
+        return validSoFar && inputCmp.reportValidity();
+      }, true);
+      
+    this.resetValidityParameters();
+
+    if (!allValid) {
+      return;
     }
 
-    /**
-    * @param {InputEvent} evt
-    */
-    onApply(evt) {
-        evt.preventDefault();
-        if(!this.inputMin.validity.valid || !this.inputMax.validity.valid) {
-            return;
+    this.dispatchEvent(new CustomEvent(
+      'apply',
+      {
+        detail: {
+          min: Number(this.minValue),
+          max: Number(this.maxValue)
         }
-        this.dispatchEvent(new CustomEvent('apply', { detail : { min: Number(this.inputMin?.value) , max: Number(this.inputMax?.value) }}));
-    }
+      })
+    );
+  }
 
-    /** @returns {HTMLInputElement} */
-    get inputMin() {
-        return this.template.querySelector('.numeric__input-min');
-    }
+  /** @returns {HTMLInputElement} */
+  get inputMin() {
+    return this.template.querySelector('.numeric__input-min');
+  }
 
-    /** @returns {HTMLInputElement} */
-    get inputMax() {
-        return this.template.querySelector('.numeric__input-max');
-    }
+  /** @returns {HTMLInputElement} */
+  get inputMax() {
+    return this.template.querySelector('.numeric__input-max');
+  }
 
-    get numberInputMinimumLabel() {
-        return I18nUtils.format(this.labels.numberInputMinimum, this.label);
-    }
+  get minValue() {
+    return this.inputMin?.value;
+  }
 
-    get numberInputMaximumLabel() {
-        return I18nUtils.format(this.labels.numberInputMaximum, this.label);
-    }
-    
-    get numberInputApplyLabel() {
-        return I18nUtils.format(this.labels.numberInputApply, this.label);
-    }
+  get maxValue() {
+    return this.inputMax?.value;
+  }
 
+  get numberInputMinimumLabel() {
+    return I18nUtils.format(this.labels.numberInputMinimum, this.label);
+  }
+
+  get numberInputMaximumLabel() {
+    return I18nUtils.format(this.labels.numberInputMaximum, this.label);
+  }
+
+  get numberInputApplyLabel() {
+    return I18nUtils.format(this.labels.numberInputApply, this.label);
+  }
 }
