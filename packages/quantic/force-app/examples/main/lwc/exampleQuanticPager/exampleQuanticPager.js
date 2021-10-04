d24cd41497add06afdb57b2c0aaab088b6435a87
Luc Bergeron <10946843+lbergeron@users.noreply.github.com>
2021-10-04 10:53:16 -0400

    feat(quantic): add pager example component

---
+++ packages/quantic/force-app/examples/main/lwc/exampleQuanticPager/exampleQuanticPager.js
@@ -0,0 +1,23 @@
+import {api, LightningElement, track} from 'lwc';
+
+export default class ExampleQuanticPager extends LightningElement {
+  @api engineId = 'quantic-pager-engine';
+  @track config = {};
+  isConfigured = false;
+
+  pageTitle = 'Quantic Pager';
+  pageDescription = 'The Quantic Pager allows users to navigate the search results using pages.';
+  options = [
+    {
+      attribute: 'numberOfPages',
+      label: 'Number of pages',
+      description: 'The number of pages displayed simultaneously by the pager component.',
+      defaultValue: 5,
+    }
+  ];
+
+  handleTryItNow(evt) {
+    this.config = evt.detail;
+    this.isConfigured = true;
+  }
+}
