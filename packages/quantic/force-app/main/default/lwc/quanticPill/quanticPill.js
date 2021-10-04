03e0ca0eade1541fbe7e2048b4baaa817aeb1e18
nlafrance-coveo <nlafrance@coveo.com>
2021-10-04 08:38:43 -0400

    fix(quantic): added jsdoc comments to pill

--- packages/quantic/force-app/main/default/lwc/quanticPill/quanticPill.js
+++ packages/quantic/force-app/main/default/lwc/quanticPill/quanticPill.js
@@ -1,7 +1,27 @@
 import {LightningElement, api} from 'lwc';
 
-export default class QuanticBreadcrumb extends LightningElement {
-  @api value;
-  @api deselect;
+/**
+ * The `QuanticPill` component is used internally to display a deselectable button.
+ * @example
+ * <c-quantic-pill label="Case" alt-text="Remove Case filter" deselect={myDeselectFunction}></c-quantic-pill>
+ */
+export default class QuanticPill extends LightningElement {
+  /**
+   * The value to display inside the button.
+   * @api
+   * @type {string}
+   */
+  @api label;
+  /**
+   * The alternative text to be assigned to the button icon.
+   * @api
+   * @type {string}
+   */
   @api altText;
+  /**
+   * The function to be executed when the pill is clicked.
+   * @api
+   * @type {Function}
+   */
+  @api deselect;
 }
