b0f4a375dcb742b5103920ad8615b6cc685a69cd
nlafrance-coveo <nlafrance@coveo.com>
2021-10-08 16:22:08 -0400

    feat(quantic): added shimmer

--- packages/quantic/force-app/main/default/lwc/quanticStencil/quanticStencil.js
+++ packages/quantic/force-app/main/default/lwc/quanticStencil/quanticStencil.js
@@ -9,7 +9,29 @@ export default class QuanticStencil extends LightningElement {
   /**
    * The type of stencil to display.
    * @api
-   * @type {string}
+   * @type {'card'|'result'}
    */
   @api variant;
+  /**
+   * Number of rows to render.
+   * @api
+   * @type {number}
+   */
+  @api numberOfRows = 8;
+
+  get rows() {
+    const rows = [];
+    for (let i = 0; i < this.numberOfRows; i++) {
+      rows.push({index: i});
+    }
+    return rows;
+  }
+
+  get isCardVariant() {
+    return this.variant === 'card';
+  }
+
+  get isResultVariant() {
+    return this.variant === 'result';
+  }
 }
