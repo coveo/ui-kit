794cd4679308e0d97295ff6a18ab46f6ffa9d201
Benjamin Taillon <btaillon@coveo.com>
2021-10-12 08:09:40 -0400

    feat(atomic): added sections support to result tables
    https://coveord.atlassian.net/browse/KIT-1053

--- packages/atomic/src/components/atomic-table-result/atomic-table-cell.tsx
+++ packages/atomic/src/components/atomic-table-result/atomic-table-cell.tsx
@@ -1,11 +1,18 @@
 import {Component, h, Listen, Prop} from '@stencil/core';
 import {Result} from '@coveo/headless';
+import {
+  getResultDisplayClasses,
+  ResultDisplayDensity,
+  ResultDisplayImageSize,
+  ResultDisplayLayout,
+} from '../atomic-result/atomic-result-display-options';
 
 /**
  * The `atomic-table-cell` component is used internally by the `atomic-result-list` component.
  */
 @Component({
   tag: 'atomic-table-cell',
+  styleUrl: 'atomic-result-cell.pcss',
   shadow: true,
 })
 export class AtomicTableCell {
@@ -19,6 +26,26 @@ export class AtomicTableCell {
    */
   @Prop() content!: string;
 
+  /**
+   * Whether this result should use `atomic-result-section-*` components.
+   */
+  @Prop() useSections = true;
+
+  /**
+   * How results should be displayed.
+   */
+  @Prop() display: ResultDisplayLayout = 'list';
+
+  /**
+   * How large or small results should be.
+   */
+  @Prop() density: ResultDisplayDensity = 'normal';
+
+  /**
+   * How large or small the visual section of results should be.
+   */
+  @Prop() image: ResultDisplayImageSize = 'icon';
+
   @Listen('atomic/resolveResult')
   public resolveResult(event: CustomEvent) {
     event.preventDefault();
@@ -26,7 +53,24 @@ export class AtomicTableCell {
     event.detail(this.result);
   }
 
+  private getClasses() {
+    const classes = getResultDisplayClasses(
+      this.display,
+      this.density,
+      this.image
+    );
+    if (this.useSections) {
+      classes.push('with-sections');
+    }
+    return classes;
+  }
+
   public render() {
-    return <div innerHTML={this.content}></div>;
+    return (
+      <div
+        class={`cell-root ${this.getClasses().join(' ')}`}
+        innerHTML={this.content}
+      ></div>
+    );
   }
 }
---
+++ packages/atomic/src/utils/result-section-utils.ts
@@ -0,0 +1,16 @@
+const resultSectionTags = [
+  'atomic-result-section-visual',
+  'atomic-result-section-badges',
+  'atomic-result-section-actions',
+  'atomic-result-section-title',
+  'atomic-result-section-title-metadata',
+  'atomic-result-section-emphasized',
+  'atomic-result-section-excerpt',
+  'atomic-result-section-bottom-metadata',
+];
+
+export function containsSection(element: ParentNode) {
+  return Array.from(element.children).some((child) =>
+    resultSectionTags.includes(child.tagName.toLowerCase())
+  );
+}
