be64989692fdf033163d697fcaab6c3993442ddc
Nathan Lafrance-Berger <nlafrance@coveo.com>
2021-10-15 09:27:13 -0400

    fix(quantic): fixed sort value not being updating and added FT (#1336)
    * feat(quantic): added missing unsubscribe
    
    * feat(quantic): fixed sort
    
    * feat(quantic): started adding FT
    
    * feat(quantic): set sort value
    
    * feat(quantic): tests without running them yolo
    
    * feat(quantic): tests are working
    
    * feat(quantic): updated community
    
    * feat(quantic): relevance -> relevancy
    
    * feat(quantic): improved tests

--- packages/quantic/cypress/integration/search-expectations.ts
+++ packages/quantic/cypress/integration/search-expectations.ts
@@ -1,6 +1,12 @@
 import {InterceptAliases} from '../page-objects/search';
 
 export const SearchExpectations = {
+  sortedBy: (sortCriteria: string) => {
+    cy.wait(InterceptAliases.Search).then((interception) => {
+      expect(interception.request?.body?.sortCriteria).to.equal(sortCriteria);
+    });
+  },
+
   numberOfResults: (value: number) => {
     cy.wait(InterceptAliases.Search).then((interception) =>
       expect(interception.response?.body.results.length).to.equal(
