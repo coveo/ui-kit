d08790849fb37d0d812965ed908ecb5dd834aa96
btaillon <54454747+btaillon@users.noreply.github.com>
2021-10-18 10:40:14 -0400

    fix(atomic): made printable uri not overflow horizontally (#1333)
    https://coveord.atlassian.net/browse/KIT-1043

--- packages/atomic/src/components/result-template-components/atomic-result-printable-uri/atomic-result-printable-uri.tsx
+++ packages/atomic/src/components/result-template-components/atomic-result-printable-uri/atomic-result-printable-uri.tsx
@@ -136,6 +136,7 @@ export class AtomicResultPrintableUri {
       <LinkWithResultAnalytics
         interactiveResult={this.interactiveResult}
         href={uri}
+        title={typeof content === 'string' ? content : undefined}
         part={'result-printable-uri-link'}
         target={this.target}
       >
