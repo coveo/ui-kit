f40e538fbc77be8d4960276d441cedd64d6e3b31
Sami Sayegh <ssayegh@coveo.com>
2021-10-18 12:12:59 -0400

    add expression builder
    https://coveord.atlassian.net/browse/KIT-1131

---
+++ packages/headless/src/utils/expression-builder.ts
@@ -0,0 +1,30 @@
+function createExpressionBuilder() {
+  const parts: string[] = [];
+
+  return {
+    addStringFieldExpression(
+      field: string,
+      operator: '==' | '!=',
+      values: string[]
+    ) {
+      const stringValues = values.map((v) => `"${v}"`).join(',');
+      const expression = `${field}==(${stringValues})`;
+      const prefix = operator === '!=' ? 'NOT ' : '';
+
+      parts.push(`${prefix}${expression}`);
+    },
+
+    join(delimiter: 'AND' | 'OR') {
+      const concatenated = parts.join(`) ${delimiter} (`);
+      return `(${concatenated})`;
+    },
+  };
+}
+
+const builder = createExpressionBuilder();
+builder.addStringFieldExpression('filetype', '==', ['pdf']);
+builder.addStringFieldExpression('author', '==', ['alice', 'bob']);
+const expression = builder.join('AND');
+
+console.log(expression);
+// (filetype==("pdf")) AND (author==("alice","bob"))
