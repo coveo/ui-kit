e5973708fe7c3497ede6c377f1e013395299f2fe
Jean-Félix Thibodeau <ThibodeauJF@users.noreply.github.com>
2021-10-20 09:48:46 -0400

    Update criteria-parser.test.ts

--- packages/headless/src/features/sort-criteria/criteria-parser.test.ts
+++ packages/headless/src/features/sort-criteria/criteria-parser.test.ts
@@ -58,7 +58,7 @@ describe('#parseCriterionExpression', () => {
 
   it('when SortByField criterion has no order, should throw', () => {
     expect(() => parseCriterionExpression('size')).toThrowError(
-      'An order (i.e., ascending or descending) should be specified for a sort criterion sorted by "date"'
+      'An order (i.e., ascending or descending) should be specified for a sort criterion sorted by a field'
     );
   });
 
