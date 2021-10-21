92cbef639d20eb84eb6c40892a84c7719a5ee828
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-10-21 15:08:50 -0400

    handle entrypoints
    https://coveord.atlassian.net/browse/KIT-1099

--- packages/headless/src/api/search/date/date-format.test.ts
+++ packages/headless/src/api/search/date/date-format.test.ts
@@ -30,7 +30,9 @@ describe('#validateAbsoluteDate', () => {
   });
 
   it('should throw for an invalid date', () => {
-    expect(() => validateAbsoluteDate('hi')).toThrowError('Date is invalid');
+    expect(() => validateAbsoluteDate('hi')).toThrowError(
+      'Could not parse the provided date'
+    );
   });
 
   it('should throw for a valid date earlier than 1401', () => {
--- packages/headless/src/api/search/date/date-format.ts
+++ packages/headless/src/api/search/date/date-format.ts
@@ -2,8 +2,10 @@ import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 dayjs.extend(customParseFormat);
 
-const API_DATE_FORMAT = 'YYYY/MM/DD@HH:mm:ss';
+export const API_DATE_FORMAT = 'YYYY/MM/DD@HH:mm:ss';
 export const API_DATE_MINIMUM = '1401-01-01';
+
+export type AbsoluteDate = string | number | Date;
 
 export function formatDateForSearchApi(date: dayjs.Dayjs) {
   return date.format(API_DATE_FORMAT);
@@ -13,11 +15,18 @@ export function isSearchApiDate(date: string) {
   return formatDateForSearchApi(dayjs(date)) === date;
 }
 
-export function validateAbsoluteDate(date: string | number | Date) {
-  const dayJSDate = dayjs(date);
+export function validateAbsoluteDate(date: AbsoluteDate, dateFormat?: string) {
+  const dayJSDate = dayjs(date, dateFormat);
 
   if (!dayJSDate.isValid()) {
-    throw new Error(`Date is invalid: ${date}`);
+    const provideFormat =
+      '. Please provide a date format string in the configuration options. See https://day.js.org/docs/en/parse/string-format for more information.';
+    const withFormat = ` with the format "${dateFormat}""`;
+    throw new Error(
+      `Could not parse the provided date "${date}"${
+        dateFormat ? withFormat : provideFormat
+      }`
+    );
   }
 
   if (dayJSDate.isBefore(API_DATE_MINIMUM)) {
