4119699e3e19b523df5f3734f8be6f50c8bdabd5
Luc Bergeron <10946843+lbergeron@users.noreply.github.com>
2021-10-21 16:42:22 -0400

    chore(quantic): remove unused code

--- packages/quantic/cypress/support/commands.ts
+++ packages/quantic/cypress/support/commands.ts
@@ -1,17 +1,3 @@
 import {registerDetailedReporterCommands} from '../reporters/detailed-collector';
 
 registerDetailedReporterCommands();
-
-// The command below are not used anymore
-
-Cypress.Commands.add('lwcDevCheck', {prevSubject: 'element'}, (element) => {
-  cy.wrap(element).trigger('change', {value: true, force: true});
-});
-
-Cypress.Commands.add('lwcDevUncheck', {prevSubject: 'element'}, (element) => {
-  cy.wrap(element).trigger('change', {value: false, force: true});
-});
-
-Cypress.Commands.add('lwcDevClick', {prevSubject: 'element'}, (element) => {
-  cy.wrap(element).trigger('click', {force: true});
-});
