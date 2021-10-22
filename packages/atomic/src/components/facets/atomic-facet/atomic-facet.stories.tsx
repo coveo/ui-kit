52f91c6308c7f49836045680b05b33505d13f90f
olamothe <olamothe@coveo.com>
2021-10-22 12:35:13 -0400

    docs(atomic): map stencil documentation props to storybook args
    https://coveord.atlassian.net/browse/KIT-1145

--- packages/atomic/src/components/facets/atomic-facet/atomic-facet.stories.tsx
+++ packages/atomic/src/components/facets/atomic-facet/atomic-facet.stories.tsx
@@ -1,8 +1,11 @@
 import {h} from '@stencil/core';
 import FacetDoc from './atomic-facet.mdx';
+import {Args} from '@storybook/api';
+import {mapPropsToArgTypes} from '../../../../.storybook/map-props-to-args';
 
 export default {
   title: 'Atomic/Facet',
+  argTypes: mapPropsToArgTypes('atomic-facet'),
   parameters: {
     docs: {
       page: FacetDoc,
@@ -10,6 +13,11 @@ export default {
   },
 };
 
-export const TheDefaultFacet = () => {
-  return <div>This is where we need to define the story for a facet</div>;
+export const TheDefaultFacet = (args: Args) => {
+  return (
+    <div>
+      <div>This is where we need to define the story for a facet</div>
+      <code>Current args: {JSON.stringify(args)}</code>
+    </div>
+  );
 };
