cc5cd0864737eb84c9741b4b0e482d08a203273a
Sami Sayegh <ssayegh@coveo.com>
2021-10-22 16:25:19 -0400

    remove comment
    https://coveord.atlassian.net/browse/KIT-1131

--- packages/headless/src/utils/expression-builder.ts
+++ packages/headless/src/utils/expression-builder.ts
@@ -239,34 +239,3 @@ function getOperatorSymbol(operator: Operator) {
 
   return '';
 }
-
-// function createExpressionBuilder(expression = '') {
-//   const parts = expression ? [expression] : [];
-
-//   return {
-//     addStringFieldExpression(
-//       field: string,
-//       operator: '==' | '!=',
-//       values: string[]
-//     ) {
-//       const stringValues = values.map((v) => `"${v}"`).join(',');
-//       const expression = `${field}==(${stringValues})`;
-//       const prefix = operator === '!=' ? 'NOT ' : '';
-
-//       parts.push(`${prefix}${expression}`);
-//     },
-
-//     join(delimiter: 'AND' | 'OR') {
-//       const concatenated = parts.join(`) ${delimiter} (`);
-//       return `(${concatenated})`;
-//     },
-//   };
-// }
-
-// const builder = createExpressionBuilder();
-// builder.addStringFieldExpression('filetype', '==', ['pdf']);
-// builder.addStringFieldExpression('author', '!=', ['alice', 'bob']);
-// const expression = builder.join('AND');
-
-// console.log(expression);
-// (filetype==("pdf")) AND (NOT author==("alice","bob"))
