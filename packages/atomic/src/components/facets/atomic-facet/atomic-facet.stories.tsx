2dd0466a99fb19f5b86af170baca85b0980a0c57
olamothe <olamothe@coveo.com>
2021-10-22 10:32:25 -0400

    wip
    https://coveord.atlassian.net/browse/KIT-1144

---
+++ packages/atomic/src/components/facets/atomic-facet/atomic-facet.stories.tsx
@@ -0,0 +1,9 @@
+import {h} from '@stencil/core';
+
+export const Default = () => {
+  return (
+    <div>
+      This is where we need to define the story (playground) for a facet
+    </div>
+  );
+};
