7d420d46447f2423d5b16ba4cf85206e6f9b99d7
Sami Sayegh <ssayegh@coveo.com>
2021-10-22 17:34:57 -0400

    remove duplicate tests
    https://coveord.atlassian.net/browse/KIT-1131

--- packages/headless/src/utils/expression-builder/expression-builder.test.ts
+++ packages/headless/src/utils/expression-builder/expression-builder.test.ts
@@ -25,7 +25,7 @@ describe('createExpressionBuilder', () => {
   });
 
   describe('#addNumericField', () => {
-    it('#greaterThan operator, #toString returns the expected syntax', () => {
+    it('with one expression, #toString returns the expected syntax', () => {
       builder.addNumericField({
         field: 'size',
         operator: 'greaterThan',
@@ -33,48 +33,6 @@ describe('createExpressionBuilder', () => {
       });
 
       expect(builder.toString()).toBe('@size>10');
-    });
-
-    it('#greaterThanOrEqual operator, #toString returns the expected syntax', () => {
-      builder.addNumericField({
-        field: 'size',
-        operator: 'greaterThanOrEqual',
-        value: 10,
-      });
-
-      expect(builder.toString()).toBe('@size>=10');
-    });
-
-    it('#lowerThan operator, #toString returns the expected syntax', () => {
-      builder.addNumericField({
-        field: 'size',
-        operator: 'lowerThan',
-        value: 10,
-      });
-
-      expect(builder.toString()).toBe('@size<10');
-    });
-
-    it('#lowerThanOrEqual operator, #toString returns the expected syntax', () => {
-      builder.addNumericField({
-        field: 'size',
-        operator: 'lowerThanOrEqual',
-        value: 10,
-      });
-
-      expect(builder.toString()).toBe('@size<=10');
-    });
-
-    it(`#negate set to true,
-    #toString returns the expected syntax`, () => {
-      builder.addNumericField({
-        field: 'size',
-        operator: 'lowerThanOrEqual',
-        value: 10,
-        negate: true,
-      });
-
-      expect(builder.toString()).toBe('NOT @size<=10');
     });
   });
 });
