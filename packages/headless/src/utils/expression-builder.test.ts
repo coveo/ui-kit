dc592b28e45de118f2a9ff57a59236b90d4cf7d8
Sami Sayegh <ssayegh@coveo.com>
2021-10-22 11:37:51 -0400

    group string field tests
    https://coveord.atlassian.net/browse/KIT-1131

--- packages/headless/src/utils/expression-builder.test.ts
+++ packages/headless/src/utils/expression-builder.test.ts
@@ -6,43 +6,45 @@ describe('createExpressionBuilder', () => {
     expect(builder.toString()).toBe('');
   });
 
-  it(`#contains operator, one keyword value,
-  #toString returns the expected syntax`, () => {
-    const builder = createExpressionBuilder({
-      delimiter: 'and',
-    }).addStringFieldExpression({
-      field: 'author',
-      operator: 'contains',
-      values: ['al'],
+  describe('#addStringFieldExpression', () => {
+    it(`#contains operator, one keyword value,
+    #toString returns the expected syntax`, () => {
+      const builder = createExpressionBuilder({
+        delimiter: 'and',
+      }).addStringFieldExpression({
+        field: 'author',
+        operator: 'contains',
+        values: ['al'],
+      });
+
+      expect(builder.toString()).toBe('@author="al"');
     });
 
-    expect(builder.toString()).toBe('@author="al"');
-  });
+    it(`#isExactly operator, one exactMatch value,
+    #toString returns the expected syntax`, () => {
+      const builder = createExpressionBuilder({
+        delimiter: 'and',
+      }).addStringFieldExpression({
+        field: 'author',
+        operator: 'isExactly',
+        values: ['alice'],
+      });
 
-  it(`#isExactly operator, one exactMatch value,
-  #toString returns the expected syntax`, () => {
-    const builder = createExpressionBuilder({
-      delimiter: 'and',
-    }).addStringFieldExpression({
-      field: 'author',
-      operator: 'isExactly',
-      values: ['alice'],
+      expect(builder.toString()).toBe('@author=="alice"');
     });
 
-    expect(builder.toString()).toBe('@author=="alice"');
-  });
+    it('#contains operator with multiple values', () => {
+      const builder = createExpressionBuilder({
+        delimiter: 'and',
+      });
 
-  it('#contains operator with multiple values', () => {
-    const builder = createExpressionBuilder({
-      delimiter: 'and',
+      builder.addStringFieldExpression({
+        field: 'author',
+        operator: 'contains',
+        values: ['al', 'alice'],
+      });
+
+      expect(builder.toString()).toBe('@author=("al","alice")');
     });
-
-    builder.addStringFieldExpression({
-      field: 'author',
-      operator: 'contains',
-      values: ['al', 'alice'],
-    });
-
-    expect(builder.toString()).toBe('@author=("al","alice")');
   });
 });
