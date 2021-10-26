b3fea8b4c1f1cc6a75b0d5361e9207a81299e5b9
Asma Elfaleh <aelfaleh@coveo.com>
2021-10-26 16:30:49 -0400

    test(quantic): Add example quantic numeric facet

---
+++ packages/quantic/force-app/examples/main/lwc/exampleQuanticNumericFacet/exampleQuanticNumericFacet.js
@@ -0,0 +1,59 @@
+import {api, LightningElement, track} from 'lwc';
+
+export default class ExampleQuanticNumericFacet extends LightningElement {
+    @api engineId = 'quantic-numeric-facet-engine';
+    @track config = {};
+    isConfigured = false;
+
+    pageTitle = 'Quantic Numeric Facet';
+    pageDescription = 'The Quantic Numeric Facet displays facet values as numeric ranges.'
+    options = [
+        {
+            attribute: 'field',
+            label: 'Field',
+            description: 'The name of the field to display as a facet.',
+            defaultValue: 'ytlikecount'
+        },
+        {
+            attribute: 'label',
+            label: 'Label',
+            description: 'The label to use as the facet title.',
+            defaultValue: 'Youtube Likes'
+        },
+        {
+            attribute: 'numberOfValues',
+            label: 'Number of values',
+            description: 'The number of values displayed by the facet.',
+            defaultValue: 8
+        },
+        {
+            attribute: 'sortCriteria',
+            label: 'Sort criteria',
+            description: 'The sorting applied to the facet. Possible values are: ascending, descending',
+            defaultValue: 'ascending'
+        },
+        {
+            attribute: 'rangeAlgorithm',
+            label: 'Range algorithm',
+            description: 'The algorithm used for generating the ranges of this facet when they aren’t manually defined. Possible values are: even, equiprobable',
+            defaultValue: 'equiprobable'
+        },
+        {
+            attribute: 'withInput',
+            label: 'With input',
+            description: 'Whether this facet should contain an input allowing users to set custom ranges.',
+            defaultValue: null
+        },
+        {
+            attribute: 'isCollapsed',
+            label: 'Is Collapsed',
+            description: 'Whether to collapse the facet.',
+            defaultValue: false
+        }
+    ]
+
+    handleTryItNow(evt) {
+        this.config = evt.detail;
+        this.isConfigured = true;
+    }
+}
