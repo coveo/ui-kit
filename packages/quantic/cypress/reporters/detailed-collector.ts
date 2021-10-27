43fed34b7684a383372839ce299e11b48846ab3c
Luc Bergeron <10946843+lbergeron@users.noreply.github.com>
2021-10-27 16:29:59 -0400

    feat(quantic): add detailed reporter for Cypress tests (#1372)
    https://coveord.atlassian.net/browse/SFINT-4157

---
+++ packages/quantic/cypress/reporters/detailed-collector.ts
@@ -0,0 +1,130 @@
+import {platform} from 'os';
+import * as path from 'path';
+import {Socket} from 'net';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      logDetail: typeof logDetail;
+    }
+  }
+}
+
+export interface Message {
+  type: string;
+  content: string;
+}
+
+export interface DetailedCollector {
+  beginScope(title: string): void;
+  endScope(): void;
+  expectation(message: string): void;
+}
+
+class DetailedCollectorImpl implements DetailedCollector {
+  static _instance: DetailedCollector;
+  private _isWindows: boolean;
+
+  constructor() {
+    this._isWindows = platform() === 'win32';
+  }
+
+  beginScope(title: string) {
+    this.sendToReporter({
+      type: 'scope:begin',
+      content: title,
+    });
+  }
+
+  endScope() {
+    this.sendToReporter({
+      type: 'scope:end',
+      content: '',
+    });
+  }
+
+  expectation(message: string) {
+    this.sendToReporter({
+      type: 'expectation',
+      content: message,
+    });
+  }
+
+  sendToReporter(data: Message) {
+    const client = new Socket();
+    client.connect(this.getServerPath(), () => {
+      client.write(JSON.stringify(data));
+      client.destroy();
+    });
+  }
+
+  private getServerPath() {
+    // on Windows, the server path must be a named pipe.
+    // on other platforms, a file path is used to open a local socket.
+    return this._isWindows
+      ? '\\\\.\\pipe\\detailed-reporter'
+      : path.join(process.cwd(), 'ipc.sock');
+  }
+
+  static getInstance() {
+    if (!DetailedCollectorImpl._instance) {
+      DetailedCollectorImpl._instance = new DetailedCollectorImpl();
+    }
+
+    return DetailedCollectorImpl._instance;
+  }
+}
+
+class VoidDetailedCollector implements DetailedCollector {
+  beginScope(title: string) {}
+  endScope() {}
+  expectation(message: string) {}
+}
+
+export function getCollector(enable: boolean): DetailedCollector {
+  return enable
+    ? DetailedCollectorImpl.getInstance()
+    : new VoidDetailedCollector();
+}
+
+export function registerDetailedReporterPlugin(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+) {
+  const enable = path.basename(config.reporter) === 'detailed-reporter.js';
+  const collector = getCollector(enable);
+
+  on('task', {
+    detailedScopeBegin(title) {
+      collector.beginScope(title);
+      return null;
+    },
+    detailedScopeEnd() {
+      collector.endScope();
+      return null;
+    },
+    detailedExpectation(message: string) {
+      collector.expectation(message);
+      return null;
+    },
+  });
+}
+
+export function logDetail(message: string) {
+  cy.task('detailedExpectation', message);
+}
+
+export function registerDetailedReporterCommands() {
+  Cypress.Commands.add('logDetail', logDetail);
+}
+
+export function scope(title: string, delegate: () => void): void {
+  try {
+    cy.task('detailedScopeBegin', title);
+
+    delegate();
+  } finally {
+    cy.task('detailedScopeEnd');
+  }
+}
