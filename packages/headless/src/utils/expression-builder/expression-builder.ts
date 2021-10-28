55be2ec5e246995fed3e7bff3f29a5593ad77bfa
Sami Sayegh <ssayegh@coveo.com>
2021-10-28 16:21:11 -0400

    document remaining expression builder methods
    https://coveord.atlassian.net/browse/KIT-1131

--- packages/headless/src/utils/expression-builder/expression-builder.ts
+++ packages/headless/src/utils/expression-builder/expression-builder.ts
@@ -36,21 +36,107 @@ import {
  * A utility to help build query expressions.
  */
 export interface ExpressionBuilder {
+  /**
+   * Adds an expression to match. Terms can be in any order, and may also be expanded with stemming.
+   *
+   * @param expression - A keyword expression.
+   * @returns The `ExpressionBuilder` instance.
+   */
   addKeyword(expression: KeywordExpression): ExpressionBuilder;
+
+  /**
+   * Adds an expression that returns all of the items in which the specified `startTerm` appears no more than `maxKeywordsBetween` from the endTerm, for each element in `otherTerms`.
+   *
+   * @param expression - A near expression.
+   * @returns The `ExpressionBuilder` instance.
+   */
   addNear(expression: NearExpression): ExpressionBuilder;
+
+  /**
+   * Adds an expression that must appear in its entirety at least once in an item for that item to be returned.
+   *
+   * @param expression - An exact match expression.
+   * @returns The `ExpressionBuilder` instance.
+   */
   addExactMatch(expression: KeywordExpression): ExpressionBuilder;
+
+  /**
+   * Adds an expression returning all items where the defined field exists.
+   *
+   * @param expression - A field exists expressions.
+   * @returns The `ExpressionBuilder` instance.
+   */
   addFieldExists(expression: FieldExistsExpression): ExpressionBuilder;
+
+  /**
+   * Adds an expression that uses an `operator` to compare a string `field` to a `value`.
+   * Returns all of the items for which the expression evaluates to true.
+   *
+   * @param expression - A string field expression.
+   * @returns The `ExpressionBuilder` instance.
+   */
   addStringField(expression: StringFieldExpression): ExpressionBuilder;
+
+  /**
+   * Adds an expression that uses an `operator` to compare a string facet `field` to a `value`.
+   * Returns all of the items for which the expression evaluates to true.
+   *
+   * @param expression - A string facet field expression.
+   * @returns The `ExpressionBuilder` instance.
+   */
   addStringFacetField(
     expression: StringFacetFieldExpression
   ): ExpressionBuilder;
+
+  /**
+   * Adds an expression that uses an `operator` to compare a numeric `field` to a `value`.
+   * Returns all of the items for which the expression evaluates to true.
+   *
+   * @param expression - A numeric field expression.
+   * @returns The `ExpressionBuilder` instance.
+   */
   addNumericField(expression: NumericFieldExpression): ExpressionBuilder;
+
+  /**
+   * Adds an expression that returns all items for which the `value` of the numeric `field` is within the defined range.
+   *
+   * @param expression - A numeric field expression.
+   * @returns The `ExpressionBuilder` instance.
+   */
   addNumericRangeField(
     expression: NumericRangeFieldExpression
   ): ExpressionBuilder;
+
+  /**
+   * Adds an expression that uses an `operator` to compare a date `field` to a `value`.
+   * Returns all of the items for which the expression evaluates to true.
+   *
+   * @param expression - A date field expression.
+   * @returns The `ExpressionBuilder` instance.
+   */
   addDateField(expression: DateFieldExpression): ExpressionBuilder;
+
+  /**
+   * Adds an expression that returns all items for which the `value` of the date `field` is within the defined range.
+   *
+   * @param expression - A numeric field expression.
+   * @returns The `ExpressionBuilder` instance.
+   */
   addDateRangeField(expression: DateRangeFieldExpression): ExpressionBuilder;
+
+  /**
+   * Adds an expression that invokes a query extension.
+   *
+   * @param expression - A query extension expression.
+   * @returns The `ExpressionBuilder` instance.
+   */
   addQueryExtension(expression: QueryExtensionExpression): ExpressionBuilder;
+
+  /**
+   * Joins all expressions using the configured boolean operator.
+   *
+   * @returns A string representation of the configured expressions.
+   */
   toString(): string;
 }
 
