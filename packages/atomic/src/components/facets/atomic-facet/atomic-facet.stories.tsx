049aa85e1da9d5bac751dc959d07ce033315defd
olamothe <olamothe@coveo.com>
2021-11-01 11:27:17 -0400

    docs(atomic): improve storybook documentation for facets components
    https://coveord.atlassian.net/browse/KIT-1166

--- packages/atomic/src/components/facets/atomic-facet/atomic-facet.stories.tsx
+++ packages/atomic/src/components/facets/atomic-facet/atomic-facet.stories.tsx
@@ -1,3 +1,4 @@
+import {html} from 'lit-html';
 import defaultStory from '../../../../.storybook/default-story';
 import FacetDoc from './atomic-facet.mdx';
 
@@ -5,7 +6,15 @@ const {defaultModuleExport, exportedStory} = defaultStory(
   'Atomic/Facet',
   'atomic-facet',
   {field: 'objecttype'},
-  FacetDoc
+  FacetDoc,
+  {
+    additionalMarkup: () => html`<style>
+      atomic-facet {
+        max-width: 500px;
+        margin: auto;
+      }
+    </style>`,
+  }
 );
 
 export default defaultModuleExport;
