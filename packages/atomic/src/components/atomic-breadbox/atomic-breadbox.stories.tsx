4f66a07b05de26e77d06c734a190d40942c72ba0
olamothe <olamothe@coveo.com>
2021-11-01 11:52:42 -0400

    fix linting
    https://coveord.atlassian.net/browse/KIT-1160

--- packages/atomic/src/components/atomic-breadbox/atomic-breadbox.stories.tsx
+++ packages/atomic/src/components/atomic-breadbox/atomic-breadbox.stories.tsx
@@ -10,12 +10,26 @@ const {defaultModuleExport, exportedStory} = defaultStory(
   {
     additionalMarkup: () =>
       html`
-      <div style="margin:20px 0">Select facet value(s) to see the Breadbox component.</div>
-      <div style="display: flex; justify-content: flex-start;"> 
-        <atomic-facet field="objecttype" style="flex-grow:1" label="Object type"></atomic-facet>
-        <atomic-facet field="filetype" style="flex-grow:1" label="File type"></atomic-facet>
-        <atomic-facet field="source" style="flex-grow:1" label="Source"></atomic-facet>
-      </div>
+        <div style="margin:20px 0">
+          Select facet value(s) to see the Breadbox component.
+        </div>
+        <div style="display: flex; justify-content: flex-start;">
+          <atomic-facet
+            field="objecttype"
+            style="flex-grow:1"
+            label="Object type"
+          ></atomic-facet>
+          <atomic-facet
+            field="filetype"
+            style="flex-grow:1"
+            label="File type"
+          ></atomic-facet>
+          <atomic-facet
+            field="source"
+            style="flex-grow:1"
+            label="Source"
+          ></atomic-facet>
+        </div>
       `,
   }
 );
