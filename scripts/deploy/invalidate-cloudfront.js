46c1165b3063c14ca6bbb38ba10cff717e604b67
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2021-11-02 16:45:25 -0400

    retrieve major version
    https://coveord.atlassian.net/browse/KIT-1171

--- scripts/deploy/invalidate-cloudfront.js
+++ scripts/deploy/invalidate-cloudfront.js
@@ -1,37 +1,47 @@
 const AWS = require('aws-sdk');
 
 const cloudfront = new AWS.CloudFront();
-const pathsToInvalidate = [
-  '/atomic/latest/*',
-  '/atomic/v1/*',
-  '/headless/latest/*',
-  '/headless/v1/*',
-];
 
-const invalidationRequest = cloudfront.createInvalidation({
-  DistributionId: 'E2VWLFSCSD1GLA',
-  InvalidationBatch: {
-    CallerReference: new Date().getTime().toString(),
-    Paths: {
-      Quantity: 1,
-      Items: pathsToInvalidate,
+async function getMajorVersion(dir) {
+  const {version} = await require(`../../packages/${dir}/package.json`);
+  return version.split('.')[0];
+}
+
+async function main() {
+  const pathsToInvalidate = [
+    '/atomic/latest/*',
+    `/atomic/v${await getMajorVersion('atomic')}/*`,
+    '/headless/latest/*',
+    `/headless/v${await getMajorVersion('headless')}/*`,
+  ];
+
+  const invalidationRequest = cloudfront.createInvalidation({
+    DistributionId: 'E2VWLFSCSD1GLA',
+    InvalidationBatch: {
+      CallerReference: new Date().getTime().toString(),
+      Paths: {
+        Quantity: 1,
+        Items: pathsToInvalidate,
+      },
     },
-  },
-});
+  });
 
-invalidationRequest.send((error, success) => {
-  if (error) {
-    console.log('ERROR WHILE INVALIDATING RESSOURCES ON CLOUDFRONT');
-    console.log('*************');
-    console.log(error.message);
-    console.log('*************');
-    throw error;
-  }
-  if (success) {
-    console.log('INVALIDATION ON CLOUDFRONT SUCCESSFUL');
-    console.log('*************');
-    console.log(`PATHS INVALIDATED: ${pathsToInvalidate}`);
-    console.log(`INVALIDATION ID : ${success.Invalidation.Id}`);
-    console.log('*************');
-  }
-});
+  invalidationRequest.send((error, success) => {
+    if (error) {
+      console.log('ERROR WHILE INVALIDATING RESSOURCES ON CLOUDFRONT');
+      console.log('*************');
+      console.log(error.message);
+      console.log('*************');
+      throw error;
+    }
+    if (success) {
+      console.log('INVALIDATION ON CLOUDFRONT SUCCESSFUL');
+      console.log('*************');
+      console.log(`PATHS INVALIDATED: ${pathsToInvalidate}`);
+      console.log(`INVALIDATION ID : ${success.Invalidation.Id}`);
+      console.log('*************');
+    }
+  });
+}
+
+main();
