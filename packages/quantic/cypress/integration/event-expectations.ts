8c05fcc2eff9b9e783acd706bf3572c320f9a8ce
nlafrance-coveo <nlafrance@coveo.com>
2021-11-04 08:47:41 -0400

    feat(quantic): added missing log from event expectation

--- packages/quantic/cypress/integration/event-expectations.ts
+++ packages/quantic/cypress/integration/event-expectations.ts
@@ -2,7 +2,9 @@ import {EventSelectors} from './event-selectors';
 
 export const EventExpectations = {
   receivedEvent: (received: boolean, eventType: string) =>
-    EventSelectors[
-      received ? 'eventsReceived' : 'eventsNotReceived'
-    ]().contains(eventType),
+    EventSelectors[received ? 'eventsReceived' : 'eventsNotReceived']()
+      .contains(eventType)
+      .logDetail(
+        `should${received ? '' : ' not'} dispatch the ${eventType} event`
+      ),
 };
