cb4427f2232f21ee2aab1a8d32296012bce160e7
nlafrance-coveo <nlafrance@coveo.com>
2021-11-05 09:02:21 -0400

    feat(quantic): using should function

--- packages/quantic/cypress/integration/event-expectations.ts
+++ packages/quantic/cypress/integration/event-expectations.ts
@@ -1,10 +1,9 @@
+import {should} from './common-selectors';
 import {EventSelectors} from './event-selectors';
 
 export const EventExpectations = {
   receivedEvent: (received: boolean, eventType: string) =>
     EventSelectors[received ? 'eventsReceived' : 'eventsNotReceived']()
       .contains(eventType)
-      .logDetail(
-        `should${received ? '' : ' not'} dispatch the ${eventType} event`
-      ),
+      .logDetail(`${should(received)} dispatch the ${eventType} event`),
 };
