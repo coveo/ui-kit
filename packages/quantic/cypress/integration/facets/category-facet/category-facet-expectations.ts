abb8fd13c6f82914f960f7aa92b6663c20cfca31
Asma Elfaleh <85293211+aelfaleh@users.noreply.github.com>
2021-11-09 13:14:22 -0500

    test(quantic): Add end to end tests for the Quantic Numeric Facet (#1417)
    * test(quantic): Add tests quantic numeric facet
    Co-authored-by: Nathan Lafrance-Berger <nlafrance@coveo.com>

--- packages/quantic/cypress/integration/facets/category-facet/category-facet-expectations.ts
+++ packages/quantic/cypress/integration/facets/category-facet/category-facet-expectations.ts
@@ -1,5 +1,11 @@
 import {InterceptAliases} from '../../../page-objects/search';
+import {should} from '../../common-selectors';
 import {SearchExpectations} from '../../search-expectations';
+import {
+  baseFacetExpectations,
+  facetWithSearchExpectations,
+  facetWithShowMoreLessExpectations,
+} from '../facet-common-expectations';
 import {
   CategoryFacetSelectors,
   AllFacetSelectors,
@@ -7,146 +13,119 @@ import {
 const hierarchicalField = 'geographicalhierarchy';
 const categoryFacetExpectations = (selector: AllFacetSelectors) => {
   return {
-    isRendered: (accessible: boolean) => {
-      selector.get().should(accessible ? 'exist' : 'not.exist');
-    },
-    displayLabel: (display: boolean) => {
-      selector.label().should(display ? 'exist' : 'not.exist');
-    },
-    displayPlaceholder: (display: boolean) => {
-      selector.placeholder().should(display ? 'exist' : 'not.exist');
-    },
     displayFacetCount: (display: boolean) => {
-      selector.facetCount().should(display ? 'exist' : 'not.exist');
+      selector
+        .facetCount()
+        .should(display ? 'exist' : 'not.exist')
+        .logDetail(`${should(display)} display the facet count`);
     },
     numberOfChildValues: (value: number) => {
-      selector.childValueOption().should('have.length', value);
+      selector
+        .childValueOption()
+        .should('have.length', value)
+        .logDetail(`should display ${value} child values`);
     },
     firstChildContains: (value: string) => {
-      selector.childValueOption().first().contains(value);
+      selector
+        .childValueOption()
+        .first()
+        .contains(value)
+        .logDetail(`first child value should contain "${value}"`);
     },
     numberOfParentValues: (value: number) => {
       selector
         .activeParentValueOption()
-        .should(value > 0 ? 'be.visible' : 'not.exist');
+        .should(value > 0 ? 'be.visible' : 'not.exist')
+        .logDetail(
+          `${should(value > 0)} display the active parent value option`
+        );
       if (value <= 1) {
-        selector.parentValueOption().should('not.exist');
+        selector
+          .parentValueOption()
+          .should('not.exist')
+          .logDetail('should not display parent value option');
         return;
       }
-      selector.parentValueOption().should('have.length', value - 1);
-    },
-    displaySearchInput: (display: boolean) => {
-      selector.searchInput().should(display ? 'exist' : 'not.exist');
-    },
-    searchInputContains: (value: string) => {
-      selector.searchInput().should('contain', value);
-    },
-    numberOfValues: (value: number) => {
-      selector.values().should('have.length', value);
+      selector
+        .parentValueOption()
+        .should('have.length', value - 1)
+        .logDetail(`should display ${value} parent values`);
     },
     parentValueLabel: (value: string) => {
-      selector.parentValueLabel().first().should('contain', value);
-    },
-    displayShowMoreButton: (display: boolean) => {
-      selector.showMoreButton().should(display ? 'exist' : 'not.exist');
-    },
-    displayShowLessButton: (display: boolean) => {
-      selector.showLessButton().should(display ? 'exist' : 'not.exist');
+      selector
+        .parentValueLabel()
+        .first()
+        .should('contain', value)
+        .logDetail(`should contain "${value}" parent value`);
     },
     urlHashContains: (path: string[]) => {
       const categoryFacetListInUrl = path.join(',');
       const urlHash = `#cf[${hierarchicalField}]=${encodeURI(
         categoryFacetListInUrl
       )}`;
-      cy.url().should('include', urlHash);
+      cy.url()
+        .should('include', urlHash)
+        .logDetail(`URL hash should contain "${urlHash}"`);
     },
     noUrlHash: () => {
-      cy.url().should('not.include', '#cf');
-    },
-    displayNoMatchesFound: (display: boolean) => {
-      selector.noMatches().should(display ? 'exist' : 'not.exist');
-    },
-    displayMoreMatchesFound: (display: boolean) => {
-      selector.moreMatches().should(display ? 'exist' : 'not.exist');
-    },
-    moreMatchesFoundContainsQuery: (query: string) => {
-      selector.moreMatches().contains(query);
-    },
-    displayNoMatches: (display: boolean) => {
-      selector.noMatches().should(display ? 'exist' : 'not.exist');
-    },
-    noMatchesFoundContainsQuery: (query: string) => {
-      selector.noMatches().contains(query);
-    },
-    highlightsResults: (query: string) => {
-      selector.valueHighlight().each((element) => {
-        const text = element.text().toLowerCase();
-        expect(text).to.eq(query.toLowerCase());
-      });
+      cy.url()
+        .should('not.include', '#cf')
+        .logDetail('URL hash should be empty');
     },
     displaySearchResultsPath: () => {
-      selector.searchResultPath().should('exist');
+      selector
+        .searchResultPath()
+        .should('exist')
+        .logDetail('Result path should exist');
     },
     searchResultsPathContains: (value: string) => {
-      selector.searchResultPath().contains(value);
+      selector
+        .searchResultPath()
+        .contains(value)
+        .logDetail(`Result path should contain "${value}"`);
     },
     searchResults: (value: number) => {
-      selector.searchResults().should('have.length', value);
-    },
-    displaySearchClearButton: (display: boolean) => {
-      selector.searchClearButton().should(display ? 'exist' : 'not.exist');
+      selector
+        .searchResults()
+        .should('have.length', value)
+        .logDetail(`should display ${value} search results`);
     },
     logCategoryFacetSelected: (path: string[]) => {
-      cy.wait(InterceptAliases.UA.Facet.Select).then((interception) => {
-        const analyticsBody = interception.request.body;
-        expect(analyticsBody).to.have.property('actionCause', 'facetSelect');
-        expect(analyticsBody.customData).to.have.property(
-          'facetValue',
-          path.join(';')
-        );
-        expect(analyticsBody.customData).to.have.property(
-          'facetField',
-          hierarchicalField
-        );
+      cy.wait(InterceptAliases.UA.Facet.Select)
+        .then((interception) => {
+          const analyticsBody = interception.request.body;
+          expect(analyticsBody).to.have.property('actionCause', 'facetSelect');
+          expect(analyticsBody.customData).to.have.property(
+            'facetValue',
+            path.join(';')
+          );
+          expect(analyticsBody.customData).to.have.property(
+            'facetField',
+            hierarchicalField
+          );
 
-        expect(analyticsBody.facetState[0]).to.have.property(
-          'state',
-          'selected'
-        );
-        expect(analyticsBody.facetState[0]).to.have.property(
-          'field',
-          hierarchicalField
-        );
-        expect(analyticsBody.facetState[0]).to.have.property(
-          'facetType',
-          'hierarchical'
-        );
-      });
-    },
-    logCategoryFacetSearch: (field: string) => {
-      cy.wait(InterceptAliases.UA.Facet.Search).then((interception) => {
-        const analyticsBody = interception.request.body;
-        expect(analyticsBody).to.have.property('actionCause', 'facetSearch');
-        expect(analyticsBody.customData).to.have.property('facetField', field);
-      });
-    },
-    logCategoryFacetClearAll: (field: string) => {
-      cy.wait(InterceptAliases.UA.Facet.ClearAll).then((interception) => {
-        const analyticsBody = interception.request.body;
-        expect(analyticsBody).to.have.property('actionCause', 'facetClearAll');
-        expect(analyticsBody.customData).to.have.property('facetField', field);
-      });
-    },
-    logCategoryFacetLoad: () => {
-      cy.wait(InterceptAliases.UA.Load).then((interception) => {
-        const analyticsBody = interception.request.body;
-        expect(analyticsBody).to.have.property('actionCause', 'interfaceLoad');
-      });
+          expect(analyticsBody.facetState[0]).to.have.property(
+            'state',
+            'selected'
+          );
+          expect(analyticsBody.facetState[0]).to.have.property(
+            'field',
+            hierarchicalField
+          );
+          expect(analyticsBody.facetState[0]).to.have.property(
+            'facetType',
+            'hierarchical'
+          );
+        })
+        .logDetail('should log the "facetSelect" UA event');
     },
   };
 };
 export const CategoryFacetExpectations = {
   ...categoryFacetExpectations(CategoryFacetSelectors),
+  ...baseFacetExpectations(CategoryFacetSelectors),
+  ...facetWithShowMoreLessExpectations(CategoryFacetSelectors),
+  ...facetWithSearchExpectations(CategoryFacetSelectors),
   search: {
     ...SearchExpectations,
   },
--- packages/quantic/force-app/main/default/lwc/quanticNumericFacet/quanticNumericFacet.js
+++ packages/quantic/force-app/main/default/lwc/quanticNumericFacet/quanticNumericFacet.js
@@ -328,14 +328,17 @@ export default class QuanticNumericFacet extends LightningElement {
     if(this.filterState?.range) {
       this.numericFilter.clear();
     }
-    this.resetValidityParameters();
     this.facet?.deselectAll();
-    this.allInputs.forEach((input) => {
-      // @ts-ignore
-      input.checkValidity();
-      // @ts-ignore
-      input.reportValidity();
-    });
+    if(this.withInput) {
+      this.resetValidityParameters();
+    
+      this.allInputs.forEach((input) => {
+        // @ts-ignore
+        input.checkValidity();
+        // @ts-ignore
+        input.reportValidity();
+      });
+    }
   }
 
   toggleFacetVisibility() {
@@ -346,6 +349,9 @@ export default class QuanticNumericFacet extends LightningElement {
     evt.preventDefault();
   }
 
+  /**
+   * @param {Event} evt
+  */
   onApply(evt) {
     evt.preventDefault();
 
