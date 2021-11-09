ed078a729db1581a558c7bff0bfefa7ffa076f3f
MehdiMitiche <gm_mitiche@esi.dz>
2021-11-09 07:16:29 -0500

    getters and setters for progression updated

--- packages/quantic/force-app/main/default/lwc/descriptionStrengthIndicator/descriptionStrengthIndicator.js
+++ packages/quantic/force-app/main/default/lwc/descriptionStrengthIndicator/descriptionStrengthIndicator.js
@@ -2,23 +2,31 @@ import { LightningElement, api } from 'lwc';
 
 export default class DescriptionStrengthIndicator extends LightningElement {
     @api helpLabel = "Don’t know what to write?";
-    progression = 0;
+    _progression = 0;
 
     get variant() {
-        return this.progression < 100 ? "warning" : "base-autocomplete";
+        return this._progression < 100 ? "warning" : "base-autocomplete";
     }
 
     get message() {
-        return this.progression === 0 ? "Provide details" : this.progression < 100 ? "Provide more details " : "Thank you!";
+        return this._progression === 0 ? "Provide details" : this._progression < 100 ? "Provide more details " : "Thank you!";
     }
 
     @api isFull() {
-        return this.progression >= 100;
+        return this._progression >= 100;
     }
 
-    @api progress() {
-        if (this.progression < 100) {
-            this.progression += 25;
-        }
+    @api
+    set progression(progression) {
+        this._progression = progression;
+    }
+
+    get progression() {
+        return this._progression;
+    }
+
+    @api
+    progress() {
+        this._progression = (this._progression < 100) ? this._progression + 25 : this._progression;
     }
 }
