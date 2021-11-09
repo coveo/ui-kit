7b3cd27d9cfa43a9c2cb3c5083d150bb48b9bdfb
MehdiMitiche <gm_mitiche@esi.dz>
2021-11-09 07:42:08 -0500

    SFINT-4126 documentation added

--- packages/quantic/force-app/main/default/lwc/descriptionStrengthIndicator/descriptionStrengthIndicator.js
+++ packages/quantic/force-app/main/default/lwc/descriptionStrengthIndicator/descriptionStrengthIndicator.js
@@ -1,7 +1,21 @@
 import { LightningElement, api } from 'lwc';
 
+/**
+ * The `descriptionStrengthIndicator` component is a dynamic indicator that shows the user if their case description has enough details so far. It also give hints to the user as to what information they show include in the description. 
+ * @example
+ * <c-description-strength-indicator></c-description-strength-indicator>
+ */
 export default class DescriptionStrengthIndicator extends LightningElement {
-    @api helpLabel = "Don’t know what to write?";
+    /**
+     * The message to be shown to the user.
+     * @api
+     * @type {string}
+     * @defaultValue 'Don’t know what to write?'
+     */
+    @api
+    helpLabel = "Don’t know what to write?";
+
+    /** @type {number} */
     _progression = 0;
 
     get variant() {
@@ -12,19 +26,39 @@ export default class DescriptionStrengthIndicator extends LightningElement {
         return this._progression === 0 ? "Provide details" : this._progression < 100 ? "Provide more details " : "Thank you!";
     }
 
-    @api isFull() {
+    /**
+     * Tells if the progression indicator is full
+     * @api
+     * @returns {boolean}
+     */
+    @api
+    isFull() {
         return this._progression >= 100;
     }
 
+    /**
+     * Set the progression
+     * @api
+     * @param {number} progression - the progression to be set.
+     * @returns {void}
+     */
     @api
     set progression(progression) {
         this._progression = progression;
     }
-
+    /**
+     * Returns the progression
+     * @api
+     * @returns {number}
+     */
     get progression() {
         return this._progression;
     }
 
+    /**
+     * Increases the progression
+     * @returns {void}
+     */
     @api
     progress() {
         this._progression = (this._progression < 100) ? this._progression + 25 : this._progression;
