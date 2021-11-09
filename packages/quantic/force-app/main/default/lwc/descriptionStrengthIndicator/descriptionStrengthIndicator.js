9a1bbb38bb4e1fe6ce2c5a4d5f9d6920cef1496e
MehdiMitiche <gm_mitiche@esi.dz>
2021-11-09 09:53:47 -0500

    SFINT-4126 updates from reviews

--- packages/quantic/force-app/main/default/lwc/descriptionStrengthIndicator/descriptionStrengthIndicator.js
+++ packages/quantic/force-app/main/default/lwc/descriptionStrengthIndicator/descriptionStrengthIndicator.js
@@ -6,61 +6,57 @@ import { LightningElement, api } from 'lwc';
  * <c-description-strength-indicator></c-description-strength-indicator>
  */
 export default class DescriptionStrengthIndicator extends LightningElement {
-    /**
-     * The message to be shown to the user.
-     * @api
-     * @type {string}
-     * @defaultValue `'Don’t know what to write?'`
-     */
-    @api
-    helpLabel = "Don’t know what to write?";
+  /**
+   * The message to be shown to the user.
+   * @api
+   * @type {string}
+   * @defaultValue `'Don’t know what to write?'`
+   */
+  @api helpLabel = 'Don’t know what to write?';
 
-    /** @type {number} */
-    _progression = 0;
+  /** @type {number} */
+  _progression = 0;
 
-    get variant() {
-        return this._progression < 100 ? "warning" : "base-autocomplete";
-    }
+  get variant() {
+    return this._progression < 100 ? 'warning' : 'base-autocomplete';
+  }
 
-    get message() {
-        return this._progression === 0 ? "Provide details" : this._progression < 100 ? "Provide more details " : "Thank you!";
-    }
+  get message() {
+    return this._progression === 0 ? 'Provide details' : this._progression < 100 ? 'Provide more details' : 'Thank you!';
+  }
 
-    /**
-     * Tells if the progression indicator is full.
-     * @api
-     * @returns {boolean}
-     */
-    @api
-    isFull() {
-        return this._progression >= 100;
-    }
+  /**
+   * Tells if the progression indicator is full.
+   * @api
+   * @returns {boolean}
+   */
+  @api isFull() {
+    return this._progression >= 100;
+  }
 
-    /**
-     * Set the progression
-     * @api
-     * @param {number} progression - the progression to be set.
-     * @returns {void}
-     */
-    @api
-    set progression(progression) {
-        this._progression = progression <= 100 ? progression : 100;
-    }
-    /**
-     * Returns the progression
-     * @api
-     * @returns {number}
-     */
-    get progression() {
-        return this._progression;
-    }
+  /**
+   * Set the progression.
+   * @api
+   * @param {number} progression - the progression to be set.
+   * @returns {void}
+   */
+  @api set progression(progression) {
+    this._progression = progression <= 100 ? progression : 100;
+  }
+  /**
+   * Returns the progression.
+   * @api
+   * @returns {number}
+   */
+  get progression() {
+    return this._progression;
+  }
 
-    /**
-     * Increases the progression
-     * @returns {void}
-     */
-    @api
-    progress() {
-        this._progression = (this._progression <= 75) ? this._progression + 25 : 100;
-    }
+  /**
+   * Increases the progression by 25%.
+   * @returns {void}
+   */
+  @api increaseProgress() {
+    this._progression = (this._progression <= 75) ? this._progression + 25 : 100;
+  }
 }
