c64fc2e82f94d06e7d34c3f3b131b8020ee4ae14
MehdiMitiche <gm_mitiche@esi.dz>
2021-11-11 15:39:02 -0500

    SFINT-4125 improvments made

--- packages/quantic/force-app/main/default/lwc/descriptionInput/descriptionInput.js
+++ packages/quantic/force-app/main/default/lwc/descriptionInput/descriptionInput.js
@@ -1,26 +1,55 @@
 import { LightningElement, api } from 'lwc';
+import explainProblem from '@salesforce/label/c.cookbook_ExplainProblem';
 
+/**
+  * The `descriptionInput` component is a rich text input for the description of the case.
+  * @example
+  * <c-description-input></c-description-input>
+  */
 export default class DescriptionInput extends LightningElement {
-    @api label = "Explain the problem";
-    value = '';
-    formats = [
-        'size',
-        'bold',
-        'italic',
-        'underline',
-        'strike',
-        'list',
-        'indent',
-        'clean',
-        'table',
-        'header',
-    ];
+  labels = {
+    explainProblem
+  }
 
-    handleChange(event) {
-        this.value = event.target.value;
-    }
+  /**
+   * The label to be shown to the user.
+   * @api
+   * @type {string}
+   * @defaultValue `'Explain the problem'`
+   */
+  @api label = this.labels.explainProblem;
 
-    @api getValue() {
-        return this.value;
-    }
+  /** @type {string} */
+  _value = '';
+
+  /**
+   * List of formats to include in the editor.
+   * @api
+   * @type {Array<string>}
+   */
+  @api formats = [
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'list',
+    'indent',
+    'clean',
+    'table',
+    'header',
+  ];
+
+  handleChange(e) {
+    this._value = e.target.value;
+  }
+
+  /**
+   * Returns the value of the input.
+   * @api
+   * @returns {string}
+   */
+  @api get value() {
+    return this._value;
+  }
 }
