07957e1cad3f2a9013dbc2e80d35278a3c309747
MehdiMitiche <gm_mitiche@esi.dz>
2021-11-15 10:06:27 -0500

    SFINT-4127 translations added

--- packages/quantic/force-app/main/default/lwc/sectionTitle/sectionTitle.js
+++ packages/quantic/force-app/main/default/lwc/sectionTitle/sectionTitle.js
@@ -1,9 +1,19 @@
 import { LightningElement, api } from 'lwc';
+import sectionTitle from '@salesforce/label/c.cookbook_SectionTitle';
 
+/**
+ * The `sectionTitle` component is a title to mark the beginning of the case classification section.
+ * @example
+ * <c-section-title></c-section-title>
+ */
 export default class SectionTitle extends LightningElement {
+  labels = {
+    sectionTitle
+  }
+
   /**
    * The section title
    * @type {string}
    */
-  @api title = 'Which product is related to your problem?'
+  @api title = this.labels.sectionTitle;
 }
