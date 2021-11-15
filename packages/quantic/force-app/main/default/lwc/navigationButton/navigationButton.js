0b0a27b9b8267267d5725106211b8cdf0e1f2255
MehdiMitiche <gm_mitiche@esi.dz>
2021-11-15 15:38:21 -0500

    SFINT-4128 comments added

--- packages/quantic/force-app/main/default/lwc/navigationButton/navigationButton.js
+++ packages/quantic/force-app/main/default/lwc/navigationButton/navigationButton.js
@@ -1,13 +1,29 @@
 import { LightningElement, api } from 'lwc';
 
 export default class NavigationButton extends LightningElement {
+  /**
+   * The type of the navigation button.
+   * @type {'next'|'previous'}
+   */
   @api type = 'next';
+
+  /**
+   * The label to be shown in the button.
+   */
   @api label = 'Navigate';
 
+  /**
+   * Returns the variant of the button.
+   * @type {string}
+   */
   get variant() {
     return this.type === 'next' ? 'brand' : 'brand-outline'
   }
 
+  /**
+   * Hamdles clinks on the navigation button.
+   * @returns {void}
+   */
   handleNavigate() {
     if (this.type === 'next') {
       this.dispatchEvent(new CustomEvent('next'))
