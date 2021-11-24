8e8d2aa8a2b4de4d488a239bf56c1d4b9ec97096
nlafrance-coveo <nlafrance@coveo.com>
2021-11-24 14:23:41 -0500

    feat(headless): ut

--- packages/headless/src/features/document-suggestions/document-suggestions-slice.test.ts
+++ packages/headless/src/features/document-suggestions/document-suggestions-slice.test.ts
@@ -1,145 +1,57 @@
-import {GetCaseClassificationsResponse} from '../../api/service/case-assist/get-case-classifications/get-case-classifications-response';
 import {
-  setCaseField,
-  enableCaseClassifications,
-  disableCaseClassifications,
-  fetchCaseClassifications,
-} from './document-suggestions-actions';
-import {caseFieldsReducer} from './document-suggestions-slice';
+  DocumentSuggestion,
+  GetDocumentSuggestionsResponse,
+} from '../../api/service/case-assist/get-document-suggestions/get-document-suggestions-response';
+import {fetchDocumentSuggestions} from './document-suggestions-actions';
+import {documentSuggestionsReducer} from './document-suggestions-slice';
 import {
-  getCaseFieldsInitialState,
-  CaseFieldsState,
+  getDocumentSuggestionsInitialState,
+  DocumentSuggestionsState,
 } from './document-suggestions-state';
 
-describe('case fields slice', () => {
-  let state: CaseFieldsState;
-  const testField = {
-    fieldName: 'test-field-name',
-    fieldValue: 'test-field-value',
+describe('document suggestions slice', () => {
+  let state: DocumentSuggestionsState;
+  const mockDocument: DocumentSuggestion = {
+    clickUri: 'www.clikuri.com',
+    title: 'Mock Document',
+    excerpt: 'This is a dang good mock document.',
+    fields: {
+      type: 'mock',
+    },
+    hasHtmlVersion: true,
+    uniqueId: '123ID',
   };
 
   beforeEach(() => {
-    state = getCaseFieldsInitialState();
+    state = getDocumentSuggestionsInitialState();
   });
 
   it('should have an initial state', () => {
-    expect(caseFieldsReducer(undefined, {type: 'foo'})).toEqual(
-      getCaseFieldsInitialState()
+    expect(documentSuggestionsReducer(undefined, {type: 'foo'})).toEqual(
+      getDocumentSuggestionsInitialState()
     );
   });
 
-  describe('#setCaseField', () => {
-    describe('without caseFields', () => {
-      it('should allow to set a case field', () => {
-        expect(
-          caseFieldsReducer(state, setCaseField(testField)).fields[
-            testField.fieldName
-          ].value
-        ).toEqual(testField.fieldValue);
-      });
-    });
-
-    describe('with existing caseFields', () => {
-      const existingField = {
-        fieldName: 'existing-field-name',
-        fieldValue: 'existing-field-value',
-      };
-      const existingSuggestions = [
-        {
-          id: 'id',
-          value: 'value',
-          confidence: 0.8,
-        },
-      ];
-
-      beforeEach(() => {
-        state.fields = {
-          [existingField.fieldName]: {
-            value: existingField.fieldValue,
-            suggestions: existingSuggestions,
-          },
-        };
+  describe('#fetchDocumentSuggestions', () => {
+    const buildMockDocumentSuggestionsResponse =
+      (): GetDocumentSuggestionsResponse => ({
+        documents: [mockDocument],
+        totalCount: 0,
+        responseId: 'response-id',
       });
 
-      it('should allow to set a case field', () => {
-        expect(
-          caseFieldsReducer(state, setCaseField(testField)).fields[
-            testField.fieldName
-          ].value
-        ).toEqual(testField.fieldValue);
-      });
-
-      it('should allow to update a case field without affecting suggestions', () => {
-        const updatedValue = 'updated-value';
-        const modifiedState = caseFieldsReducer(
-          state,
-          setCaseField({...existingField, fieldValue: updatedValue})
-        );
-        expect(modifiedState.fields[existingField.fieldName].value).toEqual(
-          updatedValue
-        );
-        expect(
-          modifiedState.fields[existingField.fieldName].suggestions
-        ).toEqual(existingSuggestions);
-      });
-    });
-  });
-
-  describe('#enableCaseClassifications', () => {
-    it('should allow to enable case classifications', () => {
-      expect(state.enabled).toBe(false);
-      expect(
-        caseFieldsReducer(state, enableCaseClassifications()).enabled
-      ).toBe(true);
-    });
-  });
-
-  describe('#enableCaseClassifications', () => {
-    it('should allow to enable case classifications', () => {
-      state.enabled = true;
-      expect(
-        caseFieldsReducer(state, disableCaseClassifications()).enabled
-      ).toBe(false);
-    });
-  });
-
-  describe('#fetchCaseClassifications', () => {
-    const buildMockCaseClassificationResponse = (
-      fieldName: string
-    ): GetCaseClassificationsResponse => ({
-      fields: {
-        [fieldName]: {
-          predictions: [
-            {
-              id: 'prediction-id',
-              value: 'prediction-value',
-              confidence: 0.9,
-            },
-          ],
-        },
-      },
-      responseId: 'response-id',
-    });
-
-    it('when a fetchCaseClassifications fulfilled is received, it updates the state to the received payload', () => {
-      const response = buildMockCaseClassificationResponse(testField.fieldName);
-      state.fields = {
-        [testField.fieldName]: {
-          value: testField.fieldValue,
-          suggestions: [],
-        },
-      };
-      const action = fetchCaseClassifications.fulfilled(
+    it('when a fetchDocumentSuggestions fulfilled is received, it updates the state to the received payload', () => {
+      const response = buildMockDocumentSuggestionsResponse();
+      state.documents = [];
+      const action = fetchDocumentSuggestions.fulfilled(
         {
           response: response,
         },
         ''
       );
-      const finalState = caseFieldsReducer(state, action);
+      const finalState = documentSuggestionsReducer(state, action);
 
-      expect(finalState.fields[testField.fieldName].suggestions).toEqual(
-        response.fields[testField.fieldName].predictions
-      );
+      expect(finalState.documents).toEqual(response.documents);
       expect(finalState.status.loading).toBe(false);
       expect(finalState.status.error).toBeNull();
     });
@@ -150,16 +62,16 @@ describe('case fields slice', () => {
         statusCode: 500,
         type: 'type',
       };
-      const action = fetchCaseClassifications.rejected(null, '');
+      const action = fetchDocumentSuggestions.rejected(null, '');
       action.payload = err;
-      const finalState = caseFieldsReducer(state, action);
+      const finalState = documentSuggestionsReducer(state, action);
       expect(finalState.status.error).toEqual(err);
       expect(finalState.status.loading).toBe(false);
     });
 
-    it('set the isLoading state to true during getProductRecommendations.pending', () => {
-      const pendingAction = fetchCaseClassifications.pending('');
-      const finalState = caseFieldsReducer(state, pendingAction);
+    it('set the isLoading state to true during fetchDocumentSuggestions.pending', () => {
+      const pendingAction = fetchDocumentSuggestions.pending('');
+      const finalState = documentSuggestionsReducer(state, pendingAction);
       expect(finalState.status.loading).toBe(true);
     });
   });
