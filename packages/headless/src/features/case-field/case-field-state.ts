d596848cf8a38f7618f09a15e4996333fca77dd8
Nathan Lafrance-Berger <nlafrance@coveo.com>
2021-11-25 09:47:02 -0500

    feat(headless): added case classification-related actions (#1505)
    * feat(headless): started adding case assist engine stuff
    
    * feat(headless): added state section and app state
    
    * feat(headless): test title edit
    
    * feat(headless): added case input actions
    
    * feat(headless): removed analytics actions
    
    * feat(headless): removed unused interface
    
    * feat(headless): refactored state
    
    * feat(headless): added case fields actions
    
    * feat(headless): modified client args
    
    * feat(headless): added unit tests
    
    * feat(headless): fixed ut
    
    * feat(headless): visitorId conditional on analytics
    
    * feat(headless): whats going on
    
    * feat(headles): are you kidding me?
    
    * feat(headless): how what why
    
    * feat(headless): changed extra
    
    * feat(facepalm): merged in master to get async signature change
    
    * feat(headless): fixed tests
    
    * feat(headless): wrong fields were used for classify request
    
    * feat(headless): some renaming and refactoring
    
    * feat(headless): ut
    
    * feat(headless): removed enabled flag
    
    * feat(headless): copy pasta
    
    * feat(headless): renamed to singular

---
+++ packages/headless/src/features/case-field/case-field-state.ts
@@ -0,0 +1,64 @@
+import {CaseAssistAPIErrorStatusResponse} from '../../api/service/case-assist/case-assist-api-client';
+
+export const getCaseFieldInitialState = (): CaseFieldState => ({
+  status: {
+    loading: false,
+    error: null,
+    lastResponseId: '',
+  },
+  fields: {},
+});
+
+export interface CaseFieldStatus {
+  /**
+   * `true` if a request is in progress and `false` otherwise.
+   */
+  loading: boolean;
+  /**
+   * The Case Assist API error response.
+   */
+  error: CaseAssistAPIErrorStatusResponse | null;
+  /**
+   * The ID of the response.
+   */
+  lastResponseId: string;
+}
+
+export interface CaseFieldSuggestion {
+  /**
+   * The ID of the suggestion.
+   */
+  id: string;
+  /**
+   * The suggested field value.
+   */
+  value: string;
+  /**
+   * The confidence value of the suggestion in decimal.
+   */
+  confidence: number;
+}
+
+export interface CaseField {
+  /**
+   * The field to predict.
+   */
+  value: string;
+  /**
+   * The field value suggestions returned from the Coveo Customer Service API.
+   */
+  suggestions: CaseFieldSuggestion[];
+}
+
+export interface CaseFieldState {
+  /**
+   * The status of the case fields classification request.
+   */
+  status: CaseFieldStatus;
+  /**
+   * The case fields for which classifications are retrieved.
+   */
+  fields: {
+    [fieldName: string]: CaseField;
+  };
+}
---
+++ packages/headless/src/features/case-input/case-input-actions-loader.ts
@@ -0,0 +1,40 @@
+import {PayloadAction} from '@reduxjs/toolkit';
+import {CaseAssistEngine} from '../../app/case-assist-engine/case-assist-engine';
+import {caseInput} from '../../app/reducers';
+import {
+  updateCaseInput,
+  SetCaseInputActionCreatorPayload,
+} from './case-input-actions';
+
+export {SetCaseInputActionCreatorPayload};
+
+/**
+ * The case inputs action creators.
+ */
+export interface CaseInputActionCreators {
+  /**
+   * Adds or updates the state caseInputs with the specified field and value.
+   *
+   * @param payload - The action creator payload.
+   * @returns A dispatchable action.
+   */
+  updateCaseInput(
+    payload: SetCaseInputActionCreatorPayload
+  ): PayloadAction<SetCaseInputActionCreatorPayload>;
+}
+
+/**
+ * Loads the `case inputs` reducer and returns possible action creators.
+ *
+ * @param engine - The headless engine.
+ * @returns An object holding the action creators.
+ */
+export function loadCaseInputActions(
+  engine: CaseAssistEngine
+): CaseInputActionCreators {
+  engine.addReducers({caseInput});
+
+  return {
+    updateCaseInput,
+  };
+}
