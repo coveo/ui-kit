bde66a62adf41c4e1cfd1bdb06b23e7a696e8d85
olamothe <olamothe@coveo.com>
2021-11-25 17:08:27 -0500

    refactor(atomic): implement safe storage module
    https://coveord.atlassian.net/browse/KIT-1107

---
+++ packages/atomic/src/utils/local-storage-utils.tsx
@@ -0,0 +1,108 @@
+import {SearchEngine} from '@coveo/headless';
+
+export enum StorageItems {
+  RECENT_QUERIES = 'coveo-recent-queries',
+}
+
+export class SafeStorage implements Storage {
+  constructor(private logger: SearchEngine['logger']) {}
+  public clear() {
+    return this.tryAccessLocalStorageOrWarn(
+      () => localStorage.clear(),
+      () => {}
+    );
+  }
+
+  public getItem(key: StorageItems) {
+    return this.tryAccessLocalStorageOrWarn(
+      () => localStorage.getItem(key),
+      () => null
+    );
+  }
+
+  public getItemParsed<T>(key: StorageItems, fallback: T) {
+    const item = this.getItem(key);
+    if (!item) {
+      return fallback;
+    }
+    return this.tryJSONOrWarn(
+      key,
+      () => JSON.parse(item) as T,
+      () => fallback
+    );
+  }
+
+  public key(index: number) {
+    return this.tryAccessLocalStorageOrWarn(
+      () => localStorage.key(index),
+      () => null
+    );
+  }
+
+  public get length() {
+    return this.tryOrElse(
+      () => localStorage.length,
+      () => 0
+    );
+  }
+
+  public removeItem(key: StorageItems) {
+    return this.tryAccessLocalStorageOrWarn(
+      () => localStorage.removeItem(key),
+      () => {}
+    );
+  }
+
+  public setItem(key: StorageItems, value: string) {
+    return this.tryAccessLocalStorageOrWarn(
+      () => localStorage.setItem(key, value),
+      () => {}
+    );
+  }
+
+  public setObject(key: StorageItems, obj: any) {
+    const stringified = this.tryJSONOrWarn(
+      key,
+      () => JSON.stringify(obj),
+      () => JSON.stringify({})
+    );
+    return this.setItem(key, stringified);
+  }
+
+  private tryAccessLocalStorageOrWarn<OnSuccess, OnFailure>(
+    tryTo: () => OnSuccess,
+    orElse: () => OnFailure
+  ) {
+    return this.tryOrElse(tryTo, () => {
+      this.logger.warn(
+        'Error while trying to read or modify local storage. This can be caused by browser specific settings.'
+      );
+      return orElse();
+    });
+  }
+
+  private tryJSONOrWarn<OnSuccess, OnFailure>(
+    key: StorageItems,
+    tryTo: () => OnSuccess,
+    orElse: () => OnFailure
+  ) {
+    return this.tryOrElse(tryTo, () => {
+      this.logger.warn(
+        `Error while trying to do JSON manipulation with local storage entry. ${key}`
+      );
+      return orElse();
+    });
+  }
+
+  private tryOrElse<OnSuccess, OnFailure>(
+    tryTo: () => OnSuccess,
+    orElse: () => OnFailure
+  ) {
+    try {
+      return tryTo();
+    } catch (e) {
+      this.logger.warn(e as Error);
+      return orElse();
+    }
+  }
+}
