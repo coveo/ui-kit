853f3c648c61bdfc0bb52f367733cae0b8ce9581
Luc Bergeron <10946843+lbergeron@users.noreply.github.com>
2021-11-30 16:34:41 -0500

    feat(headless): add CaseField doc

--- packages/headless/src/controllers/case-field/headless-case-field.ts
+++ packages/headless/src/controllers/case-field/headless-case-field.ts
@@ -45,15 +45,39 @@ export interface CaseFieldOptions {
 }
 
 export interface CaseFieldState {
+  /**
+   * Whether suggestions are being retrieved for the field.
+   */
   loading: boolean;
+  /**
+   * The error that occurred while fetching suggestions, if any.
+   */
   error: CaseAssistAPIErrorStatusResponse | null;
+  /**
+   * The current field value.
+   */
   value: string;
+  /**
+   * The field suggestions.
+   */
   suggestions: CaseFieldSuggestion[];
 }
 
+/**
+ * The `CaseField` controller is responsible for setting the value and retrieving suggestions for a field from the case creation form and optionally trigger Case Assist API requests.
+ */
 export interface CaseField extends Controller {
+  /**
+   * Sets the value of the specified field.
+   *
+   * @param value - The field value to set.
+   * @param updatesToFetch - A set of flags dictating whether to fetch case assist data after updating the field value.
+   */
   update(value: string, updatesToFetch?: UpdateCaseFieldFetchOptions): void;
 
+  /**
+   * A scoped and simplified part of the headless state that is relevant to the `CaseField` controller.
+   */
   state: CaseFieldState;
 }
 
@@ -62,6 +86,13 @@ export interface UpdateCaseFieldFetchOptions {
   documentSuggestions?: boolean;
 }
 
+/**
+ * Creates a `CaseField` controller instance.
+ *
+ * @param engine - The headless engine.
+ * @param props - The configurable `CaseField` controller properties.
+ * @returns A `CaseField` controller instance.
+ */
 export function buildCaseField(
   engine: CaseAssistEngine,
   props: CaseFieldProps = {}
