000e6f089fa4c42853dbdd4b9369d2d3475158b9
Nathan Lafrance-Berger <nlafrance@coveo.com>
2021-12-01 15:51:43 -0500

    feat(headless): added document suggestion controller (#1547)
    * feat(headless): started adding case assist engine stuff
    
    * feat(headless): added state section and app state
    
    * feat(headless): test title edit
    
    * feat(headless): added case input actions
    
    * feat(headless): removed analytics actions
    
    * feat(headless): removed unused interface
    
    * feat(headless): refactored state
    
    * feat(headless): added case fields actions
    
    * feat(headless): modified client args
    
    * feat(headless): added unit tests
    
    * feat(headless): fixed ut
    
    * feat(headless): added controller code
    
    * feat(headless): visitorId conditional on analytics
    
    * feat(headless): whats going on
    
    * feat(headles): are you kidding me?
    
    * feat(headless): how what why
    
    * feat(headless): changed extra
    
    * feat(facepalm): merged in master to get async signature change
    
    * feat(headless): fixed tests
    
    * feat(headless): added files
    
    * feat(headless): wrong fields were used for classify request
    
    * feat(headless): some renaming and refactoring
    
    * feat(headless): ut
    
    * feat(headless): added slice and actions
    
    * feat(headless): removed enabled flag
    
    * feat(headless): ut
    
    * feat(headless): copy pasta
    
    * feat(headless): renaming
    
    * feat(headless): renamed to singular
    
    * feat(headless): fixed after merge
    
    * feat(headless): added tests for controller
    
    * feat(headless): removed prebuild controller functions
    
    * feat(headless): changed update function arguments
    
    * feat(headless): added analytics logging to controller
    
    * feat(headless): fixed it
    
    * feat(headless): removed unecessary setup
    
    * feat(headless): added reducer and comment edits
    
    * feat(headless): added document suggestion controller
    
    * feat(headless): edited jsdoc comment
    
    * feat(headless): bad comment
    
    * feat(headless): changed name to -list
    
    * feat(headless): var name change

--- packages/headless/src/case-assist.index.ts
+++ packages/headless/src/case-assist.index.ts
@@ -44,3 +44,9 @@ export {
   CaseInputProps,
   buildCaseInput,
 } from './controllers/case-input/headless-case-input';
+
+export {
+  DocumentSuggestionList as DocumentSuggestion,
+  DocumentSuggestionListState as DocumentSuggestionState,
+  buildDocumentSuggestionList as buildDocumentSuggestion,
+} from './controllers/document-suggestion-list/headless-document-suggestion-list';
