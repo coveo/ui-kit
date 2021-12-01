8af7a5e644f4597358e8efb15ac02bae796f5271
Asma Elfaleh <aelfaleh@coveo.com>
2021-11-30 20:57:47 -0500

    test(quantic): fix typo

--- packages/quantic/force-app/examples/main/lwc/exampleQuanticRecentQueriesList/exampleQuanticRecentQueriesList.js
+++ packages/quantic/force-app/examples/main/lwc/exampleQuanticRecentQueriesList/exampleQuanticRecentQueriesList.js
@@ -1,6 +1,6 @@
 import {api, LightningElement, track} from 'lwc';
 
-export default class ExampleQuanticPager extends LightningElement {
+export default class ExampleQuanticRecentQueriesList extends LightningElement {
   @api engineId = 'quantic-recent-queries-list-engine';
   @track config = {};
   isConfigured = false;
