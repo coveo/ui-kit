45c3a388ea638d68c7816b75ec2936e8c8620027
Benjamin Taillon <btaillon@coveo.com>
2021-12-06 09:49:40 -0500

    reverted command.js and added report-size job
    https://coveord.atlassian.net/browse/KIT-1268

--- scripts/reports/bundle-size/command.js
+++ scripts/reports/bundle-size/command.js
@@ -1,11 +1,29 @@
-const {resolve} = require('path');
-const {readFileSync} = require('fs');
+const { resolve } = require('path');
+const { readFileSync } = require('fs');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
 
-function computeFileSizes() {
+async function computeFileSizes() {
+  await setup();
+  await buildFiles();
+  return readFileSizes();
+}
+
+async function setup() {
+  console.log('setting up repositories');
+  await exec('npm run setup');
+}
+
+async function buildFiles() {
+  console.log('building files');
+  await exec('npm run build');
+}
+
+function readFileSizes() {
   console.log('getting file sizes');
-  const path = resolve('packages/headless/.size-snapshot.json');
-  const buffer = readFileSync(path);
+  const path = resolve('packages/headless/.size-snapshot.json')
+  const buffer = readFileSync(path)
   return JSON.parse(buffer.toString());
 }
 
-module.exports = {computeFileSizes};
+module.exports = { computeFileSizes }
