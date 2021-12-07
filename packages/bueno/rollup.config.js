2e84e01927b3b21a8724d97cd93a0d3a77944125
Sami Sayegh <ssayegh@coveo.com>
2021-12-07 17:07:08 -0500

    centralize license
    https://coveord.atlassian.net/browse/KIT-1281

--- packages/bueno/rollup.config.js
+++ packages/bueno/rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import replacePlugin from '@rollup/plugin-replace';
 import {terser} from 'rollup-plugin-terser';
+const {apacheLicense} = require('../../scripts/license/apache');
 
 const isCI = process.env.CI === 'true';
 const isProduction = process.env.BUILD === 'production';
@@ -27,14 +28,13 @@ function onWarn(warning, warn) {
 
 const browserConfig = {
   input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/browser/bueno.js',
-      format: 'umd',
-      name: 'Bueno',
-      sourcemap: true,
-    },
-  ],
+  output: {
+    file: 'dist/browser/bueno.js',
+    format: 'umd',
+    name: 'Bueno',
+    sourcemap: true,
+    banner: apacheLicense,
+  },
   plugins: [
     resolve({browser: true}),
     commonjs(),
