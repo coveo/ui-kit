8b02414cebc06b3b1078467fc67743f96c433b77
Sami Sayegh <ssayegh@coveo.com>
2021-12-07 16:20:20 -0500

    add esbuild config
    https://coveord.atlassian.net/browse/KIT-1281

---
+++ packages/bueno/esbuild.js
@@ -0,0 +1,69 @@
+const {build} = require('esbuild');
+
+const apacheLicense = () => {
+  const license = `
+  Copyright [yyyy] [name of copyright owner]
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  
+        http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.`
+    .replace('[yyyy]', new Date().getFullYear())
+    .replace('[name of copyright owner]', 'Coveo Solutions Inc.');
+
+  return `/*${license}\n*/`;
+};
+
+/**
+ * @type {import('esbuild').BuildOptions}
+ */
+const base = {
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  banner: {js: apacheLicense()},
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
+};
+
+function nodeCjs() {
+  return build({
+    ...base,
+    platform: 'node',
+    outfile: 'dist/bueno.js',
+    format: 'cjs',
+  });
+}
+
+function nodeEsm() {
+  return build({
+    ...base,
+    platform: 'node',
+    outfile: 'dist/bueno.esm.js',
+    format: 'esm',
+  });
+}
+
+function browserEsm() {
+  return build({
+    ...base,
+    platform: 'browser',
+    outfile: 'dist/browser/bueno.esm.js',
+    format: 'esm',
+    minify: true,
+    sourcemap: true,
+  });
+}
+
+async function main() {
+  await Promise.all([nodeCjs(), nodeEsm(), browserEsm()]);
+}
+
+main();
