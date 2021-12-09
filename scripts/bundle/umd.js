d7ff0f325354d20c0dfe6d1be4c02e75619d7fad
Sami Sayegh <ssayegh@coveo.com>
2021-12-09 14:01:01 -0500

    improve boilerplate readability
    https://coveord.atlassian.net/browse/KIT-1284

--- scripts/bundle/umd.js
+++ scripts/bundle/umd.js
@@ -1,8 +1,19 @@
 function umdWrapper(globalName) {
   const header = `(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-    typeof define === 'function' && define.amd ? define(['exports'], factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.${globalName} = {}));
+    const isCommonjs = typeof exports === 'object' && typeof module !== 'undefined';
+    
+    if (isCommonjs) {
+      return factory(exports);
+    }
+    
+    const isAmd = typeof define === 'function' && define.amd;
+
+    if (isAmd) {
+      return define(['exports'], factory);
+    }
+
+    global = typeof globalThis !== 'undefined' ? globalThis : global || self;
+    factory(global.${globalName} = {});
   })(this, (function (exports) { 'use strict';`;
   const footer = `}));`;
 
