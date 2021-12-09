236ed9003e1e71bd5ff0027991ccd2658f58deee
Sami Sayegh <ssayegh@coveo.com>
2021-12-09 15:04:35 -0500

    generate umd with esbuild
    https://coveord.atlassian.net/browse/KIT-1281

--- packages/bueno/esbuild.js
+++ packages/bueno/esbuild.js
@@ -1,5 +1,6 @@
 const {build} = require('esbuild');
 const {apacheLicense} = require('../../scripts/license/apache');
+const {umdWrapper} = require('../../scripts/bundle/umd');
 
 const devMode = process.argv[2] === 'dev';
 
@@ -40,13 +41,28 @@ function browserEsm() {
     outfile: 'dist/browser/bueno.esm.js',
     format: 'esm',
     watch: devMode,
-    minify: true,
-    sourcemap: true,
+  });
+}
+
+function browserUmd() {
+  const umd = umdWrapper('Bueno');
+
+  return build({
+    ...base,
+    platform: 'browser',
+    outfile: 'dist/browser/bueno.js',
+    format: 'cjs',
+    banner: {
+      js: `${base.banner.js}\n${umd.header}`
+    },
+    footer: {
+      js: umd.footer
+    }
   });
 }
 
 async function main() {
-  await Promise.all([nodeCjs(), nodeEsm(), browserEsm()]);
+  await Promise.all([nodeCjs(), nodeEsm(), browserEsm(), browserUmd()]);
 }
 
 main();
