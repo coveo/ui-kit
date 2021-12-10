ec8dc1fb9554ceb0f18fe27287cf8e9e7c3d7031
Benjamin Taillon <btaillon@coveo.com>
2021-12-10 16:10:26 -0500

    hid JSON.parse error message
    https://coveord.atlassian.net/browse/KIT-1278

--- .github/actions/get-pipeline-token/index.js
+++ .github/actions/get-pipeline-token/index.js
@@ -4,6 +4,27 @@ const {
   GetSecretValueCommand,
 } = require('@aws-sdk/client-secrets-manager');
 
+/**
+ * @param {(...args: Args) => ReturnType} func
+ * @param {string | Error} newError
+ * @returns {(...args: Args) => ReturnType}
+ * @template Args, ReturnType
+ */
+function replaceErrorMessage(func, newError) {
+  return (...args) => {
+    try {
+      return func(...args);
+    } catch (e) {
+      throw newError;
+    }
+  };
+}
+
+const safelyParseJSON = replaceErrorMessage(
+  JSON.parse,
+  'Failed to parse JSON. Details hidden to avoid leaking secrets.'
+);
+
 const client = new SecretsManagerClient({region: 'us-east-1'});
 const res = await client.send(
   new GetSecretValueCommand({
@@ -11,6 +32,6 @@ const res = await client.send(
       'arn:aws:secretsmanager:us-east-1:458176070654:secret:deployment_pipeline_tokens_dev-Iv6AOi',
   })
 );
-const token = JSON.parse(res.SecretString).jenkins;
+const token = safelyParseJSON(res.SecretString).jenkins;
 setSecret(token);
 setOutput('token', token);
