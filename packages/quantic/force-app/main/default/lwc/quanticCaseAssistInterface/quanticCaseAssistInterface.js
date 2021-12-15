6b5116320cc5197311cab26c4baf40639ac7a922
MehdiMitiche <gm_mitiche@esi.dz>
2021-12-15 15:15:40 -0500

    SFINT-4305 initialize method added

--- packages/quantic/force-app/main/default/lwc/quanticCaseAssistInterface/quanticCaseAssistInterface.js
+++ packages/quantic/force-app/main/default/lwc/quanticCaseAssistInterface/quanticCaseAssistInterface.js
@@ -7,18 +7,14 @@ import {
 } from 'c/quanticHeadlessLoader';
 // @ts-ignore
 import getHeadlessConfiguration from '@salesforce/apex/HeadlessController.getHeadlessConfiguration';
-import LOCALE from '@salesforce/i18n/locale';
 
 /** @typedef {import("coveo").CaseAssistEngine} CaseAssistEngine */
 /** @typedef {import("coveo").CaseAssistEngineOptions} CaseAssistEngineOptions */
 
 /**
- * The `QuanticCaseAssistInterface` component handles the headless search engine and localization configurations.
- * A single instance should be used for each instance of the Coveo Headless search engine.
+ * The `QuanticCaseAssistInterface` component handles the headless case assist engine.
+ * A single instance should be used for each instance of the Coveo Headless case assist engine.
  *
- * The `locale` used in the search engine options is taken from the [Language Settings](https://help.salesforce.com/s/articleView?id=sf.setting_your_language.htm&type=5).
- * Coveo Machine Learning models use this information to provide contextually relevant output.
- * Moreover, this information can be referred to in query expressions and QPL statements by using the `$locale` object.
  * @example
  * <c-quantic-case-assist-interface engine-id={engineId} case-assist-id={caseAssistId}></c-quantic-case-assist-interface>
  */
@@ -49,7 +45,6 @@ export default class QuanticSearchInterface extends LightningElement {
               configuration: {
                 ...JSON.parse(data),
                 caseAssistId: this.caseAssistId,
-                locale: LOCALE,
               },
             };
             setEngineOptions(
@@ -65,4 +60,15 @@ export default class QuanticSearchInterface extends LightningElement {
       }
     });
   }
+
+  /**
+   * @param {CaseAssistEngine} engine
+   */
+  initialize = (engine) => {
+    this.engine = engine;
+    this.actions = {
+      ...CoveoHeadlessCaseAssist.loadCaseAssistAnalyticsActions(engine),
+    };
+    this.engine.dispatch(this.actions.logCaseStart());
+  };
 }
