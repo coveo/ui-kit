436a90bae6485cf561a059fd6d45f68e933184e4
MehdiMitiche <gm_mitiche@esi.dz>
2021-12-17 08:42:54 -0500

    SFINT-4118 code improved

--- packages/quantic/force-app/examples/main/lwc/actionFetchClassifications/actionFetchClassifications.js
+++ packages/quantic/force-app/examples/main/lwc/actionFetchClassifications/actionFetchClassifications.js
@@ -1,25 +1,17 @@
 import { LightningElement, api } from 'lwc';
-import { loadDependencies } from 'c/quanticHeadlessLoader';
 
 export default class ActionFetchClassifications extends LightningElement {
   @api engineId;
   
-  headless;
   actions;
 
   handleFetchClassifications() {
     window.coveoHeadless?.[this.engineId]?.enginePromise
       .then((engine) => {
         this.actions = {
-          ...this.headless.loadCaseFieldActions(engine),
+          ...CoveoHeadlessCaseAssist.loadCaseFieldActions(engine),
         };
         engine.dispatch(this.actions.fetchCaseClassifications());
       });
-  }
-
-  connectedCallback() {
-    loadDependencies(this, 'case-assist').then((headless) => {
-      this.headless = headless;
-    })
   }
 }
