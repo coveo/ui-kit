706728ac24292898a4fae34d4ec71c325ccc1f64
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-01-04 15:30:49 -0500

    doc(headless): add dateFilter & numericFilter React samples
    https://coveord.atlassian.net/browse/KIT-793

---
+++ packages/samples/headless-react/src/components/date-filter/date-filter.fn.tsx
@@ -0,0 +1,78 @@
+import {useEffect, useState, FunctionComponent, Fragment} from 'react';
+import {
+  buildDateRange,
+  DateFilter as HeadlessDateFilter,
+} from '@coveo/headless';
+import dayjs from 'dayjs';
+
+interface DateFilterProps {
+  controller: HeadlessDateFilter;
+}
+
+function formattedDateValue(date?: string | Date) {
+  if (!date) {
+    return '';
+  }
+  return dayjs(date).format('YYYY-MM-DD');
+}
+
+export const DateFilter: FunctionComponent<DateFilterProps> = (props) => {
+  const {controller} = props;
+  const [state, setState] = useState(controller.state);
+  let startRef: HTMLInputElement;
+  let endRef: HTMLInputElement;
+
+  useEffect(() => controller.subscribe(() => setState(controller.state)), []);
+
+  const {range} = state;
+
+  return (
+    <Fragment>
+      <input
+        key="start"
+        type="Date"
+        ref={(ref) => (startRef = ref!)}
+        defaultValue={formattedDateValue(range?.start)}
+        placeholder="Start"
+      />
+      <input
+        key="end"
+        type="Date"
+        ref={(ref) => (endRef = ref!)}
+        defaultValue={formattedDateValue(range?.end)}
+        placeholder="End"
+      />
+      <button
+        key="apply"
+        onClick={() => {
+          if (!startRef.validity.valid || !endRef.validity.valid) {
+            return;
+          }
+
+          controller.setRange(
+            buildDateRange({
+              start: startRef.valueAsDate!,
+              end: endRef.valueAsDate!,
+            })
+          );
+        }}
+      >
+        Apply
+      </button>
+    </Fragment>
+  );
+};
+
+// usage
+
+/**
+ * ```tsx
+ * const controller = buildDateFilter(engine, {
+ *   options: {
+ *     field: 'date',
+ *   },
+ * });
+ *
+ * <DateFilter controller={controller} />;
+ * ```
+ */
