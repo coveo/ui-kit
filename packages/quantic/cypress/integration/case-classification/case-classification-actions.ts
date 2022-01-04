736e6de08804275378ac419b21b6822e63aba8e8
MehdiMitiche <gm_mitiche@esi.dz>
2022-01-04 09:25:56 -0500

    SFINT-4118 logActions added

--- packages/quantic/cypress/integration/case-classification/case-classification-actions.ts
+++ packages/quantic/cypress/integration/case-classification/case-classification-actions.ts
@@ -5,15 +5,38 @@ import {
 
 function caseClassificationActions(selector: CaseClassificationSelector) {
   return {
-    clickSuggestion: (idx: number) => selector.suggestedOption(idx).click(),
-    clickInlineOption: (idx: number) => selector.inlineOption(idx).click(),
-    clickSelectTitle: () => selector.selectTitle().click(),
-    openSelectInput: () => selector.selectInput().click(),
-    clickSelectOption: (idx: number) => selector.selectOption(idx).click(),
+    clickSuggestion: (idx: number) =>
+      selector
+        .suggestedOption(idx)
+        .click()
+        .logAction('When clicking on the suggestion at the specified index'),
+    clickInlineOption: (idx: number) =>
+      selector
+        .inlineOption(idx)
+        .click()
+        .logAction('When clicking the inline option at the specified index'),
+    clickSelectTitle: () =>
+      selector
+        .selectTitle()
+        .click()
+        .logAction('When clicking on the select title'),
+    openSelectInput: () =>
+      selector
+        .selectInput()
+        .click()
+        .logAction('When clicking on the select input to open the options'),
+    clickSelectOption: (idx: number) =>
+      selector
+        .selectOption(idx)
+        .click()
+        .logAction('When clicking on the select option at the specified index'),
     reportValidity: () =>
-      selector.get().then((el) => {
-        el[0].reportValidity();
-      }),
+      selector
+        .get()
+        .then((el) => {
+          el[0].reportValidity();
+        })
+        .logAction('When reporting the validity of the input'),
   };
 }
 
