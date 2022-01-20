9ba5ab1095e7780072c7cc55db09a1a427eebb88
Benjamin Taillon <btaillon@coveo.com>
2022-01-20 15:48:27 -0500

    fixed breadbox tests
    https://coveord.atlassian.net/browse/KIT-1347

--- packages/atomic/cypress/integration/breadbox/breadbox-assertions.ts
+++ packages/atomic/cypress/integration/breadbox/breadbox-assertions.ts
@@ -1,6 +1,5 @@
 import {should} from '../common-assertions';
 import {
-  BaseFacetSelector,
   FacetWithCheckboxSelector,
   FacetWithLinkSelector,
 } from '../facets/facet-common-assertions';
@@ -128,7 +127,7 @@ export function assertSelectedColorFacetsInBreadcrumb(
 }
 
 export function assertDeselectCheckboxFacet(
-  BaseFacetSelector: BaseFacetSelector,
+  FacetWithCheckboxSelector: FacetWithCheckboxSelector,
   index: number
 ) {
   it('should be deselected after removing from breadcrumb list', () => {
@@ -136,12 +135,11 @@ export function assertDeselectCheckboxFacet(
       .invoke('text')
       .then((value) => {
         deselectBreadcrumbAtIndex(index);
-        BaseFacetSelector.valueLabel()
-          .contains(value)
-          .parent()
-          .parent()
-          .find('[part="value-checkbox"]')
-          .should('have.attr', 'aria-checked', 'false');
+        FacetWithCheckboxSelector.checkboxValueWithText(value).should(
+          'have.attr',
+          'aria-checked',
+          'false'
+        );
       });
   });
 }
@@ -160,14 +158,11 @@ export function assertDeselectLinkFacet(
   it('should be deselected after removing from breadcrumb list', () => {
     BreadboxSelectors.breadcrumbValueAtIndex(index)
       .invoke('text')
-      .then((value) => {
+      .then((text) => {
         deselectBreadcrumbAtIndex(index);
-        FacetWithLinkSelector.selectedLinkValue()
-          .contains(value)
-          .parent()
-          .parent()
-          .find('[part="value-link"]')
-          .should('have.attr', 'aria-pressed', 'false');
+        FacetWithLinkSelector.selectedLinkValueWithText(text).should(
+          'not.exist'
+        );
       });
   });
 }
@@ -176,14 +171,9 @@ export function assertDeselectColorFacet(index: number) {
   it('should be deselected after removing from breadcrumb list', () => {
     BreadboxSelectors.breadcrumbValueAtIndex(index)
       .invoke('text')
-      .then((value) => {
+      .then((text) => {
         deselectBreadcrumbAtIndex(index);
-        ColorFacetSelectors.valueLabel()
-          .contains(value)
-          .parent()
-          .parent()
-          .find('[part="value-box"]')
-          .should('have.attr', 'aria-pressed', 'false');
+        ColorFacetSelectors.selectedBoxValueWithText(text).should('not.exist');
       });
   });
 }
