c39afe02fcf6f362f0c8915884342f6be90c2e52
olamothe <olamothe@coveo.com>
2022-02-04 15:14:11 -0500

    docs(atomic-angular): sample doc with atomic-angular
    https://coveord.atlassian.net/browse/KIT-1402

--- packages/samples/angular/src/app/app.module.ts
+++ packages/samples/angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {AtomicAngularModule} from '@coveo/atomic-angular';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -7,7 +8,7 @@ import {AtomicAngularPageComponent} from './atomic-angular-page/atomic-angular-p
 
 @NgModule({
   declarations: [AppComponent, AtomicAngularPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, AtomicAngularModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- packages/samples/angular/src/app/atomic-angular-page/atomic-angular-page.component.ts
+++ packages/samples/angular/src/app/atomic-angular-page/atomic-angular-page.component.ts
@@ -1,12 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-atomic-angular-page',
   templateUrl: './atomic-angular-page.component.html',
   styleUrls: ['./atomic-angular-page.component.css'],
 })
-export class AtomicAngularPageComponent implements OnInit {
+export class AtomicAngularPageComponent implements AfterViewInit {
   constructor() {}
-
-  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    const searchInterface = document.querySelector('atomic-search-interface');
+    searchInterface
+      ?.initialize({
+        accessToken: 'xxc23ce82a-3733-496e-b37e-9736168c4fd9',
+        organizationId: 'electronicscoveodemocomo0n2fu8v',
+      })
+      .then(() => {
+        searchInterface.executeFirstSearch();
+      });
+  }
 }
