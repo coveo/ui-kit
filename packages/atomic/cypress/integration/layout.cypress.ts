b30bb296709caadedfbf2b5c12029110793ed743
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-02-15 14:15:43 -0500

    add layout html file and test 2 viewports
    https://coveord.atlassian.net/browse/KIT-1419

--- packages/atomic/cypress/integration/layout.cypress.ts
+++ packages/atomic/cypress/integration/layout.cypress.ts
@@ -1,11 +1,25 @@
 import {RouteAlias, setupIntercept} from '../utils/setupComponent';
 
 describe('Layouts', () => {
-  it('should compare screenshot of a search layout', () => {
+  it('should compare screenshot of a search layout on desktop', () => {
+    cy.viewport('macbook-13');
     setupIntercept();
-    cy.visit('/index.html');
+    cy.visit('/layouts/search.html');
     cy.wait(RouteAlias.analytics);
-    cy.wait(1000);
-    cy.get('atomic-search-interface').compareSnapshot('search-layout', 0.15);
+    cy.get('atomic-search-interface').compareSnapshot(
+      'search-layout-desktop',
+      0.1
+    );
+  });
+
+  it('should compare screenshot of a search layout on mobile', () => {
+    cy.viewport('iphone-8');
+    setupIntercept();
+    cy.visit('/layouts/search.html');
+    cy.wait(RouteAlias.analytics);
+    cy.get('atomic-search-interface').compareSnapshot(
+      'search-layout-mobile',
+      0.1
+    );
   });
 });
