9d221ecb4210bfa364c6a9747b3b2f2cb02f27fb
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-02-17 14:25:45 -0500

    pr fixes
    https://coveord.atlassian.net/browse/KIT-1419

--- packages/atomic/cypress/integration/layout.cypress.ts
+++ packages/atomic/cypress/integration/layout.cypress.ts
@@ -4,22 +4,24 @@ describe('Layouts', () => {
   it('should compare screenshot of a search layout on desktop', () => {
     cy.viewport('macbook-13');
     setupIntercept();
-    cy.visit('/layouts/search.html');
+    cy.visit('/tests/search-layout.html');
     cy.wait(RouteAlias.analytics);
+    cy.wait(1000);
     cy.get('atomic-search-interface').compareSnapshot(
       'search-layout-desktop',
-      0.2
+      0.1
     );
   });
 
   it('should compare screenshot of a search layout on mobile', () => {
     cy.viewport('iphone-8');
     setupIntercept();
-    cy.visit('/layouts/search.html');
+    cy.visit('/tests/search-layout.html');
     cy.wait(RouteAlias.analytics);
+    cy.wait(1000);
     cy.get('atomic-search-interface').compareSnapshot(
       'search-layout-mobile',
-      0.2
+      0.1
     );
   });
 });
