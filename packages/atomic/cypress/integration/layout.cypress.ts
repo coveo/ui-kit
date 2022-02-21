9cbb01f2a34ee4465011220d8e2cf5db24bc831b
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-02-21 16:47:40 -0500

    update tests
    https://coveord.atlassian.net/browse/KIT-1419

--- packages/atomic/cypress/integration/layout.cypress.ts
+++ packages/atomic/cypress/integration/layout.cypress.ts
@@ -1,21 +1,12 @@
 import {RouteAlias, setupIntercept} from '../utils/setupComponent';
 
 describe('Layouts', () => {
-  it('should compare screenshot of a search layout on desktop', () => {
-    cy.viewport('macbook-13');
+  it('should compare screenshots', () => {
     setupIntercept();
     cy.visit('/tests/search-layout.html');
     cy.wait(RouteAlias.analytics);
     cy.wait(1000);
-    cy.percySnapshot();
-  });
-
-  it('should compare screenshot of a search layout on mobile', () => {
-    cy.viewport('iphone-8');
-    setupIntercept();
-    cy.visit('/tests/search-layout.html');
-    cy.wait(RouteAlias.analytics);
-    cy.wait(1000);
-    cy.percySnapshot();
+    cy.contains('atomic-search-interface');
+    cy.percySnapshot('Search layout', {widths: [500, 1200]});
   });
 });
