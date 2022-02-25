e7fb30be5a218abe9a7a5f45be66ef189be40f8e
btaillon <54454747+btaillon@users.noreply.github.com>
2022-02-25 14:17:31 -0500

    Update packages/headless/src/controllers/core/facets/category-facet/headless-core-category-facet.ts
    Co-authored-by: jpmarceau <39384459+jpmarceau@users.noreply.github.com>

--- packages/headless/src/controllers/core/facets/category-facet/headless-core-category-facet.ts
+++ packages/headless/src/controllers/core/facets/category-facet/headless-core-category-facet.ts
@@ -112,7 +112,7 @@ export interface CoreCategoryFacet extends Controller {
   showLessValues(): void;
 
   /**
-   * Enables the facet, undoing the effects of `disable`
+   * Enables the facet. I.e., undoes the effects of `disable`.
    */
   enable(): void;
 
