3e9f3987d74d0d44f4c88f4ab7ced719a1eeb88c
olamothe <olamothe@coveo.com>
2022-03-01 16:51:02 -0500

    linting

--- packages/atomic/src/components/facets/facet-value-label-highlight/facet-value-label-highlight.tsx
+++ packages/atomic/src/components/facets/facet-value-label-highlight/facet-value-label-highlight.tsx
@@ -7,16 +7,17 @@ interface FacetValueLabelHighlightProps {
   isSelected: boolean;
 }
 
-export const FacetValueLabelHighlight: FunctionalComponent<FacetValueLabelHighlightProps> =
-  (props) => {
-    return (
-      <span
-        title={props.displayValue}
-        part="value-label"
-        class={`value-label truncate group-hover:text-primary group-focus:text-primary ${
-          props.isSelected ? 'font-bold' : ''
-        }`}
-        innerHTML={highlightSearchResult(props.displayValue, props.searchQuery)}
-      ></span>
-    );
-  };
+export const FacetValueLabelHighlight: FunctionalComponent<
+  FacetValueLabelHighlightProps
+> = (props) => {
+  return (
+    <span
+      title={props.displayValue}
+      part="value-label"
+      class={`value-label truncate group-hover:text-primary group-focus:text-primary ${
+        props.isSelected ? 'font-bold' : ''
+      }`}
+      innerHTML={highlightSearchResult(props.displayValue, props.searchQuery)}
+    ></span>
+  );
+};
--- packages/samples/headless-react/src/components/standalone-search-box/standalone-search-box.fn.tsx
+++ packages/samples/headless-react/src/components/standalone-search-box/standalone-search-box.fn.tsx
@@ -6,53 +6,51 @@ import {
 import {AppContext} from '../../context/engine';
 import {standaloneSearchBoxStorageKey} from './standalone-search-box-storage-key';
 
-export const StandaloneSearchBox: FunctionComponent<StandaloneSearchBoxOptions> =
-  (props) => {
-    const {engine} = useContext(AppContext);
-    const controller = buildStandaloneSearchBox(engine!, {options: props});
-    const [state, setState] = useState(controller.state);
+export const StandaloneSearchBox: FunctionComponent<
+  StandaloneSearchBoxOptions
+> = (props) => {
+  const {engine} = useContext(AppContext);
+  const controller = buildStandaloneSearchBox(engine!, {options: props});
+  const [state, setState] = useState(controller.state);
 
-    useEffect(() => controller.subscribe(() => setState(controller.state)), []);
+  useEffect(() => controller.subscribe(() => setState(controller.state)), []);
 
-    function isEnterKey(e: React.KeyboardEvent<HTMLInputElement>) {
-      return e.key === 'Enter';
-    }
+  function isEnterKey(e: React.KeyboardEvent<HTMLInputElement>) {
+    return e.key === 'Enter';
+  }
 
-    if (!state) {
-      return null;
-    }
+  if (!state) {
+    return null;
+  }
 
-    if (state.redirectTo) {
-      const {redirectTo, value, analytics} = state;
-      const data = JSON.stringify({value, analytics});
-      localStorage.setItem(standaloneSearchBoxStorageKey, data);
-      window.location.href = redirectTo;
-      return null;
-    }
+  if (state.redirectTo) {
+    const {redirectTo, value, analytics} = state;
+    const data = JSON.stringify({value, analytics});
+    localStorage.setItem(standaloneSearchBoxStorageKey, data);
+    window.location.href = redirectTo;
+    return null;
+  }
 
-    return (
-      <div>
-        <input
-          value={state.value}
-          onChange={(e) => controller.updateText(e.target.value)}
-          onKeyDown={(e) => isEnterKey(e) && controller.submit()}
-        />
-        <ul>
-          {state.suggestions.map((suggestion) => {
-            const value = suggestion.rawValue;
-            return (
-              <li
-                key={value}
-                onClick={() => controller.selectSuggestion(value)}
-              >
-                {value}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  };
+  return (
+    <div>
+      <input
+        value={state.value}
+        onChange={(e) => controller.updateText(e.target.value)}
+        onKeyDown={(e) => isEnterKey(e) && controller.submit()}
+      />
+      <ul>
+        {state.suggestions.map((suggestion) => {
+          const value = suggestion.rawValue;
+          return (
+            <li key={value} onClick={() => controller.selectSuggestion(value)}>
+              {value}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 // usage
 
