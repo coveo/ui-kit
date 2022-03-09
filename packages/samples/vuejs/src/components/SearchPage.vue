6cafb8e1c7aa1a87f3a41c214c818edebd7da124
Livia Rett <livia@qubit.com>
2022-03-09 18:16:21 +0000

    docs(vuejs): create simple search page
    https://coveord.atlassian.net/browse/KIT-1465

---
+++ packages/samples/vuejs/src/components/SearchPage.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import {onMounted} from 'vue';
+
+async function initInterface() {
+  await customElements.whenDefined('atomic-search-interface');
+  const searchInterface = document.querySelector('atomic-search-interface');
+
+  // Initialization
+  await searchInterface.initialize({
+    accessToken: 'xx564559b1-0045-48e1-953c-3addd1ee4457',
+    organizationId: 'searchuisamples',
+  });
+
+  // Trigger a first search
+  searchInterface.executeFirstSearch();
+}
+
+const resultTemplate = `
+      <template>
+        <p>Title:</p>
+        <atomic-result-link></atomic-result-link>
+      </template>`;
+
+onMounted(initInterface);
+</script>
+
+<template>
+  <atomic-search-interface>
+    <div class="facets">
+      <atomic-facet field="author" label="Author"></atomic-facet>
+    </div>
+    <div class="main">
+      <atomic-query-summary></atomic-query-summary>
+      <atomic-result-list>
+        <atomic-result-template v-html="resultTemplate">
+        </atomic-result-template>
+      </atomic-result-list>
+    </div>
+  </atomic-search-interface>
+</template>
