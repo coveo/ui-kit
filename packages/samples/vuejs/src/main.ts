eb1b363242666fe32ea2df47f410a0cad75f7dbf
Livia Rett <livia@qubit.com>
2022-03-14 16:23:07 +0000

    docs(vuejs): add an example of custom component in result template
    https://coveord.atlassian.net/browse/KIT-1474

--- packages/samples/vuejs/src/main.ts
+++ packages/samples/vuejs/src/main.ts
@@ -1,5 +1,9 @@
-import {createApp} from 'vue';
+import {createApp, defineCustomElement} from 'vue';
 import App from './App.vue';
+import ResultLabel from './components/ResultLabel.vue';
+
+const resultLabel = defineCustomElement(ResultLabel);
+customElements.define('result-label', resultLabel);
 
 const app = createApp(App);
 
