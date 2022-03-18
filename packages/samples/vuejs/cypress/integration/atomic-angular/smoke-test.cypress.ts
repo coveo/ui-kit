bb8dc125b6b5d81f5bf5d67a21c34bc595f3a534
Livia Rett <livia@qubit.com>
2022-03-18 14:06:21 +0000

    docs(vuejs): update server port
    https://coveord.atlassian.net/browse/KIT-1493

--- packages/samples/vuejs/cypress/integration/atomic-angular/smoke-test.cypress.ts
+++ packages/samples/vuejs/cypress/integration/atomic-angular/smoke-test.cypress.ts
@@ -15,7 +15,7 @@ describe('smoke test', {viewportHeight: 2000, viewportWidth: 2000}, () => {
       path: '**/rest/ua/v15/analytics/*',
     }).as('analytics');
 
-    cy.visit('http://localhost:8081/').wait('@analytics');
+    cy.visit('http://localhost:8080/').wait('@analytics');
     cy.get('atomic-search-box')
       .should('exist')
       .shadow()
