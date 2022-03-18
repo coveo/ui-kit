d6e9577de6a7bdbb31fc2e72dc698f84a0eb7325
Olivier Lamothe <olamothe@coveo.com>
2022-03-18 16:19:13 -0400

    chore(headless): update esbuild (#1835)
    https://coveord.atlassian.net/browse/KIT-1450

--- scripts/bundle/umd.js
+++ scripts/bundle/umd.js
@@ -1,23 +1,15 @@
-function umdWrapper(globalName) {
-  const header = `(function (global, factory) {
-    const isCommonjs = typeof exports === 'object' && typeof module !== 'undefined';
-    
-    if (isCommonjs) {
-      return factory(exports);
+function umdFooter(globalName) {
+  const footer = `(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+      define(factory);
+    } else if (typeof module === 'object' && module.exports) {
+      module.exports = factory();
+    } else {
+      root.${globalName} = factory();
     }
-    
-    const isAmd = typeof define === 'function' && define.amd;
+  }(typeof self !== 'undefined' ? self : this, () => ${globalName}));`;
 
-    if (isAmd) {
-      return define(['exports'], factory);
-    }
-
-    global = typeof globalThis !== 'undefined' ? globalThis : global || self;
-    factory(global.${globalName} = {});
-  })(this, (function (exports) { 'use strict';`;
-  const footer = `}));`;
-
-  return {header, footer};
+  return footer;
 }
 
-module.exports = {umdWrapper};
+module.exports = {umdFooter};
