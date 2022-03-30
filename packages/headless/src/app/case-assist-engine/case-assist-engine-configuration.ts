d1865036ad52d81fad439ee2d461dcb991242a25
MehdiMitiche <gm_mitiche@esi.dz>
2022-03-30 15:23:19 -0400

    feat(headless, quantic): search hub added to payload of case assist analytics

--- packages/headless/src/app/case-assist-engine/case-assist-engine-configuration.ts
+++ packages/headless/src/app/case-assist-engine/case-assist-engine-configuration.ts
@@ -24,6 +24,14 @@ export interface CaseAssistEngineConfiguration extends EngineConfiguration {
    *  Moreover, this information can be referred to in query expressions and QPL statements by using the $locale object.
    */
   locale?: string;
+  /**
+   * The first level of origin of the request, typically the identifier of the graphical search interface from which the request originates.
+   * Coveo Machine Learning models use this information to provide contextually relevant output.
+   * Notes:
+   *    This parameter will be overridden if the search request is authenticated by a search token that enforces a specific searchHub.
+   *    When logging a Search usage analytics event for a query, the originLevel1 field of that event should be set to the value of the searchHub search request parameter.
+   */
+  searchHub?: string;
 }
 
 export const caseAssistEngineConfigurationSchema =
