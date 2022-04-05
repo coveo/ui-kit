3eb8509be0efe5cd56fb2200cb717659613616a3
Livia Rett <livia@qubit.com>
2022-04-05 09:59:47 +0100

    docs(atomic): add tests for content before and after children
    https://coveord.atlassian.net/browse/KIT-1521

--- packages/atomic/cypress/integration/result-list/folded-result-list.cypress.ts
+++ packages/atomic/cypress/integration/result-list/folded-result-list.cypress.ts
@@ -70,4 +70,62 @@ describe('Folded Result List Component', () => {
       .find('atomic-result-link')
       .contains('Elf Cups and Allies');
   });
+
+  it('renders content before and after children only when there are children', () => {
+    const components = makeChildComponents();
+
+    const before = generateComponentHTML('p');
+    before.slot = 'before-children';
+    before.textContent = 'Before children!';
+    components.insertAdjacentElement('afterbegin', before);
+
+    const after = generateComponentHTML('p');
+    after.slot = 'after-children';
+    after.textContent = 'After children!';
+    components.insertAdjacentElement('beforeend', after);
+
+    new TestFixture()
+      .with(
+        addFoldedResultList(
+          buildTemplateWithoutSections([
+            generateComponentHTML('atomic-result-link'),
+            components,
+          ])
+        )
+      )
+      .withCustomFoldedResponse(withFoldedResult)
+      .init();
+
+    FoldedResultListSelectors.firstResult()
+      .find('atomic-result-children')
+      .children()
+      .eq(0)
+      .contains('Before children!')
+      .should('have.attr', 'slot', 'before-children');
+
+    FoldedResultListSelectors.firstResult()
+      .find('atomic-result-children')
+      .shadow()
+      .find('slot')
+      .eq(0)
+      .should('have.attr', 'name', 'before-children');
+
+    FoldedResultListSelectors.firstResult()
+      .find('atomic-result-children')
+      .children()
+      .eq(2)
+      .contains('After children!')
+      .should('have.attr', 'slot', 'after-children');
+
+    FoldedResultListSelectors.firstResult()
+      .find('atomic-result-children')
+      .shadow()
+      .find('slot')
+      .eq(1)
+      .should('have.attr', 'name', 'after-children');
+
+    getAtomicResultAtIndex(FoldedResultListSelectors, 1)
+      .find('atomic-result-children')
+      .should('not.exist');
+  });
 });
