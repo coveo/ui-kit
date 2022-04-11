1de728fb48849827ee8198d04f6f4873e05d38a1
Olivier Lamothe <olamothe@coveo.com>
2022-04-11 12:56:34 -0400

    remove comment
    https://coveord.atlassian.net/browse/KIT-1557

--- packages/samples/headless-react/jest.setup.js
+++ packages/samples/headless-react/jest.setup.js
@@ -1,8 +1,5 @@
 const Environment = require('jest-environment-jsdom');
 
-/**
- * A custom environment to set the TextEncoder that is required by TensorFlow.js.
- */
 module.exports = class CustomTestEnvironment extends Environment {
   async setup() {
     await super.setup();
