687595c0d10dcaf368540c60238e17b0725eba1c
Livia Rett <30511433+liviarett@users.noreply.github.com>
2022-04-25 09:54:03 +0100

    feat(atomic): add styling and states for children results and loading (#1939)
    * feat(atomic): add styling and states for children results and loading
    
    https://coveord.atlassian.net/browse/KIT-1539
    
    * dont show load more button when there is no collection
    
    https://coveord.atlassian.net/browse/KIT-1539
    
    * fix extraneous row in mobile result and add spacing between results
    
    https://coveord.atlassian.net/browse/KIT-1539
    
    * keep initial results when collapsing children
    
    https://coveord.atlassian.net/browse/KIT-1539
    
    * Add generated files
    
    * quick refactor from review
    
    https://coveord.atlassian.net/browse/KIT-1539
    
    * fix logic for collapse button
    
    https://coveord.atlassian.net/browse/KIT-1539
    
    * Update storybook
    
    https://coveord.atlassian.net/browse/KIT-1539
    
    * Update packages/atomic/src/components/result-lists/atomic-result-children/atomic-result-children.tsx
    
    Co-authored-by: Olivier Lamothe <olamothe@coveo.com>
    
    * Update packages/atomic/src/components/result-lists/atomic-result-children/atomic-result-children.tsx
    
    Co-authored-by: Olivier Lamothe <olamothe@coveo.com>
    
    * update
    
    https://coveord.atlassian.net/browse/KIT-1539
    
    * update
    
    https://coveord.atlassian.net/browse/KIT-1539
    
    Co-authored-by: GitHub Actions Bot <>
    Co-authored-by: Olivier Lamothe <olamothe@coveo.com>

--- packages/atomic/src/components/atomic-load-more-children-results/atomic-load-more-children-results.tsx
+++ packages/atomic/src/components/atomic-load-more-children-results/atomic-load-more-children-results.tsx
@@ -2,9 +2,14 @@ import {Result, FoldedResultListState} from '@coveo/headless';
 import {Component, Element, h, Prop, State} from '@stencil/core';
 import {buildCustomEvent} from '../../utils/event-utils';
 import {Bindings, InitializeBindings} from '../../utils/initialization-utils';
+import {getResultDisplayClasses} from '../atomic-result/atomic-result-display-options';
 import {Button} from '../common/button';
 import {FoldedResultListStateContext} from '../result-lists/result-list-decorators';
-import {ResultContext} from '../result-template-components/result-template-decorators';
+import {
+  DisplayConfig,
+  ResultContext,
+  ResultDisplayConfigContext,
+} from '../result-template-components/result-template-decorators';
 
 /**
  * The `atomic-load-more-children-results` component allows to load the full collection for a folded result.
@@ -13,6 +18,8 @@ import {ResultContext} from '../result-template-components/result-template-decor
  */
 @Component({
   tag: 'atomic-load-more-children-results',
+  styleUrl: 'atomic-load-more-children-results.pcss',
+  shadow: true,
 })
 export class AtomicLoadMoreChildrenResults {
   @InitializeBindings() public bindings!: Bindings;
@@ -24,6 +31,9 @@ export class AtomicLoadMoreChildrenResults {
   @State()
   private foldedResultListState!: FoldedResultListState;
 
+  @ResultDisplayConfigContext()
+  private displayConfig!: DisplayConfig;
+
   /**
    * The label for the button used to load more results.
    *
@@ -32,6 +42,9 @@ export class AtomicLoadMoreChildrenResults {
   @Prop() public label = '';
 
   private getCollection() {
+    if (!this.result.raw.foldingcollection) {
+      return null;
+    }
     return this.foldedResultListState.results.find(
       (r) =>
         r.result.raw.foldingcollection === this.result.raw.foldingcollection
@@ -45,25 +58,45 @@ export class AtomicLoadMoreChildrenResults {
   }
 
   private hasMoreResults() {
-    return this.getCollection().moreResultsAvailable;
+    return this.getCollection()?.moreResultsAvailable;
   }
 
   private isLoading() {
-    return Boolean(this.getCollection().isLoadingMoreResults);
+    return Boolean(this.getCollection()?.isLoadingMoreResults);
+  }
+
+  private hasResults() {
+    return Boolean(this.getCollection()?.children.length);
+  }
+
+  private getButtonClass() {
+    if (this.hasResults()) {
+      return 'has-children';
+    }
+    return '';
+  }
+  private getWrapperClass() {
+    return getResultDisplayClasses(
+      'list',
+      this.displayConfig.density,
+      this.displayConfig.imageSize
+    ).join(' ');
   }
 
   public render() {
-    if (!this.foldedResultListState) {
+    if (!this.foldedResultListState || !this.getCollection()) {
       return null;
     }
 
     return (
-      <div>
-        {this.hasMoreResults() && !this.isLoading() && (
+      <div class={this.getWrapperClass()}>
+        {this.hasMoreResults() && (
           <Button
+            part="button"
             style="text-primary"
             onClick={() => this.loadFullCollection()}
-            part="button"
+            class={this.getButtonClass()}
+            disabled={this.isLoading()}
           >
             {this.label || this.bindings.i18n.t('load-all-results')}
           </Button>
