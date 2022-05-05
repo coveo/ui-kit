737b1fc5b5aa90ba78005fb054e9cf8cc757e8b9
btaillon <54454747+btaillon@users.noreply.github.com>
2022-05-05 08:19:03 -0400

    feat(atomic): refactored atomic-modal mobile/desktop look to fullscreen true/false prop (#1955)
    https://coveord.atlassian.net/browse/KIT-1587

--- packages/atomic/src/components/atomic-refine-modal/atomic-refine-modal.tsx
+++ packages/atomic/src/components/atomic-refine-modal/atomic-refine-modal.tsx
@@ -243,6 +243,7 @@ export class AtomicRefineModal implements InitializableComponent {
   public render() {
     return (
       <atomic-modal
+        fullscreen
         isOpen={this.isOpen}
         source={this.openButton}
         close={() => (this.isOpen = false)}
