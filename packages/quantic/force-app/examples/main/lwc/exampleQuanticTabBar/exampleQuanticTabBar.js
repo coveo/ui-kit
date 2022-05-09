07bdeb7a8e8904ae67d20d4a9747db23aabdc4d1
mmitiche <86681870+mmitiche@users.noreply.github.com>
2022-05-09 09:34:53 -0400

    Update packages/quantic/force-app/examples/main/lwc/exampleQuanticTabBar/exampleQuanticTabBar.js
    Co-authored-by: Luc Bergeron <10946843+lbergeron@users.noreply.github.com>

--- packages/quantic/force-app/examples/main/lwc/exampleQuanticTabBar/exampleQuanticTabBar.js
+++ packages/quantic/force-app/examples/main/lwc/exampleQuanticTabBar/exampleQuanticTabBar.js
@@ -6,7 +6,7 @@ export default class ExampleQuanticTabBar extends LightningElement {
   isConfigured = false;
 
   pageTitle = 'Quantic Tab Bar';
-  pageDescription = 'The QuanticTabBar component displays the Quantic Tabs in a responsive manner by showing a drop-down list that will display overflowing tabs on smaller screens..';
+  pageDescription = 'The QuanticTabBar component displays the Quantic Tabs in a responsive manner by showing a drop-down list that will display overflowing tabs on smaller screens.';
 
   handleTryItNow(evt) {
     this.config = evt.detail;
