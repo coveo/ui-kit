43da82478c9070378a5667a4effc4aaa2e871d4d
Luc Bergeron <10946843+lbergeron@users.noreply.github.com>
2022-05-25 10:28:47 -0400

    feat(headless): add insight engine

--- packages/headless/src/state/state-sections.ts
+++ packages/headless/src/state/state-sections.ts
@@ -40,6 +40,7 @@ import {CaseInputState} from '../features/case-input/case-input-state';
 import {DocumentSuggestionState} from '../features/document-suggestion/document-suggestion-state';
 import {ExcerptLengthState} from '../features/excerpt-length/excerpt-length-state';
 import {InstantResultsState} from '../features/instant-results/instant-results-state';
+import {InsightConfigurationState} from '../features/insight-configuration/insight-configuration-state';
 
 export interface QuerySection {
   /**
@@ -357,4 +358,11 @@ export interface ExcerptLengthSection {
    * The index includes the top most interesting sentences (in the order they appear in the item) that fit in the specified number of characters.
    */
   excerptLength: ExcerptLengthState;
+}
+
+export interface InsightConfigurationSection {
+  /**
+   * The insight engine configuration.
+   */
+  insightConfiguration: InsightConfigurationState;
 }
