68d9356eaa22e0acd1e243b5be497468e13ce996
Olivier Lamothe <olamothe@coveo.com>
2022-06-03 09:17:37 -0400

    remove temp
    https://coveord.atlassian.net/browse/KIT-1701

--- packages/headless/src/features/product-recommendations/product-recommendations-analytics.actions.ts
+++ packages/headless/src/features/product-recommendations/product-recommendations-analytics.actions.ts
@@ -1,11 +1,15 @@
+import {
+  ProductRecommendationAnalyticsProvider,
+  StateNeededByProductRecommendationsAnalyticsProvider,
+} from '../../api/analytics/product-recommendations-analytics';
 import {AnalyticsType, makeAnalyticsAction} from '../analytics/analytics-utils';
 
 export const logProductRecommendations = makeAnalyticsAction(
   'analytics/productrecommendations/load',
   AnalyticsType.Search,
-  (client) => client.logRecommendationInterfaceLoad()
-  /*(state) =>
+  (client) => client.logRecommendationInterfaceLoad(),
+  (state) =>
     new ProductRecommendationAnalyticsProvider(
       state as StateNeededByProductRecommendationsAnalyticsProvider
-    )*/
+    )
 );
