7ad02d92d5f8c2b1f5fe19e2ee5837fae412cdef
mmitiche <mmitiche@coveo.com>
2022-06-09 09:36:03 -0400

    SFINT-4542 require.resolve statement simplified

--- packages/quantic/scripts/npm/utils.js
+++ packages/quantic/scripts/npm/utils.js
@@ -1,25 +1,7 @@
-const pathlib = require('path');
-
 function quanticIsDependency() {
   try {
     // path to npm scripts is expected to be: project_name/node_modules/@coveo/quantic/scripts/npm
-    const upperDirectories = __dirname.split(pathlib.sep);
-    const dirCountAfterNodeModulesParent = 5;
-    const nodeModulesParent = upperDirectories.slice(
-      0,
-      -dirCountAfterNodeModulesParent
-    );
-    require.resolve(
-      pathlib.join(
-        nodeModulesParent.join(pathlib.sep),
-        'node_modules',
-        '@coveo',
-        'quantic',
-        'scripts',
-        'npm',
-        'utils.js'
-      )
-    );
+    require.resolve('@coveo/quantic/scripts/npm/utils.js');
     return true;
   } catch (err) {
     if (err.code !== 'MODULE_NOT_FOUND') {
