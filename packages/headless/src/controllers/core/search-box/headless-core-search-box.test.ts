8cc766a94835544e4847ee5014d0ba6475bdebe7
Luc Bergeron <10946843+lbergeron@users.noreply.github.com>
2022-06-09 16:56:28 -0400

    chore(headless): extract core search-box controller

---
+++ packages/headless/src/controllers/core/search-box/headless-core-search-box.test.ts
@@ -0,0 +1,306 @@
+import {
+  CoreSearchBox,
+  CoreSearchBoxProps,
+  SearchBoxOptions,
+  buildCoreSearchBox,
+} from './headless-core-search-box';
+import {
+  registerQuerySuggest,
+  clearQuerySuggest,
+  fetchQuerySuggestions,
+  selectQuerySuggestion,
+} from '../../../features/query-suggest/query-suggest-actions';
+import {createMockState} from '../../../test/mock-state';
+import {executeSearch} from '../../../features/search/search-actions';
+import {updateQuery} from '../../../features/query/query-actions';
+import {
+  registerQuerySetQuery,
+  updateQuerySetQuery,
+} from '../../../features/query-set/query-set-actions';
+import {buildMockQuerySuggest} from '../../../test/mock-query-suggest';
+import {
+  buildMockSearchAppEngine,
+  MockSearchEngine,
+} from '../../../test/mock-engine';
+import {updatePage} from '../../../features/pagination/pagination-actions';
+import {SearchAppState} from '../../../state/search-app-state';
+import {
+  configuration,
+  query,
+  querySet,
+  querySuggest,
+  search,
+} from '../../../app/reducers';
+import {deselectAllBreadcrumbs} from '../../../features/breadcrumb/breadcrumb-actions';
+import {updateFacetAutoSelection} from '../../../features/facets/generic/facet-actions';
+
+describe('headless CoreSearchBox', () => {
+  const id = 'search-box-123';
+  let state: SearchAppState;
+
+  let engine: MockSearchEngine;
+  let searchBox: CoreSearchBox;
+  let props: CoreSearchBoxProps;
+
+  beforeEach(() => {
+    const options: SearchBoxOptions = {
+      id,
+      numberOfSuggestions: 10,
+      highlightOptions: {
+        notMatchDelimiters: {
+          open: '<a>',
+          close: '<a>',
+        },
+        correctionDelimiters: {
+          open: '<i>',
+          close: '<i>',
+        },
+      },
+    };
+
+    props = {
+      options,
+      executeSearchActionCreator: executeSearch,
+      fetchQuerySuggestionsActionCreator: fetchQuerySuggestions,
+    };
+
+    initState();
+    initController();
+  });
+
+  function initState() {
+    state = createMockState();
+    state.querySet[id] = 'query';
+    state.querySuggest[id] = buildMockQuerySuggest({
+      id,
+      q: 'some value',
+      completions: [
+        {
+          expression: 'a',
+          score: 0,
+          executableConfidence: 0,
+          highlighted: '[hi]{light}(ed)',
+        },
+      ],
+    });
+  }
+
+  function initController() {
+    engine = buildMockSearchAppEngine({state});
+    searchBox = buildCoreSearchBox(engine, props);
+  }
+
+  it('it adds the correct reducers to engine', () => {
+    expect(engine.addReducers).toHaveBeenCalledWith({
+      query,
+      querySuggest,
+      configuration,
+      querySet,
+      search,
+    });
+  });
+
+  describe('validating options', () => {
+    it(`when passing an invalid id as option
+      creating the controller should throw`, () => {
+      props.options!.id = 1 as unknown as string;
+      expect(() => initController()).toThrow();
+    });
+
+    it(`when passing an invalid numberOfSuggestions as option
+      creating the controller should throw`, () => {
+      props.options!.numberOfSuggestions = -2;
+      expect(() => initController()).toThrow();
+    });
+
+    it(`when passing an invalid highlightOptions as option
+      creating the controller should throw`, () => {
+      props.options!.highlightOptions = {
+        notMatchDelimiters: {
+          open: 1 as unknown as string,
+          close: 2 as unknown as string,
+        },
+      };
+      expect(() => initController()).toThrow();
+    });
+
+    it('when passing an invalid option, it throws an error', () => {
+      props.options!.id = 1 as unknown as string;
+      expect(() => initController()).toThrow(
+        'Check the options of buildSearchBox'
+      );
+    });
+  });
+
+  it('should return the right state', () => {
+    expect(searchBox.state).toEqual({
+      value: state.querySet[id],
+      suggestions: state.querySuggest[id]!.completions.map((completion) => ({
+        highlightedValue: '<a>hi<a>light<i>ed<i>',
+        rawValue: completion.expression,
+      })),
+      isLoading: false,
+      isLoadingSuggestions: false,
+    });
+  });
+
+  it('should dispatch a registerQuerySetQuery action at initialization', () => {
+    expect(engine.actions).toContainEqual(
+      registerQuerySetQuery({id, query: state.query.q})
+    );
+  });
+
+  it('should dispatch a registerQuerySuggest action at initialization', () => {
+    expect(engine.actions).toContainEqual(
+      registerQuerySuggest({
+        id,
+        q: state.query.q,
+        count: props.options!.numberOfSuggestions,
+      })
+    );
+  });
+
+  describe('when calling updateText', () => {
+    it('updates the search box query in the querySet', () => {
+      const text = 'query';
+      searchBox.updateText(text);
+
+      const action = updateQuerySetQuery({id, query: text});
+      expect(engine.actions).toContainEqual(action);
+    });
+
+    it('should call the showSuggestions method', () => {
+      jest.spyOn(searchBox, 'showSuggestions');
+      searchBox.updateText('how can i fix');
+
+      expect(searchBox.showSuggestions).toHaveBeenCalled();
+    });
+  });
+
+  it(`when calling clear
+      should dispatch a updateQuerySetQuery action`, () => {
+    searchBox.clear();
+    expect(engine.actions).toContainEqual(
+      updateQuerySetQuery({id: id, query: ''})
+    );
+  });
+
+  it(`when calling clear
+      should dispatch a clearQuerySuggest action`, () => {
+    searchBox.clear();
+    expect(engine.actions).toContainEqual(clearQuerySuggest({id}));
+  });
+
+  describe('#showSuggestions', () => {
+    it(`when numberOfQuerySuggestions is greater than 0,
+      it dispatches fetchQuerySuggestions`, async () => {
+      searchBox.showSuggestions();
+
+      const action = engine.actions.find(
+        (a) => a.type === fetchQuerySuggestions.pending.type
+      );
+      expect(action).toEqual(
+        fetchQuerySuggestions.pending(action!.meta.requestId, {id})
+      );
+    });
+
+    it(`when numberOfQuerySuggestions is 0,
+      it does not dispatch fetchQuerySuggestions`, () => {
+      props.options!.numberOfSuggestions = 0;
+      initController();
+
+      searchBox.showSuggestions();
+
+      const action = engine.actions.find(
+        (a) => a.type === fetchQuerySuggestions.pending.type
+      );
+
+      expect(action).toBe(undefined);
+    });
+  });
+
+  describe('#selectSuggestion', () => {
+    it('dispatches a selectQuerySuggestion action', () => {
+      const value = 'i like this expression';
+      searchBox.selectSuggestion(value);
+
+      expect(engine.actions).toContainEqual(
+        selectQuerySuggestion({id, expression: value})
+      );
+    });
+
+    it('dispatches executeSearch', () => {
+      const suggestion = 'a';
+      searchBox.selectSuggestion(suggestion);
+
+      expect(engine.findAsyncAction(executeSearch.pending)).toBeTruthy();
+    });
+  });
+
+  describe('when calling submit', () => {
+    it('it deselects all facets', () => {
+      searchBox.submit();
+
+      expect(engine.actions).toContainEqual(deselectAllBreadcrumbs());
+    });
+
+    it('allows autoSelection', () => {
+      searchBox.submit();
+
+      expect(engine.actions).toContainEqual(
+        updateFacetAutoSelection({allow: true})
+      );
+    });
+
+    it('autoSelection should be allowed after deselecting facets', () => {
+      searchBox.submit();
+
+      const deselectAllBreadcrumbsIndex = engine.actions.findIndex(
+        (action) => action.type === deselectAllBreadcrumbs.type
+      );
+      const updateFacetAutoSelectionIndex = engine.actions.findIndex(
+        (action) => action.type === updateFacetAutoSelection.type
+      );
+      expect(deselectAllBreadcrumbsIndex).toBeLessThanOrEqual(
+        updateFacetAutoSelectionIndex
+      );
+    });
+
+    it('dispatches updateQuery with the correct parameters', () => {
+      const expectedQuery = state.querySet[id];
+      searchBox.submit();
+
+      const action = updateQuery({
+        q: expectedQuery,
+        enableQuerySyntax: false,
+      });
+
+      expect(engine.actions).toContainEqual(action);
+    });
+
+    it('updates the page to the first one', () => {
+      searchBox.submit();
+      expect(engine.actions).toContainEqual(updatePage(1));
+    });
+
+    it('it dispatches an executeSearch action', () => {
+      searchBox.submit();
+
+      const action = engine.actions.find(
+        (a) => a.type === executeSearch.pending.type
+      );
+      expect(action).toBeTruthy();
+    });
+
+    it('it dispatches a clear suggestions action', () => {
+      searchBox.submit();
+      expect(engine.actions).toContainEqual(clearQuerySuggest({id}));
+    });
+  });
+
+  it(`when querySuggest #isLoading state is true,
+    #state.isLoadingSuggestions is true`, () => {
+    state.querySuggest[id]!.isLoading = true;
+    expect(searchBox.state.isLoadingSuggestions).toBe(true);
+  });
+});
---
+++ packages/headless/src/controllers/core/search-box/headless-core-search-box.ts
@@ -0,0 +1,271 @@
+import {
+  defaultSearchBoxOptions,
+  SearchBoxOptions,
+  searchBoxOptionsSchema,
+} from './headless-core-search-box-options';
+import {
+  SuggestionHighlightingOptions,
+  Delimiters,
+  getHighlightedSuggestion,
+} from '../../../utils/highlight';
+import {
+  buildController,
+  Controller,
+} from '../../controller/headless-controller';
+import {CoreEngine} from '../../..';
+import {
+  ConfigurationSection,
+  QuerySection,
+  QuerySetSection,
+  QuerySuggestionSection,
+  SearchSection,
+} from '../../../state/state-sections';
+import {
+  configuration,
+  query,
+  querySet,
+  querySuggest,
+  search,
+} from '../../../app/reducers';
+import {loadReducerError} from '../../../utils/errors';
+import {randomID} from '../../../utils/utils';
+import {validateOptions} from '../../../utils/validate-payload';
+import {
+  registerQuerySetQuery,
+  updateQuerySetQuery,
+} from '../../../features/query-set/query-set-actions';
+import {
+  clearQuerySuggest,
+  FetchQuerySuggestionsActionCreatorPayload,
+  registerQuerySuggest,
+  selectQuerySuggestion,
+} from '../../../features/query-suggest/query-suggest-actions';
+import {SearchAction} from '../../../features/analytics/analytics-utils';
+import {prepareForSearchWithQuery} from '../../../features/search/search-actions';
+import {logQuerySuggestionClick} from '../../../features/query-suggest/query-suggest-analytics-actions';
+import {logSearchboxSubmit} from '../../../features/query/query-analytics-actions';
+import {QuerySuggestState} from '../../../features/query-suggest/query-suggest-state';
+import {AsyncThunkAction} from '@reduxjs/toolkit';
+
+export type {SearchBoxOptions, SuggestionHighlightingOptions, Delimiters};
+
+export interface CoreSearchBoxProps {
+  /**
+   * The `CoreSearchBox` controller options.
+   */
+  options?: SearchBoxOptions;
+
+  executeSearchActionCreator?: (
+    arg: SearchAction
+  ) => AsyncThunkAction<any, SearchAction, any>;
+  fetchQuerySuggestionsActionCreator?: (
+    arg: FetchQuerySuggestionsActionCreatorPayload
+  ) => AsyncThunkAction<any, FetchQuerySuggestionsActionCreatorPayload, any>;
+}
+
+/**
+ * A scoped and simplified part of the headless state that is relevant to the `SearchBox` controller.
+ */
+export interface SearchBoxState {
+  /**
+   * The current query in the search box.
+   */
+  value: string;
+
+  /**
+   * The query suggestions for the search box query.
+   */
+  suggestions: Suggestion[];
+
+  /**
+   * Determines if a search is in progress.
+   */
+  isLoading: boolean;
+
+  /**
+   * Determines if a query suggest request is in progress.
+   */
+  isLoadingSuggestions: boolean;
+}
+
+export interface Suggestion {
+  /**
+   * The suggestion after applying any `highlightOptions`.
+   */
+  highlightedValue: string;
+
+  /**
+   * The suggestion text.
+   */
+  rawValue: string;
+}
+
+/**
+ * The `SearchBox` headless controller offers a high-level interface for designing a common search box UI controller
+ * with [highlighting for query suggestions](https://docs.coveo.com/en/headless/latest/usage/highlighting/).
+ */
+export interface CoreSearchBox extends Controller {
+  /**
+   * Updates the search box text value and shows the suggestions for that value.
+   *
+   * @param value - The string value to update the search box with.
+   */
+  updateText(value: string): void;
+
+  /**
+   * Clears the search box text and the suggestions.
+   */
+  clear(): void;
+
+  /**
+   * Shows the suggestions for the current search box value.
+   */
+  showSuggestions(): void;
+
+  /**
+   * Selects a suggestion and calls `submit`.
+   *
+   * @param value - The string value of the suggestion to select
+   */
+  selectSuggestion(value: string): void;
+
+  /**
+   * Deselects all facets and triggers a search query.
+   */
+  submit(): void;
+
+  /**
+   * The state of the `SearchBox` controller.
+   */
+  state: SearchBoxState;
+}
+
+export function buildCoreSearchBox(
+  engine: CoreEngine,
+  props: CoreSearchBoxProps = {}
+): CoreSearchBox {
+  if (!loadSearchBoxReducers(engine)) {
+    throw loadReducerError;
+  }
+
+  const controller = buildController(engine);
+  const {dispatch} = engine;
+  const getState = () => engine.state;
+
+  const id = props.options?.id || randomID('search_box');
+  const options: Required<SearchBoxOptions> = {
+    id,
+    highlightOptions: {...props.options?.highlightOptions},
+    ...defaultSearchBoxOptions,
+    ...props.options,
+  };
+
+  validateOptions(engine, searchBoxOptionsSchema, options, 'buildSearchBox');
+  dispatch(registerQuerySetQuery({id, query: engine.state.query.q}));
+  if (options.numberOfSuggestions) {
+    dispatch(
+      registerQuerySuggest({
+        id,
+        q: engine.state.query.q,
+        count: options.numberOfSuggestions,
+      })
+    );
+  }
+
+  const getValue = () => engine.state.querySet[options.id];
+
+  const performSearch = async (analytics: SearchAction) => {
+    const {enableQuerySyntax} = options;
+
+    dispatch(prepareForSearchWithQuery({q: getValue(), enableQuerySyntax}));
+
+    if (props.executeSearchActionCreator) {
+      await dispatch(props.executeSearchActionCreator(analytics));
+    }
+  };
+
+  return {
+    ...controller,
+
+    updateText(value: string) {
+      dispatch(updateQuerySetQuery({id, query: value}));
+      this.showSuggestions();
+    },
+
+    clear() {
+      dispatch(updateQuerySetQuery({id, query: ''}));
+      dispatch(clearQuerySuggest({id}));
+    },
+
+    showSuggestions() {
+      if (options.numberOfSuggestions) {
+        if (props.fetchQuerySuggestionsActionCreator) {
+          dispatch(props.fetchQuerySuggestionsActionCreator({id}));
+        }
+      }
+    },
+
+    selectSuggestion(value: string) {
+      dispatch(selectQuerySuggestion({id, expression: value}));
+      performSearch(logQuerySuggestionClick({id, suggestion: value})).then(
+        () => {
+          dispatch(clearQuerySuggest({id}));
+        }
+      );
+    },
+
+    submit() {
+      performSearch(logSearchboxSubmit());
+      dispatch(clearQuerySuggest({id}));
+    },
+
+    get state() {
+      const state = getState();
+      const querySuggest = state.querySuggest[options.id];
+      const suggestions = getSuggestions(
+        querySuggest,
+        options.highlightOptions
+      );
+      const isLoadingSuggestions = querySuggest
+        ? querySuggest.isLoading
+        : false;
+
+      return {
+        value: getValue(),
+        suggestions,
+        isLoading: state.search.isLoading,
+        isLoadingSuggestions,
+      };
+    },
+  };
+}
+
+function getSuggestions(
+  state: QuerySuggestState | undefined,
+  highlightOptions: SuggestionHighlightingOptions
+) {
+  if (!state) {
+    return [];
+  }
+
+  return state.completions.map((completion) => ({
+    highlightedValue: getHighlightedSuggestion(
+      completion.highlighted,
+      highlightOptions
+    ),
+    rawValue: completion.expression,
+  }));
+}
+
+function loadSearchBoxReducers(
+  engine: CoreEngine
+): engine is CoreEngine<
+  QuerySection &
+    QuerySuggestionSection &
+    ConfigurationSection &
+    QuerySetSection &
+    SearchSection
+> {
+  engine.addReducers({query, querySuggest, configuration, querySet, search});
+  return true;
+}
