6389b97fb8ecea94f9cb156b100288d570649cf7
Livia Rett <livia@qubit.com>
2022-06-14 19:00:35 +0100

    docs(atomic): move search box tests to separate page
    https://coveord.atlassian.net/browse/KIT-1649

--- packages/atomic/cypress/integration/query-summary.cypress.ts
+++ packages/atomic/cypress/integration/query-summary.cypress.ts
@@ -2,7 +2,7 @@ import {generateComponentHTML, TestFixture} from '../fixtures/test-fixture';
 import {addQuerySummary} from './query-summary-actions';
 import {QuerySummarySelectors} from './query-summary-selectors';
 import * as CommonAssertions from './common-assertions';
-import {addSearchBox} from './search-box-actions';
+import {addSearchBox} from './search-box/search-box-actions';
 
 const addResultsPerPage = (count: number) => (fixture: TestFixture) => {
   fixture.withElement(
