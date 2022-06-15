83118fe15a054e4a46e79af8b193c07e6631add2
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-06-15 13:18:52 -0400

    add index example, use slot
    https://coveord.atlassian.net/browse/KIT-1722

--- packages/atomic/src/components/facets/atomic-segmented-facet-scrollable/atomic-segmented-facet-scrollable.tsx
+++ packages/atomic/src/components/facets/atomic-segmented-facet-scrollable/atomic-segmented-facet-scrollable.tsx
@@ -2,31 +2,21 @@ import {Component, Element, h, State} from '@stencil/core';
 
 @Component({
   tag: 'atomic-segmented-facet-scrollable',
-  shadow: false,
+  shadow: true,
 })
 export class AtomicSegmentedFacetScrollable {
   @Element() private host!: HTMLElement;
   @State() children: Array<any> = [];
 
-  componentWillLoad() {
-    this.children = Array.from(
-      this.host.getElementsByTagName('atomic-segmented-facet')
-    );
-
-    console.log(this.children.map((child) => child.label));
-
-    this.children.forEach(this.host.removeChild);
+  connectedCallback() {
+    this.children = Array.from(this.host.children);
+    console.log(this.children);
   }
 
   render() {
     return (
       <div class="flex overflow-x-scroll">
-        {this.children.map((child) => (
-          <atomic-segmented-facet
-            label={child.label}
-            field={child.field}
-          ></atomic-segmented-facet>
-        ))}
+        <slot></slot>
       </div>
     );
   }
