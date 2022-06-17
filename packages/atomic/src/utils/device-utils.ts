fa1f361708a5f0112e0876dd5368c92a7ecb2ea6
Benjamin Taillon <btaillon@coveo.com>
2022-06-17 11:37:37 -0400

    replaced isIOS
    https://coveord.atlassian.net/browse/KIT-1726

--- packages/atomic/src/utils/device-utils.ts
+++ packages/atomic/src/utils/device-utils.ts
@@ -1,11 +1,15 @@
 export function isIOS() {
-  // Source: https://stackoverflow.com/a/58065241
-  const platform = navigator.platform;
-  if (!platform) {
-    return false;
-  }
-  return (
-    /iPad|iPhone|iPod/.test(platform) ||
-    (platform === 'MacIntel' && navigator.maxTouchPoints > 1)
-  );
+  // Source: https://stackoverflow.com/a/62094756
+  const iosQuirkPresent = () => {
+    const audio = new Audio();
+
+    audio.volume = 0.5;
+    return audio.volume === 1; // volume cannot be changed from "1" on iOS 12 and below
+  };
+
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const isAppleDevice = navigator.userAgent.includes('Macintosh');
+  const isTouchScreen = navigator.maxTouchPoints >= 1; // true for iOS 13 (and hopefully beyond)
+
+  return isIOS || (isAppleDevice && (isTouchScreen || iosQuirkPresent()));
 }
