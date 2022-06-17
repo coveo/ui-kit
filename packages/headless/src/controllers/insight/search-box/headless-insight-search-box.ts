35b6bafdf6d406d6c43838e448549fc826a872ba
Luc Bergeron <10946843+lbergeron@users.noreply.github.com>
2022-06-17 13:52:13 -0400

    chore(headless): simplify code

--- packages/headless/src/controllers/insight/search-box/headless-insight-search-box.ts
+++ packages/headless/src/controllers/insight/search-box/headless-insight-search-box.ts
@@ -3,9 +3,9 @@ import {
   executeSearch,
   fetchQuerySuggestions,
 } from '../../../features/insight-search/insight-search-actions';
-import {Controller} from '../../controller/headless-controller';
 import {
   buildCoreSearchBox,
+  CoreSearchBox,
   Delimiters,
   SearchBoxState,
   Suggestion,
@@ -32,41 +32,7 @@ export interface InsightSearchBoxProps {
  * The `InsightSearchBox` headless controller offers a high-level interface for designing a common search box UI controller
  * with [highlighting for query suggestions](https://docs.coveo.com/en/headless/latest/usage/highlighting/).
  */
-export interface InsightSearchBox extends Controller {
-  /**
-   * Updates the search box text value and shows the suggestions for that value.
-   *
-   * @param value - The string value to update the search box with.
-   */
-  updateText(value: string): void;
-
-  /**
-   * Clears the search box text and the suggestions.
-   */
-  clear(): void;
-
-  /**
-   * Shows the suggestions for the current search box value.
-   */
-  showSuggestions(): void;
-
-  /**
-   * Selects a suggestion and calls `submit`.
-   *
-   * @param value - The string value of the suggestion to select
-   */
-  selectSuggestion(value: string): void;
-
-  /**
-   * Deselects all facets and triggers a search query.
-   */
-  submit(): void;
-
-  /**
-   * The state of the `SearchBox` controller.
-   */
-  state: SearchBoxState;
-}
+export interface InsightSearchBox extends CoreSearchBox {}
 
 /**
  * Creates an `InsightSearchBox` controller instance.
