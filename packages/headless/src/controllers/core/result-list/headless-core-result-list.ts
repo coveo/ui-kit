6b3e29f764825f2636bd64d6148a3e1c3ff96a83
Luc Bergeron <10946843+lbergeron@users.noreply.github.com>
2022-06-17 09:48:25 -0400

    chore(headless): silence linter

--- packages/headless/src/controllers/core/result-list/headless-core-result-list.ts
+++ packages/headless/src/controllers/core/result-list/headless-core-result-list.ts
@@ -44,6 +44,7 @@ export interface CoreResultListProps {
   /**
    * The action creator to build the `fetchMoreResults` action.
    */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   fetchMoreResultsActionCreator?: () => AsyncThunkAction<any, void, any>;
 }
 
