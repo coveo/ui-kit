6a1b60afbd9f25a6f4f794059011ca24599b7916
Livia Rett <livia@qubit.com>
2022-06-24 11:33:05 +0100

    fix(atomic): dedup recent queries keys
    https://coveord.atlassian.net/browse/KIT-1788

--- packages/atomic/src/utils/string-utils.ts
+++ packages/atomic/src/utils/string-utils.ts
@@ -9,3 +9,19 @@ export function cleanUpString(str: string) {
     .replace(/\s/g, '-')
     .toLocaleLowerCase();
 }
+
+// https://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+export function hashString(str: string) {
+  let hash = 0,
+    i,
+    chr;
+  if (str.length === 0) {
+    return hash;
+  }
+  for (i = 0; i < str.length; i++) {
+    chr = str.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    // hash |= 0; // Convert to 32bit integer
+  }
+  return JSON.stringify(hash);
+}
