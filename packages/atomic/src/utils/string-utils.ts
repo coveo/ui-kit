d70adaf18d5324a5fc1487ab45e7dbdf7c7f4d01
Livia Rett <livia@qubit.com>
2022-06-27 17:01:14 +0100

    update algorithm
    https://coveord.atlassian.net/browse/KIT-1788

--- packages/atomic/src/utils/string-utils.ts
+++ packages/atomic/src/utils/string-utils.ts
@@ -10,18 +10,20 @@ export function cleanUpString(str: string) {
     .toLocaleLowerCase();
 }
 
-// https://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
-export function hashString(str: string) {
-  let hash = 0,
-    i,
-    chr;
-  if (str.length === 0) {
-    return hash;
+// https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript/52171480#52171480/
+export function hashString(str: string, seed = 0) {
+  let h1 = 0xdeadbeef ^ seed,
+    h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
   }
-  for (i = 0; i < str.length; i++) {
-    chr = str.charCodeAt(i);
-    hash = (hash << 5) - hash + chr;
-    // hash |= 0; // Convert to 32bit integer
-  }
-  return JSON.stringify(hash);
+  h1 =
+    Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^
+    Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 =
+    Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^
+    Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
 }
