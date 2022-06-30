5b79250df834875cbf3240a0b1b5dcc14709c5c3
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-06-30 16:07:36 -0400

    upgrade lint
    https://coveord.atlassian.net/browse/KIT-1797

--- packages/samples/atomic-next/pages/index.tsx
+++ packages/samples/atomic-next/pages/index.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
-const DynamicSearchPage = dynamic(() => import('../components/search-page'), {
+const DynamicSearchPage = dynamic(() => import('./search-page'), {
   ssr: false,
 });
 
