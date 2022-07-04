6c7c6e9c86bcdd55f2e96d03e4ce32a59cd03250
Livia Rett <livia@qubit.com>
2022-07-04 18:27:56 +0100

    chore(atomic): update stencil config to remove ts error
    https://coveord.atlassian.net/browse/KIT-1813

--- packages/atomic/stencil.config.ts
+++ packages/atomic/stencil.config.ts
@@ -122,12 +122,14 @@ export const config: Config = {
       copy: [
         {src: 'pages', keepDirStructure: false},
         {src: 'themes'},
-        isDevWatch && {src: 'external-builds', dest: 'build/headless'},
+        isDevWatch
+          ? {src: 'external-builds', dest: 'build/headless'}
+          : {src: ''},
         {
           src: '../node_modules/@salesforce-ux/design-system/assets/icons/{doctype,standard}/*.svg',
           dest: 'build/assets',
         },
-      ].filter((n) => n),
+      ].filter((n) => n.src),
     },
   ],
   testing: {
@@ -198,6 +200,13 @@ export const config: Config = {
                   replacement: path.resolve(
                     __dirname,
                     './src/external-builds/product-recommendation/headless.esm.js'
+                  ),
+                },
+                {
+                  find: '@coveo/headless/insight',
+                  replacement: path.resolve(
+                    __dirname,
+                    './src/external-builds/insight/headless.esm.js'
                   ),
                 },
                 {
