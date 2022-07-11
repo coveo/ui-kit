63b1f12c6650c3208912a23a57f0b37374c2604e
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-07-11 16:15:41 -0400

    scope atomic & headless
    https://coveord.atlassian.net/browse/KIT-1827

--- scripts/snyk/remove-samples-workspace.js
+++ scripts/snyk/remove-samples-workspace.js
@@ -5,7 +5,9 @@ const {cwd} = require('process');
 const packageJsonPath = resolve(cwd(), 'package.json');
 
 const packageJSON = JSON.parse(readFileSync(packageJsonPath));
-packageJSON.workspaces = packageJSON.workspaces.filter(wp => wp !== 'packages/samples/*');
+packageJSON.workspaces = packageJSON.workspaces.filter((wp) =>
+  ['packages/headless', 'packages/atomic'].includes(wp)
+);
 writeFileSync(
   packageJsonPath,
   JSON.stringify(packageJSON, undefined, 2) + '\n'
