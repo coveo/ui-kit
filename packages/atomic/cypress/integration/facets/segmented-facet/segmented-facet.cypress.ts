c688f674869e783e56e7a8d483ef84c1d12fcaf4
mbenkirane-coveo <97299176+mbenkirane-coveo@users.noreply.github.com>
2022-07-11 13:09:29 -0400

    formatting/renaming
    https://coveord.atlassian.net/browse/KIT-1736

--- packages/atomic/cypress/integration/facets/segmented-facet/segmented-facet.cypress.ts
+++ packages/atomic/cypress/integration/facets/segmented-facet/segmented-facet.cypress.ts
@@ -3,36 +3,48 @@ import {
   segmentedFacetComponent,
   SegmentedFacetSelectors,
 } from './segmented-facet-selectors';
-import {
-  addSegmentedFacet,
-  field,
-  label,
-  defaultNumberOfValues,
-} from './segmented-facet-actions';
+import {addSegmentedFacet, field, label} from './segmented-facet-actions';
 import * as CommonAssertions from '../../common-assertions';
 import * as CommonFacetAssertions from '../facet-common-assertions';
 
-describe('Segmented Facet v2 Test Suites', () => {
-  describe('with checkbox values', () => {
-    function setupWithCheckboxValues() {
-      new TestFixture().with(addSegmentedFacet({field, label})).init();
-    }
+describe('Segmented Facet Test Suites', () => {
+  function setupSegmentedFacet() {
+    new TestFixture().with(addSegmentedFacet({field, label})).init();
+  }
 
-    describe('verify rendering', () => {
-      before(setupWithCheckboxValues);
+  describe('verify rendering', () => {
+    before(setupSegmentedFacet);
 
-      CommonAssertions.assertAccessibility(segmentedFacetComponent);
-      CommonAssertions.assertContainsComponentError(
-        SegmentedFacetSelectors,
-        false
-      );
-      CommonFacetAssertions.assertDisplayFacet(SegmentedFacetSelectors, true);
-      CommonFacetAssertions.assertLabelContains(SegmentedFacetSelectors, label);
-      CommonFacetAssertions.assertDisplayValues(SegmentedFacetSelectors, true);
-      CommonFacetAssertions.assertDisplayPlaceholder(
-        SegmentedFacetSelectors,
-        false
-      );
-    });
+    CommonAssertions.assertAccessibility(segmentedFacetComponent);
+    CommonAssertions.assertContainsComponentError(
+      SegmentedFacetSelectors,
+      false
+    );
+    CommonFacetAssertions.assertDisplayFacet(SegmentedFacetSelectors, true);
+    CommonFacetAssertions.assertLabelContains(SegmentedFacetSelectors, label);
+    CommonFacetAssertions.assertDisplayValues(SegmentedFacetSelectors, true);
+    CommonFacetAssertions.assertDisplayPlaceholder(
+      SegmentedFacetSelectors,
+      false
+    );
   });
+
+  describe('when select a value', () => {
+    describe('verify rendering', () => {});
+    describe('verify analytic', () => {});
+  });
+
+  describe('with custom #numberOfValues', () => {});
+
+  describe('with no label', () => {});
+
+  describe('when field returns no results', () => {});
+
+  describe('with a selected path in the URL', () => {});
+
+  describe('with depends-on', () => {});
+
+  describe('when the dependency is met', () => {});
+
+  describe('with two dependencies', () => {});
 });
