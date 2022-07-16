0cee5372308fd40f7e7e2b66278ea98a7606ea24
mbenkirane-coveo <97299176+mbenkirane-coveo@users.noreply.github.com>
2022-07-16 14:59:44 -0400

    added tests for segmented facets
    https://coveord.atlassian.net/browse/KIT-1857

--- packages/atomic/cypress/integration/facets/segmented-facet/segmented-facet.cypress.ts
+++ packages/atomic/cypress/integration/facets/segmented-facet/segmented-facet.cypress.ts
@@ -3,9 +3,17 @@ import {
   segmentedFacetComponent,
   SegmentedFacetSelectors,
 } from './segmented-facet-selectors';
-import {addSegmentedFacet, field, label} from './segmented-facet-actions';
+import {
+  addSegmentedFacet,
+  defaultNumberOfValues,
+  field,
+  label,
+  selectIdleBoxValueAt,
+} from './segmented-facet-actions';
 import * as CommonAssertions from '../../common-assertions';
 import * as CommonFacetAssertions from '../facet-common-assertions';
+import * as FacetAssertions from './segmented-facet-assertions';
+import {selectIdleCheckboxValueAt} from '../facet-common-actions';
 
 describe('Segmented Facet Test Suites', () => {
   function setupSegmentedFacet() {
@@ -29,22 +37,193 @@ describe('Segmented Facet Test Suites', () => {
     );
   });
 
-  describe.skip('when select a value', () => {
-    describe('verify rendering', () => {});
-    describe('verify analytic', () => {});
+  describe('when selecting a value', () => {
+    const selectionIndex = 2;
+    function setupSelectSegmentedFacet() {
+      setupSegmentedFacet();
+      selectIdleBoxValueAt(selectionIndex);
+    }
+
+    describe('verify rendering', () => {
+      before(setupSelectSegmentedFacet);
+
+      CommonAssertions.assertAccessibility(segmentedFacetComponent);
+      FacetAssertions.assertNumberOfSelectedBoxValues(1);
+      FacetAssertions.assertNumberOfIdleBoxValues(defaultNumberOfValues - 1);
+    });
+    describe('verify analytics', () => {
+      before(setupSelectSegmentedFacet);
+      FacetAssertions.assertLogFacetSelect(field, selectionIndex);
+    });
   });
 
-  describe.skip('with custom #numberOfValues', () => {});
+  describe('with custom #numberOfValues', () => {
+    const customNumValues = 20;
+    function setupCustomSegmentedFacet() {
+      new TestFixture()
+        .with(
+          addSegmentedFacet({
+            field: 'language',
+            label: 'Language',
+            'number-of-values': customNumValues,
+          })
+        )
+        .init();
+    }
 
-  describe.skip('with no label', () => {});
+    describe('verify rendering', () => {
+      before(setupCustomSegmentedFacet);
 
-  describe.skip('when field returns no results', () => {});
+      CommonAssertions.assertAccessibility(segmentedFacetComponent);
+      CommonFacetAssertions.assertLabelContains(
+        SegmentedFacetSelectors,
+        'Language'
+      );
+      FacetAssertions.assertNumberOfIdleBoxValues(customNumValues);
+    });
+  });
 
-  describe.skip('with a selected path in the URL', () => {});
+  describe.skip('when no search has yet been executed', () => {
+    before(() => {
+      new TestFixture()
+        .with(addSegmentedFacet({field, label}))
+        .withoutFirstAutomaticSearch()
+        .init();
+    });
 
-  describe.skip('with depends-on', () => {});
+    CommonFacetAssertions.assertDisplayFacet(SegmentedFacetSelectors, false);
+    CommonFacetAssertions.assertDisplayPlaceholder(
+      SegmentedFacetSelectors,
+      true
+    );
+  });
 
-  describe.skip('when the dependency is met', () => {});
+  describe('with no label', () => {
+    function setupSegmentedFacetNoLabel() {
+      new TestFixture().with(addSegmentedFacet({field})).init();
+    }
 
-  describe.skip('with two dependencies', () => {});
+    describe('verify rendering', () => {
+      before(setupSegmentedFacetNoLabel);
+
+      CommonAssertions.assertAccessibility(segmentedFacetComponent);
+      CommonFacetAssertions.assertDisplayFacet(SegmentedFacetSelectors, true);
+      FacetAssertions.assertLabelExists(false);
+      FacetAssertions.assertNumberOfIdleBoxValues(defaultNumberOfValues);
+    });
+  });
+
+  describe('when field returns no results', () => {
+    before(() => {
+      new TestFixture()
+        .with(addSegmentedFacet({field: 'notanactualfield', label}))
+        .init();
+    });
+
+    CommonFacetAssertions.assertDisplayFacet(SegmentedFacetSelectors, false);
+    CommonFacetAssertions.assertDisplayPlaceholder(
+      SegmentedFacetSelectors,
+      false
+    );
+    CommonAssertions.assertContainsComponentError(
+      SegmentedFacetSelectors,
+      false
+    );
+  });
+
+  describe('with a selected path in the URL', () => {
+    before(() => {
+      new TestFixture()
+        .with(addSegmentedFacet({field, label}))
+        .withHash(`f[${field}]=Cervantes`)
+        .init();
+    });
+
+    CommonFacetAssertions.assertDisplayFacet(SegmentedFacetSelectors, true);
+    FacetAssertions.assertNumberOfSelectedBoxValues(1);
+    FacetAssertions.assertNumberOfIdleBoxValues(defaultNumberOfValues - 1);
+    CommonFacetAssertions.assertFirstValueContains(
+      SegmentedFacetSelectors,
+      'Cervantes'
+    );
+  });
+
+  describe('with custom #sortCriteria, occurrences', () => {
+    before(() => {
+      new TestFixture()
+        .with(addSegmentedFacet({field, label, 'sort-criteria': 'occurrences'}))
+        .init();
+    });
+
+    FacetAssertions.assertValuesSortedByOccurrences();
+  });
+
+  describe.skip('with depends-on', () => {
+    describe('as a dependent & parent', () => {
+      const facetId = 'abc';
+      const parentFacetId = 'def';
+      const parentField = 'filetype';
+      const expectedValue = 'txt';
+      before(() => {
+        new TestFixture()
+          .with(
+            addSegmentedFacet({
+              'facet-id': facetId,
+              field,
+              [`depends-on-${parentFacetId}`]: expectedValue,
+            })
+          )
+          .with(
+            addSegmentedFacet({'facet-id': parentFacetId, field: parentField})
+          )
+          .init();
+      });
+
+      CommonFacetAssertions.assertDisplayFacet(
+        SegmentedFacetSelectors.withId(facetId),
+        true
+      );
+      CommonFacetAssertions.assertDisplayFacet(
+        SegmentedFacetSelectors.withId(parentFacetId),
+        true
+      );
+
+      describe('when the dependency is met', () => {
+        before(() => {
+          selectIdleBoxValueAt(0);
+        });
+
+        CommonFacetAssertions.assertDisplayFacet(
+          SegmentedFacetSelectors.withId(facetId),
+          true
+        );
+        CommonFacetAssertions.assertDisplayFacet(
+          SegmentedFacetSelectors.withId(parentFacetId),
+          true
+        );
+      });
+    });
+    describe('with two dependencies', () => {
+      before(() => {
+        new TestFixture()
+          .with(addSegmentedFacet({'facet-id': 'abc', field: 'objecttype'}))
+          .with(addSegmentedFacet({'facet-id': 'def', field: 'filetype'}))
+          .with(
+            addSegmentedFacet({
+              'facet-id': 'ghi',
+              field,
+              'depends-on-objecttype': '',
+              'depends-on-filetype': 'pdf',
+            })
+          )
+          .init();
+      });
+
+      CommonAssertions.assertConsoleError(true);
+      CommonAssertions.assertContainsComponentError(
+        SegmentedFacetSelectors.withId('ghi'),
+        true
+      );
+    });
+  });
 });
