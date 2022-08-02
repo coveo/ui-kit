10c0bca0b4594c55d4fba26941bf7c3b199106ff
mbenkirane-coveo <97299176+mbenkirane-coveo@users.noreply.github.com>
2022-08-02 21:58:41 +0100

    Update packages/atomic/src/components/search/facets/atomic-popover/atomic-popover.tsx
    Co-authored-by: jpmarceau <39384459+jpmarceau@users.noreply.github.com>

--- packages/atomic/src/components/search/facets/atomic-popover/atomic-popover.tsx
+++ packages/atomic/src/components/search/facets/atomic-popover/atomic-popover.tsx
@@ -23,7 +23,7 @@ import {InitPopoverEventPayload} from './popover-event';
  * @part arrow-icon - The arrow icon
  * @part placeholder - The placeholder displayed when the facet isn't ready
  * @part popover-wrapper - The wrapper that contains the value-button and the slot-wrapper
- * @part slot-wrapper - The wrapper that contains the facet/slot
+ * @part slot-wrapper - The wrapper that contains the 'facet' or 'slot'.
  */
 
 @Component({
