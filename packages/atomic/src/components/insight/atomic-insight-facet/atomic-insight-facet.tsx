0167a11d393ee43446cea2b693d35ad0a92425c2
Livia Rett <30511433+liviarett@users.noreply.github.com>
2022-08-03 13:56:54 +0100

    Update packages/atomic/src/components/insight/atomic-insight-facet/atomic-insight-facet.tsx
    Co-authored-by: Jean-Félix <ThibodeauJF@users.noreply.github.com>

--- packages/atomic/src/components/insight/atomic-insight-facet/atomic-insight-facet.tsx
+++ packages/atomic/src/components/insight/atomic-insight-facet/atomic-insight-facet.tsx
@@ -94,7 +94,7 @@ export class AtomicInsightFacet
   @Prop({mutable: true, reflect: true}) public facetId?: string;
   /**
    * The non-localized label for the facet.
-   * Used in the `atomic-breadbox` component through the bindings store.
+   * Used in the `atomic-insight-breadbox` component through the bindings store.
    */
   @Prop({reflect: true}) public label = 'no-label';
   /**
