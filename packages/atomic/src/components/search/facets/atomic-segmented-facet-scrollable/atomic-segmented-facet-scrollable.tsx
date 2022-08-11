2c1c077210a079283a67b7be92b6083a3dd72c50
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-08-11 11:49:12 -0400

    add gap
    https://coveord.atlassian.net/browse/KIT-1942

--- packages/atomic/src/components/search/facets/atomic-segmented-facet-scrollable/atomic-segmented-facet-scrollable.tsx
+++ packages/atomic/src/components/search/facets/atomic-segmented-facet-scrollable/atomic-segmented-facet-scrollable.tsx
@@ -169,7 +169,7 @@ export class AtomicSegmentedFacetScrollable implements InitializableComponent {
         {this.renderArrow('left')}
         <div
           part="horizontal-scroll"
-          class="wrapper-segmented flex flex-row overflow-x-scroll scroll-smooth"
+          class="wrapper-segmented flex flex-row overflow-x-scroll scroll-smooth gap-2"
           ref={(el) => (this.horizontalScroll = el)}
         >
           <slot></slot>
