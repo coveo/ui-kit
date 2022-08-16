3425d088feb9117446f7f6d208883e4172e092f1
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2022-08-16 11:26:51 -0400

    make component internal

--- packages/atomic/src/components/common/atomic-icon-button/atomic-icon-button.tsx
+++ packages/atomic/src/components/common/atomic-icon-button/atomic-icon-button.tsx
@@ -4,9 +4,7 @@ import {AnyBindings} from '../interface/bindings';
 import {Button} from '../button';
 
 /**
- * The `atomic-icon-button` component displays a button and an SVG icon with a 1:1 aspect ratio.
- *
- * This component can display an icon from those available in the Atomic package, from a specific location, or as an inline SVG element.
+ * @internal
  *
  * @part button - The button element.
  * @part badge - The span element that wraps the badge.
@@ -19,39 +17,11 @@ import {Button} from '../button';
 export class AtomicIconButton {
   @InitializeBindings() public bindings!: AnyBindings;
   @State() public error!: Error;
-
-  /**
-   * The callback function to execute when the button is clicked.
-   */
   @Prop({mutable: true}) public clickCallback: () => void = () => {};
-
-  /**
-   * The button title.
-   */
   @Prop({mutable: true}) public tooltip = '';
-
-  /**
-   * The i18next key of the value to use as button label (see [Localization](https://docs.coveo.com/en/atomic/latest/usage/atomic-localization/)).
-   */
   @Prop({mutable: true}) public labelI18nKey!: string;
-
-  /**
-   * The SVG icon to display.
-   *
-   * - Use a value that starts with `http://`, `https://`, `./`, or `../`, to fetch and display an icon from a given location.
-   * - Use a value that starts with `assets://`, to display an icon from the Atomic package.
-   * - Use a stringified SVG to display it directly.
-   */
   @Prop({mutable: true}) public icon!: string;
-
-  /**
-   *
-   */
   @Prop({mutable: true}) public buttonRef?: (el?: HTMLButtonElement) => void;
-
-  /**
-   * The button badge.
-   */
   @Prop({mutable: true}) public badge?: VNode;
 
   public render() {
