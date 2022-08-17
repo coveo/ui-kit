63b973841c507e5d4c1db79cbf07cc8ad1fe58bd
Asma Elfaleh <aelfaleh@coveo.com>
2022-08-17 10:32:16 -0400

    test(quantic):  fix tests

--- packages/quantic/cypress/integration/facets/timeframe-facet/timeframe-facet.cypress.ts
+++ packages/quantic/cypress/integration/facets/timeframe-facet/timeframe-facet.cypress.ts
@@ -27,13 +27,13 @@ describe('quantic-timeframe-facet', () => {
   const pageUrl = 's/quantic-timeframe-facet';
 
   const validRange = {
-    start: '2000-01-01',
-    end: '2000-12-31',
+    start: '01/01/2000',
+    end: '31/12/2000',
     filter: '2000/01/01@00:00:00..2000/12/31@23:59:59',
   };
   const invalidRange = {
-    start: '2000-12-31',
-    end: '2000-01-01',
+    start: '31/12/2000',
+    end: '01/01/2000',
     filter: '2000/12/31@00:00:00..2000/01/01@23:59:59',
   };
 
