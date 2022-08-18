6c69eb3c2273224d011b6128b31138a72a03280f
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2022-08-18 09:32:23 -0400

    typo
    https://coveord.atlassian.net/browse/KIT-1972

--- packages/atomic/src/components/search/result-templates/atomic-result-children-template/atomic-result-children-template.tsx
+++ packages/atomic/src/components/search/result-templates/atomic-result-children-template/atomic-result-children-template.tsx
@@ -7,7 +7,7 @@ import {ResultTemplateCommon} from '../result-template-common';
 /**
  * The `atomic-result-children-template` component determines the format of the child results, depending on the conditions that are defined for each template. A `template` element must be the child of an `atomic-result-children-template`, and an `atomic-result-children` must be the parent of each `atomic-result-children-template`.
  *
- * Note: Any `<script>` tags defined inside of a `<template>` element will not be executed when results are being rendered.*
+ * Note: Any `<script>` tags defined inside of a `<template>` element will not be executed when results are being rendered.
  * @MapProp name: mustMatch;attr: must-match;docs: The field and values that define which result items the condition must be applied to. For example, a template with the following attribute only applies to result items whose `filetype` is `lithiummessage` or `YouTubePlaylist`: `must-match-filetype="lithiummessage,YouTubePlaylist"`;type: Record<string, string[]> ;default: {}
  * @MapProp name: mustNotMatch;attr: must-not-match;docs: The field and values that define which result items the condition must not be applied to. For example, a template with the following attribute only applies to result items whose `filetype` is not `lithiummessage`: `must-not-match-filetype="lithiummessage";type: Record<string, string[]> ;default: {}
  */
