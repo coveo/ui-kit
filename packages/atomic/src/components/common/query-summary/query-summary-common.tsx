59d307d0a35b5a104d7c07f475768eef58d36c0e
Olivier Lamothe <olamothe@coveo.com>
2022-08-25 17:18:43 -0400

    refactor summary-common
    https://coveord.atlassian.net/browse/KIT-1991

--- packages/atomic/src/components/common/query-summary/query-summary-common.tsx
+++ packages/atomic/src/components/common/query-summary/query-summary-common.tsx
@@ -1,6 +1,6 @@
 import {FunctionalComponent, h} from '@stencil/core';
+import {LocalizedString} from '../../../utils/jsx-utils';
 import {AnyBindings} from '../interface/bindings';
-import escape from 'escape-html';
 
 interface QuerySummaryCommonProps {
   bindings: AnyBindings;
@@ -21,87 +21,26 @@ interface QuerySummaryCommonProps {
   setAriaLive: (message: string) => void;
 }
 
+const WrapHighlight: FunctionalComponent<{part?: string}> = (
+  props,
+  children
+) => {
+  return (
+    <span
+      class="font-bold"
+      part={`highlight${props.part ? ` ${props.part}` : ''}`}
+    >
+      {children}
+    </span>
+  );
+};
+
 export const QuerySummaryCommon: FunctionalComponent<
   QuerySummaryCommonProps
 > = (props) => {
-  const getDurationInSeconds = (count: number) =>
-    props.bindings.i18n.t('in-seconds', {count});
-
-  const renderDuration = () => {
-    const shouldShow =
-      props.enableDuration && props.querySummaryState.hasDuration;
-    return `<span class="${
-      shouldShow ? '' : 'hidden'
-    }" part="duration"> ${getDurationInSeconds(
-      props.querySummaryState.durationInSeconds
-    )}</span>`;
-  };
-
-  const wrapHighlight = (content: string) => {
-    return `<span class="font-bold" part="highlight">${content}</span>`;
-  };
-
-  const wrapQuery = (content: string) => {
-    return `<span class="font-bold" part="highlight query">${content}</span>`;
-  };
-
-  const getResultOfProps = () => {
-    const locales = props.bindings.i18n.languages as string[];
-    return {
-      count: props.querySummaryState.lastResult,
-      first: props.querySummaryState.firstResult.toLocaleString(locales),
-      last: props.querySummaryState.lastResult.toLocaleString(locales),
-      total: props.querySummaryState.total.toLocaleString(locales),
-      query: props.querySummaryState.query,
-    };
-  };
-
-  const getHighlightedResultOfProps = () => {
-    const {first, last, total, query, ...options} = getResultOfProps();
-    return {
-      ...options,
-      interpolation: {escapeValue: false},
-      first: wrapHighlight(first),
-      last: wrapHighlight(last),
-      total: wrapHighlight(total),
-      query: wrapQuery(escape(query)),
-    };
-  };
-
-  const getSummary = () => {
-    if (props.querySummaryState.isLoading) {
-      return props.bindings.i18n.t('loading-results');
-    }
-    return props.bindings.i18n.t(
-      props.querySummaryState.hasQuery
-        ? 'showing-results-of-with-query'
-        : 'showing-results-of',
-      getResultOfProps()
-    );
-  };
-
-  const renderHasResults = () => {
-    const content = props.bindings.i18n.t(
-      props.querySummaryState.hasQuery
-        ? 'showing-results-of-with-query'
-        : 'showing-results-of',
-      getHighlightedResultOfProps()
-    );
-
-    return (
-      // deepcode ignore ReactSetInnerHtml: This is not React code
-      <div
-        part="results"
-        class="overflow-hidden overflow-ellipsis"
-        innerHTML={content + renderDuration()}
-      ></div>
-    );
-  };
-
   if (props.querySummaryState.hasError) {
     return;
   }
-
   if (!props.querySummaryState.firstSearchExecuted) {
     return (
       <div
@@ -112,13 +51,43 @@ export const QuerySummaryCommon: FunctionalComponent<
     );
   }
 
+  const key = props.querySummaryState.hasQuery
+    ? 'showing-results-of-with-query'
+    : 'showing-results-of';
+
+  const params = {
+    first: props.querySummaryState.firstResult.toLocaleString(),
+    last: props.querySummaryState.lastResult.toLocaleString(),
+    total: props.querySummaryState.total.toLocaleString(),
+    query: props.querySummaryState.query,
+    count: props.querySummaryState.lastResult,
+  };
+
   if (props.querySummaryState.hasResults) {
-    props.setAriaLive(getSummary());
+    if (props.querySummaryState.isLoading) {
+      props.setAriaLive(props.bindings.i18n.t('loading-results'));
+    } else {
+      props.setAriaLive(props.bindings.i18n.t(key, params));
+    }
   }
+
+  const paramsWithHighlights = {
+    first: <WrapHighlight>{params.first}</WrapHighlight>,
+    last: <WrapHighlight>{params.last}</WrapHighlight>,
+    total: <WrapHighlight>{params.total}</WrapHighlight>,
+    query: <WrapHighlight part="query">{params.query}</WrapHighlight>,
+  };
 
   return (
     <div class="text-on-background" part="container">
-      {props.querySummaryState.hasResults && renderHasResults()}
+      {props.querySummaryState.hasResults && (
+        <LocalizedString
+          key={key}
+          bindings={props.bindings}
+          params={paramsWithHighlights}
+          i18nCount={params.count}
+        />
+      )}
     </div>
   );
 };
