478320b78c1e521fe86bc7bfef42bc58ed25cf8d
Olivier Lamothe <olamothe@coveo.com>
2022-08-26 15:30:07 -0400

    test(atomic): add end to end tests for insights panel
    https://coveord.atlassian.net/browse/KIT-1952

--- packages/atomic/cypress/integration-insights-panel/insight-panel-selectors.ts
+++ packages/atomic/cypress/integration-insights-panel/insight-panel-selectors.ts
@@ -8,4 +8,21 @@ export const InsightPanelsSelectors = {
     InsightPanelsSelectors.resultList()
       .shadow()
       .find('atomic-result-placeholder'),
+  pager: () => InsightPanelsSelectors.interface().find('atomic-insight-pager'),
+  querySummary: () =>
+    InsightPanelsSelectors.interface().find('atomic-insight-query-summary'),
+  searchbox: () =>
+    InsightPanelsSelectors.interface().find('atomic-insight-search-box'),
+  refineToggle: () =>
+    InsightPanelsSelectors.interface().find('atomic-insight-refine-toggle'),
+  refineModal: () =>
+    InsightPanelsSelectors.interface().find('atomic-insight-refine-modal'),
+  filtersModal: () =>
+    InsightPanelsSelectors.refineModal().shadow().find('atomic-modal'),
+  filters: () => InsightPanelsSelectors.refineModal().find('[slot~="facets"]'),
+  editToggle: () =>
+    InsightPanelsSelectors.interface().find('atomic-insight-edit-toggle'),
+  historyToggle: () =>
+    InsightPanelsSelectors.interface().find('atomic-insight-history-toggle'),
+  tabs: () => InsightPanelsSelectors.interface().find('atomic-insight-tabs'),
 };
--- packages/atomic/cypress/integration-insights-panel/insights-panel.cypress.ts
+++ packages/atomic/cypress/integration-insights-panel/insights-panel.cypress.ts
@@ -25,5 +25,152 @@ describe('Insights panel test suites', () => {
         .find('a')
         .should('have.attr', 'href');
     });
+
+    it('should display pagination', () => {
+      InsightPanelsSelectors.pager()
+        .should('exist')
+        .shadow()
+        .find('input')
+        .eq(1)
+        .click();
+      InsightPanelsSelectors.pager()
+        .shadow()
+        .find('[part~="active-page-button"]')
+        .should('have.attr', 'value', '2');
+    });
+
+    it('should display query summary', () => {
+      InsightPanelsSelectors.querySummary()
+        .should('exist')
+        .shadow()
+        .should('have.text', 'Insights related to this case');
+    });
+
+    it('should display a search box', () => {
+      InsightPanelsSelectors.searchbox()
+        .should('exist')
+        .shadow()
+        .find('input')
+        .should('exist')
+        .should('have.attr', 'placeholder', 'Search');
+
+      InsightPanelsSelectors.searchbox()
+        .shadow()
+        .find('button')
+        .should('exist')
+        .find('atomic-icon')
+        .should('have.attr', 'icon');
+    });
+
+    it('should display refine-toggle', () => {
+      InsightPanelsSelectors.refineToggle()
+        .should('exist')
+        .shadow()
+        .find('atomic-icon-button')
+        .shadow()
+        .find('button')
+        .should('exist')
+        .should('have.attr', 'title', 'Filters');
+    });
+
+    it('should display refine-modal', () => {
+      InsightPanelsSelectors.refineToggle().click();
+      InsightPanelsSelectors.refineModal().should('exist');
+
+      InsightPanelsSelectors.filtersModal().should('have.attr', 'is-open');
+
+      InsightPanelsSelectors.filtersModal()
+        .find('[slot~="header"]')
+        .should('have.text', 'Filters');
+
+      InsightPanelsSelectors.filters()
+        .find('atomic-insight-facet')
+        .should('exist')
+        .should('have.length.at.least', 1);
+
+      InsightPanelsSelectors.filters()
+        .find('atomic-insight-timeframe-facet')
+        .should('exist')
+        .should('have.length.at.least', 1);
+
+      InsightPanelsSelectors.filters()
+        .find('atomic-insight-numeric-facet')
+        .should('exist')
+        .should('have.length.at.least', 1);
+
+      InsightPanelsSelectors.filtersModal().find('[slot="footer"]').click();
+
+      InsightPanelsSelectors.filtersModal().should('not.have.attr', 'is-open');
+    });
+
+    it('should display edit toggle', () => {
+      InsightPanelsSelectors.editToggle()
+        .should('exist')
+        .should('have.attr', 'tooltip');
+
+      InsightPanelsSelectors.editToggle()
+        .shadow()
+        .find('atomic-icon-button')
+        .shadow()
+        .find('button')
+        .should('have.attr', 'title');
+      InsightPanelsSelectors.editToggle()
+        .shadow()
+        .find('atomic-icon-button')
+        .shadow()
+        .find('button')
+        .find('atomic-icon')
+        .should('exist');
+    });
+
+    it('should display history toggle', () => {
+      InsightPanelsSelectors.historyToggle()
+        .should('exist')
+        .should('have.attr', 'tooltip');
+
+      InsightPanelsSelectors.historyToggle()
+        .shadow()
+        .find('atomic-icon-button')
+        .shadow()
+        .find('button')
+        .should('have.attr', 'title');
+      InsightPanelsSelectors.historyToggle()
+        .shadow()
+        .find('atomic-icon-button')
+        .shadow()
+        .find('button')
+        .find('atomic-icon')
+        .should('exist');
+    });
+
+    it('should display tabs', () => {
+      InsightPanelsSelectors.tabs()
+        .should('exist')
+        .find('atomic-insight-tab')
+        .should('have.length.at.least', 1)
+        .eq(1)
+        .click();
+
+      InsightPanelsSelectors.tabs()
+        .find('button[aria-pressed="true"]')
+        .should('have.text', 'PDF');
+    });
+  });
+
+  describe('when there is something written in the search box', () => {
+    before(() => {
+      cy.visit(host);
+      InsightPanelsSelectors.searchbox()
+        .shadow()
+        .find('input')
+        .type('test{enter}');
+    });
+    it('display query summary', () => {
+      InsightPanelsSelectors.querySummary()
+        .should('exist')
+        .shadow()
+        .should('contain.text', 'Results 1-10')
+        .should('contain.text', 'for test');
+    });
   });
 });
