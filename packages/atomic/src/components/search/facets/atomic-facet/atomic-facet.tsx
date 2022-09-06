2fccc27070b3a81f221e1392502cce7c287b4c3e
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-09-06 16:59:30 -0400

    chore(atomic): add comment in facets about isCollapsed & the facet manager
    https://coveord.atlassian.net/browse/KIT-1958

--- packages/atomic/src/components/search/facets/atomic-facet/atomic-facet.tsx
+++ packages/atomic/src/components/search/facets/atomic-facet/atomic-facet.tsx
@@ -120,7 +120,7 @@ export class AtomicFacet implements InitializableComponent, BaseFacet<Facet> {
   @Prop({reflect: true}) public displayValuesAs: 'checkbox' | 'link' | 'box' =
     'checkbox';
   /**
-   * Specifies if the facet is collapsed.
+   * Specifies if the facet is collapsed. When the facet is the child of an `atomic-facet-manager` component, it will control this property.
    */
   @Prop({reflect: true, mutable: true}) public isCollapsed = false;
   /**
