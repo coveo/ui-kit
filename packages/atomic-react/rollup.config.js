f2295641b9eb70851fb4b28ab9565ffa07339441
Olivier Lamothe <olamothe@coveo.com>
2022-09-14 14:05:44 -0400

    feat(atomic-react): add iife output for atomic react + publish to CDN (#2426)
    https://coveord.atlassian.net/browse/KIT-2025

---
+++ packages/atomic-react/rollup.config.js
@@ -0,0 +1,40 @@
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import {terser} from 'rollup-plugin-terser';
+
+const outputIIFE = ({minify}) => ({
+  file: `dist/iife/atomic-react${minify ? '.min' : ''}.js`,
+  format: 'iife',
+  name: 'CoveoAtomicReact',
+  globals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    '@coveo/atomic': 'CoveoAtomic',
+    '@coveo/atomic/headless': 'CoveoHeadless',
+  },
+  plugins: minify ? [terser()] : [],
+});
+
+export default {
+  input: 'src/index.ts',
+  output: [outputIIFE({minify: true}), outputIIFE({minify: false})],
+  external: ['react', 'react-dom', '@coveo/atomic', '@coveo/atomic/headless'],
+  plugins: [
+    nodePolyfills(),
+    typescript({tsconfig: 'tsconfig.iife.json'}),
+    commonjs(),
+    nodeResolve(),
+    replace({
+      delimiters: ['', ''],
+      values: {
+        'process.env.NODE_ENV': JSON.stringify('dev'),
+        'util.TextEncoder();': 'TextEncoder();',
+        "import { defineCustomElements } from '@coveo/atomic/loader';": '',
+        'defineCustomElements();': '',
+      },
+    }),
+  ],
+};
