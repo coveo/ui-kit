bb8cf73a35f02663a6abecad0dbeb598bfba1426
btaillon <54454747+btaillon@users.noreply.github.com>
2022-09-15 13:47:37 -0500

    fix(atomic): added smart snippet inline link analytics (#2417)
    https://coveord.atlassian.net/browse/KIT-2014

--- packages/atomic/cypress/integration/smart-snippet-actions.ts
+++ packages/atomic/cypress/integration/smart-snippet-actions.ts
@@ -23,15 +23,15 @@ export const addSmartSnippetDefaultOptions: Required<AddSmartSnippetOptions> = {
   question: 'Creating an In-Product Experience (IPX)',
   answer: `
     <ol>
-      <li>On the <b>In-Product Experiences</b> page, click Add <b>In-Product Experience</b>.</li>
+      <li>On the <a href="https://platform.cloud.coveo.com/admin/#/orgid/search/in-app-widgets/">In-Product Experiences</a> page, click Add <b>In-Product Experience</b>.</li>
       <li>In the Configuration tab, fill the Basic settings section.</li>
-      <li>(Optional) Use the Design and Content access tabs to customize your <b>IPX</b> interface.</li>
+      <li>(Optional) Use the Design and Content access tabs to <a href="https://docs.coveo.com/en/3160/#customizing-an-ipx-interface">customize your <b>IPX</b> interface</a>.</li>
       <li>Click Save.</li>
       <li>In the Loader snippet panel that appears, you may click Copy to save the loader snippet for your <b>IPX</b> to your clipboard, and then click Save.  You can Always retrieve the loader snippet later.</li>
     </ol>
 
     <p>
-      You're now ready to load your IPX interface in the desired web site or application. However, we recommend that you configure query pipelines for your IPX interface before.
+      You're now ready to <a href="https://docs.coveo.com/en/3160/build-a-search-ui/manage-coveo-in-product-experiences-ipx#embed-your-ipx-interface-in-sites-and-applications">embed your IPX interface</a>. However, we recommend that you <a href="https://docs.coveo.com/en/3160/build-a-search-ui/manage-coveo-in-product-experiences-ipx#configuring-query-pipelines-for-an-ipx-interface-recommended">configure query pipelines for your IPX interface</a> before.
     </p>
   `,
   sourceTitle: 'Manage the Coveo In-Product Experiences (IPX)',
--- packages/atomic/cypress/integration/smart-snippet-suggestions-assertions.ts
+++ packages/atomic/cypress/integration/smart-snippet-suggestions-assertions.ts
@@ -1,3 +1,4 @@
+import {InlineLink} from '@coveo/headless';
 import {AnalyticsTracker} from '../utils/analyticsUtils';
 import {should} from './common-assertions';
 import {SmartSnippetSuggestionsSelectors} from './smart-snippet-suggestions-selectors';
@@ -27,12 +28,34 @@ export function assertAnswerBottomMargin(
   });
 }
 
-export function assertlogOpenSmartSnippetSuggestionsSource(log: boolean) {
+export function assertLogOpenSmartSnippetSuggestionsSource(log: boolean) {
   it(`${should(
     log
   )} log a openSmartSnippetSuggestionSource click event`, () => {
     if (log) {
       cy.expectClickEvent('openSmartSnippetSuggestionSource');
+    } else {
+      cy.wait(50);
+      cy.wrap(AnalyticsTracker).invoke('getLastClickEvent').should('not.exist');
+    }
+  });
+}
+
+export function assertLogOpenSmartSnippetSuggestionsInlineLink(
+  getLinkToLog: (() => InlineLink) | null
+) {
+  it(`${should(
+    !!getLinkToLog
+  )} log a openSmartSnippetSuggestionsInlineLink click event`, () => {
+    if (getLinkToLog) {
+      const {linkText, linkURL} = getLinkToLog();
+      cy.expectClickEvent('openSmartSnippetSuggestionsInlineLink').should(
+        (clickEvent) => {
+          expect(clickEvent).to.have.property('customData');
+          expect(clickEvent.customData).to.have.property('linkText', linkText);
+          expect(clickEvent.customData).to.have.property('linkURL', linkURL);
+        }
+      );
     } else {
       cy.wait(50);
       cy.wrap(AnalyticsTracker).invoke('getLastClickEvent').should('not.exist');
