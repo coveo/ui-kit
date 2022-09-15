e95dabf4c31fc9d16b6a2ea895669ac1e88bf599
mmitiche <mmitiche@coveo.com>
2022-09-15 08:22:53 -0400

    SFINT-4642 insightSearchBox controller updated to use the correct analytics action when performing a search

--- packages/headless/src/controllers/insight/search-box/headless-insight-search-box.ts
+++ packages/headless/src/controllers/insight/search-box/headless-insight-search-box.ts
@@ -1,8 +1,13 @@
 import {InsightEngine} from '../../../app/insight-engine/insight-engine';
+import {SearchAction} from '../../../features/analytics/analytics-utils';
 import {
   executeSearch,
   fetchQuerySuggestions,
 } from '../../../features/insight-search/insight-search-actions';
+import {clearQuerySuggest} from '../../../features/query-suggest/query-suggest-actions';
+import {logSearchboxSubmit} from '../../../features/query/query-insight-analytics-actions';
+import {prepareForSearchWithQuery} from '../../../features/search/search-actions';
+import {randomID} from '../../../utils/utils';
 import {
   buildCoreSearchBox,
   Delimiters,
@@ -11,7 +16,10 @@ import {
   Suggestion,
   SuggestionHighlightingOptions,
 } from '../../core/search-box/headless-core-search-box';
-import {SearchBoxOptions} from '../../core/search-box/headless-core-search-box-options';
+import {
+  defaultSearchBoxOptions,
+  SearchBoxOptions,
+} from '../../core/search-box/headless-core-search-box-options';
 
 export type {
   SearchBoxOptions,
@@ -40,9 +48,41 @@ export function buildSearchBox(
   engine: InsightEngine,
   props: SearchBoxProps = {}
 ): SearchBox {
-  return buildCoreSearchBox(engine, {
+  const {dispatch} = engine;
+  const searchBox = buildCoreSearchBox(engine, {
     ...props,
     executeSearchActionCreator: executeSearch,
     fetchQuerySuggestionsActionCreator: fetchQuerySuggestions,
   });
+  const id = props.options?.id || randomID('search_box');
+  const options: Required<SearchBoxOptions> = {
+    id,
+    highlightOptions: {...props.options?.highlightOptions},
+    ...defaultSearchBoxOptions,
+    ...props.options,
+  };
+
+  const getValue = () => engine.state.querySet?.[options.id];
+
+  const performSearch = async (analytics: SearchAction) => {
+    const {enableQuerySyntax, clearFilters} = options;
+
+    dispatch(
+      prepareForSearchWithQuery({
+        q: getValue(),
+        enableQuerySyntax,
+        clearFilters,
+      })
+    );
+
+    await dispatch(executeSearch(analytics));
+  };
+
+  return {
+    ...searchBox,
+    submit() {
+      performSearch(logSearchboxSubmit());
+      dispatch(clearQuerySuggest({id}));
+    },
+  };
 }
