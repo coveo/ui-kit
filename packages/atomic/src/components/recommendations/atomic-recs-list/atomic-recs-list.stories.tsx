a3c9ac61c68f6068e51ffbfef78f6899a8bdf1ab
Jean-Félix <ThibodeauJF@users.noreply.github.com>
2022-10-17 15:52:52 -0400

    chore(atomic): add recslist to storybook (#2504)
    https://coveord.atlassian.net/browse/KIT-2077

---
+++ packages/atomic/src/components/recommendations/atomic-recs-list/atomic-recs-list.stories.tsx
@@ -0,0 +1,19 @@
+import recsStory from '../../../../.storybook/recommendation/recs-story';
+import {html} from 'lit-html';
+
+const {defaultModuleExport, exportedStory} = recsStory(
+  'Atomic/RecsList',
+  'atomic-recs-list',
+  {},
+  {
+    additionalMarkup: () =>
+      html` <style>
+        atomic-recs-list {
+          margin: 24px;
+        }
+      </style>`,
+  }
+);
+
+export default defaultModuleExport;
+export const DefaultRecsList = exportedStory;
