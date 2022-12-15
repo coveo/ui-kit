169e2dab3eb1c47b5238b896399631dd38a43e1f
Nathan Lafrance-Berger <nathan.lafranceb@gmail.com>
2022-12-14 20:50:38 -0500

    feat(quantic): started adding quantic-result-multi-value-text

---
+++ packages/quantic/force-app/main/default/lwc/quanticResultMultiValueText/__tests__/quanticResultMultiValueText.test.js
@@ -0,0 +1,120 @@
+// @ts-ignore
+import {createElement} from 'lwc';
+import QuanticResultMultiValueText from '../quanticResultMultiValueText';
+
+const exampleField = 'examplenumberfield';
+const exampleFieldValue = 1670118414000;
+const exampleResult = {
+  raw: {
+    [exampleField]: exampleFieldValue,
+  },
+};
+
+const errorSelector = '.error-message';
+
+const defaultOptions = {
+  result: exampleResult,
+  field: exampleField,
+};
+
+function createTestComponent(options = defaultOptions) {
+  const element = createElement('c-quantic-result-multi-value-text', {
+    is: QuanticResultMultiValueText,
+  });
+
+  for (const [key, value] of Object.entries(options)) {
+    element[key] = value;
+  }
+
+  document.body.appendChild(element);
+  return element;
+}
+
+// Helper function to wait until the microtask queue is empty.
+function flushPromises() {
+  // eslint-disable-next-line @lwc/lwc/no-async-operation
+  return new Promise((resolve) => setTimeout(resolve, 0));
+}
+
+describe('c-quantic-result-multi-value-text', () => {
+  let mockedConsoleError;
+  let isStringMock;
+  let isNumberMock;
+  let formattingFunctionMock;
+
+  function cleanup() {
+    // The jsdom instance is shared across test cases in a single file so reset the DOM
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+    jest.clearAllMocks();
+  }
+
+  beforeEach(() => {
+    mockedConsoleError = jest.fn();
+    isStringMock = jest.fn().mockReturnValue(true);
+    isNumberMock = jest.fn().mockReturnValue(true);
+    formattingFunctionMock = jest
+      .fn()
+      .mockImplementation((value) => `formatted ${value}`);
+    console.error = mockedConsoleError;
+    // @ts-ignore
+    global.Bueno = {
+      isString: isStringMock,
+      isNumber: isNumberMock,
+    };
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  describe('when no result is given', () => {
+    it('should show an error', async () => {
+      // @ts-ignore
+      const element = createTestComponent({field: exampleField});
+      await flushPromises();
+      const errorMessage = element.shadowRoot.querySelector(errorSelector);
+
+      expect(errorMessage).not.toBeNull();
+      expect(console.error).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('when no field is given', () => {
+    it('should show an error', async () => {
+      // @ts-ignore
+      const element = createTestComponent({result: exampleResult});
+      await flushPromises();
+
+      const errorMessage = element.shadowRoot.querySelector(errorSelector);
+
+      expect(errorMessage).not.toBeNull();
+      expect(console.error).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('when the field value is not a valid multi-value field', () => {
+    it('should show an error', async () => {
+      isNumberMock.mockReturnValue(false);
+      const element = createTestComponent();
+      await flushPromises();
+
+      const errorMessage = element.shadowRoot.querySelector(errorSelector);
+
+      expect(errorMessage).not.toBeNull();
+    });
+  });
+
+  describe('when a formatting function is given', () => {
+    it('should render the formatted field value', async () => {
+      createTestComponent({
+        ...defaultOptions,
+        formattingFunction: formattingFunctionMock,
+      });
+      await flushPromises();
+
+      expect(formattingFunctionMock).toHaveBeenCalledWith(exampleFieldValue);
+    });
+  });
+});
---
+++ packages/quantic/force-app/main/default/lwc/quanticResultMultiValueText/quanticResultMultiValueText.js
@@ -0,0 +1,122 @@
+import nMore from '@salesforce/label/c.quantic_NMore';
+import {getBueno} from 'c/quanticHeadlessLoader';
+import {I18nUtils} from 'c/quanticUtils';
+import {LightningElement, api} from 'lwc';
+
+/** @typedef {import("coveo").Result} Result */
+
+/**
+ * The `QuanticResultMultiValueText` component displays a given result multi-value field value.
+ * @category Result Template
+ * @example
+ * <c-quantic-result-multi-value-text result={result} label="Likes" field="ytlikecount" max-values-to-display="4"></c-quantic-result-multi-value>
+ */
+export default class QuanticResultMultiValueText extends LightningElement {
+  /**
+   * The [result item](https://docs.coveo.com/en/headless/latest/reference/search/controllers/result-list/#result) to use.
+   * @api
+   * @type {Result}
+   */
+  @api result;
+  /**
+   * The multi-value field whose values you want to display.
+   * @api
+   * @type {string}
+   */
+  @api field;
+  /**
+   * (Optional) The label to display.
+   * @api
+   * @type {string}
+   * @defaultValue `none`
+   */
+  @api label;
+  /**
+   * The delimiter used to separate values when the field isn’t indexed as a multi value field.
+   * @api
+   * @type {string}
+   * @defaultValue `undefined`
+   */
+  @api delimiter;
+  /**
+   * The maximum number of field values to display. If there are _n_ more values than the specified maximum, the last displayed value will be "_n more..._".
+   * @api
+   * @type {number}
+   * @defaultValue `3`
+   */
+  @api maxValuesToDisplay = 3;
+
+  /** @type {string} */
+  error;
+  joinSeparator = ', ';
+  validated = false;
+
+  labels = {
+    nMore,
+  };
+
+  connectedCallback() {
+    getBueno(this).then(() => {
+      if (!this.result || !this.field || !Bueno.isString(this.field)) {
+        console.error(
+          `The ${this.template.host.localName} requires a result and a multi-value field to be specified.`
+        );
+        this.setError();
+      }
+      if (this.label && !Bueno.isString(this.label)) {
+        console.error(`The "${this.label}" label is not a valid string.`);
+        this.setError();
+      }
+      if (!this.fieldValue) {
+        this.error = `Could not parse value from field "${this.field}" as a string array.`;
+      }
+      this.validated = true;
+    });
+  }
+
+  setError() {
+    this.error = `${this.template.host.localName} Error`;
+  }
+
+  get isVisible() {
+    return this.validated && !this.error;
+  }
+
+  /**
+   * The value of the given result field.
+   * @returns {string[]}
+   */
+  get fieldValue() {
+    if (!this.field) {
+      return undefined;
+    }
+    const value = this.result?.raw[this.field];
+
+    if (Array.isArray(value)) {
+      return value.map((v) => `${v}`.trim());
+    }
+
+    if (typeof value !== 'string' || value.trim() === '') {
+      return undefined;
+    }
+
+    return this.delimiter
+      ? value.split(this.delimiter).map((v) => v.trim())
+      : [value];
+  }
+
+  get valueToDisplay() {
+    const value = this.fieldValue;
+    if (value.length < this.maxValuesToDisplay) {
+      return value.join(this.joinSeparator);
+    }
+    const truncatedArray = value.slice(0, this.maxValuesToDisplay);
+    truncatedArray.push(
+      I18nUtils.format(
+        this.labels.nMore,
+        value.length - this.maxValuesToDisplay
+      )
+    );
+    return truncatedArray.join(this.joinSeparator);
+  }
+}
