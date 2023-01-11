176a8f95c59f9ee28ad12a9ef780978e4c409006
Olivier Lamothe <olamothe@coveo.com>
2023-01-11 11:13:36 -0500

    remove disabled prop
    https://coveord.atlassian.net/browse/KIT-2206

--- packages/atomic/src/components/search/result-template-components/atomic-quickview-sidebar/atomic-quickview-sidebar.tsx
+++ packages/atomic/src/components/search/result-template-components/atomic-quickview-sidebar/atomic-quickview-sidebar.tsx
@@ -149,7 +149,6 @@ const Keywords: FunctionalComponent<
             </div>
 
             <atomic-icon-button
-              disabled={!wordIsEnabled}
               class={`${
                 highlightKeywords.highlightAll
                   ? ''
