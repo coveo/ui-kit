10e5c8b7f2d9da8017b29ff225da00d979ac0075
Benjamin Taillon <54454747+btaillon@users.noreply.github.com>
2023-01-26 12:56:37 -0500

    docs(atomic-react): various atomic-react samples improvements (#2679)
    https://coveord.atlassian.net/browse/KIT-2195

--- packages/samples/headless-react/src/pages/StandaloneSearchBoxPage.tsx
+++ packages/samples/headless-react/src/pages/StandaloneSearchBoxPage.tsx
@@ -2,15 +2,20 @@ import {
   buildSearchEngine,
   getSampleSearchEngineConfiguration,
 } from '@coveo/headless';
+import {useMemo} from 'react';
 import {StandaloneSearchBox} from '../components/standalone-search-box/standalone-search-box.class';
 import {StandaloneSearchBox as StandaloneSearchBoxFn} from '../components/standalone-search-box/standalone-search-box.fn';
 import {AppContext} from '../context/engine';
 import {Section} from '../layout/section';
 
 export function StandaloneSearchBoxPage() {
-  const engine = buildSearchEngine({
-    configuration: getSampleSearchEngineConfiguration(),
-  });
+  const engine = useMemo(
+    () =>
+      buildSearchEngine({
+        configuration: getSampleSearchEngineConfiguration(),
+      }),
+    []
+  );
 
   return (
     <AppContext.Provider value={{engine}}>
