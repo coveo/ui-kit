867695c58082ea35a26c52b55c95e0d6a834fb81
Olivier Lamothe <olamothe@coveo.com>
2023-01-31 10:29:51 -0500

    WIP
    https://coveord.atlassian.net/browse/KIT-2250

--- packages/atomic/cypress/e2e/result-list/result-components/quickview-selectors.ts
+++ packages/atomic/cypress/e2e/result-list/result-components/quickview-selectors.ts
@@ -1,9 +1,27 @@
 import {ResultListSelectors} from '../result-list-selectors';
 
 export const quickviewComponent = 'atomic-quickview';
+export const quickviewModalComponent = 'atomic-quickview-modal';
+export const focusTrapComponent = 'atomic-focus-trap';
 
 export const QuickviewSelectors = {
   shadow: () => cy.get(quickviewComponent),
   firstInResult: () =>
     ResultListSelectors.firstResult().find(quickviewComponent),
+  button: () => QuickviewSelectors.firstInResult().shadow().find('button'),
+};
+
+export const QuickviewModalSelectors = {
+  shadow: () => cy.get(quickviewComponent).shadow(),
+  focusTrap: () =>
+    QuickviewModalSelectors.shadow().find(focusTrapComponent, {
+      includeShadowDom: true,
+    }),
+  focusTrapContainer: () =>
+    QuickviewModalSelectors.focusTrap().shadow().find('[part="container"]'),
+  closeButton: () =>
+    QuickviewModalSelectors.shadow().find('[part="close-button"]'),
+  footerButton: () =>
+    QuickviewModalSelectors.shadow().find('[part="footer-button"]'),
+  facets: () => cy.get(quickviewModalComponent).find('[slot="facets"]'),
 };
--- packages/atomic/cypress/e2e/result-list/result-components/quickview.cypress.ts
+++ packages/atomic/cypress/e2e/result-list/result-components/quickview.cypress.ts
@@ -1,7 +1,16 @@
 import {generateComponentHTML} from '../../../fixtures/fixture-common';
 import {TestFixture} from '../../../fixtures/test-fixture';
 import * as CommonAssertions from '../../common-assertions';
+import {addFacet} from '../../facets/facet/facet-actions';
+import {addResultList, buildTemplateWithSections} from '../result-list-actions';
 import {quickviewComponent, QuickviewSelectors} from './quickview-selectors';
+
+const addQuickviewInResultList = () =>
+  addResultList(
+    buildTemplateWithSections({
+      actions: generateComponentHTML(quickviewComponent),
+    })
+  );
 
 describe('Quickview Component', () => {
   describe('when not used inside a result template', () => {
@@ -13,5 +22,23 @@ describe('Quickview Component', () => {
 
     CommonAssertions.assertRemovesComponent(QuickviewSelectors.shadow);
     CommonAssertions.assertConsoleError();
+  });
+
+  describe('when used on pdf file inside a result list', () => {
+    beforeEach(() => {
+      new TestFixture()
+        .with(addFacet({field: 'filetype'}))
+        .withHash('f-filetype=pdf')
+        .with(addQuickviewInResultList())
+
+        .init();
+    });
+
+    CommonAssertions.assertAccessibility(QuickviewSelectors.firstInResult);
+    CommonAssertions.assertConsoleError(false);
+
+    it('should open a modal on click', () => {
+      QuickviewSelectors.button().click();
+    });
   });
 });
