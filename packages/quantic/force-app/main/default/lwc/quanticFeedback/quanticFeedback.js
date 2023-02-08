9c1a902c113ad4ef550cd18b2d494bfc0dd425d3
mmitiche <86681870+mmitiche@users.noreply.github.com>
2023-02-08 16:11:21 -0500

    Update packages/quantic/force-app/main/default/lwc/quanticFeedback/quanticFeedback.js
    Co-authored-by: Nathan Lafrance-Berger <nathan.lafranceb@gmail.com>

--- packages/quantic/force-app/main/default/lwc/quanticFeedback/quanticFeedback.js
+++ packages/quantic/force-app/main/default/lwc/quanticFeedback/quanticFeedback.js
@@ -5,7 +5,7 @@ import wasThisUseful from '@salesforce/label/c.quantic_WasThisUseful';
 import explainWhy from '@salesforce/label/c.quantic_ExplainWhy';
 
 /**
- * The `QuanticFeedback` component displays a question and two buttons for selecting a positive or a negative feedback.
+ * The `QuanticFeedback` component displays a question and two buttons for giving positive or negative feedback.
  * @fires CustomEvent#like
  * @fires CustomEvent#dislike
  * @fires CustomEvent#pressExplainWhy
