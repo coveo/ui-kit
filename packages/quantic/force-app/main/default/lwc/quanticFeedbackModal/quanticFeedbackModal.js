49ae6a8d570836c90a30f8e056c3501eb9208ef8
mmitiche <mmitiche@coveo.com>
2023-02-10 10:39:00 -0500

    SFINT-4801 string labels added

--- packages/quantic/force-app/main/default/lwc/quanticFeedbackModal/quanticFeedbackModal.js
+++ packages/quantic/force-app/main/default/lwc/quanticFeedbackModal/quanticFeedbackModal.js
@@ -1,4 +1,13 @@
 // @ts-nocheck
+import cancel from '@salesforce/label/c.quantic_Cancel';
+import done from '@salesforce/label/c.quantic_Done';
+import explainWhy from '@salesforce/label/c.quantic_ExplainWhy';
+import fillOutField from '@salesforce/label/c.quantic_FillOutField';
+import selectOneOfOptions from '@salesforce/label/c.quantic_SelectOneOfOptions';
+import selectTheReason from '@salesforce/label/c.quantic_SelectTheReason';
+import sendFeedback from '@salesforce/label/c.quantic_SendFeedback';
+import whatWasTheReason from '@salesforce/label/c.quantic_WhatWasTheReason';
+import yourResponsesHelp from '@salesforce/label/c.quantic_YourResponsesHelp';
 import LightningModal from 'lightning/modal';
 import {api} from 'lwc';
 // @ts-ignore
@@ -6,49 +15,61 @@ import feedbackFormTemplate from './feedbackForm.html';
 // @ts-ignore
 import feedbackSuccessTemplate from './feedbackSuccess.html';
 
-
 export default class QuanticFeedbackModal extends LightningModal {
   /**
    * The list of options to be displayed in the modal.
+   * @type {Array<{label: string, value: string}>}
    */
-  @api content;
+  @api options;
+
+  labels = {
+    done,
+    cancel,
+    sendFeedback,
+    explainWhy,
+    selectOneOfOptions,
+    selectTheReason,
+    whatWasTheReason,
+    fillOutField,
+    yourResponsesHelp,
+  };
 
   /** @type {string} */
   optionsValue;
   /** @type {string} */
   reasonValue;
-  /** @type {string} */
+  /** @type {boolean} */
   feedbackSubmitted = false;
-  /** @type {string} */
+  /** @type {boolean} */
   reasonInputError = false;
 
-  connectedCallback(){
-    console.log()
-  }
-
   /**
-   * 
+   * Closes the modal.
+   * @returns {void}
    */
-  get options() {
-    return this?.content?.options;
-  }
-
   closeModal() {
     this.close();
   }
 
+  /**
+   * Returns the lightning-radio-group element.
+   * @returns {{setCustomValidity: function, reportValidity: function}}
+   */
   get optionsInput() {
     return this.template.querySelector('lightning-radio-group');
   }
 
-  get reasonInput() {
-    return this.template.querySelector('textarea');
-  }
-
+  /**
+   * Returns the CSS class of the reason input.
+   */
   get reasonFormClass() {
     return `slds-form-element ${this.reasonInputError ? 'slds-has-error' : ''}`;
   }
 
+  /**
+   * Handles the option change.
+   * @param {CustomEvent} event
+   */
   handleOptionChange(event) {
     this.reasonInputError = false;
     this.optionsInput.setCustomValidity();
@@ -56,32 +77,55 @@ export default class QuanticFeedbackModal extends LightningModal {
     this.optionsValue = event.detail.value;
   }
 
+  /**
+   * Handles the reason change.
+   * @param {CustomEvent} event
+   */
   handleReasonChange(event) {
     this.reasonInputError = false;
     this.reasonValue = event.target.value;
   }
 
+  /**
+   * Indicates whether the reason input should be displayed.
+   * @returns {boolean}
+   */
   get displayReasonInput() {
     return this.optionsValue === 'other';
   }
 
-  submitFeedback() {
-    if (
+  /**
+   * Indicates whether the feedback form is valid.
+   * @returns {boolean}
+   */
+  get validForm() {
+    return (
       (this.optionsValue === 'other' && !!this.reasonValue) ||
       (!!this.optionsValue && this.optionsValue !== 'other')
-    ) {
+    );
+  }
+
+  /**
+   * Submits the feedback.
+   * @returns {void}
+   */
+  submitFeedback() {
+    if (this.validForm) {
       this.sendFeedbackSuccessEvent();
       this.feedbackSubmitted = true;
     } else {
       if (this.optionsValue === 'other') {
         this.reasonInputError = true;
       } else {
-        this.optionsInput.setCustomValidity('Value is required');
+        this.optionsInput.setCustomValidity(this.labels.selectOneOfOptions);
         this.optionsInput.reportValidity();
       }
     }
   }
 
+  /**
+   * Fires the quantic__feedbacksuccess event.
+   */
   sendFeedbackSuccessEvent() {
     this.dispatchEvent(
       new CustomEvent('quantic__feedbacksuccess', {
@@ -95,6 +139,9 @@ export default class QuanticFeedbackModal extends LightningModal {
     );
   }
 
+  /**
+   * Returns the right HTML template.
+   */
   render() {
     if (this.feedbackSubmitted) {
       return feedbackSuccessTemplate;
