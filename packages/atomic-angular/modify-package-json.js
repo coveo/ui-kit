8f14878bd2a69b8ec4ba5526f778459b4236e412
Benjamin Taillon <btaillon@coveo.com>
2023-02-13 11:06:52 -0500

    fixed atomic-angular release scripts
    https://coveord.atlassian.net/browse/KIT-2277

--- packages/atomic-angular/modify-package-json.js
+++ packages/atomic-angular/modify-package-json.js
@@ -7,8 +7,10 @@ const packageJSON = JSON.parse(readFileSync(packageJSONPath));
 if (!packageJSON.scripts) {
   packageJSON.scripts = {};
 }
-packageJSON.scripts['publish:npm'] =
-  'node ../../../../../scripts/deploy/publish.mjs';
+packageJSON.scripts['publish:npm:release'] =
+  'node ../../../../../scripts/deploy/publish.mjs release';
+packageJSON.scripts['publish:npm:prerelease'] =
+  'node ../../../../../scripts/deploy/publish.mjs prerelease';
 packageJSON.scripts['promote:npm:beta'] =
   'node ../../../../../scripts/deploy/update-npm-tag.mjs beta';
 packageJSON.scripts['promote:npm:latest'] =
