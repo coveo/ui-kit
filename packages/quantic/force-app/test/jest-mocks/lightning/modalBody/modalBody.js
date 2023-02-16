840f0ae1985dfe63b51bbd3d76e618290663ba98
mmitiche <mmitiche@coveo.com>
2023-02-16 11:02:07 -0500

    stub simplified

--- packages/quantic/force-app/test/jest-mocks/lightning/modalBody/modalBody.js
+++ packages/quantic/force-app/test/jest-mocks/lightning/modalBody/modalBody.js
@@ -1,16 +1,3 @@
-/*
- * Copyright (c) 2018, salesforce.com, inc.
- * All rights reserved.
- * SPDX-License-Identifier: MIT
- * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
- */
-import {LightningElement, api} from 'lwc';
+import {LightningElement} from 'lwc';
 
-export default class ModalBody extends LightningElement {
-  @api alternativeText;
-  @api fallbackIconName;
-  @api initials;
-  @api size;
-  @api src;
-  @api variant;
-}
+export default class ModalBody extends LightningElement {}
