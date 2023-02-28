3c89c16562ebde16141a76cc7f8a580edc6975b1
mmitiche <mmitiche@coveo.com>
2023-02-28 09:39:14 -0500

    js doc comment added

--- packages/quantic/force-app/main/default/lwc/quanticResultAction/quanticResultAction.js
+++ packages/quantic/force-app/main/default/lwc/quanticResultAction/quanticResultAction.js
@@ -38,7 +38,7 @@ export default class QuanticResultAction extends LightningElement {
    * The result to perform the action on.
    * @api
    * @type {Result}
-   * @deprecated
+   * @deprecated The component no longer needs a result.
    */
   @api result;
   /**
