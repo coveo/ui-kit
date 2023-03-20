6390b89931579d10744d70a724cf30b09f84e389
Benjamin Taillon <btaillon@coveo.com>
2023-03-20 13:55:53 -0400

    replaced execute with execa
    https://coveord.atlassian.net/browse/KIT-2363

--- scripts/bin/install-isolated.mjs
+++ scripts/bin/install-isolated.mjs
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import {execute} from '../exec.mjs';
+import {$} from 'execa';
 
 if (!['install', 'ci'].includes(process.env.npm_command)) {
   throw `Unexpected npm_command environment variable (value was "${process.env.npm_command}"). This script should only be run from a preinstall or postinstall script.`;
 }
 
-await execute('npm', [process.env.npm_command]);
+await $({stdio: 'inherit'})`npm ${process.env.npm_command}`;
