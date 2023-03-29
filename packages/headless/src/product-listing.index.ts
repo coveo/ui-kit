b8409fc9cf1cff5e5f0e205d69679c4c7140bddd
Benjamin Taillon <btaillon@coveo.com>
2023-03-29 12:49:40 -0400

    For visualization: verify where actions are used and reducers are loaded
    https://coveord.atlassian.net/browse/KIT-2374

--- packages/headless/src/product-listing.index.ts
+++ packages/headless/src/product-listing.index.ts
@@ -40,6 +40,8 @@ export type {
   ProductListingControllerState,
 } from './controllers/product-listing/headless-product-listing';
 export {buildProductListing} from './controllers/product-listing/headless-product-listing';
+// ACTION: PRODUCT-LISTING
+// REDUCER: CONFIGURATION
 
 export type {
   PagerInitialState,
@@ -49,6 +51,10 @@ export type {
   Pager,
 } from './controllers/product-listing/pager/headless-product-listing-pager';
 export {buildPager} from './controllers/product-listing/pager/headless-product-listing-pager';
+// ACTION: PRODUCT-LISTING
+// ACTION: PAGINATION
+// REDUCER: CONFIGURATION
+// REDUCER: PAGINATION
 
 export type {
   ResultsPerPageInitialState,
@@ -57,6 +63,11 @@ export type {
   ResultsPerPage,
 } from './controllers/product-listing/results-per-page/headless-product-listing-results-per-page';
 export {buildResultsPerPage} from './controllers/product-listing/results-per-page/headless-product-listing-results-per-page';
+// ACTION: PRODUCT-LISTING
+// ACTION: PAGINATION
+// ACTION: PAGINATION-ANALYTICS
+// REDUCER: CONFIGURATION
+// REDUCER: PAGINATION
 
 export type {
   ProductListingSortInitialState,
@@ -75,6 +86,11 @@ export {
   buildFieldsSortCriterion,
   buildRelevanceSortCriterion,
 } from './controllers/product-listing/sort/headless-product-listing-sort';
+// ACTION: PAGINATION
+// ACTION: PRODUCT-LISTING
+// ACTION: SORT
+// REDUCER: CONFIGURATION
+// REDUCER: SORT
 
 export type {
   FacetManager,
@@ -82,6 +98,8 @@ export type {
   FacetManagerState,
 } from './controllers/product-listing/facet/headless-product-listing-facet-manager';
 export {buildFacetManager} from './controllers/product-listing/facet/headless-product-listing-facet-manager';
+// REDUCER: PRODUCT-LISTING
+// REDUCER: FACET-OPTIONS
 
 export type {
   CoreFacet,
@@ -98,6 +116,17 @@ export type {
   SpecificFacetSearchResult,
 } from './controllers/product-listing/facet/headless-product-listing-facet';
 export {buildFacet} from './controllers/product-listing/facet/headless-product-listing-facet';
+// ACTION: PRODUCT-LISTING
+// ACTION: FACET-OPTION
+// ACTION: FACET-SET-CONTROLLER
+// ACTION: FACET-SET
+// ACTION: GENERIC-FACET-SEARCH
+// ACTION: SPECIFIC-FACET-SEARCH
+// ACTION: FACET-SET-ANALYTICS
+// REDUCER: CONFIGURATION
+// REDUCER: FACET-OPTION
+// REDUCER: FACET-SET
+// REDUCER: FACET-SEARCH-SET
 
 export type {
   CategoryFacet,
@@ -113,6 +142,16 @@ export type {
   CoreCategoryFacetState,
 } from './controllers/product-listing/category-facet/headless-product-listing-category-facet';
 export {buildCategoryFacet} from './controllers/product-listing/category-facet/headless-product-listing-category-facet';
+// ACTION: PRODUCT-LISTING
+// ACTION: FACET-OPTIONS
+// ACTION: GENERIC-FACET-SEARCH
+// ACTION: SPECIFIC-FACET-SEARCH
+// ACTION: CATEGORY-FACET-SEARCH
+// ACTION: FACET-SET-ANALYTICS
+// REDUCER: CONFIGURATION
+// REDUCER: PRODUCT-LISTING
+// REDUCER: CATEGORY-FACET-SET
+// REDUCER: CATEGORY-FACET-SEARCH-SET
 
 export type {
   DateFacet,
@@ -128,6 +167,17 @@ export {
   buildDateFacet,
   buildDateRange,
 } from './controllers/product-listing/range-facet/date-facet/headless-product-listing-date-facet';
+// ACTION: PRODUCT-LISTING
+// ACTION: FACET-OPTIONS
+// ACTION: FACET-SET
+// ACTION: RANGE-FACET-SET
+// ACTION: DATE-FACET-CONTROLLER
+// ACTION: DATE-FACET
+// ACTION: FACET-SET-ANALYTICS
+// REDUCER: CONFIGURATION
+// REDUCER: SEARCH
+// REDUCER: FACET-OPTIONS
+// REDUCER: DATE-FACET-SET
 
 export type {
   DateFilter,
@@ -138,6 +188,14 @@ export type {
   DateFilterInitialState,
 } from './controllers/product-listing/range-facet/date-facet/headless-product-listing-date-filter';
 export {buildDateFilter} from './controllers/product-listing/range-facet/date-facet/headless-product-listing-date-filter';
+// ACTION: PRODUCT-LISTING
+// ACTION: FACET-OPTIONS
+// ACTION: DATE-FACET
+// ACTION: FACET-SET-ANALYTICS
+// REDUCER: CONFIGURATION
+// REDUCER: SEARCH
+// REDUCER: FACET-OPTIONS
+// REDUCER: DATE-FACET-SET
 
 export type {
   NumericFacet,
@@ -152,6 +210,17 @@ export {
   buildNumericFacet,
   buildNumericRange,
 } from './controllers/product-listing/range-facet/numeric-facet/headless-product-listing-numeric-facet';
+// ACTION: PRODUCT-LISTING
+// ACTION: FACET-OPTIONS
+// ACTION: FACET-SET
+// ACTION: RANGE-FACET
+// ACTION: NUMERIC-FACET-CONTROLLER
+// ACTION: NUMERIC-FACET
+// ACTION: FACET-SET-ANALYTICS
+// REDUCER: CONFIGURATION
+// REDUCER: SEARCH
+// REDUCER: FACET-OPTIONS
+// REDUCER: NUMERIC-FACET-SET
 
 export type {
   NumericFilter,
@@ -162,6 +231,14 @@ export type {
   NumericFilterInitialState,
 } from './controllers/product-listing/range-facet/numeric-facet/headless-product-listing-numeric-filter';
 export {buildNumericFilter} from './controllers/product-listing/range-facet/numeric-facet/headless-product-listing-numeric-filter';
+// ACTION: PRODUCT-LISTING
+// ACTION: FACET-OPTIONS
+// ACTION: NUMERIC-FACET
+// ACTION: FACET-SET-ANALYTICS
+// REDUCER: CONFIGURATION
+// REDUCER: SEARCH
+// REDUCER: FACET-OPTIONS
+// REDUCER: NUMERIC-FACET-SET
 
 export type {
   Context,
@@ -170,6 +247,8 @@ export type {
   ContextValue,
 } from './controllers/product-listing/context/headless-product-listing-context';
 export {buildContext} from './controllers/product-listing/context/headless-product-listing-context';
+// ACTION: CONTEXT
+// REDUCER: CONTEXT
 
 export type {
   InteractiveResultOptions,
@@ -177,3 +256,4 @@ export type {
   InteractiveResult,
 } from './controllers/product-listing/result-list/headless-product-listing-interactive-result';
 export {buildInteractiveResult} from './controllers/product-listing/result-list/headless-product-listing-interactive-result';
+// REDUCER: CONFIGURATION
