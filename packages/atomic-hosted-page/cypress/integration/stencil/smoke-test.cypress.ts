7b8429983f3cedb9e63f8875eff2a799740b2c47
Olivier Lamothe <olamothe@coveo.com>
2023-04-11 16:22:45 -0400

    fix some smoke tests
    https://coveord.atlassian.net/browse/KIT-2388

--- packages/atomic-hosted-page/cypress/integration/stencil/smoke-test.cypress.ts
+++ packages/atomic-hosted-page/cypress/integration/stencil/smoke-test.cypress.ts
@@ -2,7 +2,7 @@ describe('smoke test', () => {
   it('should load atomic-hosted-page component', () => {
     cy.intercept({
       method: 'POST',
-      path: '**/rest/ua/v15/analytics/*',
+      path: '**/rest/v15/analytics/*',
     }).as('analytics');
 
     cy.visit('http://localhost:3335/').wait('@analytics');
@@ -17,7 +17,7 @@ describe('smoke test', () => {
   it.skip('should load atomic-simple-builder component', () => {
     cy.intercept({
       method: 'POST',
-      path: '**/rest/ua/v15/analytics/*',
+      path: '**/rest/v15/analytics/*',
     }).as('analytics');
 
     cy.visit('http://localhost:3335/simple-builder.html').wait('@analytics');
