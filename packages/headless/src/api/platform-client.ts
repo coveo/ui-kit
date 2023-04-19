18577ab88269dedb73718ba4233dba6810c2c9c1
Olivier Lamothe <olamothe@coveo.com>
2023-04-19 13:45:51 -0400

    feat(organizationendpoints): support organization endpoints for hosted-pages components
    https://coveord.atlassian.net/browse/KIT-2405

--- packages/headless/src/api/platform-client.ts
+++ packages/headless/src/api/platform-client.ts
@@ -142,8 +142,9 @@ export function getOrganizationEndpoints(
   const platform = `https://${orgId}.org${envSuffix}.coveo.com`;
   const analytics = `https://${orgId}.analytics.org${envSuffix}.coveo.com`;
   const search = `${platform}/rest/search/v2`;
+  const admin = `https://${orgId}.admin.org${envSuffix}.coveo.com`;
 
-  return {platform, analytics, search};
+  return {platform, analytics, search, admin};
 }
 
 /**
