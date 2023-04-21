664c5c6aab34dc13e9b54bb157223ef8025ee715
Sym Piracha <spiracha@coveo.com>
2023-04-21 09:54:11 -0400

    move cart recs in single file

--- packages/headless/src/controllers/product-recommendations/headless-cart-recommendations.ts
+++ packages/headless/src/controllers/product-recommendations/headless-cart-recommendations.ts
@@ -8,17 +8,34 @@ import {
   baseProductRecommendationsOptionsSchema,
   buildBaseProductRecommendationsList,
 } from './headless-base-product-recommendations';
-import {CartRecommendationsListOptions} from './headless-cart-recommendations-options';
 
-export type {CartRecommendationsListOptions, ProductRecommendation};
+export type {ProductRecommendation};
 
 const optionsSchema = new Schema({
   ...baseProductRecommendationsOptionsSchema,
 });
 
+export interface CartRecommendationsListOptions {
+  /**
+   * The SKUs of the products in the cart.
+   */
+  skus?: string[];
+  /**
+   * The maximum number of recommendations, from 1 to 50.
+   *
+   * @defaultValue `5`
+   */
+  maxNumberOfRecommendations?: number;
+  /**
+   * Additional fields to fetch in the results.
+   */
+  additionalFields?: string[];
+}
+
 export interface CartRecommendationsListProps {
   options?: CartRecommendationsListOptions;
 }
+
 /**
  * The `CartRecommendationsList` controller recommends other products that were frequently purchased together in previous similar carts.
 
