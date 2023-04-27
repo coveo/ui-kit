ddfdeb1bf5ca246b758e5c33d9269f285a5fbabb
erocheleau <erocheleau@coveo.com>
2023-04-27 13:59:14 -0400

    added comments

--- packages/headless/src/controllers/insight/smart-snippet/headless-insight-smart-snippet-interactive-inline-links.ts
+++ packages/headless/src/controllers/insight/smart-snippet/headless-insight-smart-snippet-interactive-inline-links.ts
@@ -17,25 +17,60 @@ import {
 import {InlineLink} from '../../smart-snippet/headless-smart-snippet-interactive-inline-links';
 
 export interface SmartSnippetInteractiveInlineLinksOptions {
+  /**
+   * The amount of time to wait before selecting the result after calling `beginDelayedSelect`.
+   *
+   * @defaultValue `1000`
+   */
   selectionDelay?: number;
 }
 
 export interface SmartSnippetInteractiveInlineLinksProps {
+  /**
+   * The options for the result controller core.
+   */
   options?: SmartSnippetInteractiveInlineLinksOptions;
 }
 
 export interface SmartSnippetInteractiveInlineLinks {
+  /**
+   * Selects the result, logging a UA event to the Coveo Platform if the result wasn't selected before.
+   *
+   * In a DOM context, it's recommended to call this method on all of the following events:
+   * * `contextmenu`
+   * * `click`
+   * * `mouseup`
+   * * `mousedown`
+   */
   selectInlineLink(link: InlineLink, questionAnswerId?: string): void;
+  /**
+   * Prepares to select the result after a certain delay, sending analytics if it was never selected before.
+   *
+   * In a DOM context, it's recommended to call this method on the `touchstart` event.
+   */
   beginDelayedSelectInlineLink(
     link: InlineLink,
     questionAnswerId?: string
   ): void;
+  /**
+   * Cancels the pending selection caused by `beginDelayedSelect`.
+   *
+   * In a DOM context, it's recommended to call this method on the `touchend` event.
+   */
   cancelPendingSelectInlineLink(
     link: InlineLink,
     questionAnswerId?: string
   ): void;
 }
 
+/**
+ * Creates the insight result controller for SmartSnippet.
+ *
+ * @param engine - The headless insight engine.
+ * @param props - The configurable controller properties.
+ * @param action - The action to be triggered on select.
+ * @returns A controller instance.
+ */
 export function buildSmartSnippetInteractiveInlineLinks(
   engine: InsightEngine,
   props?: SmartSnippetInteractiveInlineLinksProps
