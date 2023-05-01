485b0d9206bfe14fb8c04b4be0adae7e7c85197c
mmitiche <mmitiche@coveo.com>
2023-05-01 10:43:27 +0100

    controller exported properly

--- packages/headless/src/insight.index.ts
+++ packages/headless/src/insight.index.ts
@@ -284,15 +284,6 @@ export type {
   SmartSnippetQuestionsListCore,
 } from './controllers/insight/smart-snippet-questions-list/headless-insight-smart-snippet-questions-list';
 export {buildSmartSnippetQuestionsList} from './controllers/insight/smart-snippet-questions-list/headless-insight-smart-snippet-questions-list';
-export type {
-  SmartSnippet,
-  SmartSnippetOptions,
-  SmartSnippetProps,
-  SmartSnippetState,
-  QuestionAnswerDocumentIdentifier,
-  SmartSnippetCore,
-} from './controllers/insight/smart-snippet/headless-insight-smart-snippet';
-export {buildSmartSnippet} from './controllers/insight/smart-snippet/headless-insight-smart-snippet';
 
 // Features
 export type {
