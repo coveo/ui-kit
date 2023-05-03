cd282c24d1fa9546890cf960ae5cd9176f4510ca
mmitiche <mmitiche@coveo.com>
2023-05-03 08:24:52 +0100

    e2e tests added for smart snippet components in the insight use case

--- packages/quantic/cypress/e2e/default-2/smart-snippet/smart-snippet.cypress.ts
+++ packages/quantic/cypress/e2e/default-2/smart-snippet/smart-snippet.cypress.ts
@@ -1,14 +1,21 @@
+import {performSearch} from '../../../page-objects/actions/action-perform-search';
 import {configure} from '../../../page-objects/configurator';
 import {
   interceptSearch,
   mockSearchWithSmartSnippet,
   mockSearchWithoutSmartSnippet,
 } from '../../../page-objects/search';
+import {
+  useCaseEnum,
+  useCaseParamTest,
+  InsightInterfaceExpectations as InsightInterfaceExpect,
+} from '../../../page-objects/use-case';
 import {scope} from '../../../reporters/detailed-collector';
 import {SmartSnippetActions as Actions} from './smart-snippet-actions';
 import {SmartSnippetExpectations as Expect} from './smart-snippet-expectations';
 
 interface smartSnippetOptions {
+  useCase: string;
   maximumSnippetHeight: number;
 }
 
@@ -38,165 +45,180 @@ describe('quantic-smart-snippet', () => {
   const pageUrl = 's/quantic-smart-snippet';
 
   function visitPage(
-    withoutSmartSnippet = false,
-    options: Partial<smartSnippetOptions> = {}
+    options: Partial<smartSnippetOptions> = {},
+    withoutSmartSnippet = false
   ) {
     interceptSearch();
     if (withoutSmartSnippet) {
-      mockSearchWithoutSmartSnippet();
+      mockSearchWithoutSmartSnippet(options.useCase);
     } else {
-      mockSearchWithSmartSnippet({
-        question: exampleSmartSnippetQuestion,
-        answer: exampleSmartSnippetAnswer,
-        title: exampleSmartSnippetSourceTitle,
-        uri: exampleSmartSnippetSourceUri,
-        permanentId: examplePermanentId,
-      });
+      mockSearchWithSmartSnippet(
+        {
+          question: exampleSmartSnippetQuestion,
+          answer: exampleSmartSnippetAnswer,
+          title: exampleSmartSnippetSourceTitle,
+          uri: exampleSmartSnippetSourceUri,
+          permanentId: examplePermanentId,
+        },
+        options.useCase
+      );
     }
     cy.visit(pageUrl);
     configure(options);
+    if (options.useCase === useCaseEnum.insight) {
+      InsightInterfaceExpect.isInitialized();
+      performSearch();
+    }
   }
 
-  describe('when the query does not return a smart snippet', () => {
-    it('should not display the smart snippet', () => {
-      visitPage(true);
+  useCaseParamTest.forEach((param) => {
+    describe(param.label, () => {
+      describe('when the query does not return a smart snippet', () => {
+        it('should not display the smart snippet', () => {
+          visitPage({useCase: param.useCase}, true);
 
-      scope('when loading the page', () => {
-        Expect.displaySmartSnippetCard(false);
-      });
-    });
-  });
-
-  describe('when the query returns a smart snippet', () => {
-    describe('when the smart snippet answer is not collapsed', () => {
-      it('should properly display the smart snippet', () => {
-        visitPage();
-
-        scope('when loading the page', () => {
-          Expect.displaySmartSnippetCard(true);
-          Expect.displaySmartSnippetQuestion(exampleSmartSnippetQuestion);
-          Expect.displaySmartSnippetAnswer(exampleSmartSnippetAnswer);
-          Expect.displaySmartSnippetSourceUri(exampleSmartSnippetSourceUri);
-          Expect.displaySmartSnippetSourceTitle(exampleSmartSnippetSourceTitle);
-          Expect.displayCollapsedSmartSnippetAnswer(false);
-          Expect.displayExpandedSmartSnippetAnswer(false);
-          Expect.displaySmartSnippetAnswerToggle(false);
+          scope('when loading the page', () => {
+            Expect.displaySmartSnippetCard(false);
+          });
         });
+      });
 
-        scope(
-          'when an inlink within the smart snippet answer is clicked',
-          () => {
-            Actions.clickSmartSnippetInlineLink();
-            Expect.logOpenSmartSnippetInlineLink({
-              title: exampleSmartSnippetSourceTitle,
-              uri: exampleSmartSnippetSourceUri,
-              permanentId: examplePermanentId,
-              linkUrl: exampleInlineLinkUrl,
-              linkText: exampleInlineLinkText,
+      describe('when the query returns a smart snippet', () => {
+        describe('when the smart snippet answer is not collapsed', () => {
+          it('should properly display the smart snippet', () => {
+            visitPage({useCase: param.useCase});
+
+            scope('when loading the page', () => {
+              Expect.displaySmartSnippetCard(true);
+              Expect.displaySmartSnippetQuestion(exampleSmartSnippetQuestion);
+              Expect.displaySmartSnippetAnswer(exampleSmartSnippetAnswer);
+              Expect.displaySmartSnippetSourceUri(exampleSmartSnippetSourceUri);
+              Expect.displaySmartSnippetSourceTitle(
+                exampleSmartSnippetSourceTitle
+              );
+              Expect.displayCollapsedSmartSnippetAnswer(false);
+              Expect.displayExpandedSmartSnippetAnswer(false);
+              Expect.displaySmartSnippetAnswerToggle(false);
             });
-          }
-        );
 
-        scope('when the source title is clicked', () => {
-          Actions.clickSmartSnippetSourceTitle();
-          Expect.logOpenSmartSnippetSource({
-            title: exampleSmartSnippetSourceTitle,
-            uri: exampleSmartSnippetSourceUri,
-            permanentId: examplePermanentId,
+            scope(
+              'when an inlink within the smart snippet answer is clicked',
+              () => {
+                Actions.clickSmartSnippetInlineLink();
+                Expect.logOpenSmartSnippetInlineLink({
+                  title: exampleSmartSnippetSourceTitle,
+                  uri: exampleSmartSnippetSourceUri,
+                  permanentId: examplePermanentId,
+                  linkUrl: exampleInlineLinkUrl,
+                  linkText: exampleInlineLinkText,
+                });
+              }
+            );
+
+            scope('when the source title is clicked', () => {
+              Actions.clickSmartSnippetSourceTitle();
+              Expect.logOpenSmartSnippetSource({
+                title: exampleSmartSnippetSourceTitle,
+                uri: exampleSmartSnippetSourceUri,
+                permanentId: examplePermanentId,
+              });
+            });
+
+            scope('when the source uri is clicked', () => {
+              visitPage({useCase: param.useCase});
+              Actions.clickSmartSnippetSourceUri();
+              Expect.logOpenSmartSnippetSource({
+                title: exampleSmartSnippetSourceTitle,
+                uri: exampleSmartSnippetSourceUri,
+                permanentId: examplePermanentId,
+              });
+            });
           });
         });
 
-        scope('when the source uri is clicked', () => {
-          visitPage();
-          Actions.clickSmartSnippetSourceUri();
-          Expect.logOpenSmartSnippetSource({
-            title: exampleSmartSnippetSourceTitle,
-            uri: exampleSmartSnippetSourceUri,
-            permanentId: examplePermanentId,
+        describe('when the smart snippet answer is collapsed', () => {
+          it('should properly display the smart snippet', () => {
+            visitPage({useCase: param.useCase, maximumSnippetHeight: 0}, false);
+
+            scope('when loading the page', () => {
+              Expect.displaySmartSnippetCard(true);
+              Expect.displaySmartSnippetQuestion(exampleSmartSnippetQuestion);
+              Expect.displaySmartSnippetSourceUri(exampleSmartSnippetSourceUri);
+              Expect.displaySmartSnippetSourceTitle(
+                exampleSmartSnippetSourceTitle
+              );
+              Expect.displayCollapsedSmartSnippetAnswer(true);
+              Expect.displayExpandedSmartSnippetAnswer(false);
+              Expect.displaySmartSnippetShowMoreButton(true);
+            });
+
+            scope('when expanding the smart snippet answer', () => {
+              Actions.toggleSmartSnippetAnswer();
+              Expect.displayCollapsedSmartSnippetAnswer(false);
+              Expect.displayExpandedSmartSnippetAnswer(true);
+              Expect.displaySmartSnippetShowLessButton(true);
+              Expect.logExpandSmartSnippet();
+            });
+
+            scope('when collapsing the smart snippet answer', () => {
+              Actions.toggleSmartSnippetAnswer();
+              Expect.displayCollapsedSmartSnippetAnswer(true);
+              Expect.displayExpandedSmartSnippetAnswer(false);
+              Expect.displaySmartSnippetShowMoreButton(true);
+              Expect.logCollapseSmartSnippet();
+            });
           });
         });
-      });
-    });
 
-    describe('when the smart snippet answer is collapsed', () => {
-      it('should properly display the smart snippet', () => {
-        visitPage(false, {maximumSnippetHeight: 0});
+        describe('when clicking the feedback like button', () => {
+          it('should properly log the analytics', () => {
+            visitPage({useCase: param.useCase});
 
-        scope('when loading the page', () => {
-          Expect.displaySmartSnippetCard(true);
-          Expect.displaySmartSnippetQuestion(exampleSmartSnippetQuestion);
-          Expect.displaySmartSnippetSourceUri(exampleSmartSnippetSourceUri);
-          Expect.displaySmartSnippetSourceTitle(exampleSmartSnippetSourceTitle);
-          Expect.displayCollapsedSmartSnippetAnswer(true);
-          Expect.displayExpandedSmartSnippetAnswer(false);
-          Expect.displaySmartSnippetShowMoreButton(true);
-        });
-
-        scope('when expanding the smart snippet answer', () => {
-          Actions.toggleSmartSnippetAnswer();
-          Expect.displayCollapsedSmartSnippetAnswer(false);
-          Expect.displayExpandedSmartSnippetAnswer(true);
-          Expect.displaySmartSnippetShowLessButton(true);
-          Expect.logExpandSmartSnippet();
-        });
-
-        scope('when collapsing the smart snippet answer', () => {
-          Actions.toggleSmartSnippetAnswer();
-          Expect.displayCollapsedSmartSnippetAnswer(true);
-          Expect.displayExpandedSmartSnippetAnswer(false);
-          Expect.displaySmartSnippetShowMoreButton(true);
-          Expect.logCollapseSmartSnippet();
-        });
-      });
-    });
-
-    describe('when clicking the feedback like button', () => {
-      it('should properly log the analytics', () => {
-        visitPage();
-
-        scope('when clicking the like button', () => {
-          Expect.displaySmartSnippetCard(true);
-          Actions.clickSmartSnippetLikeButton();
-          Expect.logLikeSmartSnippet();
-          Expect.displayExplainWhyButton(false);
-        });
-      });
-    });
-
-    describe('when clicking the feedback dislike button', () => {
-      it('should properly log the analytics', () => {
-        visitPage();
-
-        scope('when clicking the dislike button', () => {
-          Expect.displaySmartSnippetCard(true);
-          Actions.clickSmartSnippetDislikeButton();
-          Expect.logDislikeSmartSnippet();
-          Expect.displayExplainWhyButton(true);
-        });
-
-        scope('when clicking the explain why button', () => {
-          Actions.clickSmartSnippetExplainWhyButton();
-          Expect.logOpenSmartSnippetFeedbackModal();
-        });
-
-        scope('when closing the feedback modal', () => {
-          Actions.clickFeedbackCancelButton();
-          Expect.logCloseSmartSnippetFeedbackModal();
-        });
-
-        scope('when selecting a feedback option', () => {
-          const exampleDetails = 'example details';
-          Actions.clickSmartSnippetExplainWhyButton();
-          Expect.logOpenSmartSnippetFeedbackModal();
-          Actions.clickFeedbackOption(feedbackOptions.indexOf(otherOption));
-          Actions.typeInFeedbackDetailsInput(exampleDetails);
-          Actions.clickFeedbackSubmitButton();
-          Expect.logSendSmartSnippetReason({
-            reason: otherOption,
-            details: exampleDetails,
+            scope('when clicking the like button', () => {
+              Expect.displaySmartSnippetCard(true);
+              Actions.clickSmartSnippetLikeButton();
+              Expect.logLikeSmartSnippet();
+              Expect.displayExplainWhyButton(false);
+            });
           });
-          Actions.clickFeedbackDoneButton();
-          Expect.logCloseSmartSnippetFeedbackModal();
+        });
+
+        describe('when clicking the feedback dislike button', () => {
+          it('should properly log the analytics', () => {
+            visitPage({useCase: param.useCase});
+
+            scope('when clicking the dislike button', () => {
+              Expect.displaySmartSnippetCard(true);
+              Actions.clickSmartSnippetDislikeButton();
+              Expect.logDislikeSmartSnippet();
+              Expect.displayExplainWhyButton(true);
+            });
+
+            scope('when clicking the explain why button', () => {
+              Actions.clickSmartSnippetExplainWhyButton();
+              Expect.logOpenSmartSnippetFeedbackModal();
+            });
+
+            scope('when closing the feedback modal', () => {
+              Actions.clickFeedbackCancelButton();
+              Expect.logCloseSmartSnippetFeedbackModal();
+            });
+
+            scope('when selecting a feedback option', () => {
+              const exampleDetails = 'example details';
+              Actions.clickSmartSnippetExplainWhyButton();
+              Expect.logOpenSmartSnippetFeedbackModal();
+              Actions.clickFeedbackOption(feedbackOptions.indexOf(otherOption));
+              Actions.typeInFeedbackDetailsInput(exampleDetails);
+              Actions.clickFeedbackSubmitButton();
+              Expect.logSendSmartSnippetReason({
+                reason: otherOption,
+                details: exampleDetails,
+              });
+              Actions.clickFeedbackDoneButton();
+              Expect.logCloseSmartSnippetFeedbackModal();
+            });
+          });
         });
       });
     });
