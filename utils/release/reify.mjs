647f1391f2d01ed8b22fba798942a02786504819
Benjamin Taillon <btaillon@coveo.com>
2023-05-08 11:07:16 -0500

    reify with dependencies in correct order
    https://coveord.atlassian.net/browse/KIT-2330

--- utils/release/reify.mjs
+++ utils/release/reify.mjs
@@ -1,23 +1,54 @@
 #!/usr/bin/env node
 import Arborist from '@npmcli/arborist';
-import {sync as globSync} from 'glob';
-import { existsSync, readFileSync } from 'node:fs';
+import {DepGraph} from 'dependency-graph';
 import {dirname, resolve} from 'node:path';
 import {fileURLToPath} from 'node:url';
 
-const rootFolder = resolve(dirname(fileURLToPath(import.meta.url)), '..', '..');
+/**
+ * @typedef {Map<string, Omit<Arborist.Edge, 'to'> & {workspace: boolean, to: Arborist.Link}>} EdgeMap
+ */
 
-const packagesToUpdate = globSync(
-  JSON.parse(readFileSync(resolve(rootFolder, 'package.json')).toString())
-    .workspaces,
-  {cwd: rootFolder}
-)
-  .filter((packagePath) => existsSync(resolve(packagePath, 'package.json')))
-  .map(
-    (packagePath) =>
-      JSON.parse(readFileSync(resolve(packagePath, 'package.json')).toString())
-        .name
-  );
+/**
+ * @param {Arborist.Node} rootNode
+ */
+function buildDependencyGraph(rootNode) {
+  const graph = /** @type {DepGraph<Arborist.Node>} */ (new DepGraph());
+  /**
+   * @param {Arborist.Node} node
+   */
+  function getWorkspaceDependencies(node) {
+    const edgesOut =
+      node.edgesOut instanceof Map
+        ? Array.from(node.edgesOut.values())
+        : node.edgesOut;
+    const workspaces = edgesOut.filter((edge) => edge.workspace);
+    return workspaces.map((edge) =>
+      edge.to instanceof Arborist.Link ? edge.to.target : edge.to
+    );
+  }
+
+  /**
+   * @param {Arborist.Node} node
+   */
+  function addWorkspaceDependencies(node) {
+    const dependencies = getWorkspaceDependencies(node);
+    for (const dependency of dependencies) {
+      graph.addDependency(node.name, dependency.name);
+      addWorkspaceDependencies(dependency);
+    }
+  }
+
+  const workspaces = getWorkspaceDependencies(rootNode);
+  for (const workspace of workspaces) {
+    graph.addNode(workspace.name, workspace);
+  }
+  for (const workspace of workspaces) {
+    addWorkspaceDependencies(workspace);
+  }
+  return graph;
+}
+
+const rootFolder = resolve(dirname(fileURLToPath(import.meta.url)), '..', '..');
 
 const arb = new Arborist({
   savePrefix: '',
@@ -27,7 +58,13 @@ const arb = new Arborist({
 });
 console.log('Loading virtual tree.');
 await arb.loadVirtual();
-console.log('Building ideal tree. The following packages will be updated:', packagesToUpdate);
+const packagesToUpdate = buildDependencyGraph(
+  /** @type {Arborist.Node} */ (arb.virtualTree)
+).overallOrder();
+console.log(
+  'Building ideal tree. The following packages will be updated:',
+  packagesToUpdate
+);
 await arb.buildIdealTree({
   update: {
     names: packagesToUpdate,
