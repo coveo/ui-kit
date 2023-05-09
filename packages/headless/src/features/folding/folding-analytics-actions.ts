e339365c073e37f87d953f7498566a742616b42c
simonmilord <simon.milord@gmail.com>
2023-05-09 10:18:43 -0400

    moved the folding analytics actions in their own file

---
+++ packages/headless/src/features/folding/folding-analytics-actions.ts
@@ -0,0 +1,30 @@
+import {Result} from '../../api/search/search/result';
+import {ClickAction, makeAnalyticsAction} from '../analytics/analytics-utils';
+import {
+  AnalyticsType,
+  documentIdentifier,
+  partialDocumentInformation,
+  validateResultPayload,
+} from '../analytics/analytics-utils';
+
+export const logShowMoreFoldedResults = (result: Result): ClickAction =>
+  makeAnalyticsAction(
+    'analytics/folding/showMore',
+    AnalyticsType.Click,
+    (client, state) => {
+      validateResultPayload(result);
+
+      return client.makeShowMoreFoldedResults(
+        partialDocumentInformation(result, state),
+        documentIdentifier(result)
+      );
+    }
+  );
+
+export const logShowLessFoldedResults = makeAnalyticsAction(
+  'analytics/folding/showLess',
+  AnalyticsType.Custom,
+  (client) => {
+    return client.makeShowLessFoldedResults();
+  }
+);
