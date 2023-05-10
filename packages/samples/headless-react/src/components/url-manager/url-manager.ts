fcab4fd7d6202a672fa8fd1fd56af25004c9e783
Yassine <ylakhdar@coveo.com>
2023-05-10 13:04:38 -0400

    Update packages/samples/headless-react/src/components/url-manager/url-manager.ts
    Co-authored-by: jpmarceau <39384459+jpmarceau@users.noreply.github.com>

--- packages/samples/headless-react/src/components/url-manager/url-manager.ts
+++ packages/samples/headless-react/src/components/url-manager/url-manager.ts
@@ -30,11 +30,11 @@ export function bindUrlManager(engine: SearchEngine) {
     if (!statusControllers.state.firstSearchExecuted) {
       // The purpose of using `replaceState()` instead of `pushState()` in this case is to ensure that the URL reflects the current state of the search page on the first interface load.
 
-      // If `pushState()` were used instead, you could possibly enter in a history loop, which could result in having to click the back button multiple times without being able to  return to a previous page.
-      // This situation happens when you have a component like the Tab component that adds a new state to the browser history stack.
+      // If `pushState()` were used instead, users could possibly enter in a history loop, having to click the back button multiple times without being able to return to a previous page.
+      // This situation happens with components such as the Tab component, which adds a new state to the browser history stack.
 
-      // Replacing the current state of the browser history with a new state, effectively updating the URL without adding a new entry to the history stack.
-      // For more info, visit https://docs.coveo.com/en/headless/latest/usage/synchronize-search-parameters-with-the-url/
+      // `replaceState` instead replaces the current state of the browser history with a new state, effectively updating the URL without adding a new entry to the history stack.
+      // See https://docs.coveo.com/en/headless/latest/usage/synchronize-search-parameters-with-the-url/
       history.replaceState(null, document.title, hash);
       return;
     }
