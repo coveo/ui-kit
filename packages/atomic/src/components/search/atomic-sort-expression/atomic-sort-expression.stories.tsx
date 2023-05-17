edb57331c4c1e17110bfb25f3c7e5274347d1742
Olivier Lamothe <olamothe@coveo.com>
2023-05-17 15:32:45 -0400

    docs(atomic): fix sort-expression storybook (#2905)
    https://coveord.atlassian.net/browse/KIT-2474

--- packages/atomic/src/components/search/atomic-sort-expression/atomic-sort-expression.stories.tsx
+++ packages/atomic/src/components/search/atomic-sort-expression/atomic-sort-expression.stories.tsx
@@ -4,7 +4,11 @@ const {defaultModuleExport, exportedStory} = defaultStory(
   'Atomic/SortDropdown/SortExpression',
   'atomic-sort-expression',
   {label: 'Relevance', expression: 'relevancy'},
-  {parentElement: () => document.createElement('atomic-sort-dropdown')}
+  {
+    parentElement: () => {
+      return document.createElement('atomic-sort-dropdown');
+    },
+  }
 );
 
 export default {
