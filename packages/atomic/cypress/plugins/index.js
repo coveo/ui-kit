f59b2696dcbf507b8eb00d7d137661ecb893fea8
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-05-25 16:02:50 -0700

    add required pkgs and config for code cov
    https://coveord.atlassian.net/browse/KIT-2436

--- packages/atomic/cypress/plugins/index.js
+++ packages/atomic/cypress/plugins/index.js
@@ -15,7 +15,12 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (_on, _config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+module.exports = (on, config) => {
+  require('@cypress/code-coverage/task')(on, config);
+
+  // add other tasks to be registered here
+
+  // IMPORTANT to return the config object
+  // with the any changed environment variables
+  return config;
 };
