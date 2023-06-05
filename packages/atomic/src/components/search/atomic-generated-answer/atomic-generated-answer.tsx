ef8f6ddf9de6354849b60ac15c54509b54d3b90e
Nathan Lafrance-Berger <nathan.lafranceb@gmail.com>
2023-06-05 10:33:16 -0400

    feat(atomic): prelim components

---
+++ packages/atomic/src/components/search/atomic-generated-answer/atomic-generated-answer.tsx
@@ -0,0 +1,84 @@
+import {
+  SearchStatus,
+  SearchStatusState,
+  buildSearchStatus,
+  buildGeneratedAnswer,
+  GeneratedAnswer,
+  GeneratedAnswerState,
+} from '@coveo/headless';
+import {Component, h, State} from '@stencil/core';
+import {
+  BindStateToController,
+  InitializableComponent,
+  InitializeBindings,
+} from '../../../utils/initialization-utils';
+import {Bindings} from '../atomic-search-interface/atomic-search-interface';
+
+/**
+ * @internal
+ */
+@Component({
+  tag: 'generated-answer',
+  styleUrl: 'generated-answer.css',
+  shadow: true,
+})
+export class AtomicGeneratedAnswer implements InitializableComponent {
+  @InitializeBindings() public bindings!: Bindings;
+  public generatedAnswer!: GeneratedAnswer;
+  public searchStatus!: SearchStatus;
+
+  @BindStateToController('generatedAnswer')
+  @State()
+  private generatedAnswerState!: GeneratedAnswerState;
+
+  @BindStateToController('searchStatus')
+  @State()
+  private searchStatusState!: SearchStatusState;
+
+  @State()
+  public error!: Error;
+
+  @State()
+  hidden = true;
+
+  public initialize() {
+    this.generatedAnswer = buildGeneratedAnswer(this.bindings.engine);
+    this.searchStatus = buildSearchStatus(this.bindings.engine);
+  }
+
+  private get loader() {
+    return (
+      <div class="typing-indicator">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    );
+  }
+
+  public render() {
+    if (
+      !this.generatedAnswerState.answer?.length ||
+      this.searchStatusState.hasError
+    ) {
+      return null;
+    }
+    return (
+      <aside
+        class="bg-background border border-neutral rounded-lg p-6 pb-4 text-on-background"
+        part="container"
+      >
+        <article>
+          {this.generatedAnswerState.isLoading ? (
+            this.loader
+          ) : (
+            <div part="generated-content">
+              <div part="badge">Generated answer for you</div>
+              <p part="text">{this.generatedAnswerState.answer}</p>
+            </div>
+          )}
+        </article>
+      </aside>
+    );
+  }
+}
