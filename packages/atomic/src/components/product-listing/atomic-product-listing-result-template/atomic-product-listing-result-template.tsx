908095954d16489d6da8cf55c0f299763f07c032
thudon-coveo <94566483+thudon-coveo@users.noreply.github.com>
2023-06-08 13:48:10 -0400

    review comments

--- packages/atomic/src/components/product-listing/atomic-product-listing-result-template/atomic-product-listing-result-template.tsx
+++ packages/atomic/src/components/product-listing/atomic-product-listing-result-template/atomic-product-listing-result-template.tsx
@@ -1,5 +1,8 @@
-import {ResultTemplate, ResultTemplateCondition} from '@coveo/headless';
 import {Component, Element, Prop, Method, State} from '@stencil/core';
+import {
+  ProductListingResultTemplateCondition,
+  ProductListingResultTemplate,
+} from '..';
 import {MapProp} from '../../../utils/props-utils';
 import {
   ResultTemplateCommon,
@@ -9,9 +12,7 @@ import {
 /**
  * The `atomic-product-listing-template` component determines the format of the query results, depending on the conditions that are defined for each template. A `template` element must be the child of an `atomic-product-listing-template`, and either an `atomic-result-list` or `atomic-folded-result-list` must be the parent of each `atomic-result-template`.
  *
- * Note: Any `<script>` tags defined inside of a `<template>` element will not be executed when results are being rendered.
- * @MapProp name: mustMatch;attr: must-match;docs: The field and values that define which result items the condition must be applied to. For example, a template with the following attribute only applies to result items whose `filetype` is `lithiummessage` or `YouTubePlaylist`: `must-match-filetype="lithiummessage,YouTubePlaylist"`;type: Record<string, string[]> ;default: {}
- * @MapProp name: mustNotMatch;attr: must-not-match;docs: The field and values that define which result items the condition must not be applied to. For example, a template with the following attribute only applies to result items whose `filetype` is not `lithiummessage`: `must-not-match-filetype="lithiummessage";type: Record<string, string[]> ;default: {}
+ * @internal
  */
 @Component({
   tag: 'atomic-product-listing-result-template',
@@ -30,7 +31,7 @@ export class AtomicProductListingResultTemplate {
    * For example, a template with the following condition only applies to results whose `title` contains `singapore`:
    * `[(result) => /singapore/i.test(result.title)]`
    */
-  @Prop() public conditions: ResultTemplateCondition[] = [];
+  @Prop() public conditions: ProductListingResultTemplateCondition[] = [];
 
   @MapProp({splitValues: true}) public mustMatch: Record<string, string[]> = {};
 
@@ -59,7 +60,7 @@ export class AtomicProductListingResultTemplate {
    * Gets the appropriate result template based on conditions applied.
    */
   @Method()
-  public async getTemplate(): Promise<ResultTemplate<DocumentFragment> | null> {
+  public async getTemplate(): Promise<ProductListingResultTemplate<DocumentFragment> | null> {
     return this.resultTemplateCommon.getTemplate(this.conditions, this.error);
   }
 
--- packages/atomic/src/components/product-listing/index.ts
+++ packages/atomic/src/components/product-listing/index.ts
@@ -1,9 +1,10 @@
 export {
   InteractiveResult as ProductListingInteractiveResult,
   buildInteractiveResult as buildProductListingInteractiveResult,
-  //   ResultTemplateCondition as ProductListingResultTemplateCondition,
-  //   ResultTemplate as ProductListingResultTemplate,
-  //   ResultTemplatesHelpers as ProductListingResultTemplatesHelpers,
   LogLevel as ProductListingLogLevel,
 } from '@coveo/headless/product-listing';
-export {Result as ProductListingResult} from '@coveo/headless';
+export {
+  Result as ProductListingResult,
+  ResultTemplateCondition as ProductListingResultTemplateCondition,
+  ResultTemplate as ProductListingResultTemplate,
+} from '@coveo/headless';
