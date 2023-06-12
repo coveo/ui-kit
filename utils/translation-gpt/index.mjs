74678143792403650e8c9a0d1353a4750231774c
Olivier Lamothe <olamothe@coveo.com>
2023-06-12 15:24:21 -0400

    feat(atomic): mass translation of atomic with gpt model
    https://coveord.atlassian.net/browse/KIT-2545

---
+++ utils/translation-gpt/index.mjs
@@ -0,0 +1,97 @@
+import {readFileSync, writeFileSync} from 'fs';
+import {env} from 'process';
+
+const fileToTranslate = JSON.parse(
+  readFileSync('../../packages/atomic/src/locales.json')
+);
+const fileAlreadyTranslated = JSON.parse(readFileSync('./temporary.json'));
+
+const languages = [
+  'en',
+  'fr',
+  'cs',
+  'da',
+  'de',
+  'el',
+  'es',
+  'fi',
+  'hu',
+  'id',
+  'it',
+  'ja',
+  'ko',
+  'nl',
+  'no',
+  'pl',
+  'pt',
+  'pt-br',
+  'ru',
+  'sv',
+  'th',
+  'tr',
+  'zh',
+  'zh-cn',
+  'zh-tw',
+];
+
+const prompt = (mainKey, toTranslate) =>
+  `I will give you a string to translate. The string can possible contain variables between braces similar to this: {{variable}}. 
+  Those variables are meant to represent dynamic values.
+  If there are no variable, then do a normal translation.
+  I want you to translate it in the language code ${languages.join(
+    ', '
+  )} and to place the variable with braces correctly in the new string if it applies.
+  For each language code, answer in valid JSON format, with the the main key being ${mainKey} and each individual language code is a property of ${mainKey}. 
+  The value of each property of ${mainKey} should be the translated string.
+  The string to translate is: 
+  "${toTranslate}"`;
+
+async function main() {
+  for (const translationKey of Object.keys(fileToTranslate)) {
+    const englishTranslation = fileToTranslate[translationKey]['en'];
+    if (fileAlreadyTranslated[translationKey]) {
+      continue;
+    }
+
+    try {
+      const res = await fetch(env.COVEO_AZURE_OPEN_AI_ENDPOINT, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'api-key': env.COVEO_AZURE_OPEN_AI_KEY,
+        },
+        body: JSON.stringify({
+          messages: [
+            {role: 'user', content: prompt(translationKey, englishTranslation)},
+          ],
+          stream: false,
+        }),
+      });
+
+      const data = await res.json();
+      const answer = data.choices[0].message.content;
+
+      fileAlreadyTranslated[translationKey] =
+        JSON.parse(answer)[translationKey];
+      writeFileSync(
+        'temporary.json',
+        JSON.stringify(fileAlreadyTranslated, null, 2)
+      );
+
+      console.log(`${answer}`);
+    } catch (e) {
+      console.error(`Error while translating ${translationKey} : ${e}.`);
+      console.error(
+        'Troubleshoot the issue, then restart to script to continue translation.'
+      );
+    }
+  }
+
+  writeFileSync(
+    '../../packages/atomic/src/locales.json',
+    JSON.stringify(fileAlreadyTranslated, null, 2)
+  );
+  writeFileSync('temporary.json', JSON.stringify({}, null, 2));
+}
+
+main();
