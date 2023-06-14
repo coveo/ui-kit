94d0c2d20620ae7d1cc7ea9742f20828abce5445
aprudhomme-coveo <132079077+aprudhomme-coveo@users.noreply.github.com>
2023-06-14 09:11:01 -0400

    Update packages/headless/src/features/facets/automatic-facets/automatic-facets-actions-loader.ts
    Co-authored-by: jpmarceau <39384459+jpmarceau@users.noreply.github.com>

--- packages/headless/src/features/facets/automatic-facets/automatic-facets-actions-loader.ts
+++ packages/headless/src/features/facets/automatic-facets/automatic-facets-actions-loader.ts
@@ -17,7 +17,7 @@ export interface AutomaticFacetsActionCreators {
 }
 
 /**
- * Loads the automatic facets actions and adds the reducer to the search engine.
+ * Loads the automatic facet actions and adds the reducer to the search engine.
  *
  * @param engine - The headless search engine
  * @returns An object holding the automatic facets action creators
