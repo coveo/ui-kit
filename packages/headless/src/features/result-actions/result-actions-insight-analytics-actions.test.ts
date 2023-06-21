22d9cac152f4202bb507d0d2ac3670204cb8e0a1
erocheleau <erocheleau@coveo.com>
2023-06-21 16:08:33 -0400

    feat(SFINT-5082): Added the logCaseSendEmail, logFeedItemTextPost insight analytics actions

--- packages/headless/src/features/result-actions/result-actions-insight-analytics-actions.test.ts
+++ packages/headless/src/features/result-actions/result-actions-insight-analytics-actions.test.ts
@@ -2,14 +2,22 @@ import {buildMockRaw, buildMockResult} from '../../test';
 import {buildMockInsightEngine} from '../../test/mock-engine';
 import {buildMockInsightState} from '../../test/mock-insight-state';
 import {buildMockSearchState} from '../../test/mock-search-state';
-import {logCopyToClipboard} from './result-actions-insight-analytics-actions';
+import {
+  logCaseSendEmail,
+  logCopyToClipboard,
+  logFeedItemTextPost,
+} from './result-actions-insight-analytics-actions';
 
 const mockLogCopyToClipboard = jest.fn();
+const mockLogCaseSendEmail = jest.fn();
+const mockLogFeedItemTextPost = jest.fn();
 
 jest.mock('coveo.analytics', () => {
   const mockCoveoInsightClient = jest.fn(() => ({
     disable: () => {},
     logCopyToClipboard: mockLogCopyToClipboard,
+    logCaseSendEmail: mockLogCaseSendEmail,
+    logFeedItemTextPost: mockLogFeedItemTextPost,
   }));
 
   return {
@@ -70,7 +78,7 @@ const resultParams = {
 
 const testResult = buildMockResult(resultParams);
 
-describe('logExpandToFullUI', () => {
+describe('logCopyToClipboard', () => {
   it('should log #logCopyToClipboard with the right payload', async () => {
     const engine = buildMockInsightEngine({
       state: buildMockInsightState({
@@ -97,6 +105,70 @@ describe('logExpandToFullUI', () => {
       expectedDocumentIdentifier
     );
     expect(mockLogCopyToClipboard.mock.calls[0][2]).toStrictEqual(
+      expectedMetadata
+    );
+  });
+});
+
+describe('logCaseSendAsEmail', () => {
+  it('should log #logCaseSendAsEmail with the right payload', async () => {
+    const engine = buildMockInsightEngine({
+      state: buildMockInsightState({
+        search: buildMockSearchState({
+          results: [testResult],
+        }),
+        insightCaseContext: {
+          caseContext: {
+            Case_Subject: exampleSubject,
+            Case_Description: exampleDescription,
+          },
+          caseId: exampleCaseId,
+          caseNumber: exampleCaseNumber,
+        },
+      }),
+    });
+    await engine.dispatch(logCaseSendEmail(testResult));
+
+    expect(mockLogCaseSendEmail).toBeCalledTimes(1);
+    expect(mockLogCaseSendEmail.mock.calls[0][0]).toStrictEqual(
+      expectedDocumentInfo
+    );
+    expect(mockLogCaseSendEmail.mock.calls[0][1]).toStrictEqual(
+      expectedDocumentIdentifier
+    );
+    expect(mockLogCaseSendEmail.mock.calls[0][2]).toStrictEqual(
+      expectedMetadata
+    );
+  });
+});
+
+describe('logFeedItemTextPost', () => {
+  it('should log #logFeedItemTextPost with the right payload', async () => {
+    const engine = buildMockInsightEngine({
+      state: buildMockInsightState({
+        search: buildMockSearchState({
+          results: [testResult],
+        }),
+        insightCaseContext: {
+          caseContext: {
+            Case_Subject: exampleSubject,
+            Case_Description: exampleDescription,
+          },
+          caseId: exampleCaseId,
+          caseNumber: exampleCaseNumber,
+        },
+      }),
+    });
+    await engine.dispatch(logFeedItemTextPost(testResult));
+
+    expect(mockLogFeedItemTextPost).toBeCalledTimes(1);
+    expect(mockLogFeedItemTextPost.mock.calls[0][0]).toStrictEqual(
+      expectedDocumentInfo
+    );
+    expect(mockLogFeedItemTextPost.mock.calls[0][1]).toStrictEqual(
+      expectedDocumentIdentifier
+    );
+    expect(mockLogFeedItemTextPost.mock.calls[0][2]).toStrictEqual(
       expectedMetadata
     );
   });
