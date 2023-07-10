10ff67058322e5aa9f06ea23ac780c3f880fa726
David Brooke <38883189+dmbrooke@users.noreply.github.com>
2023-07-10 14:04:33 -0400

    Use safer import
    https://coveord.atlassian.net/browse/KIT-2552

--- packages/headless/src/features/facets/facet-set/interfaces/options.ts
+++ packages/headless/src/features/facets/facet-set/interfaces/options.ts
@@ -1,4 +1,4 @@
-import {FacetOptions} from '../../../../controllers';
+import {FacetOptions} from '../../../../controllers/core/facets/facet/headless-core-facet-options';
 
 export type FacetOptionalParameters = Required<
   Pick<
