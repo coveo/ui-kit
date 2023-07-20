fa15ea844e8e9fe6f1e73556178bf42ea2361010
fbeaudoincoveo <fbeaudoin@coveo.com>
2023-07-20 14:16:31 -0400

    Add missing action cases

--- packages/headless/src/features/placement-set/placement-set-slice.ts
+++ packages/headless/src/features/placement-set/placement-set-slice.ts
@@ -1,5 +1,16 @@
 import {createReducer} from '@reduxjs/toolkit';
-import {getBadge, getRecs} from './placement-set-action';
+import {
+  getBadge,
+  getRecs,
+  setCartSkus,
+  setLocale,
+  setOrderSkus,
+  setPlpSkus,
+  setProductSku,
+  setRecsSkus,
+  setSearchSkus,
+  setView,
+} from './placement-set-action';
 import {
   getBadgesInitialState,
   getPlacementSetInitialState,
@@ -10,6 +21,38 @@ export const placementSetReducer = createReducer(
   getPlacementSetInitialState(),
   (builder) => {
     builder
+      .addCase(setCartSkus, (state, action) => {
+        state.skus.cart = action.payload.skus;
+      })
+      .addCase(setOrderSkus, (state, action) => {
+        state.skus.order = action.payload.skus;
+      })
+      .addCase(setPlpSkus, (state, action) => {
+        state.skus.plp = action.payload.skus;
+      })
+      .addCase(setProductSku, (state, action) => {
+        state.skus.product = action.payload;
+      })
+      .addCase(setRecsSkus, (state, action) => {
+        state.skus.recs = action.payload.skus;
+      })
+      .addCase(setSearchSkus, (state, action) => {
+        state.skus.search = action.payload.skus;
+      })
+      .addCase(setLocale, (state, action) => {
+        state.view.currency = action.payload.currency;
+        state.view.locale = action.payload.locale;
+      })
+      .addCase(setView, (state, action) => {
+        state.view.type = action.payload.type ?? '';
+        state.view.subtype = action.payload.subtype;
+        state.view.type !== 'category' ? (state.skus.plp = []) : {};
+        state.view.type !== 'product' ? (state.skus.product = undefined) : {};
+        state.view.type !== 'search' ? (state.skus.search = []) : {};
+        state.view.type !== 'checkout' && state.view.type !== 'confirmation'
+          ? (state.skus.order = [])
+          : {};
+      })
       .addCase(getBadge.fulfilled, (state, action) => {
         state.badges[action.payload.placementId] = action.payload;
       })
@@ -23,13 +66,13 @@ export const placementSetReducer = createReducer(
         };
       })
       .addCase(getRecs.fulfilled, (state, action) => {
-        state.recs[action.payload.placementId] = action.payload;
+        state.recommendations[action.payload.placementId] = action.payload;
       })
       .addCase(getRecs.rejected, (state, action) => {
         if (!action.payload) {
           return;
         }
-        state.recs[action.payload.placementId] = {
+        state.recommendations[action.payload.placementId] = {
           error: action.payload,
           ...getRecsInitialState(),
         };
