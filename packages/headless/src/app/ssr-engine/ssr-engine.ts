6c93b5548b26f596555e714d6bc09807164fadaa
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-07-24 14:25:38 -0700

    complete copying over all required types
    https://coveord.atlassian.net/browse/KIT-2600

--- packages/headless/src/app/ssr-engine/ssr-engine.ts
+++ packages/headless/src/app/ssr-engine/ssr-engine.ts
@@ -50,6 +50,220 @@ export type EngineDefinitionOptions<
   controllers?: TControllers;
 };
 
+type HasKeys<TObject> = TObject extends {}
+  ? keyof TObject extends never
+    ? false
+    : true
+  : boolean;
+
+export type InferControllerPropsFromDefinition<
+  TController extends ControllerDefinition<CoreEngine, Controller>
+> = TController extends ControllerDefinitionWithProps<
+  CoreEngine,
+  Controller,
+  infer Props
+>
+  ? Props
+  : TController extends ControllerDefinitionWithoutProps<CoreEngine, Controller>
+  ? {}
+  : unknown;
+
+export type InferControllerPropsMapFromDefinitions<
+  TControllers extends ControllerDefinitionsMap<CoreEngine, Controller>
+> = {
+  [K in keyof TControllers as HasKeys<
+    InferControllerPropsFromDefinition<TControllers[K]>
+  > extends false
+    ? never
+    : K]: InferControllerPropsFromDefinition<TControllers[K]>;
+};
+
+export interface EngineAndControllers<
+  TEngine extends CoreEngine,
+  TControllers extends ControllersMap
+> {
+  engine: TEngine;
+  controllers: TControllers;
+}
+
+export interface ControllersMap {
+  [customName: string]: Controller;
+}
+
+export interface BuildFunctionWithProps<
+  TEngine extends CoreEngine,
+  TEngineOptions,
+  TControllersMap extends ControllersMap,
+  TControllersProps extends ControllersPropsMap
+> {
+  /**
+   * Initializes an engine and controllers from the definition.
+   */
+  build(
+    options: EngineDefinitionBuildOptionsWithProps<
+      TEngineOptions,
+      TControllersProps
+    >
+  ): Promise<EngineAndControllers<TEngine, TControllersMap>>;
+}
+
+export interface BuildFunctionWithoutProps<
+  TEngine extends CoreEngine,
+  TEngineOptions,
+  TControllersMap extends ControllersMap
+> {
+  /**
+   * Initializes an engine and controllers from the definition.
+   */
+  build(
+    options?: EngineDefinitionBuildOptionsWithoutProps<TEngineOptions>
+  ): Promise<EngineAndControllers<TEngine, TControllersMap>>;
+}
+
+export type ExecuteOnceFunctionWithoutProps<
+  TControllersSnapshot extends ControllerSnapshotsMap,
+  TSearchFulfilledAction extends AnyAction
+> = {
+  /**
+   * Executes only the initial search for a given configuration, then returns a resumable snapshot of engine state along with the state of the controllers.
+   *
+   * Useful for static generation and server-side rendering.
+   */
+  fetchInitialState(): Promise<
+    EngineSnapshot<TSearchFulfilledAction, TControllersSnapshot>
+  >;
+};
+
+export interface EngineDefinitionHydrateOptionsWithoutProps<
+  TSearchFulfilledAction extends AnyAction
+> {
+  searchFulfilledAction: TSearchFulfilledAction;
+}
+
+export type HydrateFunctionWithoutProps<
+  TEngine extends CoreEngine,
+  TControllers extends ControllersMap,
+  TSearchFulfilledAction extends AnyAction
+> = {
+  /**
+   * Creates a new engine from the snapshot of a previous engine.
+   *
+   * Useful when hydrating a server-side-rendered engine.
+   */
+  hydrateInitialState(
+    options: EngineDefinitionHydrateOptionsWithoutProps<TSearchFulfilledAction>
+  ): Promise<EngineAndControllers<TEngine, TControllers>>;
+};
+
+export interface EngineDefinitionWithoutProps<
+  TEngine extends CoreEngine,
+  TControllers extends ControllerDefinitionsMap<TEngine, Controller>,
+  TEngineOptions
+> extends ExecuteOnceFunctionWithoutProps<
+      InferControllerSnapshotsMapFromDefinitions<TControllers>,
+      AnyAction
+    >,
+    HydrateFunctionWithoutProps<
+      TEngine,
+      InferControllersMapFromDefinition<TControllers>,
+      AnyAction
+    >,
+    BuildFunctionWithoutProps<
+      TEngine,
+      TEngineOptions,
+      InferControllersMapFromDefinition<TControllers>
+    > {}
+
+export type EngineDefinitionExecuteOnceOptions<
+  TControllersSnapshot extends ControllersPropsMap
+> = {controllers: TControllersSnapshot};
+
+export type ExecuteOnceFunctionWithProps<
+  TControllersSnapshot extends ControllerSnapshotsMap,
+  TSearchFulfilledAction extends AnyAction,
+  TControllersProps extends ControllersPropsMap
+> = {
+  /**
+   * Executes only the initial search for a given configuration, then returns a resumable snapshot of engine state along with the state of the controllers.
+   *
+   * Useful for static generation and server-side rendering.
+   */
+  fetchInitialState(
+    options: EngineDefinitionExecuteOnceOptions<TControllersProps>
+  ): Promise<EngineSnapshot<TSearchFulfilledAction, TControllersSnapshot>>;
+};
+
+export interface EngineDefinitionHydrateOptionsWithProps<
+  TSearchFulfilledAction extends AnyAction,
+  TControllersProps extends ControllersPropsMap
+> extends EngineDefinitionHydrateOptionsWithoutProps<TSearchFulfilledAction> {
+  controllers: TControllersProps;
+}
+
+export type HydrateFunctionWithProps<
+  TEngine extends CoreEngine,
+  TControllers extends ControllersMap,
+  TSearchFulfilledAction extends AnyAction,
+  TControllersProps extends ControllersPropsMap
+> = {
+  /**
+   * Creates a new engine from the snapshot of a previous engine.
+   *
+   * Useful when hydrating a server-side-rendered engine.
+   */
+  hydrateInitialState(
+    options: EngineDefinitionHydrateOptionsWithProps<
+      TSearchFulfilledAction,
+      TControllersProps
+    >
+  ): Promise<EngineAndControllers<TEngine, TControllers>>;
+};
+
+export interface EngineDefinitionWithProps<
+  TEngine extends CoreEngine,
+  TControllers extends ControllerDefinitionsMap<TEngine, Controller>,
+  TEngineOptions,
+  TControllerProps extends ControllersPropsMap
+> extends ExecuteOnceFunctionWithProps<
+      InferControllerSnapshotsMapFromDefinitions<TControllers>,
+      AnyAction,
+      TControllerProps
+    >,
+    HydrateFunctionWithProps<
+      TEngine,
+      InferControllersMapFromDefinition<TControllers>,
+      AnyAction,
+      TControllerProps
+    >,
+    BuildFunctionWithProps<
+      TEngine,
+      TEngineOptions,
+      InferControllersMapFromDefinition<TControllers>,
+      TControllerProps
+    > {}
+
+export type EngineDefinition<
+  TEngine extends CoreEngine,
+  TControllers extends ControllerDefinitionsMap<TEngine, Controller>,
+  TEngineOptions
+> = HasKeys<InferControllerPropsMapFromDefinitions<TControllers>> extends true
+  ? EngineDefinitionWithProps<
+      TEngine,
+      TControllers,
+      TEngineOptions,
+      InferControllerPropsMapFromDefinitions<TControllers>
+    >
+  : InferControllerPropsMapFromDefinitions<TControllers> extends false
+  ? EngineDefinitionWithoutProps<TEngine, TControllers, TEngineOptions>
+  :
+      | EngineDefinitionWithProps<
+          TEngine,
+          TControllers,
+          TEngineOptions,
+          ControllersPropsMap
+        >
+      | EngineDefinitionWithoutProps<TEngine, TControllers, TEngineOptions>;
+
 export type SearchEngineDefinition<
   TControllers extends ControllerDefinitionsMap<SearchEngine, Controller>
   // TODO: try to import a simplified version of  EngineDefinition
