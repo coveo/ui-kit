a790cf62cab1b2489bfc6e0a9d9ada87df7680bf
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-07-28 19:06:46 -0700

    update comments

--- packages/samples/headless-ssr/app/page.tsx
+++ packages/samples/headless-ssr/app/page.tsx
@@ -1,9 +1,8 @@
 import {fetchInitialState} from '@/app/common/engine';
 import SearchPage from './components/search-page';
 
+// Entry point SSR function
 export default async function Search() {
-  // TODO: Fix headless isExpiredTokenError (KIT-2620)
-  // TODO Debug: engineSnapshot Size keeps changing non-trivially with page refresh
   const engineSnapshot = await fetchInitialState({controllers: {}});
   return <SearchPage engineSnapshot={engineSnapshot}></SearchPage>;
 }
