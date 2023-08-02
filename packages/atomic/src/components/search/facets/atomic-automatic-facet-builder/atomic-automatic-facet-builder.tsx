be0e451b126d4a3b06262db5d63b21dcebab93f5
aprudhomme-coveo <132079077+aprudhomme-coveo@users.noreply.github.com>
2023-08-02 10:03:59 -0400

    KIT-2635-2

--- packages/atomic/src/components/search/facets/atomic-automatic-facet-builder/atomic-automatic-facet-builder.tsx
+++ packages/atomic/src/components/search/facets/atomic-automatic-facet-builder/atomic-automatic-facet-builder.tsx
@@ -70,7 +70,7 @@ export class AtomicAutomaticFacetBuilder implements InitializableComponent {
 
   private validateProps() {
     new Schema({
-      desiredCount: new NumberValue({min: 1, required: true}),
+      desiredCount: new NumberValue({min: 1, max: 10, required: true}),
       areCollapsed: new BooleanValue({default: true, required: false}),
     }).validate({
       desiredCount: this.desiredCount,
