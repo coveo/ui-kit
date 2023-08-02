e5da0183e9b077c4b8d163c4532a9b32b9c8e3f6
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-08-01 18:58:54 -0700

    add loading page for automatic suspense
    https://nextjs.org/docs/app/building-your-application/routing/loading-ui-and-streaming
    https://coveord.atlassian.net/browse/KIT-2600

--- packages/samples/headless-ssr/src/app/page.tsx
+++ packages/samples/headless-ssr/src/app/page.tsx
@@ -5,9 +5,5 @@ import {Suspense} from 'react';
 // Entry point SSR function
 export default async function Search() {
   const engineSnapshot = await fetchInitialState({controllers: {}});
-  return (
-    <Suspense fallback={<div>Hydrating engine ..</div>}>
-      <SearchPage engineSnapshot={engineSnapshot}></SearchPage>
-    </Suspense>
-  );
+  return <SearchPage engineSnapshot={engineSnapshot}></SearchPage>;
 }
