fdcc88684dbdf654f7aafe7909381bf578b124d4
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-08-03 18:54:50 -0700

    add web vitals check
    https://coveord.atlassian.net/browse/KIT-2643

--- packages/samples/headless-ssr/cypress/e2e/ssr.cy.ts
+++ packages/samples/headless-ssr/cypress/e2e/ssr.cy.ts
@@ -1,3 +1,5 @@
+import 'cypress-web-vitals';
+
 const NEXT_DEV_SERVER = 'http://localhost:3000/';
 describe('headless ssr smoke tests', () => {
   it('renders page in CSR as expected', () => {
@@ -6,5 +8,10 @@ describe('headless ssr smoke tests', () => {
       `Hydrated engine with ${numResults} results`
     );
     cy.get('li').should('have.length', numResults);
+  });
+
+  it('should pass the web-vitals audits', () => {
+    const VITALS_THRESHOLD = {};
+    cy.vitals({url: NEXT_DEV_SERVER});
   });
 });
