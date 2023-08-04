1acb1aa5a31c8b594181b6c1907e8fb7549db205
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-08-04 13:09:46 -0700

    add base url config
    https://coveord.atlassian.net/browse/KIT-2643

--- packages/samples/headless-ssr/cypress/e2e/ssr.cy.ts
+++ packages/samples/headless-ssr/cypress/e2e/ssr.cy.ts
@@ -1,17 +1,14 @@
 import 'cypress-web-vitals';
 
-const NEXT_DEV_SERVER = 'http://localhost:3000/';
 describe('headless ssr smoke tests', () => {
   it('renders page in CSR as expected', () => {
     const numResults = 10;
-    cy.visit(NEXT_DEV_SERVER).contains(
-      `Hydrated engine with ${numResults} results`
-    );
+    cy.visit('/').contains(`Hydrated engine with ${numResults} results`);
     cy.get('li').should('have.length', numResults);
   });
 
   it('should pass the web-vitals audits', () => {
-    const VITALS_THRESHOLD = {};
-    cy.vitals({url: NEXT_DEV_SERVER});
+    const VITALS_THRESHOLD = {}; // TODO
+    cy.vitals();
   });
 });
