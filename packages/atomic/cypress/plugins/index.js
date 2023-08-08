bcc4cfb9c2ee2007458bf26742b12e43b6be95c6
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-08-08 11:11:09 -0700

    add is headed conditional
    https://coveord.atlassian.net/browse/KIT-2576

--- packages/atomic/cypress/plugins/index.js
+++ packages/atomic/cypress/plugins/index.js
@@ -18,17 +18,18 @@
 module.exports = (on, _config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-
-  // auto open devtools in headed mode (does not appear in screenshots, recording)
-  // https://github.com/cypress-io/cypress/issues/2024
   on('before:browser:launch', (browser = {}, launchOptions) => {
     if (browser.family === 'chromium' && browser.name !== 'electron') {
-      launchOptions.args.push('--auto-open-devtools-for-tabs');
-      // Workaround to use new headless mode until Cypress can be upgraded to >= 12.15 (KIT-2576)
-      // https://developer.chrome.com/articles/new-headless/
-      const version = parseInt(browser.majorVersion);
-      if (version >= 112) {
-        launchOptions.args.push('--headless=new');
+      if (browser.isHeadless) {
+        // Workaround to use new headless mode until Cypress can be upgraded to >= 12.15 (KIT-2576)
+        // https://developer.chrome.com/articles/new-headless/
+        const version = parseInt(browser.majorVersion);
+        if (version >= 112) {
+          launchOptions.args.push('--headless=new');
+        }
+      } else {
+        // auto open devtools in headed (local dev) mode to increase visibility of errors in console
+        launchOptions.args.push('--auto-open-devtools-for-tabs');
       }
     } else if (browser.family === 'firefox') {
       launchOptions.args.push('-devtools');
