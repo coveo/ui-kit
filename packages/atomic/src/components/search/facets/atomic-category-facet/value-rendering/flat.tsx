49004aff6d02bdb5e3bf922b7dbe8bd4e4f4bf14
Louis Bompart <lbompart@coveo.com>
2023-08-11 16:27:03 +0200

    flat
    https://coveord.atlassian.net/browse/KIT-2651

---
+++ packages/atomic/src/components/search/facets/atomic-category-facet/value-rendering/flat.tsx
@@ -0,0 +1,75 @@
+import type {CategoryFacet, CategoryFacetValue} from '@coveo/headless';
+import {Fragment, FunctionalComponent, h} from '@stencil/core';
+import {i18n} from 'i18next';
+import {FocusTargetController} from '../../../../../utils/accessibility-utils';
+import {getFieldValueCaption} from '../../../../../utils/field-utils';
+import {FacetValueLabelHighlight} from '../../../../common/facets/facet-value-label-highlight/facet-value-label-highlight';
+import {FacetValueLink} from '../../../../common/facets/facet-value-link/facet-value-link';
+
+interface FlatCategoryFacetProps {
+  facet: CategoryFacet;
+  facetValues: CategoryFacetValue[];
+  resultIndexToFocusOnShowMore: number;
+  field: string;
+  i18n: i18n;
+  facetSearchQuery: string;
+  focusTargets: {
+    activeValue: FocusTargetController;
+    showLessFocus: FocusTargetController;
+    showMoreFocus: FocusTargetController;
+  };
+}
+
+export const FlatCategoryFacet: FunctionalComponent<FlatCategoryFacetProps> = ({
+  focusTargets,
+  facet,
+  facetSearchQuery,
+  facetValues,
+  field,
+  i18n,
+  resultIndexToFocusOnShowMore,
+}: FlatCategoryFacetProps) => {
+  if (!facetValues.length) {
+    return <Fragment></Fragment>;
+  }
+
+  const renderChild = function (
+    facetValue: CategoryFacetValue,
+    isShowLessFocusTarget: boolean,
+    isShowMoreFocusTarget: boolean
+  ) {
+    const displayValue = getFieldValueCaption(field, facetValue.value, i18n);
+    const isSelected = facetValue.state === 'selected';
+    const leafOrNodePart = facetValue.isLeafValue ? 'leaf-value' : 'node-value';
+
+    return (
+      <FacetValueLink
+        displayValue={displayValue}
+        numberOfResults={facetValue.numberOfResults}
+        isSelected={isSelected}
+        i18n={i18n}
+        onClick={() => {
+          focusTargets.activeValue.focusAfterSearch();
+          facet.toggleSelect(facetValue);
+        }}
+        searchQuery={facetSearchQuery}
+        buttonRef={(element) => {
+          isShowLessFocusTarget &&
+            focusTargets.showLessFocus.setTarget(element);
+          isShowMoreFocusTarget &&
+            focusTargets.showMoreFocus.setTarget(element);
+        }}
+        additionalPart={leafOrNodePart}
+      >
+        <FacetValueLabelHighlight
+          displayValue={displayValue}
+          isSelected={isSelected}
+        ></FacetValueLabelHighlight>
+      </FacetValueLink>
+    );
+  };
+
+  return facetValues.map((value, i) =>
+    renderChild(value, i === 0, i === resultIndexToFocusOnShowMore)
+  );
+};
