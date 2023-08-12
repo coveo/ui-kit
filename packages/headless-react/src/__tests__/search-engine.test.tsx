7b52694d2448a3eb833567995e9a42ecb63cea4b
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-08-11 18:08:26 -0700

    add wip jsdom test

--- packages/headless-react/src/__tests__/search-engine.test.tsx
+++ packages/headless-react/src/__tests__/search-engine.test.tsx
@@ -1,7 +1,10 @@
 import {getSampleSearchEngineConfiguration} from '@coveo/headless';
-import {defineSearchEngine} from './search-engine';
+import '@testing-library/jest-dom';
+import {render} from '@testing-library/react';
+import {defineSearchEngine} from '../search-engine';
 
 describe('Headless react SSR utils', () => {
+  // TODO: add test using `defineResultList` controller once https://github.com/coveo/ui-kit/pull/3099 is merged.
   it('defines react search engine', () => {
     const config = getSampleSearchEngineConfiguration();
     const {
@@ -28,5 +31,15 @@ describe('Headless react SSR utils', () => {
 
     expect(controllers).toEqual({});
     expect(rest).toEqual({}); // No other return values
+  });
+
+  it('renders provider', async () => {
+    const config = getSampleSearchEngineConfiguration();
+    const {fetchInitialState, SSRStateProvider} = defineSearchEngine({
+      configuration: config,
+    });
+    const SSRControllers = await fetchInitialState({controllers: {}});
+
+    render(<SSRStateProvider controllers={SSRControllers.controllers} />);
   });
 });
