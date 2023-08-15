25c467a4e6c341895da709c09cfb8b4602d2df4c
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-08-14 19:17:38 -0700

    add config to enable code coverage

--- packages/headless-react/jest.config.js
+++ packages/headless-react/jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'jest-environment-jsdom',
   setupFiles: ['./jest.setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**.ts', './src/**.tsx'],
+  coverageThreshold: {
+    global: {
+      // TODO: enable after adding more tests
+      // lines: 90,
+    },
+  },
 };
