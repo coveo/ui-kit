dc8aab0582a44be17582ba9f4cc18a78a1e94020
Sami Sayegh <ssayegh@coveo.com>
2023-08-16 15:26:35 -0400

    feature(playground): add code editor to the playground, validation message box (coveo-platform/relay#10)
    Original-commit: coveo-platform/relay@efda6c2625dcafd0b58025b293d5c1adc40f481e

---
+++ apps/playground/app/events.ts
@@ -0,0 +1,18 @@
+export const ecPurchase = {
+  currency: "USD",
+  products: [
+    {
+      product: {
+        productId: "a",
+        name: "ski",
+        price: 10,
+        sku: "ski-large",
+      },
+      quantity: 1,
+    },
+  ],
+  transaction: {
+    id: "1234",
+    revenue: 12,
+  },
+};
--- apps/playground/app/page.tsx
+++ apps/playground/app/page.tsx
@@ -1,11 +1,48 @@
+"use client";
+
 import { createRelay } from "@coveo/relay";
+import Editor from "@monaco-editor/react";
+import styles from "./styles.module.css";
+import { useState } from "react";
+import { ecPurchase } from "./events";
 
 export default function Page() {
   createRelay().log();
 
+  const prettify = (obj: Object) => JSON.stringify(obj, null, 2);
+  const defaultEvent = prettify(ecPurchase);
+
+  const [event, setEvent] = useState(defaultEvent);
+  const [message, setMessage] = useState("");
+
+  const modified = event !== defaultEvent;
+  const reset = () => setEvent(defaultEvent);
+  const send = () => {
+    console.log(event);
+    setMessage("validation error");
+  };
+
   return (
-    <>
+    <div className={styles.container}>
       <h1>Relay</h1>
-    </>
+      <div className={styles.controls}>
+        <button onClick={send}>Send</button>
+        {modified ? <button onClick={reset}>Reset</button> : null}
+      </div>
+      <div className={styles.main}>
+        <div className={styles.section}>
+          <Editor
+            language="json"
+            theme="vs-dark"
+            value={event}
+            options={{ minimap: { enabled: false }, contextmenu: false }}
+            onChange={setEvent}
+          />
+        </div>
+        <div className={styles.section}>
+          {message && <div className={styles["validation-box"]}>{message}</div>}
+        </div>
+      </div>
+    </div>
   );
 }
