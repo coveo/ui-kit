a84e0a19e6feda17f02b7e7d39a1266e694dbfb3
aprudhomme-coveo <78121423+alexprudhomme@users.noreply.github.com>
2023-08-18 10:50:04 -0400

    clean up tests
    https://coveord.atlassian.net/browse/KIT-2652

--- packages/atomic/cypress/e2e/facets/manager/facet-manager.cypress.ts
+++ packages/atomic/cypress/e2e/facets/manager/facet-manager.cypress.ts
@@ -1,3 +1,4 @@
+import {TestFeature} from '../../../fixtures/fixture-common';
 import {TestFixture} from '../../../fixtures/test-fixture';
 import {
   assertConsoleError,
@@ -19,12 +20,42 @@ import {
 describe('Facet Manager Test Suite', () => {
   const DEFAULT_COLLAPSE_FACETS_AFTER = 4;
   const MINUS_ONE_COLLAPSE_FACETS_AFTER = -1;
+
+  function setup(
+    {
+      collapseFacetsAfter,
+      generatorCollapseFacetsAfter,
+    }: {
+      collapseFacetsAfter?: number;
+      generatorCollapseFacetsAfter?: number;
+    },
+    facetManagerFunction: (
+      options: Record<string, number>
+    ) => TestFeature<TestFixture>
+  ) {
+    const facetManagerOptions: Record<string, number> = {};
+
+    if (collapseFacetsAfter !== undefined) {
+      facetManagerOptions['collapse-facets-after'] = collapseFacetsAfter;
+    }
+
+    if (generatorCollapseFacetsAfter !== undefined) {
+      facetManagerOptions['generator-collapse-facets-after'] =
+        generatorCollapseFacetsAfter;
+    }
+
+    const facetManager = facetManagerFunction(facetManagerOptions);
+
+    new TestFixture().with(facetManager).init();
+  }
+
   it('should throw an error when collapseFacetsAfter property is invalid', () => {
     new TestFixture()
       .with(
         addFacetManagerWithStaticFacets({'collapse-facets-after': 'potato'})
       )
       .init();
+
     assertConsoleError();
     assertContainsComponentError(
       {
@@ -36,82 +67,62 @@ describe('Facet Manager Test Suite', () => {
 
   describe('with static facets only', () => {
     it(`should only keep the first ${DEFAULT_COLLAPSE_FACETS_AFTER} facets expanded by default`, () => {
-      new TestFixture().with(addFacetManagerWithStaticFacets()).init();
+      setup({}, addFacetManagerWithStaticFacets);
       assertHasNumberOfExpandedFacets(DEFAULT_COLLAPSE_FACETS_AFTER);
     });
 
     it('should respect the collapseFacetsAfter prop when set', () => {
-      new TestFixture()
-        .with(addFacetManagerWithStaticFacets({'collapse-facets-after': 1}))
-        .init();
+      setup({collapseFacetsAfter: 1}, addFacetManagerWithStaticFacets);
       assertHasNumberOfExpandedFacets(1);
     });
 
     it(`should not set the is-collapsed attribute when the collapseFacetsAfter is ${MINUS_ONE_COLLAPSE_FACETS_AFTER}`, () => {
-      new TestFixture()
-        .with(
-          addFacetManagerWithStaticFacets({
-            'collapse-facets-after': MINUS_ONE_COLLAPSE_FACETS_AFTER,
-          })
-        )
-        .init();
+      setup(
+        {collapseFacetsAfter: MINUS_ONE_COLLAPSE_FACETS_AFTER},
+        addFacetManagerWithStaticFacets
+      );
       assertFacetsNoCollapsedAttribute();
     });
   });
 
   describe('with automatic facets only', () => {
-    function setup({
-      collapseFacetsAfter,
-      generatorCollapseFacetsAfter,
-    }: {
-      collapseFacetsAfter?: number;
-      generatorCollapseFacetsAfter?: number;
-    }) {
-      const facetManagerOptions: Record<string, number> = {};
-
-      if (collapseFacetsAfter !== undefined) {
-        facetManagerOptions['collapse-facets-after'] = collapseFacetsAfter;
-      }
-
-      if (generatorCollapseFacetsAfter !== undefined) {
-        facetManagerOptions['generator-collapse-facets-after'] =
-          generatorCollapseFacetsAfter;
-      }
-
-      const facetManager =
-        addFacetManagerWithAutomaticFacets(facetManagerOptions);
-
-      new TestFixture().with(facetManager).init();
-    }
-
     it(`should only keep the first ${DEFAULT_COLLAPSE_FACETS_AFTER} facets expanded by default`, () => {
-      setup({});
+      setup({}, addFacetManagerWithAutomaticFacets);
       assertHasNumberOfExpandedAutomaticFacets(DEFAULT_COLLAPSE_FACETS_AFTER);
     });
 
     it('should respect the collapseFacetsAfter prop when set', () => {
-      setup({collapseFacetsAfter: 1});
+      setup({collapseFacetsAfter: 1}, addFacetManagerWithAutomaticFacets);
       cy.wait(500);
       assertHasNumberOfExpandedAutomaticFacets(1);
     });
 
     it(`should not set the is-collapsed attribute when the collapseFacetsAfter is ${MINUS_ONE_COLLAPSE_FACETS_AFTER}`, () => {
-      setup({collapseFacetsAfter: MINUS_ONE_COLLAPSE_FACETS_AFTER});
+      setup(
+        {collapseFacetsAfter: MINUS_ONE_COLLAPSE_FACETS_AFTER},
+        addFacetManagerWithAutomaticFacets
+      );
       cy.wait(500);
       assertAutomaticFacetsNoCollapsedAttribute();
     });
 
     it(`should override the generator's collapseFacetsAfter value when collapseFacetsAfter is ${MINUS_ONE_COLLAPSE_FACETS_AFTER}`, () => {
-      setup({
-        collapseFacetsAfter: MINUS_ONE_COLLAPSE_FACETS_AFTER,
-        generatorCollapseFacetsAfter: 2,
-      });
+      setup(
+        {
+          collapseFacetsAfter: MINUS_ONE_COLLAPSE_FACETS_AFTER,
+          generatorCollapseFacetsAfter: 2,
+        },
+        addFacetManagerWithAutomaticFacets
+      );
       cy.wait(500);
       assertAutomaticFacetsNoCollapsedAttribute();
     });
 
     it("should override the generator's collapseFacetsAfter prop when collapseFacetsAfter prop is set", () => {
-      setup({collapseFacetsAfter: 1, generatorCollapseFacetsAfter: 2});
+      setup(
+        {collapseFacetsAfter: 1, generatorCollapseFacetsAfter: 2},
+        addFacetManagerWithAutomaticFacets
+      );
       cy.wait(500);
       assertHasNumberOfExpandedAutomaticFacets(1);
     });
@@ -121,43 +132,8 @@ describe('Facet Manager Test Suite', () => {
     const collapsedStaticFacets = 3;
     const collapsedAutomaticFacets = 2;
 
-    function setup({
-      collapseFacetsAfter,
-      generatorCollapseFacetsAfter,
-    }: {
-      collapseFacetsAfter?: number;
-      generatorCollapseFacetsAfter?: number;
-    }) {
-      const facetManagerOptions: Record<string, number> = {};
-
-      if (collapseFacetsAfter !== undefined) {
-        facetManagerOptions['collapse-facets-after'] = collapseFacetsAfter;
-      }
-
-      if (generatorCollapseFacetsAfter !== undefined) {
-        facetManagerOptions['generator-collapse-facets-after'] =
-          generatorCollapseFacetsAfter;
-      }
-
-      const facetManager =
-        addFacetManagerWithBothTypesOfFacets(facetManagerOptions);
-
-      new TestFixture().with(facetManager).init();
-    }
-
-    // beforeEach(() => {
-    //   new TestFixture()
-    //     .with(
-    //       addFacetManagerWithBothTypesOfFacets({
-    //         'collapse-facets-after':
-    //           collapsedStaticFacets + collapsedAutomaticFacets,
-    //       })
-    //     )
-    //     .init();
-    // });
-
     it(`should only keep the first ${DEFAULT_COLLAPSE_FACETS_AFTER} facets expanded by default`, () => {
-      setup({});
+      setup({}, addFacetManagerWithBothTypesOfFacets);
       assertHasNumberOfExpandedAutomaticFacets(collapsedStaticFacets);
       assertHasNumberOfExpandedAutomaticFacets(
         DEFAULT_COLLAPSE_FACETS_AFTER - collapsedStaticFacets
@@ -165,21 +141,27 @@ describe('Facet Manager Test Suite', () => {
     });
 
     it('should respect the collapseFacetsAfter prop when set', () => {
-      setup({collapseFacetsAfter: 5});
+      setup({collapseFacetsAfter: 5}, addFacetManagerWithBothTypesOfFacets);
       cy.wait(500);
       assertHasNumberOfExpandedFacets(collapsedStaticFacets);
       assertHasNumberOfExpandedAutomaticFacets(collapsedAutomaticFacets);
     });
 
     it(`should not set the is-collapsed attribute when the collapseFacetsAfter is ${MINUS_ONE_COLLAPSE_FACETS_AFTER}`, () => {
-      setup({collapseFacetsAfter: MINUS_ONE_COLLAPSE_FACETS_AFTER});
+      setup(
+        {collapseFacetsAfter: MINUS_ONE_COLLAPSE_FACETS_AFTER},
+        addFacetManagerWithBothTypesOfFacets
+      );
       cy.wait(500);
       assertFacetsNoCollapsedAttribute();
       assertAutomaticFacetsNoCollapsedAttribute();
     });
 
     it("should override the generator's collapseFacetsAfter prop when collapseFacetsAfter prop is set", () => {
-      setup({collapseFacetsAfter: 5, generatorCollapseFacetsAfter: 4});
+      setup(
+        {collapseFacetsAfter: 5, generatorCollapseFacetsAfter: 4},
+        addFacetManagerWithBothTypesOfFacets
+      );
       cy.wait(500);
       assertHasNumberOfExpandedFacets(collapsedStaticFacets);
       assertHasNumberOfExpandedAutomaticFacets(collapsedAutomaticFacets);
