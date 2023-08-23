c04add71884cf735e78aa367f67fe7f886d6f3f3
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-08-23 14:53:37 -0700

    extract out common result list jsx
    https://coveord.atlassian.net/browse/KIT-2606

--- packages/samples/headless-ssr/src/app/react/components/result-list.tsx
+++ packages/samples/headless-ssr/src/app/react/components/result-list.tsx
@@ -1,15 +1,8 @@
+import ResultListCommon from '../../common/result-list';
 import {useResultList} from '../common/engine';
 
 export default function ResultList() {
   const {state} = useResultList();
 
-  return (
-    <ul>
-      {state.results.map((result) => (
-        <li key={result.uniqueId}>
-          <h3>{result.title}</h3>
-        </li>
-      ))}
-    </ul>
-  );
+  return <ResultListCommon results={state.results} />;
 }
