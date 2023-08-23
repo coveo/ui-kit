d06f8c0e76b23c780feb08832c1f041204ff5afd
fbeaudoincoveo <fbeaudoin@coveo.com>
2023-08-23 14:18:32 -0400

    Comment out commerce use case

--- packages/headless/doc-parser/doc-parser.ts
+++ packages/headless/doc-parser/doc-parser.ts
@@ -13,13 +13,14 @@ import {
   resolveEngine,
 } from './src/headless-export-resolvers/engine-resolver';
 import {caseAssistUseCase} from './use-cases/case-assist';
+// TODO: Uncomment when we're ready to make the Commerce sub-package public.
+//import {commerceUseCase} from './use-cases/commerce';
 import {insightUseCase} from './use-cases/insight';
 import {productListingUseCase} from './use-cases/product-listing';
 import {productRecommendationUseCase} from './use-cases/product-recommendation';
 import {recommendationUseCase} from './use-cases/recommendation';
 import {searchUseCase} from './use-cases/search';
 import {UseCaseConfiguration} from './use-cases/use-case-configuration';
-import {commerceUseCase} from './use-cases/commerce';
 
 interface UseCase {
   name: string;
@@ -65,11 +66,12 @@ const useCases: UseCase[] = [
     entryFile: 'temp/insight.api.json',
     config: insightUseCase,
   },
-  {
-    name: 'commerce',
-    entryFile: 'temp/commerce.api.json',
-    config: commerceUseCase,
-  },
+  // TODO: Uncomment when we're ready to make the Commerce sub-package public.
+  //{
+  //  name: 'commerce',
+  //  entryFile: 'temp/commerce.api.json',
+  //  config: commerceUseCase,
+  //},
 ];
 
 function resolveUseCase(useCase: UseCase): ResolvedUseCase {
