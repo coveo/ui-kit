8e05b01817648272cd94ed61c33d82d7b198f63b
fbeaudoincoveo <fbeaudoin@coveo.com>
2023-08-24 11:37:55 -0400

    Add basic exports

--- packages/headless/src/commerce.index.ts
+++ packages/headless/src/commerce.index.ts
@@ -2,4 +2,35 @@ import {polyfillCryptoNode} from './api/analytics/analytics-crypto-polyfill';
 
 polyfillCryptoNode();
 
-export {};
+export type {
+  CommerceEngine,
+  CommerceEngineConfiguration,
+  CommerceEngineOptions,
+} from './app/commerce-engine/commerce-engine';
+export {
+  buildCommerceEngine,
+  getSampleCommerceEngineConfiguration,
+} from './app/commerce-engine/commerce-engine';
+
+export type {CoreEngine, ExternalEngineOptions} from './app/engine';
+export type {
+  EngineConfiguration,
+  AnalyticsConfiguration,
+  AnalyticsRuntimeEnvironment,
+} from './app/engine-configuration';
+export type {LoggerOptions} from './app/logger';
+export type {LogLevel} from './app/logger';
+
+export type {ProductRecommendation} from './api/search/search/product-recommendation';
+
+// Actions
+export * from './features/product-listing/v2/product-listing-v2-actions-loader';
+export * from './features/configuration/configuration-actions-loader';
+export * from './features/analytics/search-analytics-actions-loader';
+
+// Controllers
+export type {
+  Controller,
+  Subscribable,
+} from './controllers/controller/headless-controller';
+export {buildController} from './controllers/controller/headless-controller';
