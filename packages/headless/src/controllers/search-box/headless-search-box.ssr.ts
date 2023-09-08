8f1490b5f4a6f81b40921b1de4ad993e3fa43b77
Benjamin Taillon <54454747+btaillon@users.noreply.github.com>
2023-09-08 14:32:43 -0500

    style(headless): moved ssr files next to their csr counterparts (#3150)
    https://coveord.atlassian.net/browse/KIT-2714

---
+++ packages/headless/src/controllers/search-box/headless-search-box.ssr.ts
@@ -0,0 +1,19 @@
+import {SearchEngine} from '../../app/search-engine/search-engine';
+import {ControllerDefinitionWithoutProps} from '../../app/ssr-engine/types/common';
+import {SearchBox, SearchBoxProps, buildSearchBox} from './headless-search-box';
+
+export type {
+  SearchBoxOptions,
+  SearchBoxProps,
+  SearchBoxState,
+  SearchBox,
+} from './headless-search-box';
+
+/**
+ * @internal
+ */
+export const defineSearchBox = (
+  props?: SearchBoxProps
+): ControllerDefinitionWithoutProps<SearchEngine, SearchBox> => ({
+  build: (engine) => buildSearchBox(engine, props),
+});
