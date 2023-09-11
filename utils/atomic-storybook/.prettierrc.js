a99d4a621d84f4d21849f00cb6f4c62d28c2858d
Louis Bompart <lbompart@coveo.com>
2023-09-11 00:28:59 -0400

    stabilize storybook. hafta sacrifice the code editor tho

---
+++ utils/atomic-storybook/.prettierrc.js
@@ -0,0 +1,18 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  endOfLine: 'auto',
+  overrides: [
+    {files: '**/*.{scss,css,pcss,html,mdx}', options: {printWidth: 120}},
+    {
+      files: '**/*.js',
+      options: {
+        importOrderParserPlugins: ['jsx'],
+      },
+    },
+  ],
+  importOrderParserPlugins: ['typescript'],
+  importOrder: ['^[./]', '^\\/', '^\\.\\.\\/', '^\\.\\/'],
+};
