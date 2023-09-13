d700a24de88858cea8f3c8458a0b99cb1cc5c096
Nicholas Labarre <Spuffynism@users.noreply.github.com>
2023-09-13 13:43:31 -0400

    feat(commerce): create PLP v2 product listing controller (#3147)
    * wip commerce sub-package
    
    * Add commerce/ to files
    
    * Import polyfillCryptoNode
    
    * Basic boilerplate
    
    * Comment out commerce use case
    
    * Rename analytics
    
    * Adjust test descriptions
    
    * Remove version from ProductListingV2BaseParam
    
    * Remove done TODO
    
    * Move propertyId to base params
    
    * Eliminate superfluous interfaces
    
    * Use logProductListingV2Load analytics action when building mock product listing response
    
    * Remove superfluous spread
    
    * Add missing comma
    
    * Use v2 product listing API client
    
    * Update adding sub-package documentation
    
    * Add mock commerce engine
    
    * Add basic exports
    
    * Remove comment
    
    * Undo bad changes
    
    * Linting
    
    * Rename property to tracking
    
    * Update API req and response params
    
    * Adjust state + buildProductListingRequestV2 params
    
    * Simplify buildProductListingRequestV2 function
    
    * Reorder request params and remove unreachable fallback values
    
    * Remove superfluous actions
    
    * Remove version from state
    
    * Remove TODO's
    
    * Documentation updates
    
    * Renaming + docs + small fixes
    
    * Remove superfluous response properties
    
    * Remove search API client from listing v2 API client
    
    * Update imports
    
    * Remove useless jsdocs
    
    * Use Map instead of Record for labels
    
    * Un-document get sample config function and uniformize CommerceEngineConfiguration docs
    
    * Add missing jsdoc
    
    * Remove superfluous jsdoc
    
    * Rename fetch action id
    
    * Un-document commerce mock engine
    
    * Link TODOs to CAPI Jira issue
    
    * Fix error message
    
    * Fix test description
    
    * Use plp v2 actions
    
    * Clean up plp v2 app state + state needed by fetch
    
    * Remove query string arg + fix enum value + refactoring
    
    * Use sample commerce organization
    
    * Rename Mode to ModeParam and declare as type instead of enum
    
    * Define interfaces for context + use Record instead of Map for labels
    
    * Refactor product listing v2 API client into commerce API client
    
    * Revert "Use sample commerce organization"
    
    This reverts commit a4a4bcdf53f6e8af431acd31be74bffca21c70f4.
    
    * Use commerce sample org only in commerce engine sample config
    
    * Update SelectedPageParam to latest spec
    
    * Clean up prepareRequestParams
    
    * Remove mode param
    
    * Remove mode removal leftovers
    
    * Expose mocks for tests
    
    * feat(commerce): Create PLP controller
    
    [CAPI-83]
    
    * feat(commerce): apply pr review
    
    [CAPI-83]
    
    * feat(commerce): Actually fix test name
    
    [CAPI-83]
    
    * feat(commerce): require url
    
    [CAPI-83]
    
    * feat(commerce): do not require url format
    
    [CAPI-83]
    
    ---------
    
    Co-authored-by: fbeaudoincoveo <fbeaudoin@coveo.com>

---
+++ packages/headless/src/controllers/commerce/product-listing/headless-product-listing.test.ts
@@ -0,0 +1,65 @@
+import {Action} from '@reduxjs/toolkit';
+import {configuration} from '../../../app/common-reducers';
+import {
+  fetchProductListing,
+  setProductListingUrl,
+} from '../../../features/product-listing/v2/product-listing-v2-actions';
+import {productListingV2Reducer} from '../../../features/product-listing/v2/product-listing-v2-slice';
+import {buildMockCommerceEngine, MockCommerceEngine} from '../../../test';
+import {
+  buildProductListing,
+  ProductListing,
+  ProductListingOptions,
+} from './headless-product-listing';
+
+describe('headless product-listing', () => {
+  let productListing: ProductListing;
+  let engine: MockCommerceEngine;
+
+  const baseOptions: ProductListingOptions = {
+    url: 'https://someurl.com',
+  };
+
+  beforeEach(() => {
+    engine = buildMockCommerceEngine();
+    productListing = buildProductListing(engine, {
+      options: baseOptions,
+    });
+  });
+
+  const expectContainAction = (action: Action) => {
+    const found = engine.actions.find((a) => a.type === action.type);
+    expect(engine.actions).toContainEqual(found);
+  };
+
+  it('adds the correct reducers to engine', () => {
+    expect(engine.addReducers).toHaveBeenCalledWith({
+      productListing: productListingV2Reducer,
+      configuration,
+    });
+  });
+
+  it('should validate if #url is missing', () => {
+    expect(() => {
+      buildProductListing(engine, {
+        options: {
+          url: '',
+        },
+      });
+    }).toThrow();
+  });
+
+  it('dispatches #setProductListingUrl on load', () => {
+    expectContainAction(setProductListingUrl);
+  });
+
+  it('setUrl dispatches #setProductListingUrl', () => {
+    productListing.setUrl('http://example.org');
+    expectContainAction(setProductListingUrl);
+  });
+
+  it('refresh dispatches #fetchProductListing', () => {
+    productListing.refresh();
+    expectContainAction(fetchProductListing.pending);
+  });
+});
