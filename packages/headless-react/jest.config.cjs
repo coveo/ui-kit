2202b295e88bdae3f7cac980715d31e78727490e
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-09-18 15:47:31 -0700

    add esm config for jest
    https://coveord.atlassian.net/browse/KIT-2661

--- packages/headless-react/jest.config.cjs
+++ packages/headless-react/jest.config.cjs
@@ -1,11 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  extensionsToTreatAsEsm: ['.tsx', '.ts'],
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
-        tsconfig: 'tsconfig.test.json'
+        useESM: true,
+        tsconfig: 'tsconfig.test.json',
       },
     ],
   },
