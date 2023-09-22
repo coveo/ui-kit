19d9a89f007409f5ca0524a88fb6367eb76c0dde
Olivier Lamothe <olamothe@coveo.com>
2023-09-22 10:59:04 -0400

    move hook
    https://coveord.atlassian.net/browse/KIT-2771

--- packages/quantic/cypress.config.ts
+++ packages/quantic/cypress.config.ts
@@ -12,27 +12,24 @@ export default defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome' && browser.isHeadless) {
+          launchOptions.args = launchOptions.args.map((arg) => {
+            console.log(arg);
+            if (arg === '--headless') {
+              return '--headless=new';
+            }
+
+            return arg;
+          });
+        }
+
+        return launchOptions;
+      });
       return require('./cypress/plugins/index.ts')(on, config);
     },
     specPattern: 'cypress/e2e/**/*.cypress.ts',
     baseUrl: 'https://speed-efficiency-308-dev-ed.my.site.com/examples',
-  },
-  setupNodeEvents(on) {
-    on('before:browser:launch', (browser, launchOptions) => {
-      if (browser.name === 'chrome' && browser.isHeadless) {
-        launchOptions.args = launchOptions.args.map((arg) => {
-          if (arg === '--headless') {
-            return '--headless=new';
-          }
-
-          return arg;
-        });
-      }
-
-      return launchOptions;
-    });
   },
 });
