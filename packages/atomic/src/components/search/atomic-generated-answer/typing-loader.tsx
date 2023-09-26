cf80eed8b28424a0f9d36292adea4bc5389d5d7b
David Guerin <dguerin@coveo.com>
2023-09-26 15:23:07 -0400

    fix(genAI): clearer part names

--- packages/atomic/src/components/search/atomic-generated-answer/typing-loader.tsx
+++ packages/atomic/src/components/search/atomic-generated-answer/typing-loader.tsx
@@ -1,9 +1,9 @@
 import {FunctionalComponent, h} from '@stencil/core';
 
 export const TypingLoader: FunctionalComponent = () => (
-  <div part="container-background" class="typing-indicator" aria-hidden="true">
-    <span part="container-dot"></span>
-    <span part="container-dot"></span>
-    <span part="container-dot"></span>
+  <div part="typing-animation" class="typing-indicator" aria-hidden="true">
+    <span part="typing-animation-dot"></span>
+    <span part="typing-animation-dot"></span>
+    <span part="typing-animation-dot"></span>
   </div>
 );
