0056a4fbee003b9c5385d93e4b99815e59d2db5b
aprudhomme-coveo <78121423+alexprudhomme@users.noreply.github.com>
2023-09-29 09:32:54 -0400

    Revert "Revert "Add generated files""
    This reverts commit 9b7ed8756e50070887d34a8b23aaaa9cee5d2c92.

--- packages/atomic-angular/projects/atomic-angular/src/lib/stencil-generated/components.ts
+++ packages/atomic-angular/projects/atomic-angular/src/lib/stencil-generated/components.ts
@@ -1767,28 +1767,6 @@ export declare interface AtomicSmartSnippet extends Components.AtomicSmartSnippe
 
 
 @ProxyCmp({
-  inputs: ['collapsedHeight', 'maximumHeight']
-})
-@Component({
-  selector: 'atomic-smart-snippet-collapse-wrapper',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  template: '<ng-content></ng-content>',
-  // eslint-disable-next-line @angular-eslint/no-inputs-metadata-property
-  inputs: ['collapsedHeight', 'maximumHeight'],
-})
-export class AtomicSmartSnippetCollapseWrapper {
-  protected el: HTMLElement;
-  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
-    c.detach();
-    this.el = r.nativeElement;
-  }
-}
-
-
-export declare interface AtomicSmartSnippetCollapseWrapper extends Components.AtomicSmartSnippetCollapseWrapper {}
-
-
-@ProxyCmp({
   inputs: ['isOpen', 'source']
 })
 @Component({
