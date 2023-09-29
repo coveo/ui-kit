f0e18aa2a0d1d58ac78604971cf4d08294b092ae
aprudhomme-coveo <78121423+alexprudhomme@users.noreply.github.com>
2023-09-29 17:46:55 -0400

    lint fix and lint-check better scripts
    https://coveord.atlassian.net/browse/KIT-2788

--- scripts/lint/lint-fix.mjs
+++ scripts/lint/lint-fix.mjs
@@ -2,16 +2,17 @@ import {execute} from '../exec.mjs';
 
 async function main() {
   try {
-    await execute('npx', ['eslint', '.']);
+    await execute('npx', ['eslint', '.', '--fix']);
 
     await execute('npx', [
       'cspell',
       '--no-progress',
       '"**/*.md"',
       '--no-must-find-files',
+      '--write',
     ]);
 
-    await execute('npx', ['prettier', '--check', '.']);
+    await execute('npx', ['prettier', '--write', '.']);
   } catch (err) {
     console.error('Error:', err);
     process.exit(1);
