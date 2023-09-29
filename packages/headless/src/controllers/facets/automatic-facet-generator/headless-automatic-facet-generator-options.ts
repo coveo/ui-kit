4decff2d91f415d392177b94dfd10e4c3f8566e8
Olivier Lamothe <olamothe@coveo.com>
2023-09-29 16:00:59 -0400

    feat(atomic,headless): ga release for automatic-facets feature (#3224)
    https://coveord.atlassian.net/browse/KIT-2784

--- packages/headless/src/controllers/facets/automatic-facet-generator/headless-automatic-facet-generator-options.ts
+++ packages/headless/src/controllers/facets/automatic-facet-generator/headless-automatic-facet-generator-options.ts
@@ -1,8 +1,5 @@
 export interface AutomaticFacetGeneratorOptions {
   /**
-   * @beta - This property is part of the automatic facets feature.
-   * Automatic facets are currently in beta testing and should be available soon.
-   *
    * The desired count of automatic facets.
    *
    * Minimum: `1`
@@ -12,9 +9,6 @@ export interface AutomaticFacetGeneratorOptions {
   desiredCount?: number;
 
   /**
-   * @beta - This prop is part of the automatic facets feature.
-   * Automatic facets are currently in beta testing and should be available soon.
-   *
    * The desired number of automatically generated facet values.
    *
    * Minimum: `1`
