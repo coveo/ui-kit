8668e6cf9b4586be2de60bcd177113100cdd2aef
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-10-03 14:07:41 -0700

    wip generic page working
    https://coveord.atlassian.net/browse/KIT-2708

--- packages/samples/headless-ssr/pages-router/src/pages/generic/index.tsx
+++ packages/samples/headless-ssr/pages-router/src/pages/generic/index.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-  return <div>Generic</div>;
+import {CoveoNextJsSearchParametersSerializer} from '@/src/common/search-parameters-serializer';
+import {
+  SearchStaticState,
+  fetchStaticState,
+} from '@/src/pages/generic/common/engine';
+import SearchPage from '@/src/pages/generic/components/search-page';
+
+export async function getServerSideProps() {
+  const {coveoSearchParameters} =
+    CoveoNextJsSearchParametersSerializer.fromServerSideUrlSearchParams({
+      '': '',
+    });
+  const staticState = await fetchStaticState({
+    controllers: {
+      searchParameters: {initialState: {parameters: coveoSearchParameters}},
+    },
+  });
+  return {props: {staticState}};
+}
+
+interface StaticStateProps {
+  staticState: SearchStaticState;
+}
+
+// Entry point SSR function
+export default function Search({staticState}: StaticStateProps) {
+  return <SearchPage staticState={staticState}></SearchPage>;
 }
