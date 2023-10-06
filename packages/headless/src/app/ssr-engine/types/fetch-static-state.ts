74ad0cc57d8042bad96fc0daf8a71bfc7307e902
Benjamin Taillon <54454747+btaillon-coveo@users.noreply.github.com>
2023-10-06 14:18:50 -0500

    refactor(headless): combined SSR *WithProps and *WithoutProps interfaces (#3241)
    https://coveord.atlassian.net/browse/KIT-2810

--- packages/headless/src/app/ssr-engine/types/fetch-static-state.ts
+++ packages/headless/src/app/ssr-engine/types/fetch-static-state.ts
@@ -2,28 +2,14 @@ import {AnyAction} from '@reduxjs/toolkit';
 import {
   ControllerStaticStateMap,
   ControllersPropsMap,
+  EngineDefinitionControllersPropsOption,
   EngineStaticState,
+  OptionsTuple,
 } from './common';
 
-export type EngineDefinitionFetchStaticStateOptions<
-  TControllersStaticState extends ControllersPropsMap,
-> = {controllers: TControllersStaticState};
+export type FetchStaticStateOptions = {};
 
-export type FetchStaticStateWithoutProps<
-  TControllersStaticState extends ControllerStaticStateMap,
-  TSearchAction extends AnyAction,
-> = {
-  /**
-   * Executes only the initial search for a given configuration, then returns a resumable snapshot of engine state along with the state of the controllers.
-   *
-   * Useful for static generation and server-side rendering.
-   */
-  fetchStaticState(): Promise<
-    EngineStaticState<TSearchAction, TControllersStaticState>
-  >;
-};
-
-export type FetchStaticStateWithProps<
+export type FetchStaticState<
   TControllersStaticState extends ControllerStaticStateMap,
   TSearchAction extends AnyAction,
   TControllersProps extends ControllersPropsMap,
@@ -33,7 +19,10 @@ export type FetchStaticStateWithProps<
    *
    * Useful for static generation and server-side rendering.
    */
-  fetchStaticState(
-    options: EngineDefinitionFetchStaticStateOptions<TControllersProps>
+  (
+    ...params: OptionsTuple<
+      FetchStaticStateOptions &
+        EngineDefinitionControllersPropsOption<TControllersProps>
+    >
   ): Promise<EngineStaticState<TSearchAction, TControllersStaticState>>;
 };
