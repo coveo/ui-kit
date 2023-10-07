b6cbcf25f2562397c9d8fe6c5b70d2237c3a2c06
Mohan Raj Rajamanickam <mrrajamanickam@coveo.com>
2023-10-06 19:16:30 -0700

    make common code a npm dep
    https://coveord.atlassian.net/browse/KIT-2708

--- packages/samples/headless-ssr/app-router/next.config.js
+++ packages/samples/headless-ssr/app-router/next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    // Enable access of common components between app and pages router outside `src/`.
-    //  The other alternative is to move common components into its own package but it would reduce readability.
-    //  Since readability is more important for the samples, this setting is used instead.
-    externalDir: true,
-  },
+  // Enable compiling of common components between app and pages router outside `src/`.
+  transpilePackages: ['@coveo/headless-ssr-samples-common'],
   reactStrictMode: true,
 };
 
