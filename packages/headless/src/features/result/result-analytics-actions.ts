85e147543ca9706be809be9110dec9b649fd36fc
Louis Bompart <lbompart@coveo.com>
2023-10-13 15:50:03 -0400

    feat(relay): add support for analytics/result/open

--- packages/headless/src/features/result/result-analytics-actions.ts
+++ packages/headless/src/features/result/result-analytics-actions.ts
@@ -9,14 +9,30 @@ import {
 } from '../analytics/analytics-utils';
 
 export const logDocumentOpen = (result: Result): ClickAction =>
-  makeAnalyticsAction(
-    'analytics/result/open',
-    AnalyticsType.Click,
-    (client, state) => {
+  makeAnalyticsAction({
+    prefix: 'analytics/result/open',
+    __legacy__analyticsType: AnalyticsType.Click,
+    __legacy__getBuilder: (client, state) => {
       validateResultPayload(result);
       return client.makeDocumentOpen(
         partialDocumentInformation(result, state),
         documentIdentifier(result)
       );
-    }
-  );
+    },
+    analyticsType: 'itemClick',
+    analyticsPayloadBuilder: (state) => {
+      const docInfo = partialDocumentInformation(result, state);
+      const docId = documentIdentifier(result);
+      return {
+        searchUid: state.search?.response.searchUid,
+        position: docInfo.documentPosition,
+        itemMetadata: {
+          uniqueFieldIdentifier: docId.contentIDKey,
+          uniqueFieldValue: docId.contentIDValue,
+          title: docInfo.documentTitle,
+          author: docInfo.documentAuthor,
+          url: docInfo.documentUrl,
+        },
+      };
+    },
+  });
