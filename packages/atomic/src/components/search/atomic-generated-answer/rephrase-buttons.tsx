e705f1b8302743e8912d25e364365739d8de694c
Nathan Lafrance-Berger <nathan.lafranceb@gmail.com>
2023-10-13 16:01:51 -0400

    feat(atomic): styling and such looking good

--- packages/atomic/src/components/search/atomic-generated-answer/rephrase-buttons.tsx
+++ packages/atomic/src/components/search/atomic-generated-answer/rephrase-buttons.tsx
@@ -1,64 +1,80 @@
 import {GeneratedAnswerStyle} from '@coveo/headless/dist/definitions/features/generated-answer/generated-response-format';
 import {FunctionalComponent, h} from '@stencil/core';
-import StepsIcon from '../.../../images/steps.svg';
+import {i18n} from 'i18next';
 import BubbleIcon from '../../../images/bubble.svg';
 import BulletsIcon from '../../../images/bullets.svg';
+import StepsIcon from '../../../images/steps.svg';
 import {Button} from '../../common/button';
 
 interface RephraseOption {
-  title: string;
   value: GeneratedAnswerStyle;
   icon: string;
-  tooltip: string;
+  titleKey: string;
+  tooltipKey: string;
 }
 
 const options: RephraseOption[] = [
   {
-    title: 'Steps',
     value: 'step',
     icon: StepsIcon,
-    tooltip: 'Step-by-step instructions',
+    titleKey: 'steps',
+    tooltipKey: 'steps-tooltip',
   },
   {
-    title: 'Bullets',
     value: 'bullet',
     icon: BulletsIcon,
-    tooltip: 'Bullet-point summary',
+    titleKey: 'bullets',
+    tooltipKey: 'bullets-tooltip',
   },
   {
-    title: 'Summary',
+    titleKey: 'summary',
     value: 'concise',
     icon: BubbleIcon,
-    tooltip: 'Summary',
+    tooltipKey: 'summary',
   },
 ];
 
 interface RephraseButtonProps {
   answerStyle: string;
+  i18n: i18n;
   onChange: (answerStyle: GeneratedAnswerStyle) => void;
 }
 
 export const RephraseButtons: FunctionalComponent<RephraseButtonProps> = (
   props
 ) => {
+  const {i18n} = props;
   return (
-    <div class="feedback-buttons flex gap-2 ml-auto">
-      {options.map((option) => {
-        const isActive = props.answerStyle === option.value;
-        return (
-          <Button
-            title={option.title}
-            style="text-neutral"
-            class={`feedback-button p-2 rounded-md ${isActive ? 'active' : ''}`}
-            onClick={() => {
-              props.onChange(isActive ? 'default' : option.value);
-            }}
-            ariaPressed={String(isActive)}
-          >
-            <atomic-icon class="w-5" icon={option.icon}></atomic-icon>
-          </Button>
-        );
-      })}
+    <div class="rephrase-buttons shrink-0">
+      <p part="rephrase-label" class="mb-2 text-neutral-dark shrink-0">
+        {i18n.t('rephrase')}
+      </p>
+      <div class="flex flex-wrap gap-2 ml-auto">
+        {options.map((option) => {
+          const isActive = props.answerStyle === option.value;
+          return (
+            <Button
+              title={i18n.t(option.tooltipKey)}
+              part="rephrase-button"
+              style="outline-primary"
+              class={`feedback-button flex items-center rounded-md ${
+                isActive ? 'active' : ''
+              }`}
+              onClick={() => {
+                props.onChange(isActive ? 'default' : option.value);
+              }}
+              ariaPressed={String(isActive)}
+            >
+              <div class="icon-container text-neutral-dark h-full mx-auto shrink-0 relative">
+                <atomic-icon icon={option.icon}></atomic-icon>
+              </div>
+              <div class="rephrase-btn-label hidden text-neutral-dark">
+                {i18n.t(option.titleKey)}
+              </div>
+            </Button>
+          );
+        })}
+      </div>
     </div>
   );
 };
