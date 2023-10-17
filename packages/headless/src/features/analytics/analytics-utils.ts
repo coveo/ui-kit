cb752ec3c82606289eb3c62bcf5157185db76dd9
Louis Bompart <lbompart@coveo.com>
2023-10-17 12:46:39 -0400

    exclude search from allowed type for converted makeAnalyticsAction

--- packages/headless/src/features/analytics/analytics-utils.ts
+++ packages/headless/src/features/analytics/analytics-utils.ts
@@ -333,7 +333,7 @@ export function makeAnalyticsAction<
   analyticsPayloadBuilder,
   analyticsType,
 }: AnalyticsActionOptions<
-  LegacyEventType,
+  Exclude<LegacyEventType, AnalyticsType.Search>,
   LegacyStateNeeded,
   StateNeeded,
   PayloadType
