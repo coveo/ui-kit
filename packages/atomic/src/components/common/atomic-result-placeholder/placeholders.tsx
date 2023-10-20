ecedc190ef13f222ed55ad2472efb1425ee319b2
Olivier Lamothe <olamothe@coveo.com>
2023-10-20 11:24:24 -0400

    remove some css classes duplicates

--- packages/atomic/src/components/common/atomic-result-placeholder/placeholders.tsx
+++ packages/atomic/src/components/common/atomic-result-placeholder/placeholders.tsx
@@ -48,42 +48,49 @@ export const GeneratedAnswerPlaceholder: FunctionalComponent = () => {
 };
 
 const GeneratedAnswerHeaderPlaceholder: FunctionalComponent = () => {
+  const classesAnim = 'block bg-neutral rounded h-4 animate-pulse';
+  const classesAnimHeaderActions = `${classesAnim} w-12`;
   return (
     <div class="flex items-center mb-8">
-      <div class="block bg-neutral rounded h-4 w-48 animate-pulse"></div>
+      <div class={`${classesAnim} w-48`}></div>
       <div class="flex gap-2 h-9 items-center ml-auto">
-        <div class="block bg-neutral rounded h-4 w-12 animate-pulse"></div>
-        <div class="block bg-neutral rounded h-4 w-12 animate-pulse"></div>
+        <div class={classesAnimHeaderActions}></div>
+        <div class={classesAnimHeaderActions}></div>
+        <div class={classesAnimHeaderActions}></div>
       </div>
     </div>
   );
 };
 
 const GeneratedAnswerParagraphPlaceholder: FunctionalComponent = () => {
+  const classesAnim = 'block bg-neutral rounded h-4 animate-pulse mb-4';
   return (
     <div>
-      <div class="block bg-neutral rounded h-4 w-full animate-pulse mb-4"></div>
-      <div class="block bg-neutral rounded h-4 w-3/4 animate-pulse mb-4"></div>
-      <div class="block bg-neutral rounded h-4 w-3/4 animate-pulse mb-4"></div>
-      <div class="block bg-neutral rounded h-4 w-full animate-pulse mb-4"></div>
-      <div class="block bg-neutral rounded h-4 w-1/3 animate-pulse mb-4"></div>
-      <div class="block bg-neutral rounded h-4 w-full animate-pulse mb-4"></div>
-      <div class="block bg-neutral rounded h-4 w-1/3 animate-pulse mb-4"></div>
-      <div class="block bg-neutral rounded h-4 w-full animate-pulse mb-4"></div>
-      <div class="block bg-neutral rounded h-4 w-3/4 animate-pulse mb-4"></div>
+      <div class={`${classesAnim} w-full`}></div>
+      <div class={`${classesAnim} w-3/4`}></div>
+      <div class={`${classesAnim} w-3/4`}></div>
+      <div class={`${classesAnim} w-full`}></div>
+      <div class={`${classesAnim} w-1/3`}></div>
+      <div class={`${classesAnim} w-full`}></div>
+      <div class={`${classesAnim} w-1/3`}></div>
+      <div class={`${classesAnim} w-full`}></div>
+      <div class={`${classesAnim} w-3/4`}></div>
     </div>
   );
 };
 
 const GeneratedAnswerCitationsPlaceholder: FunctionalComponent = () => {
+  const classesAnim = 'block bg-neutral rounded h-4 animate-pulse';
+  const classesAnimCitations = `${classesAnim} w-24`;
+  const classesAnimCitationsActions = `${classesAnim} w-12`;
   return (
     <div class="gap-2 mt-6 flex">
-      <div class="block bg-neutral rounded h-4 w-24 animate-pulse"></div>
-      <div class="block bg-neutral rounded h-4 w-24 animate-pulse"></div>
-      <div class="block bg-neutral rounded h-4 w-24 animate-pulse"></div>
+      <div class={classesAnimCitations}></div>
+      <div class={classesAnimCitations}></div>
+      <div class={classesAnimCitations}></div>
       <div class="flex gap-2 h-9 items-center ml-auto">
-        <div class="block bg-neutral rounded h-4 w-12 animate-pulse"></div>
-        <div class="block bg-neutral rounded h-4 w-12 animate-pulse"></div>
+        <div class={classesAnimCitationsActions}></div>
+        <div class={classesAnimCitationsActions}></div>
       </div>
     </div>
   );
