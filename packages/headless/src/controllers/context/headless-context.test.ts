b7e0fb3529fe7951377671d273579bca915a53d5
ylakhdar <ylakhdar@coveo.com>
2023-10-26 11:46:58 -0400

    add UT to headless-core-context
    https://coveord.atlassian.net/browse/KIT-2764

--- packages/headless/src/controllers/context/headless-context.test.ts
+++ packages/headless/src/controllers/context/headless-context.test.ts
@@ -1,19 +1,34 @@
+import {setContext} from '../../features/context/context-actions';
 import {
   buildMockSearchAppEngine,
   MockSearchEngine,
 } from '../../test/mock-engine';
-import {buildContext, Context} from './headless-context';
+import {buildContext, Context, ContextInitialState} from './headless-context';
 
 describe('Context', () => {
   let context: Context;
   let engine: MockSearchEngine;
+  let initialState: ContextInitialState | undefined;
+
+  function initContext() {
+    engine = buildMockSearchAppEngine();
+    context = buildContext(engine, {initialState});
+  }
 
   beforeEach(() => {
-    engine = buildMockSearchAppEngine();
-    context = buildContext(engine);
+    initialState = undefined;
+    initContext();
   });
 
   it('initializes properly', () => {
     expect(context.state.values).toEqual({});
+  });
+
+  it('when #initialState.values is defined, it sets the context', () => {
+    initialState = {
+      values: {foo: 'bar'},
+    };
+    initContext();
+    expect(engine.actions).toContainEqual(setContext({foo: 'bar'}));
   });
 });
