bab2da7067b2994280f9da29a31e832673c9cbe7
Frederic Beaudoin <fbeaudoin@coveo.com>
2023-11-03 15:32:51 -0400

    Add tests
    https://coveord.atlassian.net/browse/KIT-2865

---
+++ packages/headless/src/controllers/folded-result-list/headless-folded-result-list.test.ts
@@ -0,0 +1,46 @@
+import {SearchAppState} from '../..';
+import {loadCollection} from '../../features/folding/folding-actions';
+import {fetchMoreResults} from '../../features/search/search-actions';
+import {
+  MockSearchEngine,
+  buildMockResult,
+  buildMockSearchAppEngine,
+  createMockState,
+} from '../../test';
+import {
+  FoldedResultList,
+  buildFoldedResultList,
+} from './headless-folded-result-list';
+
+describe('FoldedResultList', () => {
+  let state: SearchAppState;
+  let engine: MockSearchEngine;
+  let foldedResultList: FoldedResultList;
+
+  function initFoldedResultList() {
+    engine = buildMockSearchAppEngine({state});
+    foldedResultList = buildFoldedResultList(engine);
+  }
+
+  beforeEach(() => {
+    state = createMockState();
+    state.search.response.totalCountFiltered = 100;
+    initFoldedResultList();
+  });
+
+  it('#loadCollection dispatches the search #loadCollection action', () => {
+    foldedResultList.loadCollection({
+      children: [],
+      isLoadingMoreResults: false,
+      moreResultsAvailable: false,
+      result: buildMockResult(),
+    });
+
+    expect(engine.findAsyncAction(loadCollection.pending)).toBeTruthy();
+  });
+  it('#fetchMoreResults dispatches the search #fetchMoreResults action', () => {
+    foldedResultList.fetchMoreResults();
+
+    expect(engine.findAsyncAction(fetchMoreResults.pending)).toBeTruthy();
+  });
+});
