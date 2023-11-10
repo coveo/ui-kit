af07ba570cdacb89a8275ae63eeeb773a1395a48
Louis Bompart <lbompart@coveo.com>
2023-11-09 21:43:04 -0500

    how to see event

--- packages/headless/src/features/result/result-analytics-actions.ts
+++ packages/headless/src/features/result/result-analytics-actions.ts
@@ -8,10 +8,24 @@ import {
 } from '../analytics/analytics-utils';
 
 export const logDocumentOpen = (result: Result): ClickAction =>
-  makeAnalyticsAction('analytics/result/open', (client, state) => {
-    validateResultPayload(result);
-    return client.makeDocumentOpen(
-      partialDocumentInformation(result, state),
-      documentIdentifier(result)
-    );
+  makeAnalyticsAction({
+    prefix: 'analytics/result/open',
+    __legacy__getBuilder: (client, state) => {
+      validateResultPayload(result);
+      return client.makeDocumentOpen(
+        partialDocumentInformation(result, state),
+        documentIdentifier(result)
+      );
+    },
+    analyticsType: 'ecProductView',
+    analyticsPayloadBuilder: (_state) => {
+      return {
+        currency: 'USD',
+        product: {
+          name: 'Fail-safe Brown Flip Flop',
+          price: 24,
+          productId: 'SP04049_00005',
+        },
+      };
+    },
   });
