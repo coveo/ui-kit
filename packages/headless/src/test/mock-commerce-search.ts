95c0e869f8b72ba3748345db69d4ebd2b39ae89b
Nicholas-David Labarre <ndlabarre@coveo.com>
2023-11-16 08:59:05 -0500

    simplify mock builder definition

--- packages/headless/src/test/mock-commerce-search.ts
+++ packages/headless/src/test/mock-commerce-search.ts
@@ -1,9 +1,3 @@
-import {CommerceSuccessResponse} from '../api/commerce/common/response';
-import {QueryCommerceAPIThunkReturn} from '../features/commerce/common/actions';
 import {buildFetchProductListingV2Response} from './mock-product-listing-v2';
 
-export function buildSearchResponse(
-  response: Partial<CommerceSuccessResponse> = {}
-): QueryCommerceAPIThunkReturn {
-  return buildFetchProductListingV2Response(response);
-}
+export {buildFetchProductListingV2Response as buildSearchResponse};
