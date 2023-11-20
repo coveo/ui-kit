ae4dec1e28555a741ce9d853b045518f806cff54
ylakhdar <ylakhdar@coveo.com>
2023-11-20 11:27:27 -0500

    add more docs
    https://coveord.atlassian.net/browse/KIT-2735

--- packages/samples/headless-ssr/app-router/src/app/generic/page.tsx
+++ packages/samples/headless-ssr/app-router/src/app/generic/page.tsx
@@ -2,7 +2,17 @@ import SearchPage from '@/common/components/generic/search-page';
 import {fetchStaticState} from '@/common/lib/generic/engine';
 import {buildSearchParameterSerializer} from '@coveo/headless';
 
-// Entry point SSR function
+/**
+ * This file defines a Search component that uses the Coveo Headless library to manage its state.
+ *
+ * The Search function is the entry point for server-side rendering (SSR). It uses the `buildSearchParameterSerializer` util from the Coveo Headless
+ * library to serialize the url search parameters into a string, which is then used by the [UrlManager](https://docs.coveo.com/en/headless/latest/reference/search/controllers/url-manager) controller.
+ *
+ * To synchronize search parameters with the URL with more control on the serialization, you can use the [SearchParameterManager](https://docs.coveo.com/en/headless/latest/reference/search/controllers/search-parameter-manager/) controller. For sake of brevity, this sample uses the UrlManager controller.
+ *
+ * The context values are hard-coded to represent a specific user segment (age group 30-45 with a main interest in sports) as the initial context.
+ * These values will be added to the payload of the search request when the search page is rendered.
+ */
 export default async function Search(url: {
   searchParams: {[key: string]: string | string[] | undefined};
 }) {
--- packages/samples/headless-ssr/app-router/src/app/react/page.tsx
+++ packages/samples/headless-ssr/app-router/src/app/react/page.tsx
@@ -6,7 +6,17 @@ import UrlManager from '@/common/components/react/url-manager';
 import {fetchStaticState} from '@/common/lib/react/engine';
 import {buildSearchParameterSerializer} from '@coveo/headless';
 
-// Entry point SSR function
+/**
+ * This file defines a Search component that uses the Coveo Headless library to manage its state.
+ *
+ * The Search function is the entry point for server-side rendering (SSR). It uses the `buildSearchParameterSerializer` util from the Coveo Headless
+ * library to serialize the url search parameters into a string, which is then used by the [UrlManager](https://docs.coveo.com/en/headless/latest/reference/search/controllers/url-manager) controller.
+ *
+ * To synchronize search parameters with the URL with more control on the serialization, you can use the [SearchParameterManager](https://docs.coveo.com/en/headless/latest/reference/search/controllers/search-parameter-manager/) controller. For sake of brevity, this sample uses the UrlManager controller.
+ *
+ * The context values are hard-coded to represent a specific user segment (age group 30-45 with a main interest in sports) as the initial context.
+ * These values will be added to the payload of the search request when the search page is rendered.
+ */
 export default async function Search(url: {
   searchParams: {[key: string]: string | string[] | undefined};
 }) {
