2627602a3db764466706a7159686ed7825a96e04
aprudhomme-coveo <78121423+alexprudhomme@users.noreply.github.com>
2023-11-24 11:13:27 -0500

    remove bad comment
    https://coveord.atlassian.net/browse/KIT-2911

--- packages/headless/src/features/query/query-analytics-actions.ts
+++ packages/headless/src/features/query/query-analytics-actions.ts
@@ -1,6 +1,5 @@
 import {makeAnalyticsAction, SearchAction} from '../analytics/analytics-utils';
 
-//TODO: KIT-2859
 export const logSearchboxSubmit = (): SearchAction =>
   makeAnalyticsAction('analytics/searchbox/submit', (client) =>
     client.makeSearchboxSubmit()
