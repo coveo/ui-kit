4f140a6097622ec5588c1deb062f363911546fbe
ylakhdar <ylakhdar@coveo.com>
2023-11-28 11:06:40 -0500

    feat: support url management with both `UrlManager` and `SearchParametersManager` (#3420)
    https://coveord.atlassian.net/browse/KIT-2735

--- packages/samples/headless-ssr/app-router/src/app/generic/page.tsx
+++ packages/samples/headless-ssr/app-router/src/app/generic/page.tsx
@@ -1,14 +1,22 @@
 import SearchPage from '@/common/components/generic/search-page';
 import {fetchStaticState} from '@/common/lib/generic/engine';
+import {buildSearchParameterSerializer} from '@coveo/headless';
 
-// Entry point SSR function
-export default async function Search() {
-  // TODO: Enable after URL management investigation https://coveord.atlassian.net/browse/KIT-2735
-  // const {coveoSearchParameters} =
-  //   CoveoNextJsSearchParametersSerializer.fromServerSideUrlSearchParams(
-  //     url.searchParams
-  //   );
-  const coveoSearchParameters = {};
+/**
+ * This file defines a Search component that uses the Coveo Headless library to manage its state.
+ *
+ * The Search function is the entry point for server-side rendering (SSR). It uses the `buildSearchParameterSerializer` util from the Coveo Headless
+ * library to serialize the URL search parameters into a string, which is then used by the [UrlManager](https://docs.coveo.com/en/headless/latest/reference/search/controllers/url-manager) controller.
+ *
+ * To synchronize search parameters with the URL with more control on the serialization, you can use the [SearchParameterManager](https://docs.coveo.com/en/headless/latest/reference/search/controllers/search-parameter-manager/) controller. For the sake of brevity, this sample uses the UrlManager controller.
+ *
+ * The context values are hard-coded to represent a specific user segment (age group 30-45 with a main interest in sports) as the initial context.
+ * These values will be added to the payload of the search request when the search page is rendered.
+ */
+export default async function Search(url: {
+  searchParams: {[key: string]: string | string[] | undefined};
+}) {
+  const fragment = buildSearchParameterSerializer().serialize(url.searchParams);
   const contextValues = {
     ageGroup: '30-45',
     mainInterest: 'sports',
@@ -20,10 +28,8 @@ export default async function Search() {
           values: contextValues,
         },
       },
-      searchParameters: {
-        initialState: {
-          parameters: coveoSearchParameters,
-        },
+      urlManager: {
+        initialState: {fragment},
       },
     },
   });
