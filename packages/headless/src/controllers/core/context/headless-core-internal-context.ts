f4abf465c4fdba9994cd9a7f35ca9942ec614f92
Louis Bompart <lbompart@coveo.com>
2023-12-01 15:56:48 -0500

    feat(context): create strict context factory (#3423)
    * feat(context): create strict context factory
    
    https://coveord.atlassian.net/browse/KIT-2901
    
    * Apply suggestions from code review
    
    Co-authored-by: Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Frederic Beaudoin <fbeaudoin@coveo.com>
    
    ---------
    
    Co-authored-by: Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
    Co-authored-by: Frederic Beaudoin <fbeaudoin@coveo.com>

---
+++ packages/headless/src/controllers/core/context/headless-core-internal-context.ts
@@ -0,0 +1,61 @@
+import {CoreEngine} from '../../../app/engine';
+import {
+  addContext,
+  removeContext,
+} from '../../../features/context/context-actions';
+import {contextReducer as context} from '../../../features/context/context-slice';
+import {ContextValue} from '../../../features/context/context-state';
+import {ContextSection} from '../../../state/state-sections';
+import {loadReducerError} from '../../../utils/errors';
+
+/**
+ * The `InternalContext` controller injects [custom contextual information](https://docs.coveo.com/en/3389/)
+ * into the search requests and usage analytics search events sent from a search interface.
+ * Reserved for strict context.
+ * @internal
+ */
+export interface InternalContext {
+  /**
+   * Sets or updates a context key-value pair.
+   *
+   * @param contextKey - The context key.
+   * @param contextValue - The context value.
+   */
+  add(contextKey: string, contextValue: ContextValue): void;
+
+  /**
+   * Removes a context key from the query.
+   * @param key - The context key to remove.
+   */
+  remove(key: string): void;
+}
+
+/**
+ * Creates an `InternalContext` controller instance
+ *
+ * @param engine - The headless engine.
+ * @param props - The configurable `Context` controller properties.
+ * @internal
+ * @returns a `Context` controller instance.
+ */
+export function buildInternalCoreContext(engine: CoreEngine): InternalContext {
+  if (!loadContextReducers(engine)) {
+    throw loadReducerError;
+  }
+  const {dispatch} = engine;
+  return {
+    add(contextKey: string, contextValue: ContextValue) {
+      dispatch(addContext({contextKey, contextValue}));
+    },
+    remove(key: string) {
+      dispatch(removeContext(key));
+    },
+  };
+}
+
+function loadContextReducers(
+  engine: CoreEngine
+): engine is CoreEngine<ContextSection> {
+  engine.addReducers({context});
+  return true;
+}
