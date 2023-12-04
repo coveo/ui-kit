27def27b9c7e01990c508b511e47504d3aa24f0f
simonmilord <simon.milord@gmail.com>
2023-12-04 13:16:56 -0500

    added feedback, moved the gen answer component, removed template logic

--- packages/quantic/force-app/examples/main/lwc/exampleRelevantGenerativeAnswering/exampleRelevantGenerativeAnswering.js
+++ packages/quantic/force-app/examples/main/lwc/exampleRelevantGenerativeAnswering/exampleRelevantGenerativeAnswering.js
@@ -1,14 +1,6 @@
 import {LightningElement, api} from 'lwc';
-// @ts-ignore
-import caseTemplate from './resultTemplates/caseResultTemplate.html';
-// @ts-ignore
-import chatterTemplate from './resultTemplates/chatterResultTemplate.html';
-// @ts-ignore
-import childTemplate from './resultTemplates/childTemplate.html';
-// @ts-ignore
-import parentTemplate from './resultTemplates/parentTemplate.html';
-// @ts-ignore
-import youtubeTemplate from './resultTemplates/youtubeResultTemplate.html';
+
+// For the 'genqatest' query pipeline, the following query should yield a relevant generated answer: How to resolve netflix connection with tivo?
 
 export default class ExampleRelevantGenerativeAnswering extends LightningElement {
   /** @type {string} */
@@ -25,58 +17,4 @@ export default class ExampleRelevantGenerativeAnswering extends LightningElement
   @api answerStyle = 'default';
   /** @type {boolean} */
   @api multilineFooter = false;
-
-  handleResultTemplateRegistration(event) {
-    event.stopPropagation();
-
-    const resultTemplatesManager = event.detail;
-
-    const isCase = CoveoHeadless.ResultTemplatesHelpers.fieldMustMatch(
-      'objecttype',
-      ['Case']
-    );
-    const isYouTube = CoveoHeadless.ResultTemplatesHelpers.fieldMustMatch(
-      'filetype',
-      ['YouTubeVideo']
-    );
-    const isChatter = CoveoHeadless.ResultTemplatesHelpers.fieldMustMatch(
-      'objecttype',
-      ['FeedItem']
-    );
-    const isThread = CoveoHeadless.ResultTemplatesHelpers.fieldMustMatch(
-      'source',
-      ['iNaturalistTaxons']
-    );
-    const isChild = CoveoHeadless.ResultTemplatesHelpers.fieldMustMatch(
-      'quantic__templateId',
-      ['myChildTemplate']
-    );
-
-    resultTemplatesManager.registerTemplates(
-      {
-        content: youtubeTemplate,
-        conditions: [isYouTube],
-        fields: ['ytvideoid', 'ytvideoduration'],
-      },
-      {
-        content: caseTemplate,
-        conditions: [isCase],
-        fields: ['sfstatus', 'sfcasestatus', 'sfcasenumber'],
-      },
-      {
-        content: chatterTemplate,
-        conditions: [isChatter],
-        fields: ['sfcreatedbyname'],
-      },
-      {
-        content: childTemplate,
-        conditions: [isChild],
-        priority: 1,
-      },
-      {
-        content: parentTemplate,
-        conditions: [isThread],
-      }
-    );
-  }
 }
