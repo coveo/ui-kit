b41f34730421e2861ef2a60498922540151c7104
Sym Piracha <104650103+spiracha-coveo@users.noreply.github.com>
2023-12-11 09:46:13 -0500

    Feat 1238 notify docs (coveo-platform/relay#62)
    Original-commit: coveo-platform/relay@db90d456009fb876ffd194649a77aa34015e2861

---
+++ packages/relay/scripts/notify-docs.mjs
@@ -0,0 +1,42 @@
+import { Octokit } from '@octokit/rest';
+import fs from 'fs';
+import path from 'path';
+
+const github = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+const owner = 'coveo';
+const repo = 'doc_jekyll-public-site';
+const event_type = 'published_relay_to_npm';
+
+async function notify() {
+    const relay_version = getRelayPackageVersion();
+    const client_payload = { relay_version };
+
+    return github.repos.createDispatchEvent({
+      owner,
+      repo,
+      event_type,
+      client_payload,
+    });
+}
+
+function getRelayPackageVersion() {
+    const packagePath = path.join('packages', 'relay', 'package.json');
+    const packageFile = fs.readFileSync(packagePath, 'utf8');
+    const packageJson = JSON.parse(packageFile);
+    return packageJson.version;
+}
+  
+async function main() {
+    try {
+        await notify();
+        console.log('notification sent to docs repo');
+    } catch (e) {
+        const { status, message, request } = e;
+        console.error('notification failed to send to docs repo', status, message);
+        console.log(request);
+        process.exit(1);
+    }
+}
+  
+main();
