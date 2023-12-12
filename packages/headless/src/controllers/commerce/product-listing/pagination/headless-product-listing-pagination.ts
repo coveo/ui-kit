d1a791410e8384bf85f31c99633036b56aac3f3d
Nicholas-David Labarre <ndlabarre@coveo.com>
2023-12-12 08:25:48 -0500

    use shared reducer loaders

--- packages/headless/src/controllers/commerce/product-listing/pagination/headless-product-listing-pagination.ts
+++ packages/headless/src/controllers/commerce/product-listing/pagination/headless-product-listing-pagination.ts
@@ -1,11 +1,11 @@
 import {CommerceEngine} from '../../../../app/commerce-engine/commerce-engine';
 import {fetchProductListing} from '../../../../features/commerce/product-listing/product-listing-actions';
-import {productListingV2Reducer as productListing} from '../../../../features/commerce/product-listing/product-listing-slice';
 import {loadReducerError} from '../../../../utils/errors';
 import {
   buildCorePagination,
   Pagination,
 } from '../../pagination/core/headless-core-commerce-pagination';
+import {loadProductListingReducer} from '../utils/load-product-listing-reducers';
 
 /**
  * Creates a `Pagination` controller instance.
@@ -16,18 +16,11 @@ import {
 export function buildProductListingPagination(
   engine: CommerceEngine
 ): Pagination {
-  if (!loadProductListingPaginationReducers(engine)) {
+  if (!loadProductListingReducer(engine)) {
     throw loadReducerError;
   }
 
   return buildCorePagination(engine, {
     fetchResultsActionCreator: fetchProductListing,
   });
-}
-
-function loadProductListingPaginationReducers(
-  engine: CommerceEngine
-): engine is CommerceEngine {
-  engine.addReducers({productListing});
-  return true;
 }
--- packages/headless/src/controllers/commerce/search/pagination/headless-search-pagination.ts
+++ packages/headless/src/controllers/commerce/search/pagination/headless-search-pagination.ts
@@ -1,11 +1,11 @@
 import {CommerceEngine} from '../../../../app/commerce-engine/commerce-engine';
 import {executeSearch} from '../../../../features/commerce/search/search-actions';
-import {commerceSearchReducer as commerceSearch} from '../../../../features/commerce/search/search-slice';
 import {loadReducerError} from '../../../../utils/errors';
 import {
   buildCorePagination,
   Pagination,
 } from '../../pagination/core/headless-core-commerce-pagination';
+import {loadSearchReducer} from '../utils/headless-search-reducers';
 
 /**
  * Creates a `Pagination` controller instance.
@@ -14,18 +14,11 @@ import {
  * @returns A `Pagination` controller instance.
  * */
 export function buildSearchPagination(engine: CommerceEngine): Pagination {
-  if (!loadSearchPagination(engine)) {
+  if (!loadSearchReducer(engine)) {
     throw loadReducerError;
   }
 
   return buildCorePagination(engine, {
     fetchResultsActionCreator: executeSearch,
   });
-}
-
-function loadSearchPagination(
-  engine: CommerceEngine
-): engine is CommerceEngine {
-  engine.addReducers({commerceSearch});
-  return true;
 }
