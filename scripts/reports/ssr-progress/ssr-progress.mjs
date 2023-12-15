a004a075606d8a82336ff80a04ccb36abcd2c2e3
aprudhomme-coveo <78121423+alexprudhomme@users.noreply.github.com>
2023-12-15 13:53:15 -0500

    clean up

--- scripts/reports/ssr-progress/ssr-progress.mjs
+++ scripts/reports/ssr-progress/ssr-progress.mjs
@@ -1,87 +1,71 @@
 import fs from 'fs';
 import {execute} from '../../exec.mjs';
 
+async function buildHeadless() {
+  await execute('npx', ['nx', 'run', 'headless:build']);
+}
+
+function getParsedDoc() {
+  const rawdata = fs.readFileSync(
+    'packages/headless/dist/parsed_doc.json',
+    'utf8'
+  );
+  const rawSSRdata = fs.readFileSync(
+    'packages/headless/dist/ssr_parsed_doc.json',
+    'utf8'
+  );
+
+  const parsed_doc = JSON.parse(rawdata);
+  const ssr_parsed_doc = JSON.parse(rawSSRdata);
+
+  return {parsed_doc, ssr_parsed_doc};
+}
+
 function toOneDecimal(num) {
   return Math.round(num * 10) / 10;
 }
 
-async function prepareData() {
-  //always checkout to current branch
-  console.log('building ssr progress files');
-  await execute('npx', ['nx', 'run', 'headless:build']);
-
-  try {
-    const rawdata = fs.readFileSync(
-      'packages/headless/dist/parsed_doc.json',
-      'utf8'
+async function prepareData(parsed_doc, ssr_parsed_doc) {
+  let rows = [];
+  let logs = [];
+  parsed_doc.forEach((useCase) => {
+    const ssrUseCase = ssr_parsed_doc.find(
+      (ssrUseCase) => ssrUseCase.name === 'ssr.' + useCase.name
     );
-    const rawSSRdata = fs.readFileSync(
-      'packages/headless/dist/ssr_parsed_doc.json',
-      'utf8'
-    );
+    if (ssrUseCase !== undefined) {
+      let counter = 0;
+      useCase.controllers.forEach((controller) => {
+        if (
+          ssrUseCase.controllers.find(
+            (ssrController) =>
+              ssrController.definer ===
+              'define' + controller.initializer.name.substring(5)
+          )
+        ) {
+          counter += 1;
+        } else {
+          logs.push(`**${useCase.name}** : ${controller.initializer.name}`);
+        }
+      });
+      rows.push([
+        useCase.name,
+        counter,
+        useCase.controllers.length,
+        toOneDecimal(counter / useCase.controllers.length),
+      ]);
+    } else {
+      rows.push([useCase.name, '0', useCase.controllers.length, '0']);
+      logs.push('There is no SSR use case for ' + useCase.name);
+    }
+  });
 
-    const parsed_doc = JSON.parse(rawdata);
-    const ssr_parsed_doc = JSON.parse(rawSSRdata);
-
-    const args = process.argv.slice(2);
-    let rows = [];
-
-    parsed_doc.forEach((useCase) => {
-      const ssrUseCase = ssr_parsed_doc.find(
-        (ssrUseCase) => ssrUseCase.name === 'ssr.' + useCase.name
-      );
-      if (ssrUseCase !== undefined) {
-        let counter = 0;
-        useCase.controllers.forEach((controller) => {
-          if (
-            ssrUseCase.controllers.find(
-              (ssrController) =>
-                ssrController.definer ===
-                'define' + controller.initializer.name.substring(5)
-            )
-          ) {
-            counter += 1;
-          } else {
-            if (args[0] === '-d') {
-              console.log(controller.initializer.name);
-            }
-          }
-        });
-        rows.push([
-          useCase.name,
-          counter,
-          useCase.controllers.length,
-          toOneDecimal(counter / useCase.controllers.length),
-        ]);
-
-        console.log(
-          useCase.name + ' ' + counter + '/' + useCase.controllers.length
-        );
-      } else {
-        console.log('There is no SSR use case for ' + useCase.name);
-        rows.push([
-          useCase.name,
-          '0',
-          useCase.controllers.length,
-          'SSR is not supported',
-        ]);
-      }
-    });
-
-    return rows;
-    // Continue with your code using the file data...
-
-    console.log('File read successfully');
-  } catch (err) {
-    console.error('Error reading file:', err);
-    return [];
-    // Handle the error or continue with default values, depending on your requirements
-  }
+  return {rows, logs};
 }
-function buildVisualReport(rows) {
+
+function buildVisualReport(rows, logs) {
   const rowsWithColumnsConcatenated = rows.map((row) => '|' + row.join('|'));
   const presentableRows = rowsWithColumnsConcatenated.join('\n');
-  const details = 'these are the details logs';
+  const presentableLogs = logs.join('\n');
   return `
   **SSR Progress**
   
@@ -89,22 +73,18 @@ function buildVisualReport(rows) {
   | ---- |:--------:|:--------:|:------:
   ${presentableRows}
   <details>
-  <summary>Click me</summary>
-  
-  ### Heading
-  1. Foo
-  2. Bar
-     * Baz
-     * Qux
-  ${details}
+  <summary>Detailed logs</summary>
+  ${presentableLogs}
 </details>
   `;
 }
 
 export async function buildSSRProgressReport() {
-  const rows = await prepareData();
-  const ssrProgress = buildVisualReport(rows);
+  await buildHeadless();
+  const {parsed_doc, ssr_parsed_doc} = getParsedDoc();
+  const {rows, logs} = await prepareData(parsed_doc, ssr_parsed_doc);
+  const ssrProgress = buildVisualReport(rows, logs);
   return ssrProgress;
 }
 
-// console.log(await buildSSRProgressReport());
+console.log(await buildSSRProgressReport());
