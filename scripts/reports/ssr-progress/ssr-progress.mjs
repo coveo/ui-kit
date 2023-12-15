6a86a0f12a11dd02a814e4fc7774b08de3b1addb
aprudhomme-coveo <78121423+alexprudhomme@users.noreply.github.com>
2023-12-15 10:45:21 -0500

    first test

---
+++ scripts/reports/ssr-progress/ssr-progress.mjs
@@ -0,0 +1,69 @@
+export async function buildSSRProgressReport() {
+  const ssrProgress = 'SSR Progress: 0%';
+  return ssrProgress;
+}
+
+const rawdata = fs.readFileSync('dist/parsed_doc.json', 'utf8');
+const rawSSRdata = fs.readFileSync('dist/ssr_parsed_doc.json', 'utf8');
+
+const parsed_doc = JSON.parse(rawdata);
+const ssr_parsed_doc = JSON.parse(rawSSRdata);
+
+function getRows() {
+  const args = process.argv.slice(2);
+  let rows = [];
+
+  parsed_doc.forEach((useCase) => {
+    const ssrUseCase = ssr_parsed_doc.find(
+      (ssrUseCase) => ssrUseCase.name === 'ssr.' + useCase.name
+    );
+    if (ssrUseCase !== undefined) {
+      let counter = 0;
+      useCase.controllers.forEach((controller) => {
+        if (
+          ssrUseCase.controllers.find(
+            (ssrController) =>
+              ssrController.definer ===
+              'define' + controller.initializer.name.substring(5)
+          )
+        ) {
+          counter += 1;
+        } else {
+          if (args[0] === '-d') {
+            console.log(controller.initializer.name);
+          }
+        }
+      });
+      rows.push([
+        useCase.name,
+        counter,
+        useCase.controllers.length,
+        counter / useCase.controllers.length,
+      ]);
+
+      console.log(
+        useCase.name + ' ' + counter + '/' + useCase.controllers.length
+      );
+    } else {
+      console.log('There is no SSR use case for ' + useCase.name);
+      rows.push([
+        useCase.name,
+        '∅',
+        useCase.controllers.length,
+        counter / useCase.controllers.length,
+      ]);
+    }
+  });
+
+  return rows;
+}
+function buildVisualReport() {
+  const presentableRows = getRows();
+  return `
+  **Bundle Size**
+  
+  | Use case | SSR (kb) | CSR (kb) | Progress (%)
+  | ---- |:--------:|:--------:|:------:
+  ${presentableRows}
+  `;
+}
