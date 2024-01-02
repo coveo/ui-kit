4456483dd1a3673a8df014e0ff84efebfbaa737b
ylakhdar <ylakhdar@coveo.com>
2024-01-02 10:46:06 -0500

    update generic app
    https://coveord.atlassian.net/browse/KIT-2945

--- packages/samples/headless-ssr/app-router/src/app/generic/page.tsx
+++ packages/samples/headless-ssr/app-router/src/app/generic/page.tsx
@@ -1,6 +1,6 @@
+import {CoveoNextJsSearchParametersSerializer} from '@/common/components/common/search-parameters-serializer';
 import SearchPage from '@/common/components/generic/search-page';
 import {fetchStaticState} from '@/common/lib/generic/engine';
-import {buildSearchParameterSerializer} from '@coveo/headless';
 
 /**
  * This file defines a Search component that uses the Coveo Headless library to manage its state.
@@ -16,7 +16,10 @@ import {buildSearchParameterSerializer} from '@coveo/headless';
 export default async function Search(url: {
   searchParams: {[key: string]: string | string[] | undefined};
 }) {
-  const fragment = buildSearchParameterSerializer().serialize(url.searchParams);
+  const {coveoSearchParameters} =
+    CoveoNextJsSearchParametersSerializer.fromUrlSearchParameters(
+      url.searchParams
+    );
   const contextValues = {
     ageGroup: '30-45',
     mainInterest: 'sports',
@@ -28,8 +31,8 @@ export default async function Search(url: {
           values: contextValues,
         },
       },
-      urlManager: {
-        initialState: {fragment},
+      searchParameterManager: {
+        initialState: {parameters: coveoSearchParameters},
       },
     },
   });
