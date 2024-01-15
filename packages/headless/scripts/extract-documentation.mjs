bb3f17ccc3301255a679ee53275fcd7aa6dee369
Frederic Beaudoin <fbeaudoin@coveo.com>
2024-01-15 16:39:20 -0500

    Remove padding library that's preventing node 20.11 update

--- packages/headless/scripts/extract-documentation.mjs
+++ packages/headless/scripts/extract-documentation.mjs
@@ -1,6 +1,5 @@
 import {execa} from 'execa';
 import {readdirSync} from 'node:fs';
-import padStream from 'pad-stream';
 
 const dir = './config/api-extractor';
 
@@ -12,9 +11,8 @@ async function main() {
 
   const jobs = paths.map((path) => {
     const apiProcess = execa('api-extractor', ['run', '-l', '-c', path], {});
-    const streamPrefix = path.split('/').pop().split('.json').shift() + ':\t';
-    apiProcess.stdout.pipe(padStream(1, streamPrefix)).pipe(process.stdout);
-    apiProcess.stderr.pipe(padStream(1, streamPrefix)).pipe(process.stderr);
+    apiProcess.stdout.pipe(process.stdout);
+    apiProcess.stderr.pipe(process.stderr);
     return apiProcess;
   });
   await Promise.all(jobs);
