7372a2bb1ce745c7442c3a50e161cf8a1b28ace8
Jihan el medini <119955059+jelmedini@users.noreply.github.com>
2024-01-15 16:09:22 -0500

    feat(atomic): change insight searchbox to be expandable by default (#3524)
    * feat(insight): change insight searchbox to be expandable by default
    
    * feat(insight): change suggestions search icon
    
    * feat(insight): fix cypress test
    
    * feat(insight): fix cypress test action
    
    * feat(insight): delete type catsing

--- packages/atomic/cypress/integration-insight-panel/insight-panel-actions.ts
+++ packages/atomic/cypress/integration-insight-panel/insight-panel-actions.ts
@@ -4,6 +4,6 @@ export const InsightPanelActions = {
   executeQuery: (query?: string) =>
     InsightPanelsSelectors.searchbox()
       .shadow()
-      .find('input')
+      .find('textarea')
       .type(`${query}{enter}`),
 };
