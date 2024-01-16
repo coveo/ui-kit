ceb58f499d18fe8255d0e79c81507f8dae3ef51f
ylakhdar <ylakhdar@coveo.com>
2024-01-16 09:27:41 -0500

    fix(samples/headless-ssr): revisit url management with Search Parameter Manager (#3516)
    https://coveord.atlassian.net/browse/KIT-2945

--- packages/samples/headless-ssr/app-router/src/app/react/page.tsx
+++ packages/samples/headless-ssr/app-router/src/app/react/page.tsx
@@ -1,26 +1,29 @@
+import {NextJsSearchParameterSerializer} from '@/common/components/common/search-parameter-serializer';
 import {AuthorFacet} from '@/common/components/react/facets';
 import ResultList from '@/common/components/react/result-list';
 import SearchBox from '@/common/components/react/search-box';
 import {SearchPageProvider} from '@/common/components/react/search-page';
-import UrlManager from '@/common/components/react/url-manager';
+import SearchParameterManager from '@/common/components/react/search-parameter-manager';
 import {fetchStaticState} from '@/common/lib/react/engine';
-import {buildSearchParameterSerializer} from '@coveo/headless';
 
 /**
  * This file defines a Search component that uses the Coveo Headless library to manage its state.
  *
  * The Search function is the entry point for server-side rendering (SSR). It uses the `buildSearchParameterSerializer` util from the Coveo Headless
- * library to serialize the url search parameters into a string, which is then used by the [UrlManager](https://docs.coveo.com/en/headless/latest/reference/search/controllers/url-manager) controller.
+ * library to serialize the url search parameters into a string, which is then used by the [SearchParameterManager](https://docs.coveo.com/en/headless/latest/reference/search/controllers/search-parameter-manager) controller.
  *
- * To synchronize search parameters with the URL with more control on the serialization, you can use the [SearchParameterManager](https://docs.coveo.com/en/headless/latest/reference/search/controllers/search-parameter-manager/) controller. For sake of brevity, this sample uses the UrlManager controller.
+ * To synchronize search parameters with the URL with more control on the serialization, you can use the [SearchParameterManager](https://docs.coveo.com/en/headless/latest/reference/search/controllers/search-parameter-manager/) controller. For sake of brevity, this sample uses the SearchParameterManager controller.
  *
  * The context values are hard-coded to represent a specific user segment (age group 30-45 with a main interest in sports) as the initial context.
  * These values will be added to the payload of the search request when the search page is rendered.
  */
+
 export default async function Search(url: {
   searchParams: {[key: string]: string | string[] | undefined};
 }) {
-  const fragment = buildSearchParameterSerializer().serialize(url.searchParams);
+  const {searchParameters: coveoSearchParameters} =
+    NextJsSearchParameterSerializer.fromUrlSearchParameters(url.searchParams);
+
   const contextValues = {
     ageGroup: '30-45',
     mainInterest: 'sports',
@@ -32,15 +35,15 @@ export default async function Search(url: {
           values: contextValues,
         },
       },
-      urlManager: {
-        initialState: {fragment},
+      searchParameterManager: {
+        initialState: {parameters: coveoSearchParameters},
       },
     },
   });
 
   return (
     <SearchPageProvider staticState={staticState}>
-      <UrlManager />
+      <SearchParameterManager />
       <SearchBox />
       <ResultList />
       <AuthorFacet />
