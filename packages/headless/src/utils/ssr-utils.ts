d4f77a8e288e1bf808dbe6be80ba007efc4b31a4
ylakhdar <ylakhdar@coveo.com>
2024-01-24 15:49:13 -0500

    Apply suggestions from code review
    Co-authored-by: Frederic Beaudoin <fbeaudoin@coveo.com>

--- packages/headless/src/utils/ssr-utils.ts
+++ packages/headless/src/utils/ssr-utils.ts
@@ -9,28 +9,25 @@ export interface CookieStore {
   /**
    * Sets a cookie with the specified name, value, and options.
    *
-   * @param name The name of the cookie.
-   * @param value The value of the cookie.
-   * @param opts The options for the cookie, including the `maxAge` property.
-   * *Note* the {@link https://datatracker.ietf.org/doc/html/rfc6265#section-5.2.2|The Max-Age Attribute}
+   * @param name - The name of the cookie.
+   * @param value - The value of the cookie.
+   * @param opts - The options to set on the cookie.
    */
   set(
     name: string,
     value: string,
     opts: {
       /**
-       * Specifies the number (in seconds) to be the value for the `Max-Age`
-       * `Set-Cookie` attribute. The given number will be converted to an integer by rounding down. By default, no maximum age is set.
-       *  For more info: {@link https://datatracker.ietf.org/doc/html/rfc6265#section-5.2.2|The Max-Age Attribute}
+       * The value to use for {@link https://datatracker.ietf.org/doc/html/rfc6265#section-5.2.2|Max-Age Set-Cookie attribute}. The specified number will be rounded down to the nearest integer. By default, no Max-Age is set.
        */
-      maxAge?: number | undefined;
+      maxAge?: number;
     }
   ): void;
 
   /**
    * Deletes a cookie
    *
-   * @param name the name of the cookie
+   * @param name - The name of the cookie.
    */
   delete(name: string): void;
 }
--- packages/samples/headless-ssr/app-router/src/middleware.ts
+++ packages/samples/headless-ssr/app-router/src/middleware.ts
@@ -2,10 +2,10 @@ import {CookieStore, analyticsTrackerMiddleware} from '@coveo/headless/ssr';
 import {NextRequest, NextResponse} from 'next/server';
 
 /**
- * This will assign users a new client ID when it is their first visit, unless they have opted out of tracking.
+ * This will assign users a new client ID on their first visit, unless they have opted out of tracking.
  * Make sure to return the response object from the middleware function.
  *
- * For more info on middlewares, visit https://nextjs.org/docs/app/building-your-application/routing/middleware.
+ * For more on middlewares, see https://nextjs.org/docs/app/building-your-application/routing/middleware.
  */
 export default function middleware(request: NextRequest) {
   const response = NextResponse.next();
@@ -23,8 +23,8 @@ export default function middleware(request: NextRequest) {
 }
 
 /**
- * Middlewares run by default for every request, incluing the ones for getting assets like JavaScript, CSS, and image files.
- * We use the `matcher` property to ensure the AnalyticsMiddleware only runs on the page load.
+ * Middlewares run by default for every request, including the ones for getting assets like JavaScript, CSS, and image files.
+ * We use the `matcher` property to ensure that the AnalyticsMiddleware only runs on the page load.
  * More info [here](https://nextjs.org/docs/pages/building-your-application/routing/middleware#matching-paths)
  */
 export const config = {
