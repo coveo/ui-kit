0ade82b74c911860db957affcb09367c911ef863
Louis Bompart <lbompart@coveo.com>
2024-01-30 17:52:52 -0500

    Apply suggestions from code review

--- packages/atomic/cypress/e2e/search-interface.cypress.ts
+++ packages/atomic/cypress/e2e/search-interface.cypress.ts
@@ -90,7 +90,6 @@ describe('Search Interface Component', () => {
               searchHub: options.engine?.searchHub,
             },
           },
-          middlewares,
         });
         await searchInterface.initializeWithSearchEngine(engine);
       });
