a249d5517fb957c305263e4d0f396887f81cb4df
ylakhdar <ylakhdar@coveo.com>
2024-02-02 15:12:19 -0500

    add E2E test
    https://coveord.atlassian.net/browse/KIT-2981

--- packages/atomic/cypress/e2e/refine-toggle.cypress.ts
+++ packages/atomic/cypress/e2e/refine-toggle.cypress.ts
@@ -1,13 +1,20 @@
 import {TestFixture} from '../fixtures/test-fixture';
 import * as CommonAssertions from './common-assertions';
 import {automaticFacetGeneratorComponent} from './facets/automatic-facet-generator/automatic-facet-generator-assertions';
+import {hierarchicalField} from './facets/category-facet/category-facet-actions';
 import {categoryFacetComponent} from './facets/category-facet/category-facet-selectors';
+import {colorFacetField} from './facets/color-facet/color-facet-actions';
 import {colorFacetComponent} from './facets/color-facet/color-facet-selectors';
+import {field as facetField} from './facets/facet/facet-actions';
 import {facetComponent} from './facets/facet/facet-selectors';
 import {facetManagerComponent} from './facets/manager/facet-manager-actions';
+import {numericFacetField} from './facets/numeric-facet/numeric-facet-actions';
 import {numericFacetComponent} from './facets/numeric-facet/numeric-facet-selectors';
+import {ratingFacetField} from './facets/rating-facet/rating-facet-actions';
 import {ratingFacetComponent} from './facets/rating-facet/rating-facet-selectors';
+import {ratingRangeFacetField} from './facets/rating-range-facet/rating-range-facet-actions';
 import {ratingRangeFacetComponent} from './facets/rating-range-facet/rating-range-facet-selectors';
+import {timeframeFacetField} from './facets/timeframe-facet/timeframe-facet-action';
 import {timeframeFacetComponent} from './facets/timeframe-facet/timeframe-facet-selectors';
 import {
   addFacetManagerWithBothTypesOfFacets,
@@ -109,6 +116,25 @@ describe('Refine Toggle Test Suites', () => {
       RefineModalSelectors.facets()
         .children()
         .should('have.length', allFacets.length);
+    });
+
+    it('should display the facets in the correct order', () => {
+      const expectedFacetOrder = [
+        facetField,
+        numericFacetField,
+        hierarchicalField,
+        ratingFacetField,
+        ratingRangeFacetField,
+        colorFacetField,
+        timeframeFacetField,
+      ];
+
+      RefineModalSelectors.facets()
+        .children()
+        .each(($facet, index) => {
+          const expectedFacet = expectedFacetOrder[index];
+          cy.wrap($facet).should('have.attr', 'field', expectedFacet);
+        });
     });
 
     it('should close when clicking close button', () => {
