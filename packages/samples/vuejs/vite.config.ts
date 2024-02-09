977b7aa1510f05cedba871ed538143299f4be25e
Louis Bompart <lbompart@coveo.com>
2024-02-09 13:11:17 -0500

    no warning+readme OK

--- packages/samples/vuejs/vite.config.ts
+++ packages/samples/vuejs/vite.config.ts
@@ -4,6 +4,15 @@ import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // treat all tags with a dash as custom elements
+          isCustomElement: (tag) => tag.startsWith('atomic-'),
+        },
+      },
+    }),
+  ],
   assetsInclude: ['**/*.html'],
 });
