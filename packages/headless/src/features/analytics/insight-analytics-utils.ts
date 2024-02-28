1935020802ab9c011e1f9cfe74b1ab1cbe673b8c
mmitiche <mmitiche@coveo.com>
2024-02-28 10:00:01 -0500

    todo added

--- packages/headless/src/features/analytics/insight-analytics-utils.ts
+++ packages/headless/src/features/analytics/insight-analytics-utils.ts
@@ -2,6 +2,7 @@ import {InsightPanel} from '@coveo/relay-event-types';
 import {InsightAppState} from '../../state/insight-app-state';
 import {getCaseContextAnalyticsMetadata} from '../case-context/case-context-state';
 
+// TODO SFINT-5420: Update from where we are grabbing the case context.
 export const analyticsEventCaseContext = (
   state: Partial<InsightAppState>
 ): InsightPanel.Context => {
