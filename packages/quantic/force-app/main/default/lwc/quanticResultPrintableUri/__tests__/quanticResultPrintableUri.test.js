567cdc7665ac628252b020c68c9e986385608781
mmitiche <86681870+mmitiche@users.noreply.github.com>
2024-02-28 10:10:06 -0500

    Update packages/quantic/force-app/main/default/lwc/quanticResultPrintableUri/__tests__/quanticResultPrintableUri.test.js

--- packages/quantic/force-app/main/default/lwc/quanticResultPrintableUri/__tests__/quanticResultPrintableUri.test.js
+++ packages/quantic/force-app/main/default/lwc/quanticResultPrintableUri/__tests__/quanticResultPrintableUri.test.js
@@ -8,7 +8,7 @@ const MIN_MAX_NUMBER_OF_PARTS = 3;
 
 jest.mock('c/quanticHeadlessLoader');
 
-// Helper function to mock Headless and Bueno  for this suite of tests.
+// Helper function to mock Headless and Bueno for this suite of tests.
 function mockHeadlessAndBueno() {
   jest.spyOn(mockHeadlessLoader, 'getHeadlessEnginePromise').mockReturnValue(
     new Promise((resolve) => {
