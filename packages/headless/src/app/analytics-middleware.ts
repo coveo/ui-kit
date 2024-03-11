4753ad006a8ef1d94413920b7b51e7d824c7637e
Olivier Lamothe <olamothe@coveo.com>
2024-03-11 14:01:47 -0400

    typeguard for analytics middleware

--- packages/headless/src/app/analytics-middleware.ts
+++ packages/headless/src/app/analytics-middleware.ts
@@ -1,8 +1,22 @@
+import {isNullOrUndefined} from '@coveo/bueno';
 import {Middleware, UnknownAction} from '@reduxjs/toolkit';
 
-type UnknownActionWithPossibleAnalyticsPayload = UnknownAction & {
-  payload: {analyticsAction?: unknown};
+type UnknownActionWithAnalyticsPayload = UnknownAction & {
+  payload: {analyticsAction?: UnknownAction};
 };
+
+function isAnalyticsAction(
+  action: unknown
+): action is UnknownActionWithAnalyticsPayload {
+  const analytics = (action as UnknownActionWithAnalyticsPayload).payload
+    ?.analyticsAction;
+
+  return isActionWithType(action) && !isNullOrUndefined(analytics);
+}
+
+function isActionWithType(action: unknown): action is UnknownAction {
+  return 'type' in (action as UnknownAction);
+}
 
 export const analyticsMiddleware: Middleware = (api) => (next) => (action) => {
   // Why all these shenanigans ?
@@ -12,47 +26,46 @@ export const analyticsMiddleware: Middleware = (api) => (next) => (action) => {
   // To meet those conditions, we ensure the analyticsMiddleware is always the first executed (in store.ts/configureStore) and that it is removed from the payload.
   // We only keep a reference to the function in order to dispatch it correctly after the reducer (next) has the chance of processing the action/state change
 
-  const actionWithPossibleAnalytics =
-    action as UnknownActionWithPossibleAnalyticsPayload;
+  let analytics: UnknownAction | undefined = undefined;
 
-  const analytics = actionWithPossibleAnalytics.payload?.analyticsAction;
-
-  if (analytics !== undefined) {
-    delete actionWithPossibleAnalytics.payload.analyticsAction;
+  if (isAnalyticsAction(action)) {
+    analytics = action.payload.analyticsAction;
+    delete action.payload.analyticsAction;
   }
 
   const ret = next(action);
 
-  if (
-    actionWithPossibleAnalytics.type === 'search/executeSearch/fullfilled' &&
-    analytics === undefined
-  ) {
-    console.error('No analytics action associated with search:', action);
-  }
+  if (isActionWithType(action)) {
+    if (
+      action.type === 'search/executeSearch/fullfilled' &&
+      analytics === undefined
+    ) {
+      console.error('No analytics action associated with search:', action);
+    }
 
-  if (
-    actionWithPossibleAnalytics.type === 'recommendation/get/fullfilled' &&
-    analytics === undefined
-  ) {
-    console.error(
-      'No analytics action associated with recommendation:',
-      action
-    );
-  }
+    if (
+      action.type === 'recommendation/get/fullfilled' &&
+      analytics === undefined
+    ) {
+      console.error(
+        'No analytics action associated with recommendation:',
+        action
+      );
+    }
 
-  if (
-    actionWithPossibleAnalytics.type ===
-      'productRecommendations/get/fullfilled' &&
-    analytics === undefined
-  ) {
-    console.error(
-      'No analytics action associated with product recommendation:',
-      action
-    );
+    if (
+      action.type === 'productRecommendations/get/fullfilled' &&
+      analytics === undefined
+    ) {
+      console.error(
+        'No analytics action associated with product recommendation:',
+        action
+      );
+    }
   }
 
   if (analytics !== undefined) {
-    api.dispatch(analytics as UnknownAction);
+    api.dispatch(analytics);
   }
 
   return ret;
