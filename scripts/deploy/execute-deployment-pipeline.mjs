95e8f7f04fa575e6a57f5849f3dea0fbd521bb56
Louis Bompart <lbompart@coveo.com>
2024-03-13 18:01:29 -0400

    debug-stuff

--- scripts/deploy/execute-deployment-pipeline.mjs
+++ scripts/deploy/execute-deployment-pipeline.mjs
@@ -23,18 +23,4 @@ const atomicReact = getVersionComposants(atomicReactJson.version);
 const atomicHostedPage = getVersionComposants(atomicHostedPageJson.version);
 execSync(`
 docker run -a stderr -a stdout 458176070654.dkr.ecr.us-east-2.amazonaws.com/jenkins/deployment_package:stable
-  deployment-package package create --with-deploy \
-    --resolve HEADLESS_MAJOR_VERSION=${headless.major} \
-    --resolve HEADLESS_MINOR_VERSION=${headless.minor} \
-    --resolve HEADLESS_PATCH_VERSION=${headless.patch} \
-    --resolve ATOMIC_MAJOR_VERSION=${atomic.major} \
-    --resolve ATOMIC_MINOR_VERSION=${atomic.minor} \
-    --resolve ATOMIC_PATCH_VERSION=${atomic.patch} \
-    --resolve ATOMIC_REACT_MAJOR_VERSION=${atomicReact.major} \
-    --resolve ATOMIC_REACT_MINOR_VERSION=${atomicReact.minor} \
-    --resolve ATOMIC_REACT_PATCH_VERSION=${atomicReact.patch} \
-    --resolve ATOMIC_HOSTED_PAGE_MAJOR_VERSION=${atomicHostedPage.major} \
-    --resolve ATOMIC_HOSTED_PAGE_MINOR_VERSION=${atomicHostedPage.minor} \
-    --resolve ATOMIC_HOSTED_PAGE_PATCH_VERSION=${atomicHostedPage.patch} \
-    --resolve GITHUB_RUN_ID=${context.runId} \
-    --changeset ${releaseCommit}`);
+  deployment-package --version`);
