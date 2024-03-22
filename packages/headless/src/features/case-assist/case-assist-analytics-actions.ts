0f5b7a516a820f6345c9d7fdb956d63d6b240f29
mmitiche <mmitiche@coveo.com>
2024-03-22 05:23:04 -0400

    case assist analytics migrated to the new protocol

--- packages/headless/src/features/case-assist/case-assist-analytics-actions.ts
+++ packages/headless/src/features/case-assist/case-assist-analytics-actions.ts
@@ -1,3 +1,4 @@
+import {CaseAssist, ItemClick} from '@coveo/relay-event-types';
 import {
   CaseAssistAction,
   makeCaseAssistAnalyticsAction,
@@ -10,14 +11,18 @@ import {
 } from './case-assist-analytics-selectors';
 
 export const logCaseStart = (): CaseAssistAction =>
-  makeCaseAssistAnalyticsAction(
-    'analytics/caseAssist/case/start',
-    (client, state) =>
-      client.logEnterInterface({
+  makeCaseAssistAnalyticsAction({
+    prefix: 'analytics/caseAssist/case/start',
+    __legacy__getBuilder: (client, state) => {
+      return client.logEnterInterface({
         ticket: caseAssistCaseSelector(state),
-      })
-  );
+      });
+    },
+    analyticsType: 'CaseAssist.Start',
+    analyticsPayloadBuilder: (): CaseAssist.Start => ({}),
+  });
 
+//TODO: SFINT-5435
 export const logCaseNextStage = (
   options?: NextStageOptions
 ): CaseAssistAction =>
@@ -31,108 +36,197 @@ export const logCaseNextStage = (
   );
 
 export const logCreateCase = (): CaseAssistAction =>
-  makeCaseAssistAnalyticsAction(
-    'analytics/caseAssist/case/create',
-    (client, state) =>
-      client.logCaseCreated({
+  makeCaseAssistAnalyticsAction({
+    prefix: 'analytics/caseAssist/case/create',
+    __legacy__getBuilder: (client, state) => {
+      return client.logCaseCreated({
         ticket: caseAssistCaseSelector(state),
-      })
-  );
+      });
+    },
+    analyticsType: 'CaseAssist.CreateTicket',
+    analyticsPayloadBuilder: (state): CaseAssist.CreateTicket => {
+      const {id, category, subject, description, productId} =
+        caseAssistCaseSelector(state);
+      return {
+        ticket: {
+          id: id!,
+          category,
+          subject,
+          description,
+          system: productId,
+        },
+      };
+    },
+  });
 
 export const logSolveCase = (): CaseAssistAction =>
-  makeCaseAssistAnalyticsAction(
-    'analytics/caseAssist/case/solve',
-    (client, state) =>
-      client.logCaseSolved({
+  makeCaseAssistAnalyticsAction({
+    prefix: 'analytics/caseAssist/case/solve',
+    __legacy__getBuilder: (client, state) => {
+      return client.logCaseSolved({
         ticket: caseAssistCaseSelector(state),
-      })
-  );
+      });
+    },
+    analyticsType: 'CaseAssist.Cancel',
+    analyticsPayloadBuilder: (): CaseAssist.Cancel => ({
+      reason: 'solved',
+    }),
+  });
 
 export const logAbandonCase = (): CaseAssistAction =>
-  makeCaseAssistAnalyticsAction(
-    'analytics/caseAssist/case/abandon',
-    (client, state) =>
-      client.logCaseCancelled({
+  makeCaseAssistAnalyticsAction({
+    prefix: 'analytics/caseAssist/case/solve',
+    __legacy__getBuilder: (client, state) => {
+      return client.logCaseCancelled({
         ticket: caseAssistCaseSelector(state),
-      })
-  );
+      });
+    },
+    analyticsType: 'CaseAssist.Cancel',
+    analyticsPayloadBuilder: (): CaseAssist.Cancel => ({
+      reason: 'quit',
+    }),
+  });
 
 export const logUpdateCaseField = (fieldName: string): CaseAssistAction =>
-  makeCaseAssistAnalyticsAction(
-    'analytics/caseAssist/case/field/update',
-    (client, state) =>
-      client.logUpdateCaseField({
+  makeCaseAssistAnalyticsAction({
+    prefix: 'analytics/caseAssist/case/field/update',
+    __legacy__getBuilder: (client, state) => {
+      return client.logUpdateCaseField({
         fieldName,
         ticket: caseAssistCaseSelector(state),
-      })
-  );
+      });
+    },
+    analyticsType: 'CaseAssist.UpdateField',
+    analyticsPayloadBuilder: (state): CaseAssist.UpdateField => {
+      const fieldValue =
+        state.caseField?.fields?.[fieldName]?.value ||
+        state.caseInput?.[fieldName]?.value ||
+        '';
+      return {
+        fieldName,
+        fieldValue,
+      };
+    },
+  });
 
 export const logAutoSelectCaseField = (
   classificationId: string
 ): CaseAssistAction =>
-  makeCaseAssistAnalyticsAction(
-    'analytics/caseAssist/classification/click',
-    (client, state) =>
-      client.logSelectFieldSuggestion({
+  makeCaseAssistAnalyticsAction({
+    prefix: 'analytics/caseAssist/classification/click',
+    __legacy__getBuilder: (client, state) => {
+      return client.logSelectFieldSuggestion({
         suggestion: caseAssistCaseClassificationSelector(
           state,
           classificationId,
           true
         ),
         ticket: caseAssistCaseSelector(state),
-      })
-  );
+      });
+    },
+    analyticsType: 'CaseAssist.SelectFieldClassification',
+    analyticsPayloadBuilder: (state): CaseAssist.SelectFieldClassification => {
+      return {
+        autoselected: true,
+        fieldClassification: {
+          id: classificationId,
+          responseId: state.caseField?.status?.lastResponseId || '',
+        },
+      };
+    },
+  });
 
 export const logClassificationClick = (
   classificationId: string
 ): CaseAssistAction =>
-  makeCaseAssistAnalyticsAction(
-    'analytics/caseAssist/classification/click',
-    (client, state) =>
-      client.logSelectFieldSuggestion({
+  makeCaseAssistAnalyticsAction({
+    prefix: 'analytics/caseAssist/classification/click',
+    __legacy__getBuilder: (client, state) => {
+      return client.logSelectFieldSuggestion({
         suggestion: caseAssistCaseClassificationSelector(
           state,
           classificationId
         ),
         ticket: caseAssistCaseSelector(state),
-      })
-  );
+      });
+    },
+    analyticsType: 'CaseAssist.SelectFieldClassification',
+    analyticsPayloadBuilder: (state): CaseAssist.SelectFieldClassification => {
+      return {
+        autoselected: false,
+        fieldClassification: {
+          id: classificationId,
+          responseId: state.caseField?.status?.lastResponseId || '',
+        },
+      };
+    },
+  });
 
 export const logDocumentSuggestionClick = (
   suggestionId: string
 ): CaseAssistAction =>
-  makeCaseAssistAnalyticsAction(
-    'analytics/caseAssist/documentSuggestion/click',
-    (client, state) =>
-      client.logSelectDocumentSuggestion({
+  makeCaseAssistAnalyticsAction({
+    prefix: 'analytics/caseAssist/documentSuggestion/click',
+    __legacy__getBuilder: (client, state) => {
+      return client.logSelectDocumentSuggestion({
         suggestion: caseAssistDocumentSuggestionSelector(state, suggestionId),
         ticket: caseAssistCaseSelector(state),
-      })
-  );
+      });
+    },
+    analyticsType: 'CaseAssist.DocumentSuggestionClick',
+    analyticsPayloadBuilder: (state): CaseAssist.DocumentSuggestionClick => {
+      return {
+        documentSuggestion: {
+          id: suggestionId,
+          responseId: state.documentSuggestion?.status?.lastResponseId || '',
+        },
+      };
+    },
+  });
 
 export const logQuickviewDocumentSuggestionClick = (
   suggestionId: string
 ): CaseAssistAction =>
-  makeCaseAssistAnalyticsAction(
-    'analytics/caseAssist/documentSuggestion/click',
-    (client, state) =>
-      client.logSelectDocumentSuggestion({
+  makeCaseAssistAnalyticsAction({
+    prefix: 'analytics/caseAssist/documentSuggestion/click',
+    __legacy__getBuilder: (client, state) => {
+      return client.logSelectDocumentSuggestion({
         suggestion: caseAssistDocumentSuggestionSelector(
           state,
           suggestionId,
           true
         ),
         ticket: caseAssistCaseSelector(state),
-      })
-  );
+      });
+    },
+    analyticsType: 'CaseAssist.DocumentSuggestionClick',
+    analyticsPayloadBuilder: (state): ItemClick => {
+      const {
+        suggestion,
+        responseId,
+        suggestionId: uniqueId,
+      } = caseAssistDocumentSuggestionSelector(state, suggestionId, true);
+      return {
+        searchUid: responseId,
+        position: suggestion.documentPosition,
+        actionCause: 'preview',
+        itemMetadata: {
+          uniqueFieldName: 'uniqueId',
+          uniqueFieldValue: uniqueId,
+          title: suggestion.documentTitle,
+          url: suggestion.documentUrl,
+        },
+      };
+    },
+  });
 
 export const logDocumentSuggestionOpen = (
   suggestionId: string
 ): CaseAssistAction =>
-  makeCaseAssistAnalyticsAction(
-    'analytics/caseAssist/documentSuggestion/click',
-    (client, state) =>
-      client.logSelectDocumentSuggestion({
+  makeCaseAssistAnalyticsAction({
+    prefix: 'analytics/caseAssist/documentSuggestion/click',
+    __legacy__getBuilder: (client, state) => {
+      return client.logSelectDocumentSuggestion({
         suggestion: caseAssistDocumentSuggestionSelector(
           state,
           suggestionId,
@@ -140,19 +234,50 @@ export const logDocumentSuggestionOpen = (
           true
         ),
         ticket: caseAssistCaseSelector(state),
-      })
-  );
+      });
+    },
+    analyticsType: 'ItemClick',
+    analyticsPayloadBuilder: (state): ItemClick => {
+      const {
+        suggestion,
+        responseId,
+        suggestionId: uniqueId,
+      } = caseAssistDocumentSuggestionSelector(state, suggestionId, true);
+      return {
+        searchUid: responseId,
+        position: suggestion.documentPosition,
+        actionCause: 'open',
+        itemMetadata: {
+          uniqueFieldName: 'uniqueId',
+          uniqueFieldValue: uniqueId,
+          title: suggestion.documentTitle,
+          url: suggestion.documentUrl,
+        },
+      };
+    },
+  });
 
 export const logDocumentSuggestionRating = (
   suggestionId: string,
   rating: number
 ): CaseAssistAction =>
-  makeCaseAssistAnalyticsAction(
-    'analytics/caseAssist/documentSuggestion/rate',
-    (client, state) =>
-      client.logRateDocumentSuggestion({
+  makeCaseAssistAnalyticsAction({
+    prefix: 'analytics/caseAssist/documentSuggestion/rate',
+    __legacy__getBuilder: (client, state) => {
+      return client.logRateDocumentSuggestion({
         rating,
         suggestion: caseAssistDocumentSuggestionSelector(state, suggestionId),
         ticket: caseAssistCaseSelector(state),
-      })
-  );
+      });
+    },
+    analyticsType: 'CaseAssist.DocumentSuggestionFeedback',
+    analyticsPayloadBuilder: (state): CaseAssist.DocumentSuggestionFeedback => {
+      return {
+        documentSuggestion: {
+          id: suggestionId,
+          responseId: state.documentSuggestion?.status?.lastResponseId || '',
+        },
+        liked: !!rating,
+      };
+    },
+  });
