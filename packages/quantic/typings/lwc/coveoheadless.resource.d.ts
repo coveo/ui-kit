88e47cbf7a91147635adb097ff27d76b0549b351
mmitiche <mmitiche@coveo.com>
2024-04-03 08:35:44 -0400

    new type declared

--- packages/quantic/typings/lwc/coveoheadless.resource.d.ts
+++ packages/quantic/typings/lwc/coveoheadless.resource.d.ts
@@ -7,3 +7,8 @@ declare module '@salesforce/resourceUrl/coveobueno' {
   var coveobueno: string;
   export default coveobueno;
 }
+
+declare module '@salesforce/resourceUrl/abortcontrollerpolyfill' {
+  var abortcontrollerpolyfill: string;
+  export default abortcontrollerpolyfill;
+}
