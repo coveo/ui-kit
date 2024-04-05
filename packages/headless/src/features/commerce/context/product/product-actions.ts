b6ea3d6e11517a8435291c70efa1b9d9e84b3e08
Nicholas-David Labarre <ndlabarre@coveo.com>
2024-04-05 10:39:15 -0400

    use currency selector

--- packages/headless/src/features/commerce/context/product/product-actions.ts
+++ packages/headless/src/features/commerce/context/product/product-actions.ts
@@ -2,6 +2,7 @@ import {Ec, Product} from '@coveo/relay-event-types';
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {AsyncThunkCommerceOptions} from '../../../../api/commerce/commerce-api-client';
 import {CommerceEngineState} from '../../../../app/commerce-engine/commerce-engine';
+import {getCurrency} from '../context-selector';
 
 export const productView = createAsyncThunk<
   void,
@@ -9,7 +10,7 @@ export const productView = createAsyncThunk<
   AsyncThunkCommerceOptions<CommerceEngineState>
 >('commerce/product/view', async (product: Product, {extra, getState}) => {
   const {relay} = extra;
-  const {currency} = getState().commerceContext;
+  const currency = getCurrency(getState().commerceContext);
   const payload: Ec.ProductView = {currency, product};
 
   relay.emit('ec.productView', payload);
@@ -25,7 +26,7 @@ export const productClick = createAsyncThunk<
   'commerce/product/click',
   async (clickPayload: ProductClickPayload, {extra, getState}) => {
     const {relay} = extra;
-    const {currency} = getState().commerceContext;
+    const currency = getCurrency(getState().commerceContext);
     const payload = {currency, ...clickPayload};
 
     relay.emit('ec.productClick', payload);
