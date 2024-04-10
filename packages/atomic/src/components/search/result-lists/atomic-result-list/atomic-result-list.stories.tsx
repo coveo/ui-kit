e6bb2ebaf17d182a50db60140ce5f85c01c72807
Louis Bompart <lbompart@coveo.com>
2024-04-09 23:15:03 -0400

    remove other (subset)

--- packages/atomic/src/components/search/result-lists/atomic-result-list/atomic-result-list.stories.tsx
+++
@@ -1,9 +0,0 @@
-import defaultStory from 'atomic-storybook/default-story';
-
-const {defaultModuleExport, exportedStory} = defaultStory(
-  'Atomic/ResultList',
-  'atomic-result-list',
-  {}
-);
-export default {...defaultModuleExport, title: 'Atomic/ResultList'};
-export const Default = exportedStory;
--- packages/atomic/src/components/search/result-template-components/atomic-result-date/atomic-result-date.stories.tsx
+++
@@ -1,9 +0,0 @@
-import defaultResultComponentStory from 'atomic-storybook/default-result-component-story';
-
-const {defaultModuleExport, exportedStory} = defaultResultComponentStory(
-  'Atomic/ResultList/ResultDate',
-  'atomic-result-date',
-  {}
-);
-export default {...defaultModuleExport, title: 'Atomic/ResultList/ResultDate'};
-export const Default = exportedStory;
