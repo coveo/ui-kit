dd1715a307410e179fac676a8e1d2e92b32c5095
Nicholas-David Labarre <ndlabarre@coveo.com>
2024-04-25 10:26:19 -0400

    use facet generator as sub-controller

--- packages/headless/src/controllers/commerce/core/sub-controller/headless-sub-controller.ts
+++ packages/headless/src/controllers/commerce/core/sub-controller/headless-sub-controller.ts
@@ -2,7 +2,16 @@ import {
   CommerceEngine,
   CommerceEngineState,
 } from '../../../../app/commerce-engine/commerce-engine';
+import {AnyFacetResponse} from '../../../../features/commerce/facets/facet-set/interfaces/response';
 import {FetchResultsActionCreator} from '../common';
+import {buildCategoryFacet} from '../facets/category/headless-commerce-category-facet';
+import {buildCommerceDateFacet} from '../facets/date/headless-commerce-date-facet';
+import {
+  buildFacetGenerator,
+  FacetGenerator,
+} from '../facets/generator/headless-commerce-facet-generator';
+import {buildCommerceNumericFacet} from '../facets/numeric/headless-commerce-numeric-facet';
+import {buildCommerceRegularFacet} from '../facets/regular/headless-commerce-regular-facet';
 import {
   buildCorePagination,
   Pagination,
@@ -24,21 +33,36 @@ export interface BaseSolutionTypeSubControllers {
   pagination: (props?: PaginationProps) => Pagination;
 }
 
-export type SearchAndListingSubControllers = BaseSolutionTypeSubControllers & {
+export interface SearchAndListingSubControllers
+  extends BaseSolutionTypeSubControllers {
   sort: (props?: SortProps) => Sort;
-};
+  facetGenerator: () => FacetGenerator;
+}
 
-interface SubControllerProps {
+interface BaseSubControllerProps {
   responseIdSelector: (state: CommerceEngineState) => string;
   fetchResultsActionCreator: FetchResultsActionCreator;
   slotId?: string;
 }
 
+export interface SearchAndListingSubControllerProps
+  extends BaseSubControllerProps {
+  facetResponseSelector: (
+    state: CommerceEngine['state'],
+    facetId: string
+  ) => AnyFacetResponse | undefined;
+  isFacetLoadingResponseSelector: (state: CommerceEngine['state']) => boolean;
+}
+
 export function buildSolutionTypeSubControllers(
   engine: CommerceEngine,
-  subControllerProps: SubControllerProps
+  subControllerProps: SearchAndListingSubControllerProps
 ): SearchAndListingSubControllers {
-  const {fetchResultsActionCreator} = subControllerProps;
+  const {
+    fetchResultsActionCreator,
+    facetResponseSelector,
+    isFacetLoadingResponseSelector,
+  } = subControllerProps;
   return {
     ...buildBaseSolutionTypeControllers(engine, subControllerProps),
     sort(props?: SortProps) {
@@ -47,12 +71,29 @@ export function buildSolutionTypeSubControllers(
         fetchResultsActionCreator,
       });
     },
+    facetGenerator() {
+      const commonOptions = {
+        fetchResultsActionCreator,
+        facetResponseSelector,
+        isFacetLoadingResponseSelector,
+      };
+      return buildFacetGenerator(engine, {
+        buildRegularFacet: (_engine, options) =>
+          buildCommerceRegularFacet(engine, {...options, ...commonOptions}),
+        buildNumericFacet: (_engine, options) =>
+          buildCommerceNumericFacet(engine, {...options, ...commonOptions}),
+        buildDateFacet: (_engine, options) =>
+          buildCommerceDateFacet(engine, {...options, ...commonOptions}),
+        buildCategoryFacet: (_engine, options) =>
+          buildCategoryFacet(engine, {...options, ...commonOptions}),
+      });
+    },
   };
 }
 
 export function buildBaseSolutionTypeControllers(
   engine: CommerceEngine,
-  subControllerProps: SubControllerProps
+  subControllerProps: BaseSubControllerProps
 ): BaseSolutionTypeSubControllers {
   const {responseIdSelector, fetchResultsActionCreator, slotId} =
     subControllerProps;
