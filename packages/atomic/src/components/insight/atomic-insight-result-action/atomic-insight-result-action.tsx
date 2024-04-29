02b98cf7a64229112d211d62ac47100c2d7d5be1
Michael Grondines <mgrondines@coveo.com>
2024-04-29 14:58:05 -0400

    Added icons from SIS to easy setup

--- packages/atomic/src/components/insight/atomic-insight-result-action/atomic-insight-result-action.tsx
+++ packages/atomic/src/components/insight/atomic-insight-result-action/atomic-insight-result-action.tsx
@@ -1,6 +1,10 @@
 import {Result} from '@coveo/headless';
 import {Component, Event, EventEmitter, Prop, State, h} from '@stencil/core';
-import Quickview from '../../../images/quickview.svg';
+import AttachIcon from '../../../images/attach.svg';
+import CopyIcon from '../../../images/copy-dark.svg';
+import EmailIcon from '../../../images/email.svg';
+import QuickviewIcon from '../../../images/preview.svg';
+import FeedIcon from '../../../images/share-post.svg';
 import {
   InitializableComponent,
   InitializeBindings,
@@ -12,6 +16,14 @@ import {ResultContext} from '../../search/result-template-components/result-temp
 export interface InsightResultActionClickedEvent {
   action: string;
   result: Result;
+}
+
+enum Actions {
+  CopyToClipboard = 'copyToClipboard',
+  AttachToCase = 'attachToCase',
+  Quickview = 'quickview',
+  PostToFeed = 'postToFeed',
+  SendAsEmail = 'sendAsEmail',
 }
 
 /**
@@ -37,7 +49,7 @@ export class AtomicInsightResultAction implements InitializableComponent {
   /**
    * Specify the result action icon to display.
    */
-  @Prop({mutable: true}) public icon = Quickview;
+  @Prop({mutable: true}) public icon = '';
 
   /**
    * The text tooltip to show on the result action icon.
@@ -53,12 +65,33 @@ export class AtomicInsightResultAction implements InitializableComponent {
     this.actionClicked.emit({action: this.action, result: this.result});
   }
 
+  private getIcon() {
+    if (this.icon) {
+      return this.icon;
+    }
+
+    switch (this.action) {
+      case Actions.CopyToClipboard:
+        return CopyIcon;
+      case Actions.AttachToCase:
+        return AttachIcon;
+      case Actions.Quickview:
+        return QuickviewIcon;
+      case Actions.PostToFeed:
+        return FeedIcon;
+      case Actions.SendAsEmail:
+        return EmailIcon;
+      default:
+        return QuickviewIcon;
+    }
+  }
+
   public render() {
     return (
       <IconButton
         partPrefix="result-action"
         style="outline-neutral"
-        icon={this.icon}
+        icon={this.getIcon()}
         title={this.tooltip}
         onClick={() => this.onClick()}
       />
