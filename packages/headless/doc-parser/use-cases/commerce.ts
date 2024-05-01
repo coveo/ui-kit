0098fcea64d1d85b84903888fe39b87ab3cb428d
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2024-05-01 14:28:36 -0400

    first pass
    https://coveord.atlassian.net/browse/KIT-3141

--- packages/headless/doc-parser/use-cases/commerce.ts
+++ packages/headless/doc-parser/use-cases/commerce.ts
@@ -2,7 +2,116 @@ import {ActionLoaderConfiguration} from '../src/headless-export-resolvers/action
 import {ControllerConfiguration} from '../src/headless-export-resolvers/controller-resolver';
 import {EngineConfiguration} from '../src/headless-export-resolvers/engine-resolver';
 
-const controllers: ControllerConfiguration[] = [];
+const controllers: ControllerConfiguration[] = [
+  {
+    initializer: 'buildContext',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildProductListing',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildCart',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildProductListingBreadcrumbManager',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildProductListingCategoryFacet',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildProductListingDateFacet',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildProductListingFacetGenerator',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildProductListingNumericFacet',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildProductListingRegularFacet',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildProductListingPagination',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildProductListingParameterManager',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildProductListingSort',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildProductListingUrlManager',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildProductView',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildRecommendations',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildSearch',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildSearchBreadcrumbManager',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildSearchCategoryFacet',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildSearchDateFacet',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildSearchFacetGenerator',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildSearchNumericFacet',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildSearchRegularFacet',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildSearchPagination',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildSearchParameterManager',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildSearchSort',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildSearchUrlManager',
+    samplePaths: {},
+  },
+  {
+    initializer: 'buildSearchBox',
+    samplePaths: {},
+  },
+];
 
 const actionLoaders: ActionLoaderConfiguration[] = [];
 
