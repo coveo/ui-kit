9aa8a007bf8c13f05efa9e3f268ed1b74d0d569b
ylakhdar <ylakhdar@coveo.com>
2024-05-06 22:34:05 -0400

    feat(Commerce Atomic): add recent queries for commerce searchbox (#3878)
    https://coveord.atlassian.net/browse/KIT-3130
    
    ---------
    
    Co-authored-by: GitHub Actions Bot <>
    Co-authored-by: Felix Perron-Brault <fpbrault@coveo.com>
    Co-authored-by: Frederic Beaudoin <fbeaudoin@coveo.com>

--- packages/headless/src/controllers/commerce/recent-queries-list/headless-recent-queries-list.ts
+++ packages/headless/src/controllers/commerce/recent-queries-list/headless-recent-queries-list.ts
@@ -185,6 +185,6 @@ export function buildRecentQueriesList(
 function loadRecentQueriesListReducer(
   engine: CommerceEngine
 ): engine is CommerceEngine<RecentQueriesSection> {
-  engine.addReducers({search, recentQueries});
+  engine.addReducers({search: search, recentQueries});
   return true;
 }
