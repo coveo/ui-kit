2219c1092ce3c7a286eb6eb6a3a58c641fcc2b30
ylakhdar <ylakhdar@coveo.com>
2024-05-08 13:20:03 -0400

    fix comment
    https://coveord.atlassian.net/browse/KIT-3132

--- packages/atomic/src/components/commerce/search-box-suggestions/atomic-commerce-search-box-instant-products/atomic-commerce-search-box-instant-products.tsx
+++ packages/atomic/src/components/commerce/search-box-suggestions/atomic-commerce-search-box-instant-products/atomic-commerce-search-box-instant-products.tsx
@@ -45,7 +45,7 @@ function buildInteractiveInstantProduct(
 /**
  * The `atomic-commerce-search-box-instant-products` component can be added as a child of an `atomic-search-box` component, allowing for the configuration of instant results behavior.
  *
- * ,,This component does not support accessibility out-of-the-box. To do so, see [Instant Results Accessibility](https://docs.coveo.com/en/atomic/latest/usage/accessibility/#instant-results-accessibility).
+ * This component does not support accessibility out-of-the-box. To do so, see [Instant Results Accessibility](https://docs.coveo.com/en/atomic/latest/usage/accessibility/#instant-results-accessibility).
  *
  * This component is not supported on mobile.
  *
