ab71f68e4123d2e1f0808a99f34f316a84138f16
Felix Perron-Brault <fpbrault@coveo.com>
2024-05-08 12:18:12 -0400

    improve default template
    https://coveord.atlassian.net/browse/KIT-3054

--- packages/atomic/src/components/commerce/product-list/product-template-provider.ts
+++ packages/atomic/src/components/commerce/product-list/product-template-provider.ts
@@ -6,16 +6,33 @@ import {
 
 function defaultTemplate() {
   const content = document.createDocumentFragment();
+
   const linkEl = document.createElement('atomic-product-link');
-  const descEl = document.createElement('atomic-product-description');
-  const priceEl = document.createElement('atomic-product-price');
-  const imgEl = document.createElement('atomic-product-image');
+  linkEl.className = 'font-bold';
+
+  const brandEl = document.createElement('atomic-product-text');
+  brandEl.setAttribute('field', 'ec_brand');
+  brandEl.className = 'block text-neutral-dark';
+
+  const imgEl = document.createElement('atomic-result-image');
   imgEl.setAttribute('field', 'ec_thumbnails');
+
+  const ratingEl = document.createElement('atomic-result-rating');
+  ratingEl.setAttribute('field', 'ec_rating');
+
+  const priceEl = document.createElement('atomic-product-price');
   priceEl.setAttribute('currency', 'USD');
+  priceEl.className = 'text-2xl';
+
+  const descEl = document.createElement('atomic-product-description');
+
   content.appendChild(linkEl);
+  content.appendChild(brandEl);
   content.appendChild(imgEl);
-  content.appendChild(descEl);
+  content.appendChild(ratingEl);
   content.appendChild(priceEl);
+  content.appendChild(descEl);
+
   return {
     content,
     conditions: [],
