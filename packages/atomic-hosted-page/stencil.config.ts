7af79aa7c76d16bb943c7f4b833e52b01f291010
Louis Bompart <lbompart@coveo.com>
2024-05-15 01:54:54 -0400

    atomic-hosted

--- packages/atomic-hosted-page/stencil.config.ts
+++ packages/atomic-hosted-page/stencil.config.ts
@@ -1,4 +1,92 @@
 import {Config} from '@stencil/core';
+import {readFileSync} from 'node:fs';
+import {resolve} from 'node:path';
+
+function getHeadlessVersion(): string {
+  return JSON.parse(readFileSync(resolve(__dirname, 'package.json'), 'utf-8'))
+    .peerDependencies['@coveo/headless'];
+}
+function getBuenoVersion(): string {
+  return JSON.parse(readFileSync(resolve(__dirname, 'package.json'), 'utf-8'))
+    .peerDependencies['@coveo/bueno'];
+}
+
+function externalizeHeadless() {
+  switch (process.env.TARGET) {
+    case 'cdn':
+      return externalizeHeadlessForCDN();
+    case 'npm':
+    default:
+      return externalizeHeadlessForNPM();
+  }
+}
+
+function externalizeBueno() {
+  switch (process.env.TARGET) {
+    case 'cdn':
+      return externalizeBuenoForCDN();
+    case 'npm':
+    default:
+      return externalizeBuenoForNPM();
+  }
+}
+
+function externalizeBuenoForCDN() {
+  return {
+    name: 'externalize-bueno',
+    resolveId(id: string) {
+      if (id.startsWith('@coveo/bueno')) {
+        return {
+          id: `/bueno/v${getBuenoVersion()}${id.replace('@coveo/bueno', '')}/bueno.esm.js`,
+          external: 'absolute',
+        };
+      }
+    },
+  };
+}
+
+function externalizeBuenoForNPM() {
+  return {
+    name: 'externalize-headless',
+    resolveId(id: string) {
+      if (id.startsWith('@coveo/headless')) {
+        return {
+          id,
+          external: true,
+        };
+      }
+    },
+  };
+}
+
+function externalizeHeadlessForNPM() {
+  return {
+    name: 'externalize-headless',
+    resolveId(id: string) {
+      if (id.startsWith('@coveo/headless')) {
+        return {
+          id,
+          external: true,
+        };
+      }
+    },
+  };
+}
+
+function externalizeHeadlessForCDN() {
+  const headlessVersion = getHeadlessVersion();
+  return {
+    name: 'externalize-headless',
+    resolveId(id: string) {
+      if (id.startsWith('@coveo/headless')) {
+        return {
+          id: `/headless/v${headlessVersion}${id.replace('@coveo/headless', '')}/headless.esm.js`,
+          external: 'absolute',
+        };
+      }
+    },
+  };
+}
 
 export const config: Config = {
   namespace: 'atomic-hosted-page',
@@ -20,4 +108,5 @@ export const config: Config = {
       copy: [{src: 'pages', keepDirStructure: false}].filter((n) => n.src),
     },
   ],
+  plugins: [externalizeBueno(), externalizeHeadless()],
 };
