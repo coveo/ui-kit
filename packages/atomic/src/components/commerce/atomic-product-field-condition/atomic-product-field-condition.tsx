140458946b670922b17477045265da64ace39bd7
Louis Bompart <lbompart@coveo.com>
2024-05-21 13:51:48 -0400

    Update packages/atomic/src/components/commerce/atomic-product-field-condition/atomic-product-field-condition.tsx
    Co-authored-by: ylakhdar <ylakhdar@coveo.com>

--- packages/atomic/src/components/commerce/atomic-product-field-condition/atomic-product-field-condition.tsx
+++ packages/atomic/src/components/commerce/atomic-product-field-condition/atomic-product-field-condition.tsx
@@ -10,6 +10,7 @@ import {
 /**
  * The `atomic-product-field-condition` component takes a list of conditions that, if fulfilled, apply the template in which it's defined.
  * The condition properties can be based on any top-level product property of the `product` object, not restricted to fields (e.g., `ec_name`).
+ * @internal
  */
 @Component({
   tag: 'atomic-product-field-condition',
