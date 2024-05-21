2908e2b5c701e32569b0765f96e0be3836aade25
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2024-05-21 09:32:35 -0400

    missing back ticks
    https://coveord.atlassian.net/browse/KIT-3140

--- packages/headless/src/controllers/commerce/core/sub-controller/headless-sub-controller.ts
+++ packages/headless/src/controllers/commerce/core/sub-controller/headless-sub-controller.ts
@@ -55,15 +55,15 @@ import {
 export interface BaseSolutionTypeSubControllers {
   /**
    * Creates an `InteractiveProduct` sub-controller.
-   * @param props - The properties for the InteractiveProduct sub-controller.
-   * @returns An instance of the InteractiveProduct sub-controller.
+   * @param props - The properties for the `InteractiveProduct` sub-controller.
+   * @returns An instance of the `InteractiveProduct` sub-controller.
    */
   interactiveProduct(props: InteractiveProductProps): InteractiveProduct;
 
   /**
    * Creates a `Pagination` sub-controller.
-   * @param props - The optional properties for the Pagination sub-controller.
-   * @returns An instance of the Pagination sub-controller.
+   * @param props - The optional properties for the `Pagination` sub-controller.
+   * @returns An instance of the `Pagination` sub-controller.
    */
   pagination(props?: PaginationProps): Pagination;
 }
@@ -72,7 +72,7 @@ export interface SearchAndListingSubControllers
   extends BaseSolutionTypeSubControllers {
   /**
    * Creates a `Sort` controller with the specified properties.
-   * @param props - Optional properties for configuring the Sort controller.
+   * @param props - Optional properties for configuring the `Sort` controller.
    * @returns A `Sort` controller.
    */
   sort(props?: SortProps): Sort;
