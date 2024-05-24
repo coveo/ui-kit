2661cb963d43c3d5fcd7ec87655a41870a1a970c
Louis Bompart <lbompart@coveo.com>
2024-05-24 17:35:22 -0400

    docs: migrate atomic-facet-manager

--- packages/atomic/src/components.d.ts
+++ packages/atomic/src/components.d.ts
@@ -694,7 +694,7 @@ export namespace Components {
         "min"?: string;
     }
     /**
-     * The `atomic-facet-manager` helps reorder facets and their values to match the most recent search response with the most relevant results. A facet component is slotted within an `atomic-facet-manager` to leverage this functionality.
+     * The `atomic-facet-manager` helps reorder facets and their values to match the most recent search response with the most relevant results.
      */
     interface AtomicFacetManager {
         /**
@@ -3507,7 +3507,7 @@ declare global {
         new (): HTMLAtomicFacetDateInputElement;
     };
     /**
-     * The `atomic-facet-manager` helps reorder facets and their values to match the most recent search response with the most relevant results. A facet component is slotted within an `atomic-facet-manager` to leverage this functionality.
+     * The `atomic-facet-manager` helps reorder facets and their values to match the most recent search response with the most relevant results.
      */
     interface HTMLAtomicFacetManagerElement extends Components.AtomicFacetManager, HTMLStencilElement {
     }
@@ -5798,7 +5798,7 @@ declare namespace LocalJSX {
         "onAtomic/dateInputApply"?: (event: AtomicFacetDateInputCustomEvent<any>) => void;
     }
     /**
-     * The `atomic-facet-manager` helps reorder facets and their values to match the most recent search response with the most relevant results. A facet component is slotted within an `atomic-facet-manager` to leverage this functionality.
+     * The `atomic-facet-manager` helps reorder facets and their values to match the most recent search response with the most relevant results.
      */
     interface AtomicFacetManager {
         /**
@@ -8408,7 +8408,7 @@ declare module "@stencil/core" {
              */
             "atomic-facet-date-input": LocalJSX.AtomicFacetDateInput & JSXBase.HTMLAttributes<HTMLAtomicFacetDateInputElement>;
             /**
-             * The `atomic-facet-manager` helps reorder facets and their values to match the most recent search response with the most relevant results. A facet component is slotted within an `atomic-facet-manager` to leverage this functionality.
+             * The `atomic-facet-manager` helps reorder facets and their values to match the most recent search response with the most relevant results.
              */
             "atomic-facet-manager": LocalJSX.AtomicFacetManager & JSXBase.HTMLAttributes<HTMLAtomicFacetManagerElement>;
             /**
---
+++ packages/atomic/src/components/search/atomic-facet-manager/atomic-facet-manager.new.stories.tsx
@@ -0,0 +1,59 @@
+import {renderComponent} from '@coveo/atomic/storybookUtils/render-component';
+import {wrapInSearchInterface} from '@coveo/atomic/storybookUtils/search-interface-wrapper';
+import type {Meta, StoryObj} from '@storybook/web-components';
+import {html} from 'lit/static-html.js';
+
+const {decorator, play} = wrapInSearchInterface();
+
+const meta: Meta = {
+  component: 'atomic-facet-manager',
+  title: 'Atomic/Breadbox',
+  id: 'atomic-facet-manager',
+
+  render: renderComponent,
+  decorators: [decorator],
+  parameters: {
+    controls: {expanded: true, hideNoControlsWarning: true},
+  },
+  play,
+  argTypes: {
+    default: {
+      control: false,
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const FirstStory: Story = {
+  name: 'atomic-facet-manager',
+  decorators: [
+    (story) => html`
+      <style>
+        atomic-facet-manager {
+          width: 500px;
+          margin: auto;
+          display: block;
+        }
+      </style>
+      ${story()}
+    `,
+  ],
+  args: {
+    default: `
+      <atomic-facet field="author" label="Authors"></atomic-facet>
+      <atomic-facet field="language" label="Language"></atomic-facet>
+      <atomic-facet
+        field="objecttype"
+        label="Type"
+        display-values-as="link"
+      ></atomic-facet>
+      <atomic-facet
+        field="year"
+        label="Year"
+        display-values-as="box"
+      ></atomic-facet>
+    `,
+  },
+};
