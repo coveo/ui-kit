ffbfaafda17fb75bcfa0157674df7eb1606fef42
Louis Bompart <lbompart@coveo.com>
2024-05-25 03:06:15 -0400

    docs: migrate result lists

---
+++ packages/atomic/src/components/search/result-lists/atomic-result-list/atomic-result-list.new.stories.tsx
@@ -0,0 +1,24 @@
+import {renderComponent} from '@coveo/atomic/storybookUtils/render-component';
+import {wrapInSearchInterface} from '@coveo/atomic/storybookUtils/search-interface-wrapper';
+import type {Meta, StoryObj} from '@storybook/web-components';
+
+const {decorator, play} = wrapInSearchInterface();
+const meta: Meta = {
+  component: 'atomic-result-list',
+  title: 'Atomic/ResultList',
+  id: 'atomic-result-list',
+
+  render: renderComponent,
+  decorators: [decorator],
+  parameters: {
+    controls: {expanded: true, hideNoControlsWarning: true},
+  },
+  play,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {
+  name: 'atomic-result-list',
+};
