384a3f0ca1843651a466a0a5037d41812faf4d11
Olivier Lamothe <olamothe@coveo.com>
2024-05-27 11:49:15 -0400

    fix(atomic): remove query syntax option from commerce search box (#3972)
    This is not something that is supported (or would even make sense to
    support) for commerce API
    https://coveord.atlassian.net/browse/KIT-3168

--- packages/atomic/src/components.d.ts
+++ packages/atomic/src/components.d.ts
@@ -530,10 +530,6 @@ export namespace Components {
          */
         "disableSearch": boolean;
         /**
-          * Whether to interpret advanced [Coveo Cloud query syntax](https://docs.coveo.com/en/1814/) in the query. You should only enable query syntax in the search box if you have good reasons to do so, as it requires end users to be familiar with Coveo Cloud query syntax, otherwise they will likely be surprised by the search box behaviour.  When the `redirection-url` property is set and redirects to a page with more `atomic-commerce-search-box` components, all `atomic-commerce-search-box` components need to have the same `enable-query-syntax` value.
-         */
-        "enableQuerySyntax": boolean;
-        /**
           * The minimum query length required to enable search. For example, to disable the search for empty queries, set this to `1`.
          */
         "minimumQueryLength": number;
@@ -5743,10 +5739,6 @@ declare namespace LocalJSX {
           * Whether to prevent the user from triggering searches and query suggestions from the component. Perfect for use cases where you need to disable the search conditionally. For the specific case when you need to disable the search based on the length of the query, refer to {@link minimumQueryLength}.
          */
         "disableSearch"?: boolean;
-        /**
-          * Whether to interpret advanced [Coveo Cloud query syntax](https://docs.coveo.com/en/1814/) in the query. You should only enable query syntax in the search box if you have good reasons to do so, as it requires end users to be familiar with Coveo Cloud query syntax, otherwise they will likely be surprised by the search box behaviour.  When the `redirection-url` property is set and redirects to a page with more `atomic-commerce-search-box` components, all `atomic-commerce-search-box` components need to have the same `enable-query-syntax` value.
-         */
-        "enableQuerySyntax"?: boolean;
         /**
           * The minimum query length required to enable search. For example, to disable the search for empty queries, set this to `1`.
          */
