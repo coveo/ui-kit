0b7bf7f03d81afc062c3dd5445c10604832730bb
Louis Bompart <lbompart@coveo.com>
2024-05-27 10:35:35 -0400

    docs: migrate atomic-timeframe (#3997)
    Closes #3996 
    
    This pull request introduces a new story for the `atomic-timeframe`
    component, following the migration pattern established with the
    `atomic-numeric-range` component. Additionally, it removes the old story
    file for `atomic-timeframe`.
    
    - **Adds a new story file** `atomic-timeframe.new.stories.tsx` which
    includes:
      - A comprehensive example wrapped in a search interface.
      - Interactive steps for demonstration purposes using `play` function.
    - **Deletes the old story file** `atomic-timeframe.stories.tsx` to align
    with the new story format and structure.
    
    
    ---
    
    For more details, open the [Copilot Workspace
    session](https://copilot-workspace.githubnext.com/coveo/ui-kit/pull/3995?shareId=b67914aa-f029-4a39-a604-061972c1c663).

---
+++ packages/atomic/src/components/common/atomic-timeframe/atomic-timeframe.new.stories.tsx
@@ -0,0 +1,31 @@
+import {renderComponent} from '@coveo/atomic/storybookUtils/render-component';
+import {wrapInSearchInterface} from '@coveo/atomic/storybookUtils/search-interface-wrapper';
+import type {Meta, StoryObj as Story} from '@storybook/web-components';
+import {html} from 'lit/static-html.js';
+
+const {decorator, play} = wrapInSearchInterface();
+
+const meta: Meta = {
+  component: 'atomic-timeframe',
+  title: 'Atomic/TimeframeFacet/Timeframe',
+  id: 'atomic-timeframe',
+
+  render: renderComponent,
+  decorators: [decorator],
+  parameters: {
+    controls: {expanded: true, hideNoControlsWarning: true},
+  },
+  play,
+};
+
+export default meta;
+
+export const Default: Story = {
+  name: 'atomic-timeframe',
+  args: {unit: 'year'},
+  decorators: [
+    (story) => html`
+      <atomic-timeframe-facet field="date"> ${story()} </atomic-timeframe-facet>
+    `,
+  ],
+};
