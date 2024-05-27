e3ca3dc7a544aaddb612bf56e3bc2a97d79a48f5
Louis Bompart <lbompart@coveo.com>
2024-05-27 10:26:35 -0400

    docs: migrate atomic-numeric-range (#3995)
    Closes #3994

---
+++ packages/atomic/src/components/common/atomic-numeric-range/atomic-numeric-range.new.stories.tsx
@@ -0,0 +1,36 @@
+import {renderComponent} from '@coveo/atomic/storybookUtils/render-component';
+import {wrapInSearchInterface} from '@coveo/atomic/storybookUtils/search-interface-wrapper';
+import type {Meta, StoryObj} from '@storybook/web-components';
+import {html} from 'lit/static-html.js';
+
+const {decorator, play} = wrapInSearchInterface();
+
+const meta: Meta = {
+  component: 'atomic-numeric-range',
+  title: 'Atomic/NumericFacet/Range',
+  id: 'atomic-numeric-range',
+
+  render: renderComponent,
+  decorators: [decorator],
+  parameters: {
+    controls: {expanded: true, hideNoControlsWarning: true},
+  },
+  play,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const FirstStory: Story = {
+  name: 'atomic-numeric-range',
+  args: {start: 0, end: 1000},
+  decorators: [
+    (story) => html`  
+        <atomic-numeric-facet
+          field="ytviewcount"
+        >
+        ${story()}
+        </atomic-facet>
+    `,
+  ],
+};
