5724d858d5b59d69754b6070f3ee39bc9a62e3f2
Louis Bompart <lbompart@coveo.com>
2024-05-27 14:17:46 +0000

    lint

--- packages/atomic/src/components/common/atomic-numeric-range/atomic-numeric-range.new.stories.tsx
+++ packages/atomic/src/components/common/atomic-numeric-range/atomic-numeric-range.new.stories.tsx
@@ -1,9 +1,7 @@
 import {renderComponent} from '@coveo/atomic/storybookUtils/render-component';
 import {wrapInSearchInterface} from '@coveo/atomic/storybookUtils/search-interface-wrapper';
-import {userEvent, waitFor, expect} from '@storybook/test';
 import type {Meta, StoryObj} from '@storybook/web-components';
 import {html} from 'lit/static-html.js';
-import {within} from 'shadow-dom-testing-library';
 
 const {decorator, play} = wrapInSearchInterface();
 
