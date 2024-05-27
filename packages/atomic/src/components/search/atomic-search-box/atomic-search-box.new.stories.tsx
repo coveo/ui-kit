db5f35dd068bcbce9e28af9de870baca653c4f18
Louis Bompart <lbompart@coveo.com>
2024-05-27 10:19:22 -0400

    docs: migrate atomic-search-box story (#3985)
    Co-authored-by: Frederic Beaudoin <fbeaudoin@coveo.com>

--- packages/atomic/src/components/search/atomic-search-box/atomic-search-box.new.stories.tsx
+++ packages/atomic/src/components/search/atomic-search-box/atomic-search-box.new.stories.tsx
@@ -1,6 +1,6 @@
 import {renderComponent} from '@coveo/atomic/storybookUtils/render-component';
 import {wrapInSearchInterface} from '@coveo/atomic/storybookUtils/search-interface-wrapper';
-import type {Meta, StoryObj} from '@storybook/web-components';
+import type {Meta, StoryObj as Story} from '@storybook/web-components';
 
 const {decorator, play} = wrapInSearchInterface();
 
@@ -17,8 +17,7 @@ const meta: Meta = {
 };
 
 export default meta;
-type Story = StoryObj;
 
-export const FirstStory: Story = {
+export const Default: Story = {
   name: 'atomic-search-box',
 };
