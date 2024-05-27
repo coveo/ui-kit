acf00f61fe29a95621cb5252d42b07e8191dfec8
Louis Bompart <lbompart@coveo.com>
2024-05-27 17:25:18 -0400

    refactor plugin

--- packages/atomic/scripts/cem-plugin.mjs
+++ packages/atomic/scripts/cem-plugin.mjs
@@ -1,58 +1,55 @@
-export function cemPlugin() {
-  // Write a custom plugin
-  return {
-    // Make sure to always give your plugins a name, this helps when debugging
-    name: 'my-plugin',
-    // Runs before analysis starts
-    initialize({ts, customElementsManifest, context}) {},
-    // Runs for all modules in a project, before continuing to the analyzePhase
-    collectPhase({ts, node, context}) {},
-    // Runs for each module
-    analyzePhase({ts, node, moduleDoc, context}) {},
-    // Runs for each module, after analyzing, all information about your module should now be available
-    moduleLinkPhase({moduleDoc, context}) {
-      for (const declaration of moduleDoc.declarations) {
-        if (declaration.kind !== 'class') {
-          continue;
-        }
-        const keptAttributes = [];
-        for (const attribute of declaration.attributes || []) {
-          const memberLike = declaration.members.find(
-            (member) => attribute.fieldName === member.name
-          );
-          if (!memberLike || memberLike.privacy === 'private') {
-            continue;
-          }
-          keptAttributes.push(attribute);
-          for (const fieldToCopy of ['type', 'default', 'description']) {
-            attribute[fieldToCopy] = declaration.members.find(
-              (member) => attribute.fieldName === member.name
-            )[fieldToCopy];
-          }
-        }
-        declaration.attributes =
-          keptAttributes.length > 0 ? keptAttributes : undefined;
-        declaration.members = [];
+const moduleLinkPhase = ({moduleDoc}) => {
+  for (const declaration of moduleDoc.declarations) {
+    if (declaration.kind !== 'class') {
+      continue;
+    }
+    const keptAttributes = [];
+    for (const attribute of declaration.attributes || []) {
+      const memberLike = findMember(declaration, attribute);
+      if (!memberLike || memberLike.privacy === 'private') {
+        continue;
       }
+      keptAttributes.push(attribute);
+      copyAttributesFromMember(attribute, memberLike);
+    }
+    declaration.attributes =
+      keptAttributes.length > 0 ? keptAttributes : undefined;
+    declaration.members = [];
+  }
 
-      const wrappedFinalExports = {exports: []};
-      const exportCandidates = {};
-      for (const exportCandidate of moduleDoc.exports) {
-        const exportName = exportCandidate.declaration.name;
-        if (!exportCandidates[exportName]) {
-          exportCandidates[exportName] = [];
-        } else if (
-          exportCandidates[exportName] !==
-          wrappedFinalExports.exports[exportName]
-        ) {
-          wrappedFinalExports.exports.push(...exportCandidates[exportName]);
-          exportCandidates[exportName] = wrappedFinalExports.exports;
-        }
-        exportCandidates[exportName].push(exportCandidate);
-      }
-      moduleDoc.exports = wrappedFinalExports.exports;
-    },
-    // Runs after modules have been parsed and after post-processing
-    packageLinkPhase({customElementsManifest, context}) {},
-  };
+  moduleDoc.exports = cleanExports(moduleDoc.exports);
+};
+
+function cleanExports(exports) {
+  const finalExports = [];
+  const exportCandidates = {};
+  for (const exportCandidate of exports) {
+    const exportName = exportCandidate.declaration.name;
+    if (!exportCandidates[exportName]) {
+      exportCandidates[exportName] = [];
+    } else if (exportCandidates[exportName] !== finalExports[exportName]) {
+      finalExports.push(...exportCandidates[exportName]);
+      exportCandidates[exportName] = finalExports;
+    }
+    exportCandidates[exportName].push(exportCandidate);
+  }
+  return finalExports;
 }
+
+function findMember(declaration, attribute) {
+  return declaration.members.find(
+    (member) => attribute.fieldName === member.name
+  );
+}
+
+function copyAttributesFromMember(attribute, member) {
+  for (const fieldToCopy of ['type', 'default', 'description']) {
+    attribute[fieldToCopy] = member[fieldToCopy];
+  }
+}
+
+export const cemPlugin = () => ({
+  name: 'my-plugin',
+  // Runs for each module, after analyzing, all information about your module should now be available
+  moduleLinkPhase,
+});
