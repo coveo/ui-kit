f6e789408ec8a51ff5599d37dfdf1638cea3b5f3
Sami Sayegh <ssayegh@coveo.com>
2024-05-28 18:14:25 -0400

    improve message

--- packages/headless/src/utils/utils.ts
+++ packages/headless/src/utils/utils.ts
@@ -49,9 +49,9 @@ const doNotTrackValues = new Set(['1', 1, 'yes', true]);
 /**
  * Logic copied from coveo.analytics.
  *
- * @deprecated Starting with Event Protocol, Coveo will no longer respect the outdated DNT standard.
- * Instead, we will provide implementers with documentation on privacy best-practices, and let
- * them decide which standards they want to respect.
+ * @deprecated Starting with Event Protocol, Coveo will no longer respect the DNT standard.
+ * Instead, we will provide implementers with documentation on privacy best-practices, letting
+ * them decide which standards to respect.
  * For more context behind the decision, see: https://coveord.atlassian.net/browse/LENS-1502
  */
 export function doNotTrack() {
