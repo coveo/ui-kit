b5a49c1cfed645ff7bcfb901fdb85b04788ceb05
Nicholas-David Labarre <ndlabarre@coveo.com>
2024-05-29 10:51:40 -0400

    load reducers in action loaders

--- packages/headless/src/features/commerce/product-listing-parameters/product-listing-parameters-actions-loader.ts
+++ packages/headless/src/features/commerce/product-listing-parameters/product-listing-parameters-actions-loader.ts
@@ -1,6 +1,10 @@
 import {PayloadAction} from '@reduxjs/toolkit';
 import {CommerceEngine} from '../../../app/commerce-engine/commerce-engine';
+import {facetOrderReducer as facetOrder} from '../../facets/facet-order/facet-order-slice';
+import {commerceFacetSetReducer as commerceFacetSet} from '../facets/facet-set/facet-set-slice';
+import {paginationReducer as commercePagination} from '../pagination/pagination-slice';
 import {Parameters} from '../parameters/parameters-actions';
+import {sortReducer as commerceSort} from '../sort/sort-slice';
 import {restoreProductListingParameters} from './product-listing-parameters-actions';
 
 /**
@@ -27,7 +31,12 @@ export interface ProductListingParametersActionCreators {
 export function loadProductListingParametersActions(
   engine: CommerceEngine
 ): ProductListingParametersActionCreators {
-  engine.addReducers({});
+  engine.addReducers({
+    commerceSort,
+    facetOrder,
+    commerceFacetSet,
+    commercePagination,
+  });
 
   return {
     restoreProductListingParameters,
--- packages/headless/src/features/commerce/search-parameters/search-parameters-actions-loader.ts
+++ packages/headless/src/features/commerce/search-parameters/search-parameters-actions-loader.ts
@@ -1,5 +1,10 @@
 import {PayloadAction} from '@reduxjs/toolkit';
 import {CommerceEngine} from '../../../app/commerce-engine/commerce-engine';
+import {facetOrderReducer as facetOrder} from '../../facets/facet-order/facet-order-slice';
+import {commerceFacetSetReducer as commerceFacetSet} from '../facets/facet-set/facet-set-slice';
+import {paginationReducer as commercePagination} from '../pagination/pagination-slice';
+import {queryReducer as query} from '../query/query-slice';
+import {sortReducer as commerceSort} from '../sort/sort-slice';
 import {
   CommerceSearchParameters,
   restoreSearchParameters,
@@ -29,7 +34,13 @@ export interface SearchParametersActionCreators {
 export function loadSearchParametersActions(
   engine: CommerceEngine
 ): SearchParametersActionCreators {
-  engine.addReducers({});
+  engine.addReducers({
+    query,
+    commerceSort,
+    facetOrder,
+    commerceFacetSet,
+    commercePagination,
+  });
 
   return {
     restoreSearchParameters,
