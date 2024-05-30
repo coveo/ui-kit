2c924cfb267a61fd779371d8c1fdcaa3ad6ddd26
Louis Bompart <lbompart@coveo.com>
2024-05-30 17:06:36 -0400

    test: atomic-commerce-search-box
    wip

---
+++ packages/atomic/src/components/commerce/atomic-commerce-search-box/e2e/pageObject.ts
@@ -0,0 +1,44 @@
+import {Page} from '@playwright/test';
+
+export class AtomicCommerceSearchBoxLocators {
+  private page: Page;
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  get submitButton() {
+    return this.page.getByLabel('Search', {exact: true});
+  }
+
+  get searchInput() {
+    return this.page.getByRole('combobox', {name: 'Search'});
+  }
+
+  searchSuggestions({
+    index,
+    total,
+    listSide,
+  }: {index?: number; total?: number; listSide?: 'Left' | 'Right'} = {}) {
+    return this.page.getByLabel(
+      new RegExp(
+        `suggested query\\. ${index ?? '\\d'} of ${total ?? '\\d'}\\.${this.listSideAffix(listSide)}`
+      )
+    );
+  }
+
+  instantResult({
+    index,
+    total,
+    listSide,
+  }: {index?: number; total?: number; listSide?: 'Left' | 'Right'} = {}) {
+    return this.page.getByLabel(
+      new RegExp(
+        `instant result\\. ${index ?? '\\d'} of ${total ?? '\\d'}\\.${this.listSideAffix(listSide)}`
+      )
+    );
+  }
+
+  private listSideAffix(listSide?: 'Left' | 'Right') {
+    return listSide ? ` In ${listSide} list\\.` : '';
+  }
+}
