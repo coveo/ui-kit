1787c8cae43993c263701956c9a16bc09a326bb2
Louis Bompart <lbompart@coveo.com>
2024-05-29 22:26:13 -0400

    derp

--- scripts/reports/build-live-sample/generate-live-sample-links.mjs
+++ scripts/reports/build-live-sample/generate-live-sample-links.mjs
@@ -1,4 +1,4 @@
 import {context} from '@actions/github';
 
 export const buildLiveExampleReport = async () => `
-* [Storybook](https://coveo.github.io/ui-kit-prs/${context.event.pull_request.number}/dist-storybook/)`;
+* [Storybook](https://coveo.github.io/ui-kit-prs/${context.event.payload.pull_request.number}/dist-storybook/)`;
