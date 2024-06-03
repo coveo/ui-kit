f7fd47582bffa0a6b71131e37e5392fbb3fb68cb
Frederic Beaudoin <fbeaudoin@coveo.com>
2024-06-03 07:32:35 -0400

    Clean up query actions loader
    https://coveord.atlassian.net/browse/KIT-3207

--- packages/headless/src/features/commerce/query/query-actions-loader.ts
+++ packages/headless/src/features/commerce/query/query-actions-loader.ts
@@ -3,12 +3,14 @@ import {CommerceEngine} from '../../../app/commerce-engine/commerce-engine';
 import {UpdateQueryActionCreatorPayload, updateQuery} from './query-actions';
 import {queryReducer as query} from './query-slice';
 
+export type {UpdateQueryActionCreatorPayload};
+
 /**
  * The query action creators.
  */
 export interface QueryActionCreators {
   /**
-   * Updates the basic query expression (`q`).
+   * Updates the basic query expression.
    * @param payload - The action creator payload.
    * @returns A dispatchable action.
    */
