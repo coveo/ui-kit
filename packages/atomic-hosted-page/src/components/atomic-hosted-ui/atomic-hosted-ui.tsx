004176da5cb5553c01239800c86e95ede9c14585
Louis Bompart <lbompart@coveo.com>
2024-06-04 17:23:28 -0400

    jsdoc & deprecation
    https://coveord.atlassian.net/browse/KIT-3004

--- packages/atomic-hosted-page/src/components/atomic-hosted-ui/atomic-hosted-ui.tsx
+++ packages/atomic-hosted-page/src/components/atomic-hosted-ui/atomic-hosted-ui.tsx
@@ -27,7 +27,6 @@ interface AtomicHostedUIInitializationOptions extends InitializationOptions {
 /**
  * A Web Component used to inject a Coveo Hosted Search Page in the DOM.
  * Pulls from the [Search Interfaces API](https://platform.cloud.coveo.com/docs?urls.primaryName=Search%20Interface%20Service#/)
- * @internal
  */
 @Component({
   tag: 'atomic-hosted-ui',
