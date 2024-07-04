9a6b3c9cc2439b4f732e65ba5ea410f0773258fc
Danny Gauthier <dgauthier@WKS-002169.corp.coveo.com>
2024-07-04 09:42:02 -0400

    feat(answer): error handling

--- packages/headless/src/features/generated-answer/generated-answer-selectors.ts
+++ packages/headless/src/features/generated-answer/generated-answer-selectors.ts
@@ -1,17 +1,23 @@
+import {createSelector} from '@reduxjs/toolkit';
 import {SearchAppState} from '../../state/search-app-state';
 import {GeneratedAnswerSection} from '../../state/state-sections';
 
-export function citationSourceSelector(
-  state: Partial<GeneratedAnswerSection>,
-  citationId: string
-) {
-  return state.generatedAnswer?.citations?.find(
-    (citation) => citation.id === citationId
-  );
-}
+export const citationSourceSelector = createSelector(
+  (state: Partial<GeneratedAnswerSection>, citationId: string) =>
+    state.generatedAnswer?.citations?.find(
+      (citation) => citation.id === citationId
+    ),
+  (citation) => citation
+);
 
-export function generativeQuestionAnsweringIdSelector(
-  state: Partial<SearchAppState>
-) {
-  return state.search?.response?.extendedResults?.generativeQuestionAnsweringId;
-}
+export const generativeQuestionAnsweringIdSelector = createSelector(
+  (state: Partial<SearchAppState>) =>
+    state.search?.response?.extendedResults?.generativeQuestionAnsweringId,
+  (generativeQuestionAnsweringId) => generativeQuestionAnsweringId
+);
+
+export const selectFieldsToIncludeInCitation = createSelector(
+  (state: Partial<GeneratedAnswerSection>) =>
+    state.generatedAnswer?.fieldsToIncludeInCitations,
+  (fieldsToInclude) => fieldsToInclude
+);
