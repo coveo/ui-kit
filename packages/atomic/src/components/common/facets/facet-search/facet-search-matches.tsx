b7d59460d71439aa1c3ac88a3e54087e3078d370
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-07-05 11:18:11 -0400

    tests for atomic-facet
    https://coveord.atlassian.net/browse/KIT-3361

--- packages/atomic/src/components/common/facets/facet-search/facet-search-matches.tsx
+++ packages/atomic/src/components/common/facets/facet-search/facet-search-matches.tsx
@@ -1,12 +1,14 @@
 import {FunctionalComponent, h} from '@stencil/core';
 import escape from 'escape-html';
 import {i18n} from 'i18next';
+import {Button} from '../../button';
 
 interface FacetSearchMatchesProps {
   i18n: i18n;
   query: string;
   numberOfMatches: number;
   hasMoreMatches: boolean;
+  showMoreMatches?: () => void;
 }
 
 function matchesFound(
@@ -15,7 +17,7 @@ function matchesFound(
   i18n: i18n
 ) {
   return i18n.t(key, {
-    query: `<span class="font-bold italic text-on-background" part="matches-query">${escape(
+    query: `<span class="font-bold italic" part="matches-query">${escape(
       query
     )}</span>`,
     interpolation: {escapeValue: false},
@@ -28,7 +30,6 @@ export const FacetSearchMatches: FunctionalComponent<
   if (!props.numberOfMatches) {
     return (
       <div class="px-2">
-        {/* file deepcode ignore ReactSetInnerHtml: This is not React code */}
         <div
           part="no-matches"
           class="truncate p-3 bg-neutral-light text-neutral-dark text-sm rounded"
@@ -43,6 +44,27 @@ export const FacetSearchMatches: FunctionalComponent<
   }
 
   if (props.hasMoreMatches) {
+    if (props.showMoreMatches) {
+      return (
+        <div class="px-2">
+          <Button
+            style="text-primary"
+            class="mt-3 p-2"
+            onClick={props.showMoreMatches}
+          >
+            <div
+              part="more-matches"
+              class="truncate text-sm"
+              innerHTML={matchesFound(
+                'more-matches-for',
+                props.query,
+                props.i18n
+              )}
+            ></div>
+          </Button>
+        </div>
+      );
+    }
     return (
       <div class="px-2">
         <div
