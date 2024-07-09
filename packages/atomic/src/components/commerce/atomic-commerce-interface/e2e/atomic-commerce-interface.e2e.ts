28e1320a9d555f1d29aaf2efa0a9c488a531b24b
Felix Perron-Brault <fpbrault@coveo.com>
2024-07-09 15:20:57 -0400

    improve tests
    https://coveord.atlassian.net/browse/KIT-3375

--- packages/atomic/src/components/commerce/atomic-commerce-interface/e2e/atomic-commerce-interface.e2e.ts
+++ packages/atomic/src/components/commerce/atomic-commerce-interface/e2e/atomic-commerce-interface.e2e.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @cspell/spellchecker */
 import {test, expect} from './fixture';
 
 test.describe('when search has not been initialized', () => {
@@ -34,10 +35,6 @@ test.describe('when search has not been initialized', () => {
   });
 });
 
-test.describe('when initializing the interface', () => {
-  test.describe("when the access token isn't a JWT", () => {});
-  test.describe('when the access token is JWT', () => {});
-});
 test.describe('when a query is performed automatically', () => {
   test.beforeEach(async ({page}) => {
     await page.goto(
@@ -53,7 +50,7 @@ test.describe('when a query is performed automatically', () => {
       await page.goto(
         'http://localhost:4400/iframe.html?id=atomic-commerce-interface--with-product-list&viewMode=story&args=attributes-language:fr'
       );
-      await expect(commerceInterface.interface()).toContainText('Résultats');
+      await expect(commerceInterface.interface()).toContainText('Produits');
     });
 
     test('should default to english when an invalid language is selected', async ({
@@ -63,7 +60,7 @@ test.describe('when a query is performed automatically', () => {
       await page.goto(
         'http://localhost:4400/iframe.html?id=atomic-commerce-interface--with-product-list&viewMode=story&args=attributes-language:foo'
       );
-      await expect(commerceInterface.interface()).toContainText('Results');
+      await expect(commerceInterface.interface()).toContainText('Products');
     });
 
     test('should default back to the non region locale (e.g., "es-ES" to "es")', async ({
@@ -73,7 +70,7 @@ test.describe('when a query is performed automatically', () => {
       await page.goto(
         'http://localhost:4400/iframe.html?id=atomic-commerce-interface--with-product-list&viewMode=story&args=attributes-language:es-ES'
       );
-      await expect(commerceInterface.interface()).toContainText('Resultados');
+      await expect(commerceInterface.interface()).toContainText('Productos');
     });
 
     test('should support changing the language of the interface after initialization', async ({
@@ -90,7 +87,7 @@ test.describe('when a query is performed automatically', () => {
         commerceInterfaceComponent.language = 'fr';
       });
 
-      await expect(commerceInterface.interface()).toContainText('Résultats');
+      await expect(commerceInterface.interface()).toContainText('Produits');
     });
     test('should revert to english when changing the language to an invalid value after initialization', async ({
       page,
@@ -109,12 +106,15 @@ test.describe('when a query is performed automatically', () => {
         commerceInterfaceComponent.language = 'foo';
       });
 
-      await expect(commerceInterface.interface()).toContainText('Results');
+      await expect(commerceInterface.interface()).toContainText('Products');
     });
   });
 
   test.describe('when selecting a facet value', () => {
-    test('should update the url', async ({page, commerceInterface}) => {
+    test('should update the url when reflectStateInUrl is enabled', async ({
+      page,
+      commerceInterface,
+    }) => {
       const facetValueLabel = commerceInterface.getFacetValue('Nike');
 
       await facetValueLabel.click();
@@ -124,9 +124,26 @@ test.describe('when a query is performed automatically', () => {
       );
 
       const currentUrl = page.url();
-      console.log(currentUrl);
 
       expect(currentUrl).toContain('Nike');
+    });
+    test('should not update the url when reflectStateInUrl is disabled', async ({
+      page,
+      commerceInterface,
+    }) => {
+      await page.goto(
+        'http://localhost:4400/iframe.html?id=atomic-commerce-interface--with-product-list&viewMode=story&args=attributes-reflect-state-in-url:false'
+      );
+
+      const facetValueLabel = commerceInterface.getFacetValue('Nike');
+
+      await facetValueLabel.click();
+
+      await page.waitForTimeout(1000);
+
+      const currentUrl = page.url();
+
+      expect(currentUrl).not.toContain('Nike');
     });
   });
 });
