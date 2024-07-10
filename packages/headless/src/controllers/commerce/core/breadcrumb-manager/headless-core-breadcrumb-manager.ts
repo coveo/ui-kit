96e6d9a5b9d32185bfa7f5bd740661cd623098b4
Frederic Beaudoin <fbeaudoin@coveo.com>
2024-07-10 16:05:31 -0400

    Correctly account for category facet value state in commerceFacetSelector, and adjust tests
    https://coveord.atlassian.net/browse/KIT-3385

--- packages/headless/src/controllers/commerce/core/breadcrumb-manager/headless-core-breadcrumb-manager.ts
+++ packages/headless/src/controllers/commerce/core/breadcrumb-manager/headless-core-breadcrumb-manager.ts
@@ -251,13 +251,20 @@ export function buildCoreBreadcrumbManager(
   const commerceFacetSelector = createSelector(
     (state: CommerceEngineState) => state.facetOrder,
     (facetOrder): BreadcrumbManagerState => {
-      const breadcrumbs = facetOrder.flatMap((facetId) => {
-        const facet = options.facetResponseSelector(engine[stateKey], facetId);
-        if (hasActiveValue(facet)) {
-          return [createBreadcrumb(facet)];
-        }
-        return [];
-      });
+      const breadcrumbs =
+        facetOrder
+          .map((facetId) =>
+            options.facetResponseSelector(engine[stateKey], facetId)
+          )
+          .filter((facet): facet is AnyFacetResponse =>
+            facet !== undefined &&
+            facet.values.length > 0 &&
+            facet.type !== 'hierarchical'
+              ? facet.values.some((value) => value.state !== 'idle')
+              : findActiveValueAncestry((facet as CategoryFacetResponse).values)
+                  .length > 0
+          )
+          .map(createBreadcrumb) ?? [];
 
       return {
         facetBreadcrumbs: breadcrumbs,
