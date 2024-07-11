091e9166fc23adeceac2f32edc36c3550fa25ca5
Frederic Beaudoin <fbeaudoin@coveo.com>
2024-07-11 07:58:38 -0400

    Simplify commerceFacetSelector
    https://coveord.atlassian.net/browse/KIT-3385

--- packages/headless/src/controllers/commerce/core/breadcrumb-manager/headless-core-breadcrumb-manager.ts
+++ packages/headless/src/controllers/commerce/core/breadcrumb-manager/headless-core-breadcrumb-manager.ts
@@ -251,20 +251,13 @@ export function buildCoreBreadcrumbManager(
   const commerceFacetSelector = createSelector(
     (state: CommerceEngineState) => state.facetOrder,
     (facetOrder): BreadcrumbManagerState => {
-      const breadcrumbs =
-        facetOrder
-          .map((facetId) =>
-            options.facetResponseSelector(engine[stateKey], facetId)
-          )
-          .filter((facet): facet is AnyFacetResponse =>
-            facet !== undefined &&
-            facet.values.length > 0 &&
-            facet.type !== 'hierarchical'
-              ? facet.values.some((value) => value.state !== 'idle')
-              : findActiveValueAncestry((facet as CategoryFacetResponse).values)
-                  .length > 0
-          )
-          .map(createBreadcrumb) ?? [];
+      const breadcrumbs = facetOrder.flatMap((facetId) => {
+        const facet = options.facetResponseSelector(engine[stateKey], facetId);
+        if (hasActiveValue(facet)) {
+          return [createBreadcrumb(facet)];
+        }
+        return [];
+      });
 
       return {
         facetBreadcrumbs: breadcrumbs,
