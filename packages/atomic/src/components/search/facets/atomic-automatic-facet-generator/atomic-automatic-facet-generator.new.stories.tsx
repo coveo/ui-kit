edac6847811f94f7c2fe33cc1859b81f83c53bf3
Louis Bompart <lbompart@coveo.com>
2024-07-16 16:45:47 +0200

    fix(storybook): add min & preset value for nbOfVal (facets) (#4191)
    https://coveord.atlassian.net/browse/KIT-3365

--- packages/atomic/src/components/search/facets/atomic-automatic-facet-generator/atomic-automatic-facet-generator.new.stories.tsx
+++ packages/atomic/src/components/search/facets/atomic-automatic-facet-generator/atomic-automatic-facet-generator.new.stories.tsx
@@ -9,11 +9,19 @@ const meta: Meta = {
   component: 'atomic-automatic-facet-generator',
   title: 'Atomic/AutomaticFacetGenerator',
   id: 'atomic-automatic-facet-generator',
-
   render: renderComponent,
   decorators: [decorator],
   parameters,
   play,
+  argTypes: {
+    'attributes-number-of-values': {
+      name: 'number-of-values',
+      control: {type: 'number', min: 1},
+    },
+  },
+  args: {
+    'attributes-number-of-values': 8,
+  },
 };
 
 export default meta;
