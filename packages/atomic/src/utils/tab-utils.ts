8c11b0f24c0098da47980a7b34a86d9f29476582
Felix Perron-Brault <fpbrault@coveo.com>
2024-07-19 16:05:44 -0400

    Add tab visibility utils

---
+++ packages/atomic/src/utils/tab-utils.ts
@@ -0,0 +1,24 @@
+/**
+ * Determines whether the component should be displayed on the current tab.
+ *
+ * @param tabsIncluded - An array of tab names that should include the facet.
+ * @param tabsExcluded - An array of tab names that should exclude the facet.
+ * @param activeTab - The name of the currently active tab.
+ * @returns A boolean indicating whether the component should be displayed on the current tab.
+ */
+export function shouldDisplayOnCurrentTab(
+  includeTabs: string[] | string,
+  excludeTabs: string[] | string,
+  activeTab: string
+) {
+  if (!activeTab) {
+    return true;
+  }
+
+  const isIncluded =
+    includeTabs.length === 0 || includeTabs.includes(activeTab);
+  const isNotExcluded =
+    excludeTabs.length === 0 || !excludeTabs.includes(activeTab);
+
+  return isIncluded && isNotExcluded;
+}
