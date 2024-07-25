9f1de83107a7c55dd6cad2416ddf8d38ac982bbe
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-07-25 16:56:49 -0400

    Update commerce-interface-wrapper.tsx

--- packages/atomic/storybookUtils/commerce-interface-wrapper.tsx
+++ packages/atomic/storybookUtils/commerce-interface-wrapper.tsx
@@ -24,6 +24,7 @@ const getSampleCommerceEngineConfiguration =
       currency: 'USD',
       view: {
         url: 'https://github.com/coveo/ui-kit',
+        referrer: document.referrer,
       },
     },
     cart: {
