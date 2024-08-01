bb0c3540bbd928f3ca176127b3ce0a952764fee7
ylakhdar <ylakhdar@coveo.com>
2024-08-01 15:47:55 -0400

    simplify config
    https://coveord.atlassian.net/browse/KIT-3394

--- packages/samples/headless-ssr-commerce/app/_lib/commerce-engine-config.ts
+++ packages/samples/headless-ssr-commerce/app/_lib/commerce-engine-config.ts
@@ -12,24 +12,9 @@ type CommerceEngineConfig = CommerceEngineDefinitionOptions<
   ControllerDefinitionsMap<CommerceEngine, Controller>
 >;
 
-const configuration = {
-  ...getSampleCommerceEngineConfiguration(),
-  analytics: {
-    trackingId: 'sports-ui-samples',
-  },
-};
-
 export default {
   configuration: {
-    ...configuration,
-    context: {
-      country: 'US',
-      currency: 'USD',
-      language: 'en',
-      view: {
-        url: 'https://sports.barca.group/browse/promotions/skis-boards/surfboards',
-      },
-    },
+    ...getSampleCommerceEngineConfiguration(),
   },
   controllers: {
     summary: defineQuerySummary(),
