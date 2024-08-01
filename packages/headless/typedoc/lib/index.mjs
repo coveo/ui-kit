98cd42375ae549b06e8d82b9f3f3c6bd69dfcdf7
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2024-08-01 09:27:52 -0400

    feat(headless): include atomic ssb in typedoc site
    https://coveord.atlassian.net/browse/KIT-3421

--- packages/headless/typedoc/lib/index.mjs
+++ packages/headless/typedoc/lib/index.mjs
@@ -29,30 +29,71 @@ export function load(app) {
       JSX.createElement('link', {
         rel: 'stylesheet',
         href: event.relativeURL('assets/material-style.css'),
+      }),
+      JSX.createElement('link', {
+        rel: 'stylesheet',
+        href: 'https://static.cloud.coveo.com/atomic/v2/themes/coveo.css',
       })
     )
   );
   app.renderer.hooks.on('body.end', () =>
     JSX.createElement(
-      'script',
+      JSX.Fragment,
       null,
-      JSX.createElement(JSX.Raw, {
-        html: /* js */ `
-          try {
-            const generateLinkElement = document.querySelector(".tsd-generator a");
-            const link = document.createElement("a");
-            Object.assign(link, {
-              href: "https://github.com/dmnsgn/typedoc-material-theme",
-              target: "_blank",
-              innerText: "typedoc-material-theme."
-            });
-            generateLinkElement.insertAdjacentElement("afterend", link);
-            generateLinkElement.insertAdjacentText("afterend", " with ");
-          } catch (error) {
+      JSX.createElement(
+        'script',
+        {
+          type: 'module',
+          src: 'https://static.cloud.coveo.com/atomic/v2/atomic.esm.js',
+        },
+        null
+      ),
+      JSX.createElement(
+        'script',
+        null,
+        JSX.createElement(JSX.Raw, {
+          html: /* js */ `
+              document.addEventListener('DOMContentLoaded', function () {
+                try {
+                  const generateLinkElement = document.querySelector(".tsd-generator a");
+                  const link = document.createElement("a");
+                  Object.assign(link, {
+                    href: "https://github.com/dmnsgn/typedoc-material-theme",
+                    target: "_blank",
+                    innerText: "typedoc-material-theme."
+                  });
+                  generateLinkElement.insertAdjacentElement("afterend", link);
+                  generateLinkElement.insertAdjacentText("afterend", " with ");
+                } catch (error) {
 
-          }
-        `,
-      })
+                }
+
+                // Insert Atomic search box in the header
+                const header = document.querySelector('.tsd-page-toolbar .tsd-toolbar-contents');
+                if (header) {
+                  const searchInterface = document.createElement('atomic-search-interface');
+                  const searchBox = document.createElement('atomic-search-box');
+                  searchBox.setAttribute('redirection-url', 'https://docs.coveo.com/en/search');
+
+                  searchInterface.appendChild(searchBox);
+                  header.insertBefore(searchInterface, header.firstChild);
+
+                  // Initialize the search interface with necessary configurations
+                  (async () => {
+                    await customElements.whenDefined('atomic-search-interface');
+                    const searchInterfaceElement = document.querySelector('atomic-search-interface');
+
+                    await searchInterfaceElement.initialize({
+                      organizationId: 'coveosearch',
+                      organizationEndpoints: await searchInterfaceElement.getOrganizationEndpoints('coveosearch'),
+                      accessToken: 'xx6ac9d08f-eb9a-48d5-9240-d7c251470c93'
+                    });
+                  })();
+                }
+              });
+            `,
+        })
+      )
     )
   );
   const onRenderEnd = () => {
