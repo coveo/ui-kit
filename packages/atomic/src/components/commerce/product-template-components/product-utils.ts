8c80f0f4b0b64c289d5e11d5b567b8d95494ebca
Felix Perron-Brault <fpbrault@coveo.com>
2024-08-06 13:46:04 -0400

    fix hrefTemplate
    https://coveord.atlassian.net/browse/KIT-3455

--- packages/atomic/src/components/commerce/product-template-components/product-utils.ts
+++ packages/atomic/src/components/commerce/product-template-components/product-utils.ts
@@ -1,4 +1,5 @@
 import {Product, ProductTemplatesHelpers} from '@coveo/headless/commerce';
+import {CommerceBindings} from '../atomic-commerce-interface/atomic-commerce-interface';
 
 export function getStringValueFromProductOrNull(
   product: Product,
@@ -11,4 +12,38 @@ export function getStringValueFromProductOrNull(
   }
 
   return value;
+}
+
+export function buildStringTemplateFromProduct(
+  template: string,
+  product: Product,
+  bindings: CommerceBindings
+) {
+  return template.replace(/\${(.*?)}/g, (value: string) => {
+    const key = value.substring(2, value.length - 1);
+    let newValue = readFromObject(product, key);
+    if (!newValue) {
+      newValue = readFromObject(window, key);
+    }
+
+    if (!newValue) {
+      bindings.engine.logger.warn(
+        `${key} used in the href template is undefined for this product: ${product.permanentid}`
+      );
+      return '';
+    }
+
+    return newValue;
+  });
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function readFromObject(object: any, key: string): string {
+  const firstPeriodIndex = key.indexOf('.');
+  if (object && firstPeriodIndex !== -1) {
+    const newKey = key.substring(firstPeriodIndex + 1);
+    key = key.substring(0, firstPeriodIndex);
+    return readFromObject(object[key], newKey);
+  }
+  return object ? object[key] : undefined;
 }
