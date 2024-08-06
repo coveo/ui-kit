26aa425d5b27c287d987aa31aad189c550f6f894
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2024-08-06 10:54:55 -0400

    feat(headless): compile typedoc plugin
    https://coveord.atlassian.net/browse/KIT-3421

---
+++ packages/headless/typedoc/lib/index.ts
@@ -0,0 +1,144 @@
+import {cpSync} from 'fs';
+import {dirname, resolve} from 'path';
+import {JSX, RendererEvent, ParameterType, Application} from 'typedoc';
+import {fileURLToPath} from 'url';
+import {getThemeCSSProperties} from './theme.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export function load(app: Application) {
+  app.options.addDeclaration({
+    name: 'themeColor',
+    help: 'Material Theme: Material 3 source color to derive the theme from.',
+    type: ParameterType.String,
+    defaultValue: '#cb9820',
+  });
+
+  app.renderer.hooks.on('head.end', (event) =>
+    JSX.createElement(
+      JSX.Fragment,
+      null,
+      JSX.createElement(
+        'style',
+        null,
+        JSX.createElement(JSX.Raw, {
+          html: getThemeCSSProperties(
+            app.options.getValue('themeColor') as string
+          ),
+        })
+      ),
+      JSX.createElement('link', {
+        rel: 'stylesheet',
+        href: event.relativeURL('assets/material-style.css'),
+      }),
+      JSX.createElement('link', {
+        rel: 'stylesheet',
+        href: 'https://static.cloud.coveo.com/atomic/v2/themes/coveo.css',
+      })
+    )
+  );
+
+  app.renderer.hooks.on('body.end', () =>
+    JSX.createElement(
+      JSX.Fragment,
+      null,
+      JSX.createElement('script', {
+        type: 'module',
+        src: 'https://static.cloud.coveo.com/atomic/v2/atomic.esm.js',
+      }),
+      JSX.createElement(
+        'script',
+        null,
+        JSX.createElement(JSX.Raw, {
+          html: `
+                    document.addEventListener('DOMContentLoaded', function () {
+                      try {
+                        const generateLinkElement = document.querySelector(".tsd-generator a");
+                        const link = document.createElement("a");
+                        Object.assign(link, {
+                          href: "https://github.com/dmnsgn/typedoc-material-theme",
+                          target: "_blank",
+                          innerText: "typedoc-material-theme."
+                        });
+                        generateLinkElement.insertAdjacentElement("afterend", link);
+                        generateLinkElement.insertAdjacentText("afterend", " with ");
+                      } catch (error) {
+                        console.error("Error adding typedoc link: ", error);
+                      }
+    
+                      // Move TypeDoc search box to the top of the left panel
+                      const tsdSearch = document.getElementById('tsd-search');
+                      const typeDocSearchBox = tsdSearch ? tsdSearch.cloneNode(true) : null;
+                      if (typeDocSearchBox) {
+                        const colSidebar = document.querySelector('.col-sidebar');
+                        if (colSidebar) {
+                          colSidebar.insertAdjacentElement('afterbegin', typeDocSearchBox);
+                        }
+                      }
+    
+                      // Insert Atomic search box in place of TypeDoc search box
+                      if (tsdSearch) {
+                        tsdSearch.innerHTML = ''; // Clear existing contents
+    
+                        const searchInterface = document.createElement('atomic-search-interface');
+                        const searchBox = document.createElement('atomic-search-box');
+                        searchBox.setAttribute('redirection-url', 'https://docs.coveo.com/en/search');
+    
+                        searchInterface.appendChild(searchBox);
+                        tsdSearch.appendChild(searchInterface);
+    
+                        // Initialize the search interface with necessary configurations
+                        (async () => {
+                          await customElements.whenDefined('atomic-search-interface');
+                          const searchInterfaceElement = document.querySelector('atomic-search-interface');
+    
+                          await searchInterfaceElement.initialize({
+                            organizationId: 'coveosearch',
+                            organizationEndpoints: await searchInterfaceElement.getOrganizationEndpoints('coveosearch'),
+                            accessToken: 'xx6ac9d08f-eb9a-48d5-9240-d7c251470c93'
+                          });
+                        })();
+                      }
+    
+                      // Ensure the TypeDoc search box is re-initialized if necessary
+                      function initTypeDocSearch() {
+                        const searchInput = document.getElementById('tsd-search-field');
+                        const searchResults = document.querySelector("#tsd-search .results");
+                        if (searchInput && searchResults) {
+                          searchInput.addEventListener('input', () => {
+                            if (window.search) {
+                              window.search.updateResults();
+                            }
+                          });
+    
+                          searchResults.addEventListener('mouseup', () => {
+                            if (window.search) {
+                              window.search.clearResults();
+                            }
+                          });
+                        }
+    
+                        if (window.search) {
+                          window.search.init();
+                        }
+                      }
+    
+                      initTypeDocSearch();
+                    });
+                    `,
+        })
+      )
+    )
+  );
+
+  const onRenderEnd = () => {
+    const from = resolve(__dirname, '../assets/style.css');
+    const to = resolve(
+      app.options.getValue('out') as string,
+      'assets/material-style.css'
+    );
+    cpSync(from, to);
+  };
+
+  app.renderer.on(RendererEvent.END, onRenderEnd);
+}
--- packages/headless/typedoc/lib/theme.ts
+++ packages/headless/typedoc/lib/theme.ts
@@ -2,10 +2,10 @@ import {
   themeFromSourceColor,
   hexFromArgb,
   argbFromHex,
+  Theme, // eslint-disable-next-line node/no-extraneous-import
 } from '@material/material-color-utilities';
 
-// https://github.com/material-foundation/material-color-utilities/issues/98#issuecomment-1535869882
-export const getSurfaceProperties = (theme, dark) =>
+export const getSurfaceProperties = (theme: Theme, dark: boolean) =>
   dark
     ? {
         'surface-dim': theme.palettes.neutral.tone(6),
@@ -25,20 +25,15 @@ export const getSurfaceProperties = (theme, dark) =>
         'surface-container-high': theme.palettes.neutral.tone(92),
         'surface-container-highest': theme.palettes.neutral.tone(90),
       };
-export function getSchemeProperties(properties) {
+
+export function getSchemeProperties(properties: Record<string, number>) {
   return Object.entries(properties).map(
     ([key, value]) =>
-      `--md-sys-color-${key
-        .replace(/([a-z])([A-Z])/g, '$1-$2')
-        .toLowerCase()}: ${hexFromArgb(value)}`
+      `--md-sys-color-${key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()}: ${hexFromArgb(value)}`
   );
 }
-/**
- * Get the CSS properties from a single color using Material 3.
- * @param hex
- * @returns The CSS string with dark and light properties
- */
-export function getThemeCSSProperties(hex) {
+
+export function getThemeCSSProperties(hex: string) {
   const theme = themeFromSourceColor(argbFromHex(hex));
   const dark = getSchemeProperties({
     ...theme.schemes.dark.toJSON(),
@@ -50,21 +45,21 @@ export function getThemeCSSProperties(hex) {
   });
   return /* css */ `
 @media (prefers-color-scheme: dark) {
-  :root {
-    ${dark.join(';\n    ')}
-  }
+    :root {
+        ${dark.join(';\n    ')}
+    }
 }
 :root[data-theme='dark'] {
-  ${dark.join(';\n  ')}
+    ${dark.join(';\n  ')}
 }
 
 @media (prefers-color-scheme: light) {
-  :root {
-    ${light.join(';\n    ')}
-  }
+    :root {
+        ${light.join(';\n    ')}
+    }
 }
 :root[data-theme='light'] {
-  ${light.join(';\n  ')}
+    ${light.join(';\n  ')}
 }
 `;
 }
