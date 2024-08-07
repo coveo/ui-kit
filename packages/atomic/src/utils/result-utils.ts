a5c9b61bd8370eb29e3772e1720b1200d05792b6
Felix Perron-Brault <fpbrault@coveo.com>
2024-08-07 11:04:59 -0400

    fix readFromObject
    https://coveord.atlassian.net/browse/KIT-3455

--- packages/atomic/src/utils/result-utils.ts
+++ packages/atomic/src/utils/result-utils.ts
@@ -83,10 +83,20 @@ export function getStringValueFromResultOrNull(result: Result, field: string) {
   return value;
 }
 
-function readFromObject<T>(object: T, key: string): string | undefined {
-  if (object && typeof object === 'object' && key in object) {
-    const value = (object as Record<string, unknown>)[key];
-    return typeof value === 'string' ? value : undefined;
+function readFromObject<T extends object>(
+  object: T,
+  key: string
+): string | undefined {
+  const keys = key.split('.');
+  let current: unknown = object;
+
+  for (const k of keys) {
+    if (current && typeof current === 'object' && k in current) {
+      current = (current as Record<string, unknown>)[k];
+    } else {
+      return undefined;
+    }
   }
-  return undefined;
+
+  return typeof current === 'string' ? current : undefined;
 }
