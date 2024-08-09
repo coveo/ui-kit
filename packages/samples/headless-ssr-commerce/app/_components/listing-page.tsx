91ab8b2223a355d279e486e2551bda933d20d70d
Frederic Beaudoin <fbeaudoin@coveo.com>
2024-08-09 07:37:27 -0400

    Use Pagination component by default
    https://coveord.atlassian.net/browse/KIT-3392

--- packages/samples/headless-ssr-commerce/app/_components/listing-page.tsx
+++ packages/samples/headless-ssr-commerce/app/_components/listing-page.tsx
@@ -7,9 +7,9 @@ import {
   ListingHydratedState,
   ListingStaticState,
 } from '../_lib/commerce-engine';
-// import Pagination from './pagination';
+import Pagination from './pagination';
 import {ProductList} from './product-list';
-import ShowMore from './show-more';
+// import ShowMore from './show-more';
 import Sort from './sort';
 import Summary from './summary';
 
@@ -51,17 +51,16 @@ export default function ListingPage({
         staticState={staticState.controllers.productList.state}
         controller={hydratedState?.controllers.productList}
       />
-
       {/* The ShowMore and Pagination components showcase two frequent ways to implement pagination. */}
-      <ShowMore
+      <Pagination
+        staticState={staticState.controllers.pagination.state}
+        controller={hydratedState?.controllers.pagination}
+      ></Pagination>
+      {/* <ShowMore
         staticState={staticState.controllers.pagination.state}
         controller={hydratedState?.controllers.pagination}
         summaryController={hydratedState?.controllers.summary}
-      />
-      {/* <Pagination
-        staticState={staticState.controllers.pagination.state}
-        controller={hydratedState?.controllers.pagination}
-      ></Pagination> */}
+      /> */}
     </>
   );
 }
