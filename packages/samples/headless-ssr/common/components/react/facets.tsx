97dabab70f96652200f5260b9d485578c1ad35cb
Felix Perron-Brault <fpbrault@coveo.com>
2024-08-21 11:32:38 -0400

    remove unused

--- packages/samples/headless-ssr/common/components/react/facets.tsx
+++ packages/samples/headless-ssr/common/components/react/facets.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {useEffect, useRef} from 'react';
+import {useEffect} from 'react';
 import {useAuthorFacet, useTabManager} from '../../lib/react/engine';
 import FacetCommon from '../common/facet';
 
