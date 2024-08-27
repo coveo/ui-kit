d9e0a7596cadfe2fd24c09b611ec031d26e85f05
mmitiche <mmitiche@coveo.com>
2024-08-27 08:15:27 -0400

    logic to handle opening smart snippet source inside salesforce added

--- packages/quantic/force-app/main/default/lwc/quanticSmartSnippetSource/quanticSmartSnippetSource.js
+++ packages/quantic/force-app/main/default/lwc/quanticSmartSnippetSource/quanticSmartSnippetSource.js
@@ -1,26 +1,32 @@
+import navigateToRecord from '@salesforce/label/c.quantic_NavigateToRecord';
+import opensInBrowserTab from '@salesforce/label/c.quantic_OpensInBrowserTab';
 import {LinkUtils} from 'c/quanticUtils';
+import {NavigationMixin} from 'lightning/navigation';
 import {LightningElement, api} from 'lwc';
+
+/**
+ * Some document types cannot be opened directly in Salesforce, but we need to open their parent record, as is the case for the Case Comment document type.
+ */
+const documentTypesRequiringParentRecord = ['CaseComment'];
+
+/** @typedef {import("coveo").Result} Result */
 
 /**
  * The `QuanticSmartSnippetSource` component displays the uri and the title of the smart snippet source.
  * @category Search
  * @example
- *  <c-quantic-smart-snippet-source uri={uri} title={title} actions={actions}></c-quantic-smart-snippet-source>
+ *  <c-quantic-smart-snippet-source actions={actions}></c-quantic-smart-snippet-source>
  * @internal
  */
-export default class QuanticSmartSnippetSource extends LightningElement {
+export default class QuanticSmartSnippetSource extends NavigationMixin(
+  LightningElement
+) {
   /**
    * The click uri of the smart snippet source.
    * @api
-   * @type {string}
+   * @type {Result}
    */
-  @api uri;
-  /**
-   * The title of the smart snippet source.
-   * @api
-   * @type {string}
-   */
-  @api title;
+  @api source;
   /**
    * The actions that need to be bound to the links of the smart snippet source.
    * @api
@@ -28,12 +34,33 @@ export default class QuanticSmartSnippetSource extends LightningElement {
    */
   @api actions;
 
+  labels = {
+    navigateToRecord,
+    opensInBrowserTab,
+  };
+
   /** @type {boolean} */
   isInitialRender = true;
   /** @type {function} */
   removeUriBindings;
   /** @type {function} */
   removeTitleBindings;
+  /** @type {string} */
+  salesforceRecordUrl;
+
+  connectedCallback() {
+    if (this.isSalesforceLink) {
+      this[NavigationMixin.GenerateUrl]({
+        type: 'standard__recordPage',
+        attributes: {
+          recordId: this.recordIdAttribute,
+          actionName: 'view',
+        },
+      }).then((url) => {
+        this.salesforceRecordUrl = url;
+      });
+    }
+  }
 
   renderedCallback() {
     if (this.isInitialRender) {
@@ -69,5 +96,83 @@ export default class QuanticSmartSnippetSource extends LightningElement {
         this.actions
       );
     }
+  }
+
+  /**
+   * Returns the smart snippet source title.
+   * @returns {string}
+   */
+  get sourceTitle() {
+    return this.source?.title;
+  }
+
+  /**
+   * Returns the smart snippet source uri.
+   * @returns {string}
+   */
+  get sourceUri() {
+    return this.source?.clickUri;
+  }
+
+  handleClick(event) {
+    if (this.isSalesforceLink) {
+      event.preventDefault();
+      this.navigateToSalesforceRecord(event);
+    }
+  }
+
+  navigateToSalesforceRecord(event) {
+    event.stopPropagation();
+    const targetPageRef = {
+      type: 'standard__recordPage',
+      attributes: {
+        recordId: this.recordIdAttribute,
+        actionName: 'view',
+      },
+    };
+    this[NavigationMixin.Navigate](targetPageRef);
+  }
+
+  get recordIdAttribute() {
+    // Knowledge article uses the knowledge article version id to navigate.
+    if (this.source?.raw?.sfkbid && this.source?.raw?.sfkavid) {
+      return this.source.raw.sfkavid;
+    }
+    if (this.shouldOpenParentRecord) {
+      return this.source?.raw?.sfparentid;
+    }
+    return this.source.raw.sfid;
+  }
+
+  get shouldOpenParentRecord() {
+    return (
+      documentTypesRequiringParentRecord.includes(
+        this.source?.raw?.documenttype
+      ) && this.source?.raw?.sfparentid
+    );
+  }
+
+  /**
+   * Checks if the Result type is Salesforce.
+   */
+  get isSalesforceLink() {
+    return !!this.source?.raw?.sfid;
+  }
+
+  /**
+   * Returns the aria label value for the link.
+   */
+  get ariaLabelValue() {
+    if (this.isSalesforceLink) {
+      return this.labels.navigateToRecord;
+    }
+    return this.labels.opensInBrowserTab;
+  }
+
+  get hrefValue() {
+    if (this.isSalesforceLink) {
+      return this.salesforceRecordUrl;
+    }
+    return this.sourceUri;
   }
 }
