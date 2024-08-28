dac7ff124e2ed23bcb46dcebe7474eddc933ffd1
ylakhdar <ylakhdar@coveo.com>
2024-08-28 16:00:36 -0400

    rename export
    https://coveord.atlassian.net/browse/KIT-3446

--- packages/samples/headless-ssr-commerce/app/recommendation/page.tsx
+++ packages/samples/headless-ssr-commerce/app/recommendation/page.tsx
@@ -1,6 +1,6 @@
 import {headers} from 'next/headers';
 import Recommendation from '../_components/pages/recommendation';
-import {recommendationEngineDefinition} from '../_lib/commerce-engine';
+import {standaloneEngineDefinition} from '../_lib/commerce-engine';
 import {NextJsNavigatorContext} from '../_lib/navigatorContextProvider';
 
 /**
@@ -11,12 +11,12 @@ import {NextJsNavigatorContext} from '../_lib/navigatorContextProvider';
 export default async function RecommendationPage() {
   // Sets the navigator context provider to use the newly created `navigatorContext` before fetching the app static state
   const navigatorContext = new NextJsNavigatorContext(headers());
-  recommendationEngineDefinition.setNavigatorContextProvider(
+  standaloneEngineDefinition.setNavigatorContextProvider(
     () => navigatorContext
   );
 
   // Fetches the static state of the app with initial state (when applicable)
-  const staticState = await recommendationEngineDefinition.fetchStaticState();
+  const staticState = await standaloneEngineDefinition.fetchStaticState();
 
   return (
     <Recommendation
