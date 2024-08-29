172c072ddda7ae17323647ac5db8557df0c05781
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-08-29 10:49:21 -0400

    delete jest setup
    https://coveord.atlassian.net/browse/KIT-3023

--- packages/headless/jest.config.js
+++ packages/headless/jest.config.js
@@ -3,7 +3,6 @@ process.env.TZ = 'Australia/Eucla';
 module.exports = {
   preset: 'ts-jest',
   silent: true,
-  setupFiles: ['./jest.setup.js'],
   moduleNameMapper: {
     '@coveo/pendragon': '<rootDir>/ponyfills/magic-cookie-node.js',
   },
