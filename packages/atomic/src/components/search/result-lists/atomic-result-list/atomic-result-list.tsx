bc6cb1f4b3c48be221da8bb70ad6b9465fa9c19d
Louis Bompart <lbompart@coveo.com>
2024-08-29 09:20:12 -0400

    fix(atomic): do not add click outside of grid layout (#4354)
    Current behaviour: All `atomic-result/product` are clickable and default
    to a 'default' atomic-result/product-link.
    
    Expected behaviour: That should have been the case only with the grid
    layout.
    
    KIT-3508
    
    Backport of #4355

--- packages/atomic/src/components/search/result-lists/atomic-result-list/atomic-result-list.tsx
+++ packages/atomic/src/components/search/result-lists/atomic-result-list/atomic-result-list.tsx
@@ -217,7 +217,10 @@ export class AtomicResultList implements InitializableComponent {
         this.imageSize
       ),
       content: this.itemTemplateProvider.getTemplateContent(result),
-      linkContent: this.itemTemplateProvider.getLinkTemplateContent(result),
+      linkContent:
+        this.display === 'grid'
+          ? this.itemTemplateProvider.getLinkTemplateContent(result)
+          : this.itemTemplateProvider.getEmptyLinkTemplateContent(),
       store: this.bindings.store,
       density: this.density,
       imageSize: this.imageSize,
