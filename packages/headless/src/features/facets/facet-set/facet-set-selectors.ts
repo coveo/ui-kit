4430a8ab8d2c478e05f433584e7969f7b78cd02e
Felix Perron-Brault <fpbrault@coveo.com>
2024-09-03 11:28:40 -0400

    simplify facet set selectors
    https://coveord.atlassian.net/browse/KIT-3501

--- packages/headless/src/features/facets/facet-set/facet-set-selectors.ts
+++ packages/headless/src/features/facets/facet-set/facet-set-selectors.ts
@@ -3,64 +3,44 @@ import {FacetSection} from '../../../state/state-sections';
 import {AnyFacetResponse} from '../generic/interfaces/generic-facet-response';
 import {FacetResponse, FacetValue} from './interfaces/response';
 
-export const baseFacetResponseSelector = (
-  state: Partial<SearchSection>,
-  id: string
-) => {
-  const findById = (response: {facetId: string}) => response.facetId === id;
-
-  if ('search' in state && state.search) {
-    return state.search.response.facets.find(findById);
-  }
-  return undefined;
-};
-
-export const facetRequestSelector = (state: FacetSection, id: string) => {
-  return state.facetSet[id]?.request;
-};
-
 function isFacetResponse(
   state: FacetSection,
   response: AnyFacetResponse | undefined
 ): response is FacetResponse {
   return !!response && response.facetId in state.facetSet;
 }
+
+export const baseFacetResponseSelector = (
+  state: Partial<SearchSection>,
+  id: string
+) => state.search?.response.facets.find((response) => response.facetId === id);
+
+export const facetRequestSelector = (state: FacetSection, id: string) =>
+  state.facetSet[id]?.request;
+
 export const facetResponseSelector = (
   state: SearchSection & FacetSection,
   facetId: string
 ) => {
   const response = baseFacetResponseSelector(state, facetId);
-  if (isFacetResponse(state, response)) {
-    return response;
-  }
-
-  return undefined;
+  return isFacetResponse(state, response) ? response : undefined;
 };
 
 export const facetResponseSelectedValuesSelector = (
   state: SearchSection & FacetSection,
   facetId: string
-) => {
-  const response = facetResponseSelector(state, facetId);
-  if (!response) {
-    return [];
-  }
-
-  return response.values.filter((value) => value.state === 'selected');
-};
+) =>
+  facetResponseSelector(state, facetId)?.values.filter(
+    (value) => value.state === 'selected'
+  ) || [];
 
 export const facetResponseActiveValuesSelector = (
   state: SearchSection & FacetSection,
   facetId: string
-): FacetValue[] => {
-  const response = facetResponseSelector(state, facetId);
-  if (!response) {
-    return [];
-  }
+): FacetValue[] =>
+  facetResponseSelector(state, facetId)?.values.filter(
+    (value) => value.state !== 'idle'
+  ) || [];
 
-  return response.values.filter((value) => value.state !== 'idle');
-};
-
-export const isFacetLoadingResponseSelector = (state: SearchSection) => {
-  return state.search.isLoading;
-};
+export const isFacetLoadingResponseSelector = (state: SearchSection) =>
+  state.search.isLoading;
