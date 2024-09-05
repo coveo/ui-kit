583e30aa12ca43dff98c543ddc96d076f37e02f1
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-09-05 13:05:29 -0400

    chore(headless)!: remove headless fetch & crypto polyfills (#4358)
    https://coveord.atlassian.net/browse/KIT-3023
    
    This PR is part of the making sure there are no 'require's in the
    headless ESM output.
    #### Before
    
    <img width="1024" alt="image"
    src="https://github.com/user-attachments/assets/7869429b-69ef-4c11-801f-35ed35208bab">
    
    
    #### After
    
    <img width="1039" alt="image"
    src="https://github.com/user-attachments/assets/a31f7733-676d-4eca-a201-dc746772222a">
    
    -  [x] Remove undici from renovate.json
    
    There is still this one that we will have to change or make it only
    available to the commonJs output.
    https://github.com/coveo/ui-kit/blob/KIT-3023/packages/headless/ponyfills/abortable-fetch-shim.js
    
    ---------
    
    Co-authored-by: Louis Bompart <lbompart@coveo.com>

--- packages/headless/jest.config.js
+++ packages/headless/jest.config.js
@@ -3,10 +3,7 @@ process.env.TZ = 'Australia/Eucla';
 module.exports = {
   preset: 'ts-jest',
   silent: true,
-  setupFiles: ['./jest.setup.js'],
   moduleNameMapper: {
-    '@coveo/please-give-me-fetch':
-      '<rootDir>/ponyfills/fetch-ponyfill-browser.js',
     '@coveo/pendragon': '<rootDir>/ponyfills/magic-cookie-node.js',
   },
 };
