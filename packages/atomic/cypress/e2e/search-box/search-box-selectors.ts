58908580c0b3326a82fe35b86050735daedf32d7
Frederic Beaudoin <fbeaudoin@coveo.com>
2024-09-06 13:00:17 -0400

    feat(atomic)!: make textarea search box the default and only behavior (#4332)
    https://coveord.atlassian.net/browse/KIT-2689
    
    ---------
    
    Co-authored-by: GitHub Actions Bot <>
    Co-authored-by: Louis Bompart <lbompart@coveo.com>

--- packages/atomic/cypress/e2e/search-box/search-box-selectors.ts
+++ packages/atomic/cypress/e2e/search-box/search-box-selectors.ts
@@ -5,7 +5,6 @@ export const searchBoxComponent = 'atomic-search-box';
 export const SearchBoxSelectors = {
   host: () => cy.get(searchBoxComponent),
   shadow: () => cy.get(searchBoxComponent).shadow(),
-  inputBox: () => SearchBoxSelectors.shadow().find('[part="input"]'),
   textArea: () => SearchBoxSelectors.shadow().find('[part="textarea"]'),
   submitButton: () =>
     SearchBoxSelectors.shadow().find('[part="submit-button"]'),
