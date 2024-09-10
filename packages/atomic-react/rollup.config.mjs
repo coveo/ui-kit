66278102bdf54b822570ef874228a33235f9ee62
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-09-10 16:16:24 -0400

    new bundle for atomic-react
    https://coveord.atlassian.net/browse/KIT-2541

--- packages/atomic-react/rollup.config.mjs
+++ packages/atomic-react/rollup.config.mjs
@@ -28,33 +28,16 @@ const commonExternal = [
 ];
 
 /** @returns {import('rollup').OutputOptions} */
-const outputIIFE = ({minify}) => ({
-  file: `dist/iife/atomic-react${minify ? '.min' : ''}.js`,
+const outputIIFE = ({minify, subpath, name}) => ({
+  file: `dist/iife/atomic-react${subpath}${minify ? '.min' : ''}.js`,
   format: 'iife',
-  name: 'CoveoAtomicReact',
+  name,
   globals,
   plugins: minify ? [terser()] : [],
 });
 
-/** @returns {import('rollup').OutputOptions} */
-const outputIIFERecs = ({minify}) => ({
-  file: `dist/iife/atomic-react/recommendation${minify ? '.min' : ''}.js`,
-  format: 'iife',
-  name: 'CoveoAtomicReactRecommendation',
-  globals,
-  plugins: minify ? [terser()] : [],
-});
-
-/** @returns {import('rollup').OutputOptions} */
-const outputIIFECommerce = ({minify}) => ({
-  file: `dist/iife/atomic-react/commerce${minify ? '.min' : ''}.js`,
-  format: 'iife',
-  name: 'CoveoAtomicReactCommerce',
-  globals,
-  plugins: minify ? [terser()] : [],
-});
-
-const plugins = [
+/**@returns {import('rollup').InputPluginOption} */
+const iifePlugins = [
   json(),
   nodePolyfills(),
   typescript({tsconfig: 'tsconfig.iife.json'}),
@@ -71,26 +54,129 @@ const plugins = [
   }),
 ];
 
-export default defineConfig([
+/**@returns {import('rollup').RollupOptions[]} */
+const rollupOptionsIIFE = [
   {
     input: 'src/index.ts',
-    output: [outputIIFE({minify: true}), outputIIFE({minify: false})],
+    output: [
+      outputIIFE({
+        minify: true,
+        subpath: '',
+        name: 'CoveoAtomicReact',
+      }),
+      outputIIFE({
+        minify: false,
+        subpath: '',
+        name: 'CoveoAtomicReact',
+      }),
+    ],
     external: commonExternal,
-    plugins,
+    plugins: iifePlugins,
   },
   {
     input: 'src/recommendation.index.ts',
-    output: [outputIIFERecs({minify: true}), outputIIFERecs({minify: false})],
+    output: [
+      outputIIFE({
+        minify: true,
+        subpath: '/recommendation',
+        name: 'CoveoAtomicReactRecommendation',
+      }),
+      outputIIFE({
+        minify: false,
+        subpath: '/recommendation',
+        name: 'CoveoAtomicReactRecommendation',
+      }),
+    ],
     external: commonExternal,
-    plugins,
+    plugins: iifePlugins,
   },
   {
     input: 'src/commerce.index.ts',
     output: [
-      outputIIFECommerce({minify: true}),
-      outputIIFECommerce({minify: false}),
+      outputIIFE({
+        minify: true,
+        subpath: '/commerce',
+        name: 'CoveoAtomicReactCommerce',
+      }),
+      outputIIFE({
+        minify: false,
+        subpath: '/commerce',
+        name: 'CoveoAtomicReactCommerce',
+      }),
     ],
     external: commonExternal,
-    plugins,
+    plugins: iifePlugins,
   },
+];
+
+/** @returns {import('rollup').OutputOptions} */
+const outputESM = ({subpath}) => ({
+  file: `dist/atomic-react${subpath}.mjs`,
+  format: 'esm',
+});
+
+/**@returns {import('rollup').InputPluginOption} */
+const esmPlugins = [typescript({tsconfig: 'tsconfig.esm.json'})];
+
+/**@returns {import('rollup').RollupOptions[]} */
+const rollupOptionsESM = [
+  {
+    input: 'src/index.ts',
+    output: outputESM({subpath: ''}),
+    external: commonExternal,
+    plugins: esmPlugins,
+  },
+  {
+    input: 'src/recommendation.index.ts',
+    output: outputESM({subpath: '/recommendation'}),
+    external: commonExternal,
+    plugins: esmPlugins,
+  },
+  {
+    input: 'src/commerce.index.ts',
+    output: outputESM({subpath: '/commerce'}),
+    external: commonExternal,
+    plugins: esmPlugins,
+  },
+];
+
+// /** @returns {import('rollup').OutputOptions} */
+// const outputCJS = ({subpath}) => ({
+//   file: `dist/cjs/atomic-react${subpath}.cjs`,
+//   format: 'cjs',
+//   exports: 'auto',
+// });
+
+// /**@returns {import('rollup').InputPluginOption} */
+// const cjsPlugins = [
+//   nodeResolve({extensions: ['.ts', '.js']}),
+//   typescript({tsconfig: 'tsconfig.cjs.json'}),
+// ];
+
+// /**@returns {import('rollup').RollupOptions[]} */
+// const rollupOptionsCJS = [
+//   {
+//     input: 'src/index.ts',
+//     output: outputCJS({subpath: ''}),
+//     external: commonExternal,
+//     plugins: cjsPlugins,
+//   },
+//   {
+//     input: 'src/recommendation.index.ts',
+//     output: outputCJS({subpath: '/recommendation'}),
+//     external: commonExternal,
+//     plugins: cjsPlugins,
+//   },
+//   {
+//     input: 'src/commerce.index.ts',
+//     output: outputCJS({subpath: '/commerce'}),
+//     external: commonExternal,
+//     plugins: cjsPlugins,
+//   },
+// ];
+
+export default defineConfig([
+  ...rollupOptionsESM,
+  // ...rollupOptionsCJS,
+  ...rollupOptionsIIFE,
 ]);
