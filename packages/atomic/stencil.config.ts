39313ea3cf518a723ca7b12f7a346b3a4e060997
Louis Bompart <lbompart@coveo.com>
2024-09-18 11:23:09 -0400

    fix(atomic, atomic-react): use root-relative URL for CDN (#4431)
    This ensures the code we deployed to the CDN is environment agnostic.
    Currently for example, we try to use Headless from
    static.cloud.coveo.com in Atomic from staticdev.cloud.coveo.com
    
    https://coveord.atlassian.net/browse/KIT-3571
    
    See
    https://developer.mozilla.org/en-US/docs/Web/API/URL_API/Resolving_relative_references#root_relative

--- packages/atomic/stencil.config.ts
+++ packages/atomic/stencil.config.ts
@@ -36,43 +36,43 @@ const packageMappings: {[key: string]: {devWatch: string; cdn: string}} = {
       __dirname,
       './src/external-builds/commerce/headless.esm.js'
     ),
-    cdn: `https://static.cloud.coveo.com/headless/${headlessVersion}/commerce/headless.esm.js`,
+    cdn: `/headless/${headlessVersion}/commerce/headless.esm.js`,
   },
   '@coveo/headless/insight': {
     devWatch: path.resolve(
       __dirname,
       './src/external-builds/insight/headless.esm.js'
     ),
-    cdn: `https://static.cloud.coveo.com/headless/${headlessVersion}/insight/headless.esm.js`,
+    cdn: `/headless/${headlessVersion}/insight/headless.esm.js`,
   },
   '@coveo/headless/product-recommendation': {
     devWatch: path.resolve(
       __dirname,
       './src/external-builds/product-recommendation/headless.esm.js'
     ),
-    cdn: `https://static.cloud.coveo.com/headless/${headlessVersion}/product-recommendation/headless.esm.js`,
+    cdn: `/headless/${headlessVersion}/product-recommendation/headless.esm.js`,
   },
   '@coveo/headless/recommendation': {
     devWatch: path.resolve(
       __dirname,
       './src/external-builds/recommendation/headless.esm.js'
     ),
-    cdn: `https://static.cloud.coveo.com/headless/${headlessVersion}/recommendation/headless.esm.js`,
+    cdn: `/headless/${headlessVersion}/recommendation/headless.esm.js`,
   },
   '@coveo/headless/case-assist': {
     devWatch: path.resolve(
       __dirname,
       './src/external-builds/case-assist/headless.esm.js'
     ),
-    cdn: `https://static.cloud.coveo.com/headless/${headlessVersion}/case-assist/headless.esm.js`,
+    cdn: `/headless/${headlessVersion}/case-assist/headless.esm.js`,
   },
   '@coveo/headless': {
     devWatch: path.resolve(__dirname, './src/external-builds/headless.esm.js'),
-    cdn: `https://static.cloud.coveo.com/headless/${headlessVersion}/headless.esm.js`,
+    cdn: `/headless/${headlessVersion}/headless.esm.js`,
   },
   /*   '@coveo/bueno': {
     devWatch: path.resolve(__dirname, './src/external-builds/bueno.esm.js'),
-    cdn: `https://static.cloud.coveo.com/bueno/${headlessVersion}/bueno.esm.js`,
+    cdn: `/bueno/${headlessVersion}/bueno.esm.js`,
   }, */
 };
 
