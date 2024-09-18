5fe789d4c2b78a612446b75ad7366d7fe48c2f6c
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-09-18 15:23:11 -0400

    all item utils use common object-util function
    https://coveord.atlassian.net/browse/KIT-3269

--- packages/atomic/src/components/commerce/product-template-components/product-utils.ts
+++ packages/atomic/src/components/commerce/product-template-components/product-utils.ts
@@ -1,4 +1,5 @@
 import {Product, ProductTemplatesHelpers} from '@coveo/headless/commerce';
+import {readFromObject} from '../../../utils/object-utils';
 import {CommerceBindings} from '../atomic-commerce-interface/atomic-commerce-interface';
 
 export function getStringValueFromProductOrNull(
@@ -35,22 +36,4 @@ export function buildStringTemplateFromProduct(
 
     return newValue;
   });
-}
-
-function readFromObject<T extends object>(
-  object: T,
-  key: string
-): string | undefined {
-  const keys = key.split('.');
-  let current: unknown = object;
-
-  for (const k of keys) {
-    if (current && typeof current === 'object' && k in current) {
-      current = (current as Record<string, unknown>)[k];
-    } else {
-      return undefined;
-    }
-  }
-
-  return typeof current === 'string' ? current : undefined;
 }
