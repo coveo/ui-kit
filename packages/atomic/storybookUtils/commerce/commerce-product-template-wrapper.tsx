b62f3c79173fa4d2f730776ce7843d3fb526d1a3
Olivier Lamothe <olamothe@coveo.com>
2024-09-19 10:05:28 -0400

    test(atomic): add tests for product link (#4203)
    https://coveord.atlassian.net/browse/KIT-3269
    
    ---------
    
    Co-authored-by: Louis Bompart <lbompart@coveo.com>
    Co-authored-by: Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>

---
+++ packages/atomic/storybookUtils/commerce/commerce-product-template-wrapper.tsx
@@ -0,0 +1,22 @@
+import {Decorator} from '@storybook/web-components';
+import {html, TemplateResult} from 'lit-html';
+
+export const wrapInProductTemplate = (): {
+  decorator: Decorator;
+} => {
+  const decorator: Decorator = (story) => {
+    // lit-html does not support adding expressions to `template` tags
+    // https://lit.dev/docs/templates/expressions/#invalid-locations
+
+    const templateTag = document.createElement('template');
+    templateTag.innerHTML = `${(story() as TemplateResult).strings.join('')}`;
+    templateTag.id = 'code-root';
+    return html`
+      <atomic-product-template>${templateTag}</atomic-product-template>
+    `;
+  };
+
+  return {
+    decorator,
+  };
+};
