6d8345dd069d6f9c2d91a6e02b6fe434b31f3fe3
Louis Bompart <lbompart@coveo.com>
2024-09-20 02:27:37 -0400

    tweak-cypresses

--- packages/atomic/cypress.config.mjs
+++ packages/atomic/cypress.config.mjs
@@ -1,5 +1,5 @@
 import {defineConfig} from 'cypress';
-import plugin from './cypress/plugins/index';
+import plugin from './cypress/plugins/index.js';
 
 export default defineConfig({
   projectId: '5ph2j4',
@@ -22,7 +22,7 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      plugin(on, config);
+      return plugin(on, config);
     },
     baseUrl: 'http://localhost:3333',
     specPattern: 'cypress/e2e/**/*.cypress.ts',
