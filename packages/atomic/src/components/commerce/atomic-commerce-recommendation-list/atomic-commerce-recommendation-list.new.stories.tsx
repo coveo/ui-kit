2f636b3d2aa6ef7324cbfb355c5b38ad27bf78d9
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-09-24 13:59:00 -0400

    Update atomic-commerce-recommendation-list.new.stories.tsx
    https://coveord.atlassian.net/browse/KIT-3248

--- packages/atomic/src/components/commerce/atomic-commerce-recommendation-list/atomic-commerce-recommendation-list.new.stories.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-recommendation-list/atomic-commerce-recommendation-list.new.stories.tsx
@@ -1,13 +1,9 @@
-import {
-  playExecuteFirstSearch,
-  wrapInCommerceInterface,
-} from '@coveo/atomic-storybook-utils/commerce/commerce-interface-wrapper';
+import {wrapInCommerceInterface} from '@coveo/atomic-storybook-utils/commerce/commerce-interface-wrapper';
 import {parameters} from '@coveo/atomic-storybook-utils/common/common-meta-parameters';
 import {renderComponent} from '@coveo/atomic-storybook-utils/common/render-component';
 import type {Meta, StoryObj as Story} from '@storybook/web-components';
-import {html} from 'lit';
 
-const {decorator, play} = wrapInCommerceInterface({skipFirstSearch: true});
+const {decorator, play} = wrapInCommerceInterface({skipFirstSearch: false});
 
 const meta: Meta = {
   component: 'atomic-commerce-recommendation-list',
@@ -17,64 +13,67 @@ const meta: Meta = {
   decorators: [decorator],
   parameters,
   play,
+  args: {'attributes-slot-id': 'd8118c04-ff59-4f03-baca-2fc5f3b81221'},
 };
 
 export default meta;
 
 export const Default: Story = {
   name: 'atomic-commerce-recommendation-list',
-  play: async (context) => {
-    await play(context);
-    await playExecuteFirstSearch(context);
+};
+
+// export const WithProductId: Story = {
+//   args: {
+//     'attributes-slot-id': 'd8118c04-ff59-4f03-baca-2fc5f3b81221',
+//     'attributes-product-id': '1',
+//   },
+// };
+
+const {play: playNoFirstQuery} = wrapInCommerceInterface({
+  skipFirstSearch: true,
+});
+export const BeforeQuery: Story = {
+  tags: ['test'],
+  play: playNoFirstQuery,
+};
+
+export const WithFullTemplate: Story = {
+  tags: ['test'],
+  args: {
+    'slots-default': ` <atomic-product-template>
+                  <template>
+ <atomic-product-section-visual>
+                <span>Visual Section</span>
+              </atomic-product-section-visual>
+              <atomic-product-section-badge>
+                <span>Badge Section</span>
+              </atomic-product-section-badge>
+              <atomic-product-section-actions>
+                <span>Actions Section</span>
+              </atomic-product-section-actions>
+              <atomic-product-section-title>
+                <span>Title Section</span>
+              </atomic-product-section-title>
+              <atomic-product-section-title-metadata>
+                  <span>Title Metadata Section</span>
+              </atomic-product-section-title-metadata>
+              <atomic-product-section-emphasized>
+                <span>Emphasized Section</span>
+              </atomic-product-section-emphasized>
+              <atomic-product-section-excerpt>
+                <span>Excerpt Section</span>
+              </atomic-product-section-excerpt>
+              <atomic-product-section-bottom-metadata>
+                <span>Bottom Metadata Section</span>
+              </atomic-product-section-bottom-metadata>
+                  </template>
+                </atomic-product-template>`,
   },
 };
 
-// What are the differences type of recommendation lists ? what to show here ?
-export const InAPage: Story = {
-  name: 'In a page',
-  decorators: [
-    (story) => html`
-      <atomic-commerce-layout>
-        <atomic-layout-section section="facets">
-          <atomic-commerce-facets></atomic-commerce-facets>
-        </atomic-layout-section>
-        <atomic-layout-section section="main">
-          ${story()}
-          <atomic-commerce-recommendation-list
-            id="cart_recommendations"
-            slot-id="d8118c04-ff59-4f03-baca-2fc5f3b81221"
-            products-per-page="3"
-          >
-            <atomic-product-template>
-              <template>
-                <atomic-product-section-name>
-                  <atomic-product-link class="font-bold"></atomic-product-link>
-                </atomic-product-section-name>
-                <atomic-product-section-visual>
-                  <atomic-product-image
-                    field="ec_thumbnails"
-                  ></atomic-product-image>
-                </atomic-product-section-visual>
-                <atomic-product-section-metadata>
-                  <atomic-product-text
-                    field="ec_brand"
-                    class="text-neutral-dark block"
-                  ></atomic-product-text>
-                  <atomic-product-rating
-                    field="ec_rating"
-                  ></atomic-product-rating>
-                </atomic-product-section-metadata>
-                <atomic-product-section-emphasized>
-                  <atomic-product-price currency="USD"></atomic-product-price>
-                </atomic-product-section-emphasized>
-                <atomic-product-section-children>
-                  <atomic-product-children></atomic-product-children>
-                </atomic-product-section-children>
-              </template>
-            </atomic-product-template>
-          </atomic-commerce-recommendation-list>
-        </atomic-layout-section>
-      </atomic-commerce-layout>
-    `,
-  ],
-};
+export const AsCarousel: Story = {};
+// display
+// density
+// image Size
+
+//carousel
