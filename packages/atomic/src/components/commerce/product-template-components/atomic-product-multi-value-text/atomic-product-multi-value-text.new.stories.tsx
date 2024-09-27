7087b4c9fe972eab3a2b394fdb50d961652b0c12
Frederic Beaudoin <fbeaudoin@coveo.com>
2024-09-27 14:58:25 -0400

    Reorganize stories

--- packages/atomic/src/components/commerce/product-template-components/atomic-product-multi-value-text/atomic-product-multi-value-text.new.stories.tsx
+++ packages/atomic/src/components/commerce/product-template-components/atomic-product-multi-value-text/atomic-product-multi-value-text.new.stories.tsx
@@ -1,16 +1,16 @@
 import {wrapInCommerceInterface} from '@coveo/atomic-storybook-utils/commerce/commerce-interface-wrapper';
+import {wrapInCommerceProductList} from '@coveo/atomic-storybook-utils/commerce/commerce-product-list-wrapper';
+import {wrapInProductTemplate} from '@coveo/atomic-storybook-utils/commerce/commerce-product-template-wrapper';
 import {parameters} from '@coveo/atomic-storybook-utils/common/common-meta-parameters';
 import {renderComponent} from '@coveo/atomic-storybook-utils/common/render-component';
-import {wrapInProduct} from '@coveo/atomic-storybook-utils/product-wrapper';
 import {getSampleCommerceEngineConfiguration} from '@coveo/headless/commerce';
 import type {Meta, StoryObj as Story} from '@storybook/web-components';
-import {render} from 'lit';
-import {unsafeHTML} from 'lit/directives/unsafe-html.js';
 import {html} from 'lit/static-html.js';
 
 const baseConfiguration = getSampleCommerceEngineConfiguration();
 
-const {decorator: productDecorator} = wrapInProduct();
+const {decorator: productDecorator} = wrapInProductTemplate();
+const {decorator: commerceProductListDecorator} = wrapInCommerceProductList();
 const {decorator: commerceInterfaceDecorator, play} = wrapInCommerceInterface({
   engineConfig: {
     ...baseConfiguration,
@@ -29,7 +29,6 @@ const meta: Meta = {
   title: 'Atomic-Commerce/Product Template Components/MultiValueText',
   id: 'atomic-product-multi-value-text',
   render: renderComponent,
-  decorators: [commerceInterfaceDecorator],
   parameters,
   play,
 };
@@ -38,38 +37,40 @@ export default meta;
 
 export const Default: Story = {
   name: 'atomic-product-multi-value-text',
+  decorators: [
+    productDecorator,
+    commerceProductListDecorator,
+    commerceInterfaceDecorator,
+  ],
   args: {
     'attributes-field': 'cat_available_sizes',
   },
-  decorators: [productDecorator],
-};
-
-const {play: playSkipFirstSearch} = wrapInCommerceInterface({
-  skipFirstSearch: true,
-});
-
-export const BeforeFirstRequest: Story = {
-  name: 'Before first request',
-  decorators: [productDecorator],
-  play: playSkipFirstSearch,
 };
 
 export const WithMaxValuesToDisplaySetToMinimum: Story = {
   name: 'With max values to display set to minimum',
+  decorators: [
+    productDecorator,
+    commerceProductListDecorator,
+    commerceInterfaceDecorator,
+  ],
   args: {
     'attributes-field': 'cat_available_sizes',
     'attributes-max-values-to-display': 1,
   },
-  decorators: [productDecorator],
 };
 
 export const WithMaxValuesToDisplaySetToTotalNumberOfValues: Story = {
   name: 'With max values to display set to total number of values',
+  decorators: [
+    productDecorator,
+    commerceProductListDecorator,
+    commerceInterfaceDecorator,
+  ],
   args: {
     'attributes-field': 'cat_available_sizes',
     'attributes-max-values-to-display': 5,
   },
-  decorators: [productDecorator],
 };
 
 export const InAPageWithTheCorrespondingFacet: Story = {
@@ -78,33 +79,20 @@ export const InAPageWithTheCorrespondingFacet: Story = {
     'attributes-field': 'cat_available_sizes',
   },
   decorators: [
+    productDecorator,
+    commerceProductListDecorator,
     (story) => {
-      const tempProductTemplate = document.createElement('div');
-      render(html` ${story()}`, tempProductTemplate);
-      tempProductTemplate.replaceChildren(
-        ...Array.from(tempProductTemplate.children)
-      );
       return html`
         <atomic-commerce-layout>
           <atomic-layout-section section="facets"
             ><atomic-commerce-facets></atomic-commerce-facets>
           </atomic-layout-section>
           <atomic-layout-section section="main">
-            <atomic-commerce-product-list
-              display="list"
-              density="normal"
-              image-size="icon"
-              style="border: 2px dashed black; padding:20px;"
-            >
-              <atomic-product-template>
-                ${unsafeHTML(
-                  `<template>${tempProductTemplate.innerHTML}</template>`
-                )}
-              </atomic-product-template>
-            </atomic-commerce-product-list>
+            ${story()}
           </atomic-layout-section>
         </atomic-commerce-layout>
       `;
     },
+    commerceInterfaceDecorator,
   ],
 };
