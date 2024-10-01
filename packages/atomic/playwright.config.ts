252cbbde3f533576f1b299d6a964b11880be4410
Felix Perron-Brault <fpbrault@coveo.com>
2024-10-01 08:47:18 -0400

    fix tests
    https://coveord.atlassian.net/browse/KIT-3574

--- packages/atomic/playwright.config.ts
+++ packages/atomic/playwright.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
         command:
           process.env.DEPLOYMENT_ENVIRONMENT === 'CDN'
             ? 'cp -r ../headless/dist/browser ./dist-storybook/headless/ && cp -r ../bueno/dist/browser ./dist-storybook/bueno/ && npx ws -c ws.config.cjs'
-            : 'npx ws ./dist-storybook -p 4400',
+            : 'npx ws -d ./dist-storybook -p 4400',
         port: 4400,
         timeout: 120 * 1000,
         reuseExistingServer: !process.env.CI,
