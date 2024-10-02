9c43bbf2275385b6453461e0f2894982c99499cb
Frederic Beaudoin <fbeaudoin@coveo.com>
2024-10-02 13:31:41 -0400

    Adjust stories & tests
    https://coveord.atlassian.net/browse/KIT-3247

--- packages/atomic/src/components/commerce/atomic-commerce-product-list/atomic-commerce-product-list.new.stories.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-product-list/atomic-commerce-product-list.new.stories.tsx
@@ -4,28 +4,44 @@ import {
 } from '@coveo/atomic-storybook-utils/commerce/commerce-interface-wrapper';
 import {parameters} from '@coveo/atomic-storybook-utils/common/common-meta-parameters';
 import {renderComponent} from '@coveo/atomic-storybook-utils/common/render-component';
-import {CommerceEngineConfiguration} from '@coveo/headless/dist/definitions/commerce.index';
 import type {Meta, StoryObj as Story} from '@storybook/web-components';
 import {html} from 'lit-html/static.js';
 
 const {decorator, play} = wrapInCommerceInterface({skipFirstSearch: false});
 
-const noResultsEngineConfig: Partial<CommerceEngineConfiguration> = {
-  preprocessRequest: (r) => {
-    const parsed = JSON.parse(r.body as string);
-    // eslint-disable-next-line @cspell/spellchecker
-    parsed.query = 'xqyzpqwlnftguscaqmzpbyuagloxhf';
-    r.body = JSON.stringify(parsed);
-    return r;
-  },
-};
-
-const {play: playNoresults} = wrapInCommerceInterface({
+const {play: playNoProducts} = wrapInCommerceInterface({
   skipFirstSearch: false,
-  engineConfig: noResultsEngineConfig,
+  engineConfig: {
+    preprocessRequest: (r) => {
+      const parsed = JSON.parse(r.body as string);
+      parsed.query = 'show me no products';
+      r.body = JSON.stringify(parsed);
+      return r;
+    },
+  },
 });
 
 const meta: Meta = {
+  argTypes: {
+    'attributes-display': {
+      options: ['grid', 'list', 'table'],
+      control: {type: 'radio'},
+    },
+    'attributes-density': {
+      options: ['compact', 'comfortable', 'normal'],
+      control: {type: 'radio'},
+    },
+    'attributes-image-size': {
+      options: ['small', 'large', 'icon', 'none'],
+      control: {type: 'radio'},
+    },
+  },
+  args: {
+    'attributes-number-of-placeholders': 24,
+    'attributes-display': 'grid',
+    'attributes-density': 'normal',
+    'attributes-image-size': 'small',
+  },
   component: 'atomic-commerce-product-list',
   title: 'Atomic-Commerce/ProductList',
   id: 'atomic-commerce-product-list',
@@ -45,48 +61,73 @@ export const Default: Story = {
   },
 };
 
-const {play: playNoFirstSearch} = wrapInCommerceInterface({
+const {play: playNoFirstQuery} = wrapInCommerceInterface({
   skipFirstSearch: true,
-  engineConfig: noResultsEngineConfig,
 });
 
-export const NoFirstSearch: Story = {
-  name: 'atomic-commerce-product-list',
+export const BeforeQuery: Story = {
+  name: 'Before a query',
   play: async (context) => {
-    await playNoFirstSearch(context);
+    await playNoFirstQuery(context);
   },
 };
 
-export const OpenInNewTab: Story = {
-  name: 'Open Product in New Tab',
-  tags: ['test'],
-  args: {'attributes-grid-cell-link-target': '_blank'},
-  play: async (context) => {
-    await wrapInCommerceInterface({skipFirstSearch: true}).play(context);
-    await playExecuteFirstSearch(context);
-  },
-};
-
-export const NoResults: Story = {
-  name: 'No Results',
-  tags: ['test'],
+export const NoProducts: Story = {
+  name: 'With no products',
   decorators: [(story) => story()],
   play: async (context) => {
-    await playNoresults(context);
+    await playNoProducts(context);
     await playExecuteFirstSearch(context);
+  },
+};
+
+export const WithProductTemplate: Story = {
+  name: 'With a product template',
+  args: {
+    'slots-default': `<atomic-product-template>
+  <template>
+    <atomic-product-section-name>
+      <atomic-product-link class="font-bold"></atomic-product-link>
+    </atomic-product-section-name>
+    <atomic-product-section-visual>
+      <atomic-product-image></atomic-product-image>
+    </atomic-product-section-visual>
+    <atomic-product-section-metadata>
+      <atomic-product-field-condition if-defined="ec_brand">
+        <atomic-product-text field="ec_brand" class="text-neutral-dark block"></atomic-product-text>
+      </atomic-product-field-condition>
+      <atomic-product-rating></atomic-product-rating>
+    </atomic-product-section-metadata>
+    <atomic-product-section-emphasized>
+      <atomic-product-price></atomic-product-price>
+    </atomic-product-section-emphasized>
+    <atomic-product-section-children>
+      <atomic-product-children></atomic-product-children>
+    </atomic-product-section-children>
+  </template>
+</atomic-product-template>`,
   },
 };
 
 export const InPage: Story = {
   name: 'In a page',
-  tags: ['test'],
   decorators: [
     (story) =>
       html` <atomic-commerce-layout>
         <atomic-layout-section section="search">
           <atomic-commerce-search-box></atomic-commerce-search-box>
         </atomic-layout-section>
+        <atomic-layout-section section="facets"
+          ><atomic-commerce-facets></atomic-commerce-facets
+        ></atomic-layout-section>
         <atomic-layout-section section="main">
+          <atomic-layout-section section="status">
+            <atomic-commerce-breadbox></atomic-commerce-breadbox>
+            <atomic-commerce-query-summary></atomic-commerce-query-summary>
+            <atomic-commerce-sort-dropdown></atomic-commerce-sort-dropdown>
+            <atomic-commerce-did-you-mean></atomic-commerce-did-you-mean>
+            <atomic-commerce-refine-toggle></atomic-commerce-refine-toggle>
+          </atomic-layout-section>
           <atomic-layout-section section="products">
             ${story()}
             <atomic-commerce-query-error></atomic-commerce-query-error>
--- packages/atomic/src/components/commerce/atomic-commerce-product-list/e2e/atomic-commerce-product-list.e2e.ts
+++ packages/atomic/src/components/commerce/atomic-commerce-product-list/e2e/atomic-commerce-product-list.e2e.ts
@@ -1,44 +1,105 @@
 import {test, expect} from './fixture';
 
-test.describe('when no first search has yet been executed', async () => {
+test.describe('before the query is loaded', async () => {
   test.beforeEach(async ({productList}) => {
-    await productList.load({story: 'no-first-search'});
+    await productList.load({story: 'before-query'});
     await productList.hydrated.waitFor();
   });
 
-  test('should display placeholders', async ({productList}) => {
-    await expect(productList.placeholders.first()).toBeVisible();
+  test('should be a11y compliant', async ({makeAxeBuilder}) => {
+    const accessibilityResults = await makeAxeBuilder().analyze();
+    expect(accessibilityResults.violations.length).toEqual(0);
   });
 
-  test('should display the default amount of placeholders', async ({
+  test('should show the default number of placeholders', async ({
     productList,
   }) => {
-    await expect(productList.placeholders.nth(23)).toBeVisible();
+    await expect
+      .poll(async () => await productList.placeholders.count())
+      .toBe(24);
+    for (const placeholder of await productList.placeholders.all()) {
+      await expect(placeholder).toBeVisible();
+    }
+  });
+
+  test('should show the specified number of placeholders', async ({
+    productList,
+  }) => {
+    await productList.load({
+      story: 'before-query',
+      args: {numberOfPlaceholders: 1},
+    });
+    await productList.hydrated.waitFor();
+
+    await expect
+      .poll(async () => await productList.placeholders.count())
+      .toBe(1);
+
+    await expect(productList.placeholders.first()).toBeVisible();
   });
 });
 
-test.describe('when executing an initial search', () => {
-  test.beforeEach(async ({productList, searchBox}) => {
-    await productList.load({story: 'in-page'});
-    await searchBox.searchInput.fill('pants');
-    await searchBox.submitButton.click();
+test.describe('after a query that yields products', () => {
+  test.beforeEach(async ({productList}) => {
+    await productList.load({story: 'default'});
     await productList.hydrated.waitFor();
   });
 
-  test('should not display placeholders', async ({productList}) => {
-    await expect(productList.placeholders.first()).not.toBeVisible();
+  test('should be a11y compliant', async ({makeAxeBuilder}) => {
+    const accessibilityResults = await makeAxeBuilder().analyze();
+    expect(accessibilityResults.violations.length).toEqual(0);
+  });
+
+  test('should not have any placeholders', async ({productList}) => {
+    await expect
+      .poll(async () => await productList.placeholders.count())
+      .toBe(0);
+  });
+
+  test('should show all products', async ({productList}) => {
+    await expect
+      .poll(async () => await productList.products.count())
+      .toBeGreaterThan(0);
+    for (const product of await productList.products.all()) {
+      await expect(product).toBeVisible();
+    }
   });
 });
 
-test.describe('when interface load yields no products', () => {
+test.describe('after a query that yields no products', () => {
   test.beforeEach(async ({productList}) => {
-    await productList.load({story: 'no-results'});
+    await productList.withNoProducts();
+    await productList.load({story: 'default'});
   });
 
-  test('should not display placeholders', async ({productList}) => {
-    await expect(productList.placeholders.first()).not.toBeVisible();
-    await expect(productList.placeholders.nth(23)).not.toBeVisible();
+  test('should not have any placeholders', async ({productList}) => {
+    await expect
+      .poll(async () => await productList.placeholders.count())
+      .toBe(0);
+  });
+
+  test('should not have any products', async ({productList}) => {
+    await expect.poll(async () => await productList.products.count()).toBe(0);
   });
 });
 
-// TODO: KIT-3247 add the rest of E2E tests
+test.describe('with a product template', () => {
+  test.beforeEach(async ({productList}) => {
+    await productList.load({story: 'with-product-template'});
+    await productList.hydrated.waitFor();
+  });
+
+  test('should be a11y compliant', async ({makeAxeBuilder}) => {
+    const accessibilityResults = await makeAxeBuilder().analyze();
+    expect(accessibilityResults.violations.length).toEqual(0);
+  });
+
+  test('should show all products', async ({productList}) => {
+    await expect
+      .poll(async () => await productList.products.count())
+      .toBeGreaterThan(0);
+    for (const product of await productList.products.all()) {
+      await expect(product).toBeVisible();
+    }
+  });
+});
