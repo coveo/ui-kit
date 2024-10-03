fc56a26bcb73321ebf4e62a14274e05d4c9a4128
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2024-10-03 12:14:54 -0400

    docs(headless): basic typedoc setup
    https://coveord.atlassian.net/browse/KIT-3422

---
+++ packages/headless/typedoc/lib/insertAtomicSearchBox.ts
@@ -0,0 +1,61 @@
+interface AtomicSearchInterfaceElement extends HTMLElement {
+  initialize(config: {
+    organizationId: string;
+    organizationEndpoints: OrganizationEndpoints;
+    accessToken: string;
+    search: {searchHub: string};
+    analytics: {originLevel2: string};
+  }): Promise<void>;
+  getOrganizationEndpoints(
+    organizationId: string
+  ): Promise<OrganizationEndpoints>;
+}
+
+interface OrganizationEndpoints {
+  platform: string;
+  analytics: string;
+  search: string;
+  admin: string;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'atomic-search-interface': AtomicSearchInterfaceElement;
+  }
+}
+
+export function insertAtomicSearchBox() {
+  document.addEventListener('DOMContentLoaded', () => {
+    const typedocSearchBox = document.getElementById('tsd-search');
+    if (typedocSearchBox) {
+      typedocSearchBox.innerHTML = '';
+      const searchInterface = document.createElement('atomic-search-interface');
+      const searchBox = document.createElement('atomic-search-box');
+      searchBox.setAttribute(
+        'redirection-url',
+        'https://docs.coveo.com/en/search'
+      );
+      searchInterface.appendChild(searchBox);
+      typedocSearchBox.appendChild(searchInterface);
+
+      (async () => {
+        await customElements.whenDefined('atomic-search-interface');
+        const searchInterfaceElement = document.querySelector(
+          'atomic-search-interface'
+        );
+        if (searchInterfaceElement) {
+          await searchInterfaceElement.initialize({
+            organizationId: 'coveosearch',
+            organizationEndpoints:
+              await searchInterfaceElement.getOrganizationEndpoints(
+                'coveosearch'
+              ),
+            accessToken: 'xx6ac9d08f-eb9a-48d5-9240-d7c251470c93',
+            search: {searchHub: 'Coveo Docs Unified Search'},
+            analytics: {originLevel2: 'All'},
+          });
+        }
+      })();
+    }
+  });
+}
