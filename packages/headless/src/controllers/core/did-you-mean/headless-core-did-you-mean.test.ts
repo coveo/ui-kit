d3c205336795dd7043c4cf11807c81140b5782ca
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-10-04 09:51:36 -0400

    fix(atomic): use `next` as default value for queryCorrectionMode in atomic-did-you-mean
    https://coveord.atlassian.net/browse/KIT-3630

--- packages/headless/src/controllers/core/did-you-mean/headless-core-did-you-mean.test.ts
+++ packages/headless/src/controllers/core/did-you-mean/headless-core-did-you-mean.test.ts
@@ -3,6 +3,7 @@ import {
   applyDidYouMeanCorrection,
   disableAutomaticQueryCorrection,
   enableDidYouMean,
+  setCorrectionMode,
 } from '../../../features/did-you-mean/did-you-mean-actions.js';
 import {didYouMeanReducer as didYouMean} from '../../../features/did-you-mean/did-you-mean-slice.js';
 import {
@@ -13,6 +14,7 @@ import {createMockState} from '../../../test/mock-state.js';
 import {
   buildCoreDidYouMean,
   DidYouMean,
+  DidYouMeanOptions,
   DidYouMeanProps,
 } from './headless-core-did-you-mean.js';
 
@@ -86,5 +88,18 @@ describe('did you mean', () => {
 
     dym.applyCorrection();
     expect(applyDidYouMeanCorrection).toHaveBeenCalledWith('bar');
+  });
+
+  it('should dispatch setCorrectionMode with the correct value', () => {
+    const options = {queryCorrectionMode: 'legacy'} as DidYouMeanOptions;
+    initDidYouMean({options});
+
+    expect(engine.dispatch).toHaveBeenCalledWith(setCorrectionMode('legacy'));
+  });
+
+  it('should dispatch setCorrectionMode with the default value "next" when no mode is provided', () => {
+    initDidYouMean();
+
+    expect(engine.dispatch).toHaveBeenCalledWith(setCorrectionMode('next'));
   });
 });
