ea2d0dbf5af6090f780e5295748a1bed67f3f580
Felix Perron-Brault <fpbrault@coveo.com>
2024-10-07 15:00:17 -0400

    hide tab area dynamically
    https://coveord.atlassian.net/browse/KIT-3548

--- packages/atomic/src/components/common/tab-manager/tab-dropdown.tsx
+++ packages/atomic/src/components/common/tab-manager/tab-dropdown.tsx
@@ -1,9 +1,11 @@
 import {FunctionalComponent, h} from '@stencil/core';
+import TabsIcon from '../../../images/arrow-bottom-rounded.svg';
 
 export interface TabDropdownProps {
   tabs: Array<{name: string; label: string}>;
   activeTab: string;
   onTabChange: (tabName: string) => void;
+  class?: string;
 }
 
 export const TabDropdown: FunctionalComponent<TabDropdownProps> = (
@@ -12,18 +14,29 @@ export const TabDropdown: FunctionalComponent<TabDropdownProps> = (
 ) => {
   return (
     <div
-      class="tab-dropdown-area hidden border-b pb-1"
+      class={`tab-dropdown-area relative ${props.class || ''}`}
       aria-label="tab-dropdown-area"
+      part="dropdown-area"
     >
       <select
         aria-label="tab-dropdown"
-        class="btn-text-primary cursor-pointer py-2 text-xl font-bold"
+        class="btn-outline-neutral w-full grow cursor-pointer appearance-none rounded-lg px-6 py-3 text-xl font-bold"
         onChange={(e) =>
           props.onTabChange((e.target as HTMLSelectElement).value)
         }
       >
         {children}
       </select>
+      <div
+        part="select-icon-wrapper"
+        class="pointer-events-none absolute bottom-0 right-0 top-0 flex items-center justify-center pr-6"
+      >
+        <atomic-icon
+          part="select-icon"
+          icon={TabsIcon}
+          class="ml-4 w-3 shrink-0 self-center"
+        ></atomic-icon>
+      </div>
     </div>
   );
 };
