ddce0049e08eeee1a9a3f959a09aad34a0079fae
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-10-09 09:20:20 -0400

    fix(atomic, headless): use `next` as default value for queryCorrectionMode (#4495)
    https://coveord.atlassian.net/browse/KIT-3630
    
    This was also not done in headless during our sprints. We never looked
    for V3 mentions in comments.

---
+++ packages/atomic/src/components/search/atomic-did-you-mean/e2e/page-object.ts
@@ -0,0 +1,8 @@
+import {Page} from '@playwright/test';
+import {BasePageObject} from '../../../../../playwright-utils/base-page-object';
+
+export class DidYouMeanPageObject extends BasePageObject<'atomic-did-you-mean'> {
+  constructor(page: Page) {
+    super(page, 'atomic-did-you-mean');
+  }
+}
