145d3a39859f3079f31334ec4daa344068902294
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-10-16 09:41:38 -0400

    test

--- packages/atomic/src/components/search/facets/atomic-facet/atomic-facet.tsx
+++ packages/atomic/src/components/search/facets/atomic-facet/atomic-facet.tsx
@@ -100,6 +100,7 @@ import {Bindings} from '../../atomic-search-interface/atomic-search-interface';
  * @part show-more - The show more results button.
  * @part show-less - The show less results button.
  * @part show-more-less-icon - The icons of the show more & show less buttons.
+ * test
  */
 @Component({
   tag: 'atomic-facet',
