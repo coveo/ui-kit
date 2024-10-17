b7b86a573b0663f7ba1aa2a64da4549efad04285
simonmilord <simon.milord@gmail.com>
2024-10-17 12:01:43 -0400

    fix notifications cypress tests

--- packages/quantic/cypress/e2e/default-1/notifications/notifications.cypress.ts
+++ packages/quantic/cypress/e2e/default-1/notifications/notifications.cypress.ts
@@ -4,36 +4,55 @@ import {
   interceptSearch,
   mockSearchWithNotifyTrigger,
 } from '../../../page-objects/search';
-import {useCaseParamTest} from '../../../page-objects/use-case';
 import {NotificationsExpectations as Expect} from './notifications-expectations';
 
 const exampleNotifications = ['Notification one', 'Notification two'];
 
+interface NotificationsOptions {
+  useCase: string;
+}
+
+const enum useCaseEnum {
+  search = 'search',
+  insight = 'insights',
+}
+
+const useCaseParamTest = [
+  {
+    useCase: useCaseEnum.search,
+    label: 'with search use case',
+  },
+  {
+    useCase: useCaseEnum.insight,
+    label: 'with insight use case',
+  },
+];
+
 describe('quantic-notifications', () => {
   const pageUrl = 's/quantic-notifications';
 
-  function visitNotifications() {
+  function visitNotifications(options: Partial<NotificationsOptions>) {
     interceptSearch();
     cy.visit(pageUrl);
-    configure();
+    configure(options);
   }
 
   useCaseParamTest.forEach((param) => {
     describe(`when no notification is fired by the pipeline trigger ${param.label}`, () => {
       it('should not render any notification', () => {
-        visitNotifications();
+        visitNotifications({useCase: param.useCase});
 
-        cy.wait(getQueryAlias());
+        cy.wait(getQueryAlias(param.useCase));
         Expect.displayNotifications(false);
       });
     });
 
     describe(`when some notifications are fired by the pipeline trigger for the ${param.label}`, () => {
       it('should render the notifications', () => {
-        visitNotifications();
+        visitNotifications({useCase: param.useCase});
         mockSearchWithNotifyTrigger(param.useCase, exampleNotifications);
 
-        cy.wait(getQueryAlias());
+        cy.wait(getQueryAlias(param.useCase));
         Expect.displayNotifications(true);
         Expect.logQueryPipelineTriggersNotification(exampleNotifications);
         exampleNotifications.forEach((notification, index) => {
