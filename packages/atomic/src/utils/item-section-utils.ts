f9202e7460619473f84c51fe75fbf1229b8ad99f
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-10-29 08:56:24 -0400

    change in /utils
    https://coveord.atlassian.net/browse/KIT-3684

--- packages/atomic/src/utils/item-section-utils.ts
+++ packages/atomic/src/utils/item-section-utils.ts
@@ -1,5 +1,6 @@
 import {containsVisualElement} from './utils';
 
 export function hideEmptySection(element: HTMLElement) {
+  console.log('change in /utils');
   element.style.display = containsVisualElement(element) ? '' : 'none';
 }
