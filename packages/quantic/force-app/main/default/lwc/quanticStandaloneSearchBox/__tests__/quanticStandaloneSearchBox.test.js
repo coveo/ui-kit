2ed2b5ee5648aed58eaa8de58af01ea4f854d792
simonmilord <simon.milord@gmail.com>
2024-10-30 13:06:43 -0400

    added unit tests for standalone searchbox

---
+++ packages/quantic/force-app/main/default/lwc/quanticStandaloneSearchBox/__tests__/quanticStandaloneSearchBox.test.js
@@ -0,0 +1,153 @@
+/* eslint-disable no-import-assign */
+import QuanticStandaloneSearchBox from 'c/quanticStandaloneSearchBox';
+// @ts-ignore
+import {createElement} from 'lwc';
+import * as mockHeadlessLoader from 'c/quanticHeadlessLoader';
+
+jest.mock('c/quanticHeadlessLoader');
+
+let isInitialized = false;
+
+const exampleEngine = {
+  id: 'engineId__standalone',
+};
+
+const functionsMocks = {
+  buildStandaloneSearchBox: jest.fn(() => ({
+    state: {},
+    subscribe: functionsMocks.subscribe,
+  })),
+  loadDependencies: jest.fn(
+    () =>
+      new Promise((resolve) => {
+        resolve();
+      })
+  ),
+  subscribe: jest.fn((cb) => {
+    cb();
+    return functionsMocks.unsubscribe;
+  }),
+  unsubscribe: jest.fn(() => {}),
+};
+
+const defaultOptions = {
+  engineId: exampleEngine.id,
+  placeholder: null,
+  withoutSubmitButton: false,
+  numberOfSuggestions: 7,
+  textarea: false,
+  disableRecentQueries: false,
+  keepFiltersOnSearch: false,
+};
+
+function createTestComponent(options = defaultOptions) {
+  prepareHeadlessState();
+
+  const element = createElement('c-quantic-standalone-search-box', {
+    is: QuanticStandaloneSearchBox,
+  });
+  for (const [key, value] of Object.entries(options)) {
+    element[key] = value;
+  }
+  document.body.appendChild(element);
+  return element;
+}
+
+function prepareHeadlessState() {
+  // @ts-ignore
+  mockHeadlessLoader.loadDependencies = functionsMocks.loadDependencies;
+  // @ts-ignore
+  global.CoveoHeadless = {
+    buildStandaloneSearchBox: functionsMocks.buildStandaloneSearchBox,
+    subscribe: functionsMocks.subscribe,
+  };
+}
+
+// Helper function to wait until the microtask queue is empty.
+function flushPromises() {
+  // eslint-disable-next-line @lwc/lwc/no-async-operation
+  return new Promise((resolve) => setTimeout(resolve, 0));
+}
+
+function mockSuccessfulHeadlessInitialization() {
+  // @ts-ignore
+  mockHeadlessLoader.initializeWithHeadless = (element, _, initialize) => {
+    if (element instanceof QuanticStandaloneSearchBox && !isInitialized) {
+      isInitialized = true;
+      initialize(exampleEngine);
+    }
+  };
+}
+
+function cleanup() {
+  // The jsdom instance is shared across test cases in a single file so reset the DOM
+  while (document.body.firstChild) {
+    document.body.removeChild(document.body.firstChild);
+  }
+  jest.clearAllMocks();
+  isInitialized = false;
+}
+
+describe('c-quantic-standalone-search-box', () => {
+  beforeAll(() => {
+    mockSuccessfulHeadlessInitialization();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('construct itself without throwing', () => {
+    expect(() =>
+      createElement('c-quantic-standalone-search-box', {
+        is: QuanticStandaloneSearchBox,
+      })
+    ).not.toThrow();
+  });
+
+  describe('controller initialization', () => {
+    it('should subscribe to the headless state changes', async () => {
+      createTestComponent();
+      await flushPromises();
+
+      expect(functionsMocks.subscribe).toHaveBeenCalledTimes(1);
+    });
+
+    describe('when keepFiltersOnSearch is false (default)', () => {
+      it('should properly initialize the controller with clear filters enabled', async () => {
+        createTestComponent();
+        await flushPromises();
+
+        expect(functionsMocks.buildStandaloneSearchBox).toHaveBeenCalledTimes(
+          1
+        );
+        expect(functionsMocks.buildStandaloneSearchBox).toHaveBeenCalledWith(
+          exampleEngine,
+          expect.objectContaining({
+            options: expect.objectContaining({clearFilters: true}),
+          })
+        );
+      });
+    });
+
+    describe('when keepFiltersOnSearch is true', () => {
+      it('should properly initialize the controller with clear filters disabled', async () => {
+        createTestComponent({
+          ...defaultOptions,
+          keepFiltersOnSearch: true,
+        });
+        await flushPromises();
+
+        expect(functionsMocks.buildStandaloneSearchBox).toHaveBeenCalledTimes(
+          1
+        );
+        expect(functionsMocks.buildStandaloneSearchBox).toHaveBeenCalledWith(
+          exampleEngine,
+          expect.objectContaining({
+            options: expect.objectContaining({clearFilters: false}),
+          })
+        );
+      });
+    });
+  });
+});
