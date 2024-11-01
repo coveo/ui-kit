fb51d0de9e0e6eacd0eb72d0cca1d641db08b403
Felix Perron-Brault <fpbrault@coveo.com>
2024-11-01 16:25:28 -0400

    fix commerce recs clicks

--- packages/atomic/src/components/commerce/atomic-commerce-recommendation-list/atomic-commerce-recommendation-list.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-recommendation-list/atomic-commerce-recommendation-list.tsx
@@ -323,10 +323,7 @@ export class AtomicCommerceRecommendationList
         this.imageSize
       ),
       content: this.productTemplateProvider.getTemplateContent(product),
-      linkContent:
-        this.display === 'grid'
-          ? this.productTemplateProvider.getLinkTemplateContent(product)
-          : this.productTemplateProvider.getEmptyLinkTemplateContent(),
+      linkContent: this.productTemplateProvider.getLinkTemplateContent(product),
       store: this.bindings.store,
       density: this.density,
       display: this.display,
