a1d7ce435365ab103ef87f43444a22a8789269ce
Frederic Beaudoin <fbeaudoin@coveo.com>
2024-11-04 14:17:44 -0500

    Add / export useAppHistoryRouter hook
    https://coveord.atlassian.net/browse/KIT-3462

--- packages/headless-react/src/ssr-commerce/client-utils.ts
+++ packages/headless-react/src/ssr-commerce/client-utils.ts
@@ -1,6 +1,13 @@
 'use client';
 
-import {DependencyList, useEffect, useReducer, useRef} from 'react';
+import {
+  DependencyList,
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+} from 'react';
 
 /**
  * Subscriber is a function that takes a single argument, which is another function `listener` that returns `void`. The Subscriber function itself returns another function that can be used to unsubscribe the `listener`.
@@ -62,4 +69,28 @@ export function useSyncMemoizedStore<T>(
   }
 
   return snapshot.current;
+}
+
+function getUrl() {
+  if (typeof window === 'undefined') {
+    return null;
+  }
+  return new URL(document.location.href);
+}
+
+export function useAppHistoryRouter() {
+  const [url, updateUrl] = useReducer(() => getUrl(), getUrl());
+  useEffect(() => {
+    window.addEventListener('popstate', updateUrl);
+    return () => window.removeEventListener('popstate', updateUrl);
+  }, []);
+  const replace = useCallback(
+    (href: string) => window.history.replaceState(null, document.title, href),
+    []
+  );
+  const push = useCallback(
+    (href: string) => window.history.pushState(null, document.title, href),
+    []
+  );
+  return useMemo(() => ({url, replace, push}), [url, replace, push]);
 }
