18efa1f36a07c52b0182972f69fa0d0804b991e5
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-11-11 15:35:47 -0500

    Update product-list.tsx
    https://coveord.atlassian.net/browse/KIT-3719

--- packages/samples/headless-ssr-commerce/components/product-list.tsx
+++ packages/samples/headless-ssr-commerce/components/product-list.tsx
@@ -4,14 +4,21 @@ import {useCart, useProductList} from '@/lib/commerce-engine';
 import {addToCart} from '@/utils/cart';
 import {Product} from '@coveo/headless-react/ssr-commerce';
 import Image from 'next/image';
+import {useRouter} from 'next/navigation';
 
 export default function ProductList() {
   const {state, methods} = useProductList();
   const {methods: cartMethods} = useCart();
 
+  const router = useRouter();
+
   const onProductClick = (product: Product) => {
     methods?.interactiveProduct({options: {product}}).select();
+    router.push(
+      `/products/${product.ec_product_id}?name=${product.ec_name}&price=${product.ec_price}`
+    );
   };
+
   return (
     <ul>
       {state.products.map((product) => (
