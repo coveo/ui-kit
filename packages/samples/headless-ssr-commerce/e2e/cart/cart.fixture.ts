5caf0bbc173ebb56fbcfef4a9a5d15aee5579d93
Felix Perron-Brault <fpbrault@coveo.com>
2024-11-11 18:13:27 -0500

    add cart tests
    https://coveord.atlassian.net/browse/KIT-3410

--- packages/samples/headless-ssr-commerce/e2e/cart/cart.fixture.ts
+++ packages/samples/headless-ssr-commerce/e2e/cart/cart.fixture.ts
@@ -1,18 +1,13 @@
 import {test as base} from '@playwright/test';
 import {CartPageObject as Cart} from '../page-objects/cart.page';
-import {SearchPageObject as Search} from '../page-objects/search.page';
 
 type MyFixtures = {
   cart: Cart;
-  search: Search;
 };
 
 export const test = base.extend<MyFixtures>({
   cart: async ({page}, use) => {
     await use(new Cart(page));
-  },
-  search: async ({page}, use) => {
-    await use(new Search(page));
   },
 });
 export {expect} from '@playwright/test';
