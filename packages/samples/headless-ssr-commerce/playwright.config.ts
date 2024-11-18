4486a763783d5944180953897b7460fad3a66191
Felix Perron-Brault <fpbrault@coveo.com>
2024-11-18 10:40:54 -0500

    adjust playwright config
    https://coveord.atlassian.net/browse/KIT-3410

--- packages/samples/headless-ssr-commerce/playwright.config.ts
+++ packages/samples/headless-ssr-commerce/playwright.config.ts
@@ -17,9 +17,13 @@ export default defineConfig({
       use: {...devices['Desktop Chrome']},
     },
   ],
+  expect: {
+    timeout: 7 * 1000,
+  },
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run prod',
     port: 3000,
+    timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 });
