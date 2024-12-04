d3b7a70189b26be7016f27c561d09952e63d85a2
simonmilord <simon.milord@gmail.com>
2024-12-04 12:16:04 -0500

    try something

--- packages/quantic/scripts/read-env.js
+++ packages/quantic/scripts/read-env.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
+const core = require('@actions/core');
 
 const envFilePath = path.resolve(__dirname, '../../quantic/.env');
 dotenv.config({path: envFilePath});
@@ -11,5 +12,8 @@ for (const key in envVariables) {
   // eslint-disable-next-line no-prototype-builtins
   if (envVariables.hasOwnProperty(key)) {
     console.log(`${key}=${envVariables[key]}`);
+    if (key === 'BASE_URL') {
+      core.setOutput('BASE_URL', envVariables[key]);
+    }
   }
 }
