f0b3d5538a8deb213a43e27a6df1d8fa38b848e6
Felix Perron-Brault <fpbrault@coveo.com>
2024-12-04 10:58:29 -0500

    cleanup
    https://coveord.atlassian.net/browse/KIT-3767

--- packages/atomic/src/components/common/radio-button.tsx
+++ packages/atomic/src/components/common/radio-button.tsx
@@ -9,7 +9,7 @@ import {
 
 export interface RadioButtonProps {
   groupName: string;
-  onKeyDown?(event: KeyboardEvent): void;
+  selectWhenFocused?: boolean;
   onChecked?(): void;
   style?: ButtonStyle;
   key?: string | number;
@@ -40,6 +40,61 @@ export const RadioButton: FunctionalComponent<RadioButtonProps> = (props) => {
     classNames.push(props.class);
   }
 
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (props.selectWhenFocused !== false) {
+      return;
+    }
+    const {key} = event;
+    const radioGroup = (event.currentTarget as HTMLElement).parentNode;
+
+    if (!radioGroup || !isArrowKey(key)) {
+      return;
+    }
+
+    event.preventDefault();
+
+    const buttons = getRadioButtons(radioGroup);
+    const currentIndex = getCurrentIndex(
+      buttons,
+      event.currentTarget as HTMLInputElement
+    );
+    const newIndex = getNewIndex(key, currentIndex, buttons.length);
+
+    if (buttons[newIndex]) {
+      buttons[newIndex].focus();
+    }
+  };
+
+  const isArrowKey = (key: string) => {
+    return ['ArrowLeft', 'ArrowRight', 'ArrowDown', 'ArrowUp'].includes(key);
+  };
+
+  const getRadioButtons = (radioGroup: ParentNode) => {
+    return Array.from(
+      radioGroup.querySelectorAll('[type="radio"]')
+    ) as HTMLInputElement[];
+  };
+
+  const getCurrentIndex = (
+    buttons: HTMLInputElement[],
+    currentButton: HTMLInputElement
+  ) => {
+    return buttons.findIndex((button) => button === currentButton);
+  };
+
+  const getNewIndex = (key: string, currentIndex: number, length: number) => {
+    switch (key) {
+      case 'ArrowLeft':
+      case 'ArrowUp':
+        return (currentIndex - 1 + length) % length;
+      case 'ArrowRight':
+      case 'ArrowDown':
+        return (currentIndex + 1) % length;
+      default:
+        return currentIndex;
+    }
+  };
+
   const attributes = {
     name: props.groupName,
     key: props.key,
@@ -54,7 +109,7 @@ export const RadioButton: FunctionalComponent<RadioButtonProps> = (props) => {
 
   return (
     <input
-      onKeyDown={props.onKeyDown}
+      onKeyDown={handleKeyDown}
       type="radio"
       onChange={(e) =>
         (e.currentTarget as HTMLInputElement).checked && props.onChecked?.()
