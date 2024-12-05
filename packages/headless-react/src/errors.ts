63b822e60eca075275f6221d09154ac2b99580e1
ylakhdar <ylakhdar@coveo.com>
2024-12-05 14:43:18 -0500

    review correction
    https://coveord.atlassian.net/browse/KIT-3783

--- packages/headless-react/src/errors.ts
+++ packages/headless-react/src/errors.ts
@@ -3,7 +3,7 @@ import {capitalize} from './utils.js';
 
 export class MissingEngineProviderError extends Error {
   static message =
-    'Unable to find Context. Please make sure you are wrapping your component with either `StaticStateProvider` or `HydratedStateProvider` component that can provide the required context.';
+    'Unable to find Context. Please make sure you are wrapping your controller with either `StaticStateProvider` or `HydratedStateProvider` controller that can provide the required context.';
   constructor() {
     super(MissingEngineProviderError.message);
   }
@@ -14,16 +14,16 @@ export class UndefinedControllerError extends Error {
     const supportedEngineDefinitionList = solutionTypes.map(
       (solutionType) => `${solutionType}EngineDefinition`
     );
-    return `This component is only available in these engine definitions:\n${supportedEngineDefinitionList.map((def) => ` • ${def}`).join('\n')}`;
+    return `This controller is only available in these engine definitions:\n${supportedEngineDefinitionList.map((def) => ` • ${def}`).join('\n')}`;
   }
 
   constructor(controllerName: string, solutionTypes: SolutionType[]) {
     super(
       [
-        `You're importing a component (use${capitalize(controllerName)}) that is not defined in the current engine definition`,
+        `You're importing a controller (use${capitalize(controllerName)}) that is not defined in the current engine definition`,
         UndefinedControllerError.createEngineSupportMessage(solutionTypes),
         '',
-        'Ensure that the component is wrapped in the appropriate State Provider.',
+        'Ensure that the controller is wrapped in the appropriate State Provider.',
         // 'Learn more: TODO: Add link to documentation on how to use hooks with providers',
       ].join('\n')
     );
