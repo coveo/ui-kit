44eed9bd252d2a84a25a3c59cc54ac8d5099a192
Louis Bompart <lbompart@coveo.com>
2024-12-10 22:12:15 -0500

    derp

--- packages/atomic/src/components/search/index.ts
+++ packages/atomic/src/components/search/index.ts
@@ -1,5 +1,5 @@
 export default {
-  'atomic-text': async () => await import('./atomic-text/atomic-text'),
+  'atomic-text': async () => await import('./atomic-text/atomic-text.js'),
 } as Record<string, () => Promise<unknown>>;
 
 export type {AtomicText} from './atomic-text/atomic-text';
