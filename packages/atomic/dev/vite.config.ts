dc1db44b41b8c72ac4ab035d84e3d1a73070e070
Louis Bompart <lbompart@coveo.com>
2024-12-11 22:46:42 -0500

    change app type

--- packages/atomic/dev/vite.config.ts
+++ packages/atomic/dev/vite.config.ts
@@ -3,6 +3,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig({
   publicDir: resolve(import.meta.dirname, '../dist/atomic'),
+  appType: 'mpa',
   server: {
     port: 3333,
   },
