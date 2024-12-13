87823770caf36ca7114f31486e34d4d2f9b77794
simonmilord <simon.milord@gmail.com>
2024-12-13 15:50:16 -0500

    added unit tests for smartsnippets

--- packages/quantic/force-app/main/default/lwc/quanticSmartSnippet/__tests__/quanticSmartSnippet.test.js
+++ packages/quantic/force-app/main/default/lwc/quanticSmartSnippet/__tests__/quanticSmartSnippet.test.js
@@ -17,8 +17,18 @@ const defaultOptions = {
   engineId: exampleEngine.id,
 };
 
+const showMoreLabel = 'Show more';
+const showLessLabel = 'Show less';
+const showMoreIcon = 'utility:chevrondown';
+const showLessIcon = 'utility:chevronup';
+
 const selectors = {
   initializationError: 'c-quantic-component-error',
+  smartSnippet: 'c-quantic-smart-snippet',
+  smartSnippetAnswer: 'c-quantic-smart-snippet-answer',
+  smartSnippetToggleButton: '[data-id="smart-snippet-answer-toggle"]',
+  smartSnippetSource: 'c-quantic-smart-snippet-source',
+  smartSnippetFeedback: 'c-quantic-feedback',
 };
 
 const defaultSearchStatusState = {
@@ -29,7 +39,15 @@ const defaultSearchStatusState = {
 let searchStatusState = defaultSearchStatusState;
 
 const defaultSmartSnippetState = {
-  isActive: false,
+  question: 'Where was Gondor when Westfold fell?',
+  answer: 'Gondor was obviously busy with other things.',
+  documentId: '1',
+  expanded: false,
+  answerFound: true,
+  liked: false,
+  disliked: false,
+  feedbackModalOpen: false,
+  source: null, // TODO: add a source (Type Result)
 };
 let smartSnippetState = defaultSmartSnippetState;
 
@@ -37,7 +55,8 @@ const functionsMocks = {
   buildSmartSnippet: jest.fn(() => ({
     state: smartSnippetState,
     subscribe: functionsMocks.smartSnippetStateSubscriber,
-    select: jest.fn(),
+    expand: jest.fn(),
+    collapse: jest.fn(),
   })),
   buildSearchStatus: jest.fn(() => ({
     state: searchStatusState,
@@ -168,6 +187,125 @@ describe('c-quantic-smart-snippet', () => {
       );
 
       expect(initializationError).not.toBeNull();
+    });
+  });
+
+  describe('when the query does not return a smart snippet', () => {
+    beforeAll(() => {
+      smartSnippetState = {...defaultSmartSnippetState, answerFound: false};
+    });
+    it('should not display the smart snippet component', async () => {
+      const element = createTestComponent();
+      await flushPromises();
+
+      const smartSnippet = element.shadowRoot.querySelector(
+        selectors.smartSnippet
+      );
+      expect(smartSnippet).toBeNull();
+    });
+  });
+
+  describe('when the query does return a smart snippet', () => {
+    describe('when the smart snippet is collapsed', () => {
+      beforeAll(() => {
+        smartSnippetState = {...defaultSmartSnippetState, expanded: false};
+      });
+
+      it('should properly display the smart snippet component', async () => {
+        const element = createTestComponent();
+        await flushPromises();
+
+        const smartSnippetAnswer = element.shadowRoot.querySelector(
+          selectors.smartSnippetAnswer
+        );
+        // smartSnippetAnswer.height = 500;
+        expect(smartSnippetAnswer).not.toBeNull();
+
+        const smartSnippetToggleButton = element.shadowRoot.querySelector(
+          selectors.smartSnippetToggleButton
+        );
+        expect(smartSnippetToggleButton).not.toBeNull();
+        expect(smartSnippetToggleButton.label).toBe(showMoreLabel);
+        expect(smartSnippetToggleButton.iconName).toBe(showMoreIcon);
+
+        const smartSnippetSource = element.shadowRoot.querySelector(
+          selectors.smartSnippetSource
+        );
+        expect(smartSnippetSource).not.toBeNull();
+
+        const smartSnippetFeedback = element.shadowRoot.querySelector(
+          selectors.smartSnippetFeedback
+        );
+        expect(smartSnippetFeedback).not.toBeNull();
+      });
+
+      describe('when clicking on the smart snippet toggle button', () => {
+        it('should call the expand method from the smartSnippet controller', async () => {
+          const element = createTestComponent();
+          await flushPromises();
+
+          const smartSnippetToggleButton = element.shadowRoot.querySelector(
+            selectors.smartSnippetToggleButton
+          );
+
+          await smartSnippetToggleButton.click();
+          await flushPromises();
+
+          expect(functionsMocks.buildSmartSnippet.expand).toHaveBeenCalledTimes(
+            1
+          );
+        });
+      });
+    });
+
+    describe('when the smart snippet is expanded', () => {
+      beforeAll(() => {
+        smartSnippetState = {...defaultSmartSnippetState, expanded: true};
+      });
+      it('should properly display the smart snippet component', async () => {
+        const element = createTestComponent();
+        await flushPromises();
+
+        const smartSnippetAnswer = element.shadowRoot.querySelector(
+          selectors.smartSnippetAnswer
+        );
+        expect(smartSnippetAnswer).not.toBeNull();
+
+        const smartSnippetToggleButton = element.shadowRoot.querySelector(
+          selectors.smartSnippetToggleButton
+        );
+        expect(smartSnippetToggleButton).not.toBeNull();
+        expect(smartSnippetToggleButton.label).toBe(showLessLabel);
+        expect(smartSnippetToggleButton.iconName).toBe(showLessIcon);
+
+        const smartSnippetSource = element.shadowRoot.querySelector(
+          selectors.smartSnippetSource
+        );
+        expect(smartSnippetSource).not.toBeNull();
+
+        const smartSnippetFeedback = element.shadowRoot.querySelector(
+          selectors.smartSnippetFeedback
+        );
+        expect(smartSnippetFeedback).not.toBeNull();
+      });
+
+      describe('when clicking on the smart snippet toggle button', () => {
+        it('should call the collapse method from the smartSnippet controller', async () => {
+          const element = createTestComponent();
+          await flushPromises();
+
+          const smartSnippetToggleButton = element.shadowRoot.querySelector(
+            selectors.smartSnippetToggleButton
+          );
+
+          await smartSnippetToggleButton.click();
+          await flushPromises();
+
+          expect(
+            functionsMocks.buildSmartSnippet.collapse
+          ).toHaveBeenCalledTimes(1);
+        });
+      });
     });
   });
 });
