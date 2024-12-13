ed5529722b4dcb03c5fff54afa1f77b3e8e84289
Frederic Beaudoin <fbeaudoin@coveo.com>
2024-12-13 08:44:27 -0500

    Revert change in cart.tsx
    https://coveord.atlassian.net/browse/KIT-3462

--- packages/samples/headless-ssr-commerce/components/cart.tsx
+++ packages/samples/headless-ssr-commerce/components/cart.tsx
@@ -31,6 +31,7 @@ export default function Cart() {
             <p>
               <span>Price: </span>
               <span>{formatCurrency(item.price, language(), currency())}</span>
+              <span> {item.price}</span>
             </p>
             <p>
               <span>Total: </span>
