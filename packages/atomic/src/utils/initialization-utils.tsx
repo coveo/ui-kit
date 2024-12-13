13cb1083d36012fc292b81cbbc88f8e168ef0947
Louis Bompart <lbompart@coveo.com>
2024-12-12 20:49:11 -0500

    derp

--- packages/atomic/src/utils/initialization-utils.tsx
+++ packages/atomic/src/utils/initialization-utils.tsx
@@ -26,6 +26,27 @@ declare global {
 
 export type InitializeEvent = CustomEvent<InitializeEventHandler>;
 
+/**
+ * Retrieves `Bindings` or `CommerceBindings` on a configured parent interface.
+ * @param event - The element on which to dispatch the event, which must be the child of a configured Atomic container element.
+ * @returns A promise that resolves upon initialization of the parent container element, and rejects otherwise.
+ */
+export function initializeBindings<
+  SpecificBindings extends AnyBindings = Bindings,
+>(element: Element) {
+  return new Promise<SpecificBindings>((resolve, reject) => {
+    const event = buildCustomEvent<InitializeEventHandler>(
+      initializeEventName,
+      (bindings) => resolve(bindings as SpecificBindings)
+    );
+    element.dispatchEvent(event);
+
+    if (!closest(element, initializableElements.join(', '))) {
+      reject(new MissingInterfaceParentError(element.nodeName.toLowerCase()));
+    }
+  });
+}
+
 export {
   MissingInterfaceParentError,
   InitializeEventHandler,
