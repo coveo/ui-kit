c469957ba26aba0d2d33f10e3f6d3ab616547987
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-12-17 15:21:35 -0500

    update proivderw
    https://coveord.atlassian.net/browse/KIT-3801

--- packages/headless-react/src/ssr-commerce/providers.tsx
+++ packages/headless-react/src/ssr-commerce/providers.tsx
@@ -15,7 +15,8 @@ import {
   Context,
   HydrateStaticStateOptions,
   ParameterManager,
-  Parameters, // Recommendations,
+  Parameters,
+  Recommendations, // Recommendations,
 } from '@coveo/headless/ssr-commerce';
 import {PropsWithChildren, useEffect, useState} from 'react';
 import {ReactCommerceEngineDefinition} from './commerce-engine.js';
@@ -79,7 +80,10 @@ export function buildProviderWithDefinition<
             break;
 
           case Kind.Recommendations:
-            //KIT-3801: Done here
+            hydrateArguments[key] = {
+              productId: (controllers as Record<string, Recommendations>)[key]
+                .state.productId,
+            };
             break;
         }
       }
