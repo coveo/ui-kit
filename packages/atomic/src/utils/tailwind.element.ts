be918d6eb823007b46264a89ae1ab409a7dff93c
Felix Perron-Brault <fpbrault@coveo.com>
2024-12-19 14:23:55 -0500

    fix react/angular build

--- packages/atomic/src/utils/tailwind.element.ts
+++ packages/atomic/src/utils/tailwind.element.ts
@@ -2,6 +2,6 @@ import {CSSResultGroup, LitElement, unsafeCSS} from 'lit';
 import theme from './coveo.tw.css';
 import styles from './tailwind.global.tw.css';
 
-export class TailwindElement extends LitElement {
+export class TailwindLitElement extends LitElement {
   static styles: CSSResultGroup = [unsafeCSS(theme), unsafeCSS(styles)];
 }
