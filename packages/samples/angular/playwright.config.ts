811f90a5b0d47221cc8322042ba484ef83a4a109
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2024-12-30 13:33:21 -0500

    fix action

--- packages/samples/angular/playwright.config.ts
+++ packages/samples/angular/playwright.config.ts
@@ -71,8 +71,8 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run dev',
-  //   url: 'http://localhost:4200/',
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:4200/',
+  },
 });
