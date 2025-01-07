1e5207b9f94c4091247495ca5beadd40025e89e9
Louis Bompart <lbompart@coveo.com>
2025-01-07 17:34:41 -0500

    comment out exports of atomic-text

--- packages/atomic/src/components/search/index.ts
+++ packages/atomic/src/components/search/index.ts
@@ -1,1 +1,5 @@
-export {AtomicText} from './atomic-text/atomic-text.js';
+// Export the class of the components migrated to Lit here, like this:
+//export {AtomicText} from './atomic-text/atomic-text.js';
+
+// TODO: Remove this line once at least one component is migrated to Lit.
+export {};
--- packages/atomic/src/components/search/lazy-index.ts
+++ packages/atomic/src/components/search/lazy-index.ts
@@ -1,5 +1,6 @@
 export default {
-  'atomic-text': async () => await import('./atomic-text/atomic-text.js'),
+  // Add entries as such when new components are added/moved to Lit.
+  // 'atomic-text': async () => await import('./atomic-text/atomic-text.js'),
 } as Record<string, () => Promise<unknown>>;
 
 export type * from './index.js';
