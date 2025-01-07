fad19dd609e969ffa01a832cc71688da720b9471
ylakhdar <ylakhdar@coveo.com>
2025-01-07 10:19:26 -0500

    add UTs
    https://coveord.atlassian.net/browse/KIT-3809

--- packages/headless/src/controllers/commerce/context/headless-context.ssr.ts
+++ packages/headless/src/controllers/commerce/context/headless-context.ssr.ts
@@ -1,5 +1,6 @@
 import {UniversalControllerDefinitionWithProps} from '../../../app/commerce-ssr-engine/types/common.js';
 import {Kind} from '../../../app/commerce-ssr-engine/types/kind.js';
+import {MissingControllerProps} from '../../../utils/errors.js';
 import {
   Context,
   buildContext,
@@ -29,6 +30,9 @@ export function defineContext(): ContextDefinition {
     standalone: true,
     recommendation: true,
     buildWithProps: (engine, props) => {
+      if (props === undefined) {
+        throw new MissingControllerProps('Context');
+      }
       return {
         ...buildContext(engine, {options: props}),
         _kind: Kind.Context,
