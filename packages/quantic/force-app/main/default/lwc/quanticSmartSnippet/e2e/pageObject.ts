322bf45e633a0d91d3d757748d3e1f9ecf23c6bd
simonmilord <simon.milord@gmail.com>
2025-01-10 13:21:18 -0500

    added tests and added stuff in the pageObject

--- packages/quantic/force-app/main/default/lwc/quanticSmartSnippet/e2e/pageObject.ts
+++ packages/quantic/force-app/main/default/lwc/quanticSmartSnippet/e2e/pageObject.ts
@@ -1,7 +1,7 @@
 import type {Locator, Page, Request} from '@playwright/test';
 import {
-  isUaSearchEvent,
   isUaClickEvent,
+  isUaCustomEvent,
 } from '../../../../../../playwright/utils/requests';
 
 export class SmartSnippetObject {
@@ -9,6 +9,7 @@ export class SmartSnippetObject {
     this.page = page;
   }
 
+  // LOCATORS
   get smartSnippet(): Locator {
     return this.page.locator('c-quantic-smart-snippet');
   }
@@ -17,11 +18,109 @@ export class SmartSnippetObject {
     return this.page.getByTestId('smart-snippet__source-title');
   }
 
-  async waitForSmartSnippetSearchUaAnalytics(
+  get smartSnippetToggleButton(): Locator {
+    return this.page.getByTestId('smart-snippet__toggle-button');
+  }
+
+  get likeButton(): Locator {
+    return this.page.getByRole('button', {name: 'This answer was helpful'});
+  }
+
+  get dislikeButton(): Locator {
+    return this.page.getByRole('button', {name: 'This answer was not helpful'});
+  }
+
+  get explainWhyButton(): Locator {
+    return this.page.getByRole('button', {name: 'Explain why'});
+  }
+
+  get feedbackModalCancelButton(): Locator {
+    return this.page.getByRole('button', {name: 'Cancel'});
+  }
+
+  get firstFeedbackOption(): Locator {
+    return this.page.getByRole('radio', {name: 'feedback'}).first();
+  }
+
+  get feedbackSubmitButton(): Locator {
+    return this.page.getByTestId('feedback-modal-footer__submit');
+  }
+
+  get feedbackCancelButton(): Locator {
+    return this.page.getByTestId('feedback-modal-footer__cancel');
+  }
+
+  // ACTIONS
+  async clickToggleButton(): Promise<void> {
+    await this.smartSnippetToggleButton.click();
+  }
+
+  async clickLikeButton(): Promise<void> {
+    await this.likeButton.click();
+  }
+
+  async clickDislikeButton(): Promise<void> {
+    await this.dislikeButton.click();
+  }
+
+  async clickExplainWhyButton(): Promise<void> {
+    await this.explainWhyButton.click();
+  }
+
+  async clickFeedbackModalCancelButton(): Promise<void> {
+    await this.feedbackModalCancelButton.click();
+  }
+
+  async selectFirstFeedbackOption(): Promise<void> {
+    await this.firstFeedbackOption.click();
+  }
+
+  async clickFeedbackSubmitButton(): Promise<void> {
+    await this.feedbackSubmitButton.click();
+  }
+
+  async clickFeedbackCancelButton(): Promise<void> {
+    await this.feedbackCancelButton.click();
+  }
+
+  // ANALYTICS
+  async waitForExpandSmartSnippetUaAnalytics(): Promise<Request> {
+    return this.waitForSmartSnippetCustomUaAnalytics('expandSmartSnippet');
+  }
+
+  async waitForCollapseSmartSnippetUaAnalytics(): Promise<Request> {
+    return this.waitForSmartSnippetCustomUaAnalytics('collapseSmartSnippet');
+  }
+
+  async waitForLikeSmartSnippetUaAnalytics(): Promise<Request> {
+    return this.waitForSmartSnippetCustomUaAnalytics('likeSmartSnippet');
+  }
+
+  async waitForDislikeSmartSnippetUaAnalytics(): Promise<Request> {
+    return this.waitForSmartSnippetCustomUaAnalytics('dislikeSmartSnippet');
+  }
+
+  async waitForOpenFeedbackModalUaAnalytics(): Promise<Request> {
+    return this.waitForSmartSnippetCustomUaAnalytics(
+      'openSmartSnippetFeedbackModal'
+    );
+  }
+
+  async waitForCloseFeedbackModalUaAnalytics(): Promise<Request> {
+    return this.waitForSmartSnippetCustomUaAnalytics(
+      'closeSmartSnippetFeedbackModal'
+    );
+  }
+
+  async waitForFeedbackSubmitUaAnalytics(): Promise<Request> {
+    return this.waitForSmartSnippetCustomUaAnalytics('sendSmartSnippetReason');
+  }
+
+  async waitForSmartSnippetClickUaAnalytics(
     actionCause: any
   ): Promise<Request> {
     const uaRequest = this.page.waitForRequest((request) => {
-      if (isUaSearchEvent(request)) {
+      if (isUaClickEvent(request)) {
         const requestBody = request.postDataJSON?.();
         const expectedFields = {
           eventValue: actionCause,
@@ -35,14 +134,15 @@ export class SmartSnippetObject {
     return uaRequest;
   }
 
-  async waitForSmartSnippetClickUaAnalytics(
-    actionCause: any
+  async waitForSmartSnippetCustomUaAnalytics(
+    eventValue: any
   ): Promise<Request> {
     const uaRequest = this.page.waitForRequest((request) => {
-      if (isUaClickEvent(request)) {
+      if (isUaCustomEvent(request)) {
         const requestBody = request.postDataJSON?.();
         const expectedFields = {
-          eventValue: actionCause,
+          eventType: 'smartSnippet',
+          eventValue: eventValue,
         };
         return Object.keys(expectedFields).every(
           (key) => requestBody?.[key] === expectedFields[key]
--- packages/quantic/force-app/main/default/lwc/quanticSmartSnippet/e2e/quanticSmartSnippet.e2e.ts
+++ packages/quantic/force-app/main/default/lwc/quanticSmartSnippet/e2e/quanticSmartSnippet.e2e.ts
@@ -21,6 +21,7 @@ useCaseTestCases.forEach((useCase) => {
         test('should send the correct UA analytics event', async ({
           smartSnippet,
         }) => {
+          await smartSnippet.smartSnippetToggleButton.click();
           await smartSnippet.smartSnippetSourceTitle.click();
           await smartSnippet.waitForSmartSnippetClickUaAnalytics(
             'openSmartSnippetSource'
@@ -39,42 +40,79 @@ useCaseTestCases.forEach((useCase) => {
       });
     });
 
-    test.describe('when the smart snippet is collapsed', () => {
-      test.describe('when expanding the smart snippet answer', () => {
-        test('should send the correct UA analytics event', async ({smartSnippet}) => {});
-      });
+    test.describe('when expanding and collapsing the smart snippet', () => {
+      test('should send the correct UA analytics events', async ({smartSnippet}) => {
+        const expandSmartSnippetAnalyticsPromise =
+          smartSnippet.waitForExpandSmartSnippetUaAnalytics();
+        await smartSnippet.clickToggleButton();
+        await expandSmartSnippetAnalyticsPromise;
 
-      test.describe('when collapsing the smart snippet answer', () => {
-        test('should send the correct UA analytics event', async ({
-          smartSnippet,
-        }) => {});
+        const collapseSmartSnippetAnalyticsPromise =
+          smartSnippet.waitForCollapseSmartSnippetUaAnalytics();
+        await smartSnippet.clickToggleButton();
+        await collapseSmartSnippetAnalyticsPromise;
       });
     });
 
     test.describe('when clicking on the feedback like button', () => {
-      test('should send the correct UA analytics event', async ({smartSnippet}) => {});
+      test('should send the correct UA analytics event', async ({smartSnippet}) => {
+        const likeAnalyticsPromise = smartSnippet.waitForLikeSmartSnippetUaAnalytics();
+        await smartSnippet.clickLikeButton();
+        await likeAnalyticsPromise;
+      });
     });
 
     test.describe('when clicking on the feedback dislike button', () => {
       test('should send the correct UA analytics event', async ({
         smartSnippet,
-      }) => {});
+      }) => {
+        const dislikeAnalyticsPromise = smartSnippet.waitForDislikeSmartSnippetUaAnalytics();
+        await smartSnippet.clickDislikeButton();
+        await dislikeAnalyticsPromise;
+      });
 
-      test.describe('when providing feedback', () => {
+      test.describe('when opening the feedback modal and providing feedback', () => {
         test('should send the correct open feedback modal UA analytics event', async ({
           smartSnippet,
-        }) => {});
+        }) => {
+          const openFeedbackModalAnalyticsPromise = smartSnippet.waitForOpenFeedbackModalUaAnalytics();
+          await smartSnippet.clickDislikeButton();
+          await smartSnippet.clickExplainWhyButton();
+          await openFeedbackModalAnalyticsPromise;
+        });
 
         test.describe('when selecting and submitting a feedback option', () => {
-          test('should send the following analytics events: logOpenSmartSnippetFeedbackModal, logSendSmartSnippetReason, logCloseSmartSnippetFeedbackModal', async () => {});
+          test('should send the correct UA analytics event', async ({
+            smartSnippet,
+          }) => {
+            await smartSnippet.clickDislikeButton();
+            await smartSnippet.clickExplainWhyButton();
+
+            const submitFeedbackAnalyticsPromise = smartSnippet.waitForFeedbackSubmitUaAnalytics();
+            await smartSnippet.selectFirstFeedbackOption();
+            await smartSnippet.clickFeedbackSubmitButton();
+            await submitFeedbackAnalyticsPromise;
+          });
         });
 
         test.describe('when trying to open the feedback modal after executing the same query', () => {
-          test('should not open the feedback modal', async () => {});
+          test('should not open the feedback modal', async () => {
+            
+          });
         });
 
         test.describe('when trying to open the feedback modal after executing a query that gave a new answer', () => {
           test('should open the feedback modal', async () => {});
+        });
+      });
+
+      test.describe('when closing the feedback modal', () => {
+        test('should send the correct close feedback modal UA analytics event', async ({smartSnippet}) => {
+          const closeFeedbackModalAnalyticsPromise = smartSnippet.waitForCloseFeedbackModalUaAnalytics();
+          await smartSnippet.clickDislikeButton();
+          await smartSnippet.clickExplainWhyButton();
+          await smartSnippet.clickFeedbackModalCancelButton();
+          await closeFeedbackModalAnalyticsPromise;
         });
       });
     });
