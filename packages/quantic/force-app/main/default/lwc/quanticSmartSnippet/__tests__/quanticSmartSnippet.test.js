10e47e23620c3c1fc84fdb339f852fd8fc4beb7a
simonmilord <simon.milord@gmail.com>
2025-01-13 16:15:37 -0500

    skip two tests

--- packages/quantic/force-app/main/default/lwc/quanticSmartSnippet/__tests__/quanticSmartSnippet.test.js
+++ packages/quantic/force-app/main/default/lwc/quanticSmartSnippet/__tests__/quanticSmartSnippet.test.js
@@ -340,13 +340,13 @@ describe('c-quantic-smart-snippet', () => {
 
     describe('when interacting with the feedback modal', () => {
       describe('when trying to open the feedback modal after executing the same query', () => {
-        it('should not open the feedback modal', async () => {
+        it.skip('should not open the feedback modal', async () => {
           // TODO: Implement this test
         });
       });
 
       describe('when trying to open the feedback modal after executing a query that gave a new answer', () => {
-        it('should open the feedback modal', async () => {
+        it.skip('should open the feedback modal', async () => {
           // TODO: Implement this test
         });
       });
