b4894dcc63cd76a6616140610cd6dcaf70df5248
ylakhdar <ylakhdar@coveo.com>
2025-01-14 09:57:28 -0500

    Update packages/atomic/src/components/common/hidden.tsx
    Co-authored-by: Frederic Beaudoin <fbeaudoin@coveo.com>

--- packages/atomic/src/components/common/hidden.tsx
+++ packages/atomic/src/components/common/hidden.tsx
@@ -1,7 +1,7 @@
 import {FunctionalComponent, Host, h} from '@stencil/core';
 
 /**
- * @deprecated use the displayIf directive instead
+ * @deprecated Should only be used for Stencil components; for Lit components, use the displayIf directive instead
  */
 export const Hidden: FunctionalComponent = () => (
   <Host class="atomic-hidden"></Host>
