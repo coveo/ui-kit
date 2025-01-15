c8629b728498597655b045319cdc8c3c850501ce
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-01-15 15:35:49 -0500

    clean up

--- packages/atomic/src/components/common/generated-answer/generated-content/markdown-utils.spec.ts
+++ packages/atomic/src/components/common/generated-answer/generated-content/markdown-utils.spec.ts
@@ -1,4 +1,3 @@
-import {describe, it, expect} from 'vitest';
 import {transformMarkdownToHtml} from './markdown-utils.js';
 
 describe('markdownUtils', () => {
--- packages/atomic/src/utils/utils.spec.ts
+++ packages/atomic/src/utils/utils.spec.ts
@@ -1,4 +1,4 @@
-import {describe, it, expect, vi} from 'vitest';
+import {vi} from 'vitest';
 import {
   once,
   camelToKebab,
