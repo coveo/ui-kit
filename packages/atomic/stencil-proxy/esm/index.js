d854df2dd8e2a7956dd77391cd70f9bdaa056ea4
Louis Bompart <lbompart@coveo.com>
2025-01-15 12:41:37 -0500

    chore: relocate dist/components to dist/atomic/components (#4859)
    With the Lit adaptation layer, we need the components directory in the
    CDN files.
    However, we only deploy dist/atomic to the CDN with the current
    structure. Changing what we deploy to the CDN is the riskiest change
    because even if we support only a given # of entry points, there is no
    protection.
    NPM and exports have this level of protection, so instead, I used the
    "CDN" file structure as the immutable and changed things for NPM.
    
    https://coveord.atlassian.net/browse/KIT-3858

--- packages/atomic/stencil-proxy/esm/index.js
+++ packages/atomic/stencil-proxy/esm/index.js
@@ -1,4 +1,4 @@
 import '../atomic/autoloader/index.esm.js';
 
 export * from './_index.js';
-export * as SearchComponentMap from '../components/components/search/lazy-index.js';
+export * as SearchComponentMap from '../atomic/components/components/search/lazy-index.js';
