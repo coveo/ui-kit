05973ea10a2f20d723f23ee252e4db6f9fb82422
ylakhdar <ylakhdar@coveo.com>
2025-01-15 12:36:23 -0500

    add heading function
    https://coveord.atlassian.net/browse/KIT-3833

--- packages/atomic/src/components/common/heading.ts
+++ packages/atomic/src/components/common/heading.ts
@@ -1,1 +1,33 @@
-// TODO:
+// import {spread} from '@open-wc/lit-helpers';
+import {ifDefined} from 'lit-html/directives/if-defined.js';
+import {html, literal, unsafeStatic} from 'lit/static-html.js';
+
+export interface HeadingProps {
+  /**
+   * The heading level.
+   *
+   * A value outside of the range of 1 to 6 will render a div instead of a heading.
+   */
+  level: number;
+  /**
+   * Additional classes to add to the heading.
+   */
+  class?: string;
+  /**
+   * Additional parts to add to the heading.
+   */
+  part?: string;
+}
+
+// TODO: KIT-3822: add unit tests to this function
+export const heading = <T>(
+  {level, class: classname, part}: HeadingProps,
+  children?: T
+) => {
+  const headingTag =
+    level > 0 && level <= 6 ? unsafeStatic(`h${level}`) : literal`div`;
+
+  return html`<${headingTag} class="${ifDefined(classname)}" part="${ifDefined(part)}">
+    ${children}
+  </${headingTag}>`;
+};
