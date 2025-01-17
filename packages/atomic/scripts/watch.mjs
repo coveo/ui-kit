640002781f9e7b3869cd1abdeaae2544c4ac4674
ylakhdar <ylakhdar@coveo.com>
2025-01-17 10:21:22 -0500

    update watch script
    https://coveord.atlassian.net/browse/KIT-3869

--- packages/atomic/scripts/watch.mjs
+++ packages/atomic/scripts/watch.mjs
@@ -1,11 +1,27 @@
 import {execSync} from 'node:child_process';
 import {watch} from 'node:fs';
 
-function rebuild() {
+function initialBuild() {
   const commands = [
     'node --max_old_space_size=6144 ../../node_modules/@stencil/core/bin/stencil build',
     'node ./scripts/stencil-proxy.mjs',
     'tsc -p tsconfig.lit.json',
+    'node process-css.mjs',
+    'esbuild src/autoloader/index.ts --format=esm --outfile=dist/atomic/autoloader/index.esm.js',
+    'esbuild src/autoloader/index.ts --format=cjs --outfile=dist/atomic/autoloader/index.cjs.js',
+  ];
+  for (const command of commands) {
+    execSync(command, {
+      stdio: 'inherit',
+      env: {...process.env, IS_DEV: 'true'},
+    });
+  }
+}
+
+function rebuild() {
+  const commands = [
+    'tsc -p tsconfig.lit.json',
+    'node process-css.mjs',
     'esbuild src/autoloader/index.ts --format=esm --outfile=dist/atomic/autoloader/index.esm.js',
     'esbuild src/autoloader/index.ts --format=cjs --outfile=dist/atomic/autoloader/index.cjs.js',
   ];
@@ -18,4 +34,4 @@ function rebuild() {
 }
 
 watch('src', {recursive: true}, rebuild);
-rebuild();
+initialBuild();
