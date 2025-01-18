24b7c2d251918ae9f0fd41d68edb4dff91410529
Louis Bompart <lbompart@coveo.com>
2025-01-17 22:16:36 -0500

    various utils fixes
    storybook: add mdx support
    
    storybook-dev: sync up lang & assets after a watch cycle
    
    dev: ignore e2e from watch cycle
    
    lit: add base page object

---
+++ packages/atomic/playwright-utils/lit-base-page-object.ts
@@ -0,0 +1,82 @@
+import type {Page} from '@playwright/test';
+import {buildArgsParam} from '@storybook/router';
+
+type Component = Record<string, unknown>;
+
+export class BasePageObject {
+  constructor(
+    public page: Page,
+    public tag: string
+  ) {}
+
+  get urlRoot() {
+    return 'http://localhost:4400/iframe.html';
+  }
+
+  get hydrated() {
+    return this.page.locator(`${this.tag}`);
+  }
+
+  async load({
+    args,
+    story = 'default',
+  }: {args?: Record<string, unknown>; story?: string} = {}) {
+    if (args) {
+      await this.page.goto(
+        `${this.urlRoot}?id=${this.tag}--${story}&args=${buildArgsParam(undefined, this.camelToKebab(args))}`
+      );
+    } else {
+      await this.page.goto(`${this.urlRoot}?id=${this.tag}--${story}`);
+    }
+  }
+
+  async noProducts() {
+    await this.page.route('**/commerce/v2/search', async (route) => {
+      const response = await route.fetch();
+      const body = await response.json();
+      body.products = [];
+      if (body.pagination) {
+        body.pagination.totalEntries = 0;
+        body.pagination.totalPages = 0;
+      }
+      body.facets = [];
+      await route.fulfill({
+        response,
+        json: body,
+      });
+    });
+
+    return this;
+  }
+
+  async noRecommendations() {
+    return this.nRecommendations(0);
+  }
+
+  async nRecommendations(numberOfRecommendations?: number) {
+    await this.page.route('**/commerce/v2/recommendations', async (route) => {
+      const response = await route.fetch();
+      const body = await response.json();
+      if (numberOfRecommendations !== undefined) {
+        body['products'] = body['products'].slice(0, numberOfRecommendations);
+      }
+      await route.fulfill({
+        response,
+        json: body,
+      });
+    });
+
+    return this;
+  }
+
+  private camelToKebab(args: Component) {
+    const toKebab: Record<string, unknown> = {};
+    Object.entries(args as Record<string, unknown>).forEach(([key, value]) => {
+      toKebab[
+        `attributes-${key.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()}`
+      ] = value;
+    });
+
+    return toKebab;
+  }
+}
