3b64abc313a4cbfe3d8ddc383c1fc303ef7b9c6d
ylakhdar <ylakhdar@coveo.com>
2025-01-20 09:47:37 -0500

    rename files

--- packages/atomic/src/components/commerce/atomic-commerce-load-more-products/atomic-commerce-load-more-products.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-load-more-products/atomic-commerce-load-more-products.tsx
@@ -13,7 +13,7 @@ import {
   BindStateToController,
   InitializeBindings,
 } from '../../../utils/initialization-utils';
-import {LoadMoreButton} from '../../common/load-more/button';
+import {LoadMoreButton} from '../../common/load-more/stencil-button';
 import {LoadMoreContainer} from '../../common/load-more/container';
 import {LoadMoreGuard} from '../../common/load-more/guard';
 import {LoadMoreProgressBar} from '../../common/load-more/progress-bar';
