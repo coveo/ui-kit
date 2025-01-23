d6d1dc22713d5123991db902aefc95f516cd3b31
simonmilord <simon.milord@gmail.com>
2025-01-23 15:57:36 -0500

    added structure for e2e tests

--- packages/quantic/force-app/main/default/lwc/quanticSmartSnippetSuggestions/e2e/quanticSmartSnippetSuggestions.ts
+++ packages/quantic/force-app/main/default/lwc/quanticSmartSnippetSuggestions/e2e/quanticSmartSnippetSuggestions.ts
@@ -9,5 +9,17 @@ const fixtures = {
 useCaseTestCases.forEach((useCase) => {
   let test = fixtures[useCase.value];
 
-  test.describe(`quantic smart snippet suggestions ${useCase.label}`, () => {});
+  test.describe(`quantic smart snippet suggestions ${useCase.label}`, () => {
+    test.describe('when expanding and collapsing the smart snippet suggestions', () => {
+      test('should send the expand and collapse analytics events', async () => {});
+    });
+
+    test.describe.skip('when interacting with a suggestion', () => {
+      test('should send the source title analytics event when the title is clicked', async () => {});
+
+      test('should send the source uri analytics event when the uri is clicked', async () => {});
+
+      test('should send the inline link analytics event when an inline link is clicked', async () => {});
+    });
+  });
 });
