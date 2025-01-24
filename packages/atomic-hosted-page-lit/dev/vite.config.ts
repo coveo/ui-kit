b47499a85b339a1fc0eba9c7efd4888f9f0b5b32
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-01-24 15:57:38 -0500

    migrate atomic-hosted-page to lit
    https://coveord.atlassian.net/browse/KIT-3868

--- packages/atomic-hosted-page-lit/dev/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import {defineConfig} from 'vite';
-
-export default defineConfig({
-  define: {
-    'process.env.DEPLOYMENT_ENVIRONMENT': JSON.stringify(
-      process.env.DEPLOYMENT_ENVIRONMENT || 'LOCAL'
-    ),
-  },
-});
