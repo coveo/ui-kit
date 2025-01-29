b49d0d247ceb0268a41e30905b82b61fe6602b50
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-01-29 12:59:43 -0500

    fix for aliases
    https://coveord.atlassian.net/browse/KIT-3908

--- packages/atomic/vitest.config.ts
+++ packages/atomic/vitest.config.ts
@@ -1,6 +1,16 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
 import {defineConfig} from 'vitest/config';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
   test: {
     include: ['src/**/*.spec.ts'],
     exclude: [
