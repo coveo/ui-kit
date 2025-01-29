a8c20cb7f062e0c426c258130f4c6c0ab0f9ca13
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-01-29 15:18:11 -0500

    no more dirname
    https://coveord.atlassian.net/browse/KIT-3908

--- packages/atomic/vitest.config.ts
+++ packages/atomic/vitest.config.ts
@@ -1,14 +1,10 @@
 import path from 'node:path';
-import {fileURLToPath} from 'node:url';
 import {defineConfig} from 'vitest/config';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './'),
+      '@': path.resolve(import.meta.dirname, './'),
     },
   },
   test: {
