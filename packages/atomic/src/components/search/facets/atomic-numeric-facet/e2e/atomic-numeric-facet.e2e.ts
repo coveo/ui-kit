9d217104573ef7600d539c41c41d6de10a1fcccb
Frederic Beaudoin <fbeaudoin@coveo.com>
2025-01-30 15:10:02 -0500

    Add depends on e2e
    https://coveord.atlassian.net/browse/KIT-3746

--- packages/atomic/src/components/search/facets/atomic-numeric-facet/e2e/atomic-numeric-facet.e2e.ts
+++ packages/atomic/src/components/search/facets/atomic-numeric-facet/e2e/atomic-numeric-facet.e2e.ts
@@ -1,23 +1,50 @@
 import {test, expect} from './fixture';
 
-test.describe('when dependent', () => {
+test.describe('when a "depends-on" prop is provided', () => {
   test.beforeEach(async ({facet}) => {
     await facet.load({story: 'with-depends-on'});
+    await facet.facet.waitFor({state: 'visible'});
   });
 
-  test('when the specified dependency is selected in the parent facet, should be visible', () => {
-    expect(true);
+  test('when the specified dependency is selected in the parent facet, should be visible', async ({
+    facet,
+  }) => {
+    expect(facet.facet).toBeVisible();
   });
 
-  test.describe('when the specified dependency is not selected in the parent facet', () => {
-    test('should not be visible', () => {
-      expect(true);
+  test.describe('when the specified dependency is cleared in the parent facet', () => {
+    test('should not be visible', async ({facet}) => {
+      const parent = facet.page.getByTestId('parent-facet');
+      await parent.getByLabel('Inclusion filter on YouTubeVideo').click();
+
+      expect(facet.facet).not.toBeVisible();
     });
-    test('should clear previously selected range', () => {
-      expect(true);
+
+    test('should clear previously selected range', async ({facet}) => {
+      await facet.getFacetValue.first().click();
+
+      const breadbox = facet.page.getByTestId('breadbox');
+      expect(breadbox).toBeVisible();
+
+      const parent = facet.page.getByTestId('parent-facet');
+      await parent.getByLabel('Inclusion filter on YouTubeVideo').click();
+
+      await breadbox.waitFor({state: 'hidden'});
+      expect(breadbox).not.toBeVisible();
     });
-    test('should clear previously selected input range', () => {
-      expect(true);
+    test('should clear previously selected input range', async ({facet}) => {
+      await facet.facetInputStart.fill('900000');
+      await facet.facetInputEnd.fill('90000000');
+      await facet.facetApplyButton.click();
+
+      const breadbox = facet.page.getByTestId('breadbox');
+      expect(breadbox).toBeVisible();
+
+      const parent = facet.page.getByTestId('parent-facet');
+      await parent.getByLabel('Inclusion filter on YouTubeVideo').click();
+
+      await breadbox.waitFor({state: 'hidden'});
+      expect(breadbox).not.toBeVisible();
     });
   });
 });
