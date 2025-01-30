e81a8a51f6c7e4aa1cf22cdec81ee3afe460c166
Frederic Beaudoin <fbeaudoin@coveo.com>
2025-01-30 15:09:26 -0500

    Improve page object
    https://coveord.atlassian.net/browse/KIT-3746

--- packages/atomic/src/components/search/facets/atomic-numeric-facet/e2e/page-object.ts
+++ packages/atomic/src/components/search/facets/atomic-numeric-facet/e2e/page-object.ts
@@ -6,23 +6,27 @@ export class AtomicNumericFacetPageObject extends BasePageObject<'atomic-numeric
     super(page, 'atomic-numeric-facet');
   }
 
+  get facet() {
+    return this.page.locator('atomic-numeric-facet');
+  }
+
   get facetInputStart() {
-    return this.page.getByLabel('Enter a minimum numerical value');
+    return this.facet.getByLabel('Enter a minimum numerical value');
   }
 
   get facetInputEnd() {
-    return this.page.getByLabel('Enter a maximum numerical value');
+    return this.facet.getByLabel('Enter a maximum numerical value');
   }
 
   get facetApplyButton() {
-    return this.page.getByLabel('Apply custom numerical values');
+    return this.facet.getByLabel('Apply custom numerical values');
   }
 
   get facetClearFilter() {
-    return this.page.getByRole('button').filter({hasText: 'Clear filter'});
+    return this.facet.getByRole('button').filter({hasText: 'Clear filter'});
   }
 
   get getFacetValue() {
-    return this.page.locator('[part="value-checkbox"]');
+    return this.facet.locator('[part="value-checkbox"]');
   }
 }
