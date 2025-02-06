c07692a0e726399ab2d972daf8bb80a4602c3248
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-02-06 10:38:05 -0500

    add tests
    https://coveord.atlassian.net/browse/KIT-3305

---
+++ packages/atomic/src/components/commerce/atomic-commerce-search-box/atomic-commerce-search-box.spec.ts
@@ -0,0 +1,7 @@
+import {describe, test} from 'vitest';
+
+describe('AtomicCommerceSearchBox', () => {
+  //TODO: Write those tests during the lit migration
+  test('when it is expanded, it sets aria-expanded to "true"', () => {});
+  test('when it is collapsed, it sets aria-expanded to "false"', () => {});
+});
