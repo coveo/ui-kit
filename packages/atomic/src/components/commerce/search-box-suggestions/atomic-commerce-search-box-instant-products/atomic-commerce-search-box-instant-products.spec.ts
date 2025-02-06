6b083e46efd02005bb05aa2a27c1d328b268064b
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-02-06 15:32:50 -0500

    more specs for commerce
    https://coveord.atlassian.net/browse/KIT-3910

---
+++ packages/atomic/src/components/commerce/search-box-suggestions/atomic-commerce-search-box-instant-products/atomic-commerce-search-box-instant-products.spec.ts
@@ -0,0 +1,23 @@
+import {describe, test} from 'vitest';
+
+//TODO: Write those tests during the lit migration
+describe('AtomicCommerceSearchBoxInstantProducts', () => {
+  describe('when clicking', () => {
+    test('on a instant product, it should redirect to the product page', async () => {});
+    test('on the "See all products" button, it should execute the proper search', async () => {});
+  });
+
+  describe('when using keyboard navigation', () => {
+    test('on instant products, it should redirect to the product page', async () => {});
+    test('on the "See all products" button, it should execute the proper search', async () => {});
+  });
+
+  describe('when using keyboard navigation and then switching to mouse and hovering a new product', () => {
+    test('it should execute the search reflected in the search box', () => {});
+  });
+
+  // This is a bug currently in the component
+  describe('when using the "See all products" button', () => {
+    test('it should reflect the query in the search box', async () => {});
+  });
+});
