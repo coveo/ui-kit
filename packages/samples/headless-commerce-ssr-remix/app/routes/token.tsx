7ee9b9bd3210b98166373eb1968bfa7f342c4a12
Frederic Beaudoin <fbeaudoin@coveo.com>
2025-02-19 14:17:09 -0500

    Add /token API route
    https://coveord.atlassian.net/browse/KIT-3987

---
+++ packages/samples/headless-commerce-ssr-remix/app/routes/token.tsx
@@ -0,0 +1,68 @@
+import {isExpired, parseJwt} from '@/lib/jwt-utils';
+import {LoaderFunctionArgs} from '@remix-run/node';
+import {coveo_accessToken} from '../cookies.server';
+
+export const loader = async ({request}: LoaderFunctionArgs) => {
+  const accessTokenCookie = await coveo_accessToken.parse(
+    request.headers.get('Cookie')
+  );
+
+  if (!isExpired(accessTokenCookie)) {
+    return Response.json({token: accessTokenCookie});
+  }
+
+  const res = await fetch(
+    'https://platform.cloud.coveo.com/rest/search/v2/token?organizationId=searchuisamples',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        userIds: [
+          {
+            name: 'anonymous',
+            type: 'User',
+            provider: 'Email Security Provider',
+            infos: {},
+            authCookie: '',
+          },
+        ],
+      }),
+      headers: {
+        Authorization: 'Bearer <API_KEY_WITH_IMPERSONATE_PRIVILEGE>',
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  if (res.status !== 200) {
+    return Response.json(
+      {message: 'Failed to fetch access token from Coveo Search API'},
+      {status: res.status}
+    );
+  }
+
+  const responseData = await res.json();
+
+  const parsedToken = parseJwt(responseData.token);
+
+  if (!parsedToken) {
+    return Response.json(
+      {
+        message:
+          'The access token returned by the Coveo Search API could not be parsed',
+      },
+      {status: 500}
+    );
+  }
+
+  const cookieExpiration = parsedToken.exp * 1000;
+
+  return Response.json(responseData, {
+    headers: {
+      'Set-Cookie': await coveo_accessToken.serialize(responseData.token, {
+        encode: (value) => atob(value).replaceAll('"', ''),
+        secure: true,
+        expires: new Date(cookieExpiration),
+      }),
+    },
+  });
+};
