edc0d157b997ee4f75e722e3915a41879e39e71a
Felix Perron-Brault <fpbrault@coveo.com>
2025-02-19 15:32:58 -0500

    include utilities in TailwindLitElement
    https://coveord.atlassian.net/browse/KIT-3925

--- packages/atomic/src/utils/tailwind.element.ts
+++ packages/atomic/src/utils/tailwind.element.ts
@@ -1,7 +1,12 @@
 import {CSSResultGroup, LitElement, unsafeCSS} from 'lit';
 import theme from './coveo.tw.css';
+import utilities from './tailwind-utilities/utilities.tw.css';
 import styles from './tailwind.global.tw.css';
 
 export class TailwindLitElement extends LitElement {
-  static styles: CSSResultGroup = [unsafeCSS(theme), unsafeCSS(styles)];
+  static styles: CSSResultGroup = [
+    unsafeCSS(theme),
+    unsafeCSS(styles),
+    unsafeCSS(utilities),
+  ];
 }
