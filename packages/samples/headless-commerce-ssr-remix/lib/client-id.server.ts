498d59e2f26a3a44ea85bd152350abab22dbffed
Frederic Beaudoin <fbeaudoin@coveo.com>
2025-02-21 11:03:31 -0500

    More docs improvements
    https://coveord.atlassian.net/browse/KIT-3988

--- packages/samples/headless-commerce-ssr-remix/lib/client-id.server.ts
+++ packages/samples/headless-commerce-ssr-remix/lib/client-id.server.ts
@@ -6,19 +6,24 @@ interface CoveoAnalyticsContext
   extends Required<Pick<NavigatorContext, 'clientId' | 'capture'>> {}
 
 /**
- * Determines whether analytics data should be captured on server-side Coveo requests, based on the provided request
- * object.
+ * Determines whether analytics data should be captured on Coveo requests, based on the provided request object.
  *
  * @param request - The request object.
  * @returns `true` if analytics data should be captured, `false` otherwise.
  */
 export const shouldCapture = async (request: Request): Promise<boolean> => {
   return (
-    (await isCoveoCaptureCookieSet(request)) &&
+    (await isUserTrackingAllowedByClient(request)) &&
     (await isUserTrackingAllowedByRequest(request))
   );
 };
 
+/**
+ * Determines whether the request allows analytics data to be captured by Coveo requests.
+ *
+ * @param request - The request object.
+ * @returns `true` if the request allows analytics data to be captured by Coveo requests, `false` otherwise.
+ */
 const isUserTrackingAllowedByRequest = async (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   request: Request
@@ -28,7 +33,14 @@ const isUserTrackingAllowedByRequest = async (
   return true; // This is a placeholder implementation that always returns `true`.
 };
 
-const isCoveoCaptureCookieSet = async (request: Request) => {
+/**
+ * Verifies whether the `coveo_capture` cookie is set in the 'Cookie' request header in order to determine whether the
+ * client allows analytics data to be captured by Coveo requests.
+ *
+ * @param request - The request object.
+ * @returns `true` if the `coveo_capture` is set, `false` otherwise.
+ */
+const isUserTrackingAllowedByClient = async (request: Request) => {
   return !!(await coveo_capture.parse(request.headers.get('Cookie')));
 };
 
@@ -51,7 +63,7 @@ export const getAnalyticsContext = async (
 };
 
 /**
- * Generates a 'Set-Cookie' responsﬁe header with the provided argument serialized into the `coveo_visitorId` cookie.
+ * Generates a 'Set-Cookie' response header with the provided argument serialized into the `coveo_visitorId` cookie.
  *
  * @param clientId - The clientId to serialize into the `coveo_visitorId` Set-Cookie response header.
  * @returns A Set-Cookie response header, or an empty object if the clientId is an empty string.
