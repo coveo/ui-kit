ad72d814794b219b273d569a2fabaf49f20f77e3
Felix Perron-Brault <fpbrault@coveo.com>
2025-02-24 10:58:25 -0500

    migrate atomic-commerce-interface to lit
    https://coveord.atlassian.net/browse/KIT-3932
    
    use non-stencil getAssetPath
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    remove log
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    smaller styles
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    remove unused styles
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    add missing generated files
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    fix build
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    cleanup
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    other approach for loader
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    cleanup
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    create bindings decorator
    
    https://coveord.atlassian.net/browse/KIT-4005
    
    fix bindings
    
    https://coveord.atlassian.net/browse/KIT-4005
    
    add commerce page to angular sample
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    fix interface init
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    revert setup
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    revert loader change
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    restore coveoglobal for lit interface
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    fix init
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    fix story
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    fix missing global version
    
    https://coveord.atlassian.net/browse/KIT-3932
    
    cleanup
    
    https://coveord.atlassian.net/browse/KIT-3932

--- packages/atomic/src/components/commerce/atomic-commerce-interface/e2e/page-object.ts
+++ packages/atomic/src/components/commerce/atomic-commerce-interface/e2e/page-object.ts
@@ -1,21 +1,8 @@
-import {BasePageObject} from '@/playwright-utils/base-page-object';
+import {BasePageObject} from '@/playwright-utils/lit-base-page-object';
 import type {Page} from '@playwright/test';
 
-export class CommerceInterfacePageObject extends BasePageObject<'atomic-commerce-interface'> {
+export class AtomicCommerceInterfacePageObject extends BasePageObject {
   constructor(page: Page) {
     super(page, 'atomic-commerce-interface');
-  }
-
-  getFacetValue(value: string | RegExp) {
-    return this.page.getByRole('listitem').filter({hasText: value});
-  }
-
-  interface() {
-    return this.page.locator('atomic-commerce-interface');
-  }
-
-  getBreadcrumbButtons(value?: string | RegExp) {
-    const baseLocator = this.page.locator('[part="breadcrumb-button"]:visible');
-    return value ? baseLocator.filter({hasText: value}) : baseLocator;
   }
 }
