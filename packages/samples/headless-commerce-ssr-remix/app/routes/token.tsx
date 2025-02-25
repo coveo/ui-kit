df612e4df3350ee813cd7701d2ddca3a6d0c5e40
Frederic Beaudoin <fbeaudoin@coveo.com>
2025-02-25 10:43:58 -0500

    Add comments
    https://coveord.atlassian.net/browse/KIT-3987

--- packages/samples/headless-commerce-ssr-remix/app/routes/token.tsx
+++ packages/samples/headless-commerce-ssr-remix/app/routes/token.tsx
@@ -5,6 +5,8 @@ import {LoaderFunctionArgs} from '@remix-run/node';
 import {coveo_accessToken} from '../cookies.server';
 
 export const loader = async ({request}: LoaderFunctionArgs) => {
+  // In an SSR scenario, we recommend storing the search token in a cookie to minimize the number of network requests.
+  // This is not mandatory, but it can help improve the performance of your application.
   const accessTokenCookie = await coveo_accessToken.parse(
     request.headers.get('Cookie')
   );
@@ -13,11 +15,19 @@ export const loader = async ({request}: LoaderFunctionArgs) => {
     return Response.json({token: accessTokenCookie});
   }
 
-  const endpoint = getOrganizationEndpoint(
+  const organizationEndpoint = getOrganizationEndpoint(
     engineDefinitionOptions.configuration.organizationId
   );
 
-  const res = await fetch(`${endpoint}/rest/search/v2/token`, {
+  // This example focuses on demonstrating the Coveo search token authentication flow in an SSR scenario. For the sake
+  // of simplicity, it only generates anonymous search tokens.
+  //
+  // If you use search token authentication in a real-world scenario, you will likely want to generate tokens for
+  // authenticated users.
+  //
+  // The specific implementation details for this use case will vary based on the requirements of your application and
+  // the way it handles user authentication.
+  const response = await fetch(`${organizationEndpoint}/rest/search/v2/token`, {
     method: 'POST',
     body: JSON.stringify({
       userIds: [
@@ -36,14 +46,14 @@ export const loader = async ({request}: LoaderFunctionArgs) => {
     },
   });
 
-  if (res.status !== 200) {
+  if (response.status !== 200) {
     return Response.json(
       {message: 'Failed to fetch access token from Coveo Search API'},
-      {status: res.status}
+      {status: response.status}
     );
   }
 
-  const responseData = await res.json();
+  const responseData = await response.json();
 
   const parsedToken = parseJwt(responseData.token);
 
