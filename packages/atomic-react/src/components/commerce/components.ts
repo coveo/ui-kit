ac964cef2760897b53805c3251873691fbded14e
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-02-26 16:56:03 -0500

    chore(atomic): migrate atomic-commerce-pager to lit
    https://coveord.atlassian.net/browse/KIT-3936

--- packages/atomic-react/src/components/commerce/components.ts
+++ packages/atomic-react/src/components/commerce/components.ts
@@ -1,9 +1,16 @@
 import {
+  AtomicCommercePager as LitAtomicCommercePager,
   AtomicComponentError as LitAtomicComponentError,
   AtomicIcon as LitAtomicIcon,
 } from '@coveo/atomic/components';
 import {createComponent} from '@lit/react';
 import React from 'react';
+
+export const AtomicCommercePager = createComponent({
+  tagName: 'atomic-commerce-pager',
+  react: React,
+  elementClass: LitAtomicCommercePager,
+});
 
 export const AtomicComponentError = createComponent({
   tagName: 'atomic-component-error',
