6d38fc85115276b2a427d7994d75abae10b2895d
Felix Perron-Brault <fpbrault@coveo.com>
2025-02-28 08:19:24 -0500

    create bindings decorator
    https://coveord.atlassian.net/browse/KIT-4005

--- packages/atomic/src/decorators/bindings.spec.ts
+++ packages/atomic/src/decorators/bindings.spec.ts
@@ -1,15 +1,12 @@
+import {provide} from '@lit/context';
 import i18next from 'i18next';
 import {LitElement, html} from 'lit';
 import {customElement, state} from 'lit/decorators.js';
-import {Mock, vi} from 'vitest';
+import {vi} from 'vitest';
+import {bindingsContext} from '../components/context/bindings-context';
 import type {Bindings} from '../components/search/atomic-search-interface/interfaces';
-import {InitializableComponent} from '../decorators/types';
-import {fetchBindings} from '../utils/initialization-lit-stencil-common-utils';
-import {InitializeBindingsMixin} from './bindings-mixin';
-
-vi.mock('../utils/initialization-lit-stencil-common-utils', () => ({
-  fetchBindings: vi.fn(),
-}));
+import {bindings} from './bindings';
+import {InitializableComponent} from './types';
 
 const mockBindings = () =>
   ({
@@ -17,15 +14,16 @@ const mockBindings = () =>
   }) as Bindings;
 
 @customElement('test-element')
+@bindings()
 export class TestElement
-  extends InitializeBindingsMixin(LitElement)
+  extends LitElement
   implements InitializableComponent<Bindings>
 {
   @state()
-  public bindings!: Bindings;
+  public bindings: Bindings = {} as Bindings;
+  @state() public error!: Error;
 
   public initialized = false;
-  @state() public error!: Error;
 
   public render() {
     return html``;
@@ -33,75 +31,87 @@ export class TestElement
 
   initialize = vi.fn();
 }
+@customElement('test-interface-element')
+export class TestInterfaceElement extends LitElement {
+  @state()
+  @provide({context: bindingsContext})
+  public bindings: Bindings = {} as Bindings;
+  @state() public error!: Error;
 
-describe('InitializeBindingsMixin mixin', () => {
+  public initialized = false;
+
+  public render() {
+    return html`interface`;
+  }
+
+  public initialize() {
+    this.bindings = mockBindings();
+    this.bindings.i18n.init();
+  }
+}
+
+describe('bindings decorator', () => {
   let element: InitializableComponent<Bindings> & LitElement;
-  let bindings: Bindings;
+  let interfaceElement: TestInterfaceElement;
   let consoleErrorSpy: ReturnType<typeof vi.spyOn>;
-  const mockedFetchBindings = fetchBindings as Mock;
 
   const setupElement = async <T extends LitElement>(tag = 'test-element') => {
     element = document.createElement(tag) as InitializableComponent<Bindings> &
       T;
-    document.body.appendChild(element);
-    element.bindings = bindings;
+    interfaceElement.appendChild(element);
+
     await element.updateComplete;
   };
 
   const teardownElement = async () => {
-    if (document.body.contains(element)) {
-      document.body.removeChild(element);
+    if (interfaceElement.contains(element)) {
+      interfaceElement.removeChild(element);
+    }
+    if (document.body.contains(interfaceElement)) {
+      document.body.removeChild(interfaceElement);
     }
   };
 
   beforeEach(async () => {
-    bindings = mockBindings();
-    bindings.i18n.init();
-    mockedFetchBindings.mockImplementation(() => Promise.resolve(bindings));
+    interfaceElement = document.createElement(
+      'test-interface-element'
+    ) as TestInterfaceElement;
+    document.body.appendChild(interfaceElement);
+    interfaceElement.initialize();
+
     consoleErrorSpy = vi.spyOn(console, 'error');
   });
 
   afterEach(() => {
     teardownElement();
     consoleErrorSpy.mockRestore();
-    mockedFetchBindings.mockRestore();
   });
 
   it('should subscribe to language changes', async () => {
-    vi.spyOn(bindings.i18n, 'on');
+    vi.spyOn(interfaceElement.bindings.i18n, 'on');
     await setupElement();
 
     expect(element.bindings?.i18n.on).toHaveBeenCalled();
   });
 
   it('should unsubscribe to language changes when the host is disconnected from the component tree', async () => {
-    vi.spyOn(bindings.i18n, 'off');
+    vi.spyOn(interfaceElement.bindings.i18n, 'off');
     await setupElement();
     await teardownElement();
     expect(element.bindings?.i18n.off).toHaveBeenCalled();
+    expect(interfaceElement.bindings?.i18n.off).toHaveBeenCalled();
   });
 
   it('should request a component update when the language changes', async () => {
     await setupElement();
     vi.spyOn(element, 'requestUpdate');
-    element.bindings?.i18n.changeLanguage('fr');
+    interfaceElement.bindings?.i18n.changeLanguage('fr');
     expect(element.requestUpdate).toHaveBeenCalled();
   });
 
   it('should call the initialize method when bindings are present', async () => {
     await setupElement();
+
     expect(element.initialize).toHaveBeenCalled();
-  });
-
-  test('should handle fetchBindings rejection gracefully', async () => {
-    // Mock fetchBindings to return a rejected promise
-    mockedFetchBindings.mockRejectedValue(
-      new Error('Failed to fetch bindings')
-    );
-
-    // Call the function and expect it to reject
-    await expect(fetchBindings(element)).rejects.toThrow(
-      'Failed to fetch bindings'
-    );
   });
 });
