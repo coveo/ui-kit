674472efa48a0ad3b4676f5a6d4038699bf1141b
ylakhdar <ylakhdar@coveo.com>
2025-03-03 15:51:40 -0500

    build
    https://coveord.atlassian.net/browse/KIT-3948

--- packages/atomic-react/src/components/search/components.ts
+++ packages/atomic-react/src/components/search/components.ts
@@ -1,6 +1,15 @@
-import {AtomicIcon as LitAtomicIcon} from '@coveo/atomic/components';
+import {
+  AtomicComponentError as LitAtomicComponentError,
+  AtomicIcon as LitAtomicIcon,
+} from '@coveo/atomic/components';
 import {createComponent} from '@lit/react';
 import React from 'react';
+
+export const AtomicComponentError = createComponent({
+  tagName: 'atomic-component-error',
+  react: React,
+  elementClass: LitAtomicComponentError,
+});
 
 export const AtomicIcon = createComponent({
   tagName: 'atomic-icon',
