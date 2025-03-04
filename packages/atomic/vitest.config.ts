afbe7825c62ffaef369bb09002d2e59ac2dfa79d
ylakhdar <ylakhdar@coveo.com>
2025-03-04 09:27:37 -0500

    rename folder
    https://coveord.atlassian.net/browse/KIT-4010

--- packages/atomic/vitest.config.ts
+++ packages/atomic/vitest.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
       'src/**/initialization-utils.spec.ts',
       'src/**/search-layout.spec.ts',
     ],
-    setupFiles: ['./tests/setup.ts'],
+    setupFiles: ['./vitest-utils/setup.ts'],
     globals: true,
     browser: {
       enabled: true,
