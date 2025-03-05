8cb081b0d8e5ae6d7a3e13605114e32cdcd86242
ylakhdar <ylakhdar@coveo.com>
2025-03-05 09:17:53 -0500

    remove container selector from fixture
    https://coveord.atlassian.net/browse/KIT-4010

--- packages/atomic/vitest-utils/testing-helpers/fixture.ts
+++ packages/atomic/vitest-utils/testing-helpers/fixture.ts
@@ -1,13 +1,5 @@
 import {LitElement, render, TemplateResult} from 'lit';
-import {within} from 'shadow-dom-testing-library';
 import {fixtureWrapper} from './fixture-wrapper';
-
-const _container = {
-  lastParentNode: null as HTMLElement | null,
-  getLastParentNode() {
-    return this.lastParentNode;
-  },
-};
 
 /**
  * Asynchronously renders a Lit template into a parentNode and waits for the element to update.
@@ -22,24 +14,8 @@ export async function fixture<T extends LitElement>(
   const wrapper = fixtureWrapper(parentNode);
   render(template, wrapper);
 
-  _container.lastParentNode = parentNode;
-
   const element = wrapper.firstElementChild as T;
   await element.updateComplete;
 
   return element;
 }
-
-/**
- * Retrieves a testing-library `within` object for the last parentNode used in the fixture.
- * @returns {ReturnType<typeof within>} The `within` object for the last parentNode.
- * @throws Will throw an error if no parentNode is found.
- */
-export const container = () => {
-  const lastParentNode = _container.getLastParentNode();
-  if (lastParentNode) {
-    return within(lastParentNode);
-  } else {
-    throw 'No parentNode found. Make sure to call fixture() first.';
-  }
-};
