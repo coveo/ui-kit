eb48da9ee50b05314582b17aa07c6f41939bd7e6
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-03-12 09:29:11 -0400

    storyboook & vitest fixes
    https://coveord.atlassian.net/browse/KIT-3935

--- packages/atomic/src/components/commerce/atomic-commerce-product-list/atomic-commerce-product-list.new.stories.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-product-list/atomic-commerce-product-list.new.stories.tsx
@@ -1,12 +1,25 @@
+import {wrapInCommerceInterface} from '@/storybook-utils/commerce/commerce-interface-wrapper';
 import {parameters} from '@/storybook-utils/common/common-meta-parameters';
 import {renderComponent} from '@/storybook-utils/common/render-component';
-import {wrapInSearchInterface} from '@/storybook-utils/search/search-interface-wrapper';
-// import {wrapInCommerceInterface} from '@/storybook-utils/commerce/commerce-interface-wrapper';
+import {getSampleCommerceEngineConfiguration} from '@coveo/headless/commerce';
 import type {Meta, StoryObj as Story} from '@storybook/web-components';
 import './atomic-commerce-product-list';
 
 // Wrap it in whatever interface/component you need
-const {decorator, play} = wrapInSearchInterface();
+const {context} = getSampleCommerceEngineConfiguration();
+
+const {decorator, play} = wrapInCommerceInterface({
+  engineConfig: {
+    context: {
+      ...context,
+      view: {
+        url: 'https://sports.barca.group/browse/promotions/clothing/pants',
+      },
+    },
+  },
+  skipFirstRequest: false,
+  type: 'product-listing',
+});
 // const {decorator, play} = wrapInCommerceInterface();
 
 const meta: Meta = {
@@ -23,4 +36,9 @@ export default meta;
 
 export const Default: Story = {
   name: 'atomic-commerce-product-list',
+  args: {
+    'attributes-display': 'grid',
+    'attributes-density': 'compact',
+    'attributes-image-size': 'small',
+  },
 };
