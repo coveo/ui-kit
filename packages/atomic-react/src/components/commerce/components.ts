d2072102ff66c071296bda32247cb026738d5aec
ylakhdar <ylakhdar@coveo.com>
2025-03-17 09:32:25 -0400

    build
    https://coveord.atlassian.net/browse/KIT-3919

--- packages/atomic-react/src/components/commerce/components.ts
+++ packages/atomic-react/src/components/commerce/components.ts
@@ -1,18 +1,25 @@
 import {
-  AtomicIcon as LitAtomicIcon,
+  AtomicCommerceSortDropdown as LitAtomicCommerceSortDropdown,
   AtomicComponentError as LitAtomicComponentError,
+  AtomicIcon as LitAtomicIcon,
 } from '@coveo/atomic/components';
 import {createComponent} from '@lit/react';
 import React from 'react';
 
-export const AtomicIcon = createComponent({
-  tagName: 'atomic-icon',
+export const AtomicCommerceSortDropdown = createComponent({
+  tagName: 'atomic-commerce-sort-dropdown',
   react: React,
-  elementClass: LitAtomicIcon,
+  elementClass: LitAtomicCommerceSortDropdown,
 });
 
 export const AtomicComponentError = createComponent({
   tagName: 'atomic-component-error',
   react: React,
   elementClass: LitAtomicComponentError,
+});
+
+export const AtomicIcon = createComponent({
+  tagName: 'atomic-icon',
+  react: React,
+  elementClass: LitAtomicIcon,
 });
