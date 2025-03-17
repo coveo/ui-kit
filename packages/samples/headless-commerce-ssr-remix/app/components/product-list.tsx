5586b986435df651ea41e378a399722fffd4b839
Frederic Beaudoin <fbeaudoin@coveo.com>
2025-03-17 11:16:24 -0400

    Add highlighting

--- packages/samples/headless-commerce-ssr-remix/app/components/product-list.tsx
+++ packages/samples/headless-commerce-ssr-remix/app/components/product-list.tsx
@@ -1,6 +1,6 @@
 import {useProductList} from '@/lib/commerce-engine';
 import {formatCurrency} from '@/utils/format-utils';
-import {Product} from '@coveo/headless-react/ssr-commerce';
+import {HighlightKeyword, Product} from '@coveo/headless-react/ssr-commerce';
 import {useNavigate} from '@remix-run/react';
 
 interface IProductListProps {
@@ -53,19 +53,63 @@ export default function ProductList(props: IProductListProps) {
     return <hr />;
   };
 
+  const highlightKeywords = (text: string, highlights: HighlightKeyword[]) => {
+    let highlightedText = '';
+    let currentIndex = 0;
+
+    highlights.forEach((highlight) => {
+      highlightedText += text.slice(currentIndex, highlight.offset);
+      highlightedText += `<mark>${text.slice(
+        highlight.offset,
+        highlight.offset + highlight.length
+      )}</mark>`;
+      currentIndex = highlight.offset + highlight.length;
+    });
+
+    highlightedText += text.slice(currentIndex);
+
+    return {__html: highlightedText};
+  };
+
+  const getHighlightedTitle = (product: Product) => {
+    if (!product.ec_name) {
+      return {__html: ''};
+    }
+
+    if (!product.nameHighlights) {
+      return {__html: product.ec_name};
+    }
+
+    return highlightKeywords(product.ec_name, product.nameHighlights);
+  };
+
+  const getHighlightedExcerpt = (product: Product) => {
+    if (!product.excerpt) {
+      return {__html: ''};
+    }
+
+    if (!product.excerptHighlights) {
+      return {__html: product.excerpt};
+    }
+
+    return highlightKeywords(product.excerpt, product.excerptHighlights);
+  };
+
   return (
     <ul aria-label="Product List">
       {state.products.map((product, index) => (
         <li key={product.ec_product_id}>
-          <button disabled={!methods} onClick={() => onProductClick(product)}>
-            {product.ec_name}
-          </button>
+          <button
+            disabled={!methods}
+            onClick={() => onProductClick(product)}
+            dangerouslySetInnerHTML={getHighlightedTitle(product)}
+          ></button>
           <div style={{display: 'flex', flexDirection: 'row'}}>
             <div style={{flex: 3}}>
               <img src={product.ec_images[0]} height="100px"></img>
             </div>
             <div style={{flex: 9}}>
-              <p>{product.excerpt}</p>
+              <p dangerouslySetInnerHTML={getHighlightedExcerpt(product)}></p>
             </div>
           </div>
           <div style={{display: 'flex', flexDirection: 'row'}}>
