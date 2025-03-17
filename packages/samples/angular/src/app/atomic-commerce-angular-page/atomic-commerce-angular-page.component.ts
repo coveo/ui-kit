b826ac1ffe3569a2da0cc7cb72b74f9d59d945c3
Felix Perron-Brault <fpbrault@coveo.com>
2025-03-17 11:24:39 -0400

    add commerce page to angular sample
    https://coveord.atlassian.net/browse/KIT-3932

---
+++ packages/samples/angular/src/app/atomic-commerce-angular-page/atomic-commerce-angular-page.component.ts
@@ -0,0 +1,62 @@
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-atomic-commerce-angular-page',
+  templateUrl: './atomic-commerce-angular-page.component.html',
+  styleUrls: ['./atomic-commerce-angular-page.component.css'],
+})
+export class AtomicCommerceAngularPageComponent implements AfterViewInit {
+  @ViewChild('commerceInterface')
+  commerceInterface!: HTMLAtomicCommerceInterfaceElement;
+
+  constructor() {}
+  async ngAfterViewInit(): Promise<void> {
+    this.commerceInterface
+      ?.initialize({
+        accessToken: 'xx564559b1-0045-48e1-953c-3addd1ee4457',
+        analytics: {
+          trackingId: 'sports-ui-samples',
+        },
+        context: {
+          language: 'en',
+          country: 'US',
+          currency: 'USD',
+          view: {
+            url: 'https://sports.barca.group',
+          },
+        },
+        cart: {
+          items: [
+            {
+              productId: 'SP01057_00001',
+              quantity: 1,
+              name: 'Kayaker Canoe',
+              price: 800,
+            },
+            {
+              productId: 'SP00081_00001',
+              quantity: 1,
+              name: 'Bamboo Canoe Paddle',
+              price: 120,
+            },
+            {
+              productId: 'SP04236_00005',
+              quantity: 1,
+              name: 'Eco-Brave Rashguard',
+              price: 33,
+            },
+            {
+              productId: 'SP04236_00005',
+              quantity: 1,
+              name: 'Eco-Brave Rashguard',
+              price: 33,
+            },
+          ],
+        },
+        organizationId: 'searchuisamples',
+      })
+      .then(() => {
+        this?.commerceInterface.executeFirstRequest();
+      });
+  }
+}
