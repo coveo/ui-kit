f9f74df21faff31c72ebe27e85e94f8058968f9e
Louis Bompart <lbompart@coveo.com>
2025-03-28 01:05:11 -0400

    revert tailwindcss update

--- packages/atomic/src/components/commerce/atomic-commerce-load-more-products/atomic-commerce-load-more-products.new.stories.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-load-more-products/atomic-commerce-load-more-products.new.stories.tsx
@@ -1,9 +1,11 @@
 import {wrapInCommerceInterface} from '@/storybook-utils/commerce/commerce-interface-wrapper';
 import {parameters} from '@/storybook-utils/common/common-meta-parameters';
-import {renderComponent} from '@/storybook-utils/common/render-component';
+import {
+  renderComponent,
+  renderComponentWithoutCodeRoot,
+} from '@/storybook-utils/common/render-component';
 import type {Meta, StoryObj as Story} from '@storybook/web-components';
 import {html} from 'lit';
-import './atomic-commerce-load-more-products';
 
 const {decorator, play} = wrapInCommerceInterface({skipFirstRequest: false});
 
@@ -23,6 +25,7 @@ export const Default: Story = {};
 
 export const InPage: Story = {
   name: 'In a page',
+  render: renderComponentWithoutCodeRoot,
   decorators: [
     (story) =>
       html` <atomic-commerce-layout>
@@ -46,7 +49,7 @@ export const InPage: Story = {
             </atomic-commerce-product-list>
             <atomic-commerce-query-error></atomic-commerce-query-error>
           </atomic-layout-section>
-          <atomic-layout-section section="pagination">
+          <atomic-layout-section section="pagination" id="code-root">
             ${story()}
           </atomic-layout-section>
         </atomic-layout-section>
