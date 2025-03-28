9067eea228b59c66df7d07e50709a936db063720
Joshua Smith <199138957+jsmith2-coveo@users.noreply.github.com>
2025-03-28 16:14:31 -0400

    docs(headless,headless-react): typedoc styling update and nav cookies as functional
    https://coveord.atlassian.net/browse/KIT-3964

---
+++ packages/headless/typedoc/lib/insertMetaTags.ts
@@ -0,0 +1,11 @@
+export function insertMetaTags() {
+  const head = document.getElementsByTagName('head')[0];
+  if (head) {
+    head.innerHTML += `
+      <meta name="color-scheme" content="light dark">
+      <meta name="theme-color" content="light">
+      <meta name="docsSiteUrl" content="https://docs.coveo.com">
+      <meta name="docsSiteBaseUrl" content="/en">
+      `;
+  }
+}
