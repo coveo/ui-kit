c04ab11e5e05239dd9b6f5f49246ea441dbbdbd5
Frederic Beaudoin <fbeaudoin@coveo.com>
2025-03-31 16:34:21 -0400

    Use NotifyTrigger / QueryTrigger in samples
    https://coveord.atlassian.net/browse/KIT-4053

--- packages/samples/headless-commerce-react/src/components/triggers/notify-trigger.tsx
+++ packages/samples/headless-commerce-react/src/components/triggers/notify-trigger.tsx
@@ -1,9 +1,22 @@
-import {useNotifyTrigger} from '@/lib/commerce-engine';
+import {NotifyTrigger as HeadlessNotifyTrigger} from '@coveo/headless/commerce';
+import {useEffect, useState} from 'react';
+
+interface INotifyTriggerProps {
+  controller: HeadlessNotifyTrigger;
+}
 
 // Submit the query 'notify me' from the search box to activate the notification trigger.
 // A notification trigger is also automatically activated when accessing the Surf Accessories listing page.
-export default function NotifyTrigger() {
-  const {state} = useNotifyTrigger();
+export default function NotifyTrigger(props: INotifyTriggerProps) {
+  const {controller} = props;
+
+  const [state, setState] = useState(controller.state);
+
+  useEffect(() => {
+    controller.subscribe(() => {
+      setState(controller.state);
+    });
+  }, [controller]);
 
   if (state.notifications.length === 0) {
     return null;
---
+++ packages/samples/headless-commerce-react/src/components/triggers/query-trigger.tsx
@@ -0,0 +1,33 @@
+import {QueryTrigger as HeadlessQueryTrigger} from '@coveo/headless/commerce';
+import {useEffect, useState} from 'react';
+
+interface IQueryTriggerProps {
+  controller: HeadlessQueryTrigger;
+}
+
+// Submit the query 'query me' from the search box to activate the query trigger.
+export default function QueryTrigger(props: IQueryTriggerProps) {
+  const {controller} = props;
+
+  const [state, setState] = useState(controller.state);
+
+  useEffect(() => {
+    controller.subscribe(() => {
+      setState(controller.state);
+    });
+  }, [controller]);
+
+  if (!state.wasQueryModified) {
+    return null;
+  }
+
+  return (
+    <div className="QueryTrigger">
+      <p>
+        {' '}
+        The query changed from <b>{state.originalQuery}</b> to{' '}
+        <b>{state.newQuery}</b>
+      </p>
+    </div>
+  );
+}
