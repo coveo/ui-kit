ef1557eaa037a10d4e37e8f4e9965effcf33eac9
Felix Perron-Brault <fpbrault@coveo.com>
2025-04-09 15:57:44 -0400

    add lit carousel
    https://coveord.atlassian.net/browse/KIT-3937

---
+++ packages/atomic/src/components/common/carousel.ts
@@ -0,0 +1,80 @@
+import {FunctionalComponentWithChildren} from '@/src/utils/functional-component-utils';
+import {TemplateResult} from 'lit';
+import {html} from 'lit';
+import ArrowRight from '../../images/arrow-right.svg';
+import './atomic-icon/atomic-icon';
+import {button, ButtonProps} from './button';
+import {AnyBindings} from './interface/bindings';
+
+export interface CarouselProps {
+  bindings: AnyBindings;
+  previousPage(): void;
+  nextPage(): void;
+  numberOfPages: number;
+  currentPage: number;
+}
+
+export const carousel: FunctionalComponentWithChildren<CarouselProps> = ({
+  props,
+}) => {
+  const {previousPage, nextPage, numberOfPages, currentPage} = props;
+
+  const renderPreviousButton = () => {
+    const buttonProps: ButtonProps = {
+      style: 'outline-primary',
+      part: 'previous-button',
+      class: 'group',
+      onClick: previousPage,
+      ariaLabel: props.bindings.i18n.t('previous'),
+    };
+    return button({
+      props: buttonProps,
+    })(
+      html`<atomic-icon
+        icon="${ArrowRight}"
+        class="carousel-previous"
+      ></atomic-icon>`
+    );
+  };
+
+  const renderNextButton = () => {
+    const buttonProps: ButtonProps = {
+      style: 'outline-primary',
+      part: 'next-button',
+      class: 'group',
+      onClick: nextPage,
+      ariaLabel: props.bindings.i18n.t('next'),
+    };
+    return button({
+      props: buttonProps,
+    })(
+      html`<atomic-icon
+        icon="${ArrowRight}"
+        class="carousel-next"
+      ></atomic-icon>`
+    );
+  };
+
+  const renderIndicators = () => html`
+    <ul part="indicators" class="mt-6 flex justify-center gap-2">
+      ${Array.from({length: numberOfPages}, (_, index) => {
+        const isActive = index === currentPage % numberOfPages;
+        return html`
+          <li
+            part=${`indicator ${isActive ? 'active-indicator' : ''}`}
+            class=${`h-1 w-12 rounded-md ${
+              isActive ? 'bg-primary' : 'bg-neutral'
+            }`}
+          ></li>
+        `;
+      })}
+    </ul>
+  `;
+
+  return (children: TemplateResult) => html`
+    <div class="carousel relative">
+      ${renderPreviousButton()} ${children} ${renderNextButton()}
+    </div>
+    ${renderIndicators()}
+  `;
+};
