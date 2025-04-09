6fda2deceb54a0688751a572f6fb65977f6de336
Felix Perron-Brault <fpbrault@coveo.com>
2025-04-09 16:04:07 -0400

    add commerce smoke tests for atomic commerce search/recs
    https://coveord.atlassian.net/browse/KIT-3937

---
+++ packages/samples/atomic-react/src/pages/CommerceRecommendationPage.tsx
@@ -0,0 +1,81 @@
+import {
+  AtomicCommerceRecommendationInterface,
+  AtomicProductLink,
+  AtomicProductText,
+  AtomicProductPrice,
+  AtomicProductDescription,
+  AtomicProductSectionVisual,
+  AtomicProductImage,
+  AtomicProductRating,
+  AtomicProductSectionDescription,
+  AtomicProductSectionEmphasized,
+  AtomicProductSectionMetadata,
+  AtomicProductSectionName,
+  AtomicCommerceRecommendationList,
+} from '@coveo/atomic-react/commerce';
+import {
+  buildCommerceEngine,
+  getSampleCommerceEngineConfiguration,
+} from '@coveo/headless/commerce';
+import {useMemo} from 'react';
+
+export const CommerceRecommendationPage = () => {
+  const engine = useMemo(
+    () =>
+      buildCommerceEngine({
+        configuration: getSampleCommerceEngineConfiguration(),
+      }),
+    []
+  );
+
+  return (
+    <>
+      <style>
+        {`
+    atomic-commerce-recommendation-list {
+      margin-top: var(--atomic-layout-spacing-y);
+      --atomic-recs-number-of-columns: 3;
+    }
+
+    @media only screen and (max-width: 1024px) {
+      atomic-commerce-recommendation-list {
+        --atomic-recs-number-of-columns: 1;
+      }
+    }`}
+      </style>
+      <AtomicCommerceRecommendationInterface engine={engine}>
+        <AtomicCommerceRecommendationList
+          productsPerPage={3}
+          slotId="af4fb7ba-6641-4b67-9cf9-be67e9f30174"
+          template={MyTemplate}
+        ></AtomicCommerceRecommendationList>
+      </AtomicCommerceRecommendationInterface>
+    </>
+  );
+};
+
+function MyTemplate() {
+  return (
+    <>
+      <AtomicProductSectionName>
+        <AtomicProductLink />
+      </AtomicProductSectionName>
+      <AtomicProductSectionVisual>
+        <AtomicProductImage field="ec_thumbnails"></AtomicProductImage>
+      </AtomicProductSectionVisual>
+      <AtomicProductSectionMetadata>
+        <AtomicProductText
+          field="ec_brand"
+          class="text-neutral-dark block"
+        ></AtomicProductText>
+        <AtomicProductRating field="ec_rating"></AtomicProductRating>
+      </AtomicProductSectionMetadata>
+      <AtomicProductSectionEmphasized>
+        <AtomicProductPrice />
+      </AtomicProductSectionEmphasized>
+      <AtomicProductSectionDescription>
+        <AtomicProductDescription />
+      </AtomicProductSectionDescription>
+    </>
+  );
+}
