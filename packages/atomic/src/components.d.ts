240894590e3c62b717951abc3b10e731a43ff5ca
Felix Perron-Brault <fpbrault@coveo.com>
2025-04-09 16:02:18 -0400

    migrate atomic-commerce-recommendation-list to lit
    https://coveord.atlassian.net/browse/KIT-3937
    
    add commerce recs list and commerce recs itnerfae wrapper
    
    https://coveord.atlassian.net/browse/KIT-3937
    
    add commerce smoke tests for atomic commerce search/recs
    
    https://coveord.atlassian.net/browse/KIT-3937
    
    fix css
    
    https://coveord.atlassian.net/browse/KIT-3937
    
    fix sections
    
    https://coveord.atlassian.net/browse/KIT-3937
    
    fix styles
    
    https://coveord.atlassian.net/browse/KIT-3937
    
    lint fix
    
    https://coveord.atlassian.net/browse/KIT-3937

--- packages/atomic/src/components.d.ts
+++ packages/atomic/src/components.d.ts
@@ -453,53 +453,6 @@ export namespace Components {
     interface AtomicCommerceQuerySummary {
     }
     /**
-     * The `atomic-commerce-recommendation-list` component displays a list of product recommendations by applying one or more product templates.
-     * @alpha 
-     */
-    interface AtomicCommerceRecommendationList {
-        /**
-          * The spacing of various elements in the product list, including the gap between products, the gap between parts of a product, and the font sizes of the parts of a product.
-         */
-        "density": ItemDisplayDensity;
-        /**
-          * The layout to apply when displaying the products. This does not affect the display of the surrounding list itself. To modify the number of products per column, modify the `--atomic-recs-number-of-columns` CSS variable.
-         */
-        "display": ItemDisplayBasicLayout;
-        /**
-          * The [heading level](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Heading_Elements) to use for the heading label, from 1 to 6.
-         */
-        "headingLevel": number;
-        /**
-          * The expected size of the image displayed on the recommended products.
-         */
-        "imageSize": ItemDisplayImageSize;
-        /**
-          * Moves to the next page, when the carousel is activated.
-         */
-        "nextPage": () => Promise<void>;
-        /**
-          * Moves to the previous page, when the carousel is activated.
-         */
-        "previousPage": () => Promise<void>;
-        /**
-          * The unique identifier of the product to use for seeded recommendations.
-         */
-        "productId"?: string;
-        /**
-          * The number of products to display per page. The products will be displayed in a carousel if this property is set. This does not affect the display of the list itself, only the number of recommendation pages. If you want to display the recommendations in a carousel with a single row, set the `--atomic-recs-number-fof-columns` CSS variable to the same value as this property.
-         */
-        "productsPerPage"?: number;
-        /**
-          * Sets a rendering function to bypass the standard HTML template mechanism when rendering products. You can use this function while working with web frameworks that don't use plain HTML syntax, e.g., React, Angular, or Vue.  Do not use this method if you integrate Atomic in a plain HTML implementation.
-          * @param productRenderingFunction
-         */
-        "setRenderFunction": (productRenderingFunction: ItemRenderingFunction) => Promise<void>;
-        /**
-          * The identifier used by the Commerce API to retrieve the desired recommendation list for the component. You can configure recommendation lists and get their respective slot IDs through the Coveo Merchandising Hub (CMH). You can include multiple `atomic-commerce-recommendation-list` components with different slot IDs in the same page to display several recommendation lists.
-         */
-        "slotId": string;
-    }
-    /**
      * The `atomic-commerce-refine-modal` is automatically created as a child of the `atomic-commerce-search-interface` when the `atomic-commerce-refine-toggle` is initialized.
      * When the modal is opened, the class `atomic-modal-opened` is added to the interface element and the body, allowing further customization.
      * @alpha 
@@ -4030,16 +3983,6 @@ declare global {
         new (): HTMLAtomicCommerceQuerySummaryElement;
     };
     /**
-     * The `atomic-commerce-recommendation-list` component displays a list of product recommendations by applying one or more product templates.
-     * @alpha 
-     */
-    interface HTMLAtomicCommerceRecommendationListElement extends Components.AtomicCommerceRecommendationList, HTMLStencilElement {
-    }
-    var HTMLAtomicCommerceRecommendationListElement: {
-        prototype: HTMLAtomicCommerceRecommendationListElement;
-        new (): HTMLAtomicCommerceRecommendationListElement;
-    };
-    /**
      * The `atomic-commerce-refine-modal` is automatically created as a child of the `atomic-commerce-search-interface` when the `atomic-commerce-refine-toggle` is initialized.
      * When the modal is opened, the class `atomic-modal-opened` is added to the interface element and the body, allowing further customization.
      * @alpha 
@@ -5932,7 +5875,6 @@ declare global {
         "atomic-commerce-products-per-page": HTMLAtomicCommerceProductsPerPageElement;
         "atomic-commerce-query-error": HTMLAtomicCommerceQueryErrorElement;
         "atomic-commerce-query-summary": HTMLAtomicCommerceQuerySummaryElement;
-        "atomic-commerce-recommendation-list": HTMLAtomicCommerceRecommendationListElement;
         "atomic-commerce-refine-modal": HTMLAtomicCommerceRefineModalElement;
         "atomic-commerce-refine-toggle": HTMLAtomicCommerceRefineToggleElement;
         "atomic-commerce-search-box": HTMLAtomicCommerceSearchBoxElement;
@@ -6492,40 +6434,6 @@ declare namespace LocalJSX {
      * @alpha 
      */
     interface AtomicCommerceQuerySummary {
-    }
-    /**
-     * The `atomic-commerce-recommendation-list` component displays a list of product recommendations by applying one or more product templates.
-     * @alpha 
-     */
-    interface AtomicCommerceRecommendationList {
-        /**
-          * The spacing of various elements in the product list, including the gap between products, the gap between parts of a product, and the font sizes of the parts of a product.
-         */
-        "density"?: ItemDisplayDensity;
-        /**
-          * The layout to apply when displaying the products. This does not affect the display of the surrounding list itself. To modify the number of products per column, modify the `--atomic-recs-number-of-columns` CSS variable.
-         */
-        "display"?: ItemDisplayBasicLayout;
-        /**
-          * The [heading level](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Heading_Elements) to use for the heading label, from 1 to 6.
-         */
-        "headingLevel"?: number;
-        /**
-          * The expected size of the image displayed on the recommended products.
-         */
-        "imageSize"?: ItemDisplayImageSize;
-        /**
-          * The unique identifier of the product to use for seeded recommendations.
-         */
-        "productId"?: string;
-        /**
-          * The number of products to display per page. The products will be displayed in a carousel if this property is set. This does not affect the display of the list itself, only the number of recommendation pages. If you want to display the recommendations in a carousel with a single row, set the `--atomic-recs-number-fof-columns` CSS variable to the same value as this property.
-         */
-        "productsPerPage"?: number;
-        /**
-          * The identifier used by the Commerce API to retrieve the desired recommendation list for the component. You can configure recommendation lists and get their respective slot IDs through the Coveo Merchandising Hub (CMH). You can include multiple `atomic-commerce-recommendation-list` components with different slot IDs in the same page to display several recommendation lists.
-         */
-        "slotId"?: string;
     }
     /**
      * The `atomic-commerce-refine-modal` is automatically created as a child of the `atomic-commerce-search-interface` when the `atomic-commerce-refine-toggle` is initialized.
@@ -9633,7 +9541,6 @@ declare namespace LocalJSX {
         "atomic-commerce-products-per-page": AtomicCommerceProductsPerPage;
         "atomic-commerce-query-error": AtomicCommerceQueryError;
         "atomic-commerce-query-summary": AtomicCommerceQuerySummary;
-        "atomic-commerce-recommendation-list": AtomicCommerceRecommendationList;
         "atomic-commerce-refine-modal": AtomicCommerceRefineModal;
         "atomic-commerce-refine-toggle": AtomicCommerceRefineToggle;
         "atomic-commerce-search-box": AtomicCommerceSearchBox;
@@ -9919,11 +9826,6 @@ declare module "@stencil/core" {
              * @alpha 
              */
             "atomic-commerce-query-summary": LocalJSX.AtomicCommerceQuerySummary & JSXBase.HTMLAttributes<HTMLAtomicCommerceQuerySummaryElement>;
-            /**
-             * The `atomic-commerce-recommendation-list` component displays a list of product recommendations by applying one or more product templates.
-             * @alpha 
-             */
-            "atomic-commerce-recommendation-list": LocalJSX.AtomicCommerceRecommendationList & JSXBase.HTMLAttributes<HTMLAtomicCommerceRecommendationListElement>;
             /**
              * The `atomic-commerce-refine-modal` is automatically created as a child of the `atomic-commerce-search-interface` when the `atomic-commerce-refine-toggle` is initialized.
              * When the modal is opened, the class `atomic-modal-opened` is added to the interface element and the body, allowing further customization.
