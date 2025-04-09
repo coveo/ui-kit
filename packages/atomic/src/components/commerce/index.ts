2c335a950078abf2f4faaf00671b5b05aa15928b
Felix Perron-Brault <fpbrault@coveo.com>
2025-04-09 16:02:18 -0400

    migrate atomic-commerce-recommendation-list to lit
    https://coveord.atlassian.net/browse/KIT-3937

--- packages/atomic/src/components/commerce/index.ts
+++ packages/atomic/src/components/commerce/index.ts
@@ -1,3 +1,4 @@
 // Auto-generated file
 export {AtomicCommerceInterface} from './atomic-commerce-interface/atomic-commerce-interface.js';
 export {AtomicCommerceProductList} from './atomic-commerce-product-list/atomic-commerce-product-list.js';
+export {AtomicCommerceRecommendationList} from './atomic-commerce-recommendation-list/atomic-commerce-recommendation-list.js';
