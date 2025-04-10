a4dbc38fc5ecdb44fe8da5b37429af0b37c7cde4
Felix Perron-Brault <fpbrault@coveo.com>
2025-04-10 10:10:12 -0400

    fix build
    https://coveord.atlassian.net/browse/KIT-3932
    Signed-off-by: Felix Perron-Brault <fpbrault@coveo.com>

--- packages/atomic-angular/projects/atomic-angular/src/lib/stencil-generated/components.ts
+++ packages/atomic-angular/projects/atomic-angular/src/lib/stencil-generated/components.ts
@@ -3031,32 +3031,6 @@ export declare interface AtomicCommerceInterface extends LitAtomicCommerceInterf
 }
 
 @ProxyCmp({
-  inputs: ['element', 'error'],
-  methods: [],
-  defineCustomElementFn: () => {customElements.get('atomic-component-error') || customElements.define('atomic-component-error', LitAtomicComponentError);}
-})
-@Component({
-  selector: 'atomic-component-error',
-  standalone: false,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  template: '<ng-content></ng-content>',
-  // eslint-disable-next-line @angular-eslint/no-inputs-metadata-property
-  inputs: ['element', 'error']
-})
-export class AtomicComponentError {
-  protected readonly el: HTMLElement;
-  constructor(c: ChangeDetectorRef, el: ElementRef, protected z: NgZone) {
-    c.detach();
-    this.el = el.nativeElement;
-    
-  }
-}
-
-export declare interface AtomicComponentError extends LitAtomicComponentError {
-
-}
-
-@ProxyCmp({
   inputs: ['icon'],
   methods: ['updateIcon', 'initialize', 'initBindings'],
   defineCustomElementFn: () => {customElements.get('atomic-icon') || customElements.define('atomic-icon', LitAtomicIcon);}
@@ -3079,6 +3053,31 @@ export class AtomicIcon {
 }
 
 export declare interface AtomicIcon extends LitAtomicIcon {
+
+}
+
+@ProxyCmp({
+  inputs: ['element', 'error'],
+  methods: [],
+  defineCustomElementFn: () => {customElements.get('atomic-component-error') || customElements.define('atomic-component-error', LitAtomicComponentError);}
+})
+@Component({
+  selector: 'atomic-component-error',
+  standalone: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  // eslint-disable-next-line @angular-eslint/no-inputs-metadata-property
+  inputs: ['element', 'error']
+})
+export class AtomicComponentError {
+  protected readonly el: HTMLElement;
+  constructor(c: ChangeDetectorRef, el: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = el.nativeElement;
+  }
+}
+
+export declare interface AtomicComponentError extends LitAtomicComponentError {
 
 }
 
