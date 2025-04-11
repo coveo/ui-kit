ffd1e338b122937e7a1e9f6fbde8f60842a38f45
ylakhdar <ylakhdar@coveo.com>
2025-04-11 12:59:40 -0400

    build
    https://coveord.atlassian.net/browse/KIT-3919

--- packages/atomic/src/components/commerce/index.ts
+++ packages/atomic/src/components/commerce/index.ts
@@ -1,4 +1,2 @@
-// Export the class of the components migrated to Lit here, like this:
-export {AtomicComponentError} from '../common/atomic-component-error/atomic-component-error';
-export {AtomicIcon} from '../common/atomic-icon/atomic-icon';
-export {AtomicCommerceSortDropdown} from './atomic-commerce-sort-dropdown/atomic-commerce-sort-dropdown';
+// Auto-generated file
+export {AtomicCommerceSortDropdown} from './atomic-commerce-sort-dropdown/atomic-commerce-sort-dropdown.js';
