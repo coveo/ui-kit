b74354a70f5142971d07291dfef30d53babba3bc
Felix Perron-Brault <fpbrault@coveo.com>
2025-04-14 10:14:15 -0400

    linting fix
    https://coveord.atlassian.net/browse/KIT-3932

--- packages/atomic/src/components/common/lazy-index.ts
+++ packages/atomic/src/components/common/lazy-index.ts
@@ -1,6 +1,7 @@
 // Auto-generated file
 export default {
-  'atomic-component-error': async () => await import('./atomic-component-error/atomic-component-error.js'),
+  'atomic-component-error': async () =>
+    await import('./atomic-component-error/atomic-component-error.js'),
   'atomic-icon': async () => await import('./atomic-icon/atomic-icon.js'),
 } as Record<string, () => Promise<unknown>>;
 
