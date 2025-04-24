3a6100e614f44ab7dc02eeb74d2b6fe5d4a916c3
Felix Perron-Brault <fpbrault@coveo.com>
2025-04-24 16:01:25 -0400

    persist recent queries in headless-react samples
    https://coveord.atlassian.net/browse/KIT-4136
    Signed-off-by: Felix Perron-Brault <fpbrault@coveo.com>

--- packages/samples/headless-react/src/components/recent-queries/recent-queries.class.tsx
+++ packages/samples/headless-react/src/components/recent-queries/recent-queries.class.tsx
@@ -13,22 +13,39 @@ export class RecentQueriesList extends Component<
   RecentQueriesState
 > {
   static contextType = AppContext;
-  context!: ContextType<typeof AppContext>;
+  declare context: ContextType<typeof AppContext>;
 
   private controller!: HeadlessRecentQueriesList;
   private unsubscribe: Unsubscribe = () => {};
 
   componentDidMount() {
+    const initialState = {queries: this.retrieveLocalStorage()};
     this.controller = buildRecentQueriesList(this.context.engine!, {
       options: this.props,
+      initialState,
     });
     this.updateState();
 
-    this.unsubscribe = this.controller.subscribe(() => this.updateState());
+    this.unsubscribe = this.controller.subscribe(() => {
+      this.updateState();
+      this.updateLocalStorage();
+    });
   }
 
   componentWillUnmount() {
     this.unsubscribe();
+  }
+
+  private retrieveLocalStorage() {
+    const storedQueries = localStorage.getItem('recentQueries');
+    return storedQueries ? JSON.parse(storedQueries) : [];
+  }
+
+  private updateLocalStorage() {
+    localStorage.setItem(
+      'recentQueries',
+      JSON.stringify(this.controller.state.queries)
+    );
   }
 
   private updateState() {
