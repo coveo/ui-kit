7e3050823a1a0143b25fb572270542d8b84f4360
Frederic Beaudoin <fbeaudoin@coveo.com>
2025-04-25 16:19:52 -0400

    Add unit tests for display wrapper functional components
    https://coveord.atlassian.net/browse/KIT-3935

---
+++ packages/atomic/src/components/common/item-list/display-wrapper-lit.spec.ts
@@ -0,0 +1,87 @@
+import {renderFunctionFixture} from '@/vitest-utils/testing-helpers/fixture';
+import {html, nothing, TemplateResult} from 'lit';
+import {
+  DisplayWrapperProps,
+  renderListRoot,
+  renderListWrapper,
+} from './display-wrapper-lit';
+
+describe('renderListWrapper', () => {
+  const setupElement = async (
+    props: Partial<DisplayWrapperProps>,
+    children?: TemplateResult
+  ) => {
+    return await renderFunctionFixture(
+      html`${renderListWrapper({props: {listClasses: props.listClasses || ''}})(
+        children || nothing
+      )}`
+    );
+  };
+
+  it('should render with correct class', async () => {
+    const props = {listClasses: 'test-class'};
+    const element = await setupElement(props);
+
+    const renderedElements = element.querySelectorAll('*');
+
+    expect(renderedElements.length).toBe(1);
+    expect(renderedElements[0]?.classList).toContain('test-class');
+  });
+
+  it('should render children', async () => {
+    const children = html`<div id="test-child"></div>`;
+    const element = await setupElement({}, children);
+
+    const renderedElement = element.querySelectorAll('*')[0];
+
+    const renderedChildren = renderedElement.querySelectorAll('*');
+
+    expect(renderedChildren.length).toBe(1);
+    expect(renderedChildren[0]?.id).toBe('test-child');
+  });
+});
+
+describe('renderListRoot', () => {
+  const setupElement = async (
+    props: Partial<DisplayWrapperProps>,
+    children?: TemplateResult
+  ) => {
+    return await renderFunctionFixture(
+      html`${renderListRoot({
+        props: {
+          listClasses: props.listClasses || '',
+        },
+      })(children || nothing)}`
+    );
+  };
+
+  it('should render with correct class', async () => {
+    const props = {listClasses: 'test-class'};
+    const element = await setupElement(props);
+
+    const renderedElements = element.querySelectorAll('*');
+
+    expect(renderedElements.length).toBe(1);
+    expect(renderedElements[0]?.classList).toContain('test-class');
+  });
+
+  it('should render with correct part', async () => {
+    const element = await setupElement({});
+
+    const renderedElement = element.querySelectorAll('*')[0];
+
+    expect(renderedElement?.part.value).toBe('result-list');
+  });
+
+  it('should render children', async () => {
+    const children = html`<div id="test-child"></div>`;
+    const element = await setupElement({}, children);
+
+    const renderedElement = element.querySelectorAll('*')[0];
+
+    const renderedChildren = renderedElement.querySelectorAll('*');
+
+    expect(renderedChildren.length).toBe(1);
+    expect(renderedChildren[0]?.id).toBe('test-child');
+  });
+});
