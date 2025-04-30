ea9fd072b77c798db10c41ad3d09e142f349e6da
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-04-30 16:00:38 -0400

    Delete bla.spec.ts
    https://coveord.atlassian.net/browse/KIT-3936

--- packages/atomic/src/components/commerce/atomic-commerce-pager/bla.spec.ts
+++
@@ -1,8 +0,0 @@
-import {userEvent} from '@vitest/browser/context';
-import {describe, test} from 'vitest';
-
-describe('bla', () => {
-  test('boom', async () => {
-    await userEvent.click(document.querySelector('body')!);
-  });
-});
