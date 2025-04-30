fa9f6630e10071ba2e8a1ec72816d1924ec9d6e3
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-04-30 12:26:15 -0400

    final stuff
    https://coveord.atlassian.net/browse/KIT-3936

--- packages/atomic/vitest.config.ts
+++ packages/atomic/vitest.config.ts
@@ -90,7 +90,7 @@ export default defineConfig({
         {
           browser: 'chromium',
           context: {
-            actionTimeout: 3000,
+            actionTimeout: 5000,
           },
         },
       ],
