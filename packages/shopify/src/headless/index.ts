25fc47f4add02b33915380521c3d18ae7a5ad072
alanclarke <640611+alanclarke@users.noreply.github.com>
2025-05-01 18:11:11 +0100

    refactor(shopify): rename shopify package

--- packages/shopify/src/headless/index.ts
+++ packages/shopify/src/headless/index.ts
@@ -7,9 +7,10 @@ import {
   clientIdKey,
   CustomEnvironment,
 } from '@coveo/relay';
-import {v5} from 'uuid';
+import {getClientId} from '../utils';
 
 export * from '@coveo/headless/commerce';
+export * from '../utils';
 
 export interface AppProxyOptions {
   appProxyUrl?: string;
@@ -84,20 +85,6 @@ export function buildShopifyCommerceEngine({
   );
 
   return buildCommerceEngine(commerceEngineOptions);
-}
-
-/**
- * Generates a unique client identifier for the Shopify store.
- *
- * This function creates a version 5 UUID based on the provided
- * shop-specific data and the Shopify cookie value.
- *
- * @param shop - The identifier of the Shopify store.
- * @param shopifyCookie - The value of the Shopify _shopify_y cookie.
- * @returns A version 5 UUID string uniquely representing the client.
- */
-export function getClientId(shop: string, shopifyCookie: string): string {
-  return v5(shopifyCookie, shop);
 }
 
 function getCookie(name: string) {
