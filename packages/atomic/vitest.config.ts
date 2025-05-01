37800b1d6716bd4753e1461572b596f3acbe6fb1
Louis Bompart <lbompart@coveo.com>
2025-05-01 01:46:13 -0400

    use tsconfigPaths
    add up tsconfig for vitest to allow the alias to automagicallywork

--- packages/atomic/vitest.config.ts
+++ packages/atomic/vitest.config.ts
@@ -1,6 +1,7 @@
 import {readFileSync} from 'fs';
 import path from 'node:path';
 import {dirname, resolve} from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import {defineConfig} from 'vitest/config';
 //@ts-expect-error - normal json import
 import packageJson from './package.json' with {type: 'json'};
@@ -32,11 +33,6 @@ export default defineConfig({
   },
   server: {
     port: port,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(import.meta.dirname, './'),
-    },
   },
   plugins: [
     {
@@ -70,6 +66,7 @@ export default defineConfig({
         return null;
       },
     },
+    tsconfigPaths(),
   ],
   test: {
     include: ['src/**/*.spec.ts', 'scripts/stencil-proxy.spec.mjs'],
