a83bb2a18e1216115b4493fa3498d5a774fb90de
Michael Grondines <mgrondines@coveo.com>
2025-05-05 11:10:36 -0400

    fix test

--- packages/headless/src/controllers/insight-interface/insight-interface.test.ts
+++ packages/headless/src/controllers/insight-interface/insight-interface.test.ts
@@ -1,6 +1,5 @@
 import {configuration} from '../../app/common-reducers.js';
 import {insightConfigurationReducer as insightConfiguration} from '../../features/insight-configuration/insight-configuration-slice.js';
-import {fetchInterface} from '../../features/insight-interface/insight-interface-actions.js';
 import {insightInterfaceReducer as insightInterface} from '../../features/insight-interface/insight-interface-slice.js';
 import {searchHubReducer as searchHub} from '../../features/search-hub/search-hub-slice.js';
 import {
@@ -8,21 +7,14 @@ import {
   MockedInsightEngine,
 } from '../../test/mock-engine-v2.js';
 import {buildMockInsightState} from '../../test/mock-insight-state.js';
-import {buildInsightInterface, InsightInterface} from './insight-interface.js';
 
 vi.mock('../../features/insight-interface/insight-interface-actions');
 
 describe('Insight Interface', () => {
   let engine: MockedInsightEngine;
-  let controller: InsightInterface;
-
-  function initInsightInterface() {
-    controller = buildInsightInterface(engine);
-  }
 
   beforeEach(() => {
     engine = buildMockInsightEngine(buildMockInsightState());
-    initInsightInterface();
   });
 
   it('adds the correct reducers to the engine', () => {
@@ -31,13 +23,6 @@ describe('Insight Interface', () => {
       insightConfiguration,
       insightInterface,
       searchHub,
-    });
-  });
-
-  describe('#fetch', () => {
-    it('should dispatch a #fetchInterface action', () => {
-      controller.fetch();
-      expect(fetchInterface).toHaveBeenCalled();
     });
   });
 });
