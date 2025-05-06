3733cc3bd021f20fbe2a313c3b6b3f5414f9d320
Frederic Beaudoin <fbeaudoin@coveo.com>
2025-05-06 10:04:56 -0400

    Add ProductTemplateProvider spec
    https://coveord.atlassian.net/browse/KIT-4203

---
+++ packages/atomic/src/components/commerce/product-list/product-template-provider.spec.ts
@@ -0,0 +1,154 @@
+import {buildProductTemplatesManager, Product} from '@coveo/headless/commerce';
+import {beforeEach, describe, expect, it, vi} from 'vitest';
+import {ItemTarget} from '../../common/layout/display-options';
+import {TemplateProviderProps} from '../../common/template-provider/template-provider';
+import {ProductTemplateProvider} from './product-template-provider';
+
+vi.mock('@coveo/headless/commerce', {spy: true});
+
+describe('ProductTemplateProvider', () => {
+  const registerTemplates = vi.fn();
+
+  beforeEach(() => {
+    vi.mocked(buildProductTemplatesManager).mockReturnValue({
+      registerTemplates,
+      selectLinkTemplate: vi.fn(),
+      selectTemplate: vi.fn(),
+    });
+  });
+
+  describe('#constructor', () => {
+    it('should create an instance', () => {
+      const productTemplateProvider = productTemplateProviderFixture();
+
+      expect(productTemplateProvider).toBeInstanceOf(ProductTemplateProvider);
+    });
+
+    it('should call the headless #buildProductTemplatesManager function', () => {
+      productTemplateProviderFixture();
+
+      expect(buildProductTemplatesManager).toHaveBeenCalled();
+    });
+
+    describe("when #includeDefaultTemplate is 'true' and #templateElements is empty", () => {
+      it('should register the default template without conditions', async () => {
+        productTemplateProviderFixture({
+          includeDefaultTemplate: true,
+          templateElements: [],
+        });
+
+        await completeMicrotasks();
+
+        expect(registerTemplates).toHaveBeenCalledOnce();
+        expect(registerTemplates.mock.lastCall?.[0].conditions.length).toBe(0);
+      });
+
+      it('should register the correct default product template', async () => {
+        productTemplateProviderFixture({
+          includeDefaultTemplate: true,
+          templateElements: [],
+        });
+
+        await completeMicrotasks();
+
+        expect(registerTemplates).toHaveBeenCalledOnce();
+        expect(registerTemplates.mock.lastCall?.[0].conditions.length).toBe(0);
+
+        const defaultTemplate = registerTemplates.mock.lastCall?.[0].content;
+        expect(defaultTemplate).toBeInstanceOf(DocumentFragment);
+
+        expect(defaultTemplate.children.length).toBe(2);
+
+        expect(defaultTemplate.children[0]?.nodeName).toBe(
+          'ATOMIC-PRODUCT-SECTION-NAME'
+        );
+        expect(defaultTemplate.children[0].children.length).toBe(1);
+        expect(defaultTemplate.children[0].children[0]?.nodeName).toBe(
+          'ATOMIC-PRODUCT-LINK'
+        );
+        expect(defaultTemplate.children[0].children[0]).toHaveClass(
+          'font-bold'
+        );
+
+        expect(defaultTemplate.children[1]?.nodeName).toBe(
+          'ATOMIC-PRODUCT-SECTION-VISUAL'
+        );
+        expect(defaultTemplate.children[1].children.length).toBe(1);
+        expect(defaultTemplate.children[1].children[0]?.nodeName).toBe(
+          'ATOMIC-PRODUCT-IMAGE'
+        );
+        expect(defaultTemplate.children[1].children[0]).toHaveAttribute(
+          'field',
+          'ec_thumbnails'
+        );
+      });
+
+      it('should register the correct default link template when #gridCellLinkTarget is undefined', async () => {
+        productTemplateProviderFixture({
+          includeDefaultTemplate: true,
+          templateElements: [],
+        });
+
+        await completeMicrotasks();
+
+        const defaultLinkTemplate =
+          registerTemplates.mock.lastCall?.[0].linkContent;
+        expect(defaultLinkTemplate).toBeInstanceOf(DocumentFragment);
+
+        expect(defaultLinkTemplate.children.length).toBe(1);
+        expect(defaultLinkTemplate.children[0]?.nodeName).toBe(
+          'ATOMIC-PRODUCT-LINK'
+        );
+      });
+
+      it('should register the correct default link template when #gridCellLinkTarget is defined', async () => {
+        productTemplateProviderFixture(
+          {includeDefaultTemplate: true, templateElements: []},
+          '_blank'
+        );
+
+        await completeMicrotasks();
+
+        const defaultLinkTemplate =
+          registerTemplates.mock.lastCall?.[0].linkContent;
+        expect(defaultLinkTemplate).toBeInstanceOf(DocumentFragment);
+
+        expect(defaultLinkTemplate.children.length).toBe(1);
+        expect(defaultLinkTemplate.children[0]?.nodeName).toBe(
+          'ATOMIC-PRODUCT-LINK'
+        );
+        expect(defaultLinkTemplate.children[0].children.length).toBe(1);
+        expect(defaultLinkTemplate.children[0].children[0]).toHaveAttribute(
+          'slot',
+          'attributes'
+        );
+        expect(defaultLinkTemplate.children[0].children[0]).toHaveAttribute(
+          'target',
+          '_blank'
+        );
+      });
+    });
+  });
+
+  const productTemplateProviderFixture = (
+    props: Partial<TemplateProviderProps<Product>> = {},
+    gridCellLinkTarget?: ItemTarget
+  ) => {
+    return new ProductTemplateProvider(
+      {
+        getResultTemplateRegistered: vi.fn(),
+        getTemplateHasError: vi.fn(),
+        setTemplateHasError: vi.fn(),
+        templateElements: [],
+        includeDefaultTemplate: true,
+        setResultTemplateRegistered: vi.fn(),
+        ...props,
+      },
+      gridCellLinkTarget
+    );
+  };
+
+  const completeMicrotasks = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+  };
+});
