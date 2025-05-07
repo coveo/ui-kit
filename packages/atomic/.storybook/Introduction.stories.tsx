08114eb98454c32c0877fb5bf46c60e151a3a8ca
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2025-05-07 11:37:57 -0400

    draft
    https://coveord.atlassian.net/browse/KIT-4055

--- packages/atomic/.storybook/Introduction.stories.tsx
+++ packages/atomic/.storybook/Introduction.stories.tsx
@@ -9,8 +9,12 @@ export default meta;
 
 export const Default: StoryObj = {
   name: 'Default',
-  render: () => '<h1>Welcome to the Coveo Atomic Storybook</h1>',
-  // Would be nice to hide the panel, but not currently possible
+  render: () => `<h1>Welcome to the Coveo Atomic Storybook</h1>
+  <p>
+    This Storybook showcases interactive documentation for the <a href="https://docs.coveo.com/en/atomic/latest" target="_blank">Coveo Atomic library</a>.
+  </p>
+  `,
+  // It would be nice to hide the panel, but it isn't currently possible
   // https://github.com/storybookjs/storybook/issues/14442
 };
 
