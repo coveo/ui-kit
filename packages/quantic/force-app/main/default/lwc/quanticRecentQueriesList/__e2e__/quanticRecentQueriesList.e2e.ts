ca2cc76082ef2914666c5349c71c849a422f1449
simonmilord <simon.milord@gmail.com>
2025-05-08 16:46:39 -0400

    added e2e tests

---
+++ packages/quantic/force-app/main/default/lwc/quanticRecentQueriesList/__e2e__/quanticRecentQueriesList.e2e.ts
@@ -0,0 +1,110 @@
+import {testSearch, expect} from './fixture';
+
+const exampleQueries = ['gandalf', 'gollum', 'boromir'];
+
+let test = testSearch;
+
+async function triggerSearchWithInput(search, query) {
+  const searchResponsePromise = search.waitForSearchResponse();
+  await search.fillSearchInput(query);
+  await search.performSearch();
+  await searchResponsePromise;
+}
+
+async function assertRecentQueriesList(recentQueriesList, expectedQueries) {
+  const recentQueriesListItems =
+    await recentQueriesList.recentQueriesListItems.all();
+  expect(recentQueriesListItems.length).toEqual(expectedQueries.length);
+
+  for (let i = 0; i < expectedQueries.length; i++) {
+    const queryText = await recentQueriesListItems[i].innerText();
+    expect(queryText).toEqual(expectedQueries[i]);
+  }
+}
+
+test.describe(`quantic recent queries list`, () => {
+  test.describe('when making a search query', () => {
+    test('should add the query to the recent queries list', async ({
+      search,
+      recentQueriesList,
+    }) => {
+      const searchResponsePromise = search.waitForSearchResponse();
+      await search.fillSearchInput(exampleQueries[0]);
+      await search.performSearch();
+      await searchResponsePromise;
+
+      const firstRecentQuery =
+        await recentQueriesList.recentQueriesListItems.first();
+      const firstRecentQueryText = await firstRecentQuery.innerText();
+      expect(firstRecentQueryText).toEqual(exampleQueries[0]);
+    });
+
+    test.describe('when the query is already in the recent queries list', () => {
+      test('should set it as the first recent query and remove the last one', async ({
+        search,
+        recentQueriesList,
+      }) => {
+        await triggerSearchWithInput(search, exampleQueries[0]);
+        await assertRecentQueriesList(recentQueriesList, [exampleQueries[0]]);
+
+        await triggerSearchWithInput(search, exampleQueries[1]);
+        await assertRecentQueriesList(recentQueriesList, [
+          exampleQueries[1],
+          exampleQueries[0],
+        ]);
+
+        await triggerSearchWithInput(search, exampleQueries[0]);
+        await assertRecentQueriesList(recentQueriesList, [
+          exampleQueries[0],
+          exampleQueries[1],
+        ]);
+      });
+    });
+  });
+
+  test.describe('when passing a q value as parameter in the URL', () => {
+    test.use({
+      urlHash: `q=${exampleQueries[0]}`,
+    });
+
+    test('should add the query to the recent queries list', async ({
+      recentQueriesList,
+    }) => {
+      const firstRecentQuery =
+        await recentQueriesList.recentQueriesListItems.first();
+      const firstRecentQueryText = await firstRecentQuery.innerText();
+      expect(firstRecentQueryText).toEqual(exampleQueries[0]);
+    });
+  });
+
+  test.describe('when clicking on a recent query', () => {
+    test('should trigger a search query, log the corresponding UA analytics events and set the query in the URL hash', async ({
+      search,
+      page,
+      recentQueriesList,
+    }) => {
+      // Make an initial search to populate the recent queries list
+      await triggerSearchWithInput(search, exampleQueries[0]);
+
+      const recentQueryClickSearchResponsePromise =
+        search.waitForSearchResponse();
+      const uaRequestPromise =
+        recentQueriesList.waitForRecentQueryClickAnalytics({
+          queryText: exampleQueries[0],
+        });
+
+      const firstRecentQuery =
+        await recentQueriesList.recentQueriesListItems.first();
+      await firstRecentQuery.click();
+
+      const searchResponse = await recentQueryClickSearchResponsePromise;
+      await uaRequestPromise;
+
+      const searchResponseBody = search.extractDataFromResponse(searchResponse);
+      expect(searchResponseBody.q).toEqual(exampleQueries[0]);
+
+      const urlHash = await page.url();
+      expect(urlHash).toContain(`q=${exampleQueries[0]}`);
+    });
+  });
+});
