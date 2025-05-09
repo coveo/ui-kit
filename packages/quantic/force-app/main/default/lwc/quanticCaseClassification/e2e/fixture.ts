30392098deec449f600cae34fa96e44940988e51
mmitiche <mmitiche@coveo.com>
2025-05-09 08:53:09 -0400

    introducing ep to playwright

--- packages/quantic/force-app/main/default/lwc/quanticCaseClassification/e2e/fixture.ts
+++ packages/quantic/force-app/main/default/lwc/quanticCaseClassification/e2e/fixture.ts
@@ -8,6 +8,8 @@ import {
   sfDefaultField,
 } from './data';
 
+type AnalyticsMode = 'ua' | 'ep';
+
 const pageUrl = 's/quantic-case-classification';
 
 interface CaseClassificationOptions {
@@ -21,16 +23,24 @@ type QuanticCaseClassificationE2EFixtures = {
   options: Partial<CaseClassificationOptions>;
 };
 
-export const test = quanticBase.extend<QuanticCaseClassificationE2EFixtures>({
+export const test = quanticBase.extend<
+  QuanticCaseClassificationE2EFixtures & {
+    analyticsMode: AnalyticsMode;
+  }
+>({
   options: {},
+  analyticsMode: ['ua', {option: true}],
   caseAssist: async ({page}, use) => {
     await use(new CaseAssistObject(page));
   },
   caseClassification: async (
-    {page, options, configuration, caseAssist},
+    {page, options, configuration, caseAssist, analyticsMode},
     use
   ) => {
-    const caseClassificationObject = new CaseClassificationObject(page);
+    const caseClassificationObject = new CaseClassificationObject(
+      page,
+      analyticsMode
+    );
     caseClassificationObject.mockSfPicklistValues(sfDefaultField, allOptions);
     caseClassificationObject.mockCaseClassification(
       coveoDefaultField,
