f7f2487c4f59f7c9c44ba270195d94c5c4842aed
Felix Perron-Brault <fpbrault@coveo.com>
2025-05-14 10:42:30 -0400

    improve carousel
    https://coveord.atlassian.net/browse/KIT-3937

--- packages/atomic/src/components/common/carousel.spec.ts
+++ packages/atomic/src/components/common/carousel.spec.ts
@@ -1,11 +1,20 @@
-import {html, render} from 'lit';
+import {renderFunctionFixture} from '@/vitest-utils/testing-helpers/fixture';
+import {createTestI18n} from '@/vitest-utils/testing-helpers/i18n-utils';
+import {page} from '@vitest/browser/context';
+import '@vitest/browser/matchers.d.ts';
+import {html, nothing, TemplateResult} from 'lit';
 import {within} from 'shadow-dom-testing-library';
-import {vi} from 'vitest';
-import {carousel, CarouselProps} from './carousel';
+import {vi, expect, describe, beforeAll, it, beforeEach} from 'vitest';
+import {renderCarousel, CarouselProps} from './carousel';
 import {AnyBindings} from './interface/bindings';
 
 describe('carousel', () => {
   let container: HTMLElement;
+  let i18n: Awaited<ReturnType<typeof createTestI18n>>;
+
+  beforeAll(async () => {
+    i18n = await createTestI18n();
+  });
 
   beforeEach(() => {
     if (container) {
@@ -15,63 +24,156 @@ describe('carousel', () => {
     document.body.appendChild(container);
   });
 
-  const renderCarousel = (
-    props: Partial<CarouselProps>,
-    children?: string
-  ): HTMLElement => {
-    render(
-      html` ${carousel({
-        props: {
-          ...props,
-          bindings: {
-            i18n: {
-              t: (key: string) => key,
-            },
-          } as AnyBindings,
-          previousPage: props.previousPage ?? vi.fn(),
-          nextPage: props.nextPage ?? vi.fn(),
-          numberOfPages: props.numberOfPages ?? 3,
-          currentPage: props.currentPage ?? 0,
-        },
-      })(html`${children}`)}`,
-      container
-    );
-    return container as HTMLElement;
+  const locators = {
+    get previousButton() {
+      return page.getByRole('button', {
+        name: 'previous',
+      });
+    },
+    get previousButtonIcon() {
+      return locators.previousButton.element().querySelector('atomic-icon');
+    },
+    get nextButton() {
+      return page.getByRole('button', {
+        name: 'next',
+      });
+    },
+    get nextButtonIcon() {
+      return locators.nextButton.element().querySelector('atomic-icon');
+    },
+    get indicator() {
+      return page.getByRole('listitem');
+    },
+    get indicatorContainer() {
+      return page.getByRole('list', {
+        name: i18n.t('carousel-indicators'),
+      });
+    },
   };
 
-  it('should render the carousel in the document', () => {
-    const props = {};
-    const carouselElement = renderCarousel(props);
-    expect(carouselElement).toBeInTheDocument();
+  const renderComponent = (
+    props: Partial<CarouselProps> = {},
+    children?: TemplateResult | typeof nothing
+  ) => {
+    const defaultChildren = html`<div class="flex justify-center gap-4">
+      <div class="rounded border p-2">product 1</div>
+      <div class="rounded border p-2">product 2</div>
+      <div class="rounded border p-2">product 3</div>
+    </div>`;
+
+    const defaultProps = {
+      bindings: {
+        i18n: i18n,
+      } as AnyBindings,
+      previousPage: vi.fn(),
+      nextPage: vi.fn(),
+      numberOfPages: 3,
+      currentPage: 0,
+    };
+    const mergedProps = {...defaultProps, ...props};
+    return renderFunctionFixture(
+      html`${renderCarousel({props: mergedProps})(children ?? defaultChildren)}`
+    );
+  };
+
+  it('should render the carousel in the document with its children', async () => {
+    const carouselElement = await renderComponent();
+    const children = within(carouselElement).getAllByRole('listitem');
+    expect(children.length).toBeGreaterThan(0);
   });
 
-  it('should render the correct number of indicators', () => {
-    const props = {numberOfPages: 5};
-    const carouselElement = renderCarousel(props);
-    console.log(carouselElement);
+  it('should render nothing when no children are passed', async () => {
+    const carouselElement = await renderComponent({}, nothing);
+    expect(carouselElement.textContent).toBe('');
+  });
+
+  it('should render the correct number of indicators', async () => {
+    const carouselElement = await renderComponent({
+      numberOfPages: 5,
+    });
     const indicators = within(carouselElement).getAllByRole('listitem');
     expect(indicators.length).toBe(5);
   });
 
-  it('should call previousPage when the previous button is clicked', () => {
-    const previousPage = vi.fn();
-    const props = {previousPage};
-    const carouselElement = renderCarousel(props);
+  it('should render an icon inside the previous button', async () => {
+    await renderComponent();
+
+    await expect.element(locators.previousButtonIcon).toBeVisible();
+    await expect
+      .element(locators.previousButtonIcon)
+      .toHaveAttribute('icon', expect.stringMatching(/<svg/));
+  });
+
+  it('should render an icon inside the next button', async () => {
+    await renderComponent();
+
+    await expect.element(locators.nextButtonIcon).toBeVisible();
+    await expect
+      .element(locators.nextButtonIcon)
+      .toHaveAttribute('icon', expect.stringMatching(/<svg/));
+  });
+
+  it('should have the correct part attribute for the "previous" button', async () => {
+    await renderComponent();
+    const showMoreButton = await locators.previousButton.element();
+    await expect
+      .element(showMoreButton)
+      .toHaveAttribute('part', 'previous-button');
+  });
+
+  it('should have the correct part attribute for the "next" button', async () => {
+    await renderComponent();
+    const showMoreButton = await locators.nextButton.element();
+    await expect(showMoreButton).toHaveAttribute('part', 'next-button');
+  });
+
+  it('should have the correct part attribute for the icon', async () => {
+    await renderComponent();
+    await expect
+      .element(locators.previousButtonIcon)
+      .toHaveAttribute('part', 'previous-icon');
+    await expect
+      .element(locators.nextButtonIcon)
+      .toHaveAttribute('part', 'next-icon');
+  });
+
+  it('should call #previousPage when the previous button is clicked', async () => {
+    const previousPageMock = vi.fn();
+
+    const carouselElement = await renderComponent({
+      previousPage: previousPageMock,
+    });
     const previousButton = carouselElement.querySelector(
       '[part="previous-button"]'
     ) as HTMLElement;
     previousButton.click();
-    expect(previousPage).toHaveBeenCalled();
+    expect(previousPageMock).toHaveBeenCalled();
   });
 
-  it('should call nextPage when the next button is clicked', () => {
-    const nextPage = vi.fn();
-    const props = {nextPage};
-    const carouselElement = renderCarousel(props);
+  it('should call #nextPage when the next button is clicked', async () => {
+    const nextPageMock = vi.fn();
+    const carouselElement = await renderComponent({
+      nextPage: nextPageMock,
+    });
     const nextButton = carouselElement.querySelector(
       '[part="next-button"]'
     ) as HTMLElement;
     nextButton.click();
-    expect(nextPage).toHaveBeenCalled();
+    expect(nextPageMock).toHaveBeenCalled();
+  });
+
+  it('should not render buttons or indicators when there is only one page', async () => {
+    const carouselElement = await renderComponent({
+      numberOfPages: 1,
+    });
+    const previousButton = carouselElement.querySelector(
+      '[part="previous-button"]'
+    );
+    const nextButton = carouselElement.querySelector('[part="next-button"]');
+    const indicators = carouselElement.querySelector('[part="indicators"]');
+
+    expect(previousButton).toBeNull();
+    expect(nextButton).toBeNull();
+    expect(indicators).toBeNull();
   });
 });
