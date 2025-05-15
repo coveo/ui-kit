4a15c3f409cb5ff44c7196b2ebf700e8a1c77d9d
Frederic Beaudoin <fbeaudoin@coveo.com>
2025-05-15 11:44:23 -0400

    Add missing tests
    https://coveord.atlassian.net/browse/KIT-4204

--- packages/atomic/src/components/common/item-list/item-list-common.spec.ts
+++ packages/atomic/src/components/common/item-list/item-list-common.spec.ts
@@ -1,12 +1,21 @@
 import {CommerceStore} from '@/src/components';
 import {getFirstFocusableDescendant} from '@/src/utils/accessibility-utils';
 import {updateBreakpoints} from '@/src/utils/replace-breakpoint';
-import {beforeEach, describe, expect, it, vi} from 'vitest';
-import {ItemListCommonProps} from './item-list-common';
-import {ItemListCommon} from './item-list-common';
+import {defer} from '@/src/utils/utils';
+import {afterEach, beforeEach, describe, expect, it, vi} from 'vitest';
+import {ItemListCommon, ItemListCommonProps} from './item-list-common';
 
 vi.mock('@/src/utils/accessibility-utils', {spy: true});
 vi.mock('@/src/utils/replace-breakpoint', {spy: true});
+vi.mock('@/src/utils/utils', async () => {
+  const actual = await vi.importActual('@/src/utils/utils');
+  return {
+    ...actual,
+    defer: vi.fn(async () => {
+      return new Promise((resolve) => resolve(true));
+    }),
+  };
+});
 
 describe('ItemListCommon', () => {
   describe('#constructor', () => {
@@ -48,7 +57,6 @@ describe('ItemListCommon', () => {
 
       itemListCommon.updateBreakpoints();
       itemListCommon.updateBreakpoints();
-      itemListCommon.updateBreakpoints();
 
       expect(mockedUpdateBreakpoints).toHaveBeenCalledOnce();
       expect(mockedUpdateBreakpoints).toHaveBeenCalledWith(host);
@@ -72,16 +80,8 @@ describe('ItemListCommon', () => {
   });
 
   describe('#setNewResultRef', () => {
-    // TODO: We should test side effects on #focusOnFirstResultAfterNexSearch instead, but this will do for now.
-    it('should set #firstResultEl to the element when resultIndex is 0', () => {
-      const itemListCommon = itemListCommonFixture();
-      const element = document.createElement('span');
-
-      itemListCommon.setNewResultRef(element, 0);
-
-      // @ts-expect-error - testing private property
-      expect(itemListCommon.firstResultEl).toBe(element);
-    });
+    // Note: the side effects this method can have when called with a defined element and a resultIndex of 0 are tested
+    // in the #focusOnFirstResultAfterNextSearch test suite.
 
     it('should not call #getFirstFocusableDescendant / #props.nextNewItemTarget.setTarget when resultIndex is not the index of the result to focus', () => {
       const getFirstFocusableDescendantMock = vi.mocked(
@@ -108,7 +108,7 @@ describe('ItemListCommon', () => {
       const getCurrentNumberOfItems = vi.fn(() => 0);
       const itemListCommon = itemListCommonFixture({getCurrentNumberOfItems});
 
-      // This will set itemListCommon.indexOfResultToFocus to the return value of getCurrentNumbnerOfItems (0).
+      // This will set itemListCommon.indexOfResultToFocus to the return value of getCurrentNumberOfItems (0).
       itemListCommon.focusOnNextNewResult();
 
       // Ensure resultIndex is equal to itemListCommon.indexOfResultToFocus to test the right condition.
@@ -261,8 +261,288 @@ describe('ItemListCommon', () => {
   });
 
   describe('#focusOnFirstResultAfterNextSearch', () => {
-    it('TODO', () => {
-      // TODO: This method would be very complex to test. We should probably rework it.
+    beforeEach(() => {
+      vi.useFakeTimers();
+    });
+
+    afterEach(() => {
+      vi.useRealTimers();
+    });
+
+    it('should call the #defer util function', async () => {
+      const mockedDefer = vi.mocked(defer);
+      const itemListCommon = itemListCommonFixture();
+
+      mockedDefer.mockClear();
+
+      itemListCommon.focusOnFirstResultAfterNextSearch();
+
+      expect(defer).toHaveBeenCalledOnce();
+    });
+
+    it('should return a promise', () => {
+      const itemListCommon = itemListCommonFixture();
+
+      const result = itemListCommon.focusOnFirstResultAfterNextSearch();
+
+      expect(result).toBeInstanceOf(Promise);
+    });
+
+    describe('when its promise resolves', () => {
+      it('should clear the current reference to the first result element when #props.getIsLoading() returns true', async () => {
+        const mockedDefer = vi.mocked(defer);
+        const unsubscribe = vi.fn();
+        const engineSubscribe = vi.fn().mockReturnValue(unsubscribe);
+        const itemListCommon = itemListCommonFixture({
+          engineSubscribe,
+          getIsLoading: vi
+            .fn()
+            .mockReturnValue(false)
+            .mockReturnValueOnce(true),
+        });
+
+        // This will set itemListCommon.firstResultEl to the element
+        itemListCommon.setNewResultRef(document.createElement('div'), 0);
+
+        // getIsLoading returns true on the first call, so itemListCommon.firstResultEl should be cleared.
+        itemListCommon.focusOnFirstResultAfterNextSearch();
+        await vi.runAllTimersAsync();
+
+        mockedDefer.mockClear();
+        unsubscribe.mockClear();
+
+        // getIsLoading now returns false but itemListCommon.firstResultEl was cleared, so we should not unsubscribe.
+        await engineSubscribe.mock.lastCall?.[0]();
+
+        expect(unsubscribe).not.toHaveBeenCalled();
+
+        // This will set itemListCommon.firstResultEl again.
+        itemListCommon.setNewResultRef(document.createElement('div'), 0);
+
+        // getIsLoading now returns false, so itemListCommon.firstResultEl should not be cleared.
+        itemListCommon.focusOnFirstResultAfterNextSearch();
+        await vi.runAllTimersAsync();
+
+        unsubscribe.mockClear();
+
+        // getIsLoading returns false and itemListCommon.firstResultEl is defined, so we should unsubscribe
+        await engineSubscribe.mock.lastCall?.[0]();
+
+        expect(unsubscribe).toHaveBeenCalledOnce();
+      });
+
+      it('should call #props.engineSubscribe with a function', async () => {
+        const engineSubscribe = vi.fn();
+        const itemListCommon = itemListCommonFixture({engineSubscribe});
+
+        engineSubscribe.mockClear();
+
+        itemListCommon.focusOnFirstResultAfterNextSearch();
+        await vi.runAllTimersAsync();
+
+        expect(engineSubscribe).toHaveBeenCalledOnce();
+        expect(engineSubscribe).toHaveBeenCalledWith(expect.any(Function));
+      });
+
+      describe('when the #props.engineSubscribe function called and its promise is awaited', () => {
+        it('should call the #defer util function', async () => {
+          const mockedDefer = vi.mocked(defer);
+          const engineSubscribe = vi.fn();
+          const itemListCommon = itemListCommonFixture({engineSubscribe});
+
+          itemListCommon.focusOnFirstResultAfterNextSearch();
+          await vi.runAllTimersAsync();
+
+          mockedDefer.mockClear();
+
+          await engineSubscribe.mock.lastCall?.[0]();
+
+          expect(mockedDefer).toHaveBeenCalledOnce();
+        });
+        it('should not unsubscribe when #props.getIsLoading() returns true even if #setNewResultRef was previously called with a defined element and a result index of 0', async () => {
+          const unsubscribe = vi.fn();
+          const engineSubscribe = vi.fn().mockReturnValue(unsubscribe);
+          const itemListCommon = itemListCommonFixture({
+            engineSubscribe,
+            getIsLoading: vi.fn(() => true),
+          });
+
+          itemListCommon.setNewResultRef(document.createElement('div'), 0);
+          itemListCommon.focusOnFirstResultAfterNextSearch();
+          await vi.runAllTimersAsync();
+
+          unsubscribe.mockClear();
+
+          await engineSubscribe.mock.lastCall?.[0]();
+
+          expect(unsubscribe).not.toHaveBeenCalled();
+        });
+
+        it('should not unsubscribe when #setNewResultRef was not previously called even if #props.getIsLoading() returns false', async () => {
+          const unsubscribe = vi.fn();
+          const engineSubscribe = vi.fn().mockReturnValue(unsubscribe);
+          const itemListCommon = itemListCommonFixture({
+            engineSubscribe,
+            getIsLoading: vi.fn(() => false),
+          });
+
+          itemListCommon.focusOnFirstResultAfterNextSearch();
+          await vi.runAllTimersAsync();
+
+          unsubscribe.mockClear();
+
+          await engineSubscribe.mock.lastCall?.[0]();
+
+          expect(unsubscribe).not.toHaveBeenCalled();
+        });
+        describe('when #props.getIsLoading() returns false and #setNewResultRef was previously called with a defined element and a result index of 0', () => {
+          it('should call the #getFirstFocusableDescendant util function with the element #setNewResultRef was called with', async () => {
+            const getFirstFocusableDescendantMock = vi.mocked(
+              getFirstFocusableDescendant
+            );
+            const engineSubscribe = vi.fn().mockReturnValue(vi.fn());
+            const itemListCommon = itemListCommonFixture({
+              engineSubscribe,
+              getIsLoading: vi.fn(() => false),
+            });
+
+            const element = document.createElement('div');
+            itemListCommon.setNewResultRef(element, 0);
+            itemListCommon.focusOnFirstResultAfterNextSearch();
+            await vi.runAllTimersAsync();
+
+            getFirstFocusableDescendantMock.mockClear();
+
+            await engineSubscribe.mock.lastCall?.[0]();
+
+            expect(getFirstFocusableDescendantMock).toHaveBeenCalledOnce();
+            expect(getFirstFocusableDescendantMock).toHaveBeenCalledWith(
+              element
+            );
+          });
+
+          it('should call ##props.nextNewItemTarget.setTarget with the value returned by the #getFirstFocusableDescendant call when this value is not null', async () => {
+            const focusableElement = document.createElement('button');
+            vi.mocked(getFirstFocusableDescendant).mockReturnValue(
+              focusableElement
+            );
+            const engineSubscribe = vi.fn().mockReturnValue(vi.fn());
+            const setTarget = vi.fn();
+            const itemListCommon = itemListCommonFixture({
+              engineSubscribe,
+              getIsLoading: vi.fn(() => false),
+              nextNewItemTarget: {
+                focus: vi.fn(),
+                focusAfterSearch: vi.fn(),
+                focusOnNextTarget: vi.fn(),
+                setTarget,
+              },
+            });
+
+            itemListCommon.setNewResultRef(document.createElement('div'), 0);
+            itemListCommon.focusOnFirstResultAfterNextSearch();
+            await vi.runAllTimersAsync();
+
+            setTarget.mockClear();
+
+            await engineSubscribe.mock.lastCall?.[0]();
+
+            expect(setTarget).toHaveBeenCalledOnce();
+            expect(setTarget).toHaveBeenCalledWith(focusableElement);
+          });
+
+          it('should call #props.nextNewItemTarget.setTarget with the element #setNewResultRef was called with when #getFirstFocusableDescendant returns null', async () => {
+            vi.mocked(getFirstFocusableDescendant).mockReturnValue(null);
+            const engineSubscribe = vi.fn().mockReturnValue(vi.fn());
+            const setTarget = vi.fn();
+            const itemListCommon = itemListCommonFixture({
+              engineSubscribe,
+              getIsLoading: vi.fn(() => false),
+              nextNewItemTarget: {
+                focus: vi.fn(),
+                focusAfterSearch: vi.fn(),
+                focusOnNextTarget: vi.fn(),
+                setTarget,
+              },
+            });
+
+            const element = document.createElement('div');
+            itemListCommon.setNewResultRef(element, 0);
+            itemListCommon.focusOnFirstResultAfterNextSearch();
+            await vi.runAllTimersAsync();
+
+            setTarget.mockClear();
+
+            await engineSubscribe.mock.lastCall?.[0]();
+
+            expect(setTarget).toHaveBeenCalledOnce();
+            expect(setTarget).toHaveBeenCalledWith(element);
+          });
+
+          it('should call #props.nextNewItemTarget.focus', async () => {
+            const engineSubscribe = vi.fn().mockReturnValue(vi.fn());
+            const focus = vi.fn();
+            const itemListCommon = itemListCommonFixture({
+              engineSubscribe,
+              getIsLoading: vi.fn(() => false),
+              nextNewItemTarget: {
+                focus,
+                focusAfterSearch: vi.fn(),
+                focusOnNextTarget: vi.fn(),
+                setTarget: vi.fn(),
+              },
+            });
+
+            itemListCommon.setNewResultRef(document.createElement('div'), 0);
+            itemListCommon.focusOnFirstResultAfterNextSearch();
+            await vi.runAllTimersAsync();
+
+            focus.mockClear();
+
+            await engineSubscribe.mock.lastCall?.[0]();
+
+            expect(focus).toHaveBeenCalledOnce();
+          });
+          it('should unsubscribe', async () => {
+            const unsubscribe = vi.fn();
+            const engineSubscribe = vi.fn().mockReturnValue(unsubscribe);
+            const itemListCommon = itemListCommonFixture({
+              engineSubscribe,
+              getIsLoading: vi.fn(() => false),
+            });
+
+            itemListCommon.setNewResultRef(document.createElement('div'), 0);
+            itemListCommon.focusOnFirstResultAfterNextSearch();
+            await vi.runAllTimersAsync();
+
+            unsubscribe.mockClear();
+
+            await engineSubscribe.mock.lastCall?.[0]();
+
+            expect(unsubscribe).toHaveBeenCalledOnce();
+          });
+          it('should not unsubscribe on subsequent calls', async () => {
+            const unsubscribe = vi.fn();
+            const engineSubscribe = vi.fn().mockReturnValue(unsubscribe);
+            const getIsLoading = vi.fn(() => false);
+            const itemListCommon = itemListCommonFixture({
+              engineSubscribe,
+              getIsLoading,
+            });
+
+            itemListCommon.setNewResultRef(document.createElement('div'), 0);
+            itemListCommon.focusOnFirstResultAfterNextSearch();
+            await vi.runAllTimersAsync();
+
+            unsubscribe.mockClear();
+
+            await engineSubscribe.mock.lastCall?.[0]();
+            await engineSubscribe.mock.lastCall?.[0]();
+
+            expect(unsubscribe).toHaveBeenCalledOnce();
+          });
+        });
+      });
     });
   });
 
