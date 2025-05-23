ae4b79cfad88a2c75e34a1b6bcdd6bf7ae65d0b3
Michael Grondines <mgrondines@coveo.com>
2025-05-23 10:04:14 -0400

    pr changes

--- packages/quantic/force-app/main/default/lwc/quanticResultTemplate/__tests__/quanticResultTemplate.test.js
+++ packages/quantic/force-app/main/default/lwc/quanticResultTemplate/__tests__/quanticResultTemplate.test.js
@@ -11,19 +11,17 @@ const selectors = {
   slotByName: (name) => `slot[name="${name}"]`,
 };
 
-const slots = {
-  LABEL: 'label',
-  BADGES: 'badges',
-  ACTIONS: 'actions',
-  DATE: 'date',
-  VISUAL: 'visual',
-  TITLE: 'title',
-  METADATA: 'metadata',
-  EMPHASIZED: 'emphasized',
-  EXCERPT: 'excerpt',
-  BOTTOM_METADATA: 'bottom-metadata',
-  CHILDREN: 'children',
-};
+const previewSlots = ['label', 'badges', 'date', 'visual'];
+
+const otherSlots = [
+  'actions',
+  'title',
+  'metadata',
+  'emphasized',
+  'excerpt',
+  'bottom-metadata',
+  'children',
+];
 
 describe('c-quantic-result-template', () => {
   afterEach(() => {
@@ -34,21 +32,59 @@ describe('c-quantic-result-template', () => {
     const element = createTestComponent();
     await flushPromises();
 
-    for (const [key, name] of Object.entries(slots)) {
+    expect(element.shadowRoot.firstChild.className).toBe(
+      'lgc-bg slds-p-vertical_medium slds-border_bottom'
+    );
+
+    for (const name of previewSlots.concat(otherSlots)) {
       let slot = element.shadowRoot.querySelectorAll(
         selectors.slotByName(name)
       );
       expect(slot).not.toBeNull();
       expect(slot.length).toBe(1);
-
-      const newElem = document.createElement('div');
-      newElem.setAttribute('slot', name);
-      newElem.innerText = key;
-      element.shadowRoot.appendChild(newElem);
-
-      slot = element.shadowRoot.querySelectorAll(selectors.slotByName(name));
-      expect(slot).not.toBeNull();
-      expect(slot.length).toBe(1);
     }
+  });
+
+  describe('with isAnyPreviewOpen set to true', () => {
+    it('should display only the slots that are set to true', async () => {
+      const element = createTestComponent({
+        isAnyPreviewOpen: true,
+      });
+      await flushPromises();
+
+      for (const name of previewSlots) {
+        let slot = element.shadowRoot.querySelectorAll(
+          selectors.slotByName(name)
+        );
+        expect(slot).not.toBeNull();
+        expect(slot.length).toBe(1);
+        expect(slot[0].parentElement.ariaHidden).toBe('true');
+      }
+    });
+  });
+
+  describe('with resultPreviewShouldNotBeAccessible set to true', () => {
+    it('should hide the actions slot', async () => {
+      const element = createTestComponent({
+        resultPreviewShouldNotBeAccessible: true,
+      });
+      await flushPromises();
+      expect(
+        element.shadowRoot.querySelector(selectors.slotByName('actions'))
+          .parentElement.ariaHidden
+      ).toBe('true');
+    });
+  });
+
+  describe('with hideBorder set to true', () => {
+    it('should set the hideBorder class', async () => {
+      const element = createTestComponent({
+        hideBorder: true,
+      });
+      await flushPromises();
+      expect(element.shadowRoot.firstChild.className).toBe(
+        'lgc-bg slds-p-vertical_medium'
+      );
+    });
   });
 });
