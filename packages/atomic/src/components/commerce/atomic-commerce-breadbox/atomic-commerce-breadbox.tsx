330a439a4d55f1a166f38be2fac8eb32e5d4f45e
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-05-23 10:35:01 -0400

    chore(atomic): migrate breadcrumb-container.tsx
    https://coveord.atlassian.net/browse/KIT-4254

--- packages/atomic/src/components/commerce/atomic-commerce-breadbox/atomic-commerce-breadbox.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-breadbox/atomic-commerce-breadbox.tsx
@@ -28,11 +28,11 @@ import {
 import {FocusTargetController} from '../../../utils/stencil-accessibility-utils';
 import {BreadcrumbButton} from '../../common/breadbox/breadcrumb-button';
 import {BreadcrumbClearAll} from '../../common/breadbox/breadcrumb-clear-all';
-import {BreadcrumbContainer} from '../../common/breadbox/breadcrumb-container';
 import {BreadcrumbContent} from '../../common/breadbox/breadcrumb-content';
 import {BreadcrumbShowLess} from '../../common/breadbox/breadcrumb-show-less';
 import {BreadcrumbShowMore} from '../../common/breadbox/breadcrumb-show-more';
 import {Breadcrumb as BreadboxBreadcrumb} from '../../common/breadbox/breadcrumb-types';
+import {BreadcrumbContainer} from '../../common/breadbox/stencil-breadcrumb-container';
 import {formatHumanReadable} from '../../common/facets/numeric-facet/formatter';
 import {
   defaultCurrencyFormatter,
