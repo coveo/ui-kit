e0bcdd72a492075efafd58cdd714d38bd7d2118f
ylakhdar <ylakhdar@coveo.com>
2025-05-28 15:16:48 -0400

    add component
    https://coveord.atlassian.net/browse/KIT-4258

--- packages/atomic/src/components/commerce/index.ts
+++ packages/atomic/src/components/commerce/index.ts
@@ -4,3 +4,4 @@ export {AtomicCommercePager} from './atomic-commerce-pager/atomic-commerce-pager
 export {AtomicCommerceSearchBoxInstantProducts} from './atomic-commerce-search-box-instant-products/atomic-commerce-search-box-instant-products.js';
 export {AtomicCommerceSearchBoxQuerySuggestions} from './atomic-commerce-search-box-query-suggestions/atomic-commerce-search-box-query-suggestions.js';
 export {AtomicCommerceSortDropdown} from './atomic-commerce-sort-dropdown/atomic-commerce-sort-dropdown.js';
+export {AtomicProductTemplate} from './atomic-product-template/atomic-product-template.js';
--- packages/atomic/src/components/commerce/lazy-index.ts
+++ packages/atomic/src/components/commerce/lazy-index.ts
@@ -16,6 +16,8 @@ export default {
     await import(
       './atomic-commerce-sort-dropdown/atomic-commerce-sort-dropdown.js'
     ),
+  'atomic-product-template': async () =>
+    await import('./atomic-product-template/atomic-product-template.js'),
 } as Record<string, () => Promise<unknown>>;
 
 export type * from './index.js';
