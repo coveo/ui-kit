7516f5a9ec2adcd41f55bdcc173fc632e1e5b9d1
fbeaudoincoveo <fbeaudoin@coveo.com>
2025-05-28 14:03:56 -0400

    Migrate atomic-commerce-product-list
    https://coveord.atlassian.net/browse/KIT-3935

--- packages/atomic/src/components/commerce/index.ts
+++ packages/atomic/src/components/commerce/index.ts
@@ -1,5 +1,6 @@
 // Auto-generated file
 export {AtomicCommerceInterface} from './atomic-commerce-interface/atomic-commerce-interface.js';
 export {AtomicCommercePager} from './atomic-commerce-pager/atomic-commerce-pager.js';
+export {AtomicCommerceProductList} from './atomic-commerce-product-list/atomic-commerce-product-list.js';
 export {AtomicCommerceSearchBoxQuerySuggestions} from './atomic-commerce-search-box-query-suggestions/atomic-commerce-search-box-query-suggestions.js';
 export {AtomicCommerceSortDropdown} from './atomic-commerce-sort-dropdown/atomic-commerce-sort-dropdown.js';
