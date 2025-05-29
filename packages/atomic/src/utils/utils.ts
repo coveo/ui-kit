619b9d3518fdf8e04ef855296bf59626725eec8f
ylakhdar <ylakhdar@coveo.com>
2025-05-29 14:16:04 -0400

    try something
    https://coveord.atlassian.net/browse/KIT-4258

--- packages/atomic/src/utils/utils.ts
+++ packages/atomic/src/utils/utils.ts
@@ -1,50 +1,33 @@
 import {getResourceUrl} from './resource-url';
 
-export function randomID(prepend?: string, length = 5) {
-  const randomStr = Math.random()
-    .toString(36)
-    .substring(2, 2 + length);
-  if (!prepend) {
-    return randomStr;
-  }
-  return prepend + randomStr;
-}
-
-export async function defer() {
-  return new Promise<void>((resolve) => setTimeout(resolve, 10));
-}
-
-/**
- * Returns a function that can be executed only once
- */
-export function once<T extends unknown[]>(fn: (...args: T) => unknown) {
-  let result: unknown;
-  return function (this: unknown, ...args: T) {
-    if (fn) {
-      result = fn.apply(this, args);
-      fn = () => {};
-    }
-    return result;
-  };
-}
-
-export function isElementNode(node: Node): node is Element {
-  return node.nodeType === Node.ELEMENT_NODE;
-}
-
-export function isTextNode(node: Node): node is Text {
-  return node.nodeType === Node.TEXT_NODE;
-}
-
-export function isVisualNode(node: Node) {
-  if (isElementNode(node)) {
-    return !(node instanceof HTMLStyleElement);
-  }
-  if (isTextNode(node)) {
-    return !!node.textContent?.trim();
-  }
-  return false;
-}
+export {
+  once,
+  camelToKebab,
+  kebabToCamel,
+  snakeToCamel,
+  titleToKebab,
+  randomID,
+  getRandomArbitrary,
+  parseXML,
+  parseHTML,
+  isElementNode,
+  isTextNode,
+  isVisualNode,
+  containsVisualElement,
+  elementHasAncestorTag,
+  sanitizeStyle,
+  getFocusedElement,
+  isFocusingOut,
+  isInDocument,
+  isPropValuesEqual,
+  getParent,
+  isAncestorOf,
+  aggregate,
+  spreadProperties,
+  closest,
+  defer,
+  sortByDocumentPosition,
+} from './stencil-utils';
 
 export function getAssetPath(path: string): string {
   const resourceUrl = getResourceUrl();
@@ -75,21 +58,4 @@ export function parseAssetURL(url: string, assetPath = './assets') {
     return getAssetPath(`${assetPath}/${remainder}.svg`);
   }
   return null;
-}
-
-export function aggregate<V, K extends PropertyKey>(
-  values: readonly V[],
-  getKey: (value: V, index: number) => K
-): Record<K, V[] | undefined> {
-  return values.reduce(
-    (aggregatedValues, value, i) => {
-      const key = getKey(value, i);
-      if (!(key in aggregatedValues)) {
-        aggregatedValues[key] = [];
-      }
-      aggregatedValues[key]!.push(value);
-      return aggregatedValues;
-    },
-    <Record<K, V[] | undefined>>{}
-  );
 }
