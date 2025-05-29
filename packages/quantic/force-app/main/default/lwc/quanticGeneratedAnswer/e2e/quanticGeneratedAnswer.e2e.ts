33949a94195c7201c73756e7b687e0bd199a5c4f
mmitiche <86681870+mmitiche@users.noreply.github.com>
2025-05-29 02:56:33 -0400

    test(quantic): introducing ep to playwright (#5297)
    ## [SFINT-6076](https://coveord.atlassian.net/browse/SFINT-6076)
    
    Created the setup logic to start testing Event Protocol in our
    Playwright E2E tests:
    
    - Created the `AnalyticsHelper ` object, this class contains various
    methods to check and setup analytics:
    
    - static method `setCookieToEnableNextAnalytics`: method responsible of
    setting the required cookie to activate Event Protocol analytics
    reporting.
    - method `waitForClickUaAnalytics`: method responsible of waiting for UA
    legacy Click event and checking its payload.
    - method `waitForCustomUaAnalytics`: method responsible of waiting for
    UA legacy Custom event and checking its payload.
    - method `waitForEventProtocolAnalytics` method responsible of waiting
    for Next EP event and checking its payload.
    - method `validateEventWithEventAPI` used internally by the
    `waitForEventProtocolAnalytics` method to confirm that EP events are
    valid using the `/validate` endpoint of the Event API.
    - static method isMatchingPayload used as a utility method to compare
    two objects.
    
    - I updated the Fixture of the Quantic Generated Answer component:
    
    - Each fixture (`testSearch` and `testInsight`) is accepting a new
    parameter `AnalyticsMode` which can be set to either `ua` or `ep`.
    - When the parameter `AnalyticsMode` is set to `ep`, the fixture calls
    the method `setCookieToEnableNextAnalytics` from the `AnalyticsHelper`
    in order to setup the testing for EP properly after setting the cookie.
    
    - I updated the Page Object of the Quantic Generated Answer component:
    
    - The Generated Answer Object constructor is not instantiate the class
    `AnalyticsHelper` in order to use its built in methods to check
    analytics for both ep and ua.
    - The Generated Answer Object constructor also now accepts a new
    parameter, the `AnalyticsMode` which can be set to either `ua` or `ep`.
    - The Generated Answer Object contains methods to check for the
    different analytics sent by the Quantic Generated Answer for both EP and
    UA. example of the structure of the methods:
    ```
      async waitForStreamEndAnalytics(): Promise<Request | boolean> {
        if (this.analyticsMode === 'ua') {
          return this.analytics.waitForCustomUaAnalytics(
            {
              eventType: 'generatedAnswer',
              eventValue: 'generatedAnswerStreamEnd',
            },
            (event) =>
              event?.customData?.generativeQuestionAnsweringId === this.streamId
          );
        }
        return this.analytics.waitForEventProtocolAnalytics(
          'Rga.AnswerReceived',
          (event) => event.answerGenerated === true
        );
      }
     ```
    
    - The generated answer spec now iterates over the object AnalyticsModeTest in order to execute the test for both EP and UA:
    ```
    analyticsModeTest.forEach((analytics) => {
     // The usual tests
    })
    ```
    
    
    
    
    ### Example error when an EP event sent is not valid:
    
    <img width="1050" alt="Screenshot 2025-05-15 at 6 40 27 AM" src="https://github.com/user-attachments/assets/e3fde6ea-7a75-45f2-9f66-2311c2b76f65" />
    
    
    
    [SFINT-6076]: https://coveord.atlassian.net/browse/SFINT-6076?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- packages/quantic/force-app/main/default/lwc/quanticGeneratedAnswer/e2e/quanticGeneratedAnswer.e2e.ts
+++ packages/quantic/force-app/main/default/lwc/quanticGeneratedAnswer/e2e/quanticGeneratedAnswer.e2e.ts
@@ -1,6 +1,7 @@
 import {testSearch, testInsight, expect} from './fixture';
 import {useCaseTestCases} from '../../../../../../playwright/utils/useCase';
 import genQaData from './data';
+import {analyticsModeTest} from '../../../../../../playwright/utils/analyticsMode';
 
 const fixtures = {
   search: testSearch,
@@ -15,184 +16,188 @@ useCaseTestCases.forEach((useCase) => {
       genQaData,
     });
 
-    test.describe('when the answer has been generated', () => {
-      test('should send a stream end analytics event', async ({
-        generatedAnswer,
-      }) => {
-        await generatedAnswer.waitForStreamEndUaAnalytics();
-      });
-    });
-
-    test.describe('when providing positive feedback', () => {
-      test('should send positive feedback analytics containing all details', async ({
-        generatedAnswer,
-      }) => {
-        const likeAnalyticRequestPromise =
-          generatedAnswer.waitForLikeGeneratedAnswerUaAnalytics();
-        await generatedAnswer.clickLikeButton();
-        await likeAnalyticRequestPromise;
-
-        const exampleDocumentUrl = 'https://www.coveo.com/';
-        const exampleDetails = 'example details...';
-        await generatedAnswer.fillFeedbackForm({
-          correctTopic: 'Yes',
-          hallucinationFree: 'Yes',
-          documented: 'Not sure',
-          readable: 'Yes',
+    analyticsModeTest.forEach((analytics) => {
+      test.describe(analytics.label, () => {
+        test.use({
+          analyticsMode: analytics.mode,
         });
-        await generatedAnswer.typeInFeedbackDocumentUrlInput(
-          exampleDocumentUrl
-        );
-        await generatedAnswer.typeInFeedbackDetailsInput(exampleDetails);
 
-        const feedbackAnalyticRequestPromise =
-          generatedAnswer.waitForFeedbackSubmitUaAnalytics({
-            correctTopic: 'yes',
-            hallucinationFree: 'yes',
-            documented: 'unknown',
-            readable: 'yes',
-            documentUrl: exampleDocumentUrl,
-            details: exampleDetails,
-            helpful: true,
+        test.describe('when the answer has been generated', () => {
+          test('should send a stream end analytics event', async ({
+            generatedAnswer,
+          }) => {
+            await generatedAnswer.waitForStreamEndAnalytics();
           });
-        await generatedAnswer.clickSubmitFeedbackButton();
-        await feedbackAnalyticRequestPromise;
-      });
-    });
-
-    test.describe('when providing negative feedback', () => {
-      test('should send negative feedback analytics containing all details', async ({
-        generatedAnswer,
-      }) => {
-        const dislikeAnalyticRequestPromise =
-          generatedAnswer.waitForDislikeGeneratedAnswerUaAnalytics();
-        await generatedAnswer.clickDislikeButton();
-        await dislikeAnalyticRequestPromise;
-
-        const exampleDocumentUrl = 'https://www.coveo.com/';
-        const exampleDetails = 'example details...';
-        await generatedAnswer.fillFeedbackForm({
-          correctTopic: 'No',
-          hallucinationFree: 'Not sure',
-          documented: 'No',
-          readable: 'No',
         });
-        await generatedAnswer.typeInFeedbackDocumentUrlInput(
-          exampleDocumentUrl
-        );
-        await generatedAnswer.typeInFeedbackDetailsInput(exampleDetails);
 
-        const feedbackAnalyticRequestPromise =
-          generatedAnswer.waitForFeedbackSubmitUaAnalytics({
-            correctTopic: 'no',
-            hallucinationFree: 'unknown',
-            documented: 'no',
-            readable: 'no',
-            documentUrl: exampleDocumentUrl,
-            details: exampleDetails,
-            helpful: false,
-          });
-        await generatedAnswer.clickSubmitFeedbackButton();
-        await feedbackAnalyticRequestPromise;
-      });
-    });
+        test.describe('when providing positive feedback', () => {
+          test('should send positive feedback analytics containing all details', async ({
+            generatedAnswer,
+          }) => {
+            const likeAnalyticRequestPromise =
+              generatedAnswer.waitForLikeGeneratedAnswerAnalytics();
+            await generatedAnswer.clickLikeButton();
+            await likeAnalyticRequestPromise;
 
-    test.describe('when copying the generated answer to clipboard', () => {
-      test('should send a copy to clipboard analytics event', async ({
-        generatedAnswer,
-      }) => {
-        const analyticRequestPromise =
-          generatedAnswer.waitForCopyToClipboardUaAnalytics();
-        await generatedAnswer.clickCopyToClipboardButton();
-        await analyticRequestPromise;
-      });
-    });
-
-    test.describe('when the property withToggle is set to true', () => {
-      test.use({
-        options: {
-          withToggle: true,
-        },
-      });
-
-      test('should allow toggling the generated OFF and ON and log analytics', async ({
-        generatedAnswer,
-      }) => {
-        const hideAnswerAnalyticRequestPromise =
-          generatedAnswer.waitForHideAnswersUaAnalytics();
-        await generatedAnswer.clickToggleButton();
-        await hideAnswerAnalyticRequestPromise;
-
-        const showAnswerAnalyticRequestPromise =
-          generatedAnswer.waitForShowAnswersUaAnalytics();
-        await generatedAnswer.clickToggleButton();
-        await showAnswerAnalyticRequestPromise;
-      });
-    });
-
-    test.describe('when interacting with citations', () => {
-      test.describe('when hovering over a citation', () => {
-        test('should log citation hover analytics', async ({
-          generatedAnswer,
-        }) => {
-          const citationIndex = 0;
-          const {id, permanentid} = genQaData.citations[citationIndex];
-          const citationHoverAnalyticRequestPromise =
-            generatedAnswer.waitForSourceHoverUaAnalytics({
-              citationId: id,
-              permanentId: permanentid,
+            const exampleDocumentUrl = 'https://www.coveo.com/';
+            const exampleDetails = 'example details...';
+            await generatedAnswer.fillFeedbackForm({
+              correctTopic: 'Yes',
+              hallucinationFree: 'Yes',
+              documented: 'Not sure',
+              readable: 'Yes',
             });
-          await generatedAnswer.hoverOverCitation(citationIndex);
-          await citationHoverAnalyticRequestPromise;
-        });
-      });
-
-      test.describe('when clicking on a citation', () => {
-        test('should log citation click analytics', async ({
-          generatedAnswer,
-        }) => {
-          const citationIndex = 0;
-          const {id, title, source, uri, clickUri, permanentid} =
-            genQaData.citations[citationIndex];
-          const citationClickAnalyticRequestPromise =
-            generatedAnswer.waitForCitationClickUaAnalytics(
-              {
-                documentTitle: title,
-                sourceName: source,
-                documentPosition: citationIndex + 1,
-                documentUri: uri,
-                documentUrl: clickUri,
-              },
-              {
-                citationId: id,
-                contentIDKey: 'permanentid',
-                contentIDValue: permanentid,
-              }
+            await generatedAnswer.typeInFeedbackDocumentUrlInput(
+              exampleDocumentUrl
             );
-          await generatedAnswer.clickOnCitation(citationIndex);
-          await citationClickAnalyticRequestPromise;
+            await generatedAnswer.typeInFeedbackDetailsInput(exampleDetails);
+
+            const feedbackAnalyticRequestPromise =
+              generatedAnswer.waitForFeedbackSubmitAnalytics({
+                correctTopic: 'yes',
+                hallucinationFree: 'yes',
+                documented: 'unknown',
+                readable: 'yes',
+                documentUrl: exampleDocumentUrl,
+                details: exampleDetails,
+                helpful: true,
+              });
+            await generatedAnswer.clickSubmitFeedbackButton();
+            await feedbackAnalyticRequestPromise;
+          });
         });
-      });
-    });
 
-    test.describe('when the answer is generated in a single shot and the answer exceeds the maximum height', () => {
-      test.use({
-        options: {
-          collapsible: true,
-        },
-      });
-      test('should display the answer as collapsed', async ({
-        generatedAnswer,
-      }) => {
-        const expectedShowMoreLabel = 'Show more';
-        await generatedAnswer.waitForStreamEndUaAnalytics();
+        test.describe('when providing negative feedback', () => {
+          test('should send negative feedback analytics containing all details', async ({
+            generatedAnswer,
+          }) => {
+            const dislikeAnalyticRequestPromise =
+              generatedAnswer.waitForDislikeGeneratedAnswerAnalytics();
+            await generatedAnswer.clickDislikeButton();
+            await dislikeAnalyticRequestPromise;
 
-        const showMoreButton = generatedAnswer.showMoreButton;
-        expect(showMoreButton).not.toBeNull();
+            const exampleDocumentUrl = 'https://www.coveo.com/';
+            const exampleDetails = 'example details...';
+            await generatedAnswer.fillFeedbackForm({
+              correctTopic: 'No',
+              hallucinationFree: 'Not sure',
+              documented: 'No',
+              readable: 'No',
+            });
+            await generatedAnswer.typeInFeedbackDocumentUrlInput(
+              exampleDocumentUrl
+            );
+            await generatedAnswer.typeInFeedbackDetailsInput(exampleDetails);
 
-        const showMoreButtonLabel = await showMoreButton.textContent();
-        expect(showMoreButtonLabel).not.toBeNull();
-        expect(showMoreButtonLabel).toEqual(expectedShowMoreLabel);
+            const feedbackAnalyticRequestPromise =
+              generatedAnswer.waitForFeedbackSubmitAnalytics({
+                correctTopic: 'no',
+                hallucinationFree: 'unknown',
+                documented: 'no',
+                readable: 'no',
+                documentUrl: exampleDocumentUrl,
+                details: exampleDetails,
+                helpful: false,
+              });
+            await generatedAnswer.clickSubmitFeedbackButton();
+            await feedbackAnalyticRequestPromise;
+          });
+        });
+
+        test.describe('when copying the generated answer to clipboard', () => {
+          test('should send a copy to clipboard analytics event', async ({
+            generatedAnswer,
+          }) => {
+            const analyticRequestPromise =
+              generatedAnswer.waitForCopyToClipboardAnalytics();
+            await generatedAnswer.clickCopyToClipboardButton();
+            await analyticRequestPromise;
+          });
+        });
+
+        test.describe('when the property withToggle is set to true', () => {
+          test.use({
+            options: {
+              withToggle: true,
+            },
+          });
+
+          test('should allow toggling the generated OFF and ON and log analytics', async ({
+            generatedAnswer,
+          }) => {
+            const hideAnswerAnalyticRequestPromise =
+              generatedAnswer.waitForHideAnswersAnalytics();
+            await generatedAnswer.clickToggleButton();
+            await hideAnswerAnalyticRequestPromise;
+
+            const showAnswerAnalyticRequestPromise =
+              generatedAnswer.waitForShowAnswersAnalytics();
+            await generatedAnswer.clickToggleButton();
+            await showAnswerAnalyticRequestPromise;
+          });
+        });
+
+        test.describe('when interacting with citations', () => {
+          test.describe('when hovering over a citation', () => {
+            test('should log citation hover analytics', async ({
+              generatedAnswer,
+            }) => {
+              const citationIndex = 0;
+              const {id, permanentid} = genQaData.citations[citationIndex];
+              const citationHoverAnalyticRequestPromise =
+                generatedAnswer.waitForSourceHoverAnalytics({
+                  citationId: id,
+                  permanentId: permanentid,
+                });
+              await generatedAnswer.hoverOverCitation(citationIndex);
+              await citationHoverAnalyticRequestPromise;
+            });
+          });
+
+          test.describe('when clicking on a citation', () => {
+            test('should log citation click analytics', async ({
+              generatedAnswer,
+            }) => {
+              const citationIndex = 0;
+              const {id, title, source, uri, clickUri, permanentid} =
+                genQaData.citations[citationIndex];
+              const citationClickAnalyticRequestPromise =
+                generatedAnswer.waitForCitationClickAnalytics({
+                  documentTitle: title,
+                  sourceName: source,
+                  documentPosition: citationIndex + 1,
+                  documentUri: uri,
+                  documentUrl: clickUri,
+                  citationId: id,
+                  contentIDKey: 'permanentid',
+                  contentIDValue: permanentid,
+                });
+              await generatedAnswer.clickOnCitation(citationIndex);
+              await citationClickAnalyticRequestPromise;
+            });
+          });
+        });
+
+        test.describe('when the answer is generated in a single shot and the answer exceeds the maximum height', () => {
+          test.use({
+            options: {
+              collapsible: true,
+            },
+          });
+          test('should display the answer as collapsed', async ({
+            generatedAnswer,
+          }) => {
+            const expectedShowMoreLabel = 'Show more';
+            await generatedAnswer.waitForStreamEndAnalytics();
+
+            const showMoreButton = generatedAnswer.showMoreButton;
+            expect(showMoreButton).not.toBeNull();
+
+            const showMoreButtonLabel = await showMoreButton.textContent();
+            expect(showMoreButtonLabel).not.toBeNull();
+            expect(showMoreButtonLabel).toEqual(expectedShowMoreLabel);
+          });
+        });
       });
     });
   });
