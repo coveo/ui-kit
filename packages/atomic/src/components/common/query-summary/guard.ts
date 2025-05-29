4fd236c4d97c8f547ba654ac59f7f1496eb9b956
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-05-29 16:53:34 -0400

    chore(atomic): migrate query-summary/guard.tsx
    https://coveord.atlassian.net/browse/KIT-4264

---
+++ packages/atomic/src/components/common/query-summary/guard.ts
@@ -0,0 +1,28 @@
+import {FunctionalComponentWithChildren} from '@/src/utils/functional-component-utils';
+import {html, nothing} from 'lit';
+
+interface Props {
+  hasResults: boolean;
+  hasError: boolean;
+  firstSearchExecuted: boolean;
+}
+
+export const renderQuerySummaryGuard: FunctionalComponentWithChildren<Props> =
+  ({props}) =>
+  (children) => {
+    if (props.hasError || (!props.hasResults && props.firstSearchExecuted)) {
+      return nothing;
+    }
+
+    if (!props.firstSearchExecuted) {
+      return html`
+        <div
+          part="placeholder"
+          aria-hidden="true"
+          class="bg-neutral my-2 h-6 w-60 animate-pulse rounded"
+        ></div>
+      `;
+    }
+
+    return html`${children}`;
+  };
