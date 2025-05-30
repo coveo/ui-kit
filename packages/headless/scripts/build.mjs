60ee346306a17da6d2fdd2fc46ff6fa533c5bfc7
Louis Bompart <lbompart@coveo.com>
2025-05-30 09:54:17 -0400

    use ts transformer

---
+++ packages/headless/scripts/build.mjs
@@ -0,0 +1,125 @@
+import chalk from 'chalk';
+import {dirname, basename, relative, join} from 'path';
+import {argv} from 'process';
+import {
+  readConfigFile,
+  getLineAndCharacterOfPosition,
+  sys,
+  parseJsonConfigFileContent,
+  getPreEmitDiagnostics,
+  createProgram,
+  flattenDiagnosticMessageText,
+  DiagnosticCategory,
+} from 'typescript';
+import {fileURLToPath} from 'url';
+import versionTransformer from './version-transform.mjs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const args = argv.slice(2);
+const configArg = args.find((arg) => arg.startsWith('--config='));
+if (configArg === undefined) {
+  throw new Error('Missing --config=[PATH] argument');
+}
+const tsConfigPath = configArg.split('=')[1];
+const isCDN = process.env.DEPLOYMENT_ENVIRONMENT === 'CDN';
+const transformers = [versionTransformer];
+
+function loadTsConfig(configPath) {
+  const configFile = readConfigFile(configPath, sys.readFile);
+  if (configFile.error) {
+    throw new Error(
+      `Error loading tsconfig file: ${configFile.error.messageText}`
+    );
+  }
+  return parseJsonConfigFileContent(
+    configFile.config,
+    sys,
+    dirname(configPath)
+  );
+}
+
+function emit(program) {
+  const targetSourceFile = undefined;
+  const cancellationToken = undefined;
+  const writeFile = undefined;
+  const emitOnlyDtsFiles = false;
+  const customTransformers = {
+    before: transformers,
+  };
+
+  return program.emit(
+    targetSourceFile,
+    cancellationToken,
+    writeFile,
+    emitOnlyDtsFiles,
+    customTransformers
+  );
+}
+
+/**
+ * Compiles TypeScript files using a custom transformer.
+ *
+ * This function mimics the behavior of running `tsc -p tsconfig.json` but applies a custom SVG transformer
+ * to all TypeScript files. It loads the TypeScript configuration from the specified `tsconfig.json` file,
+ * creates a TypeScript program, and emits the compiled JavaScript files with the custom transformer applied.
+ *
+ * Info: https://github.com/microsoft/TypeScript/wiki/Using-the-Compiler-API#a-minimal-compiler
+ */
+function compileWithTransformer() {
+  console.log(
+    chalk.blue('Using tsconfig:'),
+    chalk.green(basename(tsConfigPath))
+  );
+  const {options, fileNames} = loadTsConfig(tsConfigPath);
+  const program = createProgram(fileNames, options);
+  const emitResult = emit(program);
+
+  const allDiagnostics = getPreEmitDiagnostics(program).concat(
+    emitResult.diagnostics
+  );
+
+  let hasError = false;
+
+  allDiagnostics.forEach((diagnostic) => {
+    if (diagnostic.file) {
+      const {line, character} = getLineAndCharacterOfPosition(
+        diagnostic.file,
+        diagnostic.start
+      );
+      const message = flattenDiagnosticMessageText(
+        diagnostic.messageText,
+        '\n'
+      );
+
+      console.log(
+        `${chalk.cyan(relative(process.cwd(), diagnostic.file.fileName))}:${chalk.yellow(line + 1)}:${chalk.yellow(character + 1)} - ${chalk.red('error')} ${chalk.gray(message)}`
+      );
+    } else {
+      console.error(
+        chalk.red(flattenDiagnosticMessageText(diagnostic.messageText, '\n'))
+      );
+    }
+
+    if (diagnostic.category === DiagnosticCategory.Error) {
+      hasError = true;
+    }
+  });
+
+  let exitCode = emitResult.emitSkipped || hasError ? 1 : 0;
+  console.log(`Process exiting with code '${exitCode}'.`);
+  process.exit(exitCode);
+}
+
+try {
+  const {options} = loadTsConfig(tsConfigPath);
+
+  const srcDir = join(__dirname, '../src');
+  const outDir = options.outDir;
+
+  console.log(chalk.blue('Starting TypeScript compilation'));
+  compileWithTransformer();
+} catch (error) {
+  console.error(chalk.red('Build failed:'), error);
+  process.exit(1);
+}
