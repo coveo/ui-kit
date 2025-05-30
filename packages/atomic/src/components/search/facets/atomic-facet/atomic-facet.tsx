e56811875740585bbb2ddfdfb31c87ea17b8fcc8
ylakhdar <ylakhdar@coveo.com>
2025-05-30 11:45:27 -0400

    chore: add facet-search-value (#5329)
    https://coveord.atlassian.net/browse/KIT-4219

--- packages/atomic/src/components/search/facets/atomic-facet/atomic-facet.tsx
+++ packages/atomic/src/components/search/facets/atomic-facet/atomic-facet.tsx
@@ -49,8 +49,8 @@ import {
   shouldDisplaySearchResults,
   shouldUpdateFacetSearchComponent,
 } from '../../../common/facets/facet-search/facet-search-utils';
-import {FacetSearchValue} from '../../../common/facets/facet-search/facet-search-value';
 import {FacetSearchMatches} from '../../../common/facets/facet-search/stencil-facet-search-matches';
+import {FacetSearchValue} from '../../../common/facets/facet-search/stencil-facet-search-value';
 import {FacetShowMoreLess} from '../../../common/facets/facet-show-more-less/stencil-facet-show-more-less';
 import {
   FacetValueProps,
