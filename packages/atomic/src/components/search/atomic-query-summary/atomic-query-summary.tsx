0fa701e211daf170cd40e2d0e51d3b283d0b554d
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-05-30 09:42:17 -0400

    chore(atomic): migrate query-summary/utils.tsx
    https://coveord.atlassian.net/browse/KIT-4265

--- packages/atomic/src/components/search/atomic-query-summary/atomic-query-summary.tsx
+++ packages/atomic/src/components/search/atomic-query-summary/atomic-query-summary.tsx
@@ -13,7 +13,7 @@ import {LocalizedString} from '../../../utils/jsx-utils';
 import {AriaLiveRegion} from '../../../utils/stencil-accessibility-utils';
 import {QuerySummaryContainer} from '../../common/query-summary/container';
 import {QuerySummaryGuard} from '../../common/query-summary/guard';
-import {getQuerySummaryI18nParameters} from '../../common/query-summary/utils';
+import {getQuerySummaryI18nParameters} from '../../common/query-summary/stencil-utils';
 import {Bindings} from '../atomic-search-interface/atomic-search-interface';
 
 /**
