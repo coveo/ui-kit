876d9ce43e7ef92d0f66891dfdac204e7381b627
Louis Bompart <lbompart@coveo.com>
2025-06-03 10:59:11 -0400

    import type coveo.analytics/dist

--- packages/headless/src/api/analytics/search-analytics.ts
+++ packages/headless/src/api/analytics/search-analytics.ts
@@ -3,7 +3,7 @@ import {
   SearchPageClientProvider,
   AnalyticsClientSendEventHook,
 } from 'coveo.analytics';
-import {SearchEventRequest} from 'coveo.analytics/dist/definitions/events.js';
+import type {SearchEventRequest} from 'coveo.analytics/dist/definitions/events.js';
 import {Logger} from 'pino';
 import {
   buildFacetStateMetadata,
