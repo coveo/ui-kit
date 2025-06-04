fb70c2f56435ba2966e95feba2e0d56b7ce2c121
Felix Perron-Brault <fpbrault@coveo.com>
2025-06-04 14:40:01 -0400

    fix script not finding some components
    https://coveord.atlassian.net/browse/KIT-4290

--- packages/atomic/scripts/generate-lit-exports.mjs
+++ packages/atomic/scripts/generate-lit-exports.mjs
@@ -24,54 +24,69 @@ function toPascalCase(name) {
     .join('');
 }
 
+function findLitComponentsRecursively(dir, basePath = '') {
+  const components = [];
+  const files = fs.readdirSync(dir, {withFileTypes: true});
+
+  for (const file of files) {
+    if (file.isDirectory()) {
+      const fullPath = path.join(dir, file.name);
+      const componentPath = path.join(fullPath, `${file.name}.ts`);
+      const relativePath = basePath ? `${basePath}/${file.name}` : file.name;
+
+      if (fs.existsSync(componentPath) && isLitComponent(componentPath)) {
+        components.push({
+          name: file.name,
+          path: relativePath,
+        });
+      } else {
+        const nestedComponents = findLitComponentsRecursively(
+          fullPath,
+          relativePath
+        );
+        components.push(...nestedComponents);
+      }
+    }
+  }
+
+  return components;
+}
+
 async function generateLitExportsForDir(dir) {
   const componentsDir = path.join(baseComponentsDir, dir);
   const outputIndexFile = path.join(componentsDir, 'index.ts');
   const outputLazyIndexFile = path.join(componentsDir, 'lazy-index.ts');
 
-  const files = fs.readdirSync(componentsDir, {withFileTypes: true});
-  const litComponents = files
-    .filter((file) => {
-      const componentPath = path.join(
-        componentsDir,
-        file.name,
-        `${file.name}.ts`
-      );
-      return (
-        file.isDirectory() &&
-        fs.existsSync(componentPath) &&
-        isLitComponent(componentPath)
-      );
-    })
-    .map((file) => file.name)
-    .sort();
+  const litComponents = findLitComponentsRecursively(componentsDir).sort(
+    (a, b) => a.name.localeCompare(b.name)
+  );
 
   let indexFileContent = `
-    // Auto-generated file
-    ${
-      litComponents.length > 0
-        ? litComponents
-            .map(
-              (component) =>
-                `export {${toPascalCase(component)}} from './${component}/${component}.js';`
-            )
-            .join('\n')
-        : 'export {};'
-    }
-  `;
+// Auto-generated file
+${
+  litComponents.length > 0
+    ? litComponents
+        .map(
+          (component) =>
+            `export {${toPascalCase(component.name)}} from './${component.path}/${component.name}.js';`
+        )
+        .join('\n')
+    : 'export {};'
+}
+`;
   let lazyIndexFileContent = `
-    // Auto-generated file
-    export default {
-       ${litComponents
-         .map(
-           (component) =>
-             `'${component}': async () => await import('./${component}/${component}.js'),`
-         )
-         .join('\n  ')}
-    } as Record<string, () => Promise<unknown>>;
- 
-    export type * from './index.js';
-  `;
+// Auto-generated file
+export default {
+  ${litComponents
+    .map(
+      (component) =>
+        `'${component.name}': async () => await import('./${component.path}/${component.name}.js'),`
+    )
+    .join('\n  ')}
+} as Record<string, () => Promise<unknown>>;
+
+export type * from './index.js';
+`;
 
   indexFileContent = await formatWithPrettier(
     indexFileContent,
