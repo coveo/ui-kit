ed32d60a7c251924e5ebf7edff181512b325e8df
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-06-04 09:13:21 -0400

    ci: remove release branch update from generated files script
    https://coveord.atlassian.net/browse/KIT-4303

--- utils/ci/commit-all.mjs
+++ utils/ci/commit-all.mjs
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
 import {Octokit} from 'octokit';
-import {
-  REPO_NAME,
-  REPO_OWNER,
-  REPO_RELEASE_BRANCH,
-} from './common/constants.mjs';
 import {commitChanges, setupGit} from './common/git.mjs';
-import {removeWriteAccessRestrictions} from './lock-master.mjs';
 
 if (!process.env.INIT_CWD) {
   throw new Error('Should be called using npm run-script');
@@ -25,17 +19,6 @@ process.chdir(process.env.INIT_CWD);
   // Compile git commit message
   const commitMessage = 'Add generated files';
 
-  // Craft the commit (complex process, see function)
-  const commit = await commitChanges(commitMessage, octokit);
-
-  // Current release branch
-  await octokit.rest.git.updateRef({
-    owner: REPO_OWNER,
-    repo: REPO_NAME,
-    ref: `heads/${REPO_RELEASE_BRANCH}`,
-    sha: commit,
-    force: false,
-  });
-  // Unlock the main branch
-  await removeWriteAccessRestrictions();
+  // Craft the commit & updates the HEAD of the current branch
+  await commitChanges(commitMessage, octokit);
 })();
