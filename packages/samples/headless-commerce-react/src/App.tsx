905e7799e01b87d1464267ee79c99726ae90aea1
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2025-06-04 13:59:01 -0400

    repro

--- packages/samples/headless-commerce-react/src/App.tsx
+++ packages/samples/headless-commerce-react/src/App.tsx
@@ -4,5 +4,8 @@ import Router from './router/router.js';
 export default function App() {
   const engine = getEngine();
 
+  console.log(engine.relay.getMeta('').clientId);
+  console.log(engine.navigatorContext.clientId);
+
   return <Router engine={engine} />;
 }
--- packages/samples/headless-commerce-react/src/context/engine.ts
+++ packages/samples/headless-commerce-react/src/context/engine.ts
@@ -1,4 +1,5 @@
 import {buildCommerceEngine, CommerceEngine} from '@coveo/headless/commerce';
+import {buildBrowserEnvironment} from '@coveo/relay';
 import {loadCartItemsFromLocalStorage} from '../utils/cart-utils.js';
 
 export const getEngine = () => {
@@ -9,6 +10,12 @@ export const getEngine = () => {
   const organizationId = 'searchuisamples';
 
   _engine = buildCommerceEngine({
+    navigatorContextProvider: () => ({
+      referrer: document.referrer,
+      userAgent: navigator.userAgent,
+      location: window.location.href,
+      clientId: '59ca3c2b-a04d-56d9-b2f5-1a2bc90d84df',
+    }),
     configuration: {
       accessToken: 'xx564559b1-0045-48e1-953c-3addd1ee4457',
       organizationId,
@@ -26,6 +33,15 @@ export const getEngine = () => {
       cart: {
         items: loadCartItemsFromLocalStorage() ?? [],
       },
+    },
+  });
+
+  const {storage, generateUUID, ...rest} = buildBrowserEnvironment();
+  console.log(storage, generateUUID, rest);
+  _engine.relay.updateConfig({
+    environment: {
+      ...rest,
+      generateUUID: () => '59ca3c2b-a04d-56d9-b2f5-1a2bc90d84df',
     },
   });
 
