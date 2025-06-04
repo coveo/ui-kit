bdec7950c9d6074583983296c02562311f02a698
Felix Perron-Brault <fpbrault@coveo.com>
2025-06-04 17:11:00 -0400

    migrate item-text utils
    https://coveord.atlassian.net/browse/KIT-4311

--- packages/atomic/src/components/commerce/product-template-components/atomic-product-text/atomic-product-text.tsx
+++ packages/atomic/src/components/commerce/product-template-components/atomic-product-text/atomic-product-text.tsx
@@ -9,8 +9,8 @@ import {
   InitializableComponent,
   InitializeBindings,
 } from '../../../../utils/initialization-utils';
-import {ItemTextFallback} from '../../../common/item-text/item-text-fallback';
-import {ItemTextHighlighted} from '../../../common/item-text/item-text-highlighted';
+import {ItemTextFallback} from '../../../common/item-text/stencil-item-text-fallback';
+import {ItemTextHighlighted} from '../../../common/item-text/stencil-item-text-highlighted';
 import {CommerceBindings} from '../../atomic-commerce-interface/atomic-commerce-interface';
 import {ProductContext} from '../product-template-decorators';
 import {getStringValueFromProductOrNull} from '../product-utils';
