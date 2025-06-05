a12eaf6d8f94d21ebcad3ecf5bb2ccb05a43476a
ylakhdar <ylakhdar@coveo.com>
2025-06-05 11:58:22 -0400

    remove nothing
    https://coveord.atlassian.net/browse/KIT-4059

--- packages/atomic/src/components/common/facets/facet-search/facet-search-input.ts
+++ packages/atomic/src/components/common/facets/facet-search/facet-search-input.ts
@@ -1,6 +1,6 @@
 import {FunctionalComponent} from '@/src/utils/functional-component-utils';
 import {i18n} from 'i18next';
-import {html, nothing} from 'lit';
+import {html} from 'lit';
 import {createRef, ref} from 'lit/directives/ref.js';
 import {when} from 'lit/directives/when.js';
 import CloseIcon from '../../../../images/close.svg';
@@ -45,27 +45,24 @@ export const renderFacetSearchInput: FunctionalComponent<
         >
           <atomic-icon part="search-icon" .icon=${SearchIcon}></atomic-icon>
         </div>
-        ${when(
-          props.query !== '',
-          () =>
-            renderButton({
-              props: {
-                style: 'text-transparent',
-                title: clear,
-                class:
-                  'search-clear-button absolute top-px right-px bottom-px inline-flex w-9 items-center justify-center',
-                onClick: () => {
-                  props.onClear();
-                  inputRef.value?.focus();
-                },
+        ${when(props.query !== '', () =>
+          renderButton({
+            props: {
+              style: 'text-transparent',
+              title: clear,
+              class:
+                'search-clear-button absolute top-px right-px bottom-px inline-flex w-9 items-center justify-center',
+              onClick: () => {
+                props.onClear();
+                inputRef.value?.focus();
               },
-            })(
-              html`<atomic-icon
-                part="search-clear-button"
-                .icon=${CloseIcon}
-              ></atomic-icon>`
-            ),
-          () => nothing
+            },
+          })(
+            html`<atomic-icon
+              part="search-clear-button"
+              .icon=${CloseIcon}
+            ></atomic-icon>`
+          )
         )}
       </div>
     </div>
