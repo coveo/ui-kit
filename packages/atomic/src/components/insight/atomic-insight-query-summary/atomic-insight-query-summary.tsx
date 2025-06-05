843c24c27c1bfac197b4cfc9097f53e62ec417a9
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-06-05 07:25:30 -0400

    chore(atomic): migrate query-summary/utils.tsx (#5388)
    https://coveord.atlassian.net/browse/KIT-4265

--- packages/atomic/src/components/insight/atomic-insight-query-summary/atomic-insight-query-summary.tsx
+++ packages/atomic/src/components/insight/atomic-insight-query-summary/atomic-insight-query-summary.tsx
@@ -13,7 +13,7 @@ import {LocalizedString} from '../../../utils/jsx-utils';
 import {AriaLiveRegion} from '../../../utils/stencil-accessibility-utils';
 import {QuerySummaryContainer} from '../../common/query-summary/stencil-container';
 import {QuerySummaryGuard} from '../../common/query-summary/stencil-guard';
-import {getQuerySummaryI18nParameters} from '../../common/query-summary/utils';
+import {getQuerySummaryI18nParameters} from '../../common/query-summary/stencil-utils';
 import {InsightBindings} from '../atomic-insight-interface/atomic-insight-interface';
 
 /**
