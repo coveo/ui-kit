2a5cbde8f2bf536f60e0f4b031cbfd4ea0ed9bf1
mmitiche <mmitiche@coveo.com>
2025-06-08 12:01:12 +0100

    attach to case feature added to the example insight panel

--- packages/quantic/force-app/solutionExamples/main/lwc/exampleInsightPanel/exampleInsightPanel.js
+++ packages/quantic/force-app/solutionExamples/main/lwc/exampleInsightPanel/exampleInsightPanel.js
@@ -1,24 +1,54 @@
 import {
   getHeadlessBundle,
-  getHeadlessEnginePromise,
+  registerComponentForInit,
+  initializeWithHeadless,
 } from 'c/quanticHeadlessLoader';
 import {LightningElement, api} from 'lwc';
 // @ts-ignore
 import defaultTemplate from './resultTemplates/defaultResultTemplate.html';
 // @ts-ignore
 import youtubeTemplate from './resultTemplates/youtubeResultTemplate.html';
+// @ts-ignore
+import {buildAttachedResultsPayloadHeadless} from 'c/attachToCaseUtils';
+// @ts-ignore
+import {getAllAttachedResults} from 'c/attachToCaseServiceProd';
 
 export default class ExampleInsightPanel extends LightningElement {
   /** @type {string} */
   @api engineId = 'example-insight-panel';
   /** @type {string} */
   @api insightId = '142be676-703c-445f-b2d3-fcc7c0a3ded8';
+  /** @type {string} */
+  @api caseId;
+
+  /** @type {boolean} */
+  isInitAttachedResults = false;
+  loadAttachedResults() {
+    getAllAttachedResults(this.caseId)
+      .then((data) => {
+        const dataParsed = JSON.parse(data);
+        if (
+          dataParsed?.succeeded &&
+          Array.isArray(dataParsed.attachedResults)
+        ) {
+          this._attachedResults = dataParsed.attachedResults;
+          this.initAttachedResults();
+        } else {
+          console.warn(dataParsed?.message);
+        }
+      })
+      .catch((error) => {
+        console.warn(error?.body?.message);
+      });
+  }
 
   connectedCallback() {
     this.template.addEventListener(
       'quantic__insightinterfaceinitialized',
       this.handleInterfaceLoad
     );
+    this.loadAttachedResults();
+    registerComponentForInit(this, this.engineId);
   }
 
   disconnectedCallback() {
@@ -28,11 +58,47 @@ export default class ExampleInsightPanel extends LightningElement {
     );
   }
 
+  renderedCallback() {
+    initializeWithHeadless(this, this.engineId, this.initialize);
+  }
+
+  initialize = (engine) => {
+    this.headless = getHeadlessBundle(this.engineId);
+    this.engine = engine;
+
+    this.insightInterface = this.headless.buildInsightInterface(engine);
+    this.searchStatus = this.headless.buildSearchStatus(engine);
+
+    this.actions = {
+      ...this.headless.loadCaseContextActions(engine),
+      ...this.headless.loadAttachedResultsActions(engine),
+    };
+
+    this.engine.dispatch(this.actions.setCaseId(this.caseId));
+    this.initAttachedResults();
+  };
+
+  initAttachedResults = () => {
+    if (!this.isInitAttachedResults) {
+      if (this.engine && Array.isArray(this._attachedResults)) {
+        this.isInitAttachedResults = true;
+        if (this.actions.setAttachedResults) {
+          this.engine.dispatch(
+            this.actions.setAttachedResults({
+              results: buildAttachedResultsPayloadHeadless(
+                this._attachedResults
+              ),
+              loading: false,
+            })
+          );
+        }
+      }
+    }
+  };
+
   handleInterfaceLoad = (event) => {
     event.stopPropagation();
-    getHeadlessEnginePromise(this.engineId).then((engine) => {
-      engine.executeFirstSearch();
-    });
+    this.engine.executeFirstSearch();
   };
 
   handleResultTemplateRegistration(event) {
