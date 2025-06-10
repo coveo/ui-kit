102b3d6d1b42c0699596f7c75a86ae5b4b81e279
ylakhdar <ylakhdar@coveo.com>
2025-06-10 17:52:34 -0400

    add commerce layout mixin
    https://coveord.atlassian.net/browse/KIT-4283

--- packages/atomic/src/components/commerce/atomic-commerce-layout/atomic-commerce-layout.ts
+++ packages/atomic/src/components/commerce/atomic-commerce-layout/atomic-commerce-layout.ts
@@ -1,48 +1,27 @@
-import {bindings} from '@/src/decorators/bindings';
 import {errorGuard} from '@/src/decorators/error-guard';
-import {InitializableComponent} from '@/src/decorators/types';
-import {withTailwindStyles} from '@/src/decorators/with-tailwind-styles';
-import {LitElement, html, CSSResultGroup, unsafeCSS} from 'lit';
+import {CommerceLayoutMixin} from '@/src/mixins/commerce-layout-mixin';
+import {LitElement, html, unsafeCSS} from 'lit';
 import {customElement, property, state} from 'lit/decorators.js';
 import {DEFAULT_MOBILE_BREAKPOINT} from '../../../utils/replace-breakpoint';
-import {randomID} from '../../../utils/stencil-utils';
-import type {CommerceBindings} from '../atomic-commerce-interface/atomic-commerce-interface';
 import styles from './atomic-commerce-layout.tw.css';
-import {buildCommerceLayout} from './commerce-layout';
 
 /**
  * @alpha
  * The `atomic-commerce-layout` helps organize elements in the commerce page.
  */
 @customElement('atomic-commerce-layout')
-@bindings()
-@withTailwindStyles
-export class AtomicCommerceLayout
-  extends LitElement
-  implements InitializableComponent<CommerceBindings>
-{
-  @state() bindings!: CommerceBindings;
-
+export class AtomicCommerceLayout extends CommerceLayoutMixin(
+  LitElement,
+  unsafeCSS(styles)
+) {
   @state() error!: Error;
-
-  static styles: CSSResultGroup = [unsafeCSS(styles)];
 
   /**
    * CSS value that defines where the layout goes from mobile to desktop.
    * e.g., 800px, 65rem.
    */
-  @property({type: String, reflect: true})
+  @property({type: String, reflect: true, attribute: 'mobile-breakpoint'})
   mobileBreakpoint: string = DEFAULT_MOBILE_BREAKPOINT;
-
-  connectedCallback() {
-    super.connectedCallback();
-    if (!this.id) {
-      this.id = randomID('atomic-commerce-layout-');
-    }
-    const styleSheet = new CSSStyleSheet();
-    styleSheet.replaceSync(buildCommerceLayout(this, this.mobileBreakpoint));
-    this.bindings?.addAdoptedStyleSheets(styleSheet);
-  }
 
   createRenderRoot() {
     return this;
