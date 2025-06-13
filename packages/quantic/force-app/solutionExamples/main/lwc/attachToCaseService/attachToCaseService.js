702d2edd03d8552ee9053382f5ba2d696cc64947
mmitiche <mmitiche@coveo.com>
2025-06-13 21:30:16 +0100

    added real implementation of attach to case service as a comment

--- packages/quantic/force-app/solutionExamples/main/lwc/attachToCaseService/attachToCaseService.js
+++ packages/quantic/force-app/solutionExamples/main/lwc/attachToCaseService/attachToCaseService.js
@@ -1,11 +1,39 @@
 /**
+ * This file provides stubbed implementations for attaching, detaching, and retrieving results associated to a case.
+ *
+ * These methods are meant to be replaced in real implementations with actual Apex calls to the AttachToCaseController.
+ *
+ * Example of what a real implementation might look like (commented out below for reference):
+ *
+ * ```js
+ * // @ts-ignore
+ * import AttachToCase from '@salesforce/apex/CoveoV2.AttachToCaseController.AuraAttachToCase';
+ * // @ts-ignore
+ * import DetachFromCase from '@salesforce/apex/CoveoV2.AttachToCaseController.AuraDetachFromCase';
+ * // @ts-ignore
+ * import getAttachedResults from '@salesforce/apex/CoveoV2.AttachToCaseController.getAttachedResults';
+ *
+ * export function attachToCase(resultToAttach) {
+ *   return AttachToCase(resultToAttach);
+ * }
+ *
+ * export function detachFromCase(resultToDetach) {
+ *   return DetachFromCase(resultToDetach);
+ * }
+ *
+ * export function getAllAttachedResults(caseId) {
+ *   return getAttachedResults({ caseId });
+ * }
+ * ```
+ *
+ * See documentation for details on how to override this file with a real implementation.
+ */
+
+/**
  * Stub method to simulate attaching a result to a case.
  *
  * This function should be replaced with a real Apex implementation in the customer project.
  * It returns a mock success response.
- *
- * @param {{[key: string]: any}} resultToAttach - The result object to attach, typically includes case ID and result metadata.
- * @returns {Promise<string>} A promise that resolves to a JSON stringified response, e.g., `{"succeeded": true}`.
  */
 // eslint-disable-next-line no-unused-vars
 export function attachToCase(resultToAttach) {
