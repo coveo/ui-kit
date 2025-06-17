cc79bee0ecb583544a70249fe98c728387f9d6d1
Alan Clarke <640611+alanclarke@users.noreply.github.com>
2025-06-17 18:05:14 +0100

    feat(shopify): emit clientId in custom event (#5472)
    This PR adds clientId to the list of things sent to webpixel, which will
    greatly simplify the job of the webpixel

--- packages/shopify/src/utilities/shopify.test.ts
+++ packages/shopify/src/utilities/shopify.test.ts
@@ -1,9 +1,14 @@
 import {describe, it, expect, vi, beforeEach, afterEach} from 'vitest';
-import {
-  publishCustomShopifyEvent,
-  CustomEvent,
-  getShopifyCookie,
-} from './shopify';
+import {CoveoShopifyCustomEvent} from '../types';
+import {publishCustomShopifyEvent} from './shopify';
+
+const mockCustomShopifyEvent: CoveoShopifyCustomEvent = {
+  organizationId: 'org123',
+  accessToken: 'token123',
+  trackingId: 'tracking123',
+  clientId: 'client123',
+  environment: undefined,
+};
 
 describe('publishCustomShopifyEvent', () => {
   let originalShopify: typeof window.Shopify;
@@ -23,11 +28,10 @@ describe('publishCustomShopifyEvent', () => {
 
   it('should call Shopify.analytics.publish with the correct arguments', () => {
     const eventName = 'test_event';
-    const eventData: CustomEvent = {foo: 'bar'};
-    publishCustomShopifyEvent(eventName, eventData);
+    publishCustomShopifyEvent(eventName, mockCustomShopifyEvent);
     expect(window.Shopify!.analytics.publish).toHaveBeenCalledWith(
       eventName,
-      eventData
+      mockCustomShopifyEvent
     );
   });
 
@@ -36,65 +40,19 @@ describe('publishCustomShopifyEvent', () => {
       analytics: {
         publish: undefined as unknown as (
           eventName: string,
-          eventData: CustomEvent
+          eventData: CoveoShopifyCustomEvent
         ) => void,
       },
     };
     expect(() =>
-      publishCustomShopifyEvent('event', {foo: 'bar'})
+      publishCustomShopifyEvent('event', mockCustomShopifyEvent)
     ).not.toThrow();
   });
 
   it('should not throw if window.Shopify is undefined', () => {
     window.Shopify = undefined;
     expect(() =>
-      publishCustomShopifyEvent('event', {foo: 'bar'})
+      publishCustomShopifyEvent('event', mockCustomShopifyEvent)
     ).not.toThrow();
-  });
-});
-
-describe('getShopifyCookie', () => {
-  let originalCookie: PropertyDescriptor | undefined;
-
-  beforeEach(() => {
-    originalCookie = Object.getOwnPropertyDescriptor(document, 'cookie');
-  });
-
-  afterEach(() => {
-    if (originalCookie) {
-      Object.defineProperty(document, 'cookie', originalCookie);
-    }
-  });
-
-  it('should return the value of the _shopify_y cookie', () => {
-    Object.defineProperty(document, 'cookie', {
-      writable: true,
-      value: '_shopify_y=abc123; other=val',
-    });
-    expect(getShopifyCookie()).toBe('abc123');
-  });
-
-  it('should return null if the cookie is not found', () => {
-    Object.defineProperty(document, 'cookie', {
-      writable: true,
-      value: 'other=val',
-    });
-    expect(getShopifyCookie()).toBeNull();
-  });
-
-  it('should decode URI components in the cookie value', () => {
-    Object.defineProperty(document, 'cookie', {
-      writable: true,
-      value: '_shopify_y=abc%20123',
-    });
-    expect(getShopifyCookie()).toBe('abc 123');
-  });
-
-  it('should return the value of a custom cookie name', () => {
-    Object.defineProperty(document, 'cookie', {
-      writable: true,
-      value: 'custom_cookie=customValue',
-    });
-    expect(getShopifyCookie('custom_cookie')).toBe('customValue');
   });
 });
