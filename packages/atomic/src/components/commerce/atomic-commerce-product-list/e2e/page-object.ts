760e5f61fcf4d942ddb65f40348973928f761d9b
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-06-17 14:17:47 -0400

    fix
    https://coveord.atlassian.net/browse/KIT-3935

--- packages/atomic/src/components/commerce/atomic-commerce-product-list/e2e/page-object.ts
+++ packages/atomic/src/components/commerce/atomic-commerce-product-list/e2e/page-object.ts
@@ -1,7 +1,7 @@
-import {BasePageObject} from '@/playwright-utils/base-page-object';
+import {BasePageObject} from '@/playwright-utils/lit-base-page-object';
 import type {Page} from '@playwright/test';
 
-export class ProductListObject extends BasePageObject<'atomic-commerce-product-list'> {
+export class ProductListObject extends BasePageObject {
   constructor(page: Page) {
     super(page, 'atomic-commerce-product-list');
   }
