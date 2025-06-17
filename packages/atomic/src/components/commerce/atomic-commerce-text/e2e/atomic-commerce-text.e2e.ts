9f2fd23d03e95e13533588fa8939738268533949
Felix Perron-Brault <fpbrault@coveo.com>
2025-06-17 15:09:54 -0400

    apply review suggestions
    https://coveord.atlassian.net/browse/KIT-4314

--- packages/atomic/src/components/commerce/atomic-commerce-text/e2e/atomic-commerce-text.e2e.ts
+++ packages/atomic/src/components/commerce/atomic-commerce-text/e2e/atomic-commerce-text.e2e.ts
@@ -10,12 +10,15 @@ test.describe('default', () => {
   test('should be A11y compliant', async ({text, makeAxeBuilder}) => {
     await text.load();
 
+    await text.hydrated.waitFor();
     const accessibilityResults = await makeAxeBuilder().analyze();
     expect(accessibilityResults.violations).toEqual([]);
   });
 
   test('should translate text', async ({text}) => {
     await text.load({story: 'with-translations'});
+
+    await text.hydrated.waitFor();
 
     await expect(text.getText).toHaveText('A single product');
   });
@@ -25,6 +28,8 @@ test.describe('default', () => {
       args: {count: 2, value: 'translation-key'},
       story: 'with-translations',
     });
+
+    await text.hydrated.waitFor();
 
     await expect(text.getText).toHaveText('2 products');
   });
