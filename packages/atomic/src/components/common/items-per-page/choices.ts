3ed4f56073af3f9341493f42aa494ae44a7563e2
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-06-18 10:52:34 -0400

    chore(atomic): migrate items-per-page/choices.tsx
    https://coveord.atlassian.net/browse/KIT-4380

---
+++ packages/atomic/src/components/common/items-per-page/choices.ts
@@ -0,0 +1,72 @@
+import {FunctionalComponent} from '@/src/utils/functional-component-utils';
+import {html} from 'lit';
+import {radioButton} from '../radio-button';
+
+interface ChoicesProps {
+  label: string;
+  groupName: string;
+  pageSize: number;
+  choices: number[];
+  lang: string;
+  scrollToTopEvent: () => {};
+  setItemSize: (size: number) => void;
+  focusOnFirstResultAfterNextSearch: () => Promise<void> | undefined;
+  focusOnNextNewResult: () => void | undefined;
+}
+
+export const renderChoices: FunctionalComponent<ChoicesProps> = ({props}) => {
+  const {
+    label,
+    groupName,
+    pageSize,
+    choices,
+    lang,
+    scrollToTopEvent,
+    setItemSize,
+    focusOnFirstResultAfterNextSearch,
+    focusOnNextNewResult,
+  } = props;
+
+  const focusOnProperResultDependingOnChoice = (choice: number) => {
+    if (choice < pageSize) {
+      focusOnFirstResultAfterNextSearch()?.then(() => scrollToTopEvent());
+    } else if (choice > pageSize) {
+      focusOnNextNewResult();
+    }
+  };
+
+  return html`
+    <div
+      part="buttons"
+      role="radiogroup"
+      aria-label=${label}
+      class="flex flex-wrap gap-2"
+    >
+      ${choices.map((choice) => {
+        const isSelected = pageSize === choice;
+        const parts = ['button'];
+        if (isSelected) {
+          parts.push('active-button');
+        }
+        const text = choice.toLocaleString(lang);
+        return radioButton({
+          props: {
+            key: 'choice',
+            groupName,
+            style: 'outline-neutral',
+            checked: isSelected,
+            ariaLabel: text,
+            onChecked: () => {
+              focusOnProperResultDependingOnChoice(choice);
+              setItemSize(choice);
+            },
+            class: 'btn-page focus-visible:bg-neutral-light',
+            part: parts.join(' '),
+            text,
+            selectWhenFocused: false,
+          },
+        });
+      })}
+    </div>
+  `;
+};
