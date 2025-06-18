2d01d289b00094a9adc3424a872d379062707e42
fbeaudoincoveo <fbeaudoin@coveo.com>
2025-06-18 13:54:53 -0400

    Remove .only
    https://coveord.atlassian.net/browse/KIT-3934

--- packages/atomic/src/components/commerce/atomic-commerce-recommendation-interface/e2e/atomic-commerce-recommendation-interface.e2e.ts
+++ packages/atomic/src/components/commerce/atomic-commerce-recommendation-interface/e2e/atomic-commerce-recommendation-interface.e2e.ts
@@ -1,7 +1,7 @@
 import {AtomicCommerceRecommendationInterface} from '../atomic-commerce-recommendation-interface';
 import {test, expect} from './fixture';
 
-test.only('AtomicCommerceRecommendationInterface', () => {
+test('AtomicCommerceRecommendationInterface', () => {
   test('should attach itself', async ({commerceRecommendationInterface}) => {
     await commerceRecommendationInterface.load({story: 'default'});
     await expect(commerceRecommendationInterface.interface()).toBeAttached();
