1aa8309b72d453e65b31d5bc1d156417c6fb81b5
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-06-24 12:04:17 -0400

    docs(atomic): document slot in no-items components (#5519)
    ## Summary
    
    This PR addresses KIT-4377 by documenting the undocumented slot in
    no-items components.
    
    ## Changes
    
    - Added `@slot default` documentation to `atomic-no-results` component
    - Fixed component name in JSDoc for `atomic-commerce-no-products`
    component (was incorrectly named `atomic-no-products`)
    - Both components now properly document the default slot that allows
    additional content to be slotted inside
    
    ## Related
    
    - Fixes KIT-4377
    - Jira: https://coveord.atlassian.net/browse/KIT-4377
    
    ## Testing
    
    - Documentation changes only, no functionality affected
    - The slot was already working, this just adds proper documentation for
    it
    
    ---------
    
    Co-authored-by: developer-experience-bot[bot] <91079284+developer-experience-bot[bot]@users.noreply.github.com>

--- packages/atomic/src/components.d.ts
+++ packages/atomic/src/components.d.ts
@@ -370,7 +370,7 @@ export namespace Components {
     interface AtomicCommerceLoadMoreProducts {
     }
     /**
-     * @alpha The `atomic-no-products` component displays search tips when there are no products. Any additional content slotted inside of its element will be displayed as well.
+     * @alpha The `atomic-commerce-no-products` component displays search tips when there are no products. Any additional content slotted inside of its element will be displayed as well.
      */
     interface AtomicCommerceNoProducts {
     }
@@ -3768,7 +3768,7 @@ declare global {
         new (): HTMLAtomicCommerceLoadMoreProductsElement;
     };
     /**
-     * @alpha The `atomic-no-products` component displays search tips when there are no products. Any additional content slotted inside of its element will be displayed as well.
+     * @alpha The `atomic-commerce-no-products` component displays search tips when there are no products. Any additional content slotted inside of its element will be displayed as well.
      */
     interface HTMLAtomicCommerceNoProductsElement extends Components.AtomicCommerceNoProducts, HTMLStencilElement {
     }
@@ -6117,7 +6117,7 @@ declare namespace LocalJSX {
     interface AtomicCommerceLoadMoreProducts {
     }
     /**
-     * @alpha The `atomic-no-products` component displays search tips when there are no products. Any additional content slotted inside of its element will be displayed as well.
+     * @alpha The `atomic-commerce-no-products` component displays search tips when there are no products. Any additional content slotted inside of its element will be displayed as well.
      */
     interface AtomicCommerceNoProducts {
     }
@@ -9435,7 +9435,7 @@ declare module "@stencil/core" {
              */
             "atomic-commerce-load-more-products": LocalJSX.AtomicCommerceLoadMoreProducts & JSXBase.HTMLAttributes<HTMLAtomicCommerceLoadMoreProductsElement>;
             /**
-             * @alpha The `atomic-no-products` component displays search tips when there are no products. Any additional content slotted inside of its element will be displayed as well.
+             * @alpha The `atomic-commerce-no-products` component displays search tips when there are no products. Any additional content slotted inside of its element will be displayed as well.
              */
             "atomic-commerce-no-products": LocalJSX.AtomicCommerceNoProducts & JSXBase.HTMLAttributes<HTMLAtomicCommerceNoProductsElement>;
             /**
