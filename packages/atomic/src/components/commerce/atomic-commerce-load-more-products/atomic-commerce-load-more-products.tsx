2804c88362b08e86d4dfaa18e9f99044083770ac
Frederic Beaudoin <fbeaudoin@coveo.com>
2025-06-26 13:24:58 -0400

    chore(atomic): migrate LoadMoreContainer functional component to Lit (#5514)
    https://coveord.atlassian.net/browse/KIT-4407

--- packages/atomic/src/components/commerce/atomic-commerce-load-more-products/atomic-commerce-load-more-products.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-load-more-products/atomic-commerce-load-more-products.tsx
@@ -14,9 +14,9 @@ import {
   InitializeBindings,
 } from '../../../utils/initialization-utils';
 import {createAppLoadedListener} from '../../common/interface/store';
-import {LoadMoreContainer} from '../../common/load-more/container';
 import {LoadMoreGuard} from '../../common/load-more/guard';
 import {LoadMoreButton} from '../../common/load-more/stencil-button';
+import {LoadMoreContainer} from '../../common/load-more/stencil-container';
 import {LoadMoreProgressBar} from '../../common/load-more/stencil-progress-bar';
 import {LoadMoreSummary} from '../../common/load-more/stencil-summary';
 import {CommerceBindings} from '../atomic-commerce-interface/atomic-commerce-interface';
