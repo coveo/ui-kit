203455f6b6f357cede5ef9b4bfd320fb2743b9c5
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-06-26 08:17:08 -0400

    chore(atomic): migrate query-error/show-more.tsx
    https://coveord.atlassian.net/browse/KIT-4423

--- packages/atomic/src/components/commerce/atomic-commerce-query-error/atomic-commerce-query-error.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-query-error/atomic-commerce-query-error.tsx
@@ -21,7 +21,7 @@ import {QueryErrorDetails} from '../../common/query-error/details';
 import {QueryErrorGuard} from '../../common/query-error/guard';
 import {QueryErrorIcon} from '../../common/query-error/icon';
 import {QueryErrorLink} from '../../common/query-error/link';
-import {QueryErrorShowMore} from '../../common/query-error/show-more';
+import {QueryErrorShowMore} from '../../common/query-error/stencil-show-more';
 import {QueryErrorTitle} from '../../common/query-error/title';
 import {getAriaMessageFromErrorType} from '../../common/query-error/utils';
 import {CommerceBindings} from '../atomic-commerce-interface/atomic-commerce-interface';
