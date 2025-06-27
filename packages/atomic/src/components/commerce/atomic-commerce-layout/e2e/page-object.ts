952bc6f42110e356565c6acf5b276b30cc5a0b2b
ylakhdar <ylakhdar@coveo.com>
2025-06-27 12:05:53 -0400

    remove out of scope tests
    https://coveord.atlassian.net/browse/KIT-4333

--- packages/atomic/src/components/commerce/atomic-commerce-layout/e2e/page-object.ts
+++
@@ -1,8 +0,0 @@
-import {BasePageObject} from '@/playwright-utils/lit-base-page-object';
-import type {Page} from '@playwright/test';
-
-export class AtomicCommerceLayoutPageObject extends BasePageObject {
-  constructor(page: Page) {
-    super(page, 'atomic-commerce-layout');
-  }
-}
