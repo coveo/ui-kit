b5d13c47f65d505969e77eff2b5966bd7df0a1aa
fbeaudoincoveo <fbeaudoin@coveo.com>
2025-07-08 11:48:47 -0400

    Use lit base page object
    https://coveord.atlassian.net/browse/KIT-4058

--- packages/atomic/src/components/commerce/atomic-commerce-load-more-products/e2e/page-object.ts
+++ packages/atomic/src/components/commerce/atomic-commerce-load-more-products/e2e/page-object.ts
@@ -1,7 +1,7 @@
-import {BasePageObject} from '@/playwright-utils/base-page-object';
+import {BasePageObject} from '@/playwright-utils/lit-base-page-object';
 import type {Page} from '@playwright/test';
 
-export class LoadMoreProductsPageObject extends BasePageObject<'atomic-commerce-load-more-products'> {
+export class LoadMoreProductsPageObject extends BasePageObject {
   constructor(page: Page) {
     super(page, 'atomic-commerce-load-more-products');
   }
