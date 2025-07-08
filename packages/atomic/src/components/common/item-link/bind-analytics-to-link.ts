aff7450cd1b84cdb7e5badacd02cebfab69bd394
Felix Perron-Brault <fpbrault@coveo.com>
2025-07-08 11:15:31 -0400

    migrate item-link
    https://coveord.atlassian.net/browse/KIT-4498
    
    Add generated files
    
    Add generated files
    
    cleanup
    
    https://coveord.atlassian.net/browse/KIT-4498
    
    cleanup
    
    https://coveord.atlassian.net/browse/KIT-4498

---
+++ packages/atomic/src/components/common/item-link/bind-analytics-to-link.ts
@@ -0,0 +1,40 @@
+import {ItemLinkProps} from './item-link';
+
+type ItemLinkEventProps = Pick<
+  ItemLinkProps,
+  | 'onSelect'
+  | 'onBeginDelayedSelect'
+  | 'onCancelPendingSelect'
+  | 'stopPropagation'
+>;
+
+export const bindAnalyticsToLink = (
+  link: HTMLAnchorElement,
+  {
+    onSelect,
+    onBeginDelayedSelect,
+    onCancelPendingSelect,
+    stopPropagation = true,
+  }: ItemLinkEventProps
+) => {
+  const stopPropagationAndProcess = (e: Event, process: () => void) => {
+    stopPropagation && e.stopPropagation();
+    process();
+  };
+  (['click', 'contextmenu', 'mousedown', 'mouseup'] as const).forEach(
+    (eventName) =>
+      link.addEventListener(eventName, (e) =>
+        stopPropagationAndProcess(e, onSelect)
+      )
+  );
+  link.addEventListener(
+    'touchstart',
+    (e) => stopPropagationAndProcess(e, onBeginDelayedSelect),
+    {passive: true}
+  );
+  link.addEventListener(
+    'touchend',
+    (e) => stopPropagationAndProcess(e, onCancelPendingSelect),
+    {passive: true}
+  );
+};
