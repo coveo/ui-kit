a7324ca3231a492fd09918c1b80705e6a28f0e69
Felix Perron-Brault <fpbrault@coveo.com>
2025-07-08 11:15:31 -0400

    migrate item-link
    https://coveord.atlassian.net/browse/KIT-4498
    
    Add generated files
    
    Add generated files
    
    cleanup
    
    https://coveord.atlassian.net/browse/KIT-4498
    
    cleanup
    
    https://coveord.atlassian.net/browse/KIT-4498

---
+++ packages/atomic/src/components/common/item-link/bind-analytics-to-link.spec.ts
@@ -0,0 +1,124 @@
+import {describe, it, vi, expect, beforeEach} from 'vitest';
+import {bindAnalyticsToLink} from './bind-analytics-to-link';
+
+describe('#bindAnalyticsToLink', () => {
+  let mockLink: HTMLAnchorElement;
+  let eventProps: {
+    onSelect: ReturnType<typeof vi.fn>;
+    onBeginDelayedSelect: ReturnType<typeof vi.fn>;
+    onCancelPendingSelect: ReturnType<typeof vi.fn>;
+    stopPropagation: boolean;
+  };
+
+  beforeEach(() => {
+    mockLink = document.createElement('a');
+    eventProps = {
+      onSelect: vi.fn(),
+      onBeginDelayedSelect: vi.fn(),
+      onCancelPendingSelect: vi.fn(),
+      stopPropagation: true,
+    };
+  });
+
+  it('should call onSelect for click events', () => {
+    bindAnalyticsToLink(mockLink, eventProps);
+
+    const clickEvent = new MouseEvent('click', {bubbles: true});
+    mockLink.dispatchEvent(clickEvent);
+
+    expect(eventProps.onSelect).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onSelect for contextmenu events', () => {
+    bindAnalyticsToLink(mockLink, eventProps);
+
+    const contextMenuEvent = new MouseEvent('contextmenu', {bubbles: true});
+    mockLink.dispatchEvent(contextMenuEvent);
+
+    expect(eventProps.onSelect).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onSelect for mousedown events', () => {
+    bindAnalyticsToLink(mockLink, eventProps);
+
+    const mouseDownEvent = new MouseEvent('mousedown', {bubbles: true});
+    mockLink.dispatchEvent(mouseDownEvent);
+
+    expect(eventProps.onSelect).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onSelect for mouseup events', () => {
+    bindAnalyticsToLink(mockLink, eventProps);
+
+    const mouseUpEvent = new MouseEvent('mouseup', {bubbles: true});
+    mockLink.dispatchEvent(mouseUpEvent);
+
+    expect(eventProps.onSelect).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onBeginDelayedSelect for touchstart events', () => {
+    bindAnalyticsToLink(mockLink, eventProps);
+
+    const touchStartEvent = new TouchEvent('touchstart', {bubbles: true});
+    mockLink.dispatchEvent(touchStartEvent);
+
+    expect(eventProps.onBeginDelayedSelect).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onCancelPendingSelect for touchend events', () => {
+    bindAnalyticsToLink(mockLink, eventProps);
+
+    const touchEndEvent = new TouchEvent('touchend', {bubbles: true});
+    mockLink.dispatchEvent(touchEndEvent);
+
+    expect(eventProps.onCancelPendingSelect).toHaveBeenCalledTimes(1);
+  });
+
+  it('should stop propagation when stopPropagation is true', () => {
+    bindAnalyticsToLink(mockLink, {...eventProps, stopPropagation: true});
+
+    const clickEvent = new MouseEvent('click', {bubbles: true});
+    const stopPropagationSpy = vi.spyOn(clickEvent, 'stopPropagation');
+
+    mockLink.dispatchEvent(clickEvent);
+
+    expect(stopPropagationSpy).toHaveBeenCalledTimes(1);
+    expect(eventProps.onSelect).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not stop propagation when stopPropagation is false', () => {
+    bindAnalyticsToLink(mockLink, {...eventProps, stopPropagation: false});
+
+    const clickEvent = new MouseEvent('click', {bubbles: true});
+    const stopPropagationSpy = vi.spyOn(clickEvent, 'stopPropagation');
+
+    mockLink.dispatchEvent(clickEvent);
+
+    expect(stopPropagationSpy).not.toHaveBeenCalled();
+    expect(eventProps.onSelect).toHaveBeenCalledTimes(1);
+  });
+
+  it('should stop propagation for touch events when enabled', () => {
+    bindAnalyticsToLink(mockLink, {...eventProps, stopPropagation: true});
+
+    const touchStartEvent = new TouchEvent('touchstart', {bubbles: true});
+    const stopPropagationSpy = vi.spyOn(touchStartEvent, 'stopPropagation');
+
+    mockLink.dispatchEvent(touchStartEvent);
+
+    expect(stopPropagationSpy).toHaveBeenCalledTimes(1);
+    expect(eventProps.onBeginDelayedSelect).toHaveBeenCalledTimes(1);
+  });
+
+  it('should default to stopPropagation=true when not specified', () => {
+    const {stopPropagation, ...propsWithoutStopPropagation} = eventProps;
+    bindAnalyticsToLink(mockLink, propsWithoutStopPropagation);
+
+    const clickEvent = new MouseEvent('click', {bubbles: true});
+    const stopPropagationSpy = vi.spyOn(clickEvent, 'stopPropagation');
+
+    mockLink.dispatchEvent(clickEvent);
+
+    expect(stopPropagationSpy).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ packages/atomic/src/components/common/item-link/item-link.ts
@@ -0,0 +1,93 @@
+import type {FunctionalComponentWithChildren} from '@/src/utils/functional-component-utils';
+import {html} from 'lit';
+import {ifDefined} from 'lit/directives/if-defined.js';
+import {ref} from 'lit/directives/ref.js';
+
+import {filterProtocol} from '@/src/utils/xss-utils.js';
+import type {ItemTarget} from '@/src/components/common/layout/display-options';
+import {bindAnalyticsToLink} from './bind-analytics-to-link';
+export interface ItemLinkProps {
+  href: string;
+  className?: string;
+  part?: string;
+  title?: string;
+  stopPropagation?: boolean;
+  ref?: (elm?: HTMLAnchorElement) => void;
+  attributes?: Attr[];
+  tabIndex?: number;
+  target?: ItemTarget;
+  rel?: string;
+  onMouseOver?: () => void;
+  onMouseLeave?: () => void;
+  onFocus?: () => void;
+  onBlur?: () => void;
+  onSelect: () => void;
+  onBeginDelayedSelect: () => void;
+  onCancelPendingSelect: () => void;
+}
+
+export const renderLinkWithItemAnalytics: FunctionalComponentWithChildren<
+  ItemLinkProps
+> =
+  ({props}) =>
+  (children) => {
+    const {
+      href,
+      className,
+      part,
+      title,
+      stopPropagation = true,
+      ref: refCallback,
+      attributes,
+      tabIndex,
+      target = '_self',
+      rel,
+      onMouseOver,
+      onMouseLeave,
+      onFocus,
+      onBlur,
+      onSelect,
+      onBeginDelayedSelect,
+      onCancelPendingSelect,
+    } = props;
+
+    return html`
+      <a
+        class=${ifDefined(className)}
+        part=${ifDefined(part)}
+        href=${filterProtocol(href)}
+        target=${target}
+        title=${ifDefined(title)}
+        rel=${ifDefined(rel)}
+        ${ref((el) => {
+          if (refCallback) {
+            refCallback(el as HTMLAnchorElement);
+          }
+
+          if (!el) {
+            return;
+          }
+
+          bindAnalyticsToLink(el as HTMLAnchorElement, {
+            onSelect,
+            onBeginDelayedSelect,
+            onCancelPendingSelect,
+            stopPropagation,
+          });
+
+          if (attributes?.length) {
+            [...attributes].forEach(({nodeName, nodeValue}) => {
+              el.setAttribute(nodeName, nodeValue!);
+            });
+          }
+        })}
+        tabindex=${ifDefined(tabIndex)}
+        @mouseover=${onMouseOver}
+        @mouseleave=${onMouseLeave}
+        @focus=${onFocus}
+        @blur=${onBlur}
+      >
+        ${children}
+      </a>
+    `;
+  };
