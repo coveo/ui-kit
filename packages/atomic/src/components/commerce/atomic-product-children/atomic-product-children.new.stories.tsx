323ff31583cb4ed00f5d07ee8bf533db5ce9ce80
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-07-10 14:21:22 -0400

    chore(atomic): migrate atomic-product-children
    https://coveord.atlassian.net/browse/KIT-4444

--- packages/atomic/src/components/commerce/atomic-product-children/atomic-product-children.new.stories.tsx
+++ packages/atomic/src/components/commerce/atomic-product-children/atomic-product-children.new.stories.tsx
@@ -1,19 +1,39 @@
-// import {wrapInCommerceInterface} from '@/storybook-utils/commerce/commerce-interface-wrapper';
 import type {Meta, StoryObj as Story} from '@storybook/web-components';
+import {html} from 'lit';
+import {wrapInCommerceInterface} from '@/storybook-utils/commerce/commerce-interface-wrapper';
+import {wrapInCommerceProductList} from '@/storybook-utils/commerce/commerce-product-list-wrapper';
+import {wrapInProductTemplate} from '@/storybook-utils/commerce/commerce-product-template-wrapper';
 import {parameters} from '@/storybook-utils/common/common-meta-parameters';
 import {renderComponent} from '@/storybook-utils/common/render-component';
-import {wrapInSearchInterface} from '@/storybook-utils/search/search-interface-wrapper';
 
-// Wrap it in whatever interface/component you need
-const {decorator, play} = wrapInSearchInterface();
-// const {decorator, play} = wrapInCommerceInterface();
+const {decorator: commerceInterfaceDecorator, play} = wrapInCommerceInterface({
+  engineConfig: {
+    preprocessRequest: (request) => {
+      const parsed = JSON.parse(request.body as string);
+      parsed.perPage = 1;
+      request.body = JSON.stringify(parsed);
+      return request;
+    },
+  },
+});
+const {decorator: commerceProductListDecorator} = wrapInCommerceProductList();
+const {decorator: productTemplateDecorator} = wrapInProductTemplate();
 
 const meta: Meta = {
   component: 'atomic-product-children',
-  title: 'TODO/atomic-product-children',
+  title: 'Commerce/atomic-product-children',
   id: 'atomic-product-children',
   render: renderComponent,
-  decorators: [decorator],
+  decorators: [
+    (story) => html`
+    <atomic-product-section-children>
+      ${story()}
+    </atomic-product-section-children>    
+  `,
+    productTemplateDecorator,
+    commerceProductListDecorator,
+    commerceInterfaceDecorator,
+  ],
   parameters,
   play,
 };
--- packages/atomic/src/components/commerce/atomic-product-children/e2e/atomic-product-children.e2e.ts
+++ packages/atomic/src/components/commerce/atomic-product-children/e2e/atomic-product-children.e2e.ts
@@ -3,10 +3,20 @@ import {expect, test} from './fixture';
 test.describe('AtomicProductChildren', () => {
   test.beforeEach(async ({productChildren}) => {
     await productChildren.load();
+    await productChildren.hydrated.waitFor();
   });
 
   test('should be accessible', async ({makeAxeBuilder}) => {
     const accessibilityResults = await makeAxeBuilder().analyze();
-    expect(accessibilityResults.violations.length).toEqual(0);
+    expect(accessibilityResults.violations).toEqual([]);
+  });
+
+  test('should render a label', async ({productChildren}) => {
+    await expect(productChildren.label).toHaveText('Available in:');
+  });
+
+  test('should render child products', async ({productChildren}) => {
+    const count = await productChildren.childProducts.count();
+    expect(count).toBeGreaterThan(2);
   });
 });
