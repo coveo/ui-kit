606984745c8b9fe986d7d35a9875129ea0706899
ylakhdar <ylakhdar@coveo.com>
2025-07-10 10:11:22 -0400

    chore(atomic): migrate CategoryFacetTreeValueContainer (#5602)
    https://coveord.atlassian.net/browse/KIT-4501
    
    ---------
    
    Co-authored-by: Frederic Beaudoin <fbeaudoin@coveo.com>

---
+++ packages/atomic/src/components/common/facets/category-facet/value-as-tree-container.spec.ts
@@ -0,0 +1,29 @@
+import {renderFunctionFixture} from '@/vitest-utils/testing-helpers/fixture';
+import {html} from 'lit';
+import {describe, it, expect} from 'vitest';
+import {renderCategoryFacetTreeValueContainer} from './value-as-tree-container';
+
+describe('#renderCategoryFacetTreeValueContainer', () => {
+  const renderComponent = async (children = html`<span>Test child</span>`) => {
+    const container = await renderFunctionFixture(
+      html`${renderCategoryFacetTreeValueContainer({props: {}})(children)}`
+    );
+
+    return {
+      li: container.querySelector('li'),
+      subItems: container.querySelectorAll('.subcategories li'),
+    };
+  };
+
+  it('should render tree group structure', async () => {
+    const {subItems, li} = await renderComponent(html`
+      <ul class="subcategories">
+        <li>Subcategory 1</li>
+        <li>Subcategory 2</li>
+      </ul>
+    `);
+
+    expect(subItems).toHaveLength(2);
+    expect(li).toBeInTheDocument();
+  });
+});
--- packages/atomic/src/components/search/facets/atomic-category-facet/atomic-category-facet.tsx
+++ packages/atomic/src/components/search/facets/atomic-category-facet/atomic-category-facet.tsx
@@ -39,7 +39,7 @@ import {CategoryFacetParentButton} from '../../../common/facets/category-facet/s
 import {CategoryFacetParentValueLink} from '../../../common/facets/category-facet/stencil-parent-value-link';
 import {CategoryFacetSearchResultsContainer} from '../../../common/facets/category-facet/stencil-search-results-container';
 import {CategoryFacetSearchValue} from '../../../common/facets/category-facet/stencil-search-value';
-import {CategoryFacetTreeValueContainer} from '../../../common/facets/category-facet/value-as-tree-container';
+import {CategoryFacetTreeValueContainer} from '../../../common/facets/category-facet/stencil-value-as-tree-container';
 import {parseDependsOn} from '../../../common/facets/depends-on';
 import {FacetInfo} from '../../../common/facets/facet-common-store';
 import {FacetContainer} from '../../../common/facets/facet-container/stencil-facet-container';
