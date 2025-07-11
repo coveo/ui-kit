3ac7d301a26d7f1cb68438b17c13f9deb4721210
Felix Perron-Brault <fpbrault@coveo.com>
2025-07-11 16:05:14 -0400

    add cleanup function
    https://coveord.atlassian.net/browse/KIT-4498

--- packages/atomic/src/components/common/item-link/bind-analytics-to-link.ts
+++ packages/atomic/src/components/common/item-link/bind-analytics-to-link.ts
@@ -16,25 +16,29 @@ export const bindAnalyticsToLink = (
     onCancelPendingSelect,
     stopPropagation = true,
   }: ItemLinkEventProps
-) => {
+): (() => void) => {
   const stopPropagationAndProcess = (e: Event, process: () => void) => {
     stopPropagation && e.stopPropagation();
     process();
   };
+
+  const selectHandler = (e: Event) => stopPropagationAndProcess(e, onSelect);
+  const touchStartHandler = (e: Event) =>
+    stopPropagationAndProcess(e, onBeginDelayedSelect);
+  const touchEndHandler = (e: Event) =>
+    stopPropagationAndProcess(e, onCancelPendingSelect);
+
   (['click', 'contextmenu', 'mousedown', 'mouseup'] as const).forEach(
-    (eventName) =>
-      link.addEventListener(eventName, (e) =>
-        stopPropagationAndProcess(e, onSelect)
-      )
+    (eventName) => link.addEventListener(eventName, selectHandler)
   );
-  link.addEventListener(
-    'touchstart',
-    (e) => stopPropagationAndProcess(e, onBeginDelayedSelect),
-    {passive: true}
-  );
-  link.addEventListener(
-    'touchend',
-    (e) => stopPropagationAndProcess(e, onCancelPendingSelect),
-    {passive: true}
-  );
+  link.addEventListener('touchstart', touchStartHandler, {passive: true});
+  link.addEventListener('touchend', touchEndHandler, {passive: true});
+
+  return () => {
+    (['click', 'contextmenu', 'mousedown', 'mouseup'] as const).forEach(
+      (eventName) => link.removeEventListener(eventName, selectHandler)
+    );
+    link.removeEventListener('touchstart', touchStartHandler);
+    link.removeEventListener('touchend', touchEndHandler);
+  };
 };
