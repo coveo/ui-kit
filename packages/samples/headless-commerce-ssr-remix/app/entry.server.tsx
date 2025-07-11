f6d898146baf563f433282e3f56f4a3dc74aac18
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-07-11 16:03:24 -0400

    chore: enable recommended biome rules for commerce remix sample (#5646)
    https://coveord.atlassian.net/browse/KIT-4534

--- packages/samples/headless-commerce-ssr-remix/app/entry.server.tsx
+++ packages/samples/headless-commerce-ssr-remix/app/entry.server.tsx
@@ -1,7 +1,7 @@
+import {PassThrough} from 'node:stream';
 import type {EntryContext} from '@remix-run/node';
 import {createReadableStreamFromReadable} from '@remix-run/node';
 import {RemixServer} from '@remix-run/react';
-import {PassThrough} from 'node:stream';
 import {renderToPipeableStream} from 'react-dom/server';
 
 const ABORT_DELAY = 5_000;
