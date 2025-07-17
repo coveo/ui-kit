837e1bf221121ae2f87f58f7776170a63b547c96
ylakhdar <ylakhdar@coveo.com>
2025-07-17 11:45:11 -0400

    update index
    https://coveord.atlassian.net/browse/KIT-4447

--- packages/atomic/src/components/common/lazy-index.ts
+++ packages/atomic/src/components/common/lazy-index.ts
@@ -2,6 +2,8 @@
 export default {
   'atomic-component-error': async () =>
     await import('./atomic-component-error/atomic-component-error.js'),
+  'atomic-facet-date-input': async () =>
+    await import('./atomic-facet-date-input/atomic-facet-date-input.js'),
   'atomic-icon': async () => await import('./atomic-icon/atomic-icon.js'),
 } as Record<string, () => Promise<unknown>>;
 
