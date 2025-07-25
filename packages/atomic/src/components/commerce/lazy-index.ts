31b73d16eb84dd3e262866f7b3e2865a9a4fec52
Felix Perron-Brault <fpbrault@coveo.com>
2025-07-25 11:33:53 -0400

    cleanup
    https://coveord.atlassian.net/browse/KIT-4467

--- packages/atomic/src/components/commerce/lazy-index.ts
+++ packages/atomic/src/components/commerce/lazy-index.ts
@@ -78,8 +78,8 @@ export default {
     await import('./atomic-product-children/atomic-product-children.js'),
   'atomic-product-excerpt': async () =>
     await import('./atomic-product-excerpt/atomic-product-excerpt.js'),
-  'atomic-product-link': async () =>
-    await import('./atomic-product-link/atomic-product-link.js'),
+  'atomic-product-image': async () =>
+    await import('./atomic-product-image/atomic-product-image.js'),
   'atomic-product-multi-value-text': async () =>
     await import(
       './atomic-product-multi-value-text/atomic-product-multi-value-text.js'
@@ -90,8 +90,6 @@ export default {
     ),
   'atomic-product-price': async () =>
     await import('./atomic-product-price/atomic-product-price.js'),
-  'atomic-product-image': async () =>
-    await import('./atomic-product-image/atomic-product-image.js'),
   'atomic-product-template': async () =>
     await import('./atomic-product-template/atomic-product-template.js'),
   'atomic-product-text': async () =>
