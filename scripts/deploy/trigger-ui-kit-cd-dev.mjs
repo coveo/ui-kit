5b7f57fc4f40a4c2ef211ccc596e626b043ce3b1
Louis Bompart <lbompart@coveo.com>
2025-07-25 21:42:39 +0200

    add sha wiring

--- scripts/deploy/trigger-ui-kit-cd-dev.mjs
+++ scripts/deploy/trigger-ui-kit-cd-dev.mjs
@@ -6,6 +6,7 @@ await octokit.rest.repos.createDispatchEvent({
   event_type: 'deploy-dev',
   client_payload: {
     run_Id: context.runId,
+    sha: context.sha,
   },
   owner: 'coveo-platform',
   repo: 'ui-kit-cd',
--- scripts/deploy/trigger-ui-kit-cd-pr.mjs
+++ scripts/deploy/trigger-ui-kit-cd-pr.mjs
@@ -6,6 +6,7 @@ await octokit.rest.repos.createDispatchEvent({
   event_type: 'deploy-dev',
   client_payload: {
     run_Id: context.runId,
+    sha: context.sha,
     patch_only: 'true',
   },
   owner: 'coveo-platform',
