c8567671fefc0a4d05ac8ad86f12f54ecfeb0ecc
fbeaudoincoveo <fbeaudoin@coveo.com>
2025-07-28 16:19:08 -0400

    Property transform Atomic version
    https://coveord.atlassian.net/browse/KIT-4606

--- packages/atomic/scripts/version-transform.mjs
+++ packages/atomic/scripts/version-transform.mjs
@@ -21,28 +21,33 @@ export default function versionTransformer(context) {
   const {factory} = context;
 
   function visit(node) {
+    // Check if this node is process.env.VERSION
     if (
       isPropertyAccessExpression(node) &&
-      isIdentifier(node.expression) &&
-      node.expression.escapedText === 'process' &&
-      node.name.escapedText === 'env'
+      isPropertyAccessExpression(node.expression) &&
+      isIdentifier(node.expression.expression) &&
+      node.expression.expression.escapedText === 'process' &&
+      node.expression.name.escapedText === 'env' &&
+      node.name.escapedText === 'VERSION'
     ) {
-      const parent = node.parent;
-      if (
-        isPropertyAccessExpression(parent) &&
-        parent.name.escapedText === 'VERSION'
-      ) {
-        console.log('Replacing process.env.VERSION with:', version);
-        return factory.createStringLiteral(version);
-      } else if (
-        isNonNullExpression(parent) &&
-        isPropertyAccessExpression(parent.expression) &&
-        parent.expression.name.escapedText === 'VERSION'
-      ) {
-        console.log('Replacing process.env.VERSION! with:', version);
-        return factory.createStringLiteral(version);
-      }
+      console.log('Replacing process.env.VERSION with:', version);
+      return factory.createStringLiteral(version);
     }
+
+    // Check if this node is process.env.VERSION! (non-null assertion)
+    if (
+      isNonNullExpression(node) &&
+      isPropertyAccessExpression(node.expression) &&
+      isPropertyAccessExpression(node.expression.expression) &&
+      isIdentifier(node.expression.expression.expression) &&
+      node.expression.expression.expression.escapedText === 'process' &&
+      node.expression.expression.name.escapedText === 'env' &&
+      node.expression.name.escapedText === 'VERSION'
+    ) {
+      console.log('Replacing process.env.VERSION! with:', version);
+      return factory.createStringLiteral(version);
+    }
+
     return visitEachChild(node, visit, context);
   }
 
