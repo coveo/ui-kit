2ff6669be752b7e6625dc84f57277b0aca3151cc
Felix Perron-Brault <fpbrault@coveo.com>
2025-07-29 09:32:48 -0400

    fix next build
    https://coveord.atlassian.net/browse/KIT-4634

--- packages/atomic/src/decorators/with-tailwind-styles.ts
+++ packages/atomic/src/decorators/with-tailwind-styles.ts
@@ -3,23 +3,26 @@ import theme from '@/src/utils/coveo.tw.css';
 import styles from '@/src/utils/tailwind.global.tw.css';
 import utilities from '@/src/utils/tailwind-utilities/utilities.tw.css';
 
-const tailwindPropertiesSheet = (() => {
-  const propCss = styles as unknown as string[] | string;
-  const fullCss = Array.isArray(propCss) ? propCss.join('') : propCss;
+const tailwindPropertiesSheet: CSSStyleSheet | null =
+  typeof window !== 'undefined'
+    ? (() => {
+        const propCss = styles as unknown as string[] | string;
+        const fullCss = Array.isArray(propCss) ? propCss.join('') : propCss;
 
-  // Extract only the variable definitions inside @layer properties
-  const match = fullCss.match(
-    /@layer\s+properties\s*{[^{}]*@supports[^{}]*{([^}]*)}|@layer\s+properties\s*{([^}]*)}/
-  );
-  const cssString = (match?.[1] || match?.[2] || '').trim();
-  const sheet = new CSSStyleSheet();
-  sheet.replaceSync(cssString);
-  return sheet;
-})();
+        // Extract only the variable definitions inside @layer properties
+        const match = fullCss.match(
+          /@layer\s+properties\s*{[^{}]*@supports[^{}]*{([^}]*)}|@layer\s+properties\s*{([^}]*)}/
+        );
+        const cssString = (match?.[1] || match?.[2] || '').trim();
+        const sheet = new CSSStyleSheet();
+        sheet.replaceSync(cssString);
+        return sheet;
+      })()
+    : null;
 
 // biome-ignore lint/suspicious/noExplicitAny: <>
 function injectTailwindProperties(element: any) {
-  if (typeof document === 'undefined') return;
+  if (typeof window === 'undefined' || !tailwindPropertiesSheet) return;
 
   const parent = element.getRootNode();
   const isDocumentOrShadowRoot =
