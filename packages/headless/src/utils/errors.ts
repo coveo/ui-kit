6f24baeac63ee4098d0f4b2e027f2f4b0bcec0eb
ylakhdar <ylakhdar@coveo.com>
2025-07-30 15:50:15 -0400

    chore(headless): centralize SSR code under `src/ssr` (#5771)
    To help with future SSR development, I have consolidated all SSR-related
    code under the `ssr` directory. The types have also been reorganized
    into more focused and descriptive files. In the previous file structure,
    ssr-commerce code was pulling stuff from ssr-search which was confusing.
    Now, there is a `ssr/common` folder for that.
    
    ### Updated file structure:
    
    ```
    src/
      ssr/
        common/
          ...
        commerce/
          controllers/
          engine/
          ...
        search/
          controllers/
          engine/
    ```
    So in an upcoming PR, the `ssr-next` folder will be added to house
    future breaking changes, ensuring that current SSR clients remain
    unaffected:
    
    ```
    src/
      ssr/
        ...
      ssr-next/
        ...
    ```
    
    I also split the `types/common.ts` file which was essentially a gigantic
    definition file for everything into 4 meaningful definition files:
    
    1. `controller-constants.ts`
    2. `controller-definitions.ts`
    3. `controller-inference.ts`
    4. `controller-scopes.ts`

--- packages/headless/src/utils/errors.ts
+++ packages/headless/src/utils/errors.ts
@@ -1,5 +1,3 @@
-import {SolutionType} from '../app/commerce-ssr-engine/types/common.js';
-
 export const loadReducerError = new Error('Failed to load reducers.');
 
 export class ExpiredTokenError extends Error {
@@ -7,35 +5,6 @@ export class ExpiredTokenError extends Error {
     super();
     this.name = 'ExpiredToken';
     this.message = 'The token being used to perform the request is expired.';
-  }
-}
-
-export class InvalidControllerDefinition extends Error {
-  constructor() {
-    super();
-    this.name = 'InvalidControllerDefinition';
-    this.message = `A controller must be defined for at least one solution type: ${Object.keys(
-      SolutionType
-    )
-      .map((s) => s.toLowerCase())
-      .join(', ')}`;
-  }
-}
-
-export class MissingControllerProps extends Error {
-  constructor(controller: string) {
-    super();
-    this.name = 'MissingControllerProps';
-    this.message = `${controller} props are required but were undefined. Ensure they are included when calling \`fetchStaticState\` or \`hydrateStaticState\`.`;
-    // + '\nSee [TODO: add link to fetchStaticState example] for more information.';
-  }
-}
-
-export class MultipleRecommendationError extends Error {
-  constructor(slotId: string) {
-    super();
-    this.name = 'MultipleRecommendationError';
-    this.message = `Multiple recommendation controllers found for the same slotId: ${slotId}. Only one recommendation controller per slotId is supported.`;
   }
 }
 
