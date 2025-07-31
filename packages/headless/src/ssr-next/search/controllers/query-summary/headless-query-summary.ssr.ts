d4e6afa18029279f382175ef42c6c96747502173
ylakhdar <ylakhdar@coveo.com>
2025-07-31 15:04:24 -0400

    chore(headless-ssr): create ssr-next endpoints (#5779)
    Created new `ssr-next` and `ssr-commerce-next` sub-packages under
    `@coveo/headless` and `@coveo/headless-react` to support future breaking
    changes.
    
    **Everything inside `headless/src/ssr-next` is a copy paste of
    `headless/src/ssr`** No changes!
    
    > These endpoints are experimental and will be the entry point for
    upcoming major updates, allowing us to iterate on improvements without
    disrupting the current stable packages.

---
+++ packages/headless/src/ssr-next/search/controllers/query-summary/headless-query-summary.ssr.ts
@@ -0,0 +1,23 @@
+import type {SearchEngine} from '../../../../app/search-engine/search-engine.js';
+import {
+  buildQuerySummary,
+  type QuerySummary,
+} from '../../../../controllers/query-summary/headless-query-summary.js';
+import type {ControllerDefinitionWithoutProps} from '../../../common/types/controllers.js';
+
+export * from '../../../../controllers/query-summary/headless-query-summary.js';
+
+export interface QuerySummaryDefinition
+  extends ControllerDefinitionWithoutProps<SearchEngine, QuerySummary> {}
+
+/**
+ * Defines a `QuerySummary` controller instance.
+ * @group Definers
+ *
+ * @returns The `QuerySummary` controller definition.
+ * */
+export function defineQuerySummary(): QuerySummaryDefinition {
+  return {
+    build: (engine) => buildQuerySummary(engine),
+  };
+}
