6185cadecd0001990d93f250a40bfb7aa5e3b2dc
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-08-08 14:04:34 -0400

    fix other tests

--- packages/atomic/src/components/commerce/atomic-commerce-interface/atomic-commerce-interface.new.stories.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-interface/atomic-commerce-interface.new.stories.tsx
@@ -48,3 +48,38 @@ export const SearchBeforeInit: Story = {
     await commerceInterface!.executeFirstRequest();
   },
 };
+
+export const WithProductList: Story = {
+  args: {
+    'slots-default': `
+      <atomic-commerce-layout>
+        <atomic-layout-section section="search">
+          <atomic-commerce-search-box></atomic-commerce-search-box>
+        </atomic-layout-section>
+        <atomic-layout-section section="facets">
+          <atomic-commerce-facets></atomic-commerce-facets>
+        </atomic-layout-section>
+        <atomic-layout-section section="main">
+          <atomic-layout-section section="status">
+            <atomic-commerce-breadbox></atomic-commerce-breadbox>
+            <atomic-commerce-query-summary></atomic-commerce-query-summary>
+            <atomic-commerce-sort-dropdown></atomic-commerce-sort-dropdown>
+          </atomic-layout-section>
+          <atomic-layout-section section="products">
+            <atomic-commerce-product-list
+              display="grid"
+              density="compact"
+              image-size="small"
+            ></atomic-commerce-product-list>
+            <atomic-commerce-query-error></atomic-commerce-query-error>
+          </atomic-layout-section>
+          <atomic-layout-section section="pagination">
+            <atomic-commerce-pager></atomic-commerce-pager>
+            <atomic-commerce-products-per-page>
+            </atomic-commerce-products-per-page>
+          </atomic-layout-section>
+        </atomic-layout-section>
+      </atomic-commerce-layout>
+    `,
+  },
+};
