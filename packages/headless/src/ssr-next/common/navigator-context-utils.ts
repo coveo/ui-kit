33c88c68ccf2851d4ac9556373307698a8b04dc2
vhilotii-coveo <vhilotii@coveo.com>
2025-08-19 19:22:27 -0400

    enhance params type
    https://coveord.atlassian.net/browse/KIT-4616

--- packages/headless/src/ssr-next/common/navigator-context-utils.ts
+++ packages/headless/src/ssr-next/common/navigator-context-utils.ts
@@ -6,25 +6,27 @@ import type {
 } from '../../app/navigator-context-provider.js';
 import {augmentPreprocessRequestWithForwardedFor} from './augment-preprocess-request.js';
 
+type FetchStaticStateParams = [
+  {navigatorContext: NavigatorContext; controllers?: unknown},
+];
+
 /**
  * Processes fetchStaticState parameters and returns enhanced engine options with navigator context.
  * This is the single function that handles all navigator context logic for both commerce and search engines.
  */
 export function processNavigatorContext<
   TOptions extends {
-    configuration: object;
+    configuration: object & {preprocessRequest?: PreprocessRequest};
     loggerOptions?: LoggerOptions;
   },
 >(
-  params: unknown[],
+  params: FetchStaticStateParams,
   baseOptions: TOptions
 ): {
   engineOptions: TOptions;
   callOptions?: {navigatorContext: NavigatorContext; controllers?: unknown};
 } {
-  const [callOptions] = params as unknown as [
-    {navigatorContext: NavigatorContext; controllers?: unknown},
-  ];
+  const [callOptions] = params;
 
   // Navigator context is now required
   const navigatorContextProvider: NavigatorContextProvider = () =>
@@ -36,9 +38,7 @@ export function processNavigatorContext<
     configuration: {
       ...baseOptions.configuration,
       preprocessRequest: augmentPreprocessRequestWithForwardedFor({
-        preprocessRequest: (
-          baseOptions.configuration as {preprocessRequest?: PreprocessRequest}
-        )?.preprocessRequest,
+        preprocessRequest: baseOptions.configuration.preprocessRequest,
         navigatorContextProvider,
         loggerOptions: baseOptions.loggerOptions,
       }),
