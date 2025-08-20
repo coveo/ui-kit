61cabe8c7cadeb1c2fe7cbd20c8160d1b4a6559a
ylakhdar <ylakhdar@coveo.com>
2025-08-20 10:10:58 -0400

    update headless-react
    https://coveord.atlassian.net/browse/KIT-4781

--- packages/headless-react/src/ssr-commerce/providers.tsx
+++ packages/headless-react/src/ssr-commerce/providers.tsx
@@ -1,38 +1,19 @@
 'use client';
 
-import {
-  type Cart,
-  type Context,
-  type Controller,
-  type ControllerDefinitionsMap,
-  type ControllerWithKind,
-  type EngineStaticState,
-  type HydrateStaticStateOptions,
-  type InferControllerStaticStateMapFromDefinitionsWithSolutionType,
-  type InferControllersMapFromDefinition,
-  type InferHydratedState,
-  Kind,
-  type NavigatorContext,
-  type ParameterManager,
-  type Parameters,
-  type Recommendations,
-  type SolutionType,
+import type {
+  Controller,
+  ControllerDefinitionsMap,
+  EngineStaticState,
+  InferControllerStaticStateMapFromDefinitionsWithSolutionType,
+  InferControllersMapFromDefinition,
+  InferHydratedState,
+  NavigatorContext,
+  SolutionType,
 } from '@coveo/headless/ssr-commerce';
 import {type PropsWithChildren, useEffect, useState} from 'react';
 import type {ReactCommerceEngineDefinition} from './commerce-engine.js';
 
-type ControllerPropsMap = {[customName: string]: unknown};
 type UnknownAction = {type: string};
-
-function getController<T extends Controller>(
-  controllers: InferControllerStaticStateMapFromDefinitionsWithSolutionType<
-    ControllerDefinitionsMap<Controller>,
-    SolutionType
-  >,
-  key: string
-) {
-  return controllers[key] as T;
-}
 
 /**
  * Providers take care of displaying your page with the static
@@ -67,77 +48,22 @@ export function buildProviderWithDefinition<
     definition.setNavigatorContextProvider(() => navigatorContext);
 
     useEffect(() => {
-      const {searchActions, controllers} = staticState;
-      const hydrateArguments: ControllerPropsMap = {};
-
-      for (const [key, controller] of Object.entries(controllers)) {
-        const typedController = controller as ControllerWithKind;
-
-        // TODO: KIT-4742: remove state wiring as it is already done in headless
-        switch (typedController._kind) {
-          case Kind.Cart: {
-            const cart = getController<Cart>(controllers, key);
-            hydrateArguments[key] = {
-              initialState: {
-                items: cart.state.items,
-              },
-            };
-            break;
-          }
-          case Kind.Context: {
-            const context = getController<Context>(controllers, key);
-            hydrateArguments[key] = context.state;
-            break;
-          }
-
-          case Kind.ParameterManager: {
-            const parameterManager = getController<
-              ParameterManager<Parameters>
-            >(controllers, key);
-            hydrateArguments[key] = {
-              initialState: {
-                parameters: parameterManager.state.parameters,
-              },
-            };
-            break;
-          }
-          case Kind.Recommendations: {
-            const recommendations = getController<Recommendations>(
-              controllers,
-              key
-            );
-
-            hydrateArguments[key] = {
-              productId: recommendations.state.productId,
-            };
-            break;
-          }
-        }
-      }
-
-      const args: HydrateStaticStateOptions<UnknownAction> &
-        ControllerPropsMap = {
-        searchActions,
-      };
-
-      if (hydrateArguments) {
-        args.controllers = hydrateArguments;
-      }
-
       // @ts-expect-error Casting to loose definition since we don't need the inferred controllers here
       const looseDefinition = definition as ReactCommerceEngineDefinition<
         ControllerDefinitionsMap<Controller>,
         SolutionType
       >;
-      looseDefinition.hydrateStaticState(args).then(({engine, controllers}) => {
-        setHydratedState({
-          engine,
-          controllers: controllers as InferControllersMapFromDefinition<
-            TControllers,
-            TSolutionType
-          >,
+      looseDefinition
+        .hydrateStaticState(staticState)
+        .then(({engine, controllers}) => {
+          setHydratedState({
+            engine,
+            controllers: controllers as InferControllersMapFromDefinition<
+              TControllers,
+              TSolutionType
+            >,
+          });
         });
-      });
     }, [staticState, definition]);
 
     return (
