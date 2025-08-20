154db1521db1efdf9349a93b0641019ae335da77
ylakhdar <ylakhdar@coveo.com>
2025-08-20 08:33:26 -0400

    type cleaning
    https://coveord.atlassian.net/browse/KIT-4691

--- packages/headless/src/ssr-next/common/types/engine.ts
+++ packages/headless/src/ssr-next/common/types/engine.ts
@@ -1,18 +1,5 @@
 import type {UnknownAction} from '@reduxjs/toolkit';
-import type {CoreEngine, CoreEngineNext} from '../../../app/engine.js';
-import type {ControllerStaticStateMap, ControllersMap} from './controllers.js';
-
-// TODO: add deprecation on ssr/ package
-/**
- * @deprecated use either CommerceEngineDefinitionBuildResult or SearchEngineDefinitionBuildResult
- */
-export interface EngineDefinitionBuildResult<
-  TEngine extends CoreEngine | CoreEngineNext,
-  TControllers extends ControllersMap,
-> {
-  engine: TEngine;
-  controllers: TControllers;
-}
+import type {ControllerStaticStateMap} from './controllers.js';
 
 export interface EngineStaticState<
   TSearchAction extends UnknownAction,
--- packages/headless/src/ssr-next/common/types/hydrate-static-state.ts
+++
@@ -1,9 +0,0 @@
-// TODO: remove
-// import type {CoreEngine, CoreEngineNext} from '../../../app/engine.js';
-// import type {ControllersMap} from '../../common/types/controllers.js';
-// import type {EngineDefinitionBuildResult} from './engine.js';
-
-// export interface HydratedState<
-//   TEngine extends CoreEngine | CoreEngineNext,
-//   TControllers extends ControllersMap,
-// > extends EngineDefinitionBuildResult<TEngine, TControllers> {}
