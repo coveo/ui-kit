741a9133c3cd8d8be80029805f183d9a3d4d9957
Louis Bompart <lbompart@coveo.com>
2025-08-26 00:59:18 -0400

    fix(headless): ensure relay receive complete environment
    KIT-4831
    
    https://coveord.atlassian.net/browse/KIT-4831

--- packages/headless/src/api/analytics/analytics-relay-client.ts
+++ packages/headless/src/api/analytics/analytics-relay-client.ts
@@ -1,10 +1,15 @@
-import {createRelay} from '@coveo/relay';
+import {
+  buildBrowserEnvironment,
+  createRelay,
+  type Environment,
+} from '@coveo/relay';
 import {createSelector} from '@reduxjs/toolkit';
 import type {NavigatorContextProvider} from '../../app/navigator-context-provider.js';
 import type {
   CommerceConfigurationSection,
   ConfigurationSection,
 } from '../../state/state-sections.js';
+import {isBrowser} from '../../utils/runtime.js';
 import {getAnalyticsNextApiBaseUrl} from '../platform-client.js';
 import {getAnalyticsSource} from './analytics-selectors.js';
 
@@ -20,16 +25,17 @@ export const getRelayInstanceFromState = createSelector(
   (
     _state: StateNeededByRelay,
     navigatorContextProvider?: NavigatorContextProvider
-  ) => getEnvironment(navigatorContextProvider),
+  ) => navigatorContextProvider,
   (
     organizationId,
     platformEnvironment,
     token,
     {trackingId, apiBaseUrl, enabled},
     source,
-    environment
-  ) =>
-    createRelay({
+    navigatorContextProvider
+  ) => {
+    const environment = getEnvironment(navigatorContextProvider);
+    return createRelay({
       mode: enabled ? 'emit' : 'disabled',
       url:
         apiBaseUrl ??
@@ -38,20 +44,40 @@ export const getRelayInstanceFromState = createSelector(
       trackingId: trackingId ?? null,
       source,
       environment,
-    })
+    });
+  }
 );
 
-const getEnvironment = (customProvider?: NavigatorContextProvider) => {
+const noopRelayEnvironment: Environment = {
+  generateUUID: () => '',
+  getLocation: () => null,
+  getReferrer: () => null,
+  getUserAgent: () => null,
+  runtime: 'custom',
+  send: () => {},
+  storage: {
+    getItem: () => null,
+    setItem: () => {},
+    removeItem: () => {},
+  },
+};
+
+const getEnvironment = (
+  customProvider?: NavigatorContextProvider
+): Environment | undefined => {
   if (!customProvider) {
     return undefined;
   }
 
   const customContext = customProvider();
-
+  const baseEnvironment = isBrowser()
+    ? buildBrowserEnvironment()
+    : noopRelayEnvironment;
   return {
+    ...baseEnvironment,
+    generateUUID: () => customContext.clientId,
     getLocation: () => customContext.location,
     getReferrer: () => customContext.referrer,
     getUserAgent: () => customContext.userAgent,
-    generateUUID: () => customContext.clientId,
   };
 };
