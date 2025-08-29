5a32429984774c77fddc23df82f6ceaffb44c5a0
Louis Bompart <lbompart@coveo.com>
2025-08-29 16:59:16 -0400

    lab: msw setup/poc

--- packages/atomic/src/components/commerce/atomic-commerce-did-you-mean/atomic-commerce-did-you-mean.new.stories.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-did-you-mean/atomic-commerce-did-you-mean.new.stories.tsx
@@ -1,5 +1,6 @@
 import type {Meta, StoryObj as Story} from '@storybook/web-components-vite';
 import {getStorybookHelpers} from '@wc-toolkit/storybook-helpers';
+import {HttpResponse, http} from 'msw';
 import {wrapInCommerceInterface} from '@/storybook-utils/commerce/commerce-interface-wrapper';
 import {parameters} from '@/storybook-utils/common/common-meta-parameters';
 
@@ -30,10 +31,68 @@ const meta: Meta = {
     actions: {
       handles: events,
     },
+    msw: {
+      handlers: {
+        commerce: [
+          http.post(
+            'https://searchuisamples.org.coveo.com/rest/organizations/searchuisamples/commerce/v2/search',
+            () => {
+              return HttpResponse.json({
+                responseId: '3c0369c4-41ae-4f3c-8536-2840d1ac2214',
+                products: [],
+                facets: [],
+                pagination: {
+                  page: 0,
+                  perPage: 48,
+                  totalEntries: 0,
+                  totalPages: 0,
+                },
+                sort: {
+                  appliedSort: {
+                    sortCriteria: 'relevance',
+                  },
+                  availableSorts: [
+                    {
+                      sortCriteria: 'relevance',
+                    },
+                    {
+                      sortCriteria: 'fields',
+                      fields: [
+                        {
+                          field: 'ec_price',
+                          direction: 'asc',
+                          displayName: 'Price (Low to High)',
+                        },
+                      ],
+                    },
+                    {
+                      sortCriteria: 'fields',
+                      fields: [
+                        {
+                          field: 'ec_price',
+                          direction: 'desc',
+                          displayName: 'Price (High to Low)',
+                        },
+                      ],
+                    },
+                  ],
+                },
+                executionReport: null,
+                triggers: [],
+                queryCorrection: {
+                  originalQuery: 'runing shoes',
+                  correctedQuery: "I'm using MSW and it works great!",
+                  corrections: [],
+                },
+              });
+            }
+          ),
+        ],
+      },
+    },
   },
   args,
   argTypes,
-
   afterEach,
 };
 
