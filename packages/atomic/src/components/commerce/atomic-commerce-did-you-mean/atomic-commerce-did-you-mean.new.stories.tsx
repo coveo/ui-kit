c086bb62514e62e78f9786858f74b26118a53231
Louis Bompart <lbompart@coveo.com>
2025-08-29 18:57:16 -0400

    open-api based

--- packages/atomic/src/components/commerce/atomic-commerce-did-you-mean/atomic-commerce-did-you-mean.new.stories.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-did-you-mean/atomic-commerce-did-you-mean.new.stories.tsx
@@ -1,19 +1,79 @@
+import {fromOpenApi} from '@mswjs/source/open-api';
 import type {Meta, StoryObj as Story} from '@storybook/web-components-vite';
 import {getStorybookHelpers} from '@wc-toolkit/storybook-helpers';
-import {HttpResponse, http} from 'msw';
+import type {OpenAPIV3} from 'openapi-types';
+import openApiDocument from '@/oapi-specs/commerce.json' with {type: 'json'};
 import {wrapInCommerceInterface} from '@/storybook-utils/commerce/commerce-interface-wrapper';
 import {parameters} from '@/storybook-utils/common/common-meta-parameters';
 
-const {decorator, afterEach} = wrapInCommerceInterface({
-  engineConfig: {
-    preprocessRequest: (request) => {
-      const parsed = JSON.parse(request.body as string);
-      parsed.query = 'runing shoes';
-      request.body = JSON.stringify(parsed);
-      return request;
-    },
+const baseResponse = {
+  responseId: '3c0369c4-41ae-4f3c-8536-2840d1ac2214',
+  products: [],
+  facets: [],
+  pagination: {
+    page: 0,
+    perPage: 48,
+    totalEntries: 0,
+    totalPages: 0,
   },
+  sort: {
+    appliedSort: {
+      sortCriteria: 'relevance',
+    },
+    availableSorts: [
+      {
+        sortCriteria: 'relevance',
+      },
+      {
+        sortCriteria: 'fields',
+        fields: [
+          {
+            field: 'ec_price',
+            direction: 'asc',
+            displayName: 'Price (Low to High)',
+          },
+        ],
+      },
+      {
+        sortCriteria: 'fields',
+        fields: [
+          {
+            field: 'ec_price',
+            direction: 'desc',
+            displayName: 'Price (High to Low)',
+          },
+        ],
+      },
+    ],
+  },
+  executionReport: null,
+  triggers: [],
+};
+
+// Note: the OpenAPI document comes from https://platform.cloud.coveo.com/api-docs/CommerceService?group=public
+// But has been _slightly_ modified to change the Content-Type from '*/*' to 'application/json'
+Object.assign(
+  openApiDocument.paths[
+    '/rest/organizations/{organizationId}/commerce/v2/search'
+  ].post.responses[200].content['application/json'],
+  {
+    example: {
+      ...baseResponse,
+      queryCorrection: {
+        originalQuery: 'runing shoes',
+        correctedQuery: "I'm using MSW and it works great!",
+        corrections: [],
+      },
+    },
+  }
+);
+
+const handlers = await fromOpenApi({
+  ...(openApiDocument as OpenAPIV3.Document),
+  basePath: 'https://searchuisamples.org.coveo.com',
 });
+
+const {decorator, afterEach} = wrapInCommerceInterface();
 
 const {events, args, argTypes, template} = getStorybookHelpers(
   'atomic-commerce-did-you-mean',
@@ -32,63 +92,7 @@ const meta: Meta = {
       handles: events,
     },
     msw: {
-      handlers: {
-        commerce: [
-          http.post(
-            'https://searchuisamples.org.coveo.com/rest/organizations/searchuisamples/commerce/v2/search',
-            () => {
-              return HttpResponse.json({
-                responseId: '3c0369c4-41ae-4f3c-8536-2840d1ac2214',
-                products: [],
-                facets: [],
-                pagination: {
-                  page: 0,
-                  perPage: 48,
-                  totalEntries: 0,
-                  totalPages: 0,
-                },
-                sort: {
-                  appliedSort: {
-                    sortCriteria: 'relevance',
-                  },
-                  availableSorts: [
-                    {
-                      sortCriteria: 'relevance',
-                    },
-                    {
-                      sortCriteria: 'fields',
-                      fields: [
-                        {
-                          field: 'ec_price',
-                          direction: 'asc',
-                          displayName: 'Price (Low to High)',
-                        },
-                      ],
-                    },
-                    {
-                      sortCriteria: 'fields',
-                      fields: [
-                        {
-                          field: 'ec_price',
-                          direction: 'desc',
-                          displayName: 'Price (High to Low)',
-                        },
-                      ],
-                    },
-                  ],
-                },
-                executionReport: null,
-                triggers: [],
-                queryCorrection: {
-                  originalQuery: 'runing shoes',
-                  correctedQuery: "I'm using MSW and it works great!",
-                  corrections: [],
-                },
-              });
-            }
-          ),
-        ],
-      },
+      handlers,
     },
   },
   args,
