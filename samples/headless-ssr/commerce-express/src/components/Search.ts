c0dcec71d37589a7669e041f458abecdaa17705a
vhilotii-coveo <vhilotii@coveo.com>
2025-08-29 17:47:11 -0400

    update search component
    https://coveord.atlassian.net/browse/KIT-4689

--- samples/headless-ssr/commerce-express/src/components/Search.ts
+++ samples/headless-ssr/commerce-express/src/components/Search.ts
@@ -5,14 +5,20 @@ export function Search(searchBox: SearchBox) {
   if (!searchBox) return;
 
   const input = getElement<HTMLInputElement>('search-input');
-  const button = getElement<HTMLButtonElement>('search-button');
-  if (!input || !button) return;
+  if (!input) return;
+  const form = input.form;
+  if (!form) return;
 
   // Initial value
   input.value = selectSearchValue(searchBox);
-
   searchBox.subscribe(() => {
     input.value = selectSearchValue(searchBox);
+  });
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchBox.updateText(input.value);
+    searchBox.submit();
   });
 }
 
