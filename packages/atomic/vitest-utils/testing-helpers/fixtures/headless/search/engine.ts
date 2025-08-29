a17b1955e79c7744548773dc6810e4ee32f83e23
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-08-29 09:48:07 -0400

    tests
    https://coveord.atlassian.net/browse/KIT-3909

---
+++ packages/atomic/vitest-utils/testing-helpers/fixtures/headless/search/engine.ts
@@ -0,0 +1,30 @@
+import type {SearchEngine, SearchEngineConfiguration} from '@coveo/headless';
+import {vi} from 'vitest';
+
+export const buildFakeSearchEngine = ({
+  implementation,
+  state,
+}: Partial<{
+  implementation?: Partial<SearchEngine>;
+  config?: Partial<SearchEngineConfiguration>;
+  state?: Record<string, unknown>;
+}> = {}): SearchEngine => {
+  const defaultState = {};
+  const defaultImplementation = {
+    dispatch: vi.fn(),
+    logger: {
+      info: vi.fn(),
+      warn: vi.fn(),
+      error: vi.fn(),
+      debug: vi.fn(),
+    },
+    executeFirstSearch: vi.fn(),
+    executeFirstSearchAfterStandaloneSearchBoxRedirect: vi.fn(),
+  };
+
+  return {
+    ...defaultImplementation,
+    ...implementation,
+    state: {...defaultState, ...state},
+  } as SearchEngine;
+};
