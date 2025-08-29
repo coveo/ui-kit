a2b5b5c15c1ca688a8331e6f52f1d0be7ba895cc
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-08-29 08:21:58 -0400

    chore(atomic): migrate atomic-search-interface
    https://coveord.atlassian.net/browse/KIT-3909

--- packages/atomic/src/components/search/index.ts
+++ packages/atomic/src/components/search/index.ts
@@ -1,4 +1,5 @@
 // Auto-generated file
 export {AtomicQuerySummary} from './atomic-query-summary/atomic-query-summary.js';
 export {AtomicResultsPerPage} from './atomic-results-per-page/atomic-results-per-page.js';
+export {AtomicSearchInterface} from './atomic-search-interface/atomic-search-interface.js';
 export {AtomicSearchLayout} from './atomic-search-layout/atomic-search-layout.js';
