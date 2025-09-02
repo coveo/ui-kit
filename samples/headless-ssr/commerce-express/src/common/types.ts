4747b63a0159ae448e60e15048288c2413b7ee68
vhilotii-coveo <vhilotii@coveo.com>
2025-09-02 14:22:35 -0400

    cleanup unused export
    https://coveord.atlassian.net/browse/KIT-4689

--- samples/headless-ssr/commerce-express/src/common/types.ts
+++ samples/headless-ssr/commerce-express/src/common/types.ts
@@ -1,13 +1,7 @@
-import type {
-  InferHydratedState,
-  InferStaticState,
-} from '@coveo/headless/ssr-commerce-next';
+import type {InferStaticState} from '@coveo/headless/ssr-commerce-next';
 import type {searchEngineDefinition} from '../lib/engine-definition';
 
 export type SearchStaticState = InferStaticState<typeof searchEngineDefinition>;
-export type SearchHydratedState = InferHydratedState<
-  typeof searchEngineDefinition
->;
 
 declare global {
   interface Window {
