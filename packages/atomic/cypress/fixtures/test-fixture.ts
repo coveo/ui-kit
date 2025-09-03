4f134c585b677c1ac4732c401d563d5b6513ee17
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-09-03 15:12:04 -0400

    Update test-fixture.ts
    https://coveord.atlassian.net/browse/KIT-3909

--- packages/atomic/cypress/fixtures/test-fixture.ts
+++ packages/atomic/cypress/fixtures/test-fixture.ts
@@ -211,7 +211,9 @@ export class TestFixture {
     });
 
     cy.document().then((doc) => {
+      this.style = doc.importNode(this.style, true);
       doc.head.appendChild(this.style);
+      this.searchInterface = doc.importNode(this.searchInterface, true);
       doc.body.appendChild(this.searchInterface);
       cy.get('atomic-search-interface').as(this.elementAliases.SearchInterface);
     });
