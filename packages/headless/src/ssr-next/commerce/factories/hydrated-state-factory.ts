1274d033ea02bbbd92b4337eeac1a40a02250b4b
ylakhdar <ylakhdar@coveo.com>
2025-09-03 10:44:11 -0400

    chore(headless-ssr): remove unnecessary casts (#5965)
    ## Just Type cleanup
    
    The factory chain was losing type information about augmented controller
    definitions forcing to use casts a bit everywhere.
    
    ### Reasons
    * Static type errors in SSR commerce factories due to missing baked-in
    controllers (`parameterManager`, `context`, `cart`) in return types.
    * Multiple types and functions were using a generic `SolutionType`
    instead of the specific solution type parameter
    * `HydrateStaticStateParameters` was not naturally compatible with the
    buildFactory because of the additional Search action
    
    ### Additional Changes
    * Getting rid of unused internal types
    * Moving the dispatching logic (which is common to static and hydrate
    factories) into the build factory
    
    https://coveord.atlassian.net/browse/KIT-4834
    
    ---------
    
    Co-authored-by: Frederic Beaudoin <fbeaudoin@coveo.com>

--- packages/headless/src/ssr-next/commerce/factories/hydrated-state-factory.ts
+++ packages/headless/src/ssr-next/commerce/factories/hydrated-state-factory.ts
@@ -1,15 +1,8 @@
 import type {SolutionType} from '../types/controller-constants.js';
+import type {AugmentedControllerDefinition} from '../types/controller-definitions.js';
 import type {
-  AugmentedControllerDefinition,
-  FilteredBakedInControllers,
-  HydratedState,
-} from '../types/controller-definitions.js';
-import type {InferControllersMapFromDefinition} from '../types/controller-inference.js';
-import type {
-  BuildParameters,
   CommerceControllerDefinitionsMap,
   CommerceEngineDefinitionOptions,
-  HydrateStaticStateFunction,
   HydrateStaticStateParameters,
 } from '../types/engine.js';
 import {buildFactory} from './build-factory.js';
@@ -20,30 +13,21 @@ export function hydratedStaticStateFactory<
   controllerDefinitions: AugmentedControllerDefinition<TControllerDefinitions>,
   options: CommerceEngineDefinitionOptions<TControllerDefinitions>
 ) {
-  return (
-    solutionType: SolutionType
-  ): HydrateStaticStateFunction<TControllerDefinitions> =>
-    async (...params: HydrateStaticStateParameters<TControllerDefinitions>) => {
+  return <TSolutionType extends SolutionType>(solutionType: TSolutionType) =>
+    async (
+      ...params: HydrateStaticStateParameters<
+        TControllerDefinitions,
+        TSolutionType
+      >
+    ) => {
       const solutionTypeBuild = await buildFactory(
         controllerDefinitions,
         options
       )(solutionType);
-      const {engine, controllers} = await solutionTypeBuild(
-        ...(params as BuildParameters<TControllerDefinitions>)
-      );
-
-      params[0]!.searchActions.forEach((action) => {
-        engine.dispatch(action);
-      });
+      const {engine, controllers} = await solutionTypeBuild(...params);
 
       await engine.waitForRequestCompletedAction();
 
-      return {engine, controllers} as HydratedState<
-        InferControllersMapFromDefinition<
-          TControllerDefinitions,
-          SolutionType
-        > &
-          FilteredBakedInControllers<SolutionType>
-      >;
+      return {engine, controllers};
     };
 }
