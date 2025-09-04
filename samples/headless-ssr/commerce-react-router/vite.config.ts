d5a169bbeb729277dad2cbbc9ff05017f1671608
fbeaudoincoveo <fbeaudoin@coveo.com>
2025-09-04 10:12:03 -0400

    Fix dev server
    https://coveord.atlassian.net/browse/KIT-4761

--- samples/headless-ssr/commerce-react-router/vite.config.ts
+++ samples/headless-ssr/commerce-react-router/vite.config.ts
@@ -3,9 +3,21 @@ import {defineConfig} from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouter(),
+    tsconfigPaths({
+      root: './',
+      projects: ['./tsconfig.json'],
+    }),
+  ],
   build: {
     target: 'esnext',
     outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      // Force this sample to use the correct react-router version
+      'react-router-dom': 'react-router/dom',
+    },
   },
 });
