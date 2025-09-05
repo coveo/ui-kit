2b929ae3c31feba338737515669adce63eedcfcd
fbeaudoincoveo <fbeaudoin@coveo.com>
2025-09-05 14:10:29 -0400

    Order projects in alphabetical order
    https://coveord.atlassian.net/browse/KIT-4911

--- vitest.config.js
+++ vitest.config.js
@@ -3,10 +3,10 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
   test: {
     projects: [
-      './packages/headless/vitest.config.js',
-      './packages/headless-react/vitest.config.js',
       './packages/atomic/vitest.config.js',
       './packages/shopify/vitest.config.js',
+      './packages/headless/vitest.config.js',
+      './packages/headless-react/vitest.config.js',
       './samples/headless/commerce-react/vitest.config.js',
       './samples/headless/search-react/vitest.config.js',
     ],
