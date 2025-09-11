166ffdfd4ac08b35aa458111c37fa1140d5532a2
Louis Bompart <lbompart@coveo.com>
2025-09-11 10:50:13 -0400

    chore(headless-ssr): replace single search action to search action array (#6026)
    Co-authored-by: ylakhdar <ylakhdar@coveo.com>
    Co-authored-by: Frederic Beaudoin <fbeaudoin@coveo.com>

--- packages/headless/src/ssr-next/search/types/hydrate-static-state.ts
+++ packages/headless/src/ssr-next/search/types/hydrate-static-state.ts
@@ -16,7 +16,7 @@ export type HydratedState<
 > = SearchEngineDefinitionBuildResult<TEngine, TControllers>;
 
 interface HydrateStaticStateOptions<TSearchAction> {
-  searchAction: TSearchAction;
+  searchActions: TSearchAction[];
 }
 
 /**
