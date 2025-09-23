1d1ecdfe8e9d434840ed1e4fb49a08d874807bcb
vhilotii-coveo <vhilotii@coveo.com>
2025-09-23 16:55:09 -0400

    update index & lazy-index
    https://coveord.atlassian.net/browse/KIT-4936

--- packages/atomic/src/components/search/index.ts
+++ packages/atomic/src/components/search/index.ts
@@ -2,3 +2,4 @@
 export {AtomicQuerySummary} from './atomic-query-summary/atomic-query-summary.js';
 export {AtomicResultsPerPage} from './atomic-results-per-page/atomic-results-per-page.js';
 export {AtomicSearchLayout} from './atomic-search-layout/atomic-search-layout.js';
+export {AtomicSearchBoxQuerySuggestions} from './search-box-suggestions/atomic-search-box-query-suggestions/atomic-search-box-query-suggestions.js';
--- packages/atomic/src/components/search/lazy-index.ts
+++ packages/atomic/src/components/search/lazy-index.ts
@@ -4,6 +4,10 @@ export default {
     await import('./atomic-query-summary/atomic-query-summary.js'),
   'atomic-results-per-page': async () =>
     await import('./atomic-results-per-page/atomic-results-per-page.js'),
+  'atomic-search-box-query-suggestions': async () =>
+    await import(
+      './search-box-suggestions/atomic-search-box-query-suggestions/atomic-search-box-query-suggestions.js'
+    ),
   'atomic-search-layout': async () =>
     await import('./atomic-search-layout/atomic-search-layout.js'),
 } as Record<string, () => Promise<unknown>>;
