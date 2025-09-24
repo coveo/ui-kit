73e65ae882f4c685f9a0d9df59620c604c9e78f8
Victor H. <vhilotii@coveo.com>
2025-09-24 12:32:56 -0400

    chore(atomic): refactor suggestions-common.ts (#6068)
    Co-authored-by: Etienne Rocheleau <erocheleau@coveo.com>

--- packages/atomic/src/components/common/suggestions/suggestions-types.ts
+++ packages/atomic/src/components/common/suggestions/suggestions-types.ts
@@ -1,8 +1,4 @@
 import type {VNode} from '@stencil/core';
-import type {HTMLStencilElement} from '@stencil/core/internal';
-import type {LitElement} from 'lit';
-import {closest} from '../../../utils/dom-utils';
-import {buildCustomEvent} from '../../../utils/event-utils';
 import type {AnyBindings} from '../interface/bindings';
 
 /**
@@ -76,25 +72,6 @@ export interface SearchBoxSuggestions {
 }
 
 /**
- * Event sent from the registered query suggestions to the parent search box.
- */
-export type SearchBoxSuggestionsEvent<
-  SearchBoxController,
-  Bindings = AnyBindings,
-> = (
-  /**
-   * The bindings passed from the search box to the suggestions.
-   */
-  bindings: SearchBoxSuggestionsBindings<SearchBoxController, Bindings>
-) => SearchBoxSuggestions;
-
-const searchBoxElements = [
-  'atomic-search-box',
-  'atomic-insight-search-box',
-  'atomic-commerce-search-box',
-] as const;
-
-/**
  * The bindings passed from the search box to the suggestions.
  */
 export type SearchBoxSuggestionsBindings<
@@ -144,61 +121,15 @@ export type SearchBoxSuggestionsBindings<
   getSuggestionElements(): SearchBoxSuggestionElement[];
 };
 
-const isLitElementLoosely = (candidate: unknown): candidate is LitElement =>
-  'updateComplete' in (candidate as LitElement) &&
-  (candidate as LitElement).updateComplete instanceof Promise;
-
-const dispatchSearchBoxSuggestionsEventEventually = async <
-  SearchBoxController,
-  Bindings = AnyBindings,
->(
-  interfaceElement: Element,
-  element: HTMLElement,
-  event: SearchBoxSuggestionsEvent<SearchBoxController, Bindings>
-) => {
-  await customElements.whenDefined(interfaceElement.nodeName.toLowerCase());
-  if (isLitElementLoosely(interfaceElement)) {
-    await interfaceElement.updateComplete;
-  } else if ('componentOnReady' in interfaceElement) {
-    await (interfaceElement as HTMLStencilElement).componentOnReady();
-  }
-  element.dispatchEvent(
-    buildCustomEvent('atomic/searchBoxSuggestion/register', event)
-  );
-};
-
 /**
- * Dispatches an event which retrieves the `SearchBoxSuggestionsBindings` on a configured parent search box.
- * @param event Event sent from the registered query suggestions to the parent search box.
- * @param element Element on which to dispatch the event, which must be the child of a configured search box.
+ * Event sent from the registered query suggestions to the parent search box.
  */
-export const dispatchSearchBoxSuggestionsEvent = <
+export type SearchBoxSuggestionsEvent<
   SearchBoxController,
   Bindings = AnyBindings,
->(
-  event: SearchBoxSuggestionsEvent<SearchBoxController, Bindings>,
-  element: HTMLElement,
-  allowedSearchBoxElements: readonly (typeof searchBoxElements)[number][] = searchBoxElements
-) => {
-  const interfaceElement = closest(element, searchBoxElements.join(', '));
-  if (!interfaceElement) {
-    throw new Error(
-      `The "${element.nodeName.toLowerCase()}" component was not handled, as it is not a child of the following elements: ${allowedSearchBoxElements.join(
-        ', '
-      )}`
-    );
-  }
-  void dispatchSearchBoxSuggestionsEventEventually(
-    interfaceElement,
-    element,
-    event
-  );
-};
-
-export function elementHasNoQuery(el: SearchBoxSuggestionElement) {
-  return !el.query || el.query.trim() === '';
-}
-
-export function elementHasQuery(el: SearchBoxSuggestionElement) {
-  return !!el.query && el.query.trim() !== '';
-}
+> = (
+  /**
+   * The bindings passed from the search box to the suggestions.
+   */
+  bindings: SearchBoxSuggestionsBindings<SearchBoxController, Bindings>
+) => SearchBoxSuggestions;
