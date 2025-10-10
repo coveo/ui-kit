d8ecc72da056ec986734471a5d2da217254d7402
Louis Bompart <lbompart@coveo.com>
2025-10-10 00:50:55 -0400

    test: setup storybook-vitest

--- packages/atomic/.storybook/preview.ts
+++ packages/atomic/.storybook/preview.ts
@@ -80,6 +80,13 @@ export const parameters: Parameters = {
   controls: {
     expanded: true,
   },
+
+  a11y: {
+    // 'todo' - show a11y violations in the test UI only
+    // 'error' - fail CI on a11y violations
+    // 'off' - skip a11y checks entirely
+    test: 'todo',
+  },
 };
 
 export const decorators = [
