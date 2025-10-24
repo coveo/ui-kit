ca4f841a4dc7479087238f6aa6476d3e097f9b73
Felix Perron-Brault <fpbrault@coveo.com>
2025-10-24 09:34:20 -0400

    remove cypress tests
    https://coveord.atlassian.net/browse/KIT-5056

--- packages/atomic/src/components/search/atomic-result-list/e2e/atomic-result-list.e2e.ts
+++ packages/atomic/src/components/search/atomic-result-list/e2e/atomic-result-list.e2e.ts
@@ -5,6 +5,11 @@ test.describe('When using a list layout', () => {
     await resultList.load();
   });
 
+  test('should be accessible', async ({makeAxeBuilder}) => {
+    const accessibilityResults = await makeAxeBuilder().analyze();
+    expect(accessibilityResults.violations).toEqual([]);
+  });
+
   test.describe('when clicking a result', () => {
     test.beforeEach(async ({result}) => {
       await result.hydrated.first().click();
@@ -31,5 +36,16 @@ test.describe('When using a grid layout', () => {
         .poll(() => page.url())
         .toContain('https://docs.coveo.com/en/3160');
     });
+  });
+});
+
+test.describe('When using a table layout', () => {
+  test.beforeEach(async ({resultList}) => {
+    await resultList.load({story: 'table-display'});
+  });
+
+  test('should be accessible', async ({makeAxeBuilder}) => {
+    const accessibilityResults = await makeAxeBuilder().analyze();
+    expect(accessibilityResults.violations).toEqual([]);
   });
 });
