155d880135ccea2201faa62e812626559b909d69
Felix Perron-Brault <fpbrault@coveo.com>
2025-10-24 09:48:44 -0400

    add ttests
    https://coveord.atlassian.net/browse/KIT-5056

--- packages/atomic/src/components/search/atomic-result-list/e2e/atomic-result-list.e2e.ts
+++ packages/atomic/src/components/search/atomic-result-list/e2e/atomic-result-list.e2e.ts
@@ -38,7 +38,6 @@ test.describe('When using a grid layout', () => {
     });
   });
 });
-
 test.describe('When using a table layout', () => {
   test.beforeEach(async ({resultList}) => {
     await resultList.load({story: 'table-display'});
@@ -47,5 +46,15 @@ test.describe('When using a table layout', () => {
   test('should be accessible', async ({makeAxeBuilder}) => {
     const accessibilityResults = await makeAxeBuilder().analyze();
     expect(accessibilityResults.violations).toEqual([]);
+  });
+
+  test.describe('when clicking a result', () => {
+    test.beforeEach(async ({result}) => {
+      await result.hydrated.first().click();
+    });
+
+    test('should not navigate', async ({page}) => {
+      expect(page.url()).toContain('/iframe.html');
+    });
   });
 });
