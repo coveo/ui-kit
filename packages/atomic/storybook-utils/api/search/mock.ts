a9ce1b519cd4a014fd74727753c1b30f6f8dbb79
Louis Bompart <lbompart@coveo.com>
2025-10-27 06:11:53 -0400

    chore: move remaining sapi to new pattern

--- packages/atomic/storybook-utils/api/search/mock.ts
+++ packages/atomic/storybook-utils/api/search/mock.ts
@@ -1,18 +1,23 @@
 import type {HttpHandler} from 'msw';
 import {EndpointHarness, type MockApi} from '../_base.js';
+import type {APIErrorWithStatusCode} from '../_common/error.js';
 import {baseResponse as baseQuerySuggestResponse} from './querySuggest-response.js';
-import {baseResponse as baseSearchResponse} from './search-response.js';
+import {
+  type baseFoldedResponse,
+  baseResponse as baseSearchResponse,
+} from './search-response.js';
 
 export class MockSearchApi implements MockApi {
   readonly searchEndpoint;
   readonly querySuggestEndpoint;
 
   constructor(basePath: string = 'https://:orgId.org.coveo.com') {
-    this.searchEndpoint = new EndpointHarness(
-      'POST',
-      `${basePath}/rest/search/v2`,
-      baseSearchResponse
-    );
+    //TODO: Fix type after encoding Search API response typing properly.
+    this.searchEndpoint = new EndpointHarness<
+      | typeof baseSearchResponse
+      | typeof baseFoldedResponse
+      | APIErrorWithStatusCode
+    >('POST', `${basePath}/rest/search/v2`, baseSearchResponse);
     this.querySuggestEndpoint = new EndpointHarness(
       'POST',
       `${basePath}/rest/search/v2/querySuggest`,
