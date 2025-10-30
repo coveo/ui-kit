4cb994f87467b15b11bad6401a417954cf91431f
Felix Perron-Brault <fpbrault@coveo.com>
2025-10-30 11:26:41 -0400

    revert change
    https://coveord.atlassian.net/browse/KIT-5056

--- packages/atomic/src/components/search/atomic-generated-answer/e2e/fixture.ts
+++ packages/atomic/src/components/search/atomic-generated-answer/e2e/fixture.ts
@@ -1,6 +1,6 @@
 import {test as base} from '@playwright/test';
 import {type AxeFixture, makeAxeBuilder} from '@/playwright-utils/base-fixture';
-import {SearchBoxPageObject as SearchBox} from '@/src/components/search/atomic-search-box/e2e/page-object';
+import {SearchBoxPageObject as SearchBox} from '../../atomic-search-box/e2e/page-object';
 import {GeneratedAnswerPageObject as GeneratedAnswer} from './page-object';
 
 type MyFixtures = {
