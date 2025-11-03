c1f39285e952669cce18c6d915c8f6204b333e76
simonmilord <simon.milord@gmail.com>
2025-11-03 08:42:32 -0500

    added hide when query is not empty

--- packages/atomic/src/components/insight/atomic-insight-generate-answer-button/atomic-insight-generate-answer-button.ts
+++ packages/atomic/src/components/insight/atomic-insight-generate-answer-button/atomic-insight-generate-answer-button.ts
@@ -1,7 +1,10 @@
+import type {QuerySummary, QuerySummaryState} from '@coveo/headless/insight';
+import {buildQuerySummary} from '@coveo/headless/insight';
 import {html, LitElement} from 'lit';
 import {customElement, property, state} from 'lit/decorators.js';
 import {renderIconButton} from '@/src/components/common/icon-button';
 import type {InsightBindings} from '@/src/components/insight/atomic-insight-interface/atomic-insight-interface';
+import {bindStateToController} from '@/src/decorators/bind-state.js';
 import {bindingGuard} from '@/src/decorators/binding-guard.js';
 import {bindings} from '@/src/decorators/bindings.js';
 import {errorGuard} from '@/src/decorators/error-guard';
@@ -21,18 +24,29 @@ export class AtomicInsightGenerateAnswerButton
   @state() bindings!: InsightBindings;
   @state() error!: Error;
 
+  public querySummary!: QuerySummary;
+
+  @bindStateToController('querySummary')
+  @state()
+  private querySummaryState!: QuerySummaryState;
+
   /**
    * The tooltip text for the generate answer button.
    */
   @property({type: String, attribute: 'tooltip'}) tooltip = 'Generate Answer';
 
   public initialize() {
-    // No specific initialization needed for this placeholder component.
+    this.querySummary = buildQuerySummary(this.bindings.engine);
   }
 
   @errorGuard()
   @bindingGuard()
   render() {
+    const {hasQuery} = this.querySummaryState;
+    if (hasQuery) {
+      return html``;
+    }
+
     return html`
       ${renderIconButton({
         props: {
