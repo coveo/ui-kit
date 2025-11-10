5a93fff1bc797cb606ae7d04c2b0ab4c7a69e8c1
Thomas Baxter <tbaxter@coveo.com>
2025-11-10 13:17:46 -0500

    feat: migrating handwritten documentation from docs.coveo.com

--- packages/atomic/src/components/search/atomic-breadbox/atomic-breadbox.tsx
+++ packages/atomic/src/components/search/atomic-breadbox/atomic-breadbox.tsx
@@ -26,7 +26,10 @@ import {Hidden} from '../../common/stencil-hidden';
 import {Bindings} from '../atomic-search-interface/atomic-search-interface';
 
 /**
- * The `atomic-breadbox` component creates breadcrumbs that display a summary of the currently active facet values.
+ * The `atomic-breadbox` component helps the user keep track of the navigational state of the currently active facet values, located in a single place on the search page.
+ * In most cases, these are on the top of a page between the search bar and the results.
+ * By default, the field name is displayed before the field value to clarify which facet the value is from.
+ * You can clear a single selection by clicking the `x` inside of each pill, or clear all selections by using the `Clear` button.
  *
  * @part container - The container of the whole component, list & label.
  * @part breadcrumb-list-container - The container of the list of breadcrumb buttons.
