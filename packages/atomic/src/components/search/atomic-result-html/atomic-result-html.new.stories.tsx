4e0e78e2f7693afc31d2a68df734b595f7f0a64b
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-11-12 11:12:49 -0500

    Update atomic-result-html.new.stories.tsx

--- packages/atomic/src/components/search/atomic-result-html/atomic-result-html.new.stories.tsx
+++ packages/atomic/src/components/search/atomic-result-html/atomic-result-html.new.stories.tsx
@@ -1,34 +1,43 @@
 import type {Meta, StoryObj as Story} from '@storybook/web-components-vite';
 import {getStorybookHelpers} from '@wc-toolkit/storybook-helpers';
 import {parameters} from '@/storybook-utils/common/common-meta-parameters';
-import {wrapInResult} from '@/storybook-utils/search/result-wrapper';
+import {wrapInResultList} from '@/storybook-utils/search/result-list-wrapper';
+import {wrapInResultTemplate} from '@/storybook-utils/search/result-template-wrapper';
 import {wrapInSearchInterface} from '@/storybook-utils/search/search-interface-wrapper';
+
+const {decorator: searchInterfaceDecorator, play} = wrapInSearchInterface({
+  config: {
+    search: {
+      preprocessSearchResponseMiddleware: (res) => {
+        res.body.results = res.body.results.slice(0, 1);
+        res.body.results.forEach((r) => {
+          r.excerpt = '<div>Some HTML content</div>';
+        });
+        return res;
+      },
+    },
+  },
+  includeCodeRoot: false,
+});
+
+const {decorator: resultListDecorator} = wrapInResultList('list', false);
+const {decorator: resultTemplateDecorator} = wrapInResultTemplate();
 
 const {events, args, argTypes, template} = getStorybookHelpers(
   'atomic-result-html',
   {excludeCategories: ['methods']}
 );
 
-const {decorator: resultDecorator, engineConfig} = wrapInResult({
-  search: {
-    preprocessSearchResponseMiddleware: (res) => {
-      res.body.results.forEach((r) => {
-        r.excerpt = '<div>Some HTML content</div>';
-      });
-      return res;
-    },
-  },
-});
-const {decorator: searchInterfaceDecorator, play} = wrapInSearchInterface({
-  config: engineConfig,
-});
-
 const meta: Meta = {
   component: 'atomic-result-html',
-  title: 'Search/ResultList/ResultHtml',
+  title: 'Search/Result HTML',
   id: 'atomic-result-html',
   render: (args) => template(args),
-  decorators: [resultDecorator, searchInterfaceDecorator],
+  decorators: [
+    resultTemplateDecorator,
+    resultListDecorator,
+    searchInterfaceDecorator,
+  ],
   parameters: {
     ...parameters,
     actions: {
@@ -44,6 +53,5 @@ const meta: Meta = {
 export default meta;
 
 export const Default: Story = {
-  name: 'atomic-result-html',
   args: {field: 'excerpt'},
 };
