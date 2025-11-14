dd456bc4d433bb534ebd22daf7d22e4707366552
fbeaudoincoveo <fbeaudoin@coveo.com>
2025-11-14 16:00:08 -0500

    Fix BasePageObject import

--- packages/atomic/src/components/search/atomic-color-facet/e2e/page-object.ts
+++ packages/atomic/src/components/search/atomic-color-facet/e2e/page-object.ts
@@ -1,7 +1,7 @@
 import type {Page} from '@playwright/test';
-import {BasePageObject} from '@/playwright-utils/base-page-object';
+import {BasePageObject} from '@/playwright-utils/lit-base-page-object';
 
-export class AtomicColorFacetPageObject extends BasePageObject<'atomic-color-facet'> {
+export class AtomicColorFacetPageObject extends BasePageObject {
   constructor(page: Page) {
     super(page, 'atomic-color-facet');
   }
