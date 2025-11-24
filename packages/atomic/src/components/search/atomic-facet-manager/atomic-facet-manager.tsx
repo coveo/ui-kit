855981cba8e4921260010d9ebe43a2878dbf17d9
Thomas Baxter <tbaxter@coveo.com>
2025-11-24 14:22:09 -0500

    chore: addressing feedback

--- packages/atomic/src/components/search/atomic-facet-manager/atomic-facet-manager.tsx
+++ packages/atomic/src/components/search/atomic-facet-manager/atomic-facet-manager.tsx
@@ -22,9 +22,9 @@ import {Bindings} from '../atomic-search-interface/atomic-search-interface';
 /**
  * The `atomic-facet-manager` is a component that manages facets by performing three key functions:
  *
- * **Sorting facets** - Reorders facets based on the search response to show the most relevant facets first.
- * **Managing visibility** - Controls which facets should be visible or hidden based on available values and dependencies.
- * **Managing collapse state** - Automatically expands or collapses facets based on the `collapse-facets-after` property.
+ * 1. **Sorting facets** - Reorders facets based on the search response to show the most relevant facets first.
+ * 1. **Managing visibility** - Controls which facets should be visible or hidden based on available values and dependencies.
+ * 1. **Managing collapse state** - Automatically expands or collapses facets based on the `collapse-facets-after` property.
  *
  * @slot default - Facet components are slotted within to leverage this functionality.
  *
