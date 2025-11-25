39e75c7c8f4a907158648e7aba0fe4c8d1cbea71
Jad Mazzah <jmazzah@coveo.com>
2025-11-25 10:56:39 -0500

    lint error fix

--- packages/atomic/src/components/search/atomic-segmented-facet-scrollable/atomic-segmented-facet-scrollable.new.stories.tsx
+++ packages/atomic/src/components/search/atomic-segmented-facet-scrollable/atomic-segmented-facet-scrollable.new.stories.tsx
@@ -3,11 +3,12 @@ import type {
   Meta,
   StoryObj as Story,
 } from '@storybook/web-components-vite';
-import {html} from 'lit';
 import {getStorybookHelpers} from '@wc-toolkit/storybook-helpers';
+import {html} from 'lit';
 import {MockSearchApi} from '@/storybook-utils/api/search/mock';
 import {parameters} from '@/storybook-utils/common/common-meta-parameters';
 import {wrapInSearchInterface} from '@/storybook-utils/search/search-interface-wrapper';
+
 const {events, args, argTypes} = getStorybookHelpers(
   'atomic-segmented-facet-scrollable',
   {excludeCategories: ['methods']}
