ae32cabe3b6b7b52282cc3d4c90108e166dd6185
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-12-09 09:29:12 -0500

    done

--- packages/atomic/src/components/ipx/atomic-ipx-tabs/atomic-ipx-tabs.ts
+++ packages/atomic/src/components/ipx/atomic-ipx-tabs/atomic-ipx-tabs.ts
@@ -3,33 +3,33 @@ import {customElement, state} from 'lit/decorators.js';
 import type {Bindings} from '@/src/components/search/atomic-search-interface/atomic-search-interface';
 import {bindings} from '@/src/decorators/bindings';
 import type {InitializableComponent} from '@/src/decorators/types';
+import {LightDomMixin} from '@/src/mixins/light-dom';
+import {SlotsForNoShadowDOMMixin} from '../../../mixins/slots-for-no-shadow-dom-mixin';
 
 /**
  * @internal
  * The `atomic-ipx-tabs` component is a wrapper around `atomic-tab-bar` for IPX interfaces.
  *
- * @slot (default) - The tab elements to display within the tab bar.
+ * @slot default - The tab elements to display within the tab bar.
  */
 @customElement('atomic-ipx-tabs')
 @bindings()
-export class AtomicIPXTabs
-  extends LitElement
+export class AtomicIpxTabs
+  extends LightDomMixin(SlotsForNoShadowDOMMixin(LitElement))
   implements InitializableComponent<Bindings>
 {
   @state() public bindings!: Bindings;
   @state() public error!: Error;
 
-  public initialize() {
-    // No initialization needed for this wrapper component
-  }
+  public initialize() {}
 
   render() {
-    return html` <atomic-tab-bar> <slot></slot> </atomic-tab-bar> `;
+    return html`<atomic-tab-bar>${this.renderDefaultSlotContent()}</atomic-tab-bar>`;
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'atomic-ipx-tabs': AtomicIPXTabs;
+    'atomic-ipx-tabs': AtomicIpxTabs;
   }
 }
