d8b4785a2afec5294ad30d8a014aa960373201bd
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2025-12-16 12:03:50 -0500

    docs(headless): indexing title improvement

--- packages/documentation/lib/insertIndexingTitle.ts
+++ packages/documentation/lib/insertIndexingTitle.ts
@@ -10,8 +10,10 @@ export const insertIndexingTitle = (page: PageEvent) => {
   const parts = originalTitle.split(' | ');
   const name = parts[0];
   const splitName = name.replace(/([a-z])([A-Z])/g, '$1 $2');
+  const sentenceCaseName =
+    splitName.charAt(0).toUpperCase() + splitName.slice(1).toLowerCase();
 
-  parts[0] = splitName;
+  parts[0] = sentenceCaseName;
   const indexingTitle = parts.join(' | ');
 
   page.contents = page.contents.replace(
