137302bf7136312197c06459be2a06b2692a0a9b
Jad Mazzah <jmazzah@coveo.com>
2025-12-16 13:57:31 -0500

    removed visual regression

--- packages/atomic/src/components/search/facets/atomic-numeric-facet/e2e/page-object.ts
+++ packages/atomic/src/components/search/facets/atomic-numeric-facet/e2e/page-object.ts
@@ -1,11 +1,6 @@
 import type {Page} from '@playwright/test';
 import {BasePageObject} from '../../../../../../playwright-utils/base-page-object';
 
-/**
- * Page object for atomic-numeric-facet E2E tests.
- * Note: atomic-numeric-facet is still Stencil, so we use base-page-object.
- * The nested atomic-facet-number-input is Lit, but we test it through the parent.
- */
 export class AtomicNumericFacetPageObject extends BasePageObject<'atomic-numeric-facet'> {
   constructor(page: Page) {
     super(page, 'atomic-numeric-facet');
@@ -33,29 +28,5 @@ export class AtomicNumericFacetPageObject extends BasePageObject<'atomic-numeric
 
   get facetValues() {
     return this.facet.locator('[part="value-checkbox"]');
-  }
-
-  get numberInput() {
-    return this.facet.locator('atomic-facet-number-input');
-  }
-
-  /**
-   * Wait for component to be stable before taking screenshots.
-   */
-  async waitForVisualStability(): Promise<void> {
-    await this.hydrated.waitFor();
-    await this.page.evaluate(() => document.fonts.ready);
-  }
-
-  /**
-   * Capture a screenshot of the component for visual regression testing.
-   */
-  async captureScreenshot(options?: {
-    animations?: 'disabled' | 'allow';
-  }): Promise<Buffer> {
-    await this.waitForVisualStability();
-    return await this.hydrated.screenshot({
-      animations: options?.animations ?? 'disabled',
-    });
   }
 }
