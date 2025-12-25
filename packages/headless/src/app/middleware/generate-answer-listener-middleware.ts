f8f6ad14b7273d77eca2493883c5202ad698f600
mmitiche <mmitiche@coveo.com>
2025-12-25 12:50:39 -0500

    created a listener middleware that listens to search request in order to trigger head answer generation and also linking the engine to this middleware and also the answer generation api middleware

---
+++ packages/headless/src/app/middleware/generate-answer-listener-middleware.ts
@@ -0,0 +1,39 @@
+import {
+  createListenerMiddleware,
+  type ThunkDispatch,
+  type UnknownAction,
+} from '@reduxjs/toolkit';
+import type {AnswerGenerationApiState} from '../../api/knowledge/answer-generation/answer-generation-api-state.js';
+import {generateHeadAnswer} from '../../features/generated-answer/generated-answer-conversation-actions.js';
+import {isGeneratedAnswerFeatureEnabledWithAnswerGenerationAPI} from '../../features/generated-answer/generated-answer-selectors.js';
+import {selectQuery} from '../../features/query/query-selectors.js';
+import {executeSearch} from '../../features/search/search-actions.js';
+import type {SearchThunkExtraArguments} from '../search-thunk-extra-arguments.js';
+import type {RootState} from '../store.js';
+
+export const generateAnswerListener = createListenerMiddleware<
+  RootState,
+  ThunkDispatch<
+    AnswerGenerationApiState,
+    SearchThunkExtraArguments,
+    UnknownAction
+  >
+>();
+
+generateAnswerListener.startListening({
+  actionCreator: executeSearch.pending,
+
+  effect: async (_action, listenerApi) => {
+    const state = listenerApi.getState();
+
+    const q = selectQuery(state)?.q;
+    const queryIsEmpty = !q || q.trim() === '';
+    const isGeneratedAnswerFeatureEnabled =
+      isGeneratedAnswerFeatureEnabledWithAnswerGenerationAPI(state);
+
+    if (!isGeneratedAnswerFeatureEnabled || queryIsEmpty) {
+      return;
+    }
+    listenerApi.dispatch(generateHeadAnswer());
+  },
+});
