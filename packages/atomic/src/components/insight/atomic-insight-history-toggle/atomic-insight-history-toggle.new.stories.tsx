633a2fb5a4eecf32fd85515f830a303f02769b30
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2025-12-30 14:08:01 -0500

    Update atomic-insight-history-toggle.new.stories.tsx

--- packages/atomic/src/components/insight/atomic-insight-history-toggle/atomic-insight-history-toggle.new.stories.tsx
+++ packages/atomic/src/components/insight/atomic-insight-history-toggle/atomic-insight-history-toggle.new.stories.tsx
@@ -1,33 +1,29 @@
-import {html} from 'lit';
-import {wrapInInsightInterface} from '@/storybook-utils/insight/insight-interface-wrapper';
-import './atomic-insight-history-toggle';
 import type {Meta, StoryObj as Story} from '@storybook/web-components-vite';
+import {getStorybookHelpers} from '@wc-toolkit/storybook-helpers';
+import {parameters} from '@/storybook-utils/common/common-meta-parameters';
+import {wrapInInsightInterface} from '@/storybook-utils/insight/insight-interface-wrapper';
 
 const {decorator, play} = wrapInInsightInterface();
+const {events, args, argTypes, template} = getStorybookHelpers(
+  'atomic-insight-history-toggle',
+  {excludeCategories: ['methods']}
+);
 
 const meta: Meta = {
   component: 'atomic-insight-history-toggle',
   title: 'Insight/History Toggle',
   id: 'atomic-insight-history-toggle',
+  render: (args) => template(args),
   decorators: [decorator],
-  play,
-  argTypes: {
-    tooltip: {
-      control: 'text',
-      description: 'The tooltip text to display on hover',
-      table: {
-        defaultValue: {summary: ''},
-      },
+  parameters: {
+    ...parameters,
+    actions: {
+      handles: events,
     },
   },
-  args: {
-    tooltip: '',
-  },
-  render: (args) =>
-    html`<atomic-insight-history-toggle
-      .tooltip=${args.tooltip}
-      .clickCallback=${() => alert('History button clicked!')}
-    ></atomic-insight-history-toggle>`,
+  args,
+  argTypes,
+  play,
 };
 
 export default meta;
@@ -35,6 +31,7 @@ export default meta;
 export const Default: Story = {};
 
 export const WithTooltip: Story = {
+  name: 'With tooltip',
   args: {
     tooltip: 'View history',
   },
