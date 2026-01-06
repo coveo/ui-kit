a0feabde0d1ec0d0d5f268da4b90e61a1efe6f28
ylakhdar <ylakhdar@coveo.com>
2026-01-06 15:11:06 -0500

    delete cypress test

--- packages/atomic/cypress/integration-insight-panel/insight-panel-actions.ts
+++
@@ -1,9 +0,0 @@
-import {InsightPanelsSelectors} from './insight-panel-selectors';
-
-export const InsightPanelActions = {
-  executeQuery: (query?: string) =>
-    InsightPanelsSelectors.searchbox()
-      .shadow()
-      .find('textarea[part="textarea"]')
-      .type(`${query}{enter}`),
-};
