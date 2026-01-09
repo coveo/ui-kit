49262d23aa7d02ee8cc7a5579e80216f46c0f13d
Louis Bompart <lbompart@coveo.com>
2026-01-09 11:53:01 -0500

    state redacted

--- packages/headless/src/controllers/potato/potato-slice.ts
+++ packages/headless/src/controllers/potato/potato-slice.ts
@@ -1,4 +1,7 @@
 import {createReducer} from '@reduxjs/toolkit';
+import {sliceRedactorsMap} from '../../app/state-key.js';
+
+sliceRedactorsMap.potato = () => undefined;
 
 export const potato = createReducer(
   {
