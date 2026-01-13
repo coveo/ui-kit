494bfd0199f9a57c03ef370d43ee9a7fcfa0aa8d
fbeaudoincoveo <fbeaudoin@coveo.com>
2026-01-13 10:42:46 -0500

    Rework stories to use msw

--- packages/atomic/src/components/recommendations/atomic-recs-interface/atomic-recs-interface.new.stories.tsx
+++ packages/atomic/src/components/recommendations/atomic-recs-interface/atomic-recs-interface.new.stories.tsx
@@ -1,41 +1,32 @@
 import type {Meta, StoryObj as Story} from '@storybook/web-components-vite';
 import {getStorybookHelpers} from '@wc-toolkit/storybook-helpers';
 import {html} from 'lit';
+import {unsafeHTML} from 'lit/directives/unsafe-html.js';
+import {MockRecommendationApi} from '@/storybook-utils/api/recommendation/mock';
 import {parameters} from '@/storybook-utils/common/common-meta-parameters';
+import {wrapInRecommendationInterface} from '@/storybook-utils/search/recs-interface-wrapper';
 
-async function initializeRecsInterface(canvasElement: HTMLElement) {
-  await customElements.whenDefined('atomic-recs-interface');
-  const recsInterface = canvasElement.querySelector('atomic-recs-interface');
-  await recsInterface!.initialize({
-    accessToken: 'xx149e3ec9-786f-4c6c-b64f-49a403b930de',
-    organizationId: 'fashioncoveodemocomgzh7iep8',
-  });
-}
+const mockRecommendationApi = new MockRecommendationApi();
+const {decorator, play} = wrapInRecommendationInterface();
 
-const {events, args, argTypes, template} = getStorybookHelpers(
-  'atomic-recs-interface',
-  {excludeCategories: ['methods']}
-);
+const {events, args, argTypes} = getStorybookHelpers('atomic-recs-interface', {
+  excludeCategories: ['methods'],
+});
 
 const meta: Meta = {
   component: 'atomic-recs-interface',
   title: 'Recommendations/Interface',
   id: 'atomic-recs-interface',
-  render: (args) => template(args),
-  decorators: [(story) => html`<div id="code-root">${story()}</div>`],
+  render: (args) => html`${unsafeHTML(args['default-slot'] || '')}`,
+  decorators: [decorator],
   parameters: {
     ...parameters,
+    msw: {
+      handlers: [...mockRecommendationApi.handlers],
+    },
     actions: {
       handles: events,
     },
-  },
-  play: async (context) => {
-    await initializeRecsInterface(context.canvasElement);
-    const recsInterface = context.canvasElement.querySelector(
-      'atomic-recs-interface'
-    );
-    await customElements.whenDefined('atomic-recs-interface');
-    await recsInterface!.getRecommendations();
   },
   argTypes: {
     ...argTypes,
@@ -69,61 +60,55 @@ const meta: Meta = {
 
 export default meta;
 
-export const Default: Story = {};
+export const Default: Story = {
+  play,
+};
+
+const {play: playNoFirstQuery} = wrapInRecommendationInterface({
+  skipFirstQuery: true,
+});
 
 export const RecsBeforeInit: Story = {
   tags: ['!dev'],
-  play: async (context) => {
-    const recsInterface = context.canvasElement.querySelector(
-      'atomic-recs-interface'
-    );
-    await customElements.whenDefined('atomic-recs-interface');
-    await recsInterface!.getRecommendations();
-  },
+  play: playNoFirstQuery,
 };
 
 export const WithRecsList: Story = {
   args: {
-    'default-slot': `<atomic-recs-list label="Top clothing for you" display="grid" number-of-recommendations="10">
+    'default-slot': `<atomic-recs-list label="Recommended articles" display="list" density="normal" image-size="small" number-of-recommendations="10">
           <atomic-recs-result-template>
             <template>
-              <style>
-                div.result-root.with-sections.display-list.image-small atomic-result-section-visual {
-                  height: 120px;
-                }
-                .rating-wrapper {
-                  display: flex;
-                  align-items: center;
-                }
-                .rating-wrapper span {
-                  margin-left: 5px;
-                  color: #8e959d;
-                }
-              </style>
               <atomic-result-section-visual>
-                <atomic-result-image field="ec_images" aria-hidden="true"></atomic-result-image>
+                <atomic-result-image field="image" aria-hidden="true"></atomic-result-image>
               </atomic-result-section-visual>
+              <atomic-result-section-badges>
+                <atomic-result-badge field="category"></atomic-result-badge>
+              </atomic-result-section-badges>
               <atomic-result-section-title>
                 <atomic-result-link></atomic-result-link>
               </atomic-result-section-title>
-              <atomic-result-section-title-metadata>
-                <div class="rating-wrapper">
-                  <atomic-result-rating field="ec_rating"></atomic-result-rating>
-                  <atomic-field-condition class="field" if-defined="ec_rating">
-                    <span> <atomic-result-number field="cat_rating_count"></atomic-result-number> reviews </span>
+              <atomic-result-section-excerpt>
+                <atomic-result-text field="excerpt"></atomic-result-text>
+              </atomic-result-section-excerpt>
+              <atomic-result-section-bottom-metadata>
+                <atomic-result-fields-list>
+                  <atomic-field-condition class="field" if-defined="author">
+                    <span class="field-label">
+                      <atomic-text value="author"></atomic-text>:
+                    </span>
+                    <atomic-result-text field="author"></atomic-result-text>
                   </atomic-field-condition>
-                </div>
-              </atomic-result-section-title-metadata>
-              <atomic-result-section-emphasized>
-                <atomic-result-number field="ec_price">
-                  <atomic-format-currency currency="USD"></atomic-format-currency>
-                </atomic-result-number>
-              </atomic-result-section-emphasized>
-              <atomic-result-section-excerpt
-                ><atomic-result-text field="excerpt"></atomic-result-text
-              ></atomic-result-section-excerpt>
+                  <atomic-field-condition class="field" if-defined="date">
+                    <span class="field-label">
+                      <atomic-text value="date"></atomic-text>:
+                    </span>
+                    <atomic-result-date></atomic-result-date>
+                  </atomic-field-condition>
+                </atomic-result-fields-list>
+              </atomic-result-section-bottom-metadata>
             </template>
           </atomic-recs-result-template>
         </atomic-recs-list>`,
   },
+  play,
 };
