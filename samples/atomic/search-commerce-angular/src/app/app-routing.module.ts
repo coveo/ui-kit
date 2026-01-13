ba866918de84b48bf1b4cfa7e059cd508814ec89
Louis Bompart <lbompart@coveo.com>
2026-01-12 19:25:20 -0500

    sample: remove scoped out components

--- samples/atomic/search-commerce-angular/src/app/app-routing.module.ts
+++ samples/atomic/search-commerce-angular/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, type Routes} from '@angular/router';
 import {AtomicAngularPageComponent} from './atomic-angular-page/atomic-angular-page.component';
-import {AtomicAngularRecommendationPageComponent} from './atomic-angular-recommendation-page/atomic-angular-recommendation-page.component';
 import {AtomicAngularRGAPageComponent} from './atomic-angular-rga-page/atomic-angular-rga-page.component';
 import {AtomicAngularSearchPageComponent} from './atomic-angular-search-page/atomic-angular-search-page.component';
 import {AtomicCommerceAngularPageComponent} from './atomic-commerce-angular-page/atomic-commerce-angular-page.component';
@@ -18,10 +17,6 @@ const routes: Routes = [
   {
     path: 'atomic-search-angular',
     component: AtomicAngularSearchPageComponent,
-  },
-  {
-    path: 'atomic-recommendation-angular',
-    component: AtomicAngularRecommendationPageComponent,
   },
   {
     path: 'atomic-commerce-angular',
