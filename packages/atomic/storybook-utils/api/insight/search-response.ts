e1838c268714c7f455683c04479abd3492f08ca3
fbeaudoincoveo <fbeaudoin@coveo.com>
2026-01-20 10:49:44 -0500

    Fix mock response for facet stories

--- packages/atomic/storybook-utils/api/insight/search-response.ts
+++ packages/atomic/storybook-utils/api/insight/search-response.ts
@@ -125,6 +125,34 @@ export const baseResponse: InsightResponse = {
   groupByResults: [],
   facets: [
     {
+      facetId: 'objecttype',
+      field: 'objecttype',
+      moreValuesAvailable: false,
+      values: [
+        {
+          value: 'Case',
+          state: 'idle',
+          numberOfResults: 12,
+        },
+        {
+          value: 'Article',
+          state: 'idle',
+          numberOfResults: 8,
+        },
+        {
+          value: 'Ticket',
+          state: 'idle',
+          numberOfResults: 3,
+        },
+        {
+          value: 'FAQ',
+          state: 'idle',
+          numberOfResults: 2,
+        },
+      ],
+      indexScore: 0.1,
+    },
+    {
       facetId: 'source',
       field: 'source',
       moreValuesAvailable: true,
