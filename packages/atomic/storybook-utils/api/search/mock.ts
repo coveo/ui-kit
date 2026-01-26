bca8e68edfb135ac1985d8555d78089b76bbfd65
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2026-01-26 07:52:30 -0500

    mocking

--- packages/atomic/storybook-utils/api/search/mock.ts
+++ packages/atomic/storybook-utils/api/search/mock.ts
@@ -1,10 +1,11 @@
-import type {HttpHandler} from 'msw';
+import {type HttpHandler, HttpResponse} from 'msw';
 import {EndpointHarness, type MockApi} from '../_base.js';
 import type {APIErrorWithStatusCode} from '../_common/error.js';
 import {
   baseResponse as baseFacetSearchResponse,
   type FacetSearchResponse,
 } from './facetSearch-response.js';
+import {baseResponse as baseHtmlResponse} from './html-response.js';
 import {baseResponse as baseQuerySuggestResponse} from './querySuggest-response.js';
 import {baseResponse as baseSearchResponse} from './search-response.js';
 
@@ -12,6 +13,7 @@ export class MockSearchApi implements MockApi {
   readonly searchEndpoint;
   readonly querySuggestEndpoint;
   readonly facetSearchEndpoint;
+  readonly htmlEndpoint;
 
   constructor(basePath: string = 'https://:orgId.org.coveo.com') {
     //TODO: Fix type after encoding Search API response typing properly #6481
@@ -28,6 +30,16 @@ export class MockSearchApi implements MockApi {
       `${basePath}/rest/search/v2/facet`,
       baseFacetSearchResponse
     );
+    this.htmlEndpoint = new EndpointHarness<string>(
+      'POST',
+      `${basePath}/rest/search/v2/html`,
+      baseHtmlResponse,
+      (response, httpResponseInit) =>
+        new HttpResponse(response, {
+          ...httpResponseInit,
+          headers: {'Content-Type': 'text/html'},
+        })
+    );
   }
 
   get handlers(): HttpHandler[] {
@@ -35,6 +47,7 @@ export class MockSearchApi implements MockApi {
       this.searchEndpoint.generateHandler(),
       this.querySuggestEndpoint.generateHandler(),
       this.facetSearchEndpoint.generateHandler(),
+      this.htmlEndpoint.generateHandler(),
     ];
   }
 }
