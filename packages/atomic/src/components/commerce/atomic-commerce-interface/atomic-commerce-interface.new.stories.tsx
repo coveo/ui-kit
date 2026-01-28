55f8c9e1ac4761334c9f41b509485ff3dc85a674
fbeaudoincoveo <fbeaudoin@coveo.com>
2026-01-28 11:17:12 -0500

    add product listing and search stories to atomic-commerce-interface

--- packages/atomic/src/components/commerce/atomic-commerce-interface/atomic-commerce-interface.new.stories.tsx
+++ packages/atomic/src/components/commerce/atomic-commerce-interface/atomic-commerce-interface.new.stories.tsx
@@ -96,6 +96,43 @@ export const Default: Story = {};
 
 export const WithProductList: Story = {
   args: {
+    type: 'product-listing',
+    'default-slot': `
+      <atomic-commerce-layout>
+        <atomic-layout-section section="search">
+          <atomic-commerce-search-box></atomic-commerce-search-box>
+        </atomic-layout-section>
+        <atomic-layout-section section="facets">
+          <atomic-commerce-facets></atomic-commerce-facets>
+        </atomic-layout-section>
+        <atomic-layout-section section="main">
+          <atomic-layout-section section="status">
+            <atomic-commerce-breadbox></atomic-commerce-breadbox>
+            <atomic-commerce-query-summary></atomic-commerce-query-summary>
+            <atomic-commerce-sort-dropdown></atomic-commerce-sort-dropdown>
+          </atomic-layout-section>
+          <atomic-layout-section section="products">
+            <atomic-commerce-product-list
+              display="grid"
+              density="compact"
+              image-size="small"
+            ></atomic-commerce-product-list>
+            <atomic-commerce-query-error></atomic-commerce-query-error>
+          </atomic-layout-section>
+          <atomic-layout-section section="pagination">
+            <atomic-commerce-pager></atomic-commerce-pager>
+            <atomic-commerce-products-per-page>
+            </atomic-commerce-products-per-page>
+          </atomic-layout-section>
+        </atomic-layout-section>
+      </atomic-commerce-layout>
+    `,
+  },
+};
+
+export const WithSearch: Story = {
+  args: {
+    type: 'search',
     'default-slot': `
       <atomic-commerce-layout>
         <atomic-layout-section section="search">
