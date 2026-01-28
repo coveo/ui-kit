ee3a052d0528effa62ce0fc72f82f253d3fa4eb5
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2026-01-28 13:27:07 -0500

    story and mdx

--- packages/atomic/src/components/insight/atomic-insight-result-children/atomic-insight-result-children.new.stories.tsx
+++ packages/atomic/src/components/insight/atomic-insight-result-children/atomic-insight-result-children.new.stories.tsx
@@ -1,4 +1,5 @@
 import type {Meta, StoryObj as Story} from '@storybook/web-components-vite';
+import {getStorybookHelpers} from '@wc-toolkit/storybook-helpers';
 import {html} from 'lit';
 import {
   baseFoldedResponse,
@@ -7,9 +8,13 @@ import {
 import {parameters} from '@/storybook-utils/common/common-meta-parameters';
 import {wrapInInsightInterface} from '@/storybook-utils/insight/insight-interface-wrapper';
 
-const mockInsightApi = new MockInsightApi();
+const insightApiHarness = new MockInsightApi();
 
 const {decorator, play} = wrapInInsightInterface();
+const {events, args, argTypes} = getStorybookHelpers(
+  'atomic-insight-result-children',
+  {excludeCategories: ['methods']}
+);
 
 const meta: Meta = {
   component: 'atomic-insight-result-children',
@@ -18,13 +23,20 @@ const meta: Meta = {
   decorators: [decorator],
   parameters: {
     ...parameters,
+    actions: {
+      handles: events,
+    },
     msw: {
-      handlers: [...mockInsightApi.handlers],
+      handlers: [...insightApiHarness.handlers],
     },
   },
+  args: {
+    ...args,
+  },
+  argTypes,
   beforeEach: async () => {
-    mockInsightApi.searchEndpoint.clear();
-    mockInsightApi.searchEndpoint.mockOnce(() => baseFoldedResponse);
+    insightApiHarness.searchEndpoint.clear();
+    insightApiHarness.searchEndpoint.mockOnce(() => baseFoldedResponse);
   },
   play,
 };
@@ -33,132 +45,28 @@ export default meta;
 
 export const Default: Story = {
   render: () => html`
-    <atomic-insight-layout>
-      <atomic-layout-section section="results">
-        <atomic-insight-folded-result-list
-          image-size="small"
-          display="list"
-          collection-field="foldingcollection"
-          parent-field="foldingparent"
-          child-field="foldingchild"
-        >
-          <atomic-insight-result-template>
-            <template>
-              <atomic-result-section-title>
-                <atomic-result-link></atomic-result-link>
-              </atomic-result-section-title>
-              <atomic-result-section-excerpt>
-                <atomic-result-text field="excerpt"></atomic-result-text>
-              </atomic-result-section-excerpt>
-              <atomic-result-section-children>
-                <atomic-insight-result-children image-size="icon">
-                  <atomic-insight-result-children-template>
-                    <template>
-                      <atomic-result-section-title>
-                        <atomic-result-link></atomic-result-link>
-                      </atomic-result-section-title>
-                      <atomic-result-section-excerpt>
-                        <atomic-result-text field="excerpt"></atomic-result-text>
-                      </atomic-result-section-excerpt>
-                    </template>
-                  </atomic-insight-result-children-template>
-                </atomic-insight-result-children>
-              </atomic-result-section-children>
-            </template>
-          </atomic-insight-result-template>
-        </atomic-insight-folded-result-list>
-      </atomic-layout-section>
-    </atomic-insight-layout>
-  `,
-};
-
-export const WithInheritTemplates: Story = {
-  name: 'With inherit-templates',
-  render: () => html`
-    <atomic-insight-layout>
-      <atomic-layout-section section="results">
-        <atomic-insight-folded-result-list
-          image-size="small"
-          display="list"
-          collection-field="foldingcollection"
-          parent-field="foldingparent"
-          child-field="foldingchild"
-        >
-          <atomic-insight-result-template>
-            <template>
-              <atomic-result-section-title>
-                <atomic-result-link></atomic-result-link>
-              </atomic-result-section-title>
-              <atomic-result-section-excerpt>
-                <atomic-result-text field="excerpt"></atomic-result-text>
-              </atomic-result-section-excerpt>
-              <atomic-result-section-children>
-                <atomic-insight-result-children image-size="icon">
-                  <atomic-insight-result-children-template>
-                    <template>
-                      <atomic-result-section-title>
-                        <atomic-result-link></atomic-result-link>
-                      </atomic-result-section-title>
-                      <atomic-result-section-excerpt>
-                        <atomic-result-text field="excerpt"></atomic-result-text>
-                      </atomic-result-section-excerpt>
-                      <atomic-result-section-children>
-                        <atomic-insight-result-children inherit-templates>
-                        </atomic-insight-result-children>
-                      </atomic-result-section-children>
-                    </template>
-                  </atomic-insight-result-children-template>
-                </atomic-insight-result-children>
-              </atomic-result-section-children>
-            </template>
-          </atomic-insight-result-template>
-        </atomic-insight-folded-result-list>
-      </atomic-layout-section>
-    </atomic-insight-layout>
-  `,
-};
-
-export const WithCustomNoResultText: Story = {
-  name: 'With custom no-result-text',
-  render: () => html`
-    <atomic-insight-layout>
-      <atomic-layout-section section="results">
-        <atomic-insight-folded-result-list
-          image-size="small"
-          display="list"
-          collection-field="foldingcollection"
-          parent-field="foldingparent"
-          child-field="foldingchild"
-        >
-          <atomic-insight-result-template>
-            <template>
-              <atomic-result-section-title>
-                <atomic-result-link></atomic-result-link>
-              </atomic-result-section-title>
-              <atomic-result-section-excerpt>
-                <atomic-result-text field="excerpt"></atomic-result-text>
-              </atomic-result-section-excerpt>
-              <atomic-result-section-children>
-                <atomic-insight-result-children
-                  image-size="icon"
-                  no-result-text="No related documents found"
-                >
-                  <atomic-insight-result-children-template>
-                    <template>
-                      <atomic-result-section-title>
-                        <atomic-result-link></atomic-result-link>
-                      </atomic-result-section-title>
-                      <atomic-result-section-excerpt>
-                        <atomic-result-text field="excerpt"></atomic-result-text>
-                      </atomic-result-section-excerpt>
-                    </template>
-                  </atomic-insight-result-children-template>
-                </atomic-insight-result-children>
-              </atomic-result-section-children>
-            </template>
-          </atomic-insight-result-template>
-        </atomic-insight-folded-result-list>
-      </atomic-layout-section>
-    </atomic-insight-layout>
+    <atomic-insight-folded-result-list>
+      <atomic-insight-result-template>
+        <template>
+          <atomic-result-section-title>
+            <atomic-result-link></atomic-result-link>
+          </atomic-result-section-title>
+          <atomic-result-section-children>
+            <atomic-insight-result-children image-size="icon">
+              <atomic-insight-result-children-template>
+                <template>
+                  <atomic-result-section-title>
+                    <atomic-result-link></atomic-result-link>
+                  </atomic-result-section-title>
+                  <atomic-result-section-excerpt>
+                    <atomic-result-text field="excerpt"></atomic-result-text>
+                  </atomic-result-section-excerpt>
+                </template>
+              </atomic-insight-result-children-template>
+            </atomic-insight-result-children>
+          </atomic-result-section-children>
+        </template>
+      </atomic-insight-result-template>
+    </atomic-insight-folded-result-list>
   `,
 };
