6388923560830405e4eb952434824fea20b4aaf8
fbeaudoincoveo <fbeaudoin@coveo.com>
2026-01-29 09:51:11 -0500

    remove irrelevant tests

--- packages/atomic/src/components/insight/atomic-insight-user-actions-timeline/e2e/atomic-insight-user-actions-timeline.e2e.ts
+++ packages/atomic/src/components/insight/atomic-insight-user-actions-timeline/e2e/atomic-insight-user-actions-timeline.e2e.ts
@@ -1,20 +1,10 @@
 import {expect, test} from './fixture';
 
 test.describe('atomic-insight-user-actions-timeline', () => {
-  test.beforeEach(async ({userActionsTimeline}) => {
-    await userActionsTimeline.load();
-  });
-
   test('should render with the active session', async ({
     userActionsTimeline,
   }) => {
+    await userActionsTimeline.load();
     await expect(userActionsTimeline.activeSession).toBeVisible();
-  });
-
-  test('should render toggle buttons for following and preceding sessions', async ({
-    userActionsTimeline,
-  }) => {
-    await expect(userActionsTimeline.showFollowingSessionsbutton).toBeVisible();
-    await expect(userActionsTimeline.showPrecedingSessionsbutton).toBeVisible();
   });
 });
