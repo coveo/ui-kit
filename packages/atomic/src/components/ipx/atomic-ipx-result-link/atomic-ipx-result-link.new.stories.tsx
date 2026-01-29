46ba160bbf5740fab50079d72b679f0426cf7b15
fbeaudoincoveo <fbeaudoin@coveo.com>
2026-01-29 16:04:37 -0500

    refactor atomic-ipx-result-link stories to use unsafeHTML and simplify templates

--- packages/atomic/src/components/ipx/atomic-ipx-result-link/atomic-ipx-result-link.new.stories.tsx
+++ packages/atomic/src/components/ipx/atomic-ipx-result-link/atomic-ipx-result-link.new.stories.tsx
@@ -1,5 +1,6 @@
 import type {Meta, StoryObj as Story} from '@storybook/web-components-vite';
 import {html} from 'lit';
+import {unsafeHTML} from 'lit/directives/unsafe-html.js';
 import {MockSearchApi} from '@/storybook-utils/api/search/mock';
 import {parameters} from '@/storybook-utils/common/common-meta-parameters';
 import {wrapInRecommendationInterface} from '@/storybook-utils/search/recs-interface-wrapper';
@@ -8,31 +9,62 @@ const mockedSearchApi = new MockSearchApi();
 
 mockedSearchApi.searchEndpoint.mock((response) => ({
   ...response,
-  results: response.results.slice(0, 5),
-  totalCount: 5,
-  totalCountFiltered: 5,
+  results: response.results.slice(0, 1),
+  totalCount: 1,
+  totalCountFiltered: 1,
 }));
 
 const {decorator, play} = wrapInRecommendationInterface();
+
+const buildTemplate = (innerContent: string) => `
+  <atomic-recs-list display="list" density="normal" image-size="none">
+    <atomic-recs-result-template>
+      <template>
+        ${innerContent}
+      </template>
+    </atomic-recs-result-template>
+  </atomic-recs-list>
+`;
+
+const defaultTemplate = `<atomic-result-section-title>
+  <atomic-ipx-result-link></atomic-ipx-result-link>
+</atomic-result-section-title>
+<atomic-result-section-excerpt>
+  <atomic-result-text field="excerpt"></atomic-result-text>
+</atomic-result-section-excerpt>`;
+
+const customTextTemplate = `<atomic-result-section-title>
+  <atomic-ipx-result-link>
+    <atomic-result-text field="title"></atomic-result-text>
+    - With Custom Link Text
+  </atomic-ipx-result-link>
+</atomic-result-section-title>
+<atomic-result-section-excerpt>
+  <atomic-result-text field="excerpt"></atomic-result-text>
+</atomic-result-section-excerpt>`;
+
+const hrefTemplateTemplate = `<atomic-result-section-title>
+  <atomic-ipx-result-link href-template="\${clickUri}?source=ipx"></atomic-ipx-result-link>
+</atomic-result-section-title>
+<atomic-result-section-excerpt>
+  <atomic-result-text field="excerpt"></atomic-result-text>
+</atomic-result-section-excerpt>`;
+
+const targetBlankTemplate = `<atomic-result-section-title>
+  <atomic-ipx-result-link>
+    <a slot="attributes" target="_blank" rel="noopener"></a>
+  </atomic-ipx-result-link>
+</atomic-result-section-title>
+<atomic-result-section-excerpt>
+  <atomic-result-text field="excerpt"></atomic-result-text>
+</atomic-result-section-excerpt>`;
 
 const meta: Meta = {
   component: 'atomic-ipx-result-link',
   title: 'IPX/Ipx Result Link',
   id: 'atomic-ipx-result-link',
-  render: () => html`
-    <atomic-recs-list display="list" density="normal" image-size="none">
-      <atomic-recs-result-template>
-        <template>
-          <atomic-result-section-title>
-            <atomic-ipx-result-link></atomic-ipx-result-link>
-          </atomic-result-section-title>
-          <atomic-result-section-excerpt>
-            <atomic-result-text field="excerpt"></atomic-result-text>
-          </atomic-result-section-excerpt>
-        </template>
-      </atomic-recs-result-template>
-    </atomic-recs-list>
-  `,
+  render: (args: {templateContent?: string}) =>
+    html`${unsafeHTML(buildTemplate(args.templateContent || defaultTemplate))}`,
   decorators: [decorator],
   parameters: {
     ...parameters,
@@ -54,67 +86,24 @@ export const Default: Story = {
 
 export const WithCustomText: Story = {
   name: 'With custom link text',
-  render: () => html`
-    <atomic-recs-list display="list" density="normal" image-size="none">
-      <atomic-recs-result-template>
-        <template>
-          <atomic-result-section-title>
-            <atomic-ipx-result-link>
-              <atomic-result-text field="title"></atomic-result-text>
-              - Click here
-            </atomic-ipx-result-link>
-          </atomic-result-section-title>
-          <atomic-result-section-excerpt>
-            <atomic-result-text field="excerpt"></atomic-result-text>
-          </atomic-result-section-excerpt>
-        </template>
-      </atomic-recs-result-template>
-    </atomic-recs-list>
-  `,
-  decorators: [decorator],
+  args: {
+    templateContent: customTextTemplate,
+  },
   play,
 };
 
 export const WithHrefTemplate: Story = {
   name: 'With href template',
-  render: () => html`
-    <atomic-recs-list display="list" density="normal" image-size="none">
-      <atomic-recs-result-template>
-        <template>
-          <atomic-result-section-title>
-            <atomic-ipx-result-link
-              href-template="\${clickUri}?source=ipx"
-            ></atomic-ipx-result-link>
-          </atomic-result-section-title>
-          <atomic-result-section-excerpt>
-            <atomic-result-text field="excerpt"></atomic-result-text>
-          </atomic-result-section-excerpt>
-        </template>
-      </atomic-recs-result-template>
-    </atomic-recs-list>
-  `,
-  decorators: [decorator],
+  args: {
+    templateContent: hrefTemplateTemplate,
+  },
   play,
 };
 
 export const WithTargetBlank: Story = {
   name: 'With target="_blank"',
-  render: () => html`
-    <atomic-recs-list display="list" density="normal" image-size="none">
-      <atomic-recs-result-template>
-        <template>
-          <atomic-result-section-title>
-            <atomic-ipx-result-link>
-              <a slot="attributes" target="_blank" rel="noopener"></a>
-            </atomic-ipx-result-link>
-          </atomic-result-section-title>
-          <atomic-result-section-excerpt>
-            <atomic-result-text field="excerpt"></atomic-result-text>
-          </atomic-result-section-excerpt>
-        </template>
-      </atomic-recs-result-template>
-    </atomic-recs-list>
-  `,
-  decorators: [decorator],
+  args: {
+    templateContent: targetBlankTemplate,
+  },
   play,
 };
