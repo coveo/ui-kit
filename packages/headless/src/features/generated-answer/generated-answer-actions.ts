f63a9c4dd6ddd7f2139647bcc91b26c18236220d
mmitiche <mmitiche@coveo.com>
2026-02-03 12:06:52 -0500

    Created new  generateFollowUpAnswer action

--- packages/headless/src/features/generated-answer/generated-answer-actions.ts
+++ packages/headless/src/features/generated-answer/generated-answer-actions.ts
@@ -40,6 +40,7 @@ import {
   validatePayload,
 } from '../../utils/validate-payload.js';
 import {selectAgentId} from '../configuration/configuration-selectors.js';
+import {resetFollowUpAnswers} from '../follow-up-answers/follow-up-answers-actions.js';
 import {
   logGeneratedAnswerResponseLinked,
   logGeneratedAnswerStreamEnd,
@@ -398,6 +399,7 @@ export const generateHeadAnswer = createAsyncThunk<
       return;
     }
 
+    dispatch(resetFollowUpAnswers());
     dispatch(resetAnswer());
     const generateHeadAnswerParams = constructGenerateHeadAnswerParams(
       state,
