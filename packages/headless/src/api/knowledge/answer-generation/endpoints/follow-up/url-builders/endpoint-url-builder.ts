4118e0afe716a9a24d771f14104ee8ae933aeb99
mmitiche <mmitiche@coveo.com>
2026-02-05 15:48:37 -0500

    endpoint updated to target the kgas

--- packages/headless/src/api/knowledge/answer-generation/endpoints/follow-up/url-builders/endpoint-url-builder.ts
+++ packages/headless/src/api/knowledge/answer-generation/endpoints/follow-up/url-builders/endpoint-url-builder.ts
@@ -7,17 +7,17 @@ import type {AnswerGenerationApiState} from '../../../answer-generation-api-stat
 export const buildFollowUpEndpointUrl = (
   state: AnswerGenerationApiState
 ): string => {
-  const {configuration, generatedAnswer} = state;
-  const {organizationId, environment} = configuration;
+  const {configuration} = state;
+  const {
+    organizationId,
+    environment,
+    knowledge: {agentId},
+  } = configuration;
   const platformEndpoint = getOrganizationEndpoint(organizationId, environment);
 
-  if (
-    !platformEndpoint ||
-    !organizationId ||
-    !generatedAnswer.answerConfigurationId
-  ) {
+  if (!platformEndpoint || !organizationId || !agentId) {
     throw new Error('Missing required parameters for follow up endpoint');
   }
-  const basePath = `/rest/organizations/${organizationId}/answer/v1/configs`;
-  return `${platformEndpoint}${basePath}/${generatedAnswer.answerConfigurationId}/generate`;
+  const basePath = `/api/preview/organizations/${organizationId}/agents`;
+  return `${platformEndpoint}${basePath}/${agentId}/followup`;
 };
