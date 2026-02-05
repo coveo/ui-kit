a46175314945d8a989e96897e3ec568a2cd701e4
Tooni <nsayers@coveo.com>
2026-02-05 16:18:12 +0000

    add a comment

--- packages/headless/src/api/commerce/search/plan/plan-response.ts
+++ packages/headless/src/api/commerce/search/plan/plan-response.ts
@@ -1,3 +1,8 @@
 export interface CommercePlanSuccessResponse {
+  /**
+   * The URL to which to redirect the user.
+   * (Null if there isn't one.)
+   * @example https://www.example.com/
+   */
   redirect: string | null;
 }
