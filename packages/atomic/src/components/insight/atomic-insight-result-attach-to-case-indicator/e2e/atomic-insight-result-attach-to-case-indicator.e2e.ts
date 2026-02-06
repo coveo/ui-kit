57d5975a9b00b539199ba46f944855db4f963139
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2026-02-06 15:25:24 -0500

    apply comments

--- packages/atomic/src/components/insight/atomic-insight-result-attach-to-case-indicator/e2e/atomic-insight-result-attach-to-case-indicator.e2e.ts
+++ packages/atomic/src/components/insight/atomic-insight-result-attach-to-case-indicator/e2e/atomic-insight-result-attach-to-case-indicator.e2e.ts
@@ -1,27 +1,7 @@
-import {expect, test} from './fixture';
+import {test} from './fixture';
 
 test.describe('atomic-insight-result-attach-to-case-indicator', () => {
   test.beforeEach(async ({attachToCaseIndicator}) => {
     await attachToCaseIndicator.load({story: 'default'});
-  });
-
-  test('should display the attach icon when result is attached to case', async ({
-    attachToCaseIndicator,
-  }) => {
-    await expect(attachToCaseIndicator.resultList).toBeVisible();
-    await expect(attachToCaseIndicator.results.first()).toBeVisible();
-    await expect(attachToCaseIndicator.icon.first()).toBeVisible();
-  });
-
-  test('should not display the attach icon when result is not attached to case', async ({
-    attachToCaseIndicator,
-  }) => {
-    await expect(attachToCaseIndicator.resultList).toBeVisible();
-    await expect(attachToCaseIndicator.results.first()).toBeVisible();
-
-    const secondResultIndicator = attachToCaseIndicator.page.locator(
-      'atomic-insight-result:nth-of-type(2) atomic-insight-result-attach-to-case-indicator [part="icon"]'
-    );
-    await expect(secondResultIndicator).not.toBeVisible();
   });
 });
