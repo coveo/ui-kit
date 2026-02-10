b1da256eef8a074580bc6b9a91fd1a3b702a69a4
fbeaudoincoveo <fbeaudoin@coveo.com>
2026-02-10 13:13:29 -0500

    update atomic-ipx-refine-toggle story structure and improve modal handling

--- packages/atomic/src/components/ipx/atomic-ipx-refine-toggle/atomic-ipx-refine-toggle.new.stories.tsx
+++ packages/atomic/src/components/ipx/atomic-ipx-refine-toggle/atomic-ipx-refine-toggle.new.stories.tsx
@@ -1,48 +1,54 @@
-import {getSampleSearchEngineConfiguration} from '@coveo/headless';
 import type {Meta, StoryObj as Story} from '@storybook/web-components-vite';
 import {getStorybookHelpers} from '@wc-toolkit/storybook-helpers';
-import {html} from 'lit/static-html.js';
+import {html} from 'lit';
+import {MockSearchApi} from '@/storybook-utils/api/search/mock';
 import {parameters} from '@/storybook-utils/common/common-meta-parameters';
+import {wrapInSearchInterface} from '@/storybook-utils/search/search-interface-wrapper';
 
+const mockSearchApi = new MockSearchApi();
+
+const {decorator, play} = wrapInSearchInterface();
 const {events, args, argTypes, template} = getStorybookHelpers(
   'atomic-ipx-refine-toggle',
   {excludeCategories: ['methods']}
 );
-
-async function initializeInterface(canvasElement: HTMLElement) {
-  await customElements.whenDefined('atomic-search-interface');
-  const searchInterface = canvasElement.querySelector(
-    'atomic-search-interface'
-  );
-  await searchInterface!.initialize(getSampleSearchEngineConfiguration());
-  await searchInterface!.executeFirstSearch();
-}
 
 const meta: Meta = {
   component: 'atomic-ipx-refine-toggle',
   title: 'IPX/Refine Toggle',
   id: 'atomic-ipx-refine-toggle',
   render: (args) => html`
-    <atomic-search-interface>
+    <div style="display: flex; justify-content: center; padding: 1rem;">
       ${template(args)}
-      <atomic-ipx-refine-modal>
-        <atomic-facet field="author" label="Author"></atomic-facet>
-        <atomic-facet field="source" label="Source"></atomic-facet>
-        <atomic-facet field="filetype" label="File Type"></atomic-facet>
-      </atomic-ipx-refine-modal>
-    </atomic-search-interface>
+    </div>
+    <atomic-ipx-refine-modal>
+      <atomic-facet field="author" label="Author"></atomic-facet>
+      <atomic-facet field="source" label="Source"></atomic-facet>
+      <atomic-facet field="filetype" label="File Type"></atomic-facet>
+    </atomic-ipx-refine-modal>
   `,
+  decorators: [decorator],
   parameters: {
     ...parameters,
+    layout: 'fullscreen',
     actions: {
       handles: events,
     },
+    docs: {
+      ...parameters.docs,
+      story: {
+        ...parameters.docs?.story,
+        height: '150px',
+      },
+    },
+    msw: {handlers: [...mockSearchApi.handlers]},
   },
   args,
   argTypes,
-  play: async (context) => {
-    await initializeInterface(context.canvasElement);
+  beforeEach: () => {
+    mockSearchApi.searchEndpoint.clear();
   },
+  play,
 };
 
 export default meta;
@@ -52,8 +58,8 @@ export const Default: Story = {
 };
 
 export const WithCollapsedFacets: Story = {
-  name: 'With collapsed facets after 2',
+  name: 'With collapsed facets after 1',
   args: {
-    'collapse-facets-after': 2,
+    'collapse-facets-after': 1,
   },
 };
