bee703dd226ab62dff97a87d45dd39bdce8ce76e
ylakhdar <ylakhdar@coveo.com>
2026-02-16 15:13:14 -0500

    add script to generate rules

--- packages/atomic-a11y/src/shared/constants.ts
+++ packages/atomic-a11y/src/shared/constants.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_A11Y_REPORT_OUTPUT_DIR = 'a11y/reports';
 export const DEFAULT_A11Y_REPORT_FILENAME = 'a11y-report.json';
-export const DEFAULT_WCAG_22_AA_CRITERIA_COUNT = 56;
+export const DEFAULT_WCAG_22_AA_CRITERIA_COUNT = 55;
 export const BASELINE_FILE_PATTERN =
   /^manual-audit-(?!.*-violations)([\w-]+)\.json$/;
