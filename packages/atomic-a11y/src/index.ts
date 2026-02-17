61496ceed2ee3e3409c314ab7307c79a7966a337
ylakhdar <ylakhdar@coveo.com>
2026-02-17 09:13:37 -0500

    feat(atomic-a11y): export merge-shards API and add CLI script

--- packages/atomic-a11y/src/index.ts
+++ packages/atomic-a11y/src/index.ts
@@ -1,6 +1,7 @@
 export {getCriterionMetadata} from './data/criterion-metadata.js';
 export type {WcagCriterionDefinition} from './data/wcag-criteria.js';
 export {wcagCriteriaDefinitions} from './data/wcag-criteria.js';
+export {mergeA11yShardReports} from './reporter/merge-shards.js';
 export {
   type A11yReporterOptions,
   VitestA11yReporter,
