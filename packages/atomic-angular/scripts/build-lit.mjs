6d679d620485913c88d61a83b0a50fc4f7daf8fc
Louis Bompart <lbompart@coveo.com>
2026-02-18 16:08:23 -0500

    noshim

--- packages/atomic-angular/scripts/build-lit.mjs
+++ packages/atomic-angular/scripts/build-lit.mjs
@@ -127,38 +127,10 @@ const DECLARATIONS = [
   ${[...litDeclarations].sort().join(',\n  ')}
 ];
 
-const shimTemplates = ()=> {
-  // Angular's renderer will add children to a <template> instead of to its
-  // content. This shim will force any children added to a <template> to be
-  // added to its content instead.
-  // https://github.com/angular/angular/issues/15557
-  const nativeAppend = HTMLTemplateElement && HTMLTemplateElement.prototype && HTMLTemplateElement.prototype.appendChild;
-  if(!nativeAppend) {
-    return;
-  }
-  HTMLTemplateElement.prototype.appendChild = function<T extends Node>(
-    childNode: T
-  ) {
-    if (this.content) {
-      return this.content.appendChild(childNode);
-    } else {
-      return <T>nativeAppend.apply(this, [childNode]);
-    }
-  };
-}
-
-        
-const SHIM_TEMPLATES_PROVIDER: Provider = {
-  provide: APP_INITIALIZER,
-  multi: true,
-  useValue: shimTemplates
-}
-
         
 @NgModule({
   declarations: DECLARATIONS,
   exports: DECLARATIONS,
-  providers: [SHIM_TEMPLATES_PROVIDER],
   imports: [CommonModule],
 })
 export class AtomicAngularModule {
