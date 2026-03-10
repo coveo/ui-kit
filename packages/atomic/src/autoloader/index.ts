726d1be0835266cf052fdeb0f89bfe1ad770bd64
Louis Bompart <lbompart@coveo.com>
2026-03-10 15:49:01 -0400

    chore: remove cypress from atomic

--- packages/atomic/src/autoloader/index.ts
+++ packages/atomic/src/autoloader/index.ts
@@ -49,13 +49,8 @@ export function registerAutoloader(
   const discover = async (root: Element | ShadowRoot | DocumentFragment) => {
     visitedNodes.add(root);
 
-    // TODO: KIT-5085 remove once we get rid of cypress
     const rootTagName =
-      (root.ownerDocument.defaultView &&
-        root instanceof root.ownerDocument.defaultView.Element) ||
-      root instanceof Element
-        ? root.tagName.toLowerCase()
-        : '';
+      root instanceof Element ? root.tagName.toLowerCase() : '';
     const rootIsCustomElement = rootTagName?.includes('-');
     const allCustomElements = [...root.querySelectorAll('*')].filter((el) =>
       el.tagName.toLowerCase().includes('-')
