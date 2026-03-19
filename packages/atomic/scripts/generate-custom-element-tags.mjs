ccccf81be7d96d6e3dd634affa9395ac27243843
Louis Bompart <lbompart@coveo.com>
2026-03-18 23:15:13 -0400

    Update packages/atomic/scripts/generate-custom-element-tags.mjs

--- packages/atomic/scripts/generate-custom-element-tags.mjs
+++ packages/atomic/scripts/generate-custom-element-tags.mjs
@@ -1,4 +1,3 @@
-import {execSync} from 'node:child_process';
 import {readdirSync, readFileSync, writeFileSync} from 'node:fs';
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
