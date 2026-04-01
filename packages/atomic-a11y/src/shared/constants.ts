98f76315302fb84f14d4b03b7c53df749378ff78
ylakhdar <ylakhdar@coveo.com>
2026-04-01 11:10:57 -0400

    cleanup

--- packages/atomic-a11y/src/shared/constants.ts
+++ packages/atomic-a11y/src/shared/constants.ts
@@ -1,5 +1,3 @@
 export const DEFAULT_WCAG_22_AA_CRITERIA_COUNT = 55;
-export const UNKNOWN_CATEGORY = 'unknown';
-export const UNKNOWN_FRAMEWORK = 'unknown';
 export const BASELINE_FILE_PATTERN =
   /^manual-audit-(?!.*-violations)([\w-]+)\.json$/;
