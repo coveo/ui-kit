d9bea55ea42ff18b4daa55dd8cd0450d78f7abb7
fbeaudoincoveo <fbeaudoin@coveo.com>
2026-04-22 11:04:41 -0400

    Sample cleanup: remove other framework samples, restore CSS

--- samples/commerce-agent-chat/vue/src/main.ts
+++
@@ -1,7 +0,0 @@
-import {createApp} from 'vue';
-import '@coveo/commerce-agent-chat-components/register';
-
-import '@core/styles/base.css';
-import App from './App.vue';
-
-createApp(App).mount('#app');
