ac729a423be24a3f4690ec70e9dcc78ef0106bbd
simonmilord <simon.milord@gmail.com>
2026-05-04 11:40:40 -0400

    removed redundant check for answer text empty

--- packages/headless/src/features/generated-answer/generated-answer-analytics-actions.ts
+++ packages/headless/src/features/generated-answer/generated-answer-analytics-actions.ts
@@ -261,16 +261,13 @@ export const logGeneratedAnswerStreamEnd = (
     __legacy__getBuilder: (client, state) => {
       const generativeQuestionAnsweringId =
         answerId ?? generativeQuestionAnsweringIdSelector(state);
-      const resolvedAnswerTextIsEmpty = answerGenerated
-        ? answerTextIsEmpty
-        : undefined;
       if (!generativeQuestionAnsweringId) {
         return null;
       }
       return client.makeGeneratedAnswerStreamEnd({
         generativeQuestionAnsweringId,
         answerGenerated,
-        answerTextIsEmpty: resolvedAnswerTextIsEmpty,
+        answerTextIsEmpty,
         ...(conversationId && {conversationId}),
       });
     },
