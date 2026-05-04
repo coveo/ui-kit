6f526f4cb290a0f63b8fe5d084ccd4736e5cb96c
simonmilord <simon.milord@gmail.com>
2026-05-04 11:44:06 -0400

    revert

--- packages/headless/src/features/generated-answer/generated-answer-analytics-actions.ts
+++ packages/headless/src/features/generated-answer/generated-answer-analytics-actions.ts
@@ -261,13 +261,18 @@ export const logGeneratedAnswerStreamEnd = (
     __legacy__getBuilder: (client, state) => {
       const generativeQuestionAnsweringId =
         answerId ?? generativeQuestionAnsweringIdSelector(state);
+      const resolvedAnswerTextIsEmpty = answerGenerated
+        ? (answerTextIsEmpty ??
+          (!state.generatedAnswer?.answer ||
+            !state.generatedAnswer?.answer.length))
+        : undefined;
       if (!generativeQuestionAnsweringId) {
         return null;
       }
       return client.makeGeneratedAnswerStreamEnd({
         generativeQuestionAnsweringId,
         answerGenerated,
-        answerTextIsEmpty,
+        answerTextIsEmpty: resolvedAnswerTextIsEmpty,
         ...(conversationId && {conversationId}),
       });
     },
