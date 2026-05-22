fe31166cf5cc35e6f8e52e06865899b74211d5e6
Simon Milord <simon.milord@gmail.com>
2026-05-22 13:16:12 -0400

    added size property on the copyToClipboard action for the icon size to match the pattern used for the feedback buttons

--- packages/quantic/force-app/main/default/lwc/quanticGeneratedAnswerCopyToClipboard/quanticGeneratedAnswerCopyToClipboard.js
+++ packages/quantic/force-app/main/default/lwc/quanticGeneratedAnswerCopyToClipboard/quanticGeneratedAnswerCopyToClipboard.js
@@ -12,8 +12,27 @@ import {LightningElement, api} from 'lwc';
 export default class QuanticGeneratedAnswerCopyToClipboard extends LightningElement {
   /**
    * The answer to copy
+  * @type {string}
    */
-  @api answer;
+  @api answer = '';
+
+  /**
+   * The size of the copy icon.
+   * @api
+   * @type {'xx-small' | 'x-small' | 'small' | 'medium' | 'large'}
+   */
+  @api
+  get size() {
+    return this._size;
+  }
+  set size(value) {
+    if (['xx-small', 'x-small', 'small', 'medium', 'large'].includes(value)) {
+      this._size = value;
+    }
+  }
+
+  /** @type {'xx-small' | 'x-small' | 'small' | 'medium' | 'large'} */
+  _size = 'xx-small';
 
   labels = {
     copy,
@@ -22,7 +41,7 @@ export default class QuanticGeneratedAnswerCopyToClipboard extends LightningElem
   isSelected = false;
   tooltip = this.labels.copy;
 
-  dispatchCopyToClipboardEvent(option) {
+    dispatchCopyToClipboardEvent(option) {
     this.dispatchEvent(
       new CustomEvent('quantic__generatedanswercopy', {
         detail: option,
