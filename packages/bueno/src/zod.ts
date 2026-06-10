9b00a465f0c0c2dfd302d27ad06fcd459ec3bb89
Louis Bompart <lbompart@coveo.com>
2026-06-10 16:33:47 -0400

    minimal export

--- packages/bueno/src/zod.ts
+++ packages/bueno/src/zod.ts
@@ -1,1 +1,20 @@
-export * from 'zod/mini';
+export {
+  object,
+  string,
+  number,
+  boolean,
+  enum,
+  array,
+  optional,
+  unknown,
+  union,
+  url,
+  record,
+  _default,
+  minimum,
+  maximum,
+  minLength,
+  maxLength,
+  regex,
+} from 'zod/v4-mini';
+export type {ZodMiniType, core} from 'zod/v4-mini';
