ee870436d13e10e4efa895eae6ea340074fe9247
Louis Bompart <lbompart@coveo.com>
2026-06-19 17:24:32 -0400

    pull the lever kronk

--- packages/atomic/.storybook/preview.ts
+++ packages/atomic/.storybook/preview.ts
@@ -97,7 +97,7 @@ const preview: Preview = {
         textColor: '#282829',
       }),
     },
-    chromatic: {disableSnapshot: true},
+    chromatic: {disableSnapshot: false},
   },
   beforeEach({canvasElement, canvas}) {
     Object.assign(canvas, {...within(canvasElement)});
