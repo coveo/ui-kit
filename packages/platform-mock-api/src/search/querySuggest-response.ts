862e7934ace5c0489047f0e67ced58ea40f01c03
Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>
2026-06-19 11:11:46 -0400

    feat: extract @coveo/platform-mock-api shared package (#7838)

---
+++ packages/platform-mock-api/src/search/querySuggest-response.ts
@@ -0,0 +1,34 @@
+export const baseResponse = {
+  completions: [
+    {
+      expression: 'coveo',
+      score: 100,
+      executableConfidence: 1.0,
+      highlighted: '[coveo]',
+    },
+    {
+      expression: 'coveo platform',
+      score: 95,
+      executableConfidence: 0.95,
+      highlighted: '[coveo] platform',
+    },
+    {
+      expression: 'coveo search',
+      score: 90,
+      executableConfidence: 0.9,
+      highlighted: '[coveo] search',
+    },
+    {
+      expression: 'coveo ui kit',
+      score: 85,
+      executableConfidence: 0.85,
+      highlighted: '[coveo] ui kit',
+    },
+    {
+      expression: 'coveo headless',
+      score: 80,
+      executableConfidence: 0.8,
+      highlighted: '[coveo] headless',
+    },
+  ],
+};
