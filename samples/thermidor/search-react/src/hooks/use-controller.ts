240d41dfeaaa108a5c60c973ed021133df266f26
jfcere-coveo <jfcere@coveo.com>
2026-07-15 15:00:12 -0400

    add getServerSnapshot arg and remove subscription wrapper in useController

--- samples/thermidor/search-react/src/hooks/use-controller.ts
+++ samples/thermidor/search-react/src/hooks/use-controller.ts
@@ -3,7 +3,8 @@ import type {Controller} from '@coveo/thermidor';
 
 export function useController<T>(controller: Controller<T>): T {
   return useSyncExternalStore(
-    (onStoreChange) => controller.subscribe(() => onStoreChange()),
+    controller.subscribe,
+    () => controller.state,
     () => controller.state
   );
 }
