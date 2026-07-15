a0b4e704722e8718fcea634db59f5cdddeaca425
fbeaudoincoveo <fbeaudoin@coveo.com>
2026-07-15 14:15:46 -0400

    Apply the useController with useSyncExternalStore pattern

---
+++ samples/thermidor/generative-react/src/hooks/use-controller.ts
@@ -0,0 +1,9 @@
+import {useSyncExternalStore} from 'react';
+import type {Controller} from '@coveo/thermidor';
+
+export function useController<T>(controller: Controller<T>): T {
+  return useSyncExternalStore(
+    (onStoreChange) => controller.subscribe(() => onStoreChange()),
+    () => controller.state
+  );
+}
