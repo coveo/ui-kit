810111ba355e598e1e7227312906f04f89e29c14
Frederic Beaudoin <fbeaudoin@coveo.com>
2026-07-21 16:11:03 -0400

    feat(thermidor): add converse search thunk and facade resolver

---
+++ packages/thermidor/src/internal/api/converse-search/converse-search-facade.ts
@@ -0,0 +1,12 @@
+import type {
+  FacadeResolverFactory,
+  InterfaceHandle,
+} from '@/src/internal/utils/index.js';
+import {createConverseSearchEndpointThunk} from './converse-search-thunk.js';
+
+export function createConverseSearchFacadeResolver(
+  generativeInterface: InterfaceHandle
+): FacadeResolverFactory {
+  return (engine) => (scope) =>
+    createConverseSearchEndpointThunk(engine, scope, generativeInterface);
+}
