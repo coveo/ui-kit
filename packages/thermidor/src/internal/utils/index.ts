73a89d2ef5d9638654aeb41ec8bdd62b4e9e7930
Frederic Beaudoin <fbeaudoin@coveo.com>
2026-07-31 09:10:10 -0400

    Add missing export

--- packages/thermidor/src/internal/utils/index.ts
+++ packages/thermidor/src/internal/utils/index.ts
@@ -28,10 +28,10 @@ export type {
   InterfaceHandle,
   InterfaceRegistry,
   InterfaceType,
+  InterfaceTypeBrand,
   InterfaceTypeMap,
   SearchInterface,
   Supports,
   SupportsBrand,
 } from './interface-types.js';
-export {InterfaceTypeBrand} from './interface-types.js';
 export {createNoopThunk} from './noop-thunk.js';
